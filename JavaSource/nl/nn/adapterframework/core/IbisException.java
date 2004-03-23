975c175ff11e92398cd35600bbf9d3123d84115e
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-03-23 16:48:17 +0000

    modified getMessage() to avoid endless-loop

--- JavaSource/nl/nn/adapterframework/core/IbisException.java
+++ JavaSource/nl/nn/adapterframework/core/IbisException.java
@@ -1,5 +1,13 @@
+/*
+ * $Log: IbisException.java,v $
+ * Revision 1.4  2004-03-23 16:48:17  L190409
+ * modified getMessage() to avoid endless-loop
+ *
+ */
 package nl.nn.adapterframework.core;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.commons.lang.exception.NestableException;
 
@@ -7,11 +15,11 @@ import org.apache.commons.lang.exception.NestableException;
  * Base Exception with exhaustive toString() and compact getMessage()
  * @author Gerrit van Brakel
  * 
- * <p>$Id: IbisException.java,v 1.3 2004-03-23 15:25:00 NNVZNL01#L180564 Exp $</p>
+ * <p>$Id: IbisException.java,v 1.4 2004-03-23 16:48:17 L190409 Exp $</p>
  *
  */
 public class IbisException extends NestableException {
-		public static final String version="$Id: IbisException.java,v 1.3 2004-03-23 15:25:00 NNVZNL01#L180564 Exp $";
+		public static final String version="$Id: IbisException.java,v 1.4 2004-03-23 16:48:17 L190409 Exp $";
 
 public IbisException() {
 	super();
@@ -41,26 +49,22 @@ public IbisException(Throwable arg1) {
 		    }
 	    }
 	    if (result==null) {
-		    result=toString();
+	    	// do not replace the following with toString(), this causes an endless loop. GvB
+		    result="no message, fields of this exception: "+ToStringBuilder.reflectionToString(this,ToStringStyle.MULTI_LINE_STYLE);
 	    }
 	   
 	    return result;
     }
-    
-    
-/**
- * Retrieves the messages of all exceptions
- */
-public String toString() {
+
+	public String toString() {
 		String result="";
 		Throwable t;
 		String additionalMethodNames[] = { "getLinkedException" };
  
 		for (t=this; t!=null; t=ExceptionUtils.getCause(t,additionalMethodNames)) {
-//			result += "\nclass: "+t.getClass().getName()+"\nmessage:"+t.getMessage()+"\nfields:\n"+ToStringBuilder.reflectionToString(t,ToStringStyle.MULTI_LINE_STYLE)+"\n";
 			result += "\nclass: "+t.getClass().getName()+"\nmessage:"+t.getMessage()+"\n";
+//			result += "fields:\n"+ToStringBuilder.reflectionToString(t,ToStringStyle.MULTI_LINE_STYLE)+"\n";
 		}
-		
-	return result;
- }
+		return result;
+	}
 }
