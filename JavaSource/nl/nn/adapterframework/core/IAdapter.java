d1e2d12cf42c262a0c73f1e41b6f212b10ec6854
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-03-23 17:36:58 +0000

    added methods for Transaction control

--- JavaSource/nl/nn/adapterframework/core/IAdapter.java
+++ JavaSource/nl/nn/adapterframework/core/IAdapter.java
@@ -1,19 +1,27 @@
+/*
+ * $Log: IAdapter.java,v $
+ * Revision 1.3  2004-03-23 17:36:58  L190409
+ * added methods for Transaction control
+ *
+ */
 package nl.nn.adapterframework.core;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.util.MessageKeeper;
 
 import java.util.Iterator;
+
+import javax.transaction.UserTransaction;
 /**
  * The Adapter is the central manager in the framework. It has knowledge of both
  * <code>IReceiver</code>s as well as the <code>PipeLine</code> and statistics.
  * The Adapter is the class that is responsible for configuring, initializing and
  * accessing/activating IReceivers, Pipelines, statistics etc.
  * 
- * <p>$Id: IAdapter.java,v 1.2 2004-02-04 10:02:00 a1909356#db2admin Exp $</p>
+ * <p>$Id: IAdapter.java,v 1.3 2004-03-23 17:36:58 L190409 Exp $</p>
  **/
 public interface IAdapter extends IManagable {
-	public static final String version="$Id: IAdapter.java,v 1.2 2004-02-04 10:02:00 a1909356#db2admin Exp $";
+	public static final String version="$Id: IAdapter.java,v 1.3 2004-03-23 17:36:58 L190409 Exp $";
   /**
    * Instruct the adapter to configure itself. The adapter will call the
    * pipeline to configure itself, the pipeline will call the individual
@@ -26,12 +34,21 @@ public interface IAdapter extends IManagable {
   * The messagekeeper is used to keep the last x messages, relevant to
   * display in the web-functions.
   */ 
-public MessageKeeper getMessageKeeper();
-IReceiver getReceiverByName(String receiverName);
-	Iterator getReceiverIterator();
-public PipeLineResult processMessage(String correlationID, String message);
-  public void registerPipeLine (PipeLine pipeline) throws ConfigurationException;
-  public void setName(String name);
+	public MessageKeeper getMessageKeeper();
+	public IReceiver getReceiverByName(String receiverName);
+	public Iterator getReceiverIterator();
+	public PipeLineResult processMessage(String correlationID, String message);
+  	public void registerPipeLine (PipeLine pipeline) throws ConfigurationException;
+  	public void setName(String name);
+	public String toString();
+	
+	/**
+	 *  return the userTransaction object that can be used to demarcate (begin/commit/rollback) transactions
+	 */
+	public UserTransaction getUserTransaction() throws TransactionException;
+	/**
+	 *  return true when the current thread is running under a transaction.
+	 */
+	public boolean inTransaction() throws TransactionException;
 
-  public String toString();
 }
