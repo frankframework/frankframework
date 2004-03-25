73cb9abde331cc7b89895ee6487649c2450bdd61
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-03-25 13:36:07 +0000

    table exists via count(*) rather then via metadata

--- JavaSource/nl/nn/adapterframework/util/JdbcUtil.java
+++ JavaSource/nl/nn/adapterframework/util/JdbcUtil.java
@@ -1,6 +1,9 @@
 /*
  * $Log: JdbcUtil.java,v $
- * Revision 1.1  2004-03-23 17:16:14  L190409
+ * Revision 1.2  2004-03-25 13:36:07  L190409
+ * table exists via count(*) rather then via metadata
+ *
+ * Revision 1.1  2004/03/23 17:16:14  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * initial version
  *
  */
@@ -13,19 +16,31 @@ import java.sql.SQLException;
 
 /**
  * Database-oriented utility functions
- * <p>$Id: JdbcUtil.java,v 1.1 2004-03-23 17:16:14 L190409 Exp $</p>
+ * <p>$Id: JdbcUtil.java,v 1.2 2004-03-25 13:36:07 L190409 Exp $</p>
  * @author Gerrit van Brakel
  * @since  4.1
  */
 public class JdbcUtil {
-	public static final String version="$Id: JdbcUtil.java,v 1.1 2004-03-23 17:16:14 L190409 Exp $";
+	public static final String version="$Id: JdbcUtil.java,v 1.2 2004-03-25 13:36:07 L190409 Exp $";
+	
+	private static final boolean useMetaData=false;
 	/**
 	 * @return true if tableName exists in database in this connection
 	 */
 	public static boolean tableExists(Connection conn, String tableName ) throws SQLException {
+		
+		if (useMetaData) {
 			DatabaseMetaData dbmeta = conn.getMetaData();
 			ResultSet tableset = dbmeta.getTables(null, null, tableName, null);
-			
 			return !tableset.isAfterLast();
+		} else {
+			try {
+				String query="select count(*) from "+tableName;
+				conn.createStatement().executeQuery(query);
+				return true;
+			} catch (SQLException e) {
+				return false;
+			}
+		}
 	}
 }
