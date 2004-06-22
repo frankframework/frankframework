bc2406264064a6635e3ad4441189815e2a61d0c8
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-06-22 11:51:34 +0000

    added aliasFor attribute

--- JavaSource/nl/nn/adapterframework/util/GlobalListItem.java
+++ JavaSource/nl/nn/adapterframework/util/GlobalListItem.java
@@ -1,8 +1,10 @@
 /*
  * $Log: GlobalListItem.java,v $
- * Revision 1.1  2004-06-21 15:07:51  L190409
- * first version
+ * Revision 1.2  2004-06-22 11:51:34  L190409
+ * added aliasFor attribute
  *
+ * Revision 1.1  2004/06/21 15:07:51  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
+ * first version
  *
  */
 package nl.nn.adapterframework.util;
@@ -15,6 +17,7 @@ import java.util.TreeSet;
 
 import nl.nn.adapterframework.core.INamedObject;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.log4j.Logger;
 
@@ -22,17 +25,18 @@ import org.apache.log4j.Logger;
  * Base class for items of global lists.
  * The list itself is contained as a static field.
  * New items are registerd using registerItem().
- * Typical use: SapSystemFactory.getInstance().&lt;method to execute&gt;
+ * Typical use: SapSystem.getSystem(name).&lt;method to execute&gt;
  * <br/>
  * @version Id
  * @author Gerrit van Brakel
  */
 public class GlobalListItem implements INamedObject {
-	public static final String version="$Id: GlobalListItem.java,v 1.1 2004-06-21 15:07:51 L190409 Exp $";
-	protected Logger log = Logger.getLogger(this.getClass());;
+	public static final String version="$Id: GlobalListItem.java,v 1.2 2004-06-22 11:51:34 L190409 Exp $";
+	protected Logger log = Logger.getLogger(this.getClass());
 
     private static Hashtable items = new Hashtable();
     private String name;
+    private String aliasFor;
 
 	protected void configure() {
 	}
@@ -42,10 +46,20 @@ public class GlobalListItem implements INamedObject {
      * Get an item by Name.
      * Descender classes should implement a similar method, that returns an object of its own type.
      */
-    protected static Object getItem(String itemName) {
-        Object result = null;
+    protected static GlobalListItem getItem(String itemName) {
+		GlobalListItem result = null;
 
-		result = items.get(itemName);
+		result = (GlobalListItem)items.get(itemName);
+		if (result==null) {
+			throw new NullPointerException("no list item found for name ["+itemName+"]");
+		}
+		if (!StringUtils.isEmpty(result.getAliasFor())) {
+			String aliasName = result.getAliasFor();
+			result=getItem(aliasName);
+			if (result==null) {
+				throw new NullPointerException("no alias ["+aliasName+"] list item found for name ["+itemName+"] ");
+			}
+		}
         return result;
     }
     /**
@@ -93,6 +107,21 @@ public class GlobalListItem implements INamedObject {
 	 */
 	public void setName(String string) {
 		name = string;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getAliasFor() {
+		return aliasFor;
+	}
+
+	/**
+	 * If this attribute is set, the item is only an alias for another item.
+	 * @param string
+	 */
+	public void setAliasFor(String string) {
+		aliasFor = string;
 	}
 
 }
