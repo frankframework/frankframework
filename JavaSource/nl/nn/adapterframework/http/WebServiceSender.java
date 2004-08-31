6d1904e5387475cec4cd6c08846bdd79de670dcc
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-08-31 15:52:22 +0000

    new version based on HttpSender

--- JavaSource/nl/nn/adapterframework/http/WebServiceSender.java
+++ JavaSource/nl/nn/adapterframework/http/WebServiceSender.java
@@ -1,6 +1,9 @@
 /*
  * $Log: WebServiceSender.java,v $
- * Revision 1.3  2004-08-23 13:08:14  L190409
+ * Revision 1.4  2004-08-31 15:52:22  L190409
+ * new version based on HttpSender
+ *
+ * Revision 1.3  2004/08/23 13:08:14  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * updated JavaDoc
  *
  * Revision 1.2  2004/08/17 15:22:15  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -15,18 +18,18 @@ import nl.nn.adapterframework.core.ISender;
 import nl.nn.adapterframework.core.SenderException;
 import nl.nn.adapterframework.core.TimeOutException;
 import nl.nn.adapterframework.util.ClassUtils;
+import nl.nn.adapterframework.util.XmlUtils;
 
-import org.apache.axis.Message;
-import org.apache.axis.client.Call;
-import org.apache.axis.client.Service;
-import org.apache.axis.message.SOAPBody;
-import org.apache.axis.message.SOAPEnvelope;
 
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.log4j.Logger;
 
-import org.apache.soap.SOAPException;
+
+
 /*
 import org.apache.soap.Constants;
 import org.apache.soap.Fault;
@@ -37,21 +40,22 @@ import org.apache.soap.rpc.Response;
 import org.apache.soap.transport.http.SOAPHTTPConnection;
 */
 
-import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPMessage;
 
-
-import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.Vector;
 
-import javax.net.ssl.SSLSocketFactory;
-
-import java.security.KeyStore;
-import java.security.Security;
-import java.security.cert.Certificate;
+/*
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFault;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+*/
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 
 
 /**
@@ -60,249 +64,106 @@ import java.security.cert.Certificate;
  * 
  */
 
-public class WebServiceSender implements ISender, HasPhysicalDestination {
-	public static final String version="$Id: WebServiceSender.java,v 1.3 2004-08-23 13:08:14 L190409 Exp $";
-	protected Logger log = Logger.getLogger(this.getClass());
+public class WebServiceSender extends HttpSender {
+	public static final String version="$Id: WebServiceSender.java,v 1.4 2004-08-31 15:52:22 L190409 Exp $";
 	
 
-	private String name="";
-	private String endPointUrl= "";
 	private String soapActionURI = "";
 	private String encodingStyleURI=null;
-	private String methodName = "";
+//	private String methodName = "";
 	
-	private String proxyHost;
-	private int proxyPort=80;
-	private String proxyUserName;
-	private String proxyPassword;
-
-	private String userName; 
-	private String password; 
-
-	private String keystoreType="PKCS12";
-	private String certificate;
-	private String certificatePassword;
-
+	private Transformer extractBody;
+	private final static String extractBodyNamespaces="xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"";
+	private final static String extractBodyXPath="/soapenv:Envelope/soapenv:Body/*";
 	
-	private URL url = null;
-
-	private Service service;
-	private Call call;
-//	private SOAPMappingRegistry smr = call.getSOAPMappingRegistry();
+//	private MessageFactory soapMessageFactory;
 	
 	public String getLogPrefix() {
 		return "WebServiceSender ["+getName()+"] to ["+getPhysicalDestinationName()+"] ";
 	}
-  
-	public static void main(String args[]){
-		String kenteken="38JBRX";
-		// String kenteken="9494DA";
-		// String kenteken="BE2738";
-		String xmlmsg="<Abz-Audascan-M9><VT><VT_KENTEKEN>"+kenteken+"</VT_KENTEKEN></VT></Abz-Audascan-M9>";
-		try {
-			WebServiceSender ws = new WebServiceSender();
-			ws.name="testmans";
-			ws.endPointUrl="https://demo.abzportal.nl:443/demo2/isa-ws/services/isa3";
-			ws.soapActionURI="urn:isa3/geefAntwoord";
-			ws.methodName="geefAntwoord";
-			ws.proxyHost="proxy.rsc1.ing-int";
-			ws.proxyPort=8080;
-			ws.proxyUserName="190409";
-			ws.proxyPassword="BzZYeMP";
-			ws.certificate="E:/Gerrit/KeyStoring/999023.0000000002.abc";
-			ws.certificatePassword="isa2demo";
-			ws.keystoreType="pkcs12";
-		
-			System.out.println("configuring...");
-			ws.configure();
-			ws.open();
-			System.out.println("sending msg ["+xmlmsg+"]...");
-			String result = ws.sendMessage("Abz-Audascan-M9",xmlmsg);
-			System.out.println("retrieved result:");
-			System.out.println(result);
-			System.out.println("ready.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-
-	protected void addProvider(String name) {
-		try {
-			Class clazz = Class.forName(name);
-			java.security.Security.addProvider((java.security.Provider)clazz.newInstance());
-		} catch (Throwable t) {
-			log.error("cannot add provider ["+name+"]");
-		}
-	}
+ 
+ 	public WebServiceSender() {
+ 		super();
+ 		setMethodType("POST");
+ 	}
+ 
 
 	public void configure() throws ConfigurationException {
-		if (StringUtils.isEmpty(getEndPointUrl())) {
-			throw new ConfigurationException(getLogPrefix()+" endPointUrl must be specified");
-		}
-		System.setProperty("http.proxyHost",getProxyHost());
-		System.setProperty("http.proxyPort",new Integer(getProxyPort()).toString());
-		System.setProperty("http.proxyUser",getProxyUserName());
-		System.setProperty("http.proxyPassword",getProxyPassword());
-		System.setProperty("javax.net.ssl.keyStore",getCertificate());
-		System.setProperty("javax.net.ssl.keyStorePassword",getCertificatePassword());
-		System.setProperty("javax.net.ssl.keyStoreType",getKeystoreType());
-//		System.setProperty("javax.net.ssl.trustStore","E:/Gerrit/KeyStoring/cacerts");
-//		System.setProperty("javax.net.ssl.trustStorePassword","atricom");
-		System.setProperty("java.protocol.handler.pkgs","com.sun.net.ssl.internal.www.protocol");
-
-		addProvider("sun.security.provider.Sun");
-		addProvider("com.sun.net.ssl.internal.ssl.Provider");
-//		addProvider("com.sun.rsajca.Provider");
-
+		super.configure();
 		try {
-			setEndPoint(new URL(getEndPointUrl()));
-		} catch (MalformedURLException e) {
-			throw new ConfigurationException(getLogPrefix()+"could not set endPointUrl ["+getEndPointUrl()+"]", e);
+			extractBody = XmlUtils.createXPathEvaluator(extractBodyNamespaces,extractBodyXPath,"xml");
+		} catch (TransformerConfigurationException e) {
+			throw new ConfigurationException("cannot create transformer from expression ["+extractBodyXPath+"]",e);
 		}
-
-		try {
-			service = new Service();
-			call = (Call) service.createCall();
-
-//			call.setOperation(_operations[0]);
-			call.setUseSOAPAction(true);
-			call.setSOAPActionURI(getSoapActionURI());
-			call.setEncodingStyle(getEncodingStyleURI());
-			call.setProperty(org.apache.axis.client.Call.SEND_TYPE_ATTR, Boolean.FALSE);
-			call.setProperty(org.apache.axis.AxisEngine.PROP_DOMULTIREFS, Boolean.FALSE);
-			call.setSOAPVersion(org.apache.axis.soap.SOAPConstants.SOAP11_CONSTANTS);
-			call.setTargetEndpointAddress( getEndPoint() );
-			call.setOperationName(new QName("http://soapinterop.org/", getMethodName()));
-
-		} catch (Exception e) {
-			throw new ConfigurationException("exception in configuring webservice Call",e);
-		}
-		
-		
-		try {
-/*			SOAPHTTPConnection connection = new SOAPHTTPConnection();
-			call.setSOAPTransport(connection);
-			
-			if (!StringUtils.isEmpty(getProxyHost())) {
-				log.info(getLogPrefix()+" setting proxy ["+getProxyHost()+":"+getProxyPort()+"] proxy user ["+getProxyUserName()+"]");
-				connection.setProxyHost(getProxyHost());
-				connection.setProxyPort(getProxyPort());
-				connection.setProxyUserName(getProxyUserName());
-				connection.setProxyPassword(getProxyPassword());
-			}
-			if (!StringUtils.isEmpty(getUserName())) {
-				connection.setUserName(getUserName());
-				connection.setPassword(getPassword());
-			}
-*/	  	} catch (Exception e) {
-			throw new ConfigurationException("exception in configuring proxy or user settings",e);
-		}
-/*
-		try {
-	  		
-			keystore = KeyStore.getInstance(keystoreType);
-			URL url = ClassUtils.getResourceURL(this,certificate);
-			InputStream stream = url.openStream();
-			keystore.load(stream,certificatePassword.toCharArray());
-			stream.close();
-			Enumeration aliases = keystore.aliases();
-			while (aliases.hasMoreElements()) {
-				log.info("keystore alias:"+aliases.nextElement());
-			}
-		} catch (Exception e) {
-			throw new ConfigurationException("exception in configuring keystore",e);
-		}
-*/	  		
-	}
-
-	public void open() throws SenderException {
-	}
-
-	public void close() throws SenderException {
-	}
-
-	public boolean isSynchronous() {
-		return true;
-	}
-
-	public synchronized String sendMessage(String correlationID, String message) throws SenderException, TimeOutException {
-	
-		SOAPEnvelope resultmsg; 
-
-		String soapmsg= 
-			"<soapenv:Envelope " + 
-				"xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-				"<soapenv:Body>" +						message +
-				"</soapenv:Body>"+
-			"</soapenv:Envelope>";
-			
-		Message msg = new Message(soapmsg);
-		String result;
-
-		try {
-			log.debug(getLogPrefix()+"invoking call ["+call+"] with msg ["+msg.getSOAPPartAsString()+"]");
-			log.debug(getLogPrefix()+"invoking call to endpoint ["+getEndPoint()+"] action ["+getSoapActionURI()+"]");
-			resultmsg = call.invoke( msg );
-			log.debug(getLogPrefix()+"retrieved result: "+resultmsg.getAsString());
-		} catch (Throwable t) {
-			throw new SenderException(getLogPrefix()+"cannot send message", t);
-		}
-		
-		// Check the response.
 /*		
-		if (resultmsg. generatedFault()) {
-	    	
-		  Fault fault = resp.getFault();
-	
-		  throw new SenderException(getLogPrefix()+" problems processing message, faultcode ["+fault.getFaultCode()+"]: "+ fault.getFaultString());
-		} else {
-		  Parameter retValue = resp.getReturnValue();
-		  return (String)retValue.getValue();
+		try {
+			soapMessageFactory = MessageFactory.newInstance();
+		} catch (SOAPException e) {
+			throw new ConfigurationException("cannot create SoapMessageFactory",e);
 		}
-*/
-		try { 		
-			result = resultmsg.getAsString();
-			return result;
-		} catch (Throwable t) {
-			throw new SenderException(getLogPrefix()+"cannot convert response", t);
+*/		
+	}
+
+	protected HttpMethod getMethod(String message) throws SenderException {
+		String soapmsg= 
+		"<soapenv:Envelope " + 
+			"xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+			"<soapenv:Body>" +	
+				message +
+			"</soapenv:Body>"+
+		"</soapenv:Envelope>";
+
+		HttpMethod method = super.getMethod(soapmsg);
+		method.addRequestHeader("SOAPAction",getSoapActionURI());
+		return method;
+	}
+
+	public String extractResult(HttpMethod httpmethod) throws SenderException {
+		String httpResult = super.extractResult(httpmethod);
+		
+		String result;
+		try {
+			result = XmlUtils.transformXml(extractBody, httpResult);
+		} catch (Exception e) {
+			throw new SenderException("cannot retrieve result message",e);
 		}
+		return (result);
 	}
 
 
-	public String getPhysicalDestinationName() {
-		return (url==null) ? (">"+getEndPointUrl()+"<") : getEndPoint().toString();
-//		return "unkown";
+/*
+	public String extractResult_1(HttpMethod httpmethod) throws SenderException {
+		SOAPMessage message;
+		SOAPBody body;
+		try {
+			message = soapMessageFactory.createMessage(null,httpmethod.getResponseBodyAsStream());
+			body = message.getSOAPBody();
+		} catch (Exception e) {
+			throw new SenderException("cannot create result message object",e);
+		}
+		if (body.hasFault()) {
+			SOAPFault fault = body.getFault();
+			throw new SenderException("SOAP Fault, code["+fault.getFaultCode()+"] actor["+fault.getFaultActor()+"]  string ["+fault.getFaultString()+"] string ["+ToStringBuilder.reflectionToString(fault.getDetail())+"]");
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		try {
+			message.writeTo(out);
+		} catch (Exception e) {
+			throw new SenderException("cannot write result message object to stream",e);
+		}
+		return (out.toString());
 	}
+*/	
 
-	public synchronized URL getEndPoint() {
-		return url;
-	}
-  	
-	protected synchronized void setEndPoint(URL url) {
-		this.url = url;
-	}
+
+
 
 
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
 
-
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMethodName() {
-		return methodName;
-	}
-	public void setMethodName(String methodName) {
-		this.methodName = methodName;
-	}
 
 	public String getSoapActionURI() {
 		return soapActionURI;
@@ -311,12 +172,6 @@ public class WebServiceSender implements ISender, HasPhysicalDestination {
 		this.soapActionURI = soapActionURI;
 	}
 
-	public String getEndPointUrl() {
-		return endPointUrl;
-	}
-	public void setEndPointUrl(String endPointUrl) {
-		this.endPointUrl = endPointUrl;
-	}
 
 	public String getEncodingStyleURI() {
 		return encodingStyleURI;
@@ -327,73 +182,6 @@ public class WebServiceSender implements ISender, HasPhysicalDestination {
 	}
 
 
-	public String getPassword() {
-		return password;
-	}
 
-	public String getProxyHost() {
-		return proxyHost;
-	}
-
-	public String getProxyPassword() {
-		return proxyPassword;
-	}
-
-	public int getProxyPort() {
-		return proxyPort;
-	}
-
-	public String getProxyUserName() {
-		return proxyUserName;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-	public void setPassword(String string) {
-		password = string;
-	}
-
-	public void setProxyHost(String string) {
-		proxyHost = string;
-	}
-	public void setProxyPassword(String string) {
-		proxyPassword = string;
-	}
-
-	public void setProxyPort(int proxyPort) {
-		this.proxyPort = proxyPort;
-	}
-
-	public void setProxyUserName(String string) {
-		proxyUserName = string;
-	}
-	public void setUserName(String string) {
-		userName = string;
-	}
-
-	public String getCertificate() {
-		return certificate;
-	}
-
-	public String getCertificatePassword() {
-		return certificatePassword;
-	}
-
-	public String getKeystoreType() {
-		return keystoreType;
-	}
-
-	public void setCertificate(String string) {
-		certificate = string;
-	}
-
-	public void setCertificatePassword(String string) {
-		certificatePassword = string;
-	}
-
-	public void setKeystoreType(String string) {
-		keystoreType = string;
-	}
 
 }
