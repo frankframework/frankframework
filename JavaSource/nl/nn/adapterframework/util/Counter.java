8c21af0d8ddebf5a4d3b4ea8fdc6080df56eea0c
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-09-07 08:44:09 +0000

    added increase and decrease with amount-argument

--- JavaSource/nl/nn/adapterframework/util/Counter.java
+++ JavaSource/nl/nn/adapterframework/util/Counter.java
@@ -1,22 +1,22 @@
+/*
+ * $Log: Counter.java,v $
+ * Revision 1.4  2004-09-07 08:44:09  L190409
+ * added increase and decrease with amount-argument
+ *
+ */
 package nl.nn.adapterframework.util;
 
 /**
- * Synchronized Counter
+ * Synchronized Counter.
+ * 
  * @version Id
- *
  * @author Gerrit van Brakel
  */
 public class Counter {
-	public static final String version="$Id: Counter.java,v 1.3 2004-03-26 10:42:42 NNVZNL01#L180564 Exp $";
+	public static final String version="$Id: Counter.java,v 1.4 2004-09-07 08:44:09 L190409 Exp $";
 	
 	private long value = 0 ;
 
-/**
- * Counter constructor comment.
- */
-public Counter() {
-	super();
-}
 	public Counter(int startValue) {
 		super();
 		value = startValue;
@@ -24,10 +24,16 @@ public Counter() {
 	public synchronized long decrease() {
 		return --value;
 	}
+	public synchronized long decrease(long amount) {
+		return value-=amount;
+	}
 	public synchronized long getValue() {
 		return value;
 	}
 	public synchronized long increase() {
 		return ++value;
+	}
+	public synchronized long increase(long amount) {
+		return value+=amount;
 	}
 }
