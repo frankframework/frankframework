9bb580d6b2e6e841ba2df3e593c0ba12e5b573b1
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-10-12 15:09:13 +0000

    only nested map and list

--- JavaSource/nl/nn/adapterframework/parameters/ParameterValueList.java
+++ JavaSource/nl/nn/adapterframework/parameters/ParameterValueList.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ParameterValueList.java,v $
- * Revision 1.1  2004-10-05 09:52:25  L190409
+ * Revision 1.2  2004-10-12 15:09:13  L190409
+ * only nested map and list
+ *
+ * Revision 1.1  2004/10/05 09:52:25  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * moved parameter code  to package parameters
  *
  * Revision 1.1  2004/05/21 07:58:47  unknown <unknown@ibissource.org>
@@ -10,6 +13,7 @@
 package nl.nn.adapterframework.parameters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * List of parametervalues.
@@ -17,17 +21,42 @@ import java.util.ArrayList;
  * @author Gerrit van Brakel
  * @version Id
  */
-public class ParameterValueList extends ArrayList {
+public class ParameterValueList {
+	
+	ArrayList list;
+	HashMap   map;
 	
 	public ParameterValueList() {
 		super();
+		list = new ArrayList();
+		map  = new HashMap();
 	}
 
 	public ParameterValueList(int i) {
-		super(i);
+		super();
+		list = new ArrayList(i);
+		map  = new HashMap();
+	}
+	
+	public void add(ParameterValue pv) {
+		list.add(pv);
+		map.put(pv.getDefinition().getName(),pv);
 	}
 	
 	public ParameterValue getParameterValue(int i) {
-		return (ParameterValue)get(i);
+		return (ParameterValue)(list.get(i));
 	}
+
+	public ParameterValue getParameterValue(String name) {
+		return (ParameterValue)(map.get(name));
+	}
+	
+	public boolean parameterExists(String name) {
+		return map.get(name)!=null;
+	}
+
+	public int size() {
+		return list.size();
+	}
+
 }
