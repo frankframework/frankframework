98e29006000580ecbf3334f218515e79a69d2661
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-10-12 15:07:26 +0000

    added configure()-method

--- JavaSource/nl/nn/adapterframework/parameters/ParameterList.java
+++ JavaSource/nl/nn/adapterframework/parameters/ParameterList.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ParameterList.java,v $
- * Revision 1.1  2004-10-05 09:52:25  L190409
+ * Revision 1.2  2004-10-12 15:07:26  L190409
+ * added configure()-method
+ *
+ * Revision 1.1  2004/10/05 09:52:25  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * moved parameter code  to package parameters
  *
  * Revision 1.1  2004/05/21 07:58:47  unknown <unknown@ibissource.org>
@@ -10,6 +13,9 @@
 package nl.nn.adapterframework.parameters;
 
 import java.util.ArrayList;
+
+import nl.nn.adapterframework.configuration.ConfigurationException;
+
 
 /**
  * List of parameters.
@@ -25,6 +31,12 @@ public class ParameterList extends ArrayList {
 
 	public ParameterList(int i) {
 		super(i);
+	}
+	
+	public void configure() throws ConfigurationException {
+		for (int i=0; i<size(); i++) {
+			getParameter(i).configure();
+		}
 	}
 	
 	public Parameter getParameter(int i) {
