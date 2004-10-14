5b114fb4e60cfd359d16ce34fa902e3051c9b396
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2004-10-14 15:36:25 +0000

    added includeXmlDeclaration-attribute

--- JavaSource/nl/nn/adapterframework/pipes/Text2XmlPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/Text2XmlPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: Text2XmlPipe.java,v $
- * Revision 1.2  2004-04-27 11:42:40  a1909356#db2admin
+ * Revision 1.3  2004-10-14 15:36:25  L190409
+ * added includeXmlDeclaration-attribute
+ *
+ * Revision 1.2  2004/04/27 11:42:40  unknown <unknown@ibissource.org>
  * Access properties via getters
  *
  * Revision 1.1  2004/04/27 10:51:34  unknown <unknown@ibissource.org>
@@ -23,6 +26,7 @@ import org.apache.commons.lang.StringUtils;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>{@link #setXmlTag(String) xmlag}</td><td>the xml tag to encapsulate the text in</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setIncludeXmlDeclaration(boolean) includeXmlDeclaration}</td><td>controls whether a declation is included above the Xml text</td><td>true</td></tr>
  * </table>
  * </p>
  * <p><b>Exits:</b>
@@ -38,6 +42,7 @@ import org.apache.commons.lang.StringUtils;
  */
 public class Text2XmlPipe extends FixedForwardPipe {
 	private String xmlTag;
+	private boolean includeXmlDeclaration = true;
 	
 	/** 
 	 * @see nl.nn.adapterframework.core.IPipe#configure()
@@ -54,7 +59,8 @@ public class Text2XmlPipe extends FixedForwardPipe {
 	 * @see nl.nn.adapterframework.core.IPipe#doPipe(Object, PipeLineSession)
 	 */
 	public PipeRunResult doPipe(Object input, PipeLineSession session) throws PipeRunException {
-		String resultString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><" + getXmlTag() + "><![CDATA[" + input + "]]></" + xmlTag + ">";	
+		String resultString = (isIncludeXmlDeclaration()?"<?xml version=\"1.0\" encoding=\"UTF-8\"?>":"") +
+		"<" + getXmlTag() + "><![CDATA[" + input + "]]></" + xmlTag + ">";	
 		return new PipeRunResult(getForward(), resultString);
 	}
 
@@ -70,6 +76,14 @@ public class Text2XmlPipe extends FixedForwardPipe {
 	 */
 	public void setXmlTag(String xmlTag) {
 		this.xmlTag = xmlTag;
+	}
+
+	public boolean isIncludeXmlDeclaration() {
+		return includeXmlDeclaration;
+	}
+
+	public void setIncludeXmlDeclaration(boolean b) {
+		includeXmlDeclaration = b;
 	}
 
 }
