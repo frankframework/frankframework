2b985f6180eaafb0cb027d63295af570d253fb8a
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-05-31 09:39:09 +0000

    using stringToSource to obtain a source for XSLT; cached this source

--- JavaSource/nl/nn/adapterframework/util/Variant.java
+++ JavaSource/nl/nn/adapterframework/util/Variant.java
@@ -1,11 +1,18 @@
+/*
+ * $Log: Variant.java,v $
+ * Revision 1.4  2005-05-31 09:39:09  europe\L190409
+ * using stringToSource to obtain a source for XSLT; cached this source
+ *
+ */
 package nl.nn.adapterframework.util;
 
 import org.xml.sax.InputSource;
 
 import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.dom.DOMSource;
 import java.io.Reader;
 import java.io.StringReader;
+
 /**
  * Class to handle all kinds of conversions.
  * @version Id
@@ -13,41 +20,44 @@ import java.io.StringReader;
  * @author Gerrit van Brakel IOS
  */
 public class Variant {
-	public static final String version="$Id: Variant.java,v 1.3 2004-03-26 10:42:39 NNVZNL01#L180564 Exp $";
+	public static final String version = "$RCSfile: Variant.java,v $ $Revision: 1.4 $ $Date: 2005-05-31 09:39:09 $";
+
+	private String data = null;
+	private DOMSource dataAsDOMSource = null;
 	
-	private String data=null;
-public Variant(Object obj) {
-	this(obj.toString());
-}
-public Variant(String obj) {
-    super();
-    data = obj;
-}
-public Reader asReader() {
-	return new StringReader(data);
-}
-/**
- * Insert the method's description here.
- * Creation date: (25-02-2003 16:20:38)
- * @return java.lang.String
- */
-public String asString() {
-	return data;
-}
-public InputSource asXmlInputSource() {
+	public Variant(Object obj) {
+		this(obj.toString());
+	}
+	public Variant(String obj) {
+		super();
+		data = obj;
+	}
+	public Reader asReader() {
+		return new StringReader(data);
+	}
+	public String asString() {
+		return data;
+	}
+	
+	/**
+	 * Renders an InputSource for SAX parsing
+	 */
+	public InputSource asXmlInputSource() {
 
-    StringReader sr;
+		StringReader sr;
 
-    sr = new StringReader(data);
+		sr = new StringReader(data);
 
-    return new InputSource(sr);
-}
-public Source asXmlSource() {
+		return new InputSource(sr);
+	}
 
-    StringReader sr;
-
-    sr = new StringReader(data);
-
-    return new StreamSource(sr);
-}
+	/**
+	 * Renders a Source for XSLT-transformation
+	 */
+	public Source asXmlSource() throws DomBuilderException {
+		if (dataAsDOMSource==null) {
+			dataAsDOMSource = (DOMSource)XmlUtils.stringToSource(data,true);
+		}
+		return dataAsDOMSource;
+	}
 }
