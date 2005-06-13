b4518f5df80c3658dba82c0e144a6666f5116caa
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-06-13 12:30:45 +0000

    ifsa 2.2 compatibilty connection

--- JavaSource/nl/nn/adapterframework/extensions/ifsa/IfsaConnectionFactory.java
+++ JavaSource/nl/nn/adapterframework/extensions/ifsa/IfsaConnectionFactory.java
@@ -1,6 +1,9 @@
 /*
  * $Log: IfsaConnectionFactory.java,v $
- * Revision 1.1  2005-05-03 15:58:50  L190409
+ * Revision 1.2  2005-06-13 12:30:45  europe\L190409
+ * ifsa 2.2 compatibilty connection
+ *
+ * Revision 1.1  2005/05/03 15:58:50  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * rework of shared connection code
  *
  * Revision 1.2  2005/04/26 15:16:07  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -26,6 +29,7 @@ import nl.nn.adapterframework.jms.JmsConnection;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.log4j.Logger;
 
+import com.ing.ifsa.IFSAConstants;
 import com.ing.ifsa.IFSAContext;
 import com.ing.ifsa.IFSAQueueConnectionFactory;
 
@@ -39,11 +43,11 @@ import com.ing.ifsa.IFSAQueueConnectionFactory;
  * @version Id
  */
 public class IfsaConnectionFactory extends ConnectionFactoryBase {
-	public static final String version="$Id: IfsaConnectionFactory.java,v 1.1 2005-05-03 15:58:50 L190409 Exp $";
+	public static final String version="$RCSfile: IfsaConnectionFactory.java,v $ $Revision: 1.2 $ $Date: 2005-06-13 12:30:45 $";
 	protected Logger log = Logger.getLogger(this.getClass());
 
 	private final static String IFSA_INITIAL_CONTEXT_FACTORY="com.ing.ifsa.IFSAContextFactory";
-	private final static String IFSA_PROVIDER_URL="IFSA APPLICATION BUS";
+	private final static String IFSA_PROVIDER_URL_V2_0="IFSA APPLICATION BUS";
 	
 	static private HashMap connectionMap = new HashMap();
 
@@ -60,7 +64,15 @@ public class IfsaConnectionFactory extends ConnectionFactoryBase {
 	protected Context createContext() throws NamingException {
 		Hashtable env = new Hashtable(11);
 		env.put(Context.INITIAL_CONTEXT_FACTORY, IFSA_INITIAL_CONTEXT_FACTORY);
-		env.put(Context.PROVIDER_URL, IFSA_PROVIDER_URL);
+		String providerUrl=null;
+		try {
+			providerUrl=IFSAConstants.IFSA_BAICNF;
+		} catch (Throwable t) {
+			log.info("Caught Throwable of type ["+t.getClass().getName()+"], assuming Constant IFSAConstants.IFSA_BAICNF cannot not be found, assuming IFSA version 2.0");
+			providerUrl=IFSA_PROVIDER_URL_V2_0;
+		}
+		log.info("using ifsa provider URL ["+providerUrl+"]");
+		env.put(Context.PROVIDER_URL, providerUrl);
 		// Create context as required by IFSA 2.0. Ignore the deprecation....
 		return new IFSAContext((Context) new InitialContext(env));
 	}
