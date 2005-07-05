7fd36e588bc91d7a61d722ed98cc4da8cae6e79c
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-07-05 11:04:57 +0000

    updated javadoc

--- JavaSource/nl/nn/adapterframework/core/IPushingListener.java
+++ JavaSource/nl/nn/adapterframework/core/IPushingListener.java
@@ -1,6 +1,9 @@
 /*
  * $Log: IPushingListener.java,v $
- * Revision 1.3  2004-09-08 14:15:11  L190409
+ * Revision 1.4  2005-07-05 11:04:57  europe\L190409
+ * updated javadoc
+ *
+ * Revision 1.3  2004/09/08 14:15:11  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * removed unused imports
  *
  * Revision 1.2  2004/08/03 13:09:51  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -26,18 +29,20 @@ package nl.nn.adapterframework.core;
  * @since 4.2
  */
 public interface IPushingListener extends IListener {
-	public static final String version="$Id: IPushingListener.java,v 1.3 2004-09-08 14:15:11 L190409 Exp $";
+	public static final String version = "$RCSfile: IPushingListener.java,v $ $Revision: 1.4 $ $Date: 2005-07-05 11:04:57 $";
 
 
-/**
- * Set the handler that will do the processing of the message.
- * Each of the received messages must be pushed through handler.processMessage()
- */
-void setHandler(IMessageHandler handler);
-
-/**
- * Set a (single) listener that will be notified of any exceptions.
- */
-void setExceptionListener(IbisExceptionListener listener);
+	/**
+	 * Set the handler that will do the processing of the message.
+	 * Each of the received messages must be pushed through handler.processMessage()
+	 */
+	void setHandler(IMessageHandler handler);
+	
+	/**
+	 * Set a (single) listener that will be notified of any exceptions.
+	 * The listener should use this listener to notify the receiver of 
+	 * any exception that occurs outside the processing of a message.
+	 */
+	void setExceptionListener(IbisExceptionListener listener);
 
 }
