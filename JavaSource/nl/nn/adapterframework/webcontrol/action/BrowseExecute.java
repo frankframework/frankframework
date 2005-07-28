3ba21cda5bb4e2d542bffbbf1d8d67fef2bbe72a
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-07-28 07:44:52 +0000

    rework using primitive types only for parameters

--- JavaSource/nl/nn/adapterframework/webcontrol/action/BrowseExecute.java
+++ JavaSource/nl/nn/adapterframework/webcontrol/action/BrowseExecute.java
@@ -1,6 +1,9 @@
 /*
  * $Log: BrowseExecute.java,v $
- * Revision 1.1  2005-07-19 15:29:22  europe\L190409
+ * Revision 1.2  2005-07-28 07:44:52  europe\L190409
+ * rework using primitive types only for parameters
+ *
+ * Revision 1.1  2005/07/19 15:29:22  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * introduction of BrowseExcecute
  *
  */
@@ -17,9 +20,13 @@ import javax.transaction.UserTransaction;
 import nl.nn.adapterframework.core.Adapter;
 import nl.nn.adapterframework.core.IListener;
 import nl.nn.adapterframework.core.IMessageBrowser;
+import nl.nn.adapterframework.core.IMessageBrowsingIterator;
 import nl.nn.adapterframework.core.ListenerException;
 import nl.nn.adapterframework.receivers.ReceiverBase;
+import nl.nn.adapterframework.util.DateUtils;
 import nl.nn.adapterframework.util.RunStateEnum;
+import nl.nn.adapterframework.util.XmlBuilder;
+import nl.nn.adapterframework.util.XmlUtils;
 import nl.nn.adapterframework.webcontrol.FileViewerServlet;
 import nl.nn.adapterframework.webcontrol.IniDynaActionForm;
 
@@ -35,7 +42,7 @@ import org.apache.struts.action.ActionMapping;
  * @since   4.3
  */
 public class BrowseExecute extends ActionBase {
-	public static final String version="$RCSfile: BrowseExecute.java,v $ $Revision: 1.1 $ $Date: 2005-07-19 15:29:22 $";
+	public static final String version="$RCSfile: BrowseExecute.java,v $ $Revision: 1.2 $ $Date: 2005-07-28 07:44:52 $";
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
@@ -53,20 +60,6 @@ public class BrowseExecute extends ActionBase {
 		String receiverName = request.getParameter("receiverName");
 		String messageId = request.getParameter("messageId");
 
-    
-		IniDynaActionForm browserForm = (IniDynaActionForm) form;
-
-		if (StringUtils.isNotEmpty(action)) {browserForm.set("action",action);} 
-		if (StringUtils.isNotEmpty(adapterName)) {browserForm.set("adapterName",adapterName);}
-		if (StringUtils.isNotEmpty(receiverName)) {browserForm.set("receiverName",receiverName);}
-		if (StringUtils.isNotEmpty(messageId)) {browserForm.set("messageId",messageId);}
-		
-		adapterName = (String)browserForm.get("adapterName");
-		receiverName = (String)browserForm.get("receiverName");
-		messageId = (String)browserForm.get("messageId");
-
-		browserForm.set("title","Errorqueue of receiver ["+receiverName+"] of adapter ["+adapterName+"]");
-
 		//commandIssuedBy containes information about the location the
 		// command is sent from
 		String commandIssuedBy= getCommandIssuedBy(request);
@@ -76,12 +69,9 @@ public class BrowseExecute extends ActionBase {
 		Adapter adapter = (Adapter) config.getRegisteredAdapter(adapterName);
 		ReceiverBase receiver = (ReceiverBase) adapter.getReceiverByName(receiverName);
 
-		IMessageBrowser mb = (IMessageBrowser) browserForm.get("messageBrowser");
-		mb = receiver.getErrorStorage();
-		browserForm.set("messageBrowser",mb);
-		if ("showerrorqueue".equalsIgnoreCase(action)) {
-			// nothing special
-		} 
+		IMessageBrowser mb = receiver.getErrorStorage();
+		
+
 
 		if ("deletemessage".equalsIgnoreCase(action)) {
 			if (StringUtils.isNotEmpty(messageId)) {
@@ -133,17 +123,41 @@ public class BrowseExecute extends ActionBase {
 			}
 		} 
 
-		if ("showmessage".equalsIgnoreCase(action)) {
-			try {
+		try {
+			if ("showmessage".equalsIgnoreCase(action)) {
 				Object rawmsg = mb.browseMessage(messageId);
 				IListener listener = receiver.getListener();
 				String msg = listener.getStringFromRawMessage(rawmsg,null);
 				String type = request.getParameter("type");
 				FileViewerServlet.showReaderContents(new StringReader(msg),type,response);
-			} catch (ListenerException e) {
-				log.error(e);
-				throw new ServletException(e);
+			} else {
+				IMessageBrowsingIterator mbi=mb.getIterator();
+				try {
+					XmlBuilder messages=new XmlBuilder("messages");
+					messages.addAttribute("adapterName",java.net.URLEncoder.encode(adapterName));
+					messages.addAttribute("receiverName",java.net.URLEncoder.encode(receiverName));
+					messages.addAttribute("title","Errorqueue of receiver ["+receiverName+"] of adapter ["+adapterName+"]");
+					int messageCount;
+					for (messageCount=0; mbi.hasNext(); messageCount++) {
+						Object iterItem = mbi.next();
+						XmlBuilder message=new XmlBuilder("message");
+						message.addAttribute("id",mb.getId(iterItem));
+						message.addAttribute("pos",Integer.toString(messageCount+1));
+						message.addAttribute("originalId",mb.getOriginalId(iterItem));
+						message.addAttribute("correlationId",mb.getCorrelationId(iterItem));
+						message.addAttribute("insertDate",DateUtils.format(mb.getInsertDate(iterItem), DateUtils.FORMAT_DATETIME_MILLISECONDS));
+						message.addAttribute("comment",XmlUtils.encodeChars(mb.getCommentString(iterItem)));
+						messages.addSubElement(message);
+					}
+					messages.addAttribute("messageCount",Integer.toString(messageCount));
+					request.setAttribute("messages",messages.toXML());
+				} finally {
+					mbi.close();
+				}
 			}
+		} catch (ListenerException e) {
+			log.error(e);
+			throw new ServletException(e);
 		}
 		
 		if (!errors.isEmpty()) {
