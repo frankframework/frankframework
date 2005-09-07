747032ef0bcb255d85cee2a8357bd88bbd5ec68d
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-09-07 15:35:10 +0000

    introduction of IsolatedServiceCaller

---
+++ JavaSource/nl/nn/adapterframework/pipes/IsolatedServiceCaller.java
@@ -0,0 +1,71 @@
+/*
+ * $Log: IsolatedServiceCaller.java,v $
+ * Revision 1.1  2005-09-07 15:35:10  europe\L190409
+ * introduction of IsolatedServiceCaller
+ *
+ */
+package nl.nn.adapterframework.pipes;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+import nl.nn.adapterframework.core.ListenerException;
+import nl.nn.adapterframework.receivers.ServiceDispatcher;
+
+/**
+ * Helper class for IbisLocalSender that wraps around {@link ServiceDispatcher} to make calls to a local Ibis adapter in a separate thread.
+ * 
+ * @author  Gerrit van Brakel
+ * @since   4.3
+ * @version Id
+ */
+public class IsolatedServiceCaller extends Thread {
+	public static final String version="$RCSfile: IsolatedServiceCaller.java,v $ $Revision: 1.1 $ $Date: 2005-09-07 15:35:10 $";
+	protected Logger log = Logger.getLogger(this.getClass());
+
+	String serviceName;
+	String correlationID;
+	String message;
+	HashMap context;
+	String result = null;
+	Throwable t = null;
+	
+	
+	private IsolatedServiceCaller(String serviceName, String correlationID, String message, HashMap context) {
+		super();
+		this.serviceName= serviceName;
+		this.correlationID=correlationID;
+		this.message=message;
+		this.context=context;
+	}
+	
+	public static String callServiceIsolated(String serviceName, String correlationID, String message, HashMap context) throws ListenerException {
+		IsolatedServiceCaller call = new IsolatedServiceCaller(serviceName, correlationID,  message,  context);
+		call.start();
+		try {
+			call.join();
+		} catch (InterruptedException e) {
+			throw new ListenerException(e);
+		}
+
+		if (call.t!=null) {
+			if (call.t instanceof ListenerException) {
+				throw (ListenerException)call.t;
+			} else {
+				throw new ListenerException(call.t);
+			}
+		} else {
+			return call.result; 
+		}
+	}
+	
+	public void run() {
+		try {
+			result = ServiceDispatcher.getInstance().dispatchRequestWithExceptions(serviceName, correlationID, message, context);
+		} catch (Throwable t) {
+			this.t=t;
+		}
+	}
+
+}
