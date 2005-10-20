f1e99de2c671b6359f0564e2f45f7379d6c16897
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-10-20 15:20:27 +0000

    added feature to include directories

--- JavaSource/nl/nn/adapterframework/util/Dir2Xml.java
+++ JavaSource/nl/nn/adapterframework/util/Dir2Xml.java
@@ -1,6 +1,9 @@
 /*
  * $Log: Dir2Xml.java,v $
- * Revision 1.5  2005-07-19 11:04:09  europe\L190409
+ * Revision 1.6  2005-10-20 15:20:27  europe\L190409
+ * added feature to include directories
+ *
+ * Revision 1.5  2005/07/19 11:04:09  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * use explicit FileNameComparator
  *
  * Revision 1.4  2005/05/31 09:20:37  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -13,71 +16,76 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Arrays;
+
 /**
  * List the contents of a directory as XML.
- * @version Id
- *
+ * 
  * @author Johan Verrips IOS
+ * @version Id
  */
 public class Dir2Xml  {
-	public static final String version="$RCSfile: Dir2Xml.java,v $ $Revision: 1.5 $ $Date: 2005-07-19 11:04:09 $";
+	public static final String version="$RCSfile: Dir2Xml.java,v $ $Revision: 1.6 $ $Date: 2005-10-20 15:20:27 $";
 	
   	private String path;
   	private String wildcard="*.*";
   	
-	public Dir2Xml() {
-		super();
+	public String getDirList() {
+		return getDirList(false);
 	}
 	  
-  public String getDirList() {
-    WildCardFilter filter = new WildCardFilter(wildcard);
-    File dir = new File(path);
-    File files[] = dir.listFiles(filter);
-    Arrays.sort(files, new FileNameComparator());
-    int count=(files==null ? 0 : files.length);
-    XmlBuilder dirXml=new XmlBuilder("directory");
-    dirXml.addAttribute("name", path);
-    for (int i=0; i<count; i++ ) {
-	    File file=files[i];
-	    if (file.isDirectory()) {
-		    continue;
-	    }
-	    dirXml.addSubElement(getFileAsXmlBuilder(file));
-    }
-    return dirXml.toXML();
-  }
+	public String getDirList(boolean includeDirectories) {
+		WildCardFilter filter = new WildCardFilter(wildcard);
+		File dir = new File(path);
+		File files[] = dir.listFiles(filter);
+		Arrays.sort(files, new FileNameComparator());
+		int count = (files == null ? 0 : files.length);
+		XmlBuilder dirXml = new XmlBuilder("directory");
+		dirXml.addAttribute("name", path);
+		if (includeDirectories) {
+			dirXml.addSubElement(getFileAsXmlBuilder(dir.getParentFile(),".."));
+		}
+		for (int i = 0; i < count; i++) {
+			File file = files[i];
+			if (file.isDirectory() && !includeDirectories) {
+				continue;
+			}
+			dirXml.addSubElement(getFileAsXmlBuilder(file,file.getName()));
+		}
+		return dirXml.toXML();
+	}
   
-  private XmlBuilder getFileAsXmlBuilder(File file){
-	  
-	  XmlBuilder fileXml=new XmlBuilder("file");
-	  fileXml.addAttribute("name", file.getName());
-	  fileXml.addAttribute("size", ""+file.length());
-	  try {
-	  	fileXml.addAttribute("canonicalName", file.getCanonicalPath());
-	  } catch (IOException e) { }
-      // Get the modification date of the file
-      Date modificationDate=new Date(file.lastModified());
-	  //add date
-	  String date=DateUtils.format(modificationDate, DateUtils.FORMAT_DATE);
-	  fileXml.addAttribute("modificationDate", date);
-
-      // add the time
-      String time=DateUtils.format(modificationDate, DateUtils.FORMAT_TIME_HMS);
-      fileXml.addAttribute("modificationTime", time);
-      
-	  return fileXml;
-  }
-
-  public void setPath(String path) {
-    this.path=path;
-  }
-  
-  /**
-   * Set a Wildcard
-   * @see WildCardFilter
-   */
-  public void setWildCard(String wildcard) {
-    this.wildcard=wildcard;
-
-  }
+	private XmlBuilder getFileAsXmlBuilder(File file, String nameShown) {
+	
+		XmlBuilder fileXml = new XmlBuilder("file");
+		fileXml.addAttribute("name", nameShown);
+		fileXml.addAttribute("size", "" + file.length());
+		fileXml.addAttribute("directory", "" + file.isDirectory());
+		try {
+			fileXml.addAttribute("canonicalName", file.getCanonicalPath());
+		} catch (IOException e) {
+		}
+		// Get the modification date of the file
+		Date modificationDate = new Date(file.lastModified());
+		//add date
+		String date = DateUtils.format(modificationDate, DateUtils.FORMAT_DATE);
+		fileXml.addAttribute("modificationDate", date);
+	
+		// add the time
+		String time = DateUtils.format(modificationDate, DateUtils.FORMAT_TIME_HMS);
+		fileXml.addAttribute("modificationTime", time);
+	
+		return fileXml;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	/**
+	 * Set a Wildcard
+	 * @see WildCardFilter
+	 */
+	public void setWildCard(String wildcard) {
+		this.wildcard = wildcard;
+	
+	}
 }
