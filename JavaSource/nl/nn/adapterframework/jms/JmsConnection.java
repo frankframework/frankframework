494019797b21e7fd176994234d1a605f200e6bdf
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-10-20 15:42:10 +0000

    introduced JmsConnection special for real Jms connections

--- JavaSource/nl/nn/adapterframework/jms/JmsConnection.java
+++ JavaSource/nl/nn/adapterframework/jms/JmsConnection.java
@@ -1,153 +1,36 @@
 /*
  * $Log: JmsConnection.java,v $
- * Revision 1.3  2005-10-18 06:58:46  europe\L190409
- * corrected version string
- *
- * Revision 1.2  2005/10/18 06:57:55  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
- * close returns true when underlying connection is acually closed
- *
- * Revision 1.1  2005/05/03 15:59:55  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
- * rework of shared connection code
+ * Revision 1.4  2005-10-20 15:42:10  europe\L190409
+ * introduced JmsConnection special for real Jms connections
  *
  */
 package nl.nn.adapterframework.jms;
 
 import java.util.HashMap;
 
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.QueueConnection;
-import javax.jms.QueueConnectionFactory;
-import javax.jms.Session;
-import javax.jms.TopicConnection;
-import javax.jms.TopicConnectionFactory;
+import javax.jms.Destination;
 import javax.naming.Context;
-
-import nl.nn.adapterframework.core.IbisException;
-
-import org.apache.log4j.Logger;
+import javax.naming.NamingException;
 
 /**
- * Generic JMS connection, to be shared for JMS Objects that can use the same. 
+ * Wrapper around JMS connection objects.
  * 
- * @author Gerrit van Brakel
+ * @author 	Gerrit van Brakel
+ * @since   4.4
  * @version Id
  */
-public class JmsConnection  {
-	public static final String version="$RCSfile: JmsConnection.java,v $ $Revision: 1.3 $ $Date: 2005-10-18 06:58:46 $";
-	protected Logger log = Logger.getLogger(this.getClass());
-
-	private int referenceCount;
+public class JmsConnection extends ConnectionBase {
+	public static final String version="$RCSfile: JmsConnection.java,v $ $Revision: 1.4 $ $Date: 2005-10-20 15:42:10 $";
 	
-	private String id;
-	
-	private Context context = null;
-	private ConnectionFactory connectionFactory = null;
-	private Connection connection=null;
-	
-	private HashMap connectionMap;
-	
-	public 	JmsConnection(String id, Context context, ConnectionFactory connectionFactory, HashMap connectionMap) {
-		super();
-		referenceCount=0;
-		this.id=id;
-		this.context=context;
-		this.connectionFactory=connectionFactory;
-		this.connectionMap=connectionMap;
-		connectionMap.put(id, this);
-		log.debug("set id ["+id+"] context ["+context+"] connectionFactory ["+connectionFactory+"] ");
-	}
-		
-	public synchronized boolean close() throws IbisException
-	{
-		if (--referenceCount<=0) {
-			log.debug(getLogPrefix()+" reference count ["+referenceCount+"], closing connection");
-			connectionMap.remove(getId());
-			try {
-				if (connection != null) { 
-					connection.close();
-				}
-				if (context != null) {
-					context.close(); 
-				}
-			} catch (Exception e) {
-				throw new IbisException("exception closing connection", e);
-			} finally {
-				connectionFactory = null;
-				connection=null;
-				context = null;
-				return true;
-			}
-		} else {
-			log.debug(getLogPrefix()+" not closing, reference count ["+referenceCount+"]");
-			return false;
-		}
-	}
-
-	public synchronized void increaseReferences() {
-		referenceCount++;
+	public JmsConnection(String connectionFactoryName, Context context, ConnectionFactory connectionFactory, HashMap connectionMap) {
+		super(connectionFactoryName, context, connectionFactory, connectionMap);
 	}
 	
-	public synchronized void decreaseReferences() {
-		referenceCount--;
+	public Destination lookupDestination(String destinationName) throws NamingException {
+		Destination dest=null;
+		dest=(Destination) getContext().lookup(destinationName);
+		return dest;
 	}
-
-	public Context getContext() {
-		return context;
-	}
-
-	public ConnectionFactory getConnectionFactory() {
-		return connectionFactory;
-	}
-
 	
-	protected Connection createConnection() throws JMSException {
-		if (connectionFactory instanceof QueueConnectionFactory) {
-			return ((QueueConnectionFactory)connectionFactory).createQueueConnection();
-		} else {
-			return ((TopicConnectionFactory)connectionFactory).createTopicConnection();
-		}
-	}
-
-	protected Connection getConnection() throws IbisException {
-		if (connection == null) {
-			synchronized (this) {
-				if (connection == null) {
-					try {
-						connection = createConnection();
-						connection.start();
-					} catch (JMSException e) {
-						throw new IbisException("could not obtain Connection", e);
-					}
-				}
-			}
-		}
-		return connection;
-	}
-
-
-	public Session createSession(boolean transacted, int acknowledgeMode) throws IbisException {
-		Connection connection = getConnection();
-		try {
-			if (connection instanceof QueueConnection) {
-				return ((QueueConnection)connection).createQueueSession(transacted, acknowledgeMode);
-			} else {
-				return ((TopicConnection)connection).createTopicSession(transacted, acknowledgeMode);
-			}
-		} catch (JMSException e) {
-			throw new IbisException("could not create Session", e);
-		}
-	}
-
-	protected String getLogPrefix() {
-		return "["+getId()+"] "; 
-	}
-
-	public String getId() {
-		return id;
-	}
-
-
-
 }
