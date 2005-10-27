dceeb72261c78bddae7293949a42bed03a89947b
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-10-27 08:42:54 +0000

    *** empty log message ***

--- JavaSource/nl/nn/adapterframework/util/RunStateManager.java
+++ JavaSource/nl/nn/adapterframework/util/RunStateManager.java
@@ -1,27 +1,25 @@
+/*
+ * $Log: RunStateManager.java,v $
+ * Revision 1.5  2005-10-27 08:42:54  europe\L190409
+ * *** empty log message ***
+ *
+ */
 package nl.nn.adapterframework.util;
 
 import org.apache.log4j.Logger;
 
 /**
- * Utility class to support run-state management
+ * Utility class to support run-state management.
+ * 
  * @version Id
- *
- *
  * @author Gerrit van Brakel
  */
-public class RunStateManager {
-	public static final String version =
-		"$Id: RunStateManager.java,v 1.4 2004-08-19 07:14:55 a1909356#db2admin Exp $";
+public class RunStateManager implements RunStateEnquirer {
+	public static final String version="$RCSfile: RunStateManager.java,v $ $Revision: 1.5 $ $Date: 2005-10-27 08:42:54 $";
 	
 	private Logger log = Logger.getLogger(RunStateManager.class);
 	private RunStateEnum runState = RunStateEnum.STOPPED;
 
-	/**
-	 * Constructor
-	 */
-	public RunStateManager() {
-		super();
-	}
 	public synchronized RunStateEnum getRunState() {
 		return runState;
 	}
