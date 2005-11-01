da12fb60beeb1c70d1f1c35ff0bc9be3964db6b2
John Dekker <john.dekker@ibissource.org>
2005-11-01 08:51:13 +0000

    Add support for dynamic scheduling, i.e. add a job to the scheduler using a sender

---
+++ JavaSource/nl/nn/adapterframework/scheduler/SchedulerHelper.java
@@ -0,0 +1,98 @@
+/*
+ * $Log: SchedulerHelper.java,v $
+ * Revision 1.1  2005-11-01 08:51:14  europe\m00f531
+ * Add support for dynamic scheduling, i.e. add a job to the scheduler using 
+ * a sender
+ *
+ */
+package nl.nn.adapterframework.scheduler;
+
+import java.text.ParseException;
+
+import org.apache.commons.lang.StringUtils;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * The SchedulerHelper encapsulates the quarz scheduler.
+ * 
+ * @author John Dekker
+ */
+public class SchedulerHelper {
+	public static void scheduleJob(Object config, JobDef jobdef) throws Exception {
+		JobDetail jobDetail = new JobDetail(jobdef.getName(), // job name
+			Scheduler.DEFAULT_GROUP, // job group
+			AdapterJob.class); // the java class to execute
+
+		jobDetail.getJobDataMap().put("adapterName", jobdef.getAdapterName());
+		jobDetail.getJobDataMap().put("config", config);
+		jobDetail.getJobDataMap().put("function", jobdef.getFunction());
+		jobDetail.getJobDataMap().put("receiverName", jobdef.getReceiverName());
+		
+		if (StringUtils.isNotEmpty(jobdef.getDescription()))
+			jobDetail.setDescription(jobdef.getDescription());
+
+		scheduleJob(jobdef.getName(), jobDetail, jobdef.getCronExpression(), true);
+	}
+	
+	public static void scheduleJob(String jobName, JobDetail jobDetail, String cronExpression, boolean overwrite) throws SchedulerException, ParseException {
+		scheduleJob(jobName, Scheduler.DEFAULT_GROUP, jobDetail, cronExpression, overwrite);
+	}
+	
+	public static void scheduleJob(String jobName, String jobGroup, JobDetail jobDetail, String cronExpression, boolean overwrite) throws SchedulerException, ParseException {
+		Scheduler sched = getScheduler();
+
+		// if the job already exists, remove it.
+		if ((sched.getTrigger(jobName, jobGroup)) != null) {
+			if (overwrite)
+				sched.unscheduleJob(jobName, jobGroup);
+			else
+				throw new SchedulerException("Job with name [" + jobName + "] already exists");
+		}
+
+		CronTrigger cronTrigger = new CronTrigger(jobName, jobGroup);
+		cronTrigger.setCronExpression(cronExpression);
+		sched.scheduleJob(jobDetail, cronTrigger);
+	}
+	
+	public static Trigger getTrigger(String jobName) throws SchedulerException {
+		return getTrigger(jobName, Scheduler.DEFAULT_GROUP);
+	}
+
+	public static Trigger getTrigger(String jobName, String jobGroup) throws SchedulerException {
+		return getScheduler().getTrigger(jobName, jobGroup);
+	}
+
+	public static JobDetail getJobForTrigger(String jobName) throws SchedulerException {
+		return getJobForTrigger(jobName, Scheduler.DEFAULT_GROUP);
+	}
+
+	public static JobDetail getJobForTrigger(String jobName, String jobGroup) throws SchedulerException {
+		Scheduler sched = getScheduler();
+		
+		Trigger t = sched.getTrigger(jobName, jobGroup);
+		String name = t.getJobName();
+		String group = t.getJobGroup();
+		
+		return sched.getJobDetail(name, group);
+	}
+	
+	public static void deleteTrigger(String jobName) throws SchedulerException {
+		deleteTrigger(jobName, Scheduler.DEFAULT_GROUP);
+	}
+	
+	public static void deleteTrigger(String jobName, String jobGroup) throws SchedulerException {
+		getScheduler().unscheduleJob(jobName, jobGroup);
+	}
+	
+	public static Scheduler getScheduler() throws SchedulerException {
+		SchedulerFactory schedFact = new StdSchedulerFactory();
+		Scheduler sched = schedFact.getScheduler();
+		return sched;		
+	}
+}
---
+++ JavaSource/nl/nn/adapterframework/scheduler/SchedulerSender.java
@@ -0,0 +1,141 @@
+/*
+ * $Log: SchedulerSender.java,v $
+ * Revision 1.1  2005-11-01 08:51:13  europe\m00f531
+ * Add support for dynamic scheduling, i.e. add a job to the scheduler using 
+ * a sender
+ *
+ */
+package nl.nn.adapterframework.scheduler;
+
+import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.SenderException;
+import nl.nn.adapterframework.core.SenderWithParametersBase;
+import nl.nn.adapterframework.parameters.Parameter;
+import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import nl.nn.adapterframework.parameters.ParameterValueList;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+
+/**
+ * Registers a trigger in the scheduler so that the message is send to a javalistener
+ * at a scheduled time.
+ * 
+ * <p><b>Configuration:</b>
+ * <table border="1">
+ * <tr><th>attributes</th><th>description</th><th>default</th></tr>
+ * <tr><td>classname</td><td>nl.nn.adapterframework.scheduler.SchedulerSender</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setName(String) name}</td><td>name of the sender</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setJavaListener(String) javaListener}</td><td>Java listener to be called when scheduler trigger fires</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setCronExpressionPattern(String) cronExpressionPattern}</td><td>Expression that generates the cron trigger</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setJobGroup(String) jobGroup}</td><td>Job group in which the new trigger is to be created (optional)</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setJobNamePattern(String) jobNamePattern}</td><td>Pattern that leads to the name of the registered trigger(optional)</td><td>&nbsp;</td></tr>
+ * </table>
+ * </p>
+ * 
+ * @author John Dekker
+ */
+public class SchedulerSender extends SenderWithParametersBase {
+	protected Logger log = Logger.getLogger(this.getClass());
+	
+	public static final String JAVALISTENER = "javaListener";
+	public static final String CORRELATIONID = "correlationId";
+	public static final String MESSAGE = "message";
+	
+	private String javaListener;
+	private String cronExpressionPattern;
+	private String jobGroup;
+	private String jobNamePattern;
+	
+	/* (non-Javadoc)
+	 * @see nl.nn.adapterframework.core.ISender#configure()
+	 */
+	public void configure() throws ConfigurationException {
+		if (StringUtils.isEmpty(javaListener)) {
+			throw new ConfigurationException("Property [serviceName] is empty");
+		}
+		if (StringUtils.isEmpty(cronExpressionPattern)) {
+			throw new ConfigurationException("Property [cronExpressionPattern] is empty");
+		}
+		
+		Parameter p = new Parameter();
+		p.setName("_cronexpression");
+		p.setPattern(cronExpressionPattern);
+		addParameter(p);
+
+		if (StringUtils.isNotEmpty(jobNamePattern)) {
+			p = new Parameter();
+			p.setName("_jobname");
+			p.setPattern(jobNamePattern);
+			addParameter(p);
+		}
+		
+		super.configure();
+	}
+
+	/* (non-Javadoc)
+	 * @see nl.nn.adapterframework.core.ISender#isSynchronous()
+	 */
+	public boolean isSynchronous() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see nl.nn.adapterframework.core.ISender#sendMessage(java.lang.String, java.lang.String)
+	 */
+	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException {
+		try {
+			ParameterValueList values = prc.getValues(paramList);
+			String jobName = getName() + correlationID;
+			String cronExpression = values.getParameterValue("_cronexpression").getValue().toString();
+			if (StringUtils.isNotEmpty(jobNamePattern)) {
+				jobName = values.getParameterValue("_jobname").getValue().toString();;	
+			}
+			schedule(jobName, cronExpression, correlationID, message);
+			return jobName;
+		}
+		catch(SenderException e) {
+			throw e;
+		}
+		catch(Exception e) {
+			throw new SenderException("Error during scheduling " + message, e);
+		}
+	}
+
+	/*
+	 * Schedule the job
+	 */
+	private void schedule(String jobName, String cronExpression, String correlationId, String message) throws Exception {
+		JobDetail jobDetail = new JobDetail(jobName, Scheduler.DEFAULT_GROUP, ServiceJob.class);
+		jobDetail.getJobDataMap().put(JAVALISTENER, javaListener);
+		jobDetail.getJobDataMap().put(MESSAGE, message);
+		jobDetail.getJobDataMap().put(CORRELATIONID, correlationId);
+
+		if (StringUtils.isEmpty(jobGroup))
+			SchedulerHelper.scheduleJob(jobName, jobDetail, cronExpression, false);
+		else 
+			SchedulerHelper.scheduleJob(jobName, jobGroup, jobDetail, cronExpression, false);
+
+		if (log.isDebugEnabled()) {
+			log.debug("SchedulerSender ["+ getName() +"] has send job [" + jobName + "] to the scheduler");
+		}
+	}
+	public void setCronExpressionPattern(String string) {
+		cronExpressionPattern = string;
+	}
+
+	public void setJobGroup(String string) {
+		jobGroup = string;
+	}
+
+	public void setJobNamePattern(String string) {
+		jobNamePattern = string;
+	}
+
+	public void setJavaListener(String string) {
+		javaListener = string;
+	}
+
+}
