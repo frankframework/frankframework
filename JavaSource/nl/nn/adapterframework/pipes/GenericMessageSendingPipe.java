5641ef2cfbd2ad6934e4030615c58d4193a00496
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-12-19 14:27:22 +0000

    updated javadoc

--- JavaSource/nl/nn/adapterframework/pipes/GenericMessageSendingPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/GenericMessageSendingPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: GenericMessageSendingPipe.java,v $
- * Revision 1.1  2004-04-08 15:58:59  nnvznl01#l181303
+ * Revision 1.2  2005-12-19 14:27:22  europe\L190409
+ * updated javadoc
+ *
+ * Revision 1.1  2004/04/08 15:58:59  Dennis van Loon <dennis.van.loon@ibissource.org>
  * Initial Version
  *
  */
@@ -13,16 +16,41 @@ import nl.nn.adapterframework.core.ISender;
  * Plain extension to {@link MessageSendingPipe} that can be used directly in configurations.
  * Only extension is that the setters for listener and sender have been made public, and can therefore
  * be set from the configuration file.
- * For configuration options, see {@link MessageSendingPipe}.
+ * <p><b>Configuration:</b>
+ * <table border="1">
+ * <tr><th>attributes</th><th>description</th><th>default</th></tr>
+ * <tr><td>{@link #setName(String) name}</td><td>name of the Pipe</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setMaxThreads(int) maxThreads}</td><td>maximum number of threads that may call {@link #doPipe(Object, PipeLineSession)} simultaneously</td><td>0 (unlimited)</td></tr>
+ * <tr><td>{@link #setForwardName(String) forwardName}</td>  <td>name of forward returned upon completion</td><td>"success"</td></tr>
+ * <tr><td>{@link #setResultOnTimeOut(String) resultOnTimeOut}</td><td>result returned when no return-message was received within the timeout limit</td><td>"receiver timed out"</td></tr>
+ * <tr><td>{@link #setLinkMethod(String) linkMethod}</td><td>Indicates wether the server uses the correlationID or the messageID in the correlationID field of the reply</td><td>CORRELATIONID</td></tr>
+ * <tr><td>{@link #setNamespaceAware(boolean) namespaceAware}</td><td>controls namespaceAwarenes for parameters</td><td>application default</td></tr>
+ * <tr><td><code>sender.*</td><td>any attribute of the sender instantiated by descendant classes</td><td>&nbsp;</td></tr>
+ * </table>
+ * <table border="1">
+ * <tr><th>nested elements</th><th>description</th></tr>
+ * <tr><td>{@link nl.nn.adapterframework.core.ISender sender}</td><td>specification of sender to send messages with</td></tr>
+ * <tr><td>{@link nl.nn.adapterframework.core.ICorrelatedPullingListener listener}</td><td>specification of listener to listen to for replies</td></tr>
+ * <tr><td>{@link nl.nn.adapterframework.parameters.Parameter param}</td><td>any parameters defined on the pipe will be handed to the sender, if this is a {@link IParameterizedSender}</td></tr>
+ * </table>
+ * </p>
+ * <p><b>Exits:</b>
+ * <table border="1">
+ * <tr><th>state</th><th>condition</th></tr>
+ * <tr><td>"success"</td><td>default when a good message was retrieved (synchronous sender), or the message was successfully sent and no listener was specified and the sender was not synchronous</td></tr>
+ * <tr><td><i>{@link #setForwardName(String) forwardName}</i></td><td>if specified, and otherwise under same condition as "success"</td></tr>
+ * <tr><td>"timeout"</td><td>no data was received (timeout on listening), if the sender was synchronous or a listener was specified.</td></tr>
+ * <tr><td>"exception"</td><td>an exception was thrown by the Sender or its reply-Listener. The result passed to the next pipe is the exception that was caught.</td></tr>
+ * </table>
+ * </p>
  * 
  * @version Id
  * @author  Dennis van Loon
- * @since 4.1.1
+ * @since   4.1.1
  */
 
 public class GenericMessageSendingPipe extends MessageSendingPipe {
-
-	public static final String version="$Id: GenericMessageSendingPipe.java,v 1.1 2004-04-08 15:58:59 nnvznl01#l181303 Exp $";
+	public static final String version="$RCSfile: GenericMessageSendingPipe.java,v $ $Revision: 1.2 $ $Date: 2005-12-19 14:27:22 $";
 
 	public void setListener(ICorrelatedPullingListener listener) {
 		super.setListener(listener);
@@ -31,6 +59,5 @@ public class GenericMessageSendingPipe extends MessageSendingPipe {
 	public void setSender(ISender sender) {
 		super.setSender(sender);
 	}
-
-
+	
 }
