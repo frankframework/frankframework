03ee1a22b1b65c29a456f5d58dc1ecf6437931a4
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-12-28 09:18:48 +0000

    support unicode names for environment-variables

--- JavaSource/nl/nn/adapterframework/webcontrol/action/ShowEnvironmentVariables.java
+++ JavaSource/nl/nn/adapterframework/webcontrol/action/ShowEnvironmentVariables.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ShowEnvironmentVariables.java,v $
- * Revision 1.2  2005-10-27 08:44:07  europe\L190409
+ * Revision 1.3  2005-12-28 09:18:48  europe\L190409
+ * support unicode names for environment-variables
+ *
+ * Revision 1.2  2005/10/27 08:44:07  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * moved getEnvironmentVariables to Misc
  *
  * Revision 1.1  2005/10/26 12:52:31  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -37,7 +40,7 @@ import nl.nn.adapterframework.util.XmlUtils;
  */
 
 public class ShowEnvironmentVariables extends ActionBase {
-	public static final String version = "$RCSfile: ShowEnvironmentVariables.java,v $ $Revision: 1.2 $ $Date: 2005-10-27 08:44:07 $";
+	public static final String version = "$RCSfile: ShowEnvironmentVariables.java,v $ $Revision: 1.3 $ $Date: 2005-12-28 09:18:48 $";
 
 	public void addPropertiesToXmlBuilder(XmlBuilder container, Properties props, String setName) {
 		Enumeration enum = props.keys();
@@ -48,9 +51,8 @@ public class ShowEnvironmentVariables extends ActionBase {
 		while (enum.hasMoreElements()) {
 			String propName = (String) enum.nextElement();
 			XmlBuilder property = new XmlBuilder("property");
-			property.addAttribute("name", propName);
-			property.setCdataValue(
-				XmlUtils.encodeCdataString(props.getProperty(propName)));
+			property.addAttribute("name", XmlUtils.encodeCdataString(propName));
+			property.setCdataValue(XmlUtils.encodeCdataString(props.getProperty(propName)));
 			propertySet.addSubElement(property);
 		}
 
