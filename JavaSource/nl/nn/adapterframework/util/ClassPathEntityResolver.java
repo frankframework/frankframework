7ff3193c4e42905c40ffee2d6701c846178b618e
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2005-12-28 08:28:45 +0000

    improved handling of malformed URLs

--- JavaSource/nl/nn/adapterframework/util/ClassPathEntityResolver.java
+++ JavaSource/nl/nn/adapterframework/util/ClassPathEntityResolver.java
@@ -1,37 +1,36 @@
 /*
  * $Log: ClassPathEntityResolver.java,v $
- * Revision 1.1  2004-06-16 06:57:44  NNVZNL01#L180564
+ * Revision 1.2  2005-12-28 08:28:45  europe\L190409
+ * improved handling of malformed URLs
+ *
+ * Revision 1.1  2004/06/16 06:57:44  Johan Verrips <johan.verrips@ibissource.org>
  * Initial version
  *
  *
  */
 package nl.nn.adapterframework.util;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 
 import org.apache.log4j.Logger;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-	/**
-	 * @author Frits Berger RSD SF OJM
-	 * @see org.xml.sax.EntityResolver
-	 * This class offers the resolveEntity method to resolve a systemId to
-	 * a resource on the classpath.
-	 * @since 4.1.1
-	 * @version Id
-	 */
+/**
+ * @author Frits Berger RSD SF OJM
+ * @see org.xml.sax.EntityResolver
+ * This class offers the resolveEntity method to resolve a systemId to
+ * a resource on the classpath.
+ * @since 4.1.1
+ * @version Id
+ */
 
 public class ClassPathEntityResolver implements EntityResolver {
-
-	public ClassPathEntityResolver() {
-		super();
-	}
-
-	public static final String version = "$Id: ClassPathEntityResolver.java,v 1.1 2004-06-16 06:57:44 NNVZNL01#L180564 Exp $";
-
+	public static final String version="$RCSfile: ClassPathEntityResolver.java,v $ $Revision: 1.2 $ $Date: 2005-12-28 08:28:45 $";
 	protected Logger log = Logger.getLogger(this.getClass());
+
+
 
 	/**
 	 * @see org.xml.sax.EntityResolver#resolveEntity(String, String)
@@ -40,17 +39,24 @@ public class ClassPathEntityResolver implements EntityResolver {
 		throws SAXException, IOException {
 		InputSource inputSource = null;
 
+		String classPathEntityUrl = systemId;
+	
+		// strip any file info from systemId 
+		int idx = classPathEntityUrl.lastIndexOf("/");
+		if (idx >= 0) {
+			classPathEntityUrl = classPathEntityUrl.substring(idx + 1);
+		}
+		classPathEntityUrl = "/" + classPathEntityUrl;
 		try {
-			// strip any file info from systemId 
-			int idx = systemId.lastIndexOf("/");
-			if (idx >= 0)
-				systemId = systemId.substring(idx + 1);
-			InputStream inputStream =
-				this.getClass().getResourceAsStream("/" + systemId);
-			inputSource = new InputSource(inputStream);
-			log.debug("Resolving " + systemId + " to /" + systemId);
+			log.debug("Resolving [" + systemId + "] to [" + classPathEntityUrl+"]");
+			URL url = ClassUtils.getResourceURL(this,classPathEntityUrl);
+			if (url==null) {
+				log.error("cannot find resource for ClassPathEntity [" + systemId + "] from Url [" + classPathEntityUrl+"]");
+				return null;
+			}
+			inputSource = new InputSource(url.openStream());
 		} catch (Exception e) {
-			log.error(e);
+			log.error("Exception resolving ClassPathEntity [" + systemId + "] to [" + classPathEntityUrl+"]",e);
 			// No action; just let the null InputSource pass through
 		}
 
