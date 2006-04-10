0a479a3a6d865dc4047f074ae634dc10773a6dc3
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2006-04-10 09:29:30 +0000

    added clear() and setValue()

--- JavaSource/nl/nn/adapterframework/util/Counter.java
+++ JavaSource/nl/nn/adapterframework/util/Counter.java
@@ -1,6 +1,9 @@
 /*
  * $Log: Counter.java,v $
- * Revision 1.4  2004-09-07 08:44:09  L190409
+ * Revision 1.5  2006-04-10 09:29:30  europe\L190409
+ * added clear() and setValue()
+ *
+ * Revision 1.4  2004/09/07 08:44:09  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * added increase and decrease with amount-argument
  *
  */
@@ -13,7 +16,7 @@ package nl.nn.adapterframework.util;
  * @author Gerrit van Brakel
  */
 public class Counter {
-	public static final String version="$Id: Counter.java,v 1.4 2004-09-07 08:44:09 L190409 Exp $";
+	public static final String version="$RCSfile: Counter.java,v $  $Revision: 1.5 $ $Date: 2006-04-10 09:29:30 $";
 	
 	private long value = 0 ;
 
@@ -27,13 +30,19 @@ public class Counter {
 	public synchronized long decrease(long amount) {
 		return value-=amount;
 	}
-	public synchronized long getValue() {
-		return value;
-	}
 	public synchronized long increase() {
 		return ++value;
 	}
 	public synchronized long increase(long amount) {
 		return value+=amount;
+	}
+	public synchronized void clear() {
+		value=0;	
+	}
+	public synchronized long getValue() {
+		return value;
+	}
+	public synchronized void setValue(long newValue) {
+		value=newValue;
 	}
 }
