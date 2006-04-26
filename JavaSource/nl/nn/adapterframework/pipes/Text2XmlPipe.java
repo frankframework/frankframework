cba451ffeb8a8eb5d76b1eaf9853ae45881bcc36
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2006-04-26 11:29:20 +0000

    added splitLines attribute

--- JavaSource/nl/nn/adapterframework/pipes/Text2XmlPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/Text2XmlPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: Text2XmlPipe.java,v $
- * Revision 1.3  2004-10-14 15:36:25  L190409
+ * Revision 1.4  2006-04-26 11:29:20  europe\L190409
+ * added splitLines attribute
+ *
+ * Revision 1.3  2004/10/14 15:36:25  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * added includeXmlDeclaration-attribute
  *
  * Revision 1.2  2004/04/27 11:42:40  unknown <unknown@ibissource.org>
@@ -11,6 +14,10 @@
  *
  */
 package nl.nn.adapterframework.pipes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.PipeLineSession;
@@ -27,6 +34,7 @@ import org.apache.commons.lang.StringUtils;
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>{@link #setXmlTag(String) xmlag}</td><td>the xml tag to encapsulate the text in</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setIncludeXmlDeclaration(boolean) includeXmlDeclaration}</td><td>controls whether a declation is included above the Xml text</td><td>true</td></tr>
+ * <tr><td>{@link #setSplitLines(boolean) splitLines}</td><td>controls whether the lines of the input are places in separated &lt;line&gt; tags</td><td>false</td></tr>
  * </table>
  * </p>
  * <p><b>Exits:</b>
@@ -43,6 +51,7 @@ import org.apache.commons.lang.StringUtils;
 public class Text2XmlPipe extends FixedForwardPipe {
 	private String xmlTag;
 	private boolean includeXmlDeclaration = true;
+	private boolean splitLines = false;
 	
 	/** 
 	 * @see nl.nn.adapterframework.core.IPipe#configure()
@@ -55,12 +64,35 @@ public class Text2XmlPipe extends FixedForwardPipe {
 		}
 	}
 	
+	
 	/** 
 	 * @see nl.nn.adapterframework.core.IPipe#doPipe(Object, PipeLineSession)
 	 */
 	public PipeRunResult doPipe(Object input, PipeLineSession session) throws PipeRunException {
+		if (isSplitLines() && input != null) {
+			try {
+				BufferedReader br = new BufferedReader(new StringReader(input.toString()));
+				String l;
+				StringBuffer result = new StringBuffer();
+
+				while ((l = br.readLine()) != null) {
+					result.append("<line><![CDATA["+l+"]]></line>");
+				}
+					
+				input = result.toString();
+				br.close();
+			}
+			catch (IOException e) {
+				throw new PipeRunException(this, "Unexpected exception during appending header", e); 
+			}
+			
+						
+		}
+		else
+			input = "<![CDATA["+ input +"]]>";
+			
 		String resultString = (isIncludeXmlDeclaration()?"<?xml version=\"1.0\" encoding=\"UTF-8\"?>":"") +
-		"<" + getXmlTag() + "><![CDATA[" + input + "]]></" + xmlTag + ">";	
+		"<" + getXmlTag() + ">"+input+"</" + xmlTag + ">";	
 		return new PipeRunResult(getForward(), resultString);
 	}
 
@@ -84,6 +116,14 @@ public class Text2XmlPipe extends FixedForwardPipe {
 
 	public void setIncludeXmlDeclaration(boolean b) {
 		includeXmlDeclaration = b;
+	}
+
+	public boolean isSplitLines() {
+		return splitLines;
+	}
+
+	public void setSplitLines(boolean b) {
+		splitLines = b;
 	}
 
 }
