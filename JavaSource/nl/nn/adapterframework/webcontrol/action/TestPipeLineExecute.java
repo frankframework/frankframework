384e4dd7ace2783f18a4cea5718535cb6b75c0f6
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-02-12 14:36:29 +0000

    added zipfile-upload capability

--- JavaSource/nl/nn/adapterframework/webcontrol/action/TestPipeLineExecute.java
+++ JavaSource/nl/nn/adapterframework/webcontrol/action/TestPipeLineExecute.java
@@ -1,14 +1,17 @@
+/*
+ * $Log: TestPipeLineExecute.java,v $
+ * Revision 1.6  2007-02-12 14:36:29  europe\L190409
+ * added zipfile-upload capability
+ *
+ */
 package nl.nn.adapterframework.webcontrol.action;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -27,17 +30,17 @@ import org.apache.struts.action.DynaActionForm;
 import org.apache.struts.upload.FormFile;
 
 /**
- *
- * Test the Pipeline of an adapter
- * <p>$Id: TestPipeLineExecute.java,v 1.5 2007-02-05 15:06:09 europe\L190409 Exp $</p>
+ * Test the Pipeline of an adapter.
+ * 
  * @author  Johan Verrips
  * @see nl.nn.adapterframework.configuration.Configuration
  * @see nl.nn.adapterframework.core.Adapter
  * @see nl.nn.adapterframework.core.PipeLine
+ * @version Id
  */
 
 public final class TestPipeLineExecute extends ActionBase {
-	public static final String version="$RCSfile: TestPipeLineExecute.java,v $  $Revision: 1.5 $ $Date: 2007-02-05 15:06:09 $";
+	public static final String version="$RCSfile: TestPipeLineExecute.java,v $  $Revision: 1.6 $ $Date: 2007-02-12 14:36:29 $";
 	
 	public ActionForward execute(
 	    ActionMapping mapping,
@@ -98,51 +101,78 @@ public final class TestPipeLineExecute extends ActionBase {
 	
 	    // if upload is choosen, it prevails over the message
 	    if ((form_file != null) && (form_file.getFileSize() > 0)) {
-	        form_message = new String(form_file.getFileData());
-	        log.debug("Upload of file ["+form_file.getFileName()+"] ContentType["+form_file.getContentType()+"]");
-	    }
-		// if remote directory is choosen and not upload, it prevails over the message
-		else if (form_remoteDirectory != null && form_remoteDirectory.length() > 0) {
-			form_message = "";
-			File importDir = new File(form_remoteDirectory);
-			if (!importDir.isDirectory()) {
-				errors.add("", new ActionError("errors.generic", "Remote directory is not a directory"));
-			} else {
-				//zoek alle bestanden in de directory importDir
-				FileFilter fileFilter = new FileFilter() {
-					public boolean accept(File inFile) {
-						// To return whether it ends with any extension.
-						// for example ends with .xml 
-						return inFile.isFile() && inFile.getName().endsWith(".xml");
-					}
-				};
-				File[] files = importDir.listFiles(fileFilter);
-				if (files == null) {
-					log.warn("Geen bestanden gevonden in " + importDir.getAbsolutePath());
-					errors.add("", new ActionError("errors.generic", "No files found in remote directory"));
-				} else {
-					form_resultText = "";
-					for (int i = 0; i < files.length; i++) {
-						File currentFile = files[i];
-						InputStream is = new FileInputStream(currentFile);
-						InputStreamReader reader = new InputStreamReader(is);
-						BufferedReader bufread = new BufferedReader(reader);
-						String line;
-						StringBuffer stringBuf = new StringBuffer();
-						while ((line = bufread.readLine()) != null) {
-							stringBuf.append(line);
-							stringBuf.append("\n");
+			log.debug("Upload of file ["+form_file.getFileName()+"] ContentType["+form_file.getContentType()+"]");
+	    	if (form_file.getFileName().endsWith(".zip")) {
+	    		ZipInputStream archive = new ZipInputStream(new ByteArrayInputStream(form_file.getFileData()));
+	    		for (ZipEntry entry=archive.getNextEntry(); entry!=null; entry=archive.getNextEntry()) {
+	    			String name = entry.getName();
+					int size = (int)entry.getSize();
+					if (size>0) {
+						byte[] b=new byte[size];
+						int rb=0;
+						int chunk=0;
+						while (((int)size - rb) > 0) {
+							chunk=archive.read(b,rb,(int)size - rb);
+							if (chunk==-1) {
+							   break;
+							}
+							rb+=chunk;
 						}
-						String currentMessage = stringBuf.toString();
+						String currentMessage = new String(b,0,rb);
 						PipeLineResult pipeLineResult =
-							adapter.processMessage("testmessage" + Misc.createSimpleUUID(), currentMessage);
-						form_resultText += currentFile.getName() + ":" + pipeLineResult.getState() + "\n";
+							adapter.processMessage(name+"_" + Misc.createSimpleUUID(), currentMessage);
+						form_resultText += name + ":" + pipeLineResult.getState() + "\n";
 						form_resultState = pipeLineResult.getState();
 					}
-	
-				}
-			}
-		}
+					archive.closeEntry();
+	    		}
+	    		archive.close();
+	    	} else {
+				form_message = new String(form_file.getFileData());
+	    	}
+	    }
+//		// if remote directory is choosen and not upload, it prevails over the message
+//		else if (form_remoteDirectory != null && form_remoteDirectory.length() > 0) {
+//			form_message = "";
+//			File importDir = new File(form_remoteDirectory);
+//			if (!importDir.isDirectory()) {
+//				errors.add("", new ActionError("errors.generic", "Remote directory is not a directory"));
+//			} else {
+//				//zoek alle bestanden in de directory importDir
+//				FileFilter fileFilter = new FileFilter() {
+//					public boolean accept(File inFile) {
+//						// To return whether it ends with any extension.
+//						// for example ends with .xml 
+//						return inFile.isFile() && inFile.getName().endsWith(".xml");
+//					}
+//				};
+//				File[] files = importDir.listFiles(fileFilter);
+//				if (files == null) {
+//					log.warn("Geen bestanden gevonden in " + importDir.getAbsolutePath());
+//					errors.add("", new ActionError("errors.generic", "No files found in remote directory"));
+//				} else {
+//					form_resultText = "";
+//					for (int i = 0; i < files.length; i++) {
+//						File currentFile = files[i];
+//						InputStream is = new FileInputStream(currentFile);
+//						InputStreamReader reader = new InputStreamReader(is);
+//						BufferedReader bufread = new BufferedReader(reader);
+//						String line;
+//						StringBuffer stringBuf = new StringBuffer();
+//						while ((line = bufread.readLine()) != null) {
+//							stringBuf.append(line);
+//							stringBuf.append("\n");
+//						}
+//						String currentMessage = stringBuf.toString();
+//						PipeLineResult pipeLineResult =
+//							adapter.processMessage("testmessage" + Misc.createSimpleUUID(), currentMessage);
+//						form_resultText += currentFile.getName() + ":" + pipeLineResult.getState() + "\n";
+//						form_resultState = pipeLineResult.getState();
+//					}
+//	
+//				}
+//			}
+//		}
 	
 		if(form_message != null && form_message.length() > 0) {
 	    // Execute the request
