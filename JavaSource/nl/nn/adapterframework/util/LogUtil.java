1977815690df34608b5b2ef2b5e85f32fb466897
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-02-12 15:56:27 +0000

    configure logging hierarchy from log4j.properties

--- JavaSource/nl/nn/adapterframework/util/LogUtil.java
+++ JavaSource/nl/nn/adapterframework/util/LogUtil.java
@@ -1,14 +1,20 @@
 /*
  * $Log: LogUtil.java,v $
- * Revision 1.1  2007-02-12 14:10:36  europe\L190409
+ * Revision 1.2  2007-02-12 15:56:27  europe\L190409
+ * configure logging hierarchy from log4j.properties
+ *
+ * Revision 1.1  2007/02/12 14:10:36  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * introduction of LogUtil
  *
  */
 package nl.nn.adapterframework.util;
 
+import java.net.URL;
+
 import org.apache.log4j.Hierarchy;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.spi.RootCategory;
 
 /**
@@ -18,9 +24,9 @@ import org.apache.log4j.spi.RootCategory;
  * @version Id
  */
 public class LogUtil {
-	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.1 $ $Date: 2007-02-12 14:10:36 $";
+	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.2 $ $Date: 2007-02-12 15:56:27 $";
 
-	private static Hierarchy hierarchy = new Hierarchy(new RootCategory(Level.DEBUG));
+	private static Hierarchy hierarchy=null;
 	
 	
 	public static Logger getLogger(String name) { 
@@ -36,7 +42,12 @@ public class LogUtil {
 	}
 
 
-	public static Hierarchy getHierarchy() {
+	public static synchronized Hierarchy getHierarchy() {
+		if (hierarchy==null) {
+			hierarchy = new Hierarchy(new RootCategory(Level.DEBUG));
+			URL url = ClassUtils.getResourceURL(LogUtil.class, "log4j.properties");
+			new PropertyConfigurator().doConfigure(url, hierarchy);
+		}
 		return hierarchy;
 	}
 
