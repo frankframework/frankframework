c81325f266b97e64f75a404691b1eea035f5a97f
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-05-23 09:07:57 +0000

    added attributes slotId and active

--- JavaSource/nl/nn/adapterframework/core/ITransactionalStorage.java
+++ JavaSource/nl/nn/adapterframework/core/ITransactionalStorage.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ITransactionalStorage.java,v $
- * Revision 1.3  2005-07-19 12:20:44  europe\L190409
+ * Revision 1.4  2007-05-23 09:07:57  europe\L190409
+ * added attributes slotId and active
+ *
+ * Revision 1.3  2005/07/19 12:20:44  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * adapted to work extend IMessageBrowser
  *
  * Revision 1.2  2004/03/26 10:42:44  Johan Verrips <johan.verrips@ibissource.org>
@@ -26,7 +29,7 @@ import nl.nn.adapterframework.configuration.ConfigurationException;
  * @version Id
 */
 public interface ITransactionalStorage extends IMessageBrowser, INamedObject {
-	public static final String version = "$RCSfile: ITransactionalStorage.java,v $ $Revision: 1.3 $ $Date: 2005-07-19 12:20:44 $";
+	public static final String version = "$RCSfile: ITransactionalStorage.java,v $ $Revision: 1.4 $ $Date: 2007-05-23 09:07:57 $";
 
 	/**
 	 * Prepares the object for operation. After this
@@ -45,4 +48,13 @@ public interface ITransactionalStorage extends IMessageBrowser, INamedObject {
 	public String storeMessage(String messageId, String correlationId, Date receivedDate, String comments, Serializable message) throws SenderException;
 	
 	public void setName(String name);
+
+	/**
+	 *  slotId allows using component to define a kind of 'subsection'.
+	 */	
+	public String getSlotId();
+	public void setSlotId(String string);
+	
+	public boolean isActive();
+
 }
