8181b60ab9654b0c293dbdd5c2271cea80cc3bfc
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-07-10 07:30:07 +0000

    improved javadoc

--- JavaSource/nl/nn/adapterframework/pipes/ExceptionPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/ExceptionPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ExceptionPipe.java,v $
- * Revision 1.2  2007-06-26 09:35:13  europe\L190409
+ * Revision 1.3  2007-07-10 07:30:07  europe\L190409
+ * improved javadoc
+ *
+ * Revision 1.2  2007/06/26 09:35:13  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * cleanup exception message
  *
  * Revision 1.1  2007/05/02 11:20:32  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -20,7 +23,7 @@ import nl.nn.adapterframework.core.PipeRunResult;
  * <p><b>Configuration:</b>
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
- * <tr><td>{@link #setThrowException(boolean) throwException}</td><td>when <code>true</true>, a PipeRunException is thrown. Otherwise the </td><td>true</td></tr>
+ * <tr><td>{@link #setThrowException(boolean) throwException}</td><td>when <code>true</code>, a PipeRunException is thrown. Otherwise the output is only logged as an error</td><td>true</td></tr>
  * </table>
  * </p>
  * <p><b>Exits:</b>
