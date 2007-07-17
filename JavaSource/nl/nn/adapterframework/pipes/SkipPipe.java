4573fc024a33f571f0a62b59c941a2a487af201e
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-07-17 15:12:33 +0000

    added length attribute

--- JavaSource/nl/nn/adapterframework/pipes/SkipPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/SkipPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: SkipPipe.java,v $
- * Revision 1.1  2006-08-22 12:56:32  europe\L190409
+ * Revision 1.2  2007-07-17 15:12:33  europe\L190409
+ * added length attribute
+ *
+ * Revision 1.1  2006/08/22 12:56:32  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * first version
  *
  */
@@ -17,6 +20,7 @@ import nl.nn.adapterframework.core.PipeRunResult;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>{@link #setSkip(int) skip}</td><td>number of bytes (for byte array input) or characters (for String input) to skip. An empty byte array or String is returned when skip is larger then the length of the input</td><td>0</td></tr>
+ * <tr><td>{@link #setLength(int) length}</td><td>if length>=0 only these number of bytes (for byte array input) or characters (for String input) is returned.</td><td>-1</td></tr>
  * </table>
  * </p>
  * 
@@ -25,12 +29,11 @@ import nl.nn.adapterframework.core.PipeRunResult;
  *
  */
 public class SkipPipe extends FixedForwardPipe {
-	public static final String version="$RCSfile: SkipPipe.java,v $ $Revision: 1.1 $ $Date: 2006-08-22 12:56:32 $";
-	protected int skip = 0;
+	public static final String version="$RCSfile: SkipPipe.java,v $ $Revision: 1.2 $ $Date: 2007-07-17 15:12:33 $";
+
+	private int skip = 0;
+	private int length = -1;
 	
-	/** 
-	 * @see nl.nn.adapterframework.core.IPipe#doPipe(Object, PipeLineSession)
-	 */
 	public PipeRunResult doPipe(Object input, PipeLineSession session) throws PipeRunException {
 		try {
 			Object result = "SkipPipe doesn't work for this type of object";
@@ -39,7 +42,11 @@ public class SkipPipe extends FixedForwardPipe {
 				if (skip > stringInput.length()) {
 					result = "";
 				} else {
-					result = stringInput.substring(skip);
+					if (length >= 0 && length < stringInput.length() - skip) {
+						result = stringInput.substring(skip, skip + length);
+					} else {
+						result = stringInput.substring(skip);
+					}
 				}
 			} else if (input instanceof byte[]) {
 				byte[] bytesInput = (byte[])input;
@@ -47,9 +54,16 @@ public class SkipPipe extends FixedForwardPipe {
 				if (skip > bytesInput.length) {
 					bytesResult = new byte[0];
 				} else {
-					bytesResult = new byte[bytesInput.length - skip];
-					for (int i = 0; i < bytesResult.length; i++) {
-						bytesResult[i] = bytesInput[skip + i];
+					if (length >= 0 && length < bytesInput.length - skip) {
+						bytesResult = new byte[length];
+						for (int i = 0; i < length; i++) {
+							bytesResult[i] = bytesInput[skip + i];
+						}
+					} else {
+						bytesResult = new byte[bytesInput.length - skip];
+						for (int i = 0; i < bytesResult.length; i++) {
+							bytesResult[i] = bytesInput[skip + i];
+						}
 					}
 				}
 				result = bytesResult;
@@ -67,4 +81,7 @@ public class SkipPipe extends FixedForwardPipe {
 		this.skip = skip;
 	}
 
+	public void setLength(int length) {
+		this.length = length;
+	}
 }
