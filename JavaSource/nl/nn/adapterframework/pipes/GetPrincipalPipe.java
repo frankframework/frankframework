a97d1d3fb1243211b18909f410bc5c691f5cc36a
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-08-03 08:46:29 +0000

    first version

---
+++ JavaSource/nl/nn/adapterframework/pipes/GetPrincipalPipe.java
@@ -0,0 +1,43 @@
+/*
+ * $Log: GetPrincipalPipe.java,v $
+ * Revision 1.1  2007-08-03 08:46:29  europe\L190409
+ * first version
+ *
+ */
+package nl.nn.adapterframework.pipes;
+
+import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.core.PipeRunResult;
+
+/**
+ * Returns the name of the user executing the request.
+ *
+ * <p><b>Configuration:</b>
+ * <table border="1">
+ * <tr><th>attributes</th><th>description</th><th>default</th></tr>
+ * <tr><td>{@link #setName(String) name}</td><td>name of the Pipe</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setMaxThreads(int) maxThreads}</td><td>maximum number of threads that may call {@link #doPipe(Object, nl.nn.adapterframework.core.PipeLineSession)} simultaneously</td><td>0 (unlimited)</td></tr>
+ * <tr><td>{@link #setForwardName(String) forwardName}</td>  <td>name of forward returned upon completion</td><td>"success"</td></tr>
+ * <tr><td>{@link #setGetInputFromSessionKey(String) getInputFromSessionKey}</td><td>when set, input is taken from this session key, instead of regular input</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setStoreResultInSessionKey(String) storeResultInSessionKey}</td><td>when set, the result is stored under this session key</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setPreserveInput(boolean) preserveInput}</td><td>when set <code>true</code>, the input of a pipe is restored before processing the next one</td><td>false</td></tr>
+ * </table>
+ * </p>
+ * <p><b>Exits:</b>
+ * <table border="1">
+ * <tr><th>state</th><th>condition</th></tr>
+ * <tr><td>"success"</td><td>default</td></tr>
+ * <tr><td><i>{@link #setForwardName(String) forwardName}</i></td><td>if specified</td></tr>
+ * </table>
+ * </p>
+ * 
+ * @author  Gerrit van Brakel
+ * @since   4.7
+ * @version Id
+ */
+public class GetPrincipalPipe extends FixedForwardPipe {
+	
+	public PipeRunResult doPipe(Object input, PipeLineSession session) {
+		return new PipeRunResult(getForward(),session.getPrincipal());
+	}
+}
