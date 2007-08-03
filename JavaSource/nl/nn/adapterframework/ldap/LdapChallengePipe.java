2aba75a199f03efcd3805e665a1f5c9d2f0baf01
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-08-03 08:45:28 +0000

    fixed bug, added internal entryName parameter

--- JavaSource/nl/nn/adapterframework/ldap/LdapChallengePipe.java
+++ JavaSource/nl/nn/adapterframework/ldap/LdapChallengePipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: LdapChallengePipe.java,v $
- * Revision 1.3  2007-05-16 11:42:14  europe\L190409
+ * Revision 1.4  2007-08-03 08:45:28  europe\L190409
+ * fixed bug, added internal entryName parameter
+ *
+ * Revision 1.3  2007/05/16 11:42:14  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * cleanup code, remove threading problems, improve javadoc
  *
  * Revision 1.2  2007/02/27 12:48:50  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -13,10 +16,12 @@ package nl.nn.adapterframework.ldap;
 
 import java.util.HashMap;
 
+import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.ParameterException;
 import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.parameters.Parameter;
 import nl.nn.adapterframework.parameters.ParameterResolutionContext;
 import nl.nn.adapterframework.pipes.AbstractPipe;
 
@@ -29,15 +34,16 @@ import org.apache.commons.lang.StringUtils;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>classname</td><td>nl.nn.adapterframework.ldap.LdapChallengePipe</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setLdapProviderURL(String) ldapProviderURL}</td><td>URL to the LDAP server. <br/>Example: ldap://su05b9.itc.intranet</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setInitialContextFactoryName(String) initialContextFactoryName}</td><td>class to use as initial context factory</td><td>com.sun.jndi.ldap.LdapCtxFactory</td></tr>
  * </table>
  * </p>
  * <table border="1">
  * <p><b>Parameters:</b>
  * <tr><th>name</th><th>type</th><th>remarks</th></tr>
- * <tr><td>ldapProviderURL</td><td>URL to the LDAP server. <br/>Example: ldap://su05b9.itc.intranet</td><td>Requered and must be filled</td></tr>
- * <tr><td>credentials</td><td>The LDAP password. <br/> Example: welkom01</td><td>Requered and must be filled</td></tr>
- * <tr><td>principal</td><td>The LDAP DN for the username. <br/>Example: UID=SRP,OU=DI-IUF-EP,OU=SERVICES,O=ING</td><td>Requered and must be filled</td></tr>
- * <tr><td>initialContextFactoryName</td><td>The factory which implements the LDAP DIRContext. <br/>Example: com.sun.jndi.ldap.LdapCtxFactory</td><td>Requered and must be filled</td></tr>
+ * <tr><td>ldapProviderURL</td><td>URL to the LDAP server. <br/>Example: ldap://su05b9.itc.intranet</td><td>Required only if attribute ldapProviderURL is not set</td></tr>
+ * <tr><td>principal</td><td>The LDAP DN for the username. <br/>Example: UID=SRP,OU=DI-IUF-EP,OU=SERVICES,O=ING</td><td>Required and must be filled</td></tr>
+ * <tr><td>credentials</td><td>The LDAP password. <br/> Example: welkom01</td><td>Required and must be filled</td></tr>
  * </table>
  * </p>
  * <p><b>Exits:</b>
@@ -45,7 +51,6 @@ import org.apache.commons.lang.StringUtils;
  * <tr><th>state</th><th>condition</th><th>remarks</th></tr>
  * <tr><td>success</td><td>Successful login to LDAP</td><td>should be defined in configuration</td></tr>
  * <tr><td>invalid</td><td>Unsuccessful login to LDAP</td><td>should be defined in configuration</td></tr>
- * <tr><td>error</td><td>Exception occured in the pipe</td></tr>
  * </table>
  * </p>
  * 
@@ -53,7 +58,26 @@ import org.apache.commons.lang.StringUtils;
  * @version Id
  */
 public class LdapChallengePipe extends AbstractPipe {
-	public static String version = "$RCSfile: LdapChallengePipe.java,v $  $Revision: 1.3 $ $Date: 2007-05-16 11:42:14 $";
+	public static String version = "$RCSfile: LdapChallengePipe.java,v $  $Revision: 1.4 $ $Date: 2007-08-03 08:45:28 $";
+
+	private String ldapProviderURL=null;
+	private String initialContextFactoryName=null;
+
+	public void configure() throws ConfigurationException {
+		super.configure();
+		if (StringUtils.isEmpty(ldapProviderURL) && getParameterList().findParameter("ldapProviderURL")==null) {
+			throw new ConfigurationException("ldapProviderURL must be specified, either as attribute or as parameter");
+		}
+		if (StringUtils.isNotEmpty(ldapProviderURL) && getParameterList().findParameter("ldapProviderURL")!=null) {
+			throw new ConfigurationException("ldapProviderURL can only be specified once, either as attribute or as parameter");
+		}
+		if (getParameterList().findParameter("principal")==null) {
+			throw new ConfigurationException("Parameter 'principal' must be specified");
+		}
+		if (getParameterList().findParameter("credentials")==null) {
+			throw new ConfigurationException("Parameter 'credentials' must be specified");
+		}
+	}
 
 	/** 
 	 * Checks to see if the supplied parameteres of the pipe can login to LDAP 
@@ -66,35 +90,41 @@ public class LdapChallengePipe extends AbstractPipe {
 		String ldapProviderURL;
 		String credentials;
 		String principal;
-		String initialContextFactoryName;
 					
 		try {
 			ParameterResolutionContext prc = new ParameterResolutionContext((String)msg, pls);
 			HashMap paramMap = prc.getValueMap(getParameterList());
-			ldapProviderURL = (String)paramMap.get("ldapProviderURL");
+			if (StringUtils.isNotEmpty(getLdapProviderURL())) {
+				ldapProviderURL = getLdapProviderURL();
+			} else {
+				ldapProviderURL = (String)paramMap.get("ldapProviderURL");
+			}
 			credentials = (String)paramMap.get("credentials");
 			principal = (String)paramMap.get("principal");
-			initialContextFactoryName = (String)paramMap.get("initialContextFactoryName");
 		} catch (ParameterException e) {
 			throw new PipeRunException(this, "Invalid parameter", e);
 		}
 			
-		if (StringUtils.isEmpty(ldapProviderURL) ||
-		    StringUtils.isEmpty(credentials) || 
-			StringUtils.isEmpty(principal) ||
-			StringUtils.isEmpty(ldapProviderURL)
-			) {
-			String paramfields = "ldapProviderURL: " + ldapProviderURL;
-			paramfields = paramfields + " credentials: " + credentials;
-			paramfields = paramfields + " principal: " + principal;
-			paramfields = paramfields + " ldapProviderURL " + ldapProviderURL;   	
-			throw new PipeRunException(this, "One of the following required parameters isEmpty: " + paramfields);			
+		if (StringUtils.isEmpty(ldapProviderURL)) {
+			throw new PipeRunException(this, "ldapProviderURL is empty");			
+		}
+		if (StringUtils.isEmpty(principal)) {
+			throw new PipeRunException(this, "principal is empty");			
+		}
+		if (StringUtils.isEmpty(credentials)) {
+			throw new PipeRunException(this, "credentials are empty");			
 		}
 			
-//		ldapSender.setEntryName("dummy");
+		Parameter dummyEntryName =  new Parameter();
+		dummyEntryName.setName("entryName");
+		dummyEntryName.setValue(principal);
+		ldapSender.addParameter(dummyEntryName);
+			
 		ldapSender.setUsePooling(false);
 		ldapSender.setLdapProviderURL(ldapProviderURL);
-		ldapSender.setInitialContextFactoryName(initialContextFactoryName);
+		if (StringUtils.isNotEmpty(getInitialContextFactoryName())) {
+			ldapSender.setInitialContextFactoryName(getInitialContextFactoryName());
+		}
 		ldapSender.setPrincipal(principal);
 		ldapSender.setCredentials(credentials);
 		ldapSender.setOperation(LdapSender.OPERATION_READ);
@@ -110,5 +140,18 @@ public class LdapChallengePipe extends AbstractPipe {
 		return new PipeRunResult(findForward("success"), msg);
 	}
 
+	public void setLdapProviderURL(String string) {
+		ldapProviderURL = string;
+	}
+	public String getLdapProviderURL() {
+		return ldapProviderURL;
+	}
+
+	public void setInitialContextFactoryName(String value) {
+		initialContextFactoryName = value;
+	}
+	public String getInitialContextFactoryName() {
+		return initialContextFactoryName;
+	}
 
 }
