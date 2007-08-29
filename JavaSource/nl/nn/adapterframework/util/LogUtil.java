36a8005abef1c8ca753be5c29a541e9703345c42
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-08-29 15:13:04 +0000

    enables use of log4j4ibis.properties

--- JavaSource/nl/nn/adapterframework/util/LogUtil.java
+++ JavaSource/nl/nn/adapterframework/util/LogUtil.java
@@ -1,6 +1,9 @@
 /*
  * $Log: LogUtil.java,v $
- * Revision 1.2  2007-02-12 15:56:27  europe\L190409
+ * Revision 1.3  2007-08-29 15:13:04  europe\L190409
+ * enables use of log4j4ibis.properties
+ *
+ * Revision 1.2  2007/02/12 15:56:27  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * configure logging hierarchy from log4j.properties
  *
  * Revision 1.1  2007/02/12 14:10:36  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -19,12 +22,14 @@ import org.apache.log4j.spi.RootCategory;
 
 /**
  * Convenience functions for logging.
+ * Enables a separate log4j configuartion for each Ibis-instance.
+ * Searches first for log4j4ibis.properties on the classpath. If not found, then searches for log4j.properties.
  * 
  * @author  Gerrit van Brakel
  * @version Id
  */
 public class LogUtil {
-	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.2 $ $Date: 2007-02-12 15:56:27 $";
+	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.3 $ $Date: 2007-08-29 15:13:04 $";
 
 	private static Hierarchy hierarchy=null;
 	
@@ -45,7 +50,13 @@ public class LogUtil {
 	public static synchronized Hierarchy getHierarchy() {
 		if (hierarchy==null) {
 			hierarchy = new Hierarchy(new RootCategory(Level.DEBUG));
-			URL url = ClassUtils.getResourceURL(LogUtil.class, "log4j.properties");
+			URL url = ClassUtils.getResourceURL(LogUtil.class, "log4j4ibis.properties");
+			if (url==null) {
+				url = ClassUtils.getResourceURL(LogUtil.class, "log4j.properties");
+			}
+			if (url==null) {
+				System.err.println("Did not find log4j4ibis.properties or log4j.properties on classpath. Cannot configure log4j properly.");
+			}
 			new PropertyConfigurator().doConfigure(url, hierarchy);
 		}
 		return hierarchy;
