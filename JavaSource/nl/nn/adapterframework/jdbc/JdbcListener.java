851ed71a3ff775303466a4e9b4b7d420a88d198c
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-09-12 09:17:55 +0000

    first working version

--- JavaSource/nl/nn/adapterframework/jdbc/JdbcListener.java
+++ JavaSource/nl/nn/adapterframework/jdbc/JdbcListener.java
@@ -1,6 +1,9 @@
 /*
  * $Log: JdbcListener.java,v $
- * Revision 1.1  2007-09-11 11:53:01  europe\L190409
+ * Revision 1.2  2007-09-12 09:17:55  europe\L190409
+ * first working version
+ *
+ * Revision 1.1  2007/09/11 11:53:01  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * added JdbcListeners
  *
  */
@@ -13,14 +16,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 
-import javax.transaction.UserTransaction;
-
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.IPullingListener;
 import nl.nn.adapterframework.core.ListenerException;
 import nl.nn.adapterframework.core.PipeLineResult;
-import nl.nn.adapterframework.util.JdbcUtil;
-import nl.nn.adapterframework.util.JtaUtil;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -113,32 +112,48 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 	protected Object getRawMessage(Connection conn, HashMap threadContext) throws ListenerException {
 		try {
 			execute(conn,getLockQuery());
-			UserTransaction utx = JtaUtil.getUserTransaction();
-			utx.begin();
+		} catch (Throwable t) {
+			log.debug(getLogPrefix()+"was not able to obtain lock: "+t.getMessage());
+			return null;
+		}
+		try {
+			Statement stmt= null;
 			try {
+//				log.debug("creating statement for ["+getSelectQuery()+"]");
+				stmt = conn.createStatement();
 				ResultSet rs=null;
 				try {
-					rs = executeQuery(conn,getSelectQuery());
+//					log.debug("executing query for ["+getSelectQuery()+"]");
+					rs = stmt.executeQuery(getSelectQuery());
+					if (rs.isAfterLast() || !rs.next()) {
+						return null;
+					}
 					String key=rs.getString(getKeyField());
 					threadContext.put(KEY_FIELD_KEY,key);
-					threadContext.put(MESSAGE_FIELD_KEY,rs.getString(getMessageField()));
+					if (StringUtils.isNotEmpty(getMessageField())) {
+						threadContext.put(MESSAGE_FIELD_KEY,rs.getString(getMessageField()));
+					}
 					execute(conn,getUpdateStatusToInProcessQuery(),key);
-					utx.commit();
 					return key;
-				} catch (SQLException e) {
-					throw new ListenerException(getLogPrefix() + "caught exception retrieving message", e);
 				} finally {
-					JdbcUtil.fullClose(rs);
+					if (rs!=null) {
+//						log.debug("closing resultset");
+						rs.close();
+					}
 				}
+						
 			} finally {
-				if (JtaUtil.inTransaction(utx)) {
-					utx.rollback();
+				try {
+					if (stmt!=null) {
+//						log.debug("closing statement");
+						stmt.close();
+					}
+				} finally {
+					execute(conn,getUnlockQuery());
 				}
 			}
 		} catch (Exception e) {
-			throw new ListenerException(getLogPrefix() + "cannot manage UserTransaction", e);
-		} finally {
-			execute(conn,getUnlockQuery());
+			throw new ListenerException(getLogPrefix() + "caught exception retrieving message", e);
 		}
 	}
 
@@ -148,16 +163,21 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 	}
 
 	public String getStringFromRawMessage(Object rawMessage, HashMap context) throws ListenerException {
-		String message = (String)context.get(MESSAGE_FIELD_KEY);
+		String message;
+		if (StringUtils.isNotEmpty(getMessageField())) {
+			message = (String)context.get(MESSAGE_FIELD_KEY);
+		} else {
+			message = (String)context.get(KEY_FIELD_KEY);
+		}
 		return message;
 	}
 
 	public void afterMessageProcessed(Connection c, PipeLineResult processResult, HashMap context) throws ListenerException {
 		String key=(String)context.get(KEY_FIELD_KEY);
 		if (processResult==null || "success".equals(processResult.getState())) {
-			execute(c,getUpdateStatusToProcessedQuery());
+			execute(c,getUpdateStatusToProcessedQuery(),key);
 		} else {
-			execute(c,getUpdateStatusToErrorQuery());
+			execute(c,getUpdateStatusToErrorQuery(),key);
 		}
 	}
 
@@ -165,6 +185,7 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 		if (isConnectionsArePooled()) {
 			Connection c = null;
 			try {
+//				log.debug("getting connection");
 				c = getConnection();
 				afterMessageProcessed(c,processResult, context);
 			} catch (JdbcException e) {
@@ -172,6 +193,7 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 			} finally {
 				if (c!=null) {
 					try {
+//						log.debug("closing connection");
 						c.close();
 					} catch (SQLException e) {
 						log.warn(new ListenerException(getLogPrefix() + "caught exception closing connection in afterMessageProcessed()", e));
@@ -186,23 +208,24 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 	}
 	
 	protected ResultSet executeQuery(Connection conn, String query) throws ListenerException {
-		if (StringUtils.isNotEmpty(query)) {
+		if (StringUtils.isEmpty(query)) {
 			throw new ListenerException(getLogPrefix()+"cannot execute empty query");
 		}
+//		log.debug("executing query ["+query+"]");
 		Statement stmt=null;
 		try {
 			stmt = conn.createStatement();
 			return stmt.executeQuery(query);
 		} catch (SQLException e) {
-			throw new ListenerException(getLogPrefix()+"exception executing statement ["+query+"]",e);
-		} finally {
 			if (stmt!=null) {
 				try {
+//					log.debug("closing statement for ["+query+"]");
 					stmt.close();
-				} catch (SQLException e) {
-					log.warn(getLogPrefix()+"exception closing statement ["+query+"]",e);
+				} catch (Throwable t) {
+					log.warn(getLogPrefix()+"exception closing statement ["+query+"]", t);
 				}
 			}
+			throw new ListenerException(getLogPrefix()+"exception executing statement ["+query+"]",e);
 		}
 	}
 
@@ -212,20 +235,23 @@ public class JdbcListener extends JdbcFacade implements IPullingListener {
 
 	protected void execute(Connection conn, String query, String parameter) throws ListenerException {
 		if (StringUtils.isNotEmpty(query)) {
+//			log.debug("executing query ["+query+"]");
 			PreparedStatement stmt=null;
 			try {
 				stmt = conn.prepareStatement(query);
 				stmt.clearParameters();
 				if (StringUtils.isNotEmpty(parameter)) {
+					log.debug("setting parameter 1 to ["+parameter+"]");
 					stmt.setString(1,parameter);
 				}
-				stmt.execute(query);
+				stmt.execute();
 				
 			} catch (SQLException e) {
 				throw new ListenerException(getLogPrefix()+"exception executing statement ["+query+"]",e);
 			} finally {
 				if (stmt!=null) {
 					try {
+//						log.debug("closing statement for ["+query+"]");
 						stmt.close();
 					} catch (SQLException e) {
 						log.warn(getLogPrefix()+"exception closing statement ["+query+"]",e);
