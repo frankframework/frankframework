df38931a18faecd85ff8e3c4f218a692e8dbf267
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-10-09 15:02:54 +0000

    Direct copy from Ibis-EJB: delegate work to IbisManager

--- JavaSource/nl/nn/adapterframework/cmdline/StartIbis.java
+++ JavaSource/nl/nn/adapterframework/cmdline/StartIbis.java
@@ -1,92 +1,36 @@
 /*
- * Created on 26-apr-04
+ * $Log: StartIbis.java,v $
+ * Revision 1.6  2007-10-09 15:02:54  europe\L190409
+ * Direct copy from Ibis-EJB:
+ * delegate work to IbisManager
  *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 package nl.nn.adapterframework.cmdline;
 
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectInstance;
-
-import nl.nn.adapterframework.configuration.Configuration;
-import nl.nn.adapterframework.configuration.ConfigurationDigester;
-import nl.nn.adapterframework.util.ClassUtils;
-import nl.nn.adapterframework.util.LogUtil;
-
-import org.apache.log4j.Logger;
+import nl.nn.adapterframework.configuration.IbisMain;
+import nl.nn.adapterframework.configuration.IbisManager;
 
 /**
- *  
- * Configures a queue connection and receiver and starts listening to it.
- * @author     Johan Verrips
- * created    14 februari 2003
+ * Starts up a configuration in a plain JVM.
+ * 
+ * Works only for pulling listeners.
+ * 
+ * @author  Johan Verrips
+ * @version Id
  */
 public class StartIbis {
-	public static final String version="$RCSfile: StartIbis.java,v $ $Revision: 1.5 $ $Date: 2007-02-12 13:37:59 $";
-	private  Logger log = LogUtil.getLogger(this);
+	public static final String version="$RCSfile: StartIbis.java,v $ $Revision: 1.6 $ $Date: 2007-10-09 15:02:54 $";
 
-	static final String DFLT_DIGESTER_RULES = "digester-rules.xml";
-	static final String DFLT_CONFIGURATION = "Configuration.xml";
-	static final String DFLT_AUTOSTART = "TRUE";
-
-	/**
-	 * 
-	 */
 	public StartIbis() {
 		super();
 	}
 	public static void main(String[] args) {
-		StartIbis si=new StartIbis();
-		si.initConfig(DFLT_CONFIGURATION, DFLT_DIGESTER_RULES, DFLT_AUTOSTART);
-
+        String configFile = IbisManager.DFLT_CONFIGURATION;
+        if (args.length > 0) {
+            configFile = args[0];
+        }
+        IbisMain im=new IbisMain();
+        im.initConfig(null, configFile, 
+            IbisMain.DFLT_AUTOSTART);
 	}
-	public boolean initConfig(
-		String configurationFile,
-		String digesterRulesFile,
-		String autoStart) {
-		Configuration config = null;
-		ConfigurationDigester cd = new ConfigurationDigester();
-		//Start MBean server
-		MBeanServer server=MBeanServerFactory.createMBeanServer();
-		try {
-		ObjectInstance html = server.createMBean("com.sun.jdmk.comm.HtmlAdaptorServer", 
-		null);
-            
-		server.invoke(html.getObjectName(), "start", new Object[0], new String[0]);
-		} catch (Exception e) {
-			log.error("Error with jmx:",e);
-		}
-		log.info("MBean server up and running. Monitor your application by pointing your browser to http://localhost:8082");
-		if (null == configurationFile)
-			configurationFile = DFLT_CONFIGURATION;
-		if (null == digesterRulesFile)
-			digesterRulesFile = DFLT_DIGESTER_RULES;
-		if (null == autoStart)
-			autoStart = DFLT_AUTOSTART;
-		try {
-			config =
-				cd.unmarshalConfiguration(
-					ClassUtils.getResourceURL(cd, digesterRulesFile),
-					ClassUtils.getResourceURL(cd, configurationFile));
-		} catch (Throwable e) {
-			log.error("Error occured unmarshalling configuration:", e);
-		}
-		// if configuration did not succeed, log and return.
-		if (null == config) {
-			log.error(
-				"Error occured unmarshalling configuration. See previous messages.");
-			return false;
-		}
-
-		if (autoStart.equalsIgnoreCase("TRUE")) {
-			log.info("Starting configuration");
-			config.startAdapters();
-		}
-		log.info(
-			"****" + "********** ConfigurationServlet complete **************");
-		return true;
-	}
-
 }
