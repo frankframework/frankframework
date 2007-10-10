5a2a3b3365ee770f9093ae1a23a527fc76a43e36
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-10-10 08:26:17 +0000

    added ToDo + cleanup

--- JavaSource/nl/nn/adapterframework/jms/JmsRealmFactoryBean.java
+++ JavaSource/nl/nn/adapterframework/jms/JmsRealmFactoryBean.java
@@ -1,17 +1,26 @@
+/*
+ * $Log: JmsRealmFactoryBean.java,v $
+ * Revision 1.4  2007-10-10 08:26:17  europe\L190409
+ * added ToDo + cleanup
+ *
+ */
 package nl.nn.adapterframework.jms;
 
 /**
  * Wrapper around the JmsRealmFactory, especially for the digester.
- * @version Id
+ * TODO: Possible to remove this class by putting JmsRealmFactory in Spring context
+ * 
  * @author Johan Verrips IOS
+ * @version Id
  */
 public class JmsRealmFactoryBean  {
+
 	private JmsRealmFactory jmsRealmFactory;
+
 	public JmsRealmFactoryBean(){
 		jmsRealmFactory=JmsRealmFactory.getInstance();
 	}
 	public void registerJmsRealm(JmsRealm jmsRealm) {
-	jmsRealmFactory.registerJmsRealm(jmsRealm);
-
-}
+		jmsRealmFactory.registerJmsRealm(jmsRealm);
+	}
 }
