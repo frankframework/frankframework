511b3b2d6d8c7c5dbfba2bf94cecb3e2a3102820
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2007-10-10 08:54:27 +0000

    added getAdapter()

--- JavaSource/nl/nn/adapterframework/receivers/GenericReceiver.java
+++ JavaSource/nl/nn/adapterframework/receivers/GenericReceiver.java
@@ -1,6 +1,9 @@
 /*
  * $Log: GenericReceiver.java,v $
- * Revision 1.4  2007-05-21 12:24:20  europe\L190409
+ * Revision 1.5  2007-10-10 08:54:27  europe\L190409
+ * added getAdapter()
+ *
+ * Revision 1.4  2007/05/21 12:24:20  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * added public setMessageLog()
  *
  * Revision 1.3  2005/07/19 15:27:45  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -24,6 +27,7 @@
  */
 package nl.nn.adapterframework.receivers;
 
+import nl.nn.adapterframework.core.IAdapter;
 import nl.nn.adapterframework.core.IListener;
 import nl.nn.adapterframework.core.ISender;
 import nl.nn.adapterframework.core.ITransactionalStorage;
@@ -39,7 +43,7 @@ import nl.nn.adapterframework.core.ITransactionalStorage;
  * @since   4.1
  */
 public class GenericReceiver extends ReceiverBase {
-	public static final String version="$RCSfile: GenericReceiver.java,v $ $Revision: 1.4 $ $Date: 2007-05-21 12:24:20 $";
+	public static final String version="$RCSfile: GenericReceiver.java,v $ $Revision: 1.5 $ $Date: 2007-10-10 08:54:27 $";
 
 	public void setListener(IListener listener) {
 		super.setListener(listener);
@@ -60,5 +64,8 @@ public class GenericReceiver extends ReceiverBase {
 	public void setSender(ISender sender) {
 		super.setSender(sender);
 	}
-
+    
+    public IAdapter getAdapter() {
+        return super.getAdapter();
+    }
 }
