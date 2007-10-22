26d3e587a48b4b9f33d65538843e0847d67b4649
Tim van der Leeuw <tim.van.der.leeuw@ibissource.org>
2007-10-22 14:56:21 +0000

    Make the 'Generic Factory' more generically useful by allowing beans to be created based of name of element in IBIS Configuration file.

--- JavaSource/nl/nn/adapterframework/configuration/GenericFactory.java
+++ JavaSource/nl/nn/adapterframework/configuration/GenericFactory.java
@@ -1,6 +1,9 @@
 /*
  * $Log: GenericFactory.java,v $
- * Revision 1.2  2007-10-09 15:29:43  europe\L190409
+ * Revision 1.3  2007-10-22 14:56:21  europe\M00035F
+ * Make the 'Generic Factory' more generically useful by allowing beans to be created based of name of element in IBIS Configuration file.
+ *
+ * Revision 1.2  2007/10/09 15:29:43  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * Direct copy from Ibis-EJB:
  * first version in HEAD
  *
@@ -10,16 +13,33 @@ package nl.nn.adapterframework.configuration;
 /**
  * Generic factory for instantiating beans from the Digester framework.
  * 
- * This factory always returns <code>null</code> for name of the bean,
- * so that beans can never be looked up from the Spring Factory by
- * name but always have to be looked up by className attribute.
- * 
+ * <p>
+ * This factory uses the name of the current element for name of the bean,
+ * instead of hard-wiring a bean name. The name of current element is prefixed
+ * with the string "proto-", to prevent unwanted auto-wiring cascaded of
+ * other prototype beans defined in the Spring Factory which are supposed to
+ * be defined in the IBIS Configuration File. 
+ * </p>
+ * <p>
+ * If a className attribute is specified in the configuration file, then
+ * this is used together with the bean-name to find the bean to be
+ * instantiated from the Spring Factory (see the rules laid out in
+ * {@link AbstractSpringPoweredDigesterFactory}.
+ * In particular, the bean-name is ignored when the class-name is specified
+ * and the Spring Factory contains exactly 1 bean-definition for that
+ * class.
+ * </p>
+ * <p>
  * This is useful for those kinds of rules in the digester-rules.xml where
- * the className was mandatory anyways.
- *
- * NB: It doesn't help to add a settable property for the bean-name, because
- * the Apache Digester cannot read a factory-create-rule from XML and supply
- * parameters to the factory created from the XML.
+ * the className was always mandatory in older versions, but also for those rules
+ * where className is never specified and only 1 possible implementation
+ * exists.
+ * </p>
+ * <p>
+ * NB: The Apache Digester cannot read a factory-create-rule from XML and supply
+ * parameters to the factory created from the XML digester-rules, so there is
+ * no way to configure a factory instance with a bean-name from the digester-rules.
+ * </p>
  * 
  * @author  Tim van der Leeuw
  * @since   4.8
@@ -28,14 +48,13 @@ package nl.nn.adapterframework.configuration;
 public class GenericFactory extends AbstractSpringPoweredDigesterFactory {
 
     /**
-     * Return <code>null</code> as bean-name so that beans are always
-     * looked up by class (or auto-wired if not in factory), and never
-     * looked up by name.
+     * Return name of current element prefixed with the string "proto-" as 
+     * bean-name.
      * 
      * @see nl.nn.adapterframework.configuration.AbstractSpringPoweredDigesterFactory#getBeanName()
      */
     public String getBeanName() {
-        return null;
+        return "proto-" + getDigester().getCurrentElementName();
     }
 
 }
