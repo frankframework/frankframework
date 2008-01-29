b080102b187e3f589f429001c9250b981e73fc82
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-01-29 15:39:21 +0000

    moved some code to baseclass added support for dynamic selection of sapsystem

--- JavaSource/nl/nn/adapterframework/extensions/sap/SapSender.java
+++ JavaSource/nl/nn/adapterframework/extensions/sap/SapSender.java
@@ -1,6 +1,10 @@
 /*
  * $Log: SapSender.java,v $
- * Revision 1.8  2007-05-02 11:33:49  europe\L190409
+ * Revision 1.9  2008-01-29 15:39:21  europe\L190409
+ * moved some code to baseclass
+ * added support for dynamic selection of sapsystem
+ *
+ * Revision 1.8  2007/05/02 11:33:49  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * support for handling parameters
  *
  * Revision 1.7  2007/05/01 14:22:43  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -34,12 +38,10 @@
 package nl.nn.adapterframework.extensions.sap;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
-import nl.nn.adapterframework.core.ISenderWithParameters;
 import nl.nn.adapterframework.core.SenderException;
 import nl.nn.adapterframework.core.TimeOutException;
-import nl.nn.adapterframework.parameters.Parameter;
-import nl.nn.adapterframework.parameters.ParameterList;
 import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import nl.nn.adapterframework.parameters.ParameterValue;
 import nl.nn.adapterframework.parameters.ParameterValueList;
 
 import org.apache.commons.lang.StringUtils;
@@ -54,19 +56,23 @@ import com.sap.mw.jco.JCO;
  * <tr><td>className</td><td>nl.nn.adapterframework.extensions.sap.SapListener</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setName(String) name}</td><td>Name of the Sender</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setFunctionName(String) functionName}</td><td>Name of the RFC-function to be called in the SAP system</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setFunctionNameParam(String) functionNameParam}</td><td>name of the parameter used to obtain the functionName from if the attribute functionName is empty</td><td>functionName</td></tr>
  * <tr><td>{@link #setSapSystemName(String) sapSystemName}</td><td>name of the SapSystem used by this object</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setSapSystemParam(String) sapSystemNameParam}</td><td>name of the parameter used to indicate the name of the SapSystem used by this object if the attribute sapSystem is empty</td><td>sapSystemName</td></tr>
  * <tr><td>{@link #setCorrelationIdFieldIndex(int) correlationIdFieldIndex}</td><td>Index of the field in the ImportParameterList of the RFC function that contains the correlationId</td><td>0</td></tr>
  * <tr><td>{@link #setCorrelationIdFieldName(String) correlationIdFieldName}</td><td>Name of the field in the ImportParameterList of the RFC function that contains the correlationId</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setRequestFieldIndex(int) requestFieldIndex}</td><td>Index of the field in the ImportParameterList of the RFC function that contains the whole request message contents</td><td>0</td></tr>
  * <tr><td>{@link #setRequestFieldName(String) requestFieldName}</td><td>Name of the field in the ImportParameterList of the RFC function that contains the whole request message contents</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setReplyFieldIndex(int) replyFieldIndex}</td><td>Index of the field in the ExportParameterList of the RFC function that contains the whole reply message contents</td><td>0</td></tr>
  * <tr><td>{@link #setReplyFieldName(String) replyFieldName}</td><td>Name of the field in the ExportParameterList of the RFC function that contains the whole reply message contents</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setLuwHandleSessionKey(String) luwHandleSessionKey}</td><td>session key in which LUW information is stored. When set, the action will be executed transacted.</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setSynchronous(boolean) synchronous}</td><td>synchronous functions return a result, a-synchronous functions are excecuted in a transaction</td><td>true</td></tr>
+ * <tr><td>{@link #setLuwHandleSessionKey(String) luwHandleSessionKey}</td><td>session key in which LUW information is stored. When set, actions that share a LUW-handle will be executed using the same client. Can only be used for synchronous functions</td><td>&nbsp;</td></tr>
  * </table>
  * </p>
  * <table border="1">
  * <p><b>Parameters:</b>
  * <tr><th>name</th><th>type</th><th>remarks</th></tr>
+ * <tr><td>sapSystemParam</td><td>String</td><td>points to sapSystem to use</td></tr>
  * <tr><td><i>inputfieldname</i></td><td><i>any</i></td><td>The value of the parameter is set to the (simple) input field</td></tr>
  * <tr><td><i>structurename</i>/<i>inputfieldname</i></td><td><i>any</i></td><td>The value of the parameter is set to the named field of the named structure</td></tr>
  * </table>
@@ -77,87 +83,99 @@ import com.sap.mw.jco.JCO;
  * @author Gerrit van Brakel
  * @since 4.2
  */
-public class SapSender extends SapFunctionFacade implements ISenderWithParameters {
-	public static final String version="$RCSfile: SapSender.java,v $  $Revision: 1.8 $ $Date: 2007-05-02 11:33:49 $";
+public class SapSender extends SapSenderBase {
+	public static final String version="$RCSfile: SapSender.java,v $  $Revision: 1.9 $ $Date: 2008-01-29 15:39:21 $";
 	
-	//TODO: allow functionName to be set dynamically from a parameter or from the message
 	private String functionName=null;
-	private String luwHandleSessionKey;
+	private String functionNameParam="functionName";
 
-	protected ParameterList paramList = null;
-	
+	public SapSender() {
+		super();
+		setSynchronous(true);
+	}
 	
 	public void configure() throws ConfigurationException {
 		super.configure();
-		if (paramList!=null) {
-			paramList.configure();
-		}
 		if (StringUtils.isEmpty(getFunctionName())) {
-			throw new ConfigurationException(getLogPrefix()+"Function name is mandatory");
+			if (StringUtils.isEmpty(getFunctionNameParam())) {
+				throw new ConfigurationException(getLogPrefix()+"if attribute functionName is not specified, value of attribute functionNameParam must indicate parameter to obtain functionName from");
+			}
+			if (paramList==null || paramList.findParameter(getFunctionNameParam())==null) {
+				throw new ConfigurationException(getLogPrefix()+"functionName must be specified, either in attribute functionName, or via parameter ["+getFunctionNameParam()+"]");
+			}
+		} else {
+			if (StringUtils.isNotEmpty(getFunctionNameParam()) && paramList.findParameter(getFunctionNameParam())!=null) {
+				throw new ConfigurationException(getLogPrefix()+"functionName cannot be specified both in attribute functionName ["+getFunctionName()+"] and via parameter ["+getFunctionNameParam()+"]");
+			}
 		}
 	}
 
 	public void open() throws SenderException {
-		try {
-			openFacade();
-		} catch (SapException e) {
-			close();
-			throw new SenderException(getLogPrefix()+"exception starting SapSender", e);
-		}
+		super.open();
 	}
 	
 	public void close() {
-		closeFacade();
+		super.close();
 	}
 
-	public String sendMessage(String correlationID, String message) throws SenderException, TimeOutException {
-		return sendMessage(correlationID,message,null);
+	public JCO.Function getFunction(SapSystem sapSystem, ParameterValueList pvl) throws SapException {
+		if (StringUtils.isNotEmpty(getSapSystemName()) && StringUtils.isNotEmpty(getFunctionName())) {
+			return getFunctionTemplate().getFunction();
+		}
+		String functionName=getFunctionName();
+		if (StringUtils.isEmpty(functionName)) {
+			if (pvl==null) {
+				throw new SapException("no parameters to determine functionName from");
+			}
+			ParameterValue pv = pvl.getParameterValue(getFunctionNameParam());
+			if (pv==null) {
+				throw new SapException("could not get ParameterValue for parameter ["+getFunctionNameParam()+"]");
+			}
+			functionName = pv.asStringValue(null);
+		}	
+		if (StringUtils.isEmpty(functionName)) {
+			throw new SapException("could not determine functionName using parameter ["+getFunctionNameParam()+"]");
+		}
+		return getFunctionTemplate(sapSystem, functionName).getFunction();
 	}
 
 	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException, TimeOutException {
 		try {
-			JCO.Function function = getFunctionTemplate().getFunction();
-
 			ParameterValueList pvl = null;
 			if (prc!=null) {
 				pvl=prc.getValues(paramList);
 			}
+			SapSystem sapSystem = getSystem(pvl);
+			
+			JCO.Function function=getFunction(sapSystem, pvl);
 
+			if (StringUtils.isEmpty(getSapSystemName())) {
+				pvl.removeParameterValue(getSapSystemNameParam());
+			}
+			if (StringUtils.isEmpty(getFunctionName())) {
+				pvl.removeParameterValue(getFunctionNameParam());
+			}
 		    message2FunctionCall(function, message, correlationID, pvl);
-		    if (StringUtils.isNotEmpty(getLuwHandleSessionKey())) {
-				SapLUWHandle handle = SapLUWHandle.retrieveHandle(prc.getSession(),getLuwHandleSessionKey(),true,getSapSystem(),false);
-				if (handle==null) {
-					throw new SenderException("cannot find LUW handle from session key ["+getLuwHandleSessionKey()+"]");
-				}
-				JCO.Client client = handle.getClient();
-				if (handle.isUseTid()) {
-					client.execute(function,handle.getTid());
+		    if (log.isDebugEnabled()) log.debug(getLogPrefix()+" function call ["+functionCall2message(function)+"]");
+			JCO.Client client = getClient(prc.getSession(), sapSystem);
+			try {
+				String tid = getTid(client,sapSystem);
+				if (StringUtils.isEmpty(tid)) {
+					client.execute(function);
 				} else {
-					client.execute(function);
+					client.execute(function,tid);
 				}
- 		    } else {
-				JCO.Client client = null;
-				try {
-					client = getSapSystem().getClient();
-					client.execute(function);
-				} finally {
-					
-					if (client!=null) {
-						getSapSystem().releaseClient(client);
-					}
-				}
- 		    }
+			} finally {
+				releaseClient(client,sapSystem);
+			}
 			return functionResult2message(function);
 		} catch (Exception e) {
 			throw new SenderException(e);
 		}
 	}
 
-	/**
-	 * allways returns true.
-	 */
-	public boolean isSynchronous() {
-		return true;
+	public void setSynchronous(boolean b) {
+		super.setSynchronous(b);
 	}
 
 
@@ -168,20 +186,12 @@ public class SapSender extends SapFunctionFacade implements ISenderWithParameter
 		functionName = string;
 	}
 
-	public void setLuwHandleSessionKey(String string) {
-		luwHandleSessionKey = string;
-	}
-	public String getLuwHandleSessionKey() {
-		return luwHandleSessionKey;
-	}
 
-	public void addParameter(Parameter p) {
-		if (paramList==null) {
-			paramList=new ParameterList();
-		}
-		paramList.add(p);
+	public void setFunctionNameParam(String string) {
+		functionNameParam = string;
 	}
-
-
+	public String getFunctionNameParam() {
+		return functionNameParam;
+	}
 
 }
