bfd7137144d0fc68604882794f4910f713eedaf8
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-02-07 12:12:51 +0000

    first version

---
+++ JavaSource/nl/nn/adapterframework/pipes/GetLtpaTokenPipe.java
@@ -0,0 +1,47 @@
+/*
+ * $Log: GetLtpaTokenPipe.java,v $
+ * Revision 1.1  2008-02-07 12:12:51  europe\L190409
+ * first version
+ *
+ */
+package nl.nn.adapterframework.pipes;
+
+import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.core.PipeRunException;
+import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.util.SsoUtil;
+
+/**
+ * Returns the LTPA token associated with the session.
+ *
+ * <p><b>Configuration:</b>
+ * <table border="1">
+ * <tr><th>attributes</th><th>description</th><th>default</th></tr>
+ * <tr><td>{@link #setName(String) name}</td><td>name of the Pipe</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setForwardName(String) forwardName}</td>  <td>name of forward returned upon completion</td><td>"success"</td></tr>
+ * <tr><td>{@link #setStoreResultInSessionKey(String) storeResultInSessionKey}</td><td>when set, the result is stored under this session key</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setPreserveInput(boolean) preserveInput}</td><td>when set <code>true</code>, the input of a pipe is restored before processing the next one</td><td>false</td></tr>
+ * </table>
+ * </p>
+ * <p><b>Exits:</b>
+ * <table border="1">
+ * <tr><th>state</th><th>condition</th></tr>
+ * <tr><td>"success"</td><td>default</td></tr>
+ * <tr><td><i>{@link #setForwardName(String) forwardName}</i></td><td>if specified</td></tr>
+ * </table>
+ * </p>
+ * 
+ * @author  Gerrit van Brakel
+ * @since   4.8
+ * @version Id
+ */
+public class GetLtpaTokenPipe extends FixedForwardPipe {
+	
+	public PipeRunResult doPipe(Object input, PipeLineSession session) throws PipeRunException {
+		try {
+			return new PipeRunResult(getForward(),SsoUtil.getSsoToken());
+		} catch (Exception e) {
+			throw new PipeRunException(this, "Could not obtain LtpaToken",e);
+		}
+	}
+}
