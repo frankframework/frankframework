2d543950b64fca4ec30a9f6e84aea296ae33b733
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-02-13 12:58:41 +0000

    now uses ProcessUtils

--- JavaSource/nl/nn/adapterframework/pipes/ExecutePipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/ExecutePipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: ExecutePipe.java,v $
- * Revision 1.2  2007-07-10 07:52:29  europe\L190409
+ * Revision 1.3  2008-02-13 12:58:41  europe\L190409
+ * now uses ProcessUtils
+ *
+ * Revision 1.2  2007/07/10 07:52:29  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * cosmetic changes
  *
  * Revision 1.1  2006/08/22 12:56:32  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -9,13 +12,11 @@
  */
 package nl.nn.adapterframework.pipes;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.core.SenderException;
+import nl.nn.adapterframework.util.ProcessUtil;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -34,13 +35,12 @@ import org.apache.commons.lang.StringUtils;
  * @author Jaco de Groot (***@dynasol.nl)
  */
 public class ExecutePipe extends FixedForwardPipe {
-	public static final String version = "$RCSfile: ExecutePipe.java,v $ $Revision: 1.2 $ $Date: 2007-07-10 07:52:29 $";
+	public static final String version = "$RCSfile: ExecutePipe.java,v $ $Revision: 1.3 $ $Date: 2008-02-13 12:58:41 $";
 	
 	private String command;
 	private String commandSessionKey;
 
 	public PipeRunResult doPipe(Object input, PipeLineSession session) throws PipeRunException {
-		StringBuffer result = new StringBuffer();
 		String command;
 		if (StringUtils.isNotEmpty(getCommand())) {
 			command = getCommand();
@@ -50,33 +50,10 @@ public class ExecutePipe extends FixedForwardPipe {
 			command = (String)input;
 		}
 		try {
-			Process process = Runtime.getRuntime().exec(command);
-			// Read the output of the process
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line = null;
-			// read() instead of readLine() results in JVM core dumps (this
-			// also happens when using InputStream or BufferedInputStream)
-			// using WebSphere Studio Application Developer (Windows) 
-			// Version: 5.1.2, Build id: 20040506_1735
-			while ((line = bufferedReader.readLine()) != null) {
-				result.append(line + "\n");
-			}
-			// Wait until the process is completely finished
-			try {
-				process.waitFor();
-			} catch(InterruptedException e) {
-				throw new PipeRunException(this, "Interrupted while waiting for process",e);
-			}
-			// Throw an exception if the command returns an error exit value
-			int exitValue = process.exitValue();
-			if (exitValue != 0) {
-				throw new PipeRunException(this, "Error exit value '" + exitValue + "' (not 0) for command '" + command + "', process output was: " + result.toString());
-			}
-		} catch(IOException e) {
-			throw new PipeRunException(this, "Error executing command [" + command + "]", e);
+			return new PipeRunResult(getForward(), ProcessUtil.executeCommand(command));
+		} catch(SenderException e) {
+			throw new PipeRunException(this, "Error executing command", e);
 		}
-		// Return result
-		return new PipeRunResult(getForward(), result.toString());
 	}
 
 	public void setCommand(String command) {
