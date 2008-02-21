38e694fcef5b1f8890a8f99864a23f186cee2e50
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-02-21 12:42:56 +0000

    fixed default of script options to delete or backup file after sending

--- JavaSource/nl/nn/adapterframework/extensions/fxf/FxfSender.java
+++ JavaSource/nl/nn/adapterframework/extensions/fxf/FxfSender.java
@@ -1,6 +1,10 @@
 /*
  * $Log: FxfSender.java,v $
- * Revision 1.2  2008-02-19 09:39:27  europe\L190409
+ * Revision 1.3  2008-02-21 12:42:56  europe\L190409
+ * fixed default of script
+ * options to delete or backup file after sending
+ *
+ * Revision 1.2  2008/02/19 09:39:27  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * updated javadoc
  *
  * Revision 1.1  2008/02/13 12:53:53  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -9,9 +13,14 @@
  */
 package nl.nn.adapterframework.extensions.fxf;
 
+import java.io.File;
+
 import nl.nn.adapterframework.core.SenderException;
 import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import nl.nn.adapterframework.util.FileUtils;
 import nl.nn.adapterframework.util.ProcessUtil;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Sender for transferring files using the FxF protocol. Assumes pipe input is local name
@@ -19,8 +28,12 @@ import nl.nn.adapterframework.util.ProcessUtil;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>className</td><td>nl.nn.adapterframework.extensions.fxf.FxfListener</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setScript(String) script}</td><td>full pathname to the FXF script to be executed to transfer the file</td><td>/usr/local/bin/FXF_put</td></tr>
+ * <tr><td>{@link #setScript(String) script}</td><td>full pathname to the FXF script to be executed to transfer the file</td><td>/usr/local/bin/FXF_init</td></tr>
  * <tr><td>{@link #setTransfername(String) transfername}</td><td>FXF transfername</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setProcessedDirectory(String) processedDirectory}</td><td>Directory where files are stored after being processed</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setNumberOfBackups(int) numberOfBackups}</td><td>number of copies held of a file with the same name. Backup files have a dot and a number suffixed to their name. If set to 0, no backups will be kept.</td><td>5</td></tr>
+ * <tr><td>{@link #setOverwrite(boolean) overwrite}</td><td>when set <code>true</code>, the destination file will be deleted if it already exists</td><td>false</td></tr>
+ * <tr><td>{@link #setDelete(boolean) delete}</td><td>when set <code>true</code>, the file processed will deleted after being processed, and not stored</td><td>true</td></tr>
  * </table>
  * </p>
  * 
@@ -32,11 +45,26 @@ public class FxfSender {
 
 	private String script="/usr/local/bin/FXF_init";
 	private String transfername;
+	private String processedDirectory;
+	private int numberOfBackups = 0;
+	private boolean overwrite = false;
+	private boolean delete = true;
 
 	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException {
 //		return super.sendMessage(correlationID, message, prc);
 		String command = getScript()+" put "+getTransfername() +" "+message;
-		return ProcessUtil.executeCommand(command);
+		String result=ProcessUtil.executeCommand(command);
+		
+		// delete file or move it to processed directory
+		if (isDelete() || StringUtils.isNotEmpty(getProcessedDirectory())) {
+			File f=new File(message);
+			try {
+				FileUtils.moveFileAfterProcessing(f, getProcessedDirectory(), isDelete(), isOverwrite(), getNumberOfBackups()); 
+			} catch (Exception e) {
+				throw new SenderException("Could not move file ["+message+"]",e);
+			}
+		}
+		return result;
 	}
 
 	public void setScript(String string) {
@@ -51,6 +79,34 @@ public class FxfSender {
 	}
 	public String getTransfername() {
 		return transfername;
+	}
+
+	public void setProcessedDirectory(String processedDirectory) {
+		this.processedDirectory = processedDirectory;
+	}
+	public String getProcessedDirectory() {
+		return processedDirectory;
+	}
+
+	public void setNumberOfBackups(int i) {
+		numberOfBackups = i;
+	}
+	public int getNumberOfBackups() {
+		return numberOfBackups;
+	}
+
+	public void setOverwrite(boolean b) {
+		overwrite = b;
+	}
+	public boolean isOverwrite() {
+		return overwrite;
+	}
+
+	public void setDelete(boolean b) {
+		delete = b;
+	}
+	public boolean isDelete() {
+		return delete;
 	}
 
 }
