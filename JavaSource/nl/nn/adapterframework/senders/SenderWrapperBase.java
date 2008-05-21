f8260f35b7dd467f4e2a8a8578e72d69ccfa4f6c
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-05-21 10:42:19 +0000

    same attributenames as Pipes

--- JavaSource/nl/nn/adapterframework/senders/SenderWrapperBase.java
+++ JavaSource/nl/nn/adapterframework/senders/SenderWrapperBase.java
@@ -1,6 +1,9 @@
 /*
  * $Log: SenderWrapperBase.java,v $
- * Revision 1.1  2008-05-15 15:08:26  europe\L190409
+ * Revision 1.2  2008-05-21 10:42:19  europe\L190409
+ * same attributenames as Pipes
+ *
+ * Revision 1.1  2008/05/15 15:08:26  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * created senders package
  * moved some sender to senders package
  * created special senders
@@ -23,6 +26,16 @@ import org.apache.log4j.Logger;
 
 /**
  * Baseclasse for Wrappers for senders, that allows to get input from a session variable, and to store output in a session variable.
+ * <p><b>Configuration:</b>
+ * <table border="1">
+ * <tr><th>attributes</th><th>description</th><th>default</th></tr>
+ * <tr><td>className</td><td>nl.nn.adapterframework.senders.SenderWrapperBase</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setGetInputFromSessionKey(String) getInputFromSessionKey}</td><td>when set, input is taken from this session key, instead of regular input</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setGetInputFromFixedValue(String) getInputFromFixedValue}</td><td>when set, this fixed value is taken as input, instead of regular input</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setStoreResultInSessionKey(String) storeResultInSessionKey}</td><td>when set, the result is stored under this session key</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setPreserveInput(boolean) preserveInput}</td><td>when set <code>true</code>, the input of a pipe is restored before processing the next one</td><td>false</td></tr>
+ * </table>
+ * </p>
  * 
  * @author  Gerrit van Brakel
  * @since   4.9
@@ -31,15 +44,19 @@ import org.apache.log4j.Logger;
 public abstract class SenderWrapperBase extends SenderWithParametersBase implements HasStatistics {
 	protected Logger log = LogUtil.getLogger(this);
 
-	private String getFromSession; 
-	private String putInSession; 
-	private boolean inputToOutput=false; 
+	private String getInputFromSessionKey; 
+	private String getInputFromFixedValue=null;
+	private String storeResultInSessionKey; 
+	private boolean preserveInput=false; 
 
 
 	public void configure() throws ConfigurationException {
 		super.configure();
 		if (!isSenderConfigured()) {
 			throw new ConfigurationException(getLogPrefix()+"must have at least a sender configured");
+		}
+		if (StringUtils.isNotEmpty(getGetInputFromSessionKey()) && StringUtils.isNotEmpty(getGetInputFromFixedValue())) {
+			throw new ConfigurationException(getLogPrefix()+"cannot have both attributes inputFromSessionKey and inputFromFixedValue configured");
 		}
 	}
 
@@ -49,16 +66,21 @@ public abstract class SenderWrapperBase extends SenderWithParametersBase impleme
 
 	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException, TimeOutException {
 		String senderInput=message;
-		if (StringUtils.isNotEmpty(getGetFromSession())) {
-			senderInput=(String)prc.getSession().get(getGetFromSession());
-			if (log.isDebugEnabled()) log.debug(getLogPrefix()+"set contents of session variable ["+getGetFromSession()+"] as input ["+senderInput+"]");
+		if (StringUtils.isNotEmpty(getGetInputFromSessionKey())) {
+			senderInput=(String)prc.getSession().get(getGetInputFromSessionKey());
+			if (log.isDebugEnabled()) log.debug(getLogPrefix()+"set contents of session variable ["+getGetInputFromSessionKey()+"] as input ["+senderInput+"]");
+		} else {
+			if (StringUtils.isNotEmpty(getGetInputFromFixedValue())) {
+				senderInput=getGetInputFromSessionKey();
+				if (log.isDebugEnabled()) log.debug(getLogPrefix()+"set input to fixed value ["+senderInput+"]");
+			}
 		}
 		String result = doSendMessage(correlationID, senderInput, prc);
-		if (StringUtils.isNotEmpty(getPutInSession())) {
-			if (log.isDebugEnabled()) log.debug(getLogPrefix()+"storing results in session variable ["+getPutInSession()+"]");
-			prc.getSession().put(getPutInSession(),result);
+		if (StringUtils.isNotEmpty(getStoreResultInSessionKey())) {
+			if (log.isDebugEnabled()) log.debug(getLogPrefix()+"storing results in session variable ["+getStoreResultInSessionKey()+"]");
+			prc.getSession().put(getStoreResultInSessionKey(),result);
 		}
-		return isInputToOutput()?message:result;
+		return isPreserveInput()?message:result;
 	}
 
 	protected String getLogPrefix() {
@@ -69,25 +91,32 @@ public abstract class SenderWrapperBase extends SenderWithParametersBase impleme
 
 	public abstract void setSender(ISender sender);
 	
-	public void setGetFromSession(String string) {
-		getFromSession = string;
+	public void setGetInputFromSessionKey(String string) {
+		getInputFromSessionKey = string;
 	}
-	public String getGetFromSession() {
-		return getFromSession;
-	}
-
-	public void setPutInSession(String string) {
-		putInSession = string;
-	}
-	public String getPutInSession() {
-		return putInSession;
+	public String getGetInputFromSessionKey() {
+		return getInputFromSessionKey;
 	}
 
-	public void setInputToOutput(boolean b) {
-		inputToOutput = b;
+	public void setGetInputFromFixedValue(String string) {
+		getInputFromFixedValue = string;
 	}
-	public boolean isInputToOutput() {
-		return inputToOutput;
+	public String getGetInputFromFixedValue() {
+		return getInputFromFixedValue;
+	}
+
+	public void setStoreResultInSessionKey(String string) {
+		storeResultInSessionKey = string;
+	}
+	public String getStoreResultInSessionKey() {
+		return storeResultInSessionKey;
+	}
+
+	public void setPreserveInput(boolean preserveInput) {
+		this.preserveInput = preserveInput;
+	}
+	public boolean isPreserveInput() {
+		return preserveInput;
 	}
 
 }
