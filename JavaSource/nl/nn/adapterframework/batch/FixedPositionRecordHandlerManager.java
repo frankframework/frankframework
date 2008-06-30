9c016d63d57a9c8b891dc3332d94b388260ecf74
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-06-30 08:54:20 +0000

    allow for variable endPosition

--- JavaSource/nl/nn/adapterframework/batch/FixedPositionRecordHandlerManager.java
+++ JavaSource/nl/nn/adapterframework/batch/FixedPositionRecordHandlerManager.java
@@ -1,6 +1,9 @@
 /*
  * $Log: FixedPositionRecordHandlerManager.java,v $
- * Revision 1.10  2008-02-19 09:23:48  europe\L190409
+ * Revision 1.11  2008-06-30 08:54:20  europe\L190409
+ * allow for variable endPosition
+ *
+ * Revision 1.10  2008/02/19 09:23:48  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * updated javadoc
  *
  * Revision 1.9  2008/02/15 16:06:03  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -31,6 +34,9 @@
  */
 package nl.nn.adapterframework.batch;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import nl.nn.adapterframework.core.PipeLineSession;
 
 /**
@@ -45,31 +51,50 @@ import nl.nn.adapterframework.core.PipeLineSession;
  * <tr><td>{@link #setName(String) name}</td><td>Name of the manager</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setInitial(boolean) initial}</td><td>This manager is the initial manager, i.e. to be used for the first record</td><td>false</td></tr>
  * <tr><td>{@link #setStartPosition(int) startPosition}</td><td>Startposition of the field in the record that identifies the recordtype (first character is 0)</td><td>0</td></tr>
- * <tr><td>{@link #setEndPosition(int) endPosition}</td><td>Endposition of the field in the record that identifies the recordtype</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setEndPosition(int) endPosition}</td><td>if endPosition >= 0 the this field contains the endposition of the recordtype field in the record (else it depends on the length of the recordKey in the flow</td><td>-1</td></tr>
  * </table>
  * </p>
  * 
  * @author John Dekker
  */
 public class FixedPositionRecordHandlerManager extends RecordHandlerManager {
-	public static final String version = "$RCSfile: FixedPositionRecordHandlerManager.java,v $  $Revision: 1.10 $ $Date: 2008-02-19 09:23:48 $";
+	public static final String version = "$RCSfile: FixedPositionRecordHandlerManager.java,v $  $Revision: 1.11 $ $Date: 2008-06-30 08:54:20 $";
 
 	private int startPosition;
-	private int endPosition;
+	private int endPosition=-1;
 	
 	public RecordHandlingFlow getRecordHandler(PipeLineSession session, String record) throws Exception {
 		String value = null;
 		if (startPosition >= record.length()) {
 			throw new Exception("Record size is smaller then the specified position of the recordtype within the record");
 		}
-		if (endPosition >= record.length()) {
-			value = record.substring(startPosition); 
+		if (endPosition < 0) {
+			Map valueHandlersMap = getValueHandlersMap();
+			RecordHandlingFlow rhf = null;
+			for(Iterator it=valueHandlersMap.keySet().iterator();it.hasNext() && rhf == null;) {
+				String name=(String)it.next();
+				value = record.substring(startPosition, name.length());
+				if (value.equals(name)) {
+					rhf = (RecordHandlingFlow)valueHandlersMap.get(name);
+				}
+			}
+			if  (rhf == null) {
+				rhf =(RecordHandlingFlow)getValueHandlersMap().get("*");
+				if  (rhf == null) {
+					throw new Exception("No handlers (flow) found for recordKey [" + value + "]");
+				}
+			}
+			return rhf;
+		} else {
+			if (endPosition >= record.length()) {
+				value = record.substring(startPosition); 
+			}
+			else {
+				value = record.substring(startPosition, endPosition);
+			}
+			
+			return super.getRecordHandlerByKey(value);
 		}
-		else {
-			value = record.substring(startPosition, endPosition);
-		}
-		
-		return super.getRecordHandlerByKey(value);
 	}
 
 
