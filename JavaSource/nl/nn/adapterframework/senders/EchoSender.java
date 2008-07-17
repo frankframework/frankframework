b14905f3e37116263772436e9ba3f8b98032083f
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-07-17 16:17:54 +0000

    made synchronous configurable

--- JavaSource/nl/nn/adapterframework/senders/EchoSender.java
+++ JavaSource/nl/nn/adapterframework/senders/EchoSender.java
@@ -1,6 +1,9 @@
 /*
  * $Log: EchoSender.java,v $
- * Revision 1.1  2008-05-15 15:08:27  europe\L190409
+ * Revision 1.2  2008-07-17 16:17:54  europe\L190409
+ * made synchronous configurable
+ *
+ * Revision 1.1  2008/05/15 15:08:27  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * created senders package
  * moved some sender to senders package
  * created special senders
@@ -23,6 +26,7 @@ import nl.nn.adapterframework.parameters.ParameterResolutionContext;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>{@link #setName(String) name}</td><td>name of the Sender</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setSynchronous(boolean) synchronous}</td><td>hack to allow to introduce a correlationID</td><td>true</td></tr>
  * </table>
  * 
  * @author  Gerrit van Brakel
@@ -30,13 +34,18 @@ import nl.nn.adapterframework.parameters.ParameterResolutionContext;
  * @version Id
  */
 public class EchoSender extends SenderWithParametersBase {
+	
+	private boolean synchronous=true;
 
 	public String sendMessage(String correlationID, String message, ParameterResolutionContext prc) throws SenderException, TimeOutException {
 		return message;
 	}
 
+	public void setSynchronous(boolean b) {
+		synchronous = b;
+	}
 	public boolean isSynchronous() {
-		return true;
+		return synchronous;
 	}
 
 }
