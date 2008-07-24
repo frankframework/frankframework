08abadc43ce3cd937acb26e6534448dc792542e9
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-07-24 12:24:12 +0000

    log statistics as XML

--- JavaSource/nl/nn/adapterframework/util/StatisticsKeeperLogger.java
+++ JavaSource/nl/nn/adapterframework/util/StatisticsKeeperLogger.java
@@ -1,6 +1,9 @@
 /*
  * $Log: StatisticsKeeperLogger.java,v $
- * Revision 1.4  2008-05-14 09:30:43  europe\L190409
+ * Revision 1.5  2008-07-24 12:24:12  europe\L190409
+ * log statistics as XML
+ *
+ * Revision 1.4  2008/05/14 09:30:43  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * simplified methodnames of StatisticsKeeperIterationHandler
  *
  * Revision 1.3  2007/02/12 14:12:03  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -15,7 +18,6 @@
  */
 package nl.nn.adapterframework.util;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -25,46 +27,13 @@ import org.apache.log4j.Logger;
  * @since   4.4.3
  * @version Id
  */
-public class StatisticsKeeperLogger implements StatisticsKeeperIterationHandler {
-	public static final String version="$RCSfile: StatisticsKeeperLogger.java,v $ $Revision: 1.4 $ $Date: 2008-05-14 09:30:43 $";
+public class StatisticsKeeperLogger extends StatisticsKeeperXmlBuilder {
 	protected Logger log = LogUtil.getLogger("nl.nn.adapterframework.statistics");
 
-	public Object start() {
-		log.info("********** start statistics dump **********");
-		return null;
-	}
 	public void end(Object data) {
-		log.info("**********  end statistics dump  **********");
+		super.end(data);
+		log.info(getXml().toXML());
 	}
-
-	public void handleStatisticsKeeper(Object data, StatisticsKeeper sk) {
-		String msg=sk.getName();
-		for (int i=0;i<sk.getItemCount();i++) {
-			msg+=" "+sk.getItemName(i)+"="+sk.getItemValue(i)+";";
-		}
-		log.info((String)data+" "+msg);
-	}
-
-	public void handleScalar(Object data, String scalarName, long value){
-		handleScalar(data,scalarName,""+value);
-	}
-
-	public void handleScalar(Object data, String scalarName, String value){
-		log.info((String)data+" "+scalarName+"="+value+";");
-	}
-
-	public Object openGroup(Object parentData, String name, String type) {
-		String result="";
-		if (StringUtils.isNotEmpty((String)parentData)) {
-			result+=(String)parentData+"/";
-		}
-		result+=type+"("+name+")";
-		log.debug(result+":");
-		return result;
-	}
-
-	public void closeGroup(Object data) {
-		log.debug((String)data+".");
-	}
-
+	
+	
 }
