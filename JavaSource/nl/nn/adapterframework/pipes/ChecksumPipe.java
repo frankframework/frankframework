8d541fb6837be7c917183d7308d2716733ff6032
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2008-08-07 10:56:12 +0000

    added attribute inputIsFile

--- JavaSource/nl/nn/adapterframework/pipes/ChecksumPipe.java
+++ JavaSource/nl/nn/adapterframework/pipes/ChecksumPipe.java
@@ -1,16 +1,21 @@
 /*
  * $Log: ChecksumPipe.java,v $
- * Revision 1.1  2008-08-07 07:57:06  europe\L190409
+ * Revision 1.2  2008-08-07 10:56:12  europe\L190409
+ * added attribute inputIsFile
+ *
+ * Revision 1.1  2008/08/07 07:57:06  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * first version
  *
  */
 package nl.nn.adapterframework.pipes;
 
+import java.io.FileInputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.Adler32;
 import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.PipeLineSession;
@@ -32,9 +37,9 @@ import org.apache.commons.lang.StringUtils;
  * <tr><td>{@link #setDurationThreshold(long) durationThreshold}</td><td>if durationThreshold >=0 and the duration (in milliseconds) of the message processing exceeded the value specified, then the message is logged informatory</td><td>-1</td></tr>
  * <tr><td>{@link #setGetInputFromSessionKey(String) getInputFromSessionKey}</td><td>when set, input is taken from this session key, instead of regular input</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setStoreResultInSessionKey(String) storeResultInSessionKey}</td><td>when set, the result is stored under this session key</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setPreserveInput(boolean) preserveInput}</td><td>when set <code>true</code>, the input of a pipe is restored before processing the next one</td><td>false</td></tr>
  * <tr><td>{@link #setType(String) type}</td>  <td>type of checksum to be calculated. Must be one of MD5, SHA, CRC32, Adler32</td><td>MD5</td></tr>
  * <tr><td>{@link #setCharset(String) charset}</td>  <td>character encoding to be used to encode message before calculating checksum</td><td>UTF-8</td></tr>
+ * <tr><td>{@link #setInputIsFile(boolean) inputIsFile}</td><td>when set <code>true</code>, the input is assumed to be a filename; otherwise the input itself is used in the calculations</td><td>false</td></tr>
  * </table>
  * </p>
  * 
@@ -51,6 +56,7 @@ public class ChecksumPipe extends FixedForwardPipe {
 	
 	private String charset=Misc.DEFAULT_INPUT_STREAM_ENCODING;
 	private String type=CHECKSUM_MD5;
+	private boolean inputIsFile=false;
 	
 	public void configure() throws ConfigurationException {
 		super.configure();
@@ -69,34 +75,61 @@ public class ChecksumPipe extends FixedForwardPipe {
 		}
 	}
 
-	public String calculateCRC32(byte[] b) {
-		CRC32 crc = new CRC32();
-		crc.reset();
-		crc.update(b);
-		String result=Long.toHexString(crc.getValue());
-		return result;
+	protected interface ChecksumGenerator {
+		public void update(byte b[],int length);
+		public String getResult();
+	}
+	
+	protected ChecksumGenerator createChecksumGenerator() throws NoSuchAlgorithmException {
+		if (CHECKSUM_MD5.equals(getType())) {
+			return new MessageDigestChecksumGenerator(getType());
+		} else if (CHECKSUM_CRC32.equals(getType())) {
+			return new ZipChecksumGenerator(new CRC32());
+		} else if (CHECKSUM_ADLER32.equals(getType())) {
+			return new ZipChecksumGenerator(new Adler32());
+		} else if (CHECKSUM_SHA.equals(getType())) {
+			return new MessageDigestChecksumGenerator(getType());
+		} else {
+			throw new NoSuchAlgorithmException("unsupported algorithm ["+getType()+"]");
+		}
 	}
 
-	public String calculateAdler32(byte[] b) {
-		Adler32 crc = new Adler32();
-		crc.reset();
-		crc.update(b);
-		String result=Long.toHexString(crc.getValue());
-		return result;
+	protected class ZipChecksumGenerator implements ChecksumGenerator {	
+		
+		private Checksum checksum;
+
+		ZipChecksumGenerator(Checksum checksum) {
+			super();
+			this.checksum=checksum;
+			checksum.reset();
+		}
+
+		public void update(byte b[],int length){
+			checksum.update(b,0,length);
+		}
+
+		public String getResult(){
+			String result=Long.toHexString(checksum.getValue());
+			return result;
+		}
 	}
 
-	public String calculateMD5(byte[] b) throws NoSuchAlgorithmException {
-		MessageDigest m=MessageDigest.getInstance("MD5");
-		m.update(b);
-        String result=new BigInteger(1,m.digest()).toString(16);
-        return result;
-	}
+	protected class MessageDigestChecksumGenerator implements ChecksumGenerator {	
+		
+		private MessageDigest messageDigest;
 
-	public String calculateSHA(byte[] b) throws NoSuchAlgorithmException {
-		MessageDigest m=MessageDigest.getInstance("SHA");
-		m.update(b);
-		String result=new BigInteger(1,m.digest()).toString(16);
-		return result;
+		MessageDigestChecksumGenerator(String type) throws NoSuchAlgorithmException {
+			super();
+			this.messageDigest=MessageDigest.getInstance(type);
+		}
+
+		public void update(byte b[],int length){
+			messageDigest.update(b,0,length);
+		}
+		public String getResult(){
+			String result=new BigInteger(1,messageDigest.digest()).toString(16);
+			return result;
+		}
 	}
 
 
@@ -104,23 +137,24 @@ public class ChecksumPipe extends FixedForwardPipe {
 		String message=(String)input;
 		String result;
 		try {
-			byte barr[];
-			if (StringUtils.isEmpty(getCharset())) {
-				barr=message.getBytes();
+			ChecksumGenerator cg=createChecksumGenerator();
+			if (isInputIsFile()) {
+				byte barr[]=new byte[1000];
+				FileInputStream fis=new FileInputStream((String)input);
+				int c;
+				while ((c=fis.read(barr))>=0) {
+					cg.update(barr,c);
+				}
 			} else {
-				barr=message.getBytes(getCharset());
+				byte barr[];
+				if (StringUtils.isEmpty(getCharset())) {
+					barr=message.getBytes();
+				} else {
+					barr=message.getBytes(getCharset());
+				}
+				cg.update(barr,barr.length);
 			}
-			if (CHECKSUM_MD5.equals(getType())) {
-				result = calculateMD5(barr);
-			} else if (CHECKSUM_CRC32.equals(getType())) {
-				result = calculateCRC32(barr);
-			} else if (CHECKSUM_ADLER32.equals(getType())) {
-			result = calculateAdler32(barr);
-			} else if (CHECKSUM_SHA.equals(getType())) {
-				result = calculateSHA(barr);
-			} else {
-				result = "unsupported algorithm ["+getType()+"]";
-			}
+			result=cg.getResult();
 			return new PipeRunResult(getForward(),result);
 		} catch (Exception e) {
 			throw new PipeRunException(this,"cannot calculate ["+getType()+"] using charset ["+getCharset()+"]",e);
@@ -140,6 +174,13 @@ public class ChecksumPipe extends FixedForwardPipe {
 	}
 	public String getType() {
 		return type;
+	}
+
+	public void setInputIsFile(boolean b) {
+		inputIsFile = b;
+	}
+	public boolean isInputIsFile() {
+		return inputIsFile;
 	}
 
 }
