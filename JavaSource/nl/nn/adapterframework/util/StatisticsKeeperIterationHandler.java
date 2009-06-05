afd59b66075f67c0eab4e0cf2df34b44d86290fa
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2009-06-05 07:36:54 +0000

    allow methods to throw SenderException handle scalar now accepts only long and date values

--- JavaSource/nl/nn/adapterframework/util/StatisticsKeeperIterationHandler.java
+++ JavaSource/nl/nn/adapterframework/util/StatisticsKeeperIterationHandler.java
@@ -1,6 +1,10 @@
 /*
  * $Log: StatisticsKeeperIterationHandler.java,v $
- * Revision 1.3  2008-05-14 09:30:33  europe\L190409
+ * Revision 1.4  2009-06-05 07:36:54  L190409
+ * allow methods to throw SenderException
+ * handle scalar now accepts only long and date values
+ *
+ * Revision 1.3  2008/05/14 09:30:33  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * simplified methodnames of StatisticsKeeperIterationHandler
  *
  * Revision 1.2  2006/02/09 08:02:46  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -12,6 +16,10 @@
  */
 package nl.nn.adapterframework.util;
 
+import java.util.Date;
+
+import nl.nn.adapterframework.core.SenderException;
+
 /**
  * Allows operations on iterations over all statistics keepers.
  * 
@@ -21,11 +29,11 @@ package nl.nn.adapterframework.util;
  */
 public interface StatisticsKeeperIterationHandler {
 
-	public Object start();
-	public void end(Object data);
-	public void handleStatisticsKeeper(Object data, StatisticsKeeper sk);
-	public void handleScalar(Object data, String scalarName, long value);
-	public void handleScalar(Object data, String scalarName, String value);
-	public Object openGroup(Object parentData, String name, String type);
-	public void  closeGroup(Object data);
+	public Object start() throws SenderException;
+	public void end(Object data) throws SenderException;
+	public void handleStatisticsKeeper(Object data, StatisticsKeeper sk) throws SenderException;
+	public void handleScalar(Object data, String scalarName, long value) throws SenderException;
+	public void handleScalar(Object data, String scalarName, Date value) throws SenderException;
+	public Object openGroup(Object parentData, String name, String type) throws SenderException;
+	public void  closeGroup(Object data) throws SenderException;
 }
