4ceb45e0013437bb40d42da4f20b269b9984ced9
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2010-05-14 16:50:39 +0000

    added separatorReplacement

--- JavaSource/nl/nn/adapterframework/batch/DelphiStringRecordReaderFactory.java
+++ JavaSource/nl/nn/adapterframework/batch/DelphiStringRecordReaderFactory.java
@@ -1,6 +1,9 @@
 /*
  * $Log: DelphiStringRecordReaderFactory.java,v $
- * Revision 1.1  2010-05-03 17:03:06  L190409
+ * Revision 1.2  2010-05-14 16:50:39  L190409
+ * added separatorReplacement
+ *
+ * Revision 1.1  2010/05/03 17:03:06  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * IInputstreamReader-classes to enable reading Delphi String records
  *
  */
@@ -14,7 +17,7 @@ import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.SenderException;
 
 /**
- * {@link IInputStreamReaderFactory} that reads Delphi records containing Strings.
+ * {@link IInputStreamReaderFactory} that provides a reader that reads Delphi records containing Strings.
  * <p><b>Configuration:</b>
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
@@ -22,6 +25,7 @@ import nl.nn.adapterframework.core.SenderException;
  * <tr><td>{@link #setStringLength(int) stringLength}</td><td>the maximum length of each string. Each string is preceded by a one byte length indication.</td><td>50</td></tr>
  * <tr><td>{@link #setStringsPerRecord(int) stringsPerRecord}</td><td>The number of strings read for each record. 0 means file consists of one logical record</td><td>0</td></tr>
  * <tr><td>{@link #setSeparator(String) separator}</td><td>separator placed between each string read</td><td>|</td></tr>
+ * <tr><td>{@link #setSeparatorReplacement(String) separatorReplacement}</td><td>Replacement character, used when separator is found in string read</td><td>_</td></tr>
  * </table>
  * </p>
  * 
@@ -34,12 +38,13 @@ public class DelphiStringRecordReaderFactory implements IInputStreamReaderFactor
 	private int stringLength=50;
 	private int stringsPerRecord=0; // 0 means read till end of file
 	private String separator="|";
+	private String separatorReplacement="_";
 
 	public void configure() throws ConfigurationException {
 	}
 
 	public Reader getReader(InputStream in, String charset, String streamId, PipeLineSession session) throws SenderException {
-		return new DelphiStringRecordReader(in,charset,getStringLength(),getStringsPerRecord(),getSeparator()); 
+		return new DelphiStringRecordReader(in,charset,getStringLength(),getStringsPerRecord(),getSeparator(),getSeparatorReplacement()); 
 	}
 
 	public void setSeparator(String string) {
@@ -61,6 +66,13 @@ public class DelphiStringRecordReaderFactory implements IInputStreamReaderFactor
 	}
 	public int getStringsPerRecord() {
 		return stringsPerRecord;
+	}
+
+	public void setSeparatorReplacement(String string) {
+		separatorReplacement = string;
+	}
+	public String getSeparatorReplacement() {
+		return separatorReplacement;
 	}
 
 }
