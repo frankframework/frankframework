96b8b7c2d44ebbe5155b6e938bb91cbff687866d
Jaco de Groot <jaco.de.groot@ibissource.org>
2010-09-09 14:02:35 +0000

    Upgrade log4j-1.2.7.jar to log4j-1.2.16.jar: RootCategory replaced by RootLogger

--- JavaSource/nl/nn/adapterframework/util/LogUtil.java
+++ JavaSource/nl/nn/adapterframework/util/LogUtil.java
@@ -1,6 +1,9 @@
 /*
  * $Log: LogUtil.java,v $
- * Revision 1.4  2007-08-30 15:11:46  europe\L190409
+ * Revision 1.5  2010-09-09 14:02:35  m00f069
+ * Upgrade log4j-1.2.7.jar to log4j-1.2.16.jar: RootCategory replaced by RootLogger
+ *
+ * Revision 1.4  2007/08/30 15:11:46  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * use only hierarchy if log4j4ibis.properties is present
  *
  * Revision 1.3  2007/08/29 15:13:04  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
@@ -21,7 +24,7 @@ import org.apache.log4j.Hierarchy;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.spi.RootCategory;
+import org.apache.log4j.spi.RootLogger;
 
 /**
  * Convenience functions for logging.
@@ -33,7 +36,7 @@ import org.apache.log4j.spi.RootCategory;
  * @version Id
  */
 public class LogUtil {
-	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.4 $ $Date: 2007-08-30 15:11:46 $";
+	public static final String version="$RCSfile: LogUtil.java,v $  $Revision: 1.5 $ $Date: 2010-09-09 14:02:35 $";
 
 	private static Hierarchy hierarchy=null;
 	
@@ -42,7 +45,7 @@ public class LogUtil {
 		if (url==null) {
 			System.err.println("Did not find log4j4ibis.properties, leaving it up to log4j's default initialization procedure: http://logging.apache.org/log4j/docs/manual.html#defaultInit");
 		} else {
-			hierarchy = new Hierarchy(new RootCategory(Level.DEBUG));
+			hierarchy = new Hierarchy(new RootLogger(Level.DEBUG));
 			new PropertyConfigurator().doConfigure(url, hierarchy);
 		}
 	}
