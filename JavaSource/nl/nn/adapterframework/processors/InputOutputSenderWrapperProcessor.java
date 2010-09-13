8dd5a4977e61ce57b6aa0bfd128047a400bd3ba1
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2010-09-13 14:02:11 +0000

    split SenderWrapper-processing in chain of processors

--- JavaSource/nl/nn/adapterframework/processors/InputOutputSenderWrapperProcessor.java
+++ JavaSource/nl/nn/adapterframework/processors/InputOutputSenderWrapperProcessor.java
@@ -1,7 +1,7 @@
 /*
- * $Log: SenderWrapperBaseProcessor.java,v $
- * Revision 1.2  2010-09-07 15:55:13  m00f069
- * Removed IbisDebugger, made it possible to use AOP to implement IbisDebugger functionality.
+ * $Log: InputOutputSenderWrapperProcessor.java,v $
+ * Revision 1.1  2010-09-13 14:02:11  L190409
+ * split SenderWrapper-processing in chain of processors
  *
  */
 package nl.nn.adapterframework.processors;
@@ -10,17 +10,15 @@ import nl.nn.adapterframework.core.SenderException;
 import nl.nn.adapterframework.core.TimeOutException;
 import nl.nn.adapterframework.parameters.ParameterResolutionContext;
 import nl.nn.adapterframework.senders.SenderWrapperBase;
-import nl.nn.adapterframework.util.LogUtil;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 
 /**
- * @author Jaco de Groot
+ * @author  Gerrit van Brakel
+ * @since   4.11
  * @version Id
  */
-public class SenderWrapperBaseProcessor {
-	private Logger log = LogUtil.getLogger(this);
+public class InputOutputSenderWrapperProcessor extends SenderWrapperProcessorBase {
 
 	public String sendMessage(SenderWrapperBase senderWrapperBase, String correlationID, String message, ParameterResolutionContext prc) throws SenderException, TimeOutException {
 		String senderInput=(String)message;
@@ -33,13 +31,12 @@ public class SenderWrapperBaseProcessor {
 				if (log.isDebugEnabled()) log.debug(senderWrapperBase.getLogPrefix()+"set input to fixed value ["+senderInput+"]");
 			}
 		}
-		String result = senderWrapperBase.doSendMessage(correlationID, senderInput, prc);
+		String result = senderWrapperProcessor.sendMessage(senderWrapperBase, correlationID, message, prc);
 		if (StringUtils.isNotEmpty(senderWrapperBase.getStoreResultInSessionKey())) {
 			if (log.isDebugEnabled()) log.debug(senderWrapperBase.getLogPrefix()+"storing results in session variable ["+senderWrapperBase.getStoreResultInSessionKey()+"]");
 			prc.getSession().put(senderWrapperBase.getStoreResultInSessionKey(),result);
 		}
 		return senderWrapperBase.isPreserveInput()?(String)message:result;
 	}
-
 
 }
