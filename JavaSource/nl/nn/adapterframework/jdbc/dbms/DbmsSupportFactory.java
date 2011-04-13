b14645fe770ed9a2ef89367c8c3bdc2b464f7a4e
Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
2011-04-13 08:44:10 +0000

    Spring configurable DbmsSupport

--- JavaSource/nl/nn/adapterframework/jdbc/dbms/DbmsSupportFactory.java
+++ JavaSource/nl/nn/adapterframework/jdbc/dbms/DbmsSupportFactory.java
@@ -1,6 +1,9 @@
 /*
  * $Log: DbmsSupportFactory.java,v $
- * Revision 1.1  2011-03-16 16:47:26  L190409
+ * Revision 1.2  2011-04-13 08:44:10  L190409
+ * Spring configurable DbmsSupport
+ *
+ * Revision 1.1  2011/03/16 16:47:26  Gerrit van Brakel <gerrit.van.brakel@ibissource.org>
  * introduction of DbmsSupport, including support for MS SQL Server
  *
  */
@@ -9,9 +12,12 @@ package nl.nn.adapterframework.jdbc.dbms;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Properties;
 
+import nl.nn.adapterframework.util.ClassUtils;
 import nl.nn.adapterframework.util.LogUtil;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,30 +25,63 @@ import org.apache.log4j.Logger;
  * @since  
  * @version Id
  */
-public class DbmsSupportFactory {
-	protected static Logger log = LogUtil.getLogger(DbmsSupportFactory.class);
+public class DbmsSupportFactory implements IDbmsSupportFactory {
+	protected Logger log = LogUtil.getLogger(this.getClass());
 
-	public final static int DBMS_GENERIC=0;
-	public final static int DBMS_ORACLE=1;
-	public final static int DBMS_MSSQLSERVER=2;
-	public final static int DBMS_DB2=3;
+	private final static String PRODUCT_NAME_ORACLE_="Oracle";
+	private final static String PRODUCT_NAME_MSSQLSERVER="Microsoft SQL Server";
+
+	private Properties dbmsSupportMap; 
 	
-	public final static String PRODUCT_NAME_ORACLE_="Oracle";
-	public final static String PRODUCT_NAME_MSSQLSERVER="Microsoft SQL Server";
-
-	public static IDbmsSupport getDbmsSupport(Connection conn) throws SQLException {
-		DatabaseMetaData md=conn.getMetaData();
-		String product=md.getDatabaseProductName();
-		if (PRODUCT_NAME_ORACLE_.equals(product)) {
-			log.debug("Setting databasetype to ORACLE");
-			return new OracleDbmsSupport();
+	public IDbmsSupport getDbmsSupport(Connection conn) {
+		String product;
+		try {
+			DatabaseMetaData md = conn.getMetaData();
+			product=md.getDatabaseProductName();
+		} catch (SQLException e1) {
+			throw new RuntimeException("cannot obtain product from connection metadata", e1);
 		}
-		if (PRODUCT_NAME_MSSQLSERVER.equals(product)) {
-			log.debug("Setting databasetype to MSSQLSERVER");
-			return new MsSqlServerDbmsSupport();
+		Properties supportMap=getDbmsSupportMap();
+		if (supportMap!=null) {
+			if (StringUtils.isEmpty(product)) {
+				log.warn("no product found from connection metadata");
+			} else {
+				if (!supportMap.containsKey(product)) {
+					log.warn("product ["+product+"] not configured in dbmsSupportMap");
+				} else {
+					String dbmsSupportClass=supportMap.getProperty(product);
+					if (StringUtils.isEmpty(dbmsSupportClass)) {
+						log.warn("product ["+product+"] configured empty in dbmsSupportMap");
+					} else {
+						try {
+							if (log.isDebugEnabled()) log.debug("creating dbmsSupportClass ["+dbmsSupportClass+"] for product ["+product+"]");
+							return (IDbmsSupport)ClassUtils.newInstance(dbmsSupportClass);
+						} catch (Exception e) {
+							throw new RuntimeException("Cannot create dbmsSupportClass ["+dbmsSupportClass+"] for product ["+product+"]",e);
+						} 
+					}
+				}
+			}				
+		} else {
+			log.warn("no dbmsSupportMap specified, reverting to built in types");
+			if (PRODUCT_NAME_ORACLE_.equals(product)) {
+				log.debug("Setting databasetype to ORACLE");
+				return new OracleDbmsSupport();
+			}
+			if (PRODUCT_NAME_MSSQLSERVER.equals(product)) {
+				log.debug("Setting databasetype to MSSQLSERVER");
+				return new MsSqlServerDbmsSupport();
+			}
 		}
 		log.debug("Setting databasetype to GENERIC, productName ["+product+"]");
 		return new GenericDbmsSupport();
+	}
+
+	public Properties getDbmsSupportMap() {
+		return dbmsSupportMap;
+	}
+	public void setDbmsSupportMap(Properties dbmsSupportMap) {
+		this.dbmsSupportMap = dbmsSupportMap;
 	}
 
 }
