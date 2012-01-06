343a8f56dd55acdf740f41020d580be7f58253b7
Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
2012-01-06 15:48:50 +0000

    updated javadoc

--- JavaSource/nl/nn/adapterframework/extensions/esb/EsbSoapWrapperPipe.java
+++ JavaSource/nl/nn/adapterframework/extensions/esb/EsbSoapWrapperPipe.java
@@ -1,6 +1,9 @@
 /*
  * $Log: EsbSoapWrapperPipe.java,v $
- * Revision 1.7  2012-01-06 15:40:37  europe\m168309
+ * Revision 1.8  2012-01-06 15:48:50  europe\m168309
+ * updated javadoc
+ *
+ * Revision 1.7  2012/01/06 15:40:37  Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
  * added mode 'bis'
  *
  * Revision 1.6  2012/01/04 11:02:21  Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
@@ -40,11 +43,13 @@ import nl.nn.adapterframework.util.AppConstants;
  * <p><b>Configuration </b><i>(where deviating from SoapWrapperPipe)</i><b>:</b>
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
- * <tr><td>{@link #setMode(String) mode}</td><td>either <code>i2t</code> (ifsa2tibco) or <code>reg</code> (regular)</td><td>reg</td></tr>
+ * <tr><td>{@link #setMode(String) mode}</td><td>either <code>i2t</code> (ifsa2tibco), <code>reg</code> (regular) or <code>bis</code> (Business Integration Services)</td><td>reg</td></tr>
  * <tr><td>{@link #setSoapHeaderSessionKey(String) soapHeaderSessionKey}</td><td>if direction=<code>unwrap</code>: </td><td>soapHeader</td></tr>
  * <tr><td>{@link #setSoapHeaderStyleSheet(String) soapHeaderStyleSheet}</td><td>if direction=<code>wrap</code> and mode=<code>i2t</code>:</td><td>/xml/xsl/esb/soapHeader.xsl</td></tr>
  * <tr><td></td><td>if direction=<code>wrap</code> and mode=<code>reg</code>:</td><td>TODO (for now identical to the "<code>i2t</code>" SOAP Header)</td></tr>
+ * <tr><td></td><td>if direction=<code>wrap</code> and mode=<code>bis</code>:</td><td>/xml/xsl/esb/bisSoapHeader.xsl</td></tr>
  * <tr><td>{@link #setSoapBodyStyleSheet(String) soapBodyStyleSheet}</td><td>if direction=<code>wrap</code> and mode=<code>reg</code>:</td><td>/xml/xsl/esb/soapBody.xsl</td></tr>
+ * <tr><td></td><td>if direction=<code>wrap</code> and mode=<code>bis</code>:</td><td>/xml/xsl/esb/bisSoapBody.xsl</td></tr>
  * <tr><td>{@link #setAddOutputNamespace(boolean) addOutputNamespace}</td><td>(only used when <code>direction=wrap</code>) when <code>true</code>, <code>outputNamespace</code> is automatically set using the parameters ("http://nn.nl/XSD/$businessDomain/$serviceName/$serviceContext/$serviceContextVersion/$operationName/$operationVersion")</td><td><code>false</code></td></tr>
  * </table></p>
  * <p>
@@ -94,6 +99,31 @@ import nl.nn.adapterframework.util.AppConstants;
  * </table>
  * </p>
  * <p>
+ * <b>/xml/xsl/esb/bisSoapHeader.xsl:</b>
+ * <table border="1">
+ * <tr><th>element</th><th>level</th><th>value</th></tr>
+ * <tr><td>MessageHeader</td><td>0</td><td>&nbsp;</td></tr>
+ * <tr><td>&nbsp;</td><td>&nbsp;</td><td>xmlns="http://www.ing.com/CSP/XSD/General/Message_2"</td></tr>
+ * <tr><td>From</td><td>1</td><td>&nbsp;</td></tr>
+ * <tr><td>Id</td><td>2</td><td>$fromId</td></tr>
+ * <tr><td>HeaderFields</td><td>1</td><td>&nbsp;</td></tr>
+ * <tr><td>ConversationId</td><td>2</td><td>$conversationId</td></tr>
+ * <tr><td>CorrelationId</td><td>2</td><td>$correlationId (if empty then skip this element)</td></tr>
+ * <tr><td>MessageId</td><td>2</td><td>$messageId</td></tr>
+ * <tr><td>ExternalRefToMessageId</td><td>2</td><td>$externalRefToMessageId (if empty then skip this element)</td></tr>
+ * <tr><td>Timestamp</td><td>2</td><td>$timestamp</td></tr>
+ * </table>
+ * <b>Parameters:</b>
+ * <table border="1">
+ * <tr><th>name</th><th>default</th></tr>
+ * <tr><td>fromId</td><td>property 'instance.name'</td></tr>
+ * <tr><td>conversationId</td><td>if applicable, copied from the original (received) SOAP Header</td></tr>
+ * <tr><td>messageId</td><td>parameter pattern '{hostname}_{uid}'</td></tr>
+ * <tr><td>externalRefToMessageId</td><td>if applicable, copied from MessageId in the original (received) SOAP Header</td></tr>
+ * <tr><td>timestamp</td><td>parameter pattern '{now,date,yyyy-MM-dd'T'HH:mm:ss}'</td></tr>
+ * </table>
+ * </p>
+ * <p>
  * <b>/xml/xsl/esb/soapBody.xsl:</b>
  * <table border="1">
  * <tr><th>element</th><th>level</th><th>value</th></tr>
@@ -138,6 +168,51 @@ import nl.nn.adapterframework.util.AppConstants;
  * <tr><td>operationName</td><td>&nbsp;</td></tr>
  * <tr><td>operationVersion</td><td>1</td></tr>
  * <tr><td>paradigm</td><td>&nbsp;</td></tr>
+ * </table>
+ * </p>
+ * <p>
+ * <b>/xml/xsl/esb/bisSoapBody.xsl:</b>
+ * <table border="1">
+ * <tr><th>element</th><th>level</th><th>value</th></tr>
+ * <tr><td>[Payload]</td><td>0</td><td>if $errorCode is empty then the complete payload will be copied and if not already existing a Result tag will be added<br/>else only the root tag will be copied</td></tr>
+ * <tr><td>Result</td><td>1</td><td>this element will be the last child in the copied root tag; if $errorCode is empty and a Result tag already exists then skip this element including its child elements</td></tr>
+ * <tr><td>&nbsp;</td><td>&nbsp;</td><td>xmlns="http://www.ing.com/CSP/XSD/General/Message_2"</td></tr>
+ * <tr><td>Status</td><td>2</td><td>if $errorCode is empty then 'OK'</br>else 'ERROR'</td></tr>
+ * <tr><td>ErrorList</td><td>2</td><td>if $errorCode is empty then skip this element including its child elements</td></tr>
+ * <tr><td>Error</td><td>3</td><td>&nbsp;</td></tr>
+ * <tr><td>Code</td><td>4</td><td>$errorCode</td></tr>
+ * <tr><td>Reason</td><td>4</td><td>if $errorReason is not empty then $errorReason</br>else it will be derived from $errorCode:
+ *   <table border="1">
+ *   <tr><th>errorCode</th><th>errorText</th></tr>
+ *   <tr><td>ERR6002</td><td>Service Interface Request Time Out</td></tr>
+ *   <tr><td>ERR6003</td><td>Invalid Request Message</td></tr>
+ *   <tr><td>ERR6004</td><td>Invalid Backend system response</td></tr>
+ *   <tr><td>ERR6005</td><td>Backend system failure response</td></tr>
+ *   <tr><td>ERR6999</td><td>Unspecified Errors</td></tr>
+ *  </table>
+ * </td></tr>
+ * <tr><td>Service</td><td>4</td><td>&nbsp;</td></tr>
+ * <tr><td>Name</td><td>5</td><td>$serviceName</td></tr>
+ * <tr><td>Context</td><td>5</td><td>$serviceContext</td></tr>
+ * <tr><td>Action</td><td>5</td><td>&nbsp;</td></tr>
+ * <tr><td>Name</td><td>6</td><td>$operationName</td></tr>
+ * <tr><td>Version</td><td>6</td><td>$operationVersion</td></tr>
+ * <tr><td>DetailList</td><td>4</td><td>if $errorDetailCode is empty then skip this element including its child elements</td></tr>
+ * <tr><td>Detail</td><td>5</td><td>&nbsp;</td></tr>
+ * <tr><td>Code</td><td>6</td><td>$errorDetailCode</td></tr>
+ * <tr><td>Text</td><td>6</td><td>$errorDetailText (if empty then skip this element)</td></tr>
+ * </table>
+ * <b>Parameters:</b>
+ * <table border="1">
+ * <tr><th>name</th><th>default</th></tr>
+ * <tr><td>errorCode</td><td>&nbsp;</td></tr>
+ * <tr><td>errorReason</td><td>&nbsp;</td></tr>
+ * <tr><td>errorDetailCode</td><td>&nbsp;</td></tr>
+ * <tr><td>errorDetailText</td><td>&nbsp;</td></tr>
+ * <tr><td>serviceName</td><td>&nbsp;</td></tr>
+ * <tr><td>serviceContext</td><td>&nbsp;</td></tr>
+ * <tr><td>operationName</td><td>&nbsp;</td></tr>
+ * <tr><td>operationVersion</td><td>1</td></tr>
  * </table>
  * </p>
  * @version Id
