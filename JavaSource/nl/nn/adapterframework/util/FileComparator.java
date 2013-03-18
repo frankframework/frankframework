6efc7ddbc00411a3170770def2c58aa08843b5e6
Jaco de Groot <jaco.de.groot@ibissource.org>
2013-03-18 15:41:08 +0000

    Added log and author

--- JavaSource/nl/nn/adapterframework/util/FileComparator.java
+++ JavaSource/nl/nn/adapterframework/util/FileComparator.java
@@ -1,8 +1,17 @@
+/*
+ * $Log: FileComparator.java,v $
+ * Revision 1.2  2013-03-18 15:41:08  m00f069
+ * Added log and author
+ *
+ */
 package nl.nn.adapterframework.util;
 
 import java.io.File;
 import java.util.Comparator;
 
+/**
+ * @author Jaco de Groot
+ */
 public class FileComparator implements Comparator<File> {
 
 	public int compare(File file1, File file2) {
