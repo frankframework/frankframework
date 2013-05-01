933f4c1340f11ed6b9f37a0b3eb5f5888f007770
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2013-05-01 18:47:00 +0200

    original version of the test-classes. (dont't compile any more)

---
+++ adapterframework/src/test/java/nl/nn/adapterframework/soap/WsdlUtilsTest.java
@@ -0,0 +1,28 @@
+package nl.nn.adapterframework.soap;
+
+import nl.nn.adapterframework.util.ClassUtils;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+
+/**
+ * @author Michiel Meeuwissen
+ */
+public class WsdlUtilsTest {
+
+
+
+}
