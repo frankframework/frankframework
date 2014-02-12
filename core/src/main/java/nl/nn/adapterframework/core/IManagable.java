c4cc3b240410353d8d588fcbfbe4ce6790d47dce
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-02-12 17:52:19 +0100

    Fixed odd indentation

--- core/src/main/java/nl/nn/adapterframework/core/IManagable.java
+++ core/src/main/java/nl/nn/adapterframework/core/IManagable.java
@@ -18,26 +18,26 @@ package nl.nn.adapterframework.core;
 import nl.nn.adapterframework.util.RunStateEnum;
 /**
  * Models starting and stopping of objects that support such behaviour.
- * 
+ *
  * @author Gerrit van Brakel
  * @since 4.0
  */
 public interface IManagable extends INamedObject {
-/**
- * returns the runstate of the object.
- * Possible values are defined by {@link RunStateEnum}.
- */
-RunStateEnum getRunState();
-/**
- * Instruct the object that implements <code>IManagable</code> to start working.
- * The method does not wait for completion of the command; at return of this method, 
- * the object might be still in the STARTING-runstate
- */
-void startRunning();
-/**
- * Instruct the object that implements <code>IManagable</code> to stop working. 
- * The method does not wait for completion of the command; at return of this method, 
- * the object might be still in the STOPPING-runstate
- */
-void stopRunning();
+    /**
+     * returns the runstate of the object.
+     * Possible values are defined by {@link RunStateEnum}.
+     */
+    RunStateEnum getRunState();
+    /**
+     * Instruct the object that implements <code>IManagable</code> to start working.
+     * The method does not wait for completion of the command; at return of this method,
+     * the object might be still in the STARTING-runstate
+     */
+    void startRunning();
+    /**
+     * Instruct the object that implements <code>IManagable</code> to stop working.
+     * The method does not wait for completion of the command; at return of this method,
+     * the object might be still in the STOPPING-runstate
+     */
+    void stopRunning();
 }
