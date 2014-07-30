2b684dd73c9a21317578b9df8e0e5fe264ee82dd
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-07-30 12:18:51 +0200

    enabled some test cases again.

--- core/src/test/java/nl/nn/adapterframework/soap/XSDTest.java
+++ core/src/test/java/nl/nn/adapterframework/soap/XSDTest.java
@@ -1,46 +1,91 @@
 package nl.nn.adapterframework.soap;
 
+import nl.nn.adapterframework.util.ClassUtils;
+import nl.nn.adapterframework.validation.SchemaUtils;
+import nl.nn.adapterframework.validation.XSD;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.junit.Assert.assertEquals;
+
 /**
- * TODO It seems that XSD class was refactored
+ * TODO It seems that XSD class was refactored, breaking all tests.
  * @author Michiel Meeuwissen
  */
 public class XSDTest {
 
-	/*@Test
+	@Test
 	public void xsdName() throws URISyntaxException, XMLStreamException, IOException {
-		XSD xsd = new XSD("", "http://test", ClassUtils.getResourceURL("v1 test.xsd").toURI(), 0);
+		XSD xsd = new XSD(
+                ClassUtils.getResourceURL("v1 test.xsd"),
+                "http://test",
+                false,
+                null,
+                true);
 		assertEquals("v1 test.xsd", xsd.getName());
 	}
 
-
 	@Test
 	public void xsdNamespace() throws URISyntaxException, XMLStreamException, IOException {
-		XSD xsd = new XSD("", "http://test", ClassUtils.getResourceURL("v1 test.xsd").toURI(), 0);
-		// todo assertEquals("v1 test.xsd", xsd.getRootTag());
+        XSD xsd = new XSD(
+                ClassUtils.getResourceURL("v1 test.xsd"),
+                "http://test",
+                false,
+                null,
+                true);
+        assertEquals("http://test",
+                xsd.namespace);
+        assertEquals("http://www.ing.com/pim",
+                xsd.targetNamespace);
 	}
+
 
 	@Test
-	public void baseUrlXsd() throws URISyntaxException {
-		XSD xsd = new XSD("", "http://test",
-				new URI("file:/E:/nn/workspace/wsad/Ibis4EMSWEB/WebContent/WEB-INF/classes/AuthorizeUsers/xsd/AuthorizeUsersReply.xsd"), 0);
-		assertEquals("AuthorizeUsers/xsd/", xsd.getBaseUrl());
+	public void baseUrlXsd() throws URISyntaxException, IOException, XMLStreamException {
+        URL url = ClassUtils.getResourceURL("XSDTest/pim_imported.xsd");
+		XSD xsd = new XSD(
+				url,
+                "http://test",
+                false,
+                null,
+                true);
+		assertEquals("XSDTest/", xsd.getBaseUrl());
 	}
-
 
 	@Test
 	public void baseUrlXsdWebsphere() throws URISyntaxException, XMLStreamException, IOException {
-		XSD xsd = new XSD("", "http://test",
-				new URI("file:/data/WAS/6.1/wasap02/appl/Ibis4WUB-010_20111221-1815_wasap02.ear/Ibis4WUB.war/WEB-INF/classes/CalculateQuoteAndPolicyValuesLifeRetail/xsd/Calculation.xsd"), 0);
+		XSD xsd = new XSD(
+                new URL("file:/data/WAS/6.1/wasap02/appl/Ibis4WUB-010_20111221-1815_wasap02.ear/Ibis4WUB.war/WEB-INF/classes/CalculateQuoteAndPolicyValuesLifeRetail/xsd/Calculation.xsd"),
+                "http://test",
+                false, null, true);
+
 		assertEquals("CalculateQuoteAndPolicyValuesLifeRetail/xsd/", xsd.getBaseUrl());
 	}
 
 
 	@Test
 	public void writeXSD() throws XMLStreamException, IOException, ParserConfigurationException, SAXException, URISyntaxException {
-		XSD xsd = new XSD("", "http://test", ClassUtils.getResourceURL("XSDTest/test.xsd").toURI(), 0);
+		XSD xsd = new XSD(
+                ClassUtils.getResourceURL("XSDTest/test.xsd"),
+                "http://test", false, null, true);
+
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		XMLStreamWriter writer = WsdlUtils.createWriter(out, false);
-		xsd.write(writer, new HashMap<String, String>(), true, false);
+		XMLStreamWriter writer = WsdlUtils.getWriter(out, false);
+		SchemaUtils.xsdToXmlStreamWriter(xsd, writer, true);
 
 		DocumentBuilder dbuilder = WsdlTest.createDocumentBuilder();
 		Document result = dbuilder.parse(new ByteArrayInputStream(out.toByteArray()));
@@ -52,14 +97,17 @@ public class XSDTest {
 
 
 	}
-
+    /*
 	@Test
 	public void writeXSDStripSchemaLocation() throws XMLStreamException, IOException, ParserConfigurationException, SAXException, URISyntaxException {
-		XSD xsd = new XSD("", "http://test", ClassUtils.getResourceURL("XSDTest/test.xsd").toURI(), 0);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		XMLStreamWriter writer = WsdlUtils.createWriter(out, false);
-		xsd.write(writer, new HashMap<String, String>(), true, true);
+		XSD xsd = new XSD(
+                ClassUtils.getResourceURL("XSDTest/test.xsd"),
+                "http://test",
 
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		XMLStreamWriter writer = WsdlUtils.getWriter(out, false);
+        SchemaUtils.xsdToXmlStreamWriter(xsd, writer, true);
 		DocumentBuilder dbuilder = WsdlTest.createDocumentBuilder();
 		Document result = dbuilder.parse(new ByteArrayInputStream(out.toByteArray()));
 		Document expected = dbuilder.parse(getClass().getClassLoader().getResourceAsStream("XSDTest/test_expected_removed_imported_namespaces.xsd"));
@@ -70,7 +118,7 @@ public class XSDTest {
 
 
 	}
-
+    /*
 	@Test
 	public void importedXsds() throws URISyntaxException, XMLStreamException, IOException {
 		XSD xsd = new XSD("", "http://test", ClassUtils.getResourceURL("XSDTest/test.xsd").toURI(), 0);
