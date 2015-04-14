c2a941408e0ed2c6419f33b3897adf325ea369e0
Jaco de Groot <jaco.de.groot@ibissource.org>
2015-04-14 20:11:11 +0200

    Fix/improve warning about circural dependencies between schemas

--- core/src/main/java/nl/nn/adapterframework/validation/SchemaUtils.java
+++ core/src/main/java/nl/nn/adapterframework/validation/SchemaUtils.java
@@ -469,7 +469,7 @@ public class SchemaUtils {
 		if (xsds.size() == xsdsWithDependencies.size()) {
 			String message = "Circural dependencies between schemas:";
 			for (XSD xsd : xsdsWithDependencies) {
-				message = message + " [" + xsd + " with imported namespaces " + xsd.getImportedNamespaces() + "]";
+				message = message + " [" + xsd.toString() + " with target namespace " + xsd.getTargetNamespace() + " and imported namespaces " + xsd.getImportedNamespaces() + "]";
 			}
 			throw new ConfigurationException(message);
 		}
