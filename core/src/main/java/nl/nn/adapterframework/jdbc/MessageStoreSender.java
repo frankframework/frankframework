21bfb1230ffa2f031674d3647b4eb3e0a9220915
Jaco de Groot <jaco.de.groot@ibissource.org>
2015-04-25 15:22:32 +0200

    Add default value onlyStoreWhenMessageIdUnique to javadoc

--- core/src/main/java/nl/nn/adapterframework/jdbc/MessageStoreSender.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/MessageStoreSender.java
@@ -72,7 +72,7 @@ import org.apache.commons.lang.text.StrBuilder;
  * <tr><td>classname</td><td>nl.nn.adapterframework.jdbc.JdbcTransactionalStorage</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setSlotId(String) slotId}</td><td>identifier for this service</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setSessionKeys(String) sessionKeys}</td><td>comma separated list of sessionKey's to be stored together with the message</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setOnlyStoreWhenMessageIdUnique(boolean) onlyStoreWhenMessageIdUnique}</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ * <tr><td>{@link #setOnlyStoreWhenMessageIdUnique(boolean) onlyStoreWhenMessageIdUnique}</td><td>&nbsp;</td><td>true</td></tr>
  * </table>
  * </p>
  * 
