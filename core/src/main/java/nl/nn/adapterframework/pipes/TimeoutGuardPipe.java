a9478fcf38323ae614990f01d54e26f2fb46dce3
Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
2015-09-04 14:58:32 +0200

    add parameter timeout to TimeoutGuardPipe

--- core/src/main/java/nl/nn/adapterframework/pipes/TimeoutGuardPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/TimeoutGuardPipe.java
@@ -25,8 +25,13 @@ import java.util.concurrent.TimeoutException;
 import org.apache.log4j.NDC;
 
 import nl.nn.adapterframework.core.IPipeLineSession;
+import nl.nn.adapterframework.core.ParameterException;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.parameters.Parameter;
+import nl.nn.adapterframework.parameters.ParameterList;
+import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import nl.nn.adapterframework.parameters.ParameterValueList;
 
 /**
  * Extension to FixedForwardPipe for interrupting processing when timeout is exceeded.
@@ -37,6 +42,13 @@ import nl.nn.adapterframework.core.PipeRunResult;
  * <tr><td>{@link #setName(String) name}</td><td>name of the Pipe</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setThrowException(boolean) throwException}</td><td>when <code>true</code>, a PipeRunException is thrown. Otherwise the output is only logged as an error (and returned in a XML string with 'error' tags)</td><td>true</td></tr>
  * <tr><td>{@link #setTimeout(int) timeout}</td><td>timeout in seconds of obtaining a result</td><td>30</td></tr>
+ * </table>
+ * </p>
+ * <p>
+ * <table border="1">
+ * <b>Parameters:</b>
+ * <tr><th>name</th><th>type</th><th>remarks</th></tr>
+ * <tr><td>timeout</td><td>int</td><td>When a parameter with name timeout is present, it is used instead of the timeout specified by the attribute</td></tr>
  * </table>
  * </p>
  * 
@@ -71,19 +83,38 @@ public class TimeoutGuardPipe extends FixedForwardPipe {
 
 	public PipeRunResult doPipe(Object input, IPipeLineSession session)
 			throws PipeRunException {
+		ParameterValueList pvl = null;
+		if (getParameterList() != null) {
+			ParameterResolutionContext prc = new ParameterResolutionContext(
+					(String) input, session);
+			try {
+				pvl = prc.getValues(getParameterList());
+			} catch (ParameterException e) {
+				throw new PipeRunException(this, getLogPrefix(session)
+						+ "exception on extracting parameters", e);
+			}
+		}
+		int timeout_work;
+		String timeout_work_str = getParameterValue(pvl, "timeout");
+		if (timeout_work_str == null) {
+			timeout_work = getTimeout();
+		} else {
+			timeout_work = Integer.valueOf(timeout_work_str);
+		}
+
 		DoPipe doPipe = new DoPipe(input, session, Thread.currentThread().getName(), NDC.peek());
 		ExecutorService service = Executors.newSingleThreadExecutor();
 		Future future = service.submit(doPipe);
 		String result = null;
 		try {
 			log.debug(getLogPrefix(session) + "setting timeout of ["
-					+ getTimeout() + "] s");
-			result = (String) future.get(getTimeout(), TimeUnit.SECONDS);
+					+ timeout_work + "] s");
+			result = (String) future.get(timeout_work, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			String msg;
 			if (e instanceof TimeoutException) {
 				String errorMsg = getLogPrefix(session)
-						+ "exceeds timeout of [" + getTimeout()
+						+ "exceeds timeout of [" + timeout_work
 						+ "] s, interupting";
 				future.cancel(true);
 				msg = e.getClass().getName() + ": " + errorMsg;
@@ -108,6 +139,20 @@ public class TimeoutGuardPipe extends FixedForwardPipe {
 	public String doPipeWithTimeoutGuarded(Object input,
 			IPipeLineSession session) throws PipeRunException {
 		return input.toString();
+	}
+
+	private String getParameterValue(ParameterValueList pvl,
+			String parameterName) {
+		ParameterList parameterList = getParameterList();
+		if (pvl != null && parameterList != null) {
+			for (int i = 0; i < parameterList.size(); i++) {
+				Parameter parameter = parameterList.getParameter(i);
+				if (parameter.getName().equalsIgnoreCase(parameterName)) {
+					return pvl.getParameterValue(i).asStringValue(null);
+				}
+			}
+		}
+		return null;
 	}
 
 	public void setThrowException(boolean b) {
