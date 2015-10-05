29ba267fe2cca6388b7554b101ccb1015f1333bf
Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
2015-10-05 14:15:32 +0200

    Fix for "Ladybug: Add functionality to easily replace fixed strings"

--- larva/src/main/java/nl/nn/adapterframework/testtool/TestTool.java
+++ larva/src/main/java/nl/nn/adapterframework/testtool/TestTool.java
@@ -3214,10 +3214,12 @@ public class TestTool {
 
 	public static String replaceKey(String string, String from, String to) {
 		String result = string;
-		int i = result.indexOf(from);
-		while (i != -1) {
-			result = result.substring(0, i) + to + result.substring(i + from.length());
-			i = result.indexOf(from, i);
+		if (!from.equals(to)) {
+			int i = result.indexOf(from);
+			while (i != -1) {
+				result = result.substring(0, i) + to + result.substring(i + from.length());
+				i = result.indexOf(from, i);
+			}
 		}
 		return result;
 	}
