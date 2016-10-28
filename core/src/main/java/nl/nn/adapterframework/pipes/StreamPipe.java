08bec31f9e3f0a96b4fa8bbebf0257d4ac189799
Jaco de Groot <jaco.de.groot@ibissource.org>
2016-10-28 11:39:31 +0200

    StreamPipe: Use parameters instead of attributes

--- core/src/main/java/nl/nn/adapterframework/pipes/StreamPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/StreamPipe.java
@@ -18,16 +18,18 @@ package nl.nn.adapterframework.pipes;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import nl.nn.adapterframework.core.IPipeLineSession;
+import nl.nn.adapterframework.core.ParameterException;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
 import nl.nn.adapterframework.http.HttpSender;
+import nl.nn.adapterframework.parameters.ParameterList;
+import nl.nn.adapterframework.parameters.ParameterResolutionContext;
 import nl.nn.adapterframework.util.Misc;
-
-import org.apache.commons.lang.StringUtils;
 
 /**
  * Stream an input stream to an output stream.
@@ -36,12 +38,16 @@ import org.apache.commons.lang.StringUtils;
  * <table border="1">
  * <tr><th>attributes</th><th>description</th><th>default</th></tr>
  * <tr><td>{@link #setName(String) name}</td><td>name of the Pipe</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setInputStreamSessionKey(String) inputStreamSessionKey}</td><td>when set, the input stream object is taken from this session key, instead of regular input</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setOutputStreamSessionKey(String) outputStreamSessionKey}</td><td>the session key which holds the output stream object unless httpResponseSessionKey is specified</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setHttpResponseSessionKey(String) httpResponseSessionKey}</td><td>the session key which holds an HttpServletResponse object to stream to (the output stream is retrieved by calling getOutputStream() on the HttpServletResponse object)</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setContentType(String) contentType}</td><td>the Content-Type header to set in case httpResponseSessionKey was specified</td><td>&nbsp;</td></tr>
- * <tr><td>{@link #setContentDisposition(String) contentDisposition}</td><td>the Content-Disposition header to set in case httpResponseSessionKey was specified</td><td>&nbsp;</td></tr>
  * <tr><td>{@link #setForwardName(String) forwardName}</td>  <td>name of forward returned upon completion</td><td>"success"</td></tr>
+ * </table>
+ * <b>Parameters:</b>
+ * <table border="1">
+ * <tr><th>name</th><th>default</th></tr>
+ * <tr><td>inputStream</td><td>the input stream object to use instead of an input stream object taken from pipe input</td></tr>
+ * <tr><td>outputStream</td><td>the output stream object to use unless httpResponse parameter is specified</td></tr>
+ * <tr><td>httpResponse</td><td>an HttpServletResponse object to stream to (the output stream is retrieved by calling getOutputStream() on the HttpServletResponse object)</td></tr>
+ * <tr><td>contentType</td><td>the Content-Type header to set in case httpResponse was specified</td></tr>
+ * <tr><td>contentDisposition</td><td>the Content-Disposition header to set in case httpResponse was specified</td></tr>
  * </table>
  * </p>
  * <p><b>Exits:</b>
@@ -54,73 +60,62 @@ import org.apache.commons.lang.StringUtils;
  * @author Jaco de Groot
  */
 public class StreamPipe extends FixedForwardPipe {
-	String inputStreamSessionKey;
-	String outputStreamSessionKey;
-	String httpResponseSessionKey;
-	String contentType;
-	String contentDisposition;
 
 	@Override
 	public PipeRunResult doPipe(Object input, IPipeLineSession session) throws PipeRunException {
-		InputStream inputStream;
-		if (StringUtils.isNotEmpty(getInputStreamSessionKey())) {
-			inputStream = (InputStream)session.get(getInputStreamSessionKey());
+		String inputString;
+		if (input instanceof String) {
+			inputString = (String)input;
 		} else {
+			inputString = "";
+		}
+		ParameterResolutionContext prc = new ParameterResolutionContext(inputString, session, isNamespaceAware());
+		Map parameters = null;
+		ParameterList parameterList = getParameterList();
+		if (parameterList != null) {
+			try {
+				parameters = prc.getValueMap(parameterList);
+			} catch (ParameterException e) {
+				throw new PipeRunException(this, "Could not resolve parameters", e);
+			}
+		}
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+		HttpServletResponse httpResponse = null;
+		String contentType = null;
+		String contentDisposition = null;
+		if (parameters != null) {
+			if (parameters.get("inputStream") != null) {
+				inputStream = (InputStream)parameters.get("inputStream");
+			}
+			if (parameters.get("outputStream") != null) {
+				outputStream = (OutputStream)parameters.get("outputStream");
+			}
+			if (parameters.get("httpResponse") != null) {
+				httpResponse = (HttpServletResponse)parameters.get("httpResponse");
+			}
+			if (parameters.get("contentType") != null) {
+				contentType = (String)parameters.get("contentType");
+			}
+			if (parameters.get("contentDisposition") != null) {
+				contentDisposition = (String)parameters.get("contentDisposition");
+			}
+		}
+		if (inputStream == null) {
 			inputStream = (InputStream)input;
 		}
 		try {
-			if (StringUtils.isNotEmpty(getHttpResponseSessionKey())) {
-				HttpServletResponse response = (HttpServletResponse)session.get(getHttpResponseSessionKey());
+			if (httpResponse != null) {
 				HttpSender.streamResponseBody(inputStream, contentType,
-						contentDisposition, response, log,
+						contentDisposition, httpResponse, log,
 						getLogPrefix(session));
-			} else if (StringUtils.isNotEmpty(getOutputStreamSessionKey())) {
-				Misc.streamToStream(inputStream, (OutputStream)session.get(getOutputStreamSessionKey()));
+			} else {
+				Misc.streamToStream(inputStream, outputStream);
 			}
 		} catch (IOException e) {
 			throw new PipeRunException(this, "IOException streaming input to output", e);
 		}
 		return new PipeRunResult(getForward(), input);
-	}
-
-	public String getInputStreamSessionKey() {
-		return inputStreamSessionKey;
-	}
-
-	public void setInputStreamSessionKey(String inputStreamSessionKey) {
-		this.inputStreamSessionKey = inputStreamSessionKey;
-	}
-
-	public String getOutputStreamSessionKey() {
-		return outputStreamSessionKey;
-	}
-
-	public void setOutputStreamSessionKey(String outputStreamSessionKey) {
-		this.outputStreamSessionKey = outputStreamSessionKey;
-	}
-
-	public String getHttpResponseSessionKey() {
-		return httpResponseSessionKey;
-	}
-
-	public void setHttpResponseSessionKey(String httpResponseSessionKey) {
-		this.httpResponseSessionKey = httpResponseSessionKey;
-	}
-
-	public String getContentType() {
-		return contentType;
-	}
-
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
-
-	public String getContentDisposition() {
-		return contentDisposition;
-	}
-
-	public void setContentDisposition(String contentDisposition) {
-		this.contentDisposition = contentDisposition;
 	}
 
 }
