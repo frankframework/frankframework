ae5fd1b6ca8fda98f546fd58dfb0f69ab2f3c394
bastiaan <bastiaan@integrationpartners.nl>
2017-05-05 16:35:11 +0200

    no message

---
+++ core/src/test/java/nl/nn/adapterframework/webcontrol/api/SampleSauceTest.java
@@ -0,0 +1,36 @@
+package nl.nn.adapterframework.webcontrol.api;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class SampleSauceTest {
+
+	private static WebDriver driver;
+	public static final String USERNAME = "Baswat";
+	public static final String AUTOMATE_KEY = "d835004c-97a1-4e52-b63f-daa5b1d3d3fd";
+		private static String URL = "https://"+ USERNAME + ":" + AUTOMATE_KEY +  "@localhost:4445"; 
+//	private static String URL = "https://"+ USERNAME + ":" + AUTOMATE_KEY +  "@ondemand.saucelabs.com:443/wd/hub";
+
+	@Test
+	public void SampleSauceTest() throws Exception {
+
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("platform", "Windows XP");
+		caps.setCapability("version", "43.0");
+
+		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+
+		/**
+		 * Goes to Sauce Lab's guinea-pig page and prints title
+		 */
+
+		driver.get("https://saucelabs.com/test/guinea-pig");
+		System.out.println("title of page is: " + driver.getTitle());
+
+		driver.quit();
+	}
+}
