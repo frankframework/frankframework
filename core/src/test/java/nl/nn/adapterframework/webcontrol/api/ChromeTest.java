735ba03b23fe6d6e81d598dbc2627b033a8d4aeb
MSI\Laurens <laurens@integrationpartners.nl>
2018-01-15 16:17:52 +0100

    Do sum moar tests

--- core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTest.java
+++ core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTest.java
@@ -1,37 +1,36 @@
 package nl.nn.adapterframework.webcontrol.api;
 
-import java.util.concurrent.TimeUnit;
- 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class ChromeTest {
-	private static ChromeDriver driver;
-	WebElement element;
-	 
-	@BeforeClass
-	public static void openBrowser(){
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Laurens/chromedriver_win32/chromedriver.exe");
-		driver = new ChromeDriver();
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
-	 
+
+	private static WebDriver driver;
+	public static final String USERNAME = "Baswat";
+	public static final String AUTOMATE_KEY = "d835004c-97a1-4e52-b63f-daa5b1d3d3fd";
+//	private static String URL = "http://"+ USERNAME + ":" + AUTOMATE_KEY +  "@localhost:4445/wd/hub"; 
+		private static String URL = "https://"+ USERNAME + ":" + AUTOMATE_KEY +  "@ondemand.saucelabs.com:443/wd/hub";
+
 	@Test
-	public void ibis4example (){
-	    System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-	    driver.get("http://ibis4example.ibissource.org/iaf/gui/#/status");
-	    Assert.assertEquals("IAF | Adapter Status", driver.getTitle());
-	    Assert.assertTrue(true);
-	    System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
-	}
-	
-	@AfterClass
-	public static void closeBrowser(){
-	    driver.quit();
+	public void SampleSauceTest() throws Exception {
+
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("platform", "Windows XP");
+		caps.setCapability("version", "43.0");
+
+		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+
+		/**
+		 * Goes to Sauce Lab's guinea-pig page and prints title
+		 */
+
+		driver.get("https://saucelabs.com/test/guinea-pig");
+		System.out.println("title of page is: " + driver.getTitle());
+		
+		driver.quit();
 	}
 }
