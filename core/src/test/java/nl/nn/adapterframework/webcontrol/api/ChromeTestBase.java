546d8e1a4bd0bf1cf48bfd46e8ddd5f324c42f77
MSI\Laurens <laurens@integrationpartners.nl>
2018-01-16 16:14:09 +0100

    Testing ChromeTestBase

--- core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestBase.java
+++ core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestBase.java
@@ -1,25 +1,23 @@
 package nl.nn.adapterframework.webcontrol.api;
 
-import org.junit.Test;
-import org.junit.Assert;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.URL;
-
-public class ChromeTest extends ChromeTestBase {
-
+public abstract class ChromeTestBase {
 	private static WebDriver driver;
 	public static final String USERNAME = "Baswat";
 	public static final String AUTOMATE_KEY = "d835004c-97a1-4e52-b63f-daa5b1d3d3fd";
 	private static String URL = "https://"+ USERNAME + ":" + AUTOMATE_KEY +  "@ondemand.saucelabs.com:443/wd/hub";
+	
+	public void setup() throws Exception {
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("platform", "Windows");
+		caps.setCapability("version", "43.0");
 
-	@Test
-	public void SampleSauceTest() throws Exception {
-		driver.get("http://ibis4example.ibissource.org/iaf/gui/#/status");
-		Assert.assertEquals("IAF | Adapter Status", driver.getTitle());
-		Assert.assertTrue(true);
-		driver.quit();
+		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 	}
 }
