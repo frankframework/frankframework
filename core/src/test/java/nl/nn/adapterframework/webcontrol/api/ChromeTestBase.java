f5ad18457c2287b5bac77f6671e60033f223e698
MSI\Laurens <laurens@integrationpartners.nl>
2018-01-16 16:28:02 +0100

    kekekek

--- core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestBase.java
+++ core/src/test/java/nl/nn/adapterframework/webcontrol/api/ChromeTestBase.java
@@ -8,16 +8,16 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public abstract class ChromeTestBase {
-	private static WebDriver driver;
-	public static final String USERNAME = "Baswat";
-	public static final String AUTOMATE_KEY = "d835004c-97a1-4e52-b63f-daa5b1d3d3fd";
+	public static WebDriver driver;
+	private static final String USERNAME = "Baswat";
+	private static final String AUTOMATE_KEY = "d835004c-97a1-4e52-b63f-daa5b1d3d3fd";
 	private static String URL = "https://"+ USERNAME + ":" + AUTOMATE_KEY +  "@ondemand.saucelabs.com:443/wd/hub";
 	
-	public ChromeTestBase() throws Exception {
+	public ChromeTestBase(WebDriver driver) throws Exception {
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
 		caps.setCapability("platform", "Windows");
 		caps.setCapability("version", "43.0");
 
-		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+		driver = new RemoteWebDriver(new URL(URL), caps);
 	}
 }
