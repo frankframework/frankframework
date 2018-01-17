8e943d87fa4a4602898beadd30a28a37f464db55
Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
2018-01-17 10:42:49 +0100

    Fix copyright year

--- core/src/main/java/nl/nn/adapterframework/util/JdbcUtil.java
+++ core/src/main/java/nl/nn/adapterframework/util/JdbcUtil.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2013 Nationale-Nederlanden
+   Copyright 2013, 2014, 2017, 2018 Nationale-Nederlanden
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
