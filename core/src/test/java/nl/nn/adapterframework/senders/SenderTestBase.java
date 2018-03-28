397fd3ee4a657e9fced71d699f336c4929c40189
Niels Meijer <nielsmeijer@hotmail.com>
2018-03-28 13:28:11 +0200

    Add unit tests for senders

---
+++ core/src/test/java/nl/nn/adapterframework/senders/SenderTestBase.java
@@ -0,0 +1,59 @@
+package nl.nn.adapterframework.senders;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.mockito.Mock;
+
+import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.IPipeLineSession;
+import nl.nn.adapterframework.core.ISender;
+import nl.nn.adapterframework.core.PipeStartException;
+import nl.nn.adapterframework.core.SenderException;
+import nl.nn.adapterframework.pipes.XmlValidator;
+
+public abstract class SenderTestBase<S extends ISender> {
+
+	protected Log log = LogFactory.getLog(this.getClass());
+	protected S sender;
+
+	@Mock
+	protected IPipeLineSession session;
+
+	public abstract S createSender();
+
+	@Before
+	public void setup() throws ConfigurationException, PipeStartException, SenderException {
+		sender = createSender();
+		sender.open();
+	}
+
+	@After
+	public void setdown() throws SenderException {
+		sender.close();
+	}
+
+	protected String readLines(Reader reader) throws IOException {
+		BufferedReader buf = new BufferedReader(reader);
+		StringBuilder string = new StringBuilder();
+		String line = buf.readLine();
+		while (line != null) {
+			string.append(line);
+			line = buf.readLine();
+			if (line!=null) {
+				string.append("\n");
+			}
+		}
+		return string.toString();	
+	}
+
+	protected String getFile(String file) throws IOException {
+		return readLines(new InputStreamReader(XmlValidator.class.getResourceAsStream(file)));
+	}
+}
---
+++ core/src/test/java/nl/nn/adapterframework/senders/XsltSenderTest.java
@@ -0,0 +1,35 @@
+package nl.nn.adapterframework.senders;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.SenderException;
+import nl.nn.adapterframework.core.TimeOutException;
+import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+
+import org.junit.Test;
+
+public class XsltSenderTest extends SenderTestBase<XsltSender> {
+
+	@Override
+	public XsltSender createSender() {
+		return new XsltSender();
+	}
+
+	@Test
+	public void basic() throws SenderException, TimeOutException, ConfigurationException, IOException {
+		sender.setStyleSheetName("/Xslt/duplicateImport/root.xsl");
+		sender.configure();
+
+		String input=getFile("/Xslt/duplicateImport/in.xml");
+		log.debug("inputfile ["+input+"]");
+		String expected=getFile("/Xslt/duplicateImport/out.xml");
+
+		ParameterResolutionContext prc = new ParameterResolutionContext(input, session);
+		String result = sender.sendMessage(null, input, prc);
+
+		assertEquals(expected.replaceAll("\\s",""), result.replaceAll("\\s",""));
+	}
+}
