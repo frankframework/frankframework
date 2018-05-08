5031ac7cad7fb2901529a76f53cc7b192e79b9c4
Niels Meijer <nielsmeijer@hotmail.com>
2018-05-08 17:53:49 +0200

    Add ContentType header back to WebServiceSender

--- core/src/main/java/nl/nn/adapterframework/http/WebServiceSender.java
+++ core/src/main/java/nl/nn/adapterframework/http/WebServiceSender.java
@@ -123,6 +123,7 @@ public class WebServiceSender extends HttpSender {
 	public WebServiceSender() {
 		super();
 		setMethodType("POST");
+		setContentType("text/xml; charset="+Misc.DEFAULT_INPUT_STREAM_ENCODING);
 	}
 
 	@Override
