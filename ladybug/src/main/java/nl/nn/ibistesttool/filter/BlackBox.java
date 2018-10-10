647d32a134da7ba26ca1e219315bfb688393cd7d
Jaco de Groot <jaco.de.groot@ibissource.org>
2018-10-10 15:02:33 +0200

    Add copyright statements to Ladybug module

--- ladybug/src/main/java/nl/nn/ibistesttool/filter/BlackBox.java
+++ ladybug/src/main/java/nl/nn/ibistesttool/filter/BlackBox.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2018 Nationale-Nederlanden
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package nl.nn.ibistesttool.filter;
 
 import java.util.List;
@@ -5,7 +20,6 @@ import java.util.List;
 import nl.nn.testtool.Checkpoint;
 import nl.nn.testtool.Report;
 import nl.nn.testtool.filter.CheckpointMatcher;
-import nl.nn.testtool.util.SearchUtil;
 
 /**
  * @author Jaco de Groot
--- ladybug/src/main/java/nl/nn/ibistesttool/filter/FtpFileRetrieverPipe.java
+++ ladybug/src/main/java/nl/nn/ibistesttool/filter/FtpFileRetrieverPipe.java
@@ -1,11 +1,23 @@
-package nl.nn.ibistesttool.filter;
+/*
+   Copyright 2018 Nationale-Nederlanden
 
-import java.util.List;
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package nl.nn.ibistesttool.filter;
 
 import nl.nn.testtool.Checkpoint;
 import nl.nn.testtool.Report;
 import nl.nn.testtool.filter.CheckpointMatcher;
-import nl.nn.testtool.util.SearchUtil;
 
 /**
  * The FtpFileRetrieverPipe should be converted to a Sender in the Ibis
