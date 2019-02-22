0e6dcaf0ff94c056e99e7a2447f6fec97bb344ba
Peter Leeuwenburgh <peter.leeuwenburgh@ibissource.org>
2019-02-22 12:11:36 +0100

    Add junit test for ReasonSessionKey

--- core/src/test/java/nl/nn/adapterframework/pipes/WsdlXmlValidatorTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/WsdlXmlValidatorTest.java
@@ -1,8 +1,11 @@
 package nl.nn.adapterframework.pipes;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.wsdl.WSDLException;
 
@@ -12,6 +15,7 @@ import org.xml.sax.SAXException;
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.IPipeLineSession;
 import nl.nn.adapterframework.core.PipeForward;
+import nl.nn.adapterframework.core.PipeLineSessionBase;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.validation.ValidatorTestBase;
 import nl.nn.adapterframework.validation.XmlValidatorException;
@@ -21,18 +25,13 @@ import nl.nn.adapterframework.validation.XmlValidatorException;
   * @author Michiel Meeuwissen
  */
 
-
 public class WsdlXmlValidatorTest {
-
     private static final String SIMPLE                = ValidatorTestBase.BASE_DIR_VALIDATION+"/Wsdl/SimpleWsdl/simple.wsdl";
     private static final String SIMPLE_WITH_INCLUDE   = ValidatorTestBase.BASE_DIR_VALIDATION+"/Wsdl/SimpleWsdl/simple_withinclude.wsdl";
     private static final String SIMPLE_WITH_REFERENCE = ValidatorTestBase.BASE_DIR_VALIDATION+"/Wsdl/SimpleWsdl/simple_withreference.wsdl";
     private static final String TIBCO                 = ValidatorTestBase.BASE_DIR_VALIDATION+"/Tibco/wsdl/BankingCustomer_01_GetPartyBasicDataBanking_01_concrete1.wsdl";
 
-
     private IPipeLineSession session = mock(IPipeLineSession.class);
-
-
 
     @Test
     public void wsdlValidate() throws IOException, PipeRunException, SAXException, WSDLException, ConfigurationException, XmlValidatorException {
@@ -76,8 +75,6 @@ public class WsdlXmlValidatorTest {
         val.configure();
         val.validate("<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\"><Body><TradePriceRequest xmlns=\"http://example.com/stockquote.xsd\"><tickerSymbolERROR>foo</tickerSymbolERROR></TradePriceRequest></Body></Envelope>", session);
     }
-
-
 
     @Test
     public void wsdlTibco() throws IOException, PipeRunException, SAXException, WSDLException, ConfigurationException, XmlValidatorException {
@@ -185,7 +182,32 @@ public class WsdlXmlValidatorTest {
                 "", session);
     }
 
-
-
+    @Test
+	public void wsdlReasonSessionKey() throws ConfigurationException,
+			XmlValidatorException, PipeRunException {
+		WsdlXmlValidator val = new WsdlXmlValidator();
+		val.setWsdl(SIMPLE);
+		val.setSoapBody("TradePriceRequest");
+		val.setForwardFailureToSuccess(true);
+		val.registerForward(new PipeForward("success", null));
+		val.configure();
+		IPipeLineSession pls = new PipeLineSessionBase();
+		val.validate("<xml/>", pls);
+		List<String> lines = Arrays.asList(
+				((String) pls.get(val.getReasonSessionKey())).split("\\r?\\n"));
+		assertEquals(
+				"Validation using WsdlXmlValidator with '/Validation/Wsdl/SimpleWsdl/simple.wsdl' failed:",
+				lines.get(0));
+		assertEquals(
+				"/: at (1,7): cvc-elt.1.a: Cannot find the declaration of element 'xml'.",
+				lines.get(1));
+		assertEquals(
+				"/: Illegal element 'xml'. Element(s) 'Envelope' expected.",
+				lines.get(2));
+		assertEquals("/xml: Unknown namespace ''", lines.get(3));
+		assertEquals("/Envelope/Body: Element(s) 'TradePriceRequest' not found",
+				lines.get(4));
+		assertEquals("/: Element(s) 'Envelope' not found", lines.get(5));
+	}
 }
 
