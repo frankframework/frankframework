33c75f38a551dfedcdf94dcbecb2a817c0f0d50a
alisihab <sihab.akcan@gmail.com>
2019-03-06 16:48:24 +0100

    Introduce LocalFileSystem and FileSystemSender

--- core/src/main/java/nl/nn/adapterframework/filesystem/IFileSystemBase.java
+++ core/src/main/java/nl/nn/adapterframework/filesystem/IFileSystemBase.java
@@ -3,22 +3,34 @@ package nl.nn.adapterframework.filesystem;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.Iterator;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.util.XmlBuilder;
 
 
 public interface IFileSystemBase<F> {
 
 	public void configure() throws ConfigurationException;
-	
+	public void open() throws FileSystemException;
+	public void close() throws FileSystemException;
+
 	public F toFile(String filename) throws FileSystemException;
 	public Iterator<F> listFiles() throws FileSystemException;
+	
 	public boolean exists(F f) throws FileSystemException;
-	public OutputStream createFile(F f) throws FileSystemException,IOException;
-	public OutputStream appendFile(F f) throws FileSystemException,IOException;
-	public InputStream readFile(F f) throws FileSystemException,IOException;
+	public OutputStream createFile(F f) throws FileSystemException, IOException;
+	public OutputStream appendFile(F f) throws FileSystemException, IOException;
+	public InputStream readFile(F f) throws FileSystemException, IOException;
 	public void deleteFile(F f) throws FileSystemException;
-	public String getInfo(F f) throws FileSystemException;
+	public void renameTo(F f, String destination) throws FileSystemException;
+
+	public long getFileSize(F f, boolean isFolder) throws FileSystemException;
+	public String getName(F f) throws FileSystemException;
+	public String getCanonicalName(F f, boolean isFolder) throws FileSystemException;
+	public Date getModificationTime(F f, boolean isFolder) throws FileSystemException;
+
+	public void augmentFileInfo(XmlBuilder fileInfo, F f);
 	
 }
--- core/src/main/java/nl/nn/adapterframework/filesystem/LocalFileSystem.java
+++ core/src/main/java/nl/nn/adapterframework/filesystem/LocalFileSystem.java
@@ -7,27 +7,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.Iterator;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
-import nl.nn.adapterframework.util.Dir2Xml;
+import nl.nn.adapterframework.util.XmlBuilder;
 
 public class LocalFileSystem implements IFileSystem<File> {
 
 	private String directory;
+	private boolean isForce;
 
 	private class FilePathIterator implements Iterator<File> {
 
 		private File files[];
-		int i=0;
-		
+		int i = 0;
+
 		FilePathIterator(File files[]) {
-			this.files=files;
+			this.files = files;
 		}
-		
+
 		@Override
 		public boolean hasNext() {
-			return files!=null && i<files.length;
+			return files != null && i < files.length;
 		}
 
 		@Override
@@ -39,16 +41,16 @@ public class LocalFileSystem implements IFileSystem<File> {
 		public void remove() {
 			deleteFile(files[i++]);
 		}
-		
+
 	}
-	
+
 	@Override
 	public void configure() throws ConfigurationException {
 	}
 
 	@Override
 	public File toFile(String filename) {
-		return new File(getDirectory(),filename);
+		return new File(getDirectory(), filename);
 	}
 
 	@Override
@@ -64,12 +66,12 @@ public class LocalFileSystem implements IFileSystem<File> {
 
 	@Override
 	public OutputStream createFile(File f) throws IOException {
-		return new FileOutputStream(f,false);
+		return new FileOutputStream(f, false);
 	}
 
 	@Override
 	public OutputStream appendFile(File f) throws FileNotFoundException {
-		return new FileOutputStream(f,true);
+		return new FileOutputStream(f, true);
 	}
 
 	@Override
@@ -80,11 +82,6 @@ public class LocalFileSystem implements IFileSystem<File> {
 	@Override
 	public void deleteFile(File f) {
 		f.delete();
-	}
-
-	@Override
-	public String getInfo(File f) {
-		return Dir2Xml.getFileAsXmlBuilder(f, f.getName()).toXML();
 	}
 
 	@Override
@@ -109,9 +106,69 @@ public class LocalFileSystem implements IFileSystem<File> {
 	public String getDirectory() {
 		return directory;
 	}
+
 	public void setDirectory(String directory) {
 		this.directory = directory;
 	}
 
+	@Override
+	public void renameTo(File f, String destination) throws FileSystemException {
+		File dest;
+
+		dest = new File(destination);
+		if (dest.exists()) {
+			if (isForce)
+				dest.delete();
+			else {
+				throw new FileSystemException("Cannot rename file. Destination file already exits.");
+			}
+		}
+		f.renameTo(dest);
+
+	}
+
+	@Override
+	public long getFileSize(File f, boolean isFolder) throws FileSystemException {
+		return f.length();
+	}
+
+	@Override
+	public String getName(File f) throws FileSystemException {
+		return f.getName();
+	}
+
+	@Override
+	public String getCanonicalName(File f, boolean isFolder) throws FileSystemException {
+		try {
+			return f.getCanonicalPath();
+		} catch (IOException e) {
+			throw new FileSystemException(e);
+		}
+	}
+
+	@Override
+	public Date getModificationTime(File f, boolean isFolder) throws FileSystemException {
+		return new Date(f.lastModified());
+	}
+
+	@Override
+	public void augmentFileInfo(XmlBuilder fileInfo, File f) {
+	}
+
+	@Override
+	public void open() {
+		// No Action is required
+
+	}
+
+	@Override
+	public void close() {
+		// No Action is required
+
+	}
+
+	public void setIsForce(boolean force) {
+		this.isForce = force;
+	}
 
 }
