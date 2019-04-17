2f9057064ef1be449986b11e8a340934a516c64d
Gerrit van Brakel <g.vanbrakel@flux-it.n;>
2019-04-17 12:13:25 +0200

    Extracted Samba2FileSystemTestHelper

--- core/src/it/java/nl/nn/adapterframework/senders/Samba2FileSystemTestHelper.java
+++ core/src/it/java/nl/nn/adapterframework/senders/Samba2FileSystemTestHelper.java
@@ -3,9 +3,24 @@ package nl.nn.adapterframework.senders;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.PrivilegedExceptionAction;
+import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.security.auth.kerberos.KerberosPrincipal;
+import javax.security.auth.login.LoginContext;
+
+import org.apache.commons.lang.StringUtils;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
+import org.ietf.jgss.GSSName;
+import org.ietf.jgss.Oid;
+import org.junit.After;
 
 import com.hierynomus.msdtyp.AccessMask;
 import com.hierynomus.mserref.NtStatus;
@@ -15,6 +30,7 @@ import com.hierynomus.mssmb2.SMB2ShareAccess;
 import com.hierynomus.mssmb2.SMBApiException;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.auth.GSSAuthenticationContext;
 import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
 import com.hierynomus.smbj.share.Directory;
@@ -24,33 +40,95 @@ import com.hierynomus.smbj.share.File;
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.filesystem.FileSystemException;
 import nl.nn.adapterframework.filesystem.IFileSystemTestHelper;
+import nl.nn.adapterframework.filesystem.KerberosLoginConfiguration;
+import nl.nn.adapterframework.filesystem.UsernameAndPasswordCallbackHandler;
 
 public class Samba2FileSystemTestHelper implements IFileSystemTestHelper {
 
+	private String authType = "SPNEGO";
 	private String shareName = "Share";
 	private String username = "";
 	private String password = "";
 	private String domain = "";
+	private String realm = "";
+	private String kdc = "";
 
 	private DiskShare client = null;
 	private Session session = null;
 	private Connection connection = null;
 	private SMBClient smbClient = null;
 		
-	public Samba2FileSystemTestHelper(String shareName, String username, String password, String domain) {
+	public Samba2FileSystemTestHelper(String shareName, String username, String password, String domain, String realm, String kdc) {
 		this.shareName=shareName;
 		this.username=username;
 		this.password=password;
 		this.domain=domain;
+		this.realm=realm;
+		this.kdc=kdc;
 	}
 	
 	@Override
-	public void setUp() throws ConfigurationException, IOException, FileSystemException {
-		AuthenticationContext auth = new AuthenticationContext(username, password.toCharArray(), domain);
+	public void setUp() throws IOException, ConfigurationException, FileSystemException {
+		AuthenticationContext auth = authenticate();
 		open(auth);
 	}
-	
+
+	private AuthenticationContext authenticate() throws FileSystemException {
+		if(StringUtils.equalsIgnoreCase(authType, "NTLM")) {
+			return new AuthenticationContext(username, password.toCharArray(), domain);
+		}else if(StringUtils.equalsIgnoreCase(authType, "SPNEGO")) {
+			if(!StringUtils.isEmpty(kdc) && !StringUtils.isEmpty(realm)) {
+				System.setProperty("java.security.krb5.kdc", kdc);
+				System.setProperty("java.security.krb5.realm", realm);
+			}
+			HashMap<String, String> loginParams = new HashMap<String, String>();
+			loginParams.put("principal", username);
+			LoginContext lc;
+			try {
+				lc = new LoginContext(username, null, 
+						new UsernameAndPasswordCallbackHandler(username, password),
+						new KerberosLoginConfiguration(loginParams));
+				lc.login();
+				
+				Subject subject = lc.getSubject();
+				KerberosPrincipal krbPrincipal = subject.getPrincipals(KerberosPrincipal.class).iterator().next();
+
+				Oid spnego = new Oid("1.3.6.1.5.5.2");
+				Oid kerberos5 = new Oid("1.2.840.113554.1.2.2");
+
+				final GSSManager manager = GSSManager.getInstance();
+
+				final GSSName name = manager.createName(krbPrincipal.toString(), GSSName.NT_USER_NAME);
+				Set<Oid> mechs = new HashSet<Oid>(Arrays.asList(manager.getMechsForName(name.getStringNameType())));
+				final Oid mech;
+
+				if (mechs.contains(kerberos5)) {
+					mech = kerberos5;
+				} else if (mechs.contains(spnego)) {
+					mech = spnego;
+				} else {
+					throw new IllegalArgumentException("No mechanism found");
+				}
+
+				GSSCredential creds = Subject.doAs(subject, new PrivilegedExceptionAction<GSSCredential>() {
+					@Override
+					public GSSCredential run() throws GSSException {
+						return manager.createCredential(name, GSSCredential.DEFAULT_LIFETIME, mech, GSSCredential.INITIATE_ONLY);
+					}
+				});
+
+				GSSAuthenticationContext auth = new GSSAuthenticationContext(krbPrincipal.getName(), krbPrincipal.getRealm(), subject, creds);
+				return auth;
+
+			} catch (Exception e) {
+				throw new FileSystemException(e);
+			}
+		}
+		return null;
+	}
+
 	@Override
+	@After
 	public void tearDown() throws Exception {
 		if (client != null) {
 			client.close();
@@ -62,7 +140,7 @@ public class Samba2FileSystemTestHelper implements IFileSystemTestHelper {
 			connection.close();
 		}
 	}
-	
+
 	public void open(AuthenticationContext auth) throws FileSystemException {
 		if (smbClient == null) {
 			smbClient = new SMBClient();
@@ -81,8 +159,6 @@ public class Samba2FileSystemTestHelper implements IFileSystemTestHelper {
 			throw new FileSystemException("Cannot connect to samba server", e);
 		}
 	}
-
-
 	@Override
 	public boolean _fileExists(String folder, String filename) throws Exception {
 		String path=folder==null?filename:folder+"/"+filename;
