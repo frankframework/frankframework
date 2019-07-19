700cbd65742946744d7b74e59665a4ed52731dcd
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2019-07-19 10:46:37 +0200

    Solve codacy issue

--- test-integration/src/test/java/nl/nn/adapterframework/filesystem/MailSendingTestHelper.java
+++ test-integration/src/test/java/nl/nn/adapterframework/filesystem/MailSendingTestHelper.java
@@ -2,7 +2,6 @@ package nl.nn.adapterframework.filesystem;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Properties;
 
 import org.junit.After;
 import org.junit.Before;
