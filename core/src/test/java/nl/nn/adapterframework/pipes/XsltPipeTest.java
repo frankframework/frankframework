aefeb6fefccd34e8f47ebad43d36e0bcfa5c6151
jarno <jarno@LAPTOP-UL4HNDH.interpar.nl>
2019-09-10 15:41:58 +0200

    Removed unused import statements in XsltPipeTest

--- core/src/test/java/nl/nn/adapterframework/pipes/XsltPipeTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/XsltPipeTest.java
@@ -9,12 +9,9 @@ import org.mockito.Mock;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.IPipeLineSession;
-import nl.nn.adapterframework.core.PipeLineSessionBase;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
 import nl.nn.adapterframework.core.PipeStartException;
-import nl.nn.adapterframework.core.SenderException;
-import nl.nn.adapterframework.core.TimeOutException;
 import nl.nn.adapterframework.testutil.TestFileUtils;
 
 public class XsltPipeTest extends PipeTestBase<XsltPipe> {
@@ -41,4 +38,5 @@ public class XsltPipeTest extends PipeTestBase<XsltPipe> {
 		String xmlOut=(String)prr.getResult();
 		assertEquals(expected,xmlOut.trim());
 	}
+
 }
