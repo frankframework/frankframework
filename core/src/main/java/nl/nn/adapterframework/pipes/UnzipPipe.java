a0c5283641ae93bda40ad02f317abea5b7e124b9
Martijn Dirkse <mhdirkse@live.nl>
2019-09-30 10:54:40 +0200

    UnzipPipe, better document unique number added tof extracted file name (#205)

--- core/src/main/java/nl/nn/adapterframework/pipes/UnzipPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/UnzipPipe.java
@@ -43,7 +43,6 @@ import nl.nn.adapterframework.util.XmlUtils;
 /**
  * Assumes input to be the file name of a ZIP archive, and unzips it to a
  * directory and/or an XML message.
- *
  * <br>
  * The output of each unzipped item is returned in XML as follows when
  * collectFileContents is false:
@@ -77,7 +76,14 @@ import nl.nn.adapterframework.util.XmlUtils;
  *       ...
  *  &lt;/results&gt;
  * </pre>
- * 
+ * <br>
+ * By default, this pipe takes care
+ * to produce unique file names, as follows. When the filename within
+ * the archive is:
+ * <pre>&lt;basename&gt; + "." + &lt;extension&gt;</pre>
+ * then the extracted filename (path omitted) becomes
+ * <pre>&lt;basename&gt; + &lt;unique number&gt; + "." + &lt;extension&gt;</pre>
+ * <br>
  * <p><b>Exits:</b>
  * <table border="1">
  * <tr><th>state</th><th>condition</th></tr>
