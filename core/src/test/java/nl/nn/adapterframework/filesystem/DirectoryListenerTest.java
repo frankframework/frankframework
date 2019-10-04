5d307e75b3ba6f46aac7ffc75db3e945032b25cb
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2019-10-04 13:02:54 +0200

    Clean up of DirectoryListenerTest

--- core/src/test/java/nl/nn/adapterframework/filesystem/DirectoryListenerTest.java
+++ core/src/test/java/nl/nn/adapterframework/filesystem/DirectoryListenerTest.java
@@ -1,32 +1,19 @@
 package nl.nn.adapterframework.filesystem;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import java.io.File;
+
 import org.junit.rules.TemporaryFolder;
 
 import nl.nn.adapterframework.receivers.DirectoryListener;
 
-public class DirectoryListenerTest extends FileSystemListenerTest {
+public class DirectoryListenerTest extends FileSystemListenerTest<File, LocalFileSystem> {
 
 	public TemporaryFolder folder;
 	
-	private final boolean testForCompatibilityOfLocalFileSystemListenerToReplaceDirectoryListener=true;
-	
 	@Override
-	public IFileSystemListener createFileSystemListener() {
-		IFileSystemListener result;
-		if (testForCompatibilityOfLocalFileSystemListenerToReplaceDirectoryListener) {
-			DirectoryListener localFileSystemListener=new DirectoryListener();
-			localFileSystemListener.setInputDirectory(folder.getRoot().getAbsolutePath());
-			result=localFileSystemListener;
-		} else {
-			DirectoryListenerWrapper directoryListener=new DirectoryListenerWrapper();
-			directoryListener.setInputDirectory(folder.getRoot().getAbsolutePath());
-			directoryListener.setWildcard("*");
-			result=directoryListener;
-		}
-		fileAndFolderPrefix=folder.getRoot().getAbsolutePath()+"/";
-		testFullErrorMessages=false;
+	public IFileSystemListener<File> createFileSystemListener() {
+		DirectoryListener result=new DirectoryListener();
+		result.setInputDirectory(folder.getRoot().getAbsolutePath());
 		return result;
 	}
 	
@@ -40,24 +27,6 @@ public class DirectoryListenerTest extends FileSystemListenerTest {
 	@Override
 	protected IFileSystemTestHelper getFileSystemTestHelper() {
 		return new LocalFileSystemTestHelper(folder);
-	}
-
-	@Test
-	@Ignore("messageType=contents not supported by original DirectoryListener")
-	public void fileListenerTestGetStringFromRawMessageContents() throws Exception {
-		super.fileListenerTestGetStringFromRawMessageContents();
-	}
-
-	@Test
-	@Ignore("create of inProcessFolder not supported by original DirectoryListener")
-	public void fileListenerTestCreateInProcessFolder() throws Exception {
-		super.fileListenerTestCreateInProcessFolder();
-	}
-
-	@Test
-	@Ignore("create of processedFolder not supported by original DirectoryListener")
-	public void fileListenerTestCreateProcessedFolder() throws Exception {
-		super.fileListenerTestCreateProcessedFolder();
 	}
 
 }
