bb0b97c17067ee399cfb7e8f473f9ea95af69b1a
Murat Kaan Meral <m.k.meral@student.tudelft.nl>
2019-10-07 10:14:14 +0200

    Move Larva Websocket to Larva package to solve cyclic dependency problem.

--- larva/src/main/java/nl/nn/adapterframework/testtool/websocket/LarvaSocket.java
+++ larva/src/main/java/nl/nn/adapterframework/testtool/websocket/LarvaSocket.java
@@ -28,7 +28,7 @@ public class LarvaSocket {
 	private final int INTERVAL = 100;
 	private static Logger logger = LogUtil.getLogger(LarvaSocket.class);
 	private Thread testRunner;
-	
+
 	/**
 	 * When new socket opens, it sends list for root directories and available scenarios.
 	 * @param session
@@ -55,11 +55,11 @@ public class LarvaSocket {
 
 		session.getBasicRemote().sendText(toReturn.toString());
 	}
-	
+
 	@OnMessage
 	public void onMessage(Session session, String strMessage) throws IOException {
 		JSONObject message;
-		
+
 		try {
 			message = new JSONObject(strMessage);
 		} catch (JSONException e) {
@@ -68,7 +68,7 @@ public class LarvaSocket {
 			return;
 		}
 		String type;
-		
+
 		try {
 			type = message.getString("type");
 		} catch (JSONException e) {
@@ -76,12 +76,12 @@ public class LarvaSocket {
 			returnError(session, e.getMessage());
 			return;
 		}
-		
+
 		// If execute request is send
 		if(type.equalsIgnoreCase("execute")) {
 			String paramExecute, currentScenariosRootDirectory;
 			int paramWaitBeforeCleanUp, numberOfThreads;
-			
+
 			// Check if required params exist
 			String[] params = {"paramExecute", "scenariosRootDirectory", "waitBeforeCleanUp", "numberOfThreads"};
 			for(String param : params) {
@@ -90,7 +90,7 @@ public class LarvaSocket {
 					return;
 				}
 			}
-			
+
 			try {
 				paramExecute = message.getString("paramExecute");
 				currentScenariosRootDirectory = message.getString("scenariosRootDirectory");
@@ -99,25 +99,25 @@ public class LarvaSocket {
 				testRunner = new Thread() {
 					String paramExecute, currentScenariosRootDirectory;
 					int paramWaitBeforeCleanUp, numberOfThreads;
-					
+
 					@Override
 					public void run() {
 						TestTool.runScenarios(paramExecute, paramWaitBeforeCleanUp, currentScenariosRootDirectory, numberOfThreads);
 					}
-					
+
 					public Thread initTestParams(String paramExecute, int paramWaitBeforeCleanUp, String currentScenariosRootDirectory, int numberOfThreads) {
 						this.paramExecute = paramExecute;
 						this.paramWaitBeforeCleanUp = paramWaitBeforeCleanUp;
 						this.currentScenariosRootDirectory = currentScenariosRootDirectory;
 						this.numberOfThreads = numberOfThreads;
-						
+
 						return this;
 					}
 				}.initTestParams(paramExecute, paramWaitBeforeCleanUp, currentScenariosRootDirectory, numberOfThreads);;
-				
+
 				// Start Tests
 				testRunner.start();
-				
+
 				// As tests are executed send the messages to frontend.
 				long timestamp = 0;
 				JSONArray messages;
@@ -126,10 +126,10 @@ public class LarvaSocket {
 					List<Message> messageList = MessageListener.getMessages(timestamp);
 					messages = new JSONArray(messageList);
 					timestamp = System.currentTimeMillis();
-					
+
 					toReturn = new JSONObject().append("messages", messages);
 					session.getAsyncRemote().sendText(toReturn.toString());
-					
+
 					try {
 						Thread.sleep(INTERVAL);
 					}catch (Exception e) {
@@ -138,7 +138,7 @@ public class LarvaSocket {
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			
+
 		// If Log Level Is Changed
 		}else if(type.equalsIgnoreCase("setLogLevel")) {
 			if(!message.has("logLevel")) {
@@ -154,39 +154,39 @@ public class LarvaSocket {
 				return;
 			}
 		}
-		
-		
+
+
 		// Receive params
 		// First send test groups (to create a table)
-		
+
 		// While tests are continuing
 		// Send test data
 	}
-	
+
 	@OnClose
 	public void onClose(Session session) throws IOException {
 		// Make sure tests are done running
 		// then clean the variables
-		
+
 		if (testRunner.isAlive()) {
 			testRunner.interrupt();
 		}
 	}
-	
+
 	@OnError
 	public void onError(Session session, Throwable throwable) {
 		logger.error("Error on session (" + session.getId() + ") \n" + throwable.getMessage());
 	}
-	
+
 	private void returnError(Session session, String errorMessage) throws IOException {
 		session.getBasicRemote().sendText("{\"error\": \"" + errorMessage + "\"}");
 	}
-	
+
 	private JSONObject getScenarioList(String rootDirectory) {
 		rootDirectory = TestPreparer.initScenariosRootDirectories(rootDirectory);
 		Map<String, List<File>> scenarios = TestPreparer.readScenarioFiles(rootDirectory, false);
 		Map<String, String> scenarioList = TestPreparer.getScenariosList(rootDirectory, scenarios, 100);
 		return new JSONObject(scenarioList);
 	}
-	
+
 }
