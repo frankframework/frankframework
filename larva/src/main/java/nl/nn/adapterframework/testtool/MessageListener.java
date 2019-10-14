888c3da9f4049ebfb4ea9620604137e71be9bc15
Murat Kaan <muratkaanmeral@gmail.com>
2019-10-14 15:33:33 +0200

    Add archive lis, and test name for messages.

--- larva/src/main/java/nl/nn/adapterframework/testtool/MessageListener.java
+++ larva/src/main/java/nl/nn/adapterframework/testtool/MessageListener.java
@@ -1,30 +1,14 @@
-/**
- * 
- */
 package nl.nn.adapterframework.testtool;
 
-import java.io.IOException;
+import nl.nn.adapterframework.util.LogUtil;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.Writer;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
-import edu.emory.mathcs.backport.java.util.Arrays;
-import net.sf.saxon.expr.flwor.SingularityPull;
-import nl.nn.adapterframework.util.LogUtil;
-import nl.nn.adapterframework.util.XmlUtils;
+import java.util.*;
 
 /**
  * @author Murat Kaan Meral
@@ -34,9 +18,10 @@ import nl.nn.adapterframework.util.XmlUtils;
  *
  */
 public class MessageListener {
-	
-	//public static final String LOG_LEVEL_ORDER = "[debug], [pipeline messages prepared for diff], [pipeline messages], [wrong pipeline messages prepared for diff], [wrong pipeline messages], [step passed/failed], [scenario passed/failed], [scenario failed], [totals], [error]";
+
+	//public synchronized static final String LOG_LEVEL_ORDER = "[debug], [pipeline messages prepared for diff], [pipeline messages], [wrong pipeline messages prepared for diff], [wrong pipeline messages], [step passed/failed], [scenario passed/failed], [scenario failed], [totals], [error]";
 	private static Logger logger = LogUtil.getLogger(TestTool.class);
+	private static LinkedList<Message> archive;
 	private static LinkedList<Message> messages  = new LinkedList<Message>();
 	private static int selectedLogLevel = 0;
 	private static final List<String> LOG_LEVEL = new ArrayList<String>() {{
@@ -46,104 +31,137 @@ public class MessageListener {
 		add("Pipeline Messages");
 		add("Wrong Pipeline Messages");
 		add("Step Passed/Failed");
+		add("Test Properties");
 		add("Scenario Passed/Failed");
 		add("Scenario Failed");
 		add("Total");
 		add("Errors");
 	}};
-	
+
 	/**
 	 * @return Returns all messages above selected log level.
 	 */
-	public static List<Message> getMessages() {
+	public synchronized static JSONArray getMessages() {
 		return getMessages(0);
 	}
-	
+
+
+	public synchronized static JSONArray getMessages(long timestamp) {
+		return getMessagesFromList(MessageListener.messages, timestamp);
+	}
+
 	/**
 	 * Returns messages after the given timestamp and above the selected log level.
+	 * @param messages the linked list of messages to be filtered. Note: the list should be sorted timestamps.
 	 * @param timestamp for filtering messages based on time.
 	 * @return list of messages that fit the criteria.
 	 */
-	public static List<Message> getMessages(long timestamp){
-		List<Message> result = new ArrayList<>();
-		
-		for(Message m : messages) {
-			if(m.timestamp >= timestamp && m.logLevel >= selectedLogLevel)
-				result.add(m);
-			
+	private synchronized static JSONArray getMessagesFromList(LinkedList<Message> messages, long timestamp){
+		if(messages == null)
+			return new JSONArray();
+
+		List<JSONObject> result = new ArrayList<>();
+		Iterator<Message> messageIterator = messages.descendingIterator();
+		while(messageIterator.hasNext()) {
+			Message m = messageIterator.next();
+			if(m.timestamp >= timestamp && m.logLevel >= selectedLogLevel) {
+				try {
+					JSONObject message = new JSONObject()
+							.put("name", m.testName)
+							.put("timestamp", m.timestamp)
+							.put("logLevel", LOG_LEVEL.get(m.logLevel))
+							.put("messages", m.message);
+					result.add(message);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+
 			// Since it is linked list, it will be ordered by their insertion time (in reverse order)
 			// So the moment message timestamp is before what we want, we can say no other message will be later than timestamp
 			if(m.timestamp < timestamp)
 				break;
 		}
-		
-		return result;
+
+		return new JSONArray(result);
 	}
-	
-	public static void debugMessage(String message) {
+
+	public synchronized static void debugMessage(String testName, String message) {
 		Map<String, String> map = new HashMap<>(1);
 		map.put("Message", message);
-		Message m = new Message(map, LOG_LEVEL.indexOf("DEBUG"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("DEBUG"), System.currentTimeMillis());
 		messages.add(m);
 		System.out.println(message);
 		logger.error(message);
 	}
 
-	public static void singleMessage(String message, String debugLevel) {
+	private synchronized static void singleMessage(String testName, String message, String debugLevel) {
 		Map<String, String> map = new HashMap<>(1);
 		map.put("Message", message);
-		Message m = new Message(map, LOG_LEVEL.indexOf(debugLevel), System.currentTimeMillis());
-		messages.add(m);		
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf(debugLevel), System.currentTimeMillis());
+		messages.add(m);
 	}
-	
-	public static void debugPipelineMessage(String stepDisplayName, String message, String pipelineMessage) {
+
+	static void testInitiationMessage(String testName, String directory) {
+		Map<String, String> map = new HashMap<>();
+		map.put("directory", directory);
+		messages.add(new Message(testName, map, LOG_LEVEL.indexOf("Test Properties"), System.currentTimeMillis()));
+	}
+
+	static void testStatusMessage(String testName, String status) {
+		Map<String, String> map = new HashMap<>();
+		map.put("status", status);
+		messages.add(new Message(testName, map, LOG_LEVEL.indexOf("Test Properties"), System.currentTimeMillis()));
+	}
+
+	public synchronized static void debugPipelineMessage(String testName, String stepDisplayName, String message, String pipelineMessage) {
 		Map<String, String> map = new HashMap<>(3);
 		map.put("Step Display Name", stepDisplayName);
 		map.put("Message", message);
 		map.put("Pipeline Message", pipelineMessage);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Pipeline Messages"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Pipeline Messages"), System.currentTimeMillis());
 		messages.add(m);
 	}
 
-	public static void debugPipelineMessagePreparedForDiff(String stepDisplayName, String message, String pipelineMessage) {
+	static void debugPipelineMessagePreparedForDiff(String testName, String stepDisplayName, String message, String pipelineMessage) {
 		Map<String, String> map = new HashMap<>(3);
 		map.put("Step Display Name", stepDisplayName);
 		map.put("Message", message);
 		map.put("Pipeline Message", pipelineMessage);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Pipeline Messages Prepared For Diff"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Pipeline Messages Prepared For Diff"), System.currentTimeMillis());
 		messages.add(m);
 	}
 
-	public static void wrongPipelineMessage(String message, String pipelineMessage) {
+	public synchronized static void wrongPipelineMessage(String testName, String message, String pipelineMessage) {
 		Map<String, String> map = new HashMap<>(2);
 		map.put("Message", message);
 		map.put("Pipeline Message", pipelineMessage);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Pipeline Messages Prepared For Diff"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Pipeline Messages Prepared For Diff"), System.currentTimeMillis());
 		messages.add(m);
 	}
 
-	public static void wrongPipelineMessage(String stepDisplayName, String message, String pipelineMessage, String pipelineMessageExpected) {
+	static void wrongPipelineMessage(String testName, String stepDisplayName, String message, String pipelineMessage, String pipelineMessageExpected) {
 		Map<String, String> map = new HashMap<>(3);
 		map.put("Step Display Name", stepDisplayName);
 		map.put("Message", message);
 		map.put("Pipeline Message", pipelineMessage);
 		map.put("pipelineMessageExpected", pipelineMessage);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Wrong Pipeline Messages"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Wrong Pipeline Messages"), System.currentTimeMillis());
 		messages.add(m);
 	}
-	
-	public static void wrongPipelineMessagePreparedForDiff(String stepDisplayName, String pipelineMessagePreparedForDiff, String pipelineMessageExpectedPreparedForDiff) {
+
+	static void wrongPipelineMessagePreparedForDiff(String testName, String stepDisplayName, String pipelineMessagePreparedForDiff, String pipelineMessageExpectedPreparedForDiff) {
 		Map<String, String> map = new HashMap<>(3);
 		map.put("Step Display Name", stepDisplayName);
 		map.put("Pipeline Message", pipelineMessagePreparedForDiff);
 		map.put("Pipeline Message Expected", pipelineMessageExpectedPreparedForDiff);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Wrong Pipeline Messages"), System.currentTimeMillis());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Wrong Pipeline Messages"), System.currentTimeMillis());
 		messages.add(m);
 	}
-		
-	private static String writeCommands(String target, boolean textArea, String customCommand) {
+
+	private synchronized static String writeCommands(String testName, String target, boolean textArea, String customCommand) {
 		String commands = "";
-		
+
 		commands += "<div class='commands'>";
 		commands += "<span class='widthCommands'><a href='javascript:void(0);' class='" + target + "|widthDown'>-</a><a href='javascript:void(0);' class='" + target + "|widthExact'>width</a><a href='javascript:void(0);' class='" + target + "|widthUp'>+</a></span>";
 		commands += "<span class='heightCommands'><a href='javascript:void(0);' class='" + target + "|heightDown'>-</a><a href='javascript:void(0);' class='" + target + "|heightExact'>height</a><a href='javascript:void(0);' class='" + target + "|heightUp'>+</a></span>";
@@ -155,76 +173,89 @@ public class MessageListener {
 			commands += " " + customCommand;
 		}
 		commands += "</div>";
-		
-		
+
+
 		return commands;
 	}
 
-	public static void stepMessage(String message) {
-		singleMessage(message, "Step Passed/Failed");
+	static void stepMessage(String testName, String message) {
+		singleMessage(testName, message , "Step Passed/Failed");
 	}
 
-	public static void scenarioMessage(String message) {
-		singleMessage(message, "Scenario Passed/Failed");
+	static void scenarioMessage(String testName, String message) {
+		singleMessage(testName, message, "Scenario Passed/Failed");
 	}
 
-	public static void scenarioFailedMessage(String message) {
-		singleMessage(message, "Scenario Failed");
+	static void scenarioFailedMessage(String testName, String message) {
+		singleMessage(testName, message, "Scenario Failed");
 	}
 
-	public static void scenariosTotalMessage(String message) {
-		singleMessage(message, "Total");
+	static void scenariosTotalMessage(String message) {
+		singleMessage("General", message, "Total");
 	}
 
-	public static void scenariosPassedTotalMessage(String message) {
-		singleMessage(message, "Total");
+	static void scenariosPassedTotalMessage(String message) {
+		singleMessage("General", message, "Total");
 	}
 
-	public static void scenariosAutosavedTotalMessage(String message) {
-		singleMessage(message, "Total");
+	static void scenariosAutosavedTotalMessage(String message) {
+		singleMessage("General", message, "Total");
 	}
 
-	public static void scenariosFailedTotalMessage(String message) {
-		singleMessage(message, "Total");
+	static void scenariosFailedTotalMessage(String message) {
+		singleMessage("General", message, "Total");
 	}
 
-	public static void errorMessage(String message) {
-		singleMessage(message, "Errors");
+	public synchronized static void errorMessage(String testName, String message) {
+		singleMessage(testName, message, "Errors");
 	}
 
-	public static void errorMessage(String message, Exception exception) {
+	public synchronized static void errorMessage(String testName, String message, Exception exception) {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("Message", message);
 
-		String stacktrace = "";
+		StringBuilder stacktrace = new StringBuilder();
 		Throwable throwable = exception;
 		while (throwable != null) {
 			StringWriter stringWriter = new StringWriter();
 			PrintWriter printWriter = new PrintWriter(stringWriter);
 			throwable.printStackTrace(printWriter);
 			printWriter.close();
-			stacktrace += stringWriter.toString();
+			stacktrace.append(stringWriter.toString());
 			throwable = throwable.getCause();
 		}
-		
-		map.put("Stack Trace", stacktrace);
-		Message m = new Message(map, LOG_LEVEL.indexOf("Errors"), System.currentTimeMillis());
+
+		map.put("Stack Trace", stacktrace.toString());
+		Message m = new Message(testName, map, LOG_LEVEL.indexOf("Errors"), System.currentTimeMillis());
 		messages.add(m);
 	}
-	
-	public static int getSelectedLogLevel() {
-		return selectedLogLevel;
+
+	public synchronized static String getSelectedLogLevel() {
+		return LOG_LEVEL.get(selectedLogLevel);
 	}
 
-	public static void setSelectedLogLevel(String selectedLogLevel) {
+	public synchronized static void setSelectedLogLevel(String selectedLogLevel) throws Exception {
+		if (!LOG_LEVEL.contains(selectedLogLevel))
+			throw new Exception("Given log level does not exist!");
 		MessageListener.selectedLogLevel = LOG_LEVEL.indexOf(selectedLogLevel);
 	}
 
-	public static List<String> getLogLevels() {
+	public synchronized static List<String> getLogLevels() {
 		return LOG_LEVEL;
 	}
-	
-	public static int getLevelOfLog(String logLevel) {
+
+	public synchronized static int getLevelOfLog(String logLevel) {
 		return LOG_LEVEL.indexOf(logLevel);
+	}
+
+	public synchronized static void cleanLogs(boolean archive) {
+		if(archive) {
+			MessageListener.archive = MessageListener.messages;
+		}
+		MessageListener.messages = new LinkedList<>();
+	}
+
+	public synchronized static JSONArray getArchive() {
+		return getMessagesFromList(MessageListener.archive, 0);
 	}
 }
