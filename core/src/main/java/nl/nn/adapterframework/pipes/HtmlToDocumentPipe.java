f639cd77fd9ee5715e44984704a08170d2df258e
Tom van der Heijden <tomavanderheijden@hotmail.com>
2019-11-20 11:23:47 +0100

    Removed unused imports

--- core/src/main/java/nl/nn/adapterframework/pipes/HtmlToDocumentPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/HtmlToDocumentPipe.java
@@ -20,8 +20,6 @@ import nl.nn.adapterframework.doc.IbisDoc;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
