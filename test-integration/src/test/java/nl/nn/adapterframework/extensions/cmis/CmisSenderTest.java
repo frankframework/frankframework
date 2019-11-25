407476db610bf33d4b62218654e165e7a7a5a72e
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2019-11-25 10:30:35 +0100

    Pending workspace commits

---
+++ test-integration/src/test/java/nl/nn/adapterframework/extensions/cmis/CmisSenderTest.java
@@ -0,0 +1,238 @@
+package nl.nn.adapterframework.extensions.cmis;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import nl.nn.adapterframework.core.PipeLineSessionBase;
+import nl.nn.adapterframework.http.HttpSender;
+import nl.nn.adapterframework.parameters.ParameterResolutionContext;
+import nl.nn.adapterframework.util.ClassUtils;
+
+public class CmisSenderTest {
+
+	String url;
+	String repo;
+	String titanUser;
+	String titanPassword;
+	
+	String id1;
+	String id2;
+	
+	int length1=0;
+	int length2=0;
+	
+	String testProperties="CmisSender.properties";
+	Properties properties;
+
+	int numParallel=5; // not used for ramp up test
+	
+	int numCycles=10;
+	int maxConnections=20;
+	int numSenders=2;
+	
+	boolean testViaHttpSender=false;
+	
+	int threadCount[] = {  1,  5, 10, 15, 20, 25, 30 };
+	
+	private StringBuffer results=new StringBuffer();
+	private String separator="\t";
+
+	
+	
+	private CmisSender cmisSenders[];
+	private HttpSender httpSenders[];
+	
+	@Before
+	public void setUp() throws Exception {
+		properties=new Properties();
+		properties.load(ClassUtils.getResourceURL(this, testProperties).openStream());
+		url=properties.getProperty("url");
+		repo=properties.getProperty("repo");
+		titanUser=properties.getProperty("titanUser");
+		titanPassword=properties.getProperty("titanPassword");
+		id1=properties.getProperty("id.1");
+		id2=properties.getProperty("id.2");
+		length1=Integer.parseInt(properties.getProperty("length.1"));
+		length2=Integer.parseInt(properties.getProperty("length.2"));
+		if (testViaHttpSender) {
+			httpSenders = new HttpSender[numSenders];
+			for (int i=0; i<numSenders; i++) {
+				httpSenders[i]=createHttpSender(i);
+			}
+		} else {
+			cmisSenders = new CmisSender[numSenders];
+			for (int i=0; i<numSenders; i++) {
+				cmisSenders[i]=createCmisSender(i);
+			}
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		// nothing
+	}
+	
+	public CmisSender createCmisSender(int i) throws Exception {
+		CmisSender sender = new CmisSender();
+		sender.setName("CmisSender "+i);
+		sender.setUrl(url);
+		sender.setRepository(repo);
+		sender.setAction("get");
+		sender.setBindingType("browser");
+		sender.setUsername(titanUser);
+		sender.setPassword(titanPassword);
+		sender.setMaxConnections(maxConnections);
+		sender.configure();
+		sender.open();
+		return sender;
+	}
+
+	public HttpSender createHttpSender(int i) throws Exception {
+		HttpSender sender = new HttpSender();
+		sender.setName("HttpSender "+i);
+		String fullUrl=url+"/"+repo+"/root?objectId="+id1+"&cmisselector=content";
+		System.out.println("url: "+fullUrl);
+		sender.setUrl(fullUrl);
+		sender.setMethodType("GET");
+//		sender.setRepository(repo);
+//		sender.setAction("get");
+//		sender.setBindingType("browser");
+		sender.setUserName(titanUser);
+		sender.setPassword(titanPassword);
+		sender.setMaxConnections(maxConnections);
+		sender.configure();
+		sender.open();
+		return sender;
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		testGet(0);
+	}
+	
+	public void testGet(int i) throws Exception {
+		String id=id1;
+		int expectedLength=length1;
+		String result;
+		PipeLineSessionBase session = new PipeLineSessionBase();
+	
+		int index=i % numSenders;
+		
+		if (testViaHttpSender) {
+			String message=""; 
+
+			ParameterResolutionContext prc= new ParameterResolutionContext(message, session);
+			result=httpSenders[index].sendMessage(null, message,prc);
+			
+		} else {
+			String message="<cmis><id>"+id+"</id></cmis>";
+			ParameterResolutionContext prc= new ParameterResolutionContext(message, session);
+			result=cmisSenders[index].sendMessage(null, message,prc);
+		}
+		
+		assertNotNull(result);
+//		assertEquals(expectedLength,result.length());
+	}
+
+	@Test
+	public void testGetMultipleTimes() throws Exception {
+	
+		long t0=System.currentTimeMillis();
+		for (int i=0;i<numCycles;i++) {
+			testGet();
+		}
+		long t1=System.currentTimeMillis();
+		long total=t1-t0;
+		long average=total/numCycles;
+		System.out.println("numCycles ["+numCycles+"] total ["+total+"] average ["+average+"]");
+	}
+
+    public void testParallel(int numParallel) throws Exception {
+    	System.out.println("start testing with ["+numParallel+"] threads...");
+		long t0=System.currentTimeMillis();
+    	ArrayList<CmisSenderTester> threads = new ArrayList<CmisSenderTester>();
+		for (int i=0;i<numParallel;i++) {
+			threads.add(new CmisSenderTester());
+		}
+		for (int i=0;i<numParallel;i++) {
+			threads.get(i).start();
+		}
+		System.out.println("waiting for threads to end");
+		for (int i=0;i<numParallel;i++) {
+			threads.get(i).join();
+			System.out.println("joind thread "+i);
+		}
+		long t1=System.currentTimeMillis();
+		long totalTimeTesting=t1-t0;
+		long totalTime=0;
+		int  totalCycles=0;
+		for (int i=0;i<numParallel;i++) {
+			long total=threads.get(i).total;
+			int cycles=threads.get(i).cycles;
+			totalTime+=total;
+			totalCycles+=cycles;
+			long average=total/cycles;
+			System.out.println("thread ["+i+"] cycles ["+cycles+"] time spent ["+total+"] average ["+average+"]");
+		}
+		long totalBytes=totalCycles*length1;
+		System.out.println("message size ["+length1+"] total bytes ["+totalBytes+"]");
+		System.out.println("average for each of ["+numParallel+"] threads:  numCycles ["+numCycles+"] totalCycles ["+totalCycles+"] average for each message ["+(totalTime/totalCycles)+"] [ms/#] average thread speed ["+(totalBytes/totalTime)+"] [KB/s]");
+		System.out.println("total time spent testing  ["+totalTimeTesting+"] numCycles ["+numCycles+"] totalCycles ["+totalCycles+"] average ["+(totalTimeTesting/totalCycles)+"] overall througput ["+(totalBytes/totalTimeTesting)+"] [KB/s]");
+		String result=length1+separator+numParallel+separator+totalCycles+separator+totalTime+separator+totalTimeTesting;
+		System.out.println(result);
+		results.append(result).append("\n");
+		
+    }
+
+    @Test
+    public void testParallel() throws Exception {
+    	System.out.println("warming up...");
+    	testGet();
+    	testGet();
+    	testGet();
+    	testParallel(numParallel);
+    }
+    
+    @Test
+    public void testRampUp() throws Exception {
+    	System.out.println("warming up...");
+    	testGet();
+    	testGet();
+    	testGet();
+    	results.append("msgsize"+separator+"#threads"+separator+"totcycles"+separator+"totalTime"+separator+"totalTimeTesting\n");
+    	for (int i=0; i<threadCount.length; i++) {
+        	testParallel(threadCount[i]);
+    	}
+    	System.out.println(results);
+    }
+    
+
+    private class CmisSenderTester extends Thread {
+
+		long total;
+		int cycles=0;
+    	
+    	@Override
+		public void run() {
+    		long t0=System.currentTimeMillis();
+			try {
+				for (int i=0;i<numCycles;i++) {
+					cycles++;
+					testGet();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			long t1=System.currentTimeMillis();
+			total=t1-t0;
+		}
+   	
+    }
+    
+}
---
+++ test-integration/src/test/java/nl/nn/adapterframework/pipes/EsbSoapWrapperPipeTest.java
@@ -0,0 +1,209 @@
+package nl.nn.adapterframework.pipes;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import nl.nn.adapterframework.core.PipeLineSessionBase;
+import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.extensions.esb.EsbSoapWrapperPipe;
+import nl.nn.adapterframework.parameters.Parameter;
+import nl.nn.adapterframework.util.TestAssertions;
+import nl.nn.adapterframework.util.XmlUtils;
+
+public class EsbSoapWrapperPipeTest extends SoapWrapperPipeTest<EsbSoapWrapperPipe> {
+
+	@Override
+	public EsbSoapWrapperPipe createPipe() {
+		EsbSoapWrapperPipe pipe = new EsbSoapWrapperPipe();
+		return pipe;
+	}
+
+	
+	public void addParam(String name, String value) {
+		Parameter param = new Parameter();
+		param.setName(name);
+		param.setValue(value);
+		pipe.addParameter(param);
+	}
+	
+	
+	@Test
+	public void testWrapCMH2() throws Exception {
+		pipe.setOutputNamespace("http://nn.nl/XSD/Archiving/Document/3/GetDocumentAndAttributes/1");
+		pipe.setCmhVersion(2);
+		addParam("destination","destination-value");
+		addParam("errorCode","errorCode-value");
+		addParam("errorReason","errorReason-value");
+		addParam("errorDetailCode","errorDetailCode-value");
+		addParam("errorDetailText","errorDetailText-value");
+		addParam("operation","operation-value");
+		addParam("operationVersion","operationVersion-value");
+		pipe.setNamespaceAware(true);
+		pipe.configure();
+		pipe.start();
+		
+		String input = "<GetDocumentAndAttributes_Response><attrib>1</attrib><attrib>2</attrib></GetDocumentAndAttributes_Response>";
+		
+		PipeRunResult prr = pipe.doPipe(input,new PipeLineSessionBase());
+		
+		String result = (String)prr.getResult();
+		System.out.println("result ["+result+"]");
+		TestAssertions.assertXpathValueEquals("test", 					result, "/Envelope/Header/MessageHeader/From/Id");
+//		TestAssertions.assertXpathValueEquals("test", 					result, "/Envelope/Header/MessageHeader/Service/Name");
+//		TestAssertions.assertXpathValueEquals("operationVersion-value", result, "**/Envelope/Header/MessageHeader/Service/Version");
+		TestAssertions.assertXpathValueEquals("OK", 					result, "/Envelope/Body/MessageHeader/Result/Status");
+	}
+
+	@Test
+	public void testWrapFindDocumentsOK() throws Exception {
+		
+		String outputNamespace="http://nn.nl/XSD/Archiving/Document/3/GetDocumentAndAttributes/1";
+		String rootElement="GetDocumentAndAttributes_Response";
+		
+		String messagingLayer="ESB";
+		String businessDomain="Archiving";
+		String applicationName="BS";
+		String serviceName="Document";
+		String serviceVersion="3";
+		String operation="FindDocuments";
+		String operationVersion="1";
+		String paradigm="Request";
+		
+		String errorCode=null;
+		String errorReason=null;
+		String errorDetailCode=null;
+		String errorDetailText=null;
+		
+		
+		String destination=messagingLayer+"."+businessDomain+"."+applicationName+"."+serviceName+"."+serviceVersion+"."+operation+"."+operationVersion+"."+paradigm;
+		
+		pipe.setOutputNamespace(outputNamespace);
+		pipe.setCmhVersion(2);
+		addParam("destination",destination);
+		addParam("errorCode",errorCode);
+		addParam("errorReason",errorReason);
+		addParam("errorDetailCode",errorDetailCode);
+		addParam("errorDetailText",errorDetailText);
+		addParam("operation",null);
+		addParam("operationVersion",null);
+		pipe.setNamespaceAware(true);
+		pipe.configure();
+		pipe.start();
+		
+		String input = "<"+rootElement+"><attrib>1</attrib><attrib>2</attrib></"+rootElement+">";
+		
+		PipeRunResult prr = pipe.doPipe(input,new PipeLineSessionBase());
+		
+		String result = (String)prr.getResult();
+		System.out.println("result ["+result+"]");
+		TestAssertions.assertXpathValueEquals("test", 			result, "/Envelope/Header/MessageHeader/From/Id");
+		TestAssertions.assertXpathValueEquals(destination, 		result, "/Envelope/Header/MessageHeader/To/Location");
+		TestAssertions.assertXpathValueEquals(serviceName, 		result, "/Envelope/Header/MessageHeader/Service/Name");
+		TestAssertions.assertXpathValueEquals(paradigm, 		result, "/Envelope/Header/MessageHeader/Service/Action/Paradigm");
+		TestAssertions.assertXpathValueEquals(operation, 		result, "/Envelope/Header/MessageHeader/Service/Action/Name");
+		TestAssertions.assertXpathValueEquals(operationVersion, result, "/Envelope/Header/MessageHeader/Service/Action/Version");
+
+		TestAssertions.assertXpathValueEquals("ERROR", 			result, "/Envelope/Body/"+rootElement+"/Result/Status");
+		TestAssertions.assertXpathValueEquals(errorCode, 		result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/Code");
+		TestAssertions.assertXpathValueEquals(errorReason, 		result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/Reason");
+		TestAssertions.assertXpathValueEquals(errorDetailCode, 	result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/DetailList/Detail/Code");
+		TestAssertions.assertXpathValueEquals(XmlUtils.encodeChars(errorDetailText), 	result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/DetailList/Detail/Text");
+	}
+
+	@Test
+	public void testWrapFindDocumentsWithError() throws Exception {
+		
+		String outputNamespace="http://nn.nl/XSD/Archiving/Document/3/GetDocumentAndAttributes/1";
+		String rootElement="GetDocumentAndAttributes_Response";
+		
+		String messagingLayer="ESB";
+		String businessDomain="Archiving";
+		String applicationName="BS";
+		String serviceName="Document";
+		String serviceVersion="3";
+		String operation="FindDocuments";
+		String operationVersion="1";
+		String paradigm="Request";
+		
+		String errorCode="errorCode-value";
+		String errorReason="errorReason-value";
+		String errorDetailCode="errorDetailCode-value";
+		String errorDetailText="<reasons>errorDetailText-value</reasons>";
+		
+		
+		String destination=messagingLayer+"."+businessDomain+"."+applicationName+"."+serviceName+"."+serviceVersion+"."+operation+"."+operationVersion+"."+paradigm;
+		
+		pipe.setOutputNamespace(outputNamespace);
+		pipe.setCmhVersion(2);
+		addParam("destination",destination);
+		addParam("errorCode",errorCode);
+		addParam("errorReason",errorReason);
+		addParam("errorDetailCode",errorDetailCode);
+		addParam("errorDetailText",errorDetailText);
+		addParam("operation",operation);
+		addParam("operationVersion",operationVersion);
+		pipe.setNamespaceAware(true);
+		pipe.configure();
+		pipe.start();
+		
+		String input = "<"+rootElement+"><attrib>1</attrib><attrib>2</attrib></"+rootElement+">";
+		
+		PipeRunResult prr = pipe.doPipe(input,new PipeLineSessionBase());
+		
+		String result = (String)prr.getResult();
+		System.out.println("result ["+result+"]");
+		TestAssertions.assertXpathValueEquals("test", 			result, "/Envelope/Header/MessageHeader/From/Id");
+		TestAssertions.assertXpathValueEquals(destination, 		result, "/Envelope/Header/MessageHeader/To/Location");
+		TestAssertions.assertXpathValueEquals(serviceName, 		result, "/Envelope/Header/MessageHeader/Service/Name");
+		TestAssertions.assertXpathValueEquals(paradigm, 		result, "/Envelope/Header/MessageHeader/Service/Action/Paradigm");
+		TestAssertions.assertXpathValueEquals(operation, 		result, "/Envelope/Header/MessageHeader/Service/Action/Name");
+		TestAssertions.assertXpathValueEquals(operationVersion, result, "/Envelope/Header/MessageHeader/Service/Action/Version");
+
+		TestAssertions.assertXpathValueEquals("ERROR", 			result, "/Envelope/Body/"+rootElement+"/Result/Status");
+		TestAssertions.assertXpathValueEquals(errorCode, 		result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/Code");
+		TestAssertions.assertXpathValueEquals(errorReason, 		result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/Reason");
+		TestAssertions.assertXpathValueEquals(errorDetailCode, 	result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/DetailList/Detail/Code");
+		TestAssertions.assertXpathValueEquals(XmlUtils.encodeChars(errorDetailText), 	result, "/Envelope/Body/"+rootElement+"/Result/ErrorList/Error/DetailList/Detail/Text");
+	}
+
+	@Test
+	public void testWithError() throws Exception {
+		pipe.setOutputNamespace("http://nn.nl/XSD/Archiving/Document/3/GetDocumentAndAttributes/1");
+		pipe.setCmhVersion(2);
+		addParam("destination","destination-value");
+		addParam("errorCode","errorCode-value");
+		addParam("errorReason","errorReason-value");
+		addParam("errorDetailCode","errorDetailCode-value");
+		addParam("errorDetailText","errorDetailText-value");
+		addParam("operation","operation-value");
+		addParam("operationVersion","operationVersion-value");
+		pipe.configure();
+		pipe.start();
+		
+		String input = "<GetDocumentAndAttributes_Response><attrib>1</attrib><attrib>2</attrib></GetDocumentAndAttributes_Response>";
+		
+		PipeRunResult prr = pipe.doPipe(input,new PipeLineSessionBase());
+		
+		String result = (String)prr.getResult();
+		System.out.println("result ["+result+"]");
+	}
+
+
+//	@Override
+//	@Test
+//	@Ignore("Must incorporate CMH in test")
+//	public void testBasicUnWrap() throws Exception {
+//		// TODO Auto-generated method stub
+//		super.testBasicWrap();
+//	}
+//
+//
+//	@Override
+//	@Test
+//	@Ignore("Must incorporate CMH in test")
+//	public void testBasicWrapChangeRoot() throws Exception {
+//		// TODO Auto-generated method stub
+//		super.testBasicWrapChangeRoot();
+//	}
+//	
+}
