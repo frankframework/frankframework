c2794d762258c652b9c284f0b3eb8544bf18dc17
Niels Meijer <nielsmeijer@hotmail.com>
2019-12-13 15:38:50 +0100

    Add clipboard directive

--- webapp/src/main/webapp/iaf/gui/js/directives.js
+++ webapp/src/main/webapp/iaf/gui/js/directives.js
@@ -36,6 +36,33 @@ angular.module('iaf.beheerconsole')
 	};
 }])
 
+.directive('clipboard', function() {
+	return {
+		restrict: 'A',
+		controller: function ($scope, $element, $compile) {
+			var selector = angular.element('<i ng-click="copyToClipboard()" title="copy to clipboard" class="fa fa-clipboard" aria-hidden="true"></i>');
+			$element.append(selector);
+			$element.addClass("clipboard");
+			$compile(selector)($scope);
+
+			$scope.copyToClipboard = function () {
+				var textToCopy = $element.text().trim();
+				if(textToCopy) {
+					var el = document.createElement('textarea');
+					el.value = textToCopy;
+					el.setAttribute('readonly', '');
+					el.style.position = 'absolute';
+					el.style.left = '-9999px';
+					document.body.appendChild(el);
+					el.select();
+					document.execCommand('copy');
+					document.body.removeChild(el);
+				}
+			};
+		}
+	};
+})
+
 .directive('timeSince', ['appConstants', '$interval', function(appConstants, $interval) {
 	return {
 		restrict: 'A',
