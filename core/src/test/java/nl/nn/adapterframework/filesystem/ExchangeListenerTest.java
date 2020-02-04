d6b7180402d0531295b985fc486d8fd544f151db
Niels Meijer <nielsmeijer@hotmail.com>
2020-02-04 17:23:27 +0100

    Remove old dependencies Add tests for the ExchangeListener

---
+++ core/src/test/java/nl/nn/adapterframework/filesystem/ExchangeListenerTest.java
@@ -0,0 +1,53 @@
+package nl.nn.adapterframework.filesystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.stream.XMLEventReader;
+
+import org.junit.Test;
+
+import microsoft.exchange.webservices.data.core.EwsServiceMultiResponseXmlReader;
+import microsoft.exchange.webservices.data.core.EwsXmlReader;
+import microsoft.exchange.webservices.data.security.XmlNodeType;
+
+public class ExchangeListenerTest {
+	final String validDocument   = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><test>testContent</test>";
+	final String invalidDocument = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><test>test&#x3;Content</test>";
+
+	@Test
+	public void getProperXMLEventReader() throws Exception {
+		InputStream input = new ByteArrayInputStream(validDocument.getBytes("UTF-8"));
+		EwsServiceMultiResponseXmlReader impl = EwsServiceMultiResponseXmlReader.create(input, null);
+		impl.read(new XmlNodeType(XmlNodeType.START_DOCUMENT));
+		impl.read(new XmlNodeType(XmlNodeType.START_ELEMENT));
+		XMLEventReader ewsXmlReader = impl.getXmlReaderForNode();
+		assertTrue(ewsXmlReader instanceof XMLEventReader);
+		impl.read(new XmlNodeType(XmlNodeType.END_DOCUMENT));
+	}
+
+	@Test
+	public void testReadValidDocument() throws Exception {
+		byte[] bytes = validDocument.getBytes("UTF-8");
+		EwsXmlReader impl = new EwsXmlReader(new ByteArrayInputStream(bytes));
+		impl.read(new XmlNodeType(XmlNodeType.START_DOCUMENT));
+		impl.read(new XmlNodeType(XmlNodeType.START_ELEMENT));
+		String content = impl.readValue();
+		assertEquals(content, "testContent");
+		impl.read(new XmlNodeType(XmlNodeType.END_DOCUMENT));
+	}
+
+	@Test
+	public void testReadInvalidDocument() throws Exception {
+		byte[] bytes = invalidDocument.getBytes("UTF-8");
+		EwsXmlReader impl = new EwsXmlReader(new ByteArrayInputStream(bytes));
+		impl.read(new XmlNodeType(XmlNodeType.START_DOCUMENT));
+		impl.read(new XmlNodeType(XmlNodeType.START_ELEMENT));
+		String content = impl.readValue();
+		assertEquals(content, "test\u0005Content");
+		impl.read(new XmlNodeType(XmlNodeType.END_DOCUMENT));
+	}
+}
