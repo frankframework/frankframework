6cc093e2992348af4f3eb840f08f9f70b2b0874a
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2020-02-05 16:04:37 +0100

    Revert Zephyr removal, appears to be required for WsdlUtil

--- core/src/main/java/nl/nn/adapterframework/util/XmlUtils.java
+++ core/src/main/java/nl/nn/adapterframework/util/XmlUtils.java
@@ -170,10 +170,10 @@ public class XmlUtils {
 		// Tomcat the sjsxp was used by newFactory while on WAS 8.5 with parent
 		// last woodstox was used (giving "Response already committed" error
 		// when a WSDL was generated).
-		EVENT_FACTORY = XMLEventFactory.newInstance();
-		INPUT_FACTORY = XMLInputFactory.newInstance();
-		OUTPUT_FACTORY = XMLOutputFactory.newInstance();
-		REPAIR_NAMESPACES_OUTPUT_FACTORY = XMLOutputFactory.newInstance();
+		EVENT_FACTORY = new com.sun.xml.stream.events.ZephyrEventFactory();
+		INPUT_FACTORY = new com.sun.xml.stream.ZephyrParserFactory();
+		OUTPUT_FACTORY = new com.sun.xml.stream.ZephyrWriterFactory();
+		REPAIR_NAMESPACES_OUTPUT_FACTORY = new com.sun.xml.stream.ZephyrWriterFactory();
 		REPAIR_NAMESPACES_OUTPUT_FACTORY.setProperty(XMLOutputFactory.IS_REPAIRING_NAMESPACES, Boolean.TRUE);
 	}
 
