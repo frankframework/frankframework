6d515c52c9fa0e3c59b6c06e81b8d8655c76c3a9
Murat Kaan <muratkaanmeral@gmail.com>
2020-03-06 16:41:27 +0100

    Extend layouts for masking and length limit.

---
+++ core/src/main/java/nl/nn/adapterframework/extensions/log4j/IbisMaskingLayout.java
@@ -0,0 +1,105 @@
+package nl.nn.adapterframework.extensions.log4j;
+
+import nl.nn.adapterframework.util.Misc;
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.layout.AbstractStringLayout;
+
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is a wrapper for Log4j2 layouts.
+ * It enables us to:
+ * limit log message length,
+ * apply global masking,
+ * and apply local (thread-wise) masking.
+ *
+ * @author Murat Kaan Meral
+ */
+public abstract class IbisMaskingLayout extends AbstractStringLayout {
+	private static int maxLength = -1;
+	private static String moreMessage = " ...(%d more characters)";
+	private static ThreadLocal<Set<String>> threadLocalReplace = new ThreadLocal<>();
+	private static Set<String> globalReplace = new HashSet<>();
+
+	protected AbstractStringLayout layout;
+
+	protected IbisMaskingLayout(Charset charset) {
+		super(charset);
+	}
+
+	@Override
+	public String toSerializable(LogEvent event) {
+		String result = layout.toSerializable(event);
+		if(StringUtils.isNotEmpty(result)) {
+			result = Misc.hideAll(result, globalReplace);
+			result = Misc.hideAll(result, threadLocalReplace.get());
+		}
+		int length = result.length();
+		if (maxLength > 0 && length > maxLength) {
+			int diff = length - maxLength;
+			result = result.substring(0, maxLength) + " " + String.format(moreMessage, diff) + "\r\n";
+			System.out.println("Difference is " + (diff) + " with maxlength " + maxLength);
+		}
+		return result;
+	}
+
+	public static void setMaxLength (int maxLength) {
+		IbisMaskingLayout.maxLength = maxLength;
+	}
+
+	public static int getMaxLength () {
+		return maxLength;
+	}
+
+	public static String getMoreMessageString() {
+		return moreMessage;
+	}
+
+	public static void setMoreMessageString(String moreMessage) {
+		IbisMaskingLayout.moreMessage = moreMessage;
+	}
+
+	public static void addToGlobalReplace (String regex) {
+		globalReplace.add(regex);
+	}
+
+	public static void removeFromGlobalReplace (String regex) {
+		globalReplace.remove(regex);
+	}
+
+	public static Set<String> getGlobalReplace () {
+		return globalReplace;
+	}
+
+	public static void cleanGlobalReplace () {
+		globalReplace = new HashSet<>();
+	}
+
+	public static void addToThreadLocalReplace (Collection<String> collection) {
+		if(threadLocalReplace.get() == null)
+			cleanThreadLocalReplace();
+		threadLocalReplace.get().addAll(collection);
+	}
+
+	public static void addToThreadLocalReplace (String regex) {
+		if(threadLocalReplace.get() == null)
+			cleanThreadLocalReplace();
+		threadLocalReplace.get().add(regex);
+	}
+
+	public static void removeFromThreadLocalReplace (String regex) {
+		threadLocalReplace.get().remove(regex);
+	}
+
+	public static Set<String> getThreadLocalReplace () {
+		return threadLocalReplace.get();
+	}
+
+	public static void cleanThreadLocalReplace () {
+		threadLocalReplace.set(new HashSet<>());
+	}
+}
---
+++ core/src/main/java/nl/nn/adapterframework/extensions/log4j/IbisPatternLayout.java
@@ -0,0 +1,51 @@
+package nl.nn.adapterframework.extensions.log4j;
+
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginConfiguration;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.apache.logging.log4j.core.layout.PatternSelector;
+import org.apache.logging.log4j.core.pattern.RegexReplacement;
+
+import java.nio.charset.Charset;
+
+@Plugin(name = "IbisPatternLayout", category = "Core", elementType = "layout", printObject = true)
+public class IbisPatternLayout extends IbisMaskingLayout {
+
+	IbisPatternLayout(final Configuration config, final RegexReplacement replace, final String eventPattern,
+					  final PatternSelector patternSelector, final Charset charset, final boolean alwaysWriteExceptions,
+					  final boolean disableAnsi, final boolean noConsoleNoAnsi, final String headerPattern,
+					  final String footerPattern) {
+		super(charset);
+		layout = PatternLayout.newBuilder()
+				.withConfiguration(config)
+				.withPattern(eventPattern)
+				.withRegexReplacement(replace)
+				.withPatternSelector(patternSelector)
+				.withCharset(charset)
+				.withAlwaysWriteExceptions(alwaysWriteExceptions)
+				.withDisableAnsi(disableAnsi)
+				.withNoConsoleNoAnsi(noConsoleNoAnsi)
+				.withHeader(headerPattern)
+				.withFooter(footerPattern)
+				.build();
+	}
+
+	@PluginFactory
+	public static IbisPatternLayout createLayout(
+			@PluginAttribute(value = "pattern", defaultString = PatternLayout.DEFAULT_CONVERSION_PATTERN) final String pattern,
+			@PluginElement("PatternSelector") final PatternSelector patternSelector,
+			@PluginConfiguration final Configuration config,
+			@PluginElement("Replace") final RegexReplacement replace,
+			// LOG4J2-783 use platform default by default, so do not specify defaultString for charset
+			@PluginAttribute(value = "charset") final Charset charset,
+			@PluginAttribute(value = "alwaysWriteExceptions", defaultBoolean = true) final boolean alwaysWriteExceptions,
+			@PluginAttribute(value = "noConsoleNoAnsi") final boolean noConsoleNoAnsi,
+			@PluginAttribute("header") final String headerPattern,
+			@PluginAttribute("footer") final String footerPattern) {
+		return new IbisPatternLayout(config, replace, pattern, patternSelector, charset, alwaysWriteExceptions, false, noConsoleNoAnsi, headerPattern, footerPattern);
+	}
+}
