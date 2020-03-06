ac1a78e3aa2eb1ff96553e5958f283a140fbd9d8
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2020-03-06 14:40:06 +0100

    Improve validation of XML when converting to JSON

--- core/src/main/java/nl/nn/adapterframework/pipes/Json2XmlValidator.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/Json2XmlValidator.java
@@ -167,9 +167,8 @@ public class Json2XmlValidator extends XmlValidator {
 		ValidationContext context = validator.createValidationContext(session, getRootValidations(responseMode), getInvalidRootNamespaces());
 		ValidatorHandler validatorHandler = validator.getValidatorHandler(session,context);
 		// Make sure to use Xerces' ValidatorHandlerImpl, otherwise casting below will fail.
-		XmlAligner aligner = new XmlAligner((PSVIProvider) validatorHandler);
+		XmlAligner aligner = new XmlAligner(validatorHandler);
 		Xml2Json xml2json = new Xml2Json(aligner, isCompactJsonArrays(), !isJsonWithRootElements());
-		validatorHandler.setContentHandler(aligner);
 		aligner.setContentHandler(xml2json);
 		aligner.setErrorHandler(context.getErrorHandler());
 		
---
+++ core/src/test/java/nl/nn/adapterframework/pipes/Json2XmlValidatorTest.java
@@ -0,0 +1,92 @@
+package nl.nn.adapterframework.pipes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.hamcrest.core.StringContains;
+import org.junit.Test;
+
+import nl.nn.adapterframework.core.PipeForward;
+import nl.nn.adapterframework.core.PipeLineSessionBase;
+import nl.nn.adapterframework.core.PipeRunException;
+import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.testutil.TestFileUtils;
+
+public class Json2XmlValidatorTest {
+
+	@Test
+	public void testNoNamespaceXml2Json() throws Exception {
+		Json2XmlValidator validator = new Json2XmlValidator();
+		
+		validator.setName("Response_To_Json");
+		validator.setOutputFormat("json");
+		validator.setSchema("/Align/NoNamespace/bp.xsd");
+//		validator.setRoot("GetPartiesOnAgreement_Response");
+//		validator.setTargetNamespace("http://nn.nl/XSD/CustomerAdministration/Party/1/GetPartiesOnAgreement/7");
+		validator.setThrowException(true);
+		validator.registerForward(new PipeForward("success",null));
+		validator.configure();
+		validator.start();
+		
+		String input = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response.xml");
+		String expected = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response-compact.json");
+		
+		PipeLineSessionBase session = new PipeLineSessionBase();
+		try {
+			PipeRunResult prr = validator.doPipe(input,session);
+			fail("expected to fail");
+		} catch (PipeRunException e) {
+			assertThat(e.getMessage(),StringContains.containsString("Cannot find the declaration of element 'BusinessPartner'"));
+		}
+	}
+
+	@Test
+	public void testNoNamespaceXml() throws Exception {
+		XmlValidator validator = new XmlValidator();
+		
+		validator.setName("Response_Validator");
+		validator.setSchema("/Align/NoNamespace/bp.xsd");
+//		validator.setRoot("GetPartiesOnAgreement_Response");
+//		validator.setTargetNamespace("http://nn.nl/XSD/CustomerAdministration/Party/1/GetPartiesOnAgreement/7");
+		validator.setThrowException(true);
+		validator.registerForward(new PipeForward("success",null));
+		validator.configure();
+		validator.start();
+		
+		String input = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response.xml");
+		String expected = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response-compact.json");
+		
+		PipeLineSessionBase session = new PipeLineSessionBase();
+		try {
+			PipeRunResult prr = validator.doPipe(input,session);
+			fail("expected to fail");
+		} catch (PipeRunException e) {
+			assertThat(e.getMessage(),StringContains.containsString("Cannot find the declaration of element 'BusinessPartner'"));
+		}
+	}
+
+
+	@Test
+	public void testWithNamespace() throws Exception {
+		Json2XmlValidator validator = new Json2XmlValidator();
+		
+		validator.setName("Response_To_Json");
+		validator.setOutputFormat("json");
+		validator.setSchema("/Align/NoNamespace/bp.xsd");
+//		validator.setRoot("GetPartiesOnAgreement_Response");
+//		validator.setTargetNamespace("http://nn.nl/XSD/CustomerAdministration/Party/1/GetPartiesOnAgreement/7");
+		validator.setThrowException(true);
+		validator.registerForward(new PipeForward("success",null));
+		validator.configure();
+		validator.start();
+		
+		String input = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response-withNamespace.xml");
+		String expected = TestFileUtils.getTestFile("/Align/NoNamespace/bp-response-compact.json");
+		
+		PipeLineSessionBase session = new PipeLineSessionBase();
+		PipeRunResult prr = validator.doPipe(input,session);
+		
+		assertEquals(expected, prr.getResult());
+	}
+}
