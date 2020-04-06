61501882e83a99bf87d4c2d03ded9886bdee4b66
ccamiletti@gmail.com <ccamiletti@gmail.com>
2020-04-06 08:43:49 +0200

    removing resteasy providers files

--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/AuthorizationFilter.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/AuthorizationFilter.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2016-2017 Integration Partners B.V.
+Copyright 2020-2021 WeAreFrank - Information Technology & Services.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,66 +15,95 @@ limitations under the License.
 */
 package nl.nn.adapterframework.webcontrol.api;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.security.DenyAll;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.cxf.jaxrs.impl.ResourceInfoImpl;
+import org.apache.log4j.Logger;
+
+import nl.nn.adapterframework.util.LogUtil;
+
 /**
- * Manages authorization per resource/collection.
+ * Manages authorization per resource/collection. / it was based on nl.nn.adapterframework.webcontrol.api.AuthorizationFilter
  * 
  * @since	7.0-B1
- * @author	Niels Meijer
+ * @author	Carlo Camiletti
  */
 
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
-public class AuthorizationFilter { //implements ContainerRequestFilter {
+public class AuthorizationFilter implements ContainerRequestFilter {
 
-	/*
-	 * private static final ServerResponse ACCESS_FORBIDDEN = new
-	 * ServerResponse(null, 403, new Headers<Object>()); private static final
-	 * ServerResponse SERVER_ERROR = new ServerResponse(null, 500, new
-	 * Headers<Object>()); protected Logger log = LogUtil.getLogger(this);
-	 * 
-	 * public void filter(ContainerRequestContext requestContext) throws IOException
-	 * { if(requestContext.getMethod().equalsIgnoreCase("OPTIONS")) { //Preflight in
-	 * here? return; }
-	 * 
-	 * SecurityContext securityContext = requestContext.getSecurityContext();
-	 * 
-	 * if(securityContext.getUserPrincipal() == null) { //Not logged in. Auth
-	 * restriction should be done via web.xml, if no userPrincipal is set it uses
-	 * anonymous login return; }
-	 * 
-	 * ResourceMethodInvoker methodInvoker = (ResourceMethodInvoker)
-	 * requestContext.getProperty("org.jboss.resteasy.core.ResourceMethodInvoker");
-	 * if(methodInvoker == null) {
-	 * log.error("Unable to fetch method from ResourceMethodInvoker");
-	 * requestContext.abortWith(SERVER_ERROR); } Method method =
-	 * methodInvoker.getMethod();
-	 * 
-	 * if(method.isAnnotationPresent(DenyAll.class)) { //Functionality has been
-	 * disallowed. requestContext.abortWith(ACCESS_FORBIDDEN); return; }
-	 * if(method.isAnnotationPresent(PermitAll.class)) { //No authorization
-	 * required. return; }
-	 * 
-	 * if(method.isAnnotationPresent(RolesAllowed.class)) { RolesAllowed
-	 * rolesAnnotation = method.getAnnotation(RolesAllowed.class); Set<String>
-	 * rolesSet = new HashSet<String>(Arrays.asList(rolesAnnotation.value())); //
-	 * System.out.println("Checking authentication for user ["+securityContext.
-	 * getUserPrincipal().getName()+"] method ["+method.getAnnotation(javax.ws.rs.
-	 * Path.class).value()+"] roles " + rolesSet.toString());
-	 * 
-	 * //Verifying username and password if(!doAuth(securityContext, rolesSet)) {
-	 * requestContext.abortWith(ACCESS_FORBIDDEN); return; } } //Don't do anything
-	 * if RolesAllowed annotation is not set }
-	 * 
-	 * private boolean doAuth(SecurityContext securityContext, final Set<String>
-	 * rolesSet) { // Principal userPrincipal = securityContext.getUserPrincipal();
-	 * 
-	 * for (String role : rolesSet) { if(securityContext.isUserInRole(role) == true)
-	 * return true; }
-	 * 
-	 * return false; }
-	 */}
+	protected Logger log = LogUtil.getLogger(this);
+
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		
+		if(requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+			//Preflight in here?
+			return;
+		}
+		
+		SecurityContext securityContext = requestContext.getSecurityContext();
+		
+		if(securityContext.getUserPrincipal() == null) {
+			//Not logged in. Auth restriction should be done via web.xml, if no userPrincipal is set it uses anonymous login
+			return;
+		}
+
+		ResourceInfoImpl methodInvoker = (ResourceInfoImpl) requestContext.getProperty("javax.ws.rs.container.ResourceInfo");
+
+		if(methodInvoker == null) {
+			log.error("Unable to fetch method from ResourceMethodInvoker");
+			requestContext.abortWith(Response.status(Response.Status.INTERNAL_SERVER_ERROR).build());
+		}
+		Method method = methodInvoker.getResourceMethod();
+
+		if(method.isAnnotationPresent(DenyAll.class)) {
+			//Functionality has been disallowed.
+			requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+			return;
+		}
+		if(method.isAnnotationPresent(PermitAll.class)) {
+			//No authorization required.
+			return;
+		}
+
+		if(method.isAnnotationPresent(RolesAllowed.class)) {
+			RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
+			Set<String> rolesSet = new HashSet<String>(Arrays.asList(rolesAnnotation.value()));
+//			System.out.println("Checking authentication for user ["+securityContext.getUserPrincipal().getName()+"] method ["+method.getAnnotation(javax.ws.rs.Path.class).value()+"] roles " + rolesSet.toString());
+
+			//Verifying username and password
+			if(!doAuth(securityContext, rolesSet)) {
+				requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+				return;
+			}
+		}
+		//Don't do anything if RolesAllowed annotation is not set
+	}
+
+	private boolean doAuth(SecurityContext securityContext, final Set<String> rolesSet) {
+//		Principal userPrincipal = securityContext.getUserPrincipal();
+
+		for (String role : rolesSet) {
+			if(securityContext.isUserInRole(role) == true)
+				return true;
+		}
+
+		return false;
+	}
+}
