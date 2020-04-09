c0dd76ed19545eff50bee9aeb9ba3583ef9b7bea
Murat Kaan <muratkaanmeral@gmail.com>
2020-04-09 16:05:27 +0200

    Improve abstract translator.

--- core/src/main/java/nl/nn/adapterframework/jdbc/dbms/translator/ITranslator.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/dbms/translator/ITranslator.java
@@ -2,17 +2,20 @@ package nl.nn.adapterframework.jdbc.dbms.translator;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class ITranslator {
-	Map<String, Pattern> finders;
-	Map<String, String> values;
+	Map<String, String> replacements;
+	Map<String, Pattern> regexes;
 	ITranslator target;
 
 	public ITranslator(ITranslator target) {
-		this.target = target;
+		replacements = new HashMap<>();
+		regexes = new HashMap<>();
+		setTarget(target);
 		populateMaps();
 	}
 
@@ -24,18 +27,28 @@ public abstract class ITranslator {
 	 * @return Translated query.
 	 */
 	public String translate(String query) {
-		for (String key : finders.keySet()) {
-			Matcher matcher = finders.get(key).matcher(query);
-			String regex = target.getValueRegex(key);
-			if (matcher.find() && StringUtils.isNotEmpty(key)) {
-				query = matcher.replaceAll(key);
+		for (String key : regexes.keySet()) {
+			Matcher matcher = regexes.get(key).matcher(query);
+			if (matcher.find()) {
+				String regex = target.replacements.get(key);
+				if (StringUtils.isNotEmpty(regex)) {
+					query = matcher.replaceAll(regex);
+				} else {
+					query = matcher.replaceAll("");
+				}
 			}
 		}
 		return query;
 	}
 
-	private String getValueRegex(String key) {
-		return values.get(key);
+	public void setTarget(ITranslator target) {
+		this.target = target;
+	}
+
+	protected Pattern toPattern(String str) {
+		// Make sure there are no greedy matchers.
+		str = str.replaceAll("\\.\\*", ".*?");
+		return Pattern.compile(str, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
 	}
 
 	protected abstract void populateMaps();
