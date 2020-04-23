ea4c193417ff321a4221e8a7a147fd067a808cc3
ccamiletti@gmail.com <ccamiletti@gmail.com>
2020-04-23 22:51:07 +0200

    testing for samba server was added

--- test-integration/src/test/java/nl/nn/adapterframework/filesystem/Samba2FileSystemSenderTest.java
+++ test-integration/src/test/java/nl/nn/adapterframework/filesystem/Samba2FileSystemSenderTest.java
@@ -1,15 +1,8 @@
 package nl.nn.adapterframework.filesystem;
 
-import java.io.IOException;
-
 import org.junit.Before;
-import org.junit.Test;
 
-import nl.nn.adapterframework.configuration.ConfigurationException;
-import nl.nn.adapterframework.core.SenderException;
-import nl.nn.adapterframework.core.TimeOutException;
 import nl.nn.adapterframework.senders.Samba2Sender;
-import nl.nn.adapterframework.stream.Message;
 
 /**
  *  This test class is created to test both SambaFileSystem and SambaFileSystemSender classes.
@@ -20,10 +13,11 @@ import nl.nn.adapterframework.stream.Message;
 public class Samba2FileSystemSenderTest extends FileSystemSenderTest<Samba2Sender, String, Samba2FileSystem> {
 
 	private String shareName = "share";
-	private String username = "wearefrank";
+	private String userName = "wearefrank";
 	private String password = "pass_123";
 	private String host = "localhost";
 	private Integer port = 139;
+	private String domain = null;
 
 	@Override
 	@Before
@@ -33,31 +27,18 @@ public class Samba2FileSystemSenderTest extends FileSystemSenderTest<Samba2Sende
 
 	@Override
 	protected IFileSystemTestHelper getFileSystemTestHelper() {
-		return new Samba2FileSystemTestHelper(shareName,username,password,host,port);
+		return new Samba2FileSystemTestHelper(shareName, userName, password, host, port, domain);
 	}
 
 	@Override
 	public Samba2Sender createFileSystemSender() {
 		Samba2Sender result = new Samba2Sender();
 		result.setShare(shareName);
-		result.setUsername(username);
+		result.setUsername(userName);
 		result.setPassword(password);
 		result.setDomain(host);
 		result.setPort(port);
 		return result;
 	}
-
-	@Test
-	public void createFile() throws SenderException, ConfigurationException, TimeOutException, IOException, FileSystemException {
-		fileSystemSender.setAction("upload");
-		fileSystemSender.setFilename("text6.txt");
-		fileSystemSender.configure();
-		fileSystemSender.getFileSystem().open();
-		fileSystemSender.sendMessage(new Message("text6.txt"), null);
-		System.out.println("exist file text5.txt = " + fileSystemSender.getFileSystem().exists("text5.txt"));
-		System.out.println("exist folder folder1 = " + fileSystemSender.getFileSystem().exists("folder1"));
-		System.out.println("exist folder folder2 = " + fileSystemSender.getFileSystem().exists("folder2"));
-	}
-
 
 }
--- test-integration/src/test/java/nl/nn/adapterframework/filesystem/Samba2FileSystemTestHelper.java
+++ test-integration/src/test/java/nl/nn/adapterframework/filesystem/Samba2FileSystemTestHelper.java
@@ -1,5 +1,6 @@
 package nl.nn.adapterframework.filesystem;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,38 +8,29 @@ import java.io.OutputStream;
 
 import org.junit.Before;
 
-import jcifs.smb.NtlmPasswordAuthentication;
-import jcifs.smb.SmbFile;
-import jcifs.smb.SmbFileInputStream;
-import jcifs.smb.SmbFileOutputStream;
 import nl.nn.adapterframework.configuration.ConfigurationException;
-import nl.nn.adapterframework.filesystem.FileSystemException;
-import nl.nn.adapterframework.filesystem.IFileSystemTestHelper;
+import nl.nn.adapterframework.core.SenderException;
+import nl.nn.adapterframework.core.TimeOutException;
+import nl.nn.adapterframework.senders.Samba2Sender;
+import nl.nn.adapterframework.stream.Message;
 
 public class Samba2FileSystemTestHelper implements IFileSystemTestHelper {
 
-	private String shareName = null;
-	private String username = null;
-	private String password = null;
-	private String domain = null;
-	private SmbFile context;
-	private String share = null; // the path of smb network must start with "smb://"
-	private Integer port;
-	
+	protected Samba2Sender fileSystemSender;
 		
-	public Samba2FileSystemTestHelper(String shareName, String username, String password, String domain, Integer port) {
-		this.shareName=shareName;
-		this.username=username;
-		this.password=password;
-		this.domain=domain;
-		this.port = port;
-		this.share = "smb://" + domain + ":" + port + "/" + shareName + "/"; // the path of smb network must start with "smb://"
+	public Samba2FileSystemTestHelper(String shareFolder, String username, String password, String host, Integer port, String domain) {
+		fileSystemSender = new Samba2Sender();
+		fileSystemSender.setShare(shareFolder);
+		fileSystemSender.setUsername(username);
+		fileSystemSender.setPassword(password);
+		fileSystemSender.setDomain(host);
+		fileSystemSender.setPort(port);
 	}
 	
 	@Override
 	@Before
-	public void setUp() throws ConfigurationException, IOException, FileSystemException {
-		System.out.println("");
+	public void setUp() throws ConfigurationException, IOException, FileSystemException, SenderException, TimeOutException {
+		// not necessary
 	}
 
 	@Override
@@ -46,53 +38,66 @@ public class Samba2FileSystemTestHelper implements IFileSystemTestHelper {
 		// not necessary
 	}
 	
+	private boolean isPresent(String name) throws Exception {
+		fileSystemSender.open();
+		boolean r = fileSystemSender.getFileSystem().exists(name);
+		fileSystemSender.close();
+		return r;
+	}
+	
 	@Override
-	public boolean _fileExists(String folder, String filename) throws Exception {
-		String path = folder==null ? filename : folder+"/"+filename;
-		return new SmbFile(context, path).exists();
+	public boolean _fileExists(String folder, String fileName) throws Exception {
+		return isPresent(fileName);
 	}
 
 	@Override
-	public void _deleteFile(String folder, String filename) throws Exception {
-		SmbFile f = null;
-		f = new SmbFile(context, filename);
-		f.delete();
+	public void _deleteFile(String folder, String fileName) throws Exception {
+		this.prepareAction("delete", fileName);
+		fileSystemSender.sendMessage(new Message("delete"), null);
+		fileSystemSender.close();
 	}
 
 	@Override
-	public OutputStream _createFile(String folder, String filename) throws Exception {
-		return new SmbFileOutputStream(new SmbFile(context, filename));
+	public OutputStream _createFile(String folder, String fileName) throws Exception {
+		this.prepareAction("upload", fileName);
+		Message message = fileSystemSender.sendMessage(new Message("upload"), null);
+		OutputStream os = new ByteArrayOutputStream();
+		os.write(message.asByteArray());
+		return os;
 	}
 
 	@Override
-	public InputStream _readFile(String folder, String filename) throws FileNotFoundException, Exception {
-		SmbFileInputStream is = null;
-		is = new SmbFileInputStream(new SmbFile(context, filename));
+	public InputStream _readFile(String folder, String fileName) throws FileNotFoundException, Exception {
+		this.prepareAction("read", fileName);
+		Message message = fileSystemSender.sendMessage(new Message(fileName), null);
+		InputStream is = message.asInputStream();
 		return is;
 	}
 
 	@Override
-	public void _createFolder(String filename) throws IOException {
-		try {
-			if(_folderExists(filename)) {
-				throw new FileSystemException("Create directory for [" + filename + "] has failed. Directory already exists.");
-			}
-			new SmbFile(context, filename).mkdir();
-		} catch (Exception e) {
-			throw new IOException(e);
-		}
+	public boolean _folderExists(String folderName) throws Exception {
+		return isPresent(folderName);
 	}
 
 	@Override
-	public boolean _folderExists(String folderName) throws Exception {
-		return _fileExists(null,folderName);
+	public void _createFolder(String folderName) throws IOException, ConfigurationException, SenderException, TimeOutException {
+		this.prepareAction("mkdir", folderName);
+		fileSystemSender.sendMessage(new Message(folderName), null);
+		fileSystemSender.close();
 	}
-
+	
 	@Override
 	public void _deleteFolder(String folderName) throws Exception {
-		if(!folderName.endsWith("/")) {
-			folderName += "/";
-		}
-		_deleteFile(null, folderName);
+		this.prepareAction("rmdir", folderName);
+		fileSystemSender.sendMessage(new Message(folderName), null);
+		fileSystemSender.close();
 	}
+	
+	private void prepareAction(String action, String name) throws ConfigurationException, SenderException {
+		fileSystemSender.setAction(action);
+		fileSystemSender.setFilename(name);
+		fileSystemSender.configure();
+		fileSystemSender.open();
+	}
+
 }
