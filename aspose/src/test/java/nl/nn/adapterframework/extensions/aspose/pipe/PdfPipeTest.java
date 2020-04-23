614ba989a7b73eace64d7ed070b81b892c754253
Niels Meijer <nielsmeijer@hotmail.com>
2020-04-23 17:34:05 +0200

    Fix broken test

--- aspose/src/test/java/nl/nn/adapterframework/extensions/aspose/pipe/PdfPipeTest.java
+++ aspose/src/test/java/nl/nn/adapterframework/extensions/aspose/pipe/PdfPipeTest.java
@@ -15,48 +15,36 @@
 */
 package nl.nn.adapterframework.extensions.aspose.pipe;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.File;
-import java.nio.file.Files;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.IPipeLineSession;
-import nl.nn.adapterframework.core.PipeRunException;
-import nl.nn.adapterframework.core.PipeRunResult;
-import nl.nn.adapterframework.core.PipeStartException;
-import nl.nn.adapterframework.util.Misc;
-import nl.nn.adapterframework.testutil.MatchUtils;
-import nl.nn.adapterframework.testutil.TestFileUtils;
-import nl.nn.adapterframework.core.PipeForward;
 import nl.nn.adapterframework.core.PipeLineSessionBase;
 import nl.nn.adapterframework.core.PipeRunException;
-import nl.nn.adapterframework.core.PipeRunResult;
-import nl.nn.adapterframework.parameters.Parameter;
+import nl.nn.adapterframework.core.PipeStartException;
+import nl.nn.adapterframework.pipes.PipeTestBase;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.testutil.MatchUtils;
 import nl.nn.adapterframework.testutil.TestFileUtils;
 
-import nl.nn.adapterframework.pipes.PipeTestBase;
-import nl.nn.adapterframework.extensions.aspose.pipe.PdfPipe;
-
 /**
  * Executes defined tests against the PdfPipe to ensure the correct working of this pipe.
  * 
- * @since 7.6
  * @author Laurens Mäkel
  */
 
+@Ignore
 public class PdfPipeTest extends PipeTestBase<PdfPipe> {
 	public final String REGEX_PATH_IGNORE = "(?<=convertedDocument=\").*(?=\")";
 	public final String REGEX_TIJDSTIP_IGNORE = "(?<=Tijdstip:).*(?=\" n)";
@@ -145,7 +133,7 @@ public class PdfPipeTest extends PipeTestBase<PdfPipe> {
 		String actualXml = null;
 		// String input = TestFileUtils.getTestFile(fileToConvert);
 		try {
-			PipeRunResult prr = pipe.doPipe(Message.asMessage(new File(TestFileUtils.getTestFileURL(fileToConvert).toURI())),session);
+			pipe.doPipe(Message.asMessage(new File(TestFileUtils.getTestFileURL(fileToConvert).toURI())),session);
 			actualXml = session.get("documents").toString();
 		} catch (URISyntaxException e) {
 			fail("Could not convert document due "  + e);
@@ -162,103 +150,86 @@ public class PdfPipeTest extends PipeTestBase<PdfPipe> {
 		return result;
 	}
 
-	@Ignore
 	@Test()
 	public void Bmp2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Bmp2Pdf", "/PdfPipe/bmp.bmp", "/PdfPipe/xml-results/bmp.xml", "/PdfPipe/results/bmp.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void DocWord2016Macro2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("DocWord2016Macro2Pdf", "/PdfPipe/docm-word-2016-macro.docm", "/PdfPipe/xml-results/docm-word-2016-macro.xml", "/PdfPipe/results/docm-word-2016-macro.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void DocWord20032Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("DocWord20032Pdf", "/PdfPipe/doc-word-2003.doc", "/PdfPipe/xml-results/doc-word-2003.xml", "/PdfPipe/results/doc-word-2003.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Dot2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Dot2Pdf", "/PdfPipe/dot.dot", "/PdfPipe/xml-results/dot.xml", "/PdfPipe/results/dot.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void EmlFromGroupmailbox2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("EmlFromGroupmailbox", "/PdfPipe/eml-from-groupmailbox.eml", "/PdfPipe/xml-results/eml-from-groupmailbox.xml", "/PdfPipe/results/eml-from-groupmailbox.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Gif2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Gif2Pdf", "/PdfPipe/gif.gif", "/PdfPipe/xml-results/gif.xml", "/PdfPipe/results/gif.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Htm2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Htm2Pdf", "/PdfPipe/htm.htm", "/PdfPipe/xml-results/htm.xml", "/PdfPipe/results/htm.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Html2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Html2Pdf", "/PdfPipe/html.html", "/PdfPipe/xml-results/html.xml", "/PdfPipe/results/html.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Jpeg2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Jpeg2Pdf", "/PdfPipe/jpeg.jpeg", "/PdfPipe/xml-results/jpeg.xml", "/PdfPipe/results/jpeg.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Jpg2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Jpg2Pdf", "/PdfPipe/jpg.jpg", "/PdfPipe/xml-results/jpg.xml", "/PdfPipe/results/jpg.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Log2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Log2Pdf", "/PdfPipe/log.log", "/PdfPipe/xml-results/log.xml", "/PdfPipe/results/log.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Png2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Png2Pdf", "/PdfPipe/png.png", "/PdfPipe/xml-results/png.xml", "/PdfPipe/results/png.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Ppt2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Ppt2Pdf", "/PdfPipe/ppt.ppt", "/PdfPipe/xml-results/ppt.xml", "/PdfPipe/results/ppt.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Rtf2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Rtf2Pdf", "/PdfPipe/rtf.rtf", "/PdfPipe/xml-results/rtf.xml", "/PdfPipe/results/rtf.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Tiff2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Tiff2Pdf", "/PdfPipe/tiff.tiff", "/PdfPipe/xml-results/tiff.xml", "/PdfPipe/results/tiff.pdf");
 	}
 
-	@Ignore
 	@Test()
 	public void Txt2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectSuccessfullConversion("Txt2Pdf", "/PdfPipe/txt.txt", "/PdfPipe/xml-results/txt.xml", "/PdfPipe/results/txt.pdf");
 	}
-	
-	@Ignore
+
 	@Test()
 	public void Zip2Pdf() throws ConfigurationException, PipeStartException, IOException, PipeRunException {
 		expectUnsuccessfullConversion("Zip2Pdf", "/PdfPipe/PdfPipe.zip", "/PdfPipe/xml-results/zip.xml");
