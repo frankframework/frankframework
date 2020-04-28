ab7f30bfa0739083d7ebda41267043e0f8c6c066
Niels <nielsmeijer@hotmail.com>
2020-04-28 08:40:35 +0200

    GUI 3.0 Logging bugfixes (#644)
    * Normalize log path and display filename
    * Fix bug where logfiles would not always be displayed properly

--- webapp/src/main/webapp/iaf/gui/js/controllers.js
+++ webapp/src/main/webapp/iaf/gui/js/controllers.js
@@ -1766,8 +1766,7 @@ angular.module('iaf.beheerconsole')
 				iframeBody.css({"background-color": "rgb(243, 243, 244)"});
 				iframe.css({"height": iframeBody.height() + 50});
 			};
-		}, 50);
-		$state.transitionTo('pages.logging', {directory: $scope.directory, file: file.name}, { notify: false, reload: false });
+		});
 	};
 
 	$scope.closeFile = function () {
@@ -1790,6 +1789,7 @@ angular.module('iaf.beheerconsole')
 		Api.Get(url, function(data) {
 			$scope.alert = false;
 			$.extend($scope, data);
+			$scope.path = data.directory;
 			if(data.count > 500) {
 				$scope.alert = "Total number of items ("+data.count+") exceeded maximum number, only showing first 500 items!";
 			}
@@ -1799,10 +1799,11 @@ angular.module('iaf.beheerconsole')
 	};
 
 	$scope.open = function(file) {
-		if(file.type == "directory")
+		if(file.type == "directory") {
 			$state.transitionTo('pages.logging', {directory: file.path});
-		else
-			openFile(file);
+		} else {
+			$state.transitionTo('pages.logging', {directory: $scope.directory, file: file.name}, { notify: false, reload: false });
+		}
 	};
 
 	//This is only false when the user opens the logging page
@@ -1812,6 +1813,7 @@ angular.module('iaf.beheerconsole')
 		var file = $stateParams.file;
 
 		$scope.directory = directory;
+		$scope.path = directory+"/"+file;
 		openFile({path: directory+"/"+file, name: file});
 	}
 	else {
