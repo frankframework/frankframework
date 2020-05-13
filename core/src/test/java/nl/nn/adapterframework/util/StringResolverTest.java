7c2a1d2f4fe5fcb8b58bd707dc588d8f5b6a7b8f
Sina Sen <sinasen2000@hotmail.com>
2020-05-13 13:56:33 -0500

    add util test classes

---
+++ core/src/test/java/nl/nn/adapterframework/util/StringResolverTest.java
@@ -0,0 +1,74 @@
+package nl.nn.adapterframework.util;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* StringResolver Tester. 
+* 
+* @author <Sina Sen>
+
+*/ 
+public class StringResolverTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getSystemProperty(String key, String def) 
+* 
+*/ 
+@Test
+public void testGetSystemProperty() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: substVars(String val, Map props1, Map props2, List<String> propsToHide) 
+* 
+*/ 
+@Test
+public void testSubstVarsForValProps1Props2PropsToHide() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: substVars(String val, Map props1, Map props2) 
+* 
+*/ 
+@Test
+public void testSubstVarsForValProps1Props2() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: substVars(String val, Map props) 
+* 
+*/ 
+@Test
+public void testSubstVarsForValProps() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: needsResolution(String string) 
+* 
+*/ 
+@Test
+public void testNeedsResolution() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
