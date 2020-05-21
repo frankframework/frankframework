14126dbdf9c525dd3e00d3c01b7ab42a8c676f89
Sina Sen <sinasen2000@hotmail.com>
2020-05-21 09:46:47 -0500

    Fix codacy error

--- core/src/test/java/nl/nn/adapterframework/util/CounterStatisticTest.java
+++ core/src/test/java/nl/nn/adapterframework/util/CounterStatisticTest.java
@@ -2,7 +2,6 @@ package nl.nn.adapterframework.util;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * CounterStatistic Tester.
