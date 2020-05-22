63ceec9e6189ea6bf8f408d312fec5af12b256cc
Niels Meijer <nielsmeijer@hotmail.com>
2020-05-22 14:35:29 +0200

    Remove unused import

--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/Base.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/Base.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletConfig;
-import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 
 import org.apache.commons.lang3.StringUtils;
