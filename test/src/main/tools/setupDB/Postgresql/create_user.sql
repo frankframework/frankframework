8feeca1eb322d8fdd3e3eae2a0edff763812d421
Wouter <wouter@integrationpartners.nl>
2020-07-07 11:56:25 +0200

    change database name

--- test/src/main/tools/setupDB/Postgresql/create_user.sql
+++ test/src/main/tools/setupDB/Postgresql/create_user.sql
@@ -1,14 +1,14 @@
 --Prevent all connections to existing database
 --Otherwise it cannot be dropped
-REVOKE CONNECT ON DATABASE wearefrank_db FROM public;
+REVOKE CONNECT ON DATABASE testiaf FROM public;
 SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'wearefrank_db'
+WHERE pg_stat_activity.datname = 'testiaf'
 AND pid <> pg_backend_pid();
 --Drop database and user
-DROP DATABASE IF EXISTS wearefrank_db;
+DROP DATABASE IF EXISTS testiaf;
 DROP ROLE IF EXISTS testiaf_user;
 --Create new database and user
-CREATE DATABASE wearefrank_db;
+CREATE DATABASE testiaf;
 CREATE USER testiaf_user WITH PASSWORD 'testiaf_pass01';
-GRANT ALL PRIVILEGES ON DATABASE wearefrank_db TO testiaf_user;
+GRANT ALL PRIVILEGES ON DATABASE testiaf TO testiaf_user;
