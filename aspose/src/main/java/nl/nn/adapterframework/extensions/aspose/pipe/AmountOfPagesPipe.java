2f22df7734ad4412b058d1e724e5f7c2a12ce594
Laurens Mäkel <laurens.makel@nn-group.com>
2020-08-10 10:49:56 +0200

    Add java/ibis doc and license info

--- aspose/src/main/java/nl/nn/adapterframework/extensions/aspose/pipe/AmountOfPagesPipe.java
+++ aspose/src/main/java/nl/nn/adapterframework/extensions/aspose/pipe/AmountOfPagesPipe.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2019, 2020 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package nl.nn.adapterframework.extensions.aspose.pipe;
 
 import java.io.ByteArrayInputStream;
@@ -14,8 +29,11 @@ import nl.nn.adapterframework.stream.Message;
 import com.aspose.pdf.Document;
 
 /**
+ * Returns the amount of pages of a PDF file.
  * 
- * @author M63H114 Laurens Mäkel
+ * 
+ * @author Laurens Mäkel
+ * @since  7.6
  *
  */
 public class AmountOfPagesPipe extends FixedForwardPipe {
@@ -44,6 +62,7 @@ public class AmountOfPagesPipe extends FixedForwardPipe {
 		return new PipeRunResult(getForward(), Integer.toString(result) );
 	}
 
+	@IbisDoc({ "charset to be used to encode the given input string ", "ISO-8859-1" })
 	public void setCharset(String charset){
 		this.charset = charset;
 	}
