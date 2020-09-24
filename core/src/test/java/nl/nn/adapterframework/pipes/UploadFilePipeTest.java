fd25ff48d42c8e72b68b2b428ef79a8633e6f169
Niels Meijer <nielsmeijer@hotmail.com>
2020-09-24 14:10:16 +0200

    remove unused import

--- core/src/test/java/nl/nn/adapterframework/pipes/UploadFilePipeTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/UploadFilePipeTest.java
@@ -1,7 +1,6 @@
 package nl.nn.adapterframework.pipes;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 import java.io.BufferedInputStream;
