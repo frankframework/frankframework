3a1c13e80a9fca05ada7b51c20e2ba8d75596626
Martijn Dirkse <martijn@integrationpartners.nl>
2020-10-12 11:22:46 +0200

    Fix ModelBuilderTest

---
+++ core/src/test/java/nl/nn/adapterframework/doc/testtarget/simple/ListenerChild.java
@@ -0,0 +1,5 @@
+package nl.nn.adapterframework.doc.testtarget.simple;
+
+public class ListenerChild extends ListenerParent {
+
+}
---
+++ core/src/test/java/nl/nn/adapterframework/doc/testtarget/simple/ListenerParent.java
@@ -0,0 +1,5 @@
+package nl.nn.adapterframework.doc.testtarget.simple;
+
+public class ListenerParent implements IListener {
+
+}
