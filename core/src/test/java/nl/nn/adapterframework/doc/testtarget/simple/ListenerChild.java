a6c5b1b5a768eb7d249827b92d7957fee5af54f2
Martijn Dirkse <martijn@integrationpartners.nl>
2020-10-12 11:28:43 +0200

    Fix FrankDocGroupTest

--- core/src/test/java/nl/nn/adapterframework/doc/testtarget/simple/ListenerChild.java
+++ core/src/test/java/nl/nn/adapterframework/doc/testtarget/simple/ListenerChild.java
@@ -1,5 +1,10 @@
 package nl.nn.adapterframework.doc.testtarget.simple;
 
 public class ListenerChild extends ListenerParent {
+	public void setChildAttribute(String value) {
+	}
 
+	public String getChildAttribute() {
+		return null;
+	}
 }
