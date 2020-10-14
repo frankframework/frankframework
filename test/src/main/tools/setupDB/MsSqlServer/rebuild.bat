8c65ef87dffcd0985f441b3866f358fcb766ff06
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2020-10-14 17:42:43 +0200

    Name database docker images rather than containers

--- test/src/main/tools/setupDB/MsSqlServer/rebuild.bat
+++ test/src/main/tools/setupDB/MsSqlServer/rebuild.bat
@@ -2,4 +2,8 @@ cd %~dp0
 docker-compose down --volumes
 docker image prune --force
 docker volume prune --force
+docker container rm iaf-db-test-mssql
+
+docker build -t iaf-test-db-mssql .
+
 docker-compose up --build
