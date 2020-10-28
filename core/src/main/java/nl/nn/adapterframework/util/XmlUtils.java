9bb314c80ff0c729098fa5c456853dd5c007c18f
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2020-10-28 17:31:58 +0100

    Show XML Component versions in GUI

--- core/src/main/java/nl/nn/adapterframework/util/XmlUtils.java
+++ core/src/main/java/nl/nn/adapterframework/util/XmlUtils.java
@@ -67,7 +67,6 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.SystemUtils;
 import org.apache.logging.log4j.Logger;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
@@ -90,6 +89,9 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.ext.LexicalHandler;
+
+import com.ctc.wstx.api.ReaderConfig;
+import com.ctc.wstx.stax.WstxInputFactory;
 
 import nl.nn.adapterframework.configuration.Configuration;
 import nl.nn.adapterframework.configuration.ConfigurationException;
@@ -1722,46 +1724,63 @@ public class XmlUtils {
 		return writer.toString();
 	}
 
-	public static String getVersionInfo() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(AppConstants.getInstance().getProperty("application.name") + " "
-				+ AppConstants.getInstance().getProperty("application.version")).append(SystemUtils.LINE_SEPARATOR);
-		sb.append("XML tool version info:").append(SystemUtils.LINE_SEPARATOR);
+
+	public static Set<Entry<String,String>> getVersionInfo() {
+		Map<String,String> map = new LinkedHashMap<>();
+		
+		map.put("XML tools:",null);
 
 		SAXParserFactory spFactory = getSAXParserFactory();
-		sb.append("SAXParserFactory-class =").append(spFactory.getClass().getName()).append(SystemUtils.LINE_SEPARATOR);
+		map.put("SAXParserFactory-class", spFactory.getClass().getName());
 		DocumentBuilderFactory domFactory1 = getDocumentBuilderFactory(false);
-		sb.append("DocumentBuilderFactory1-class =").append(domFactory1.getClass().getName()).append(SystemUtils.LINE_SEPARATOR);
+		map.put("DocumentBuilderFactory1-class", domFactory1.getClass().getName());
 		DocumentBuilderFactory domFactory2 = getDocumentBuilderFactory(true);
-		sb.append("DocumentBuilderFactory2-class =").append(domFactory2.getClass().getName()).append(SystemUtils.LINE_SEPARATOR);
+		map.put("DocumentBuilderFactory2-class", domFactory2.getClass().getName());
 
 		TransformerFactory tFactory1 = getTransformerFactory(1);
-		sb.append("TransformerFactory1-class =").append(tFactory1.getClass().getName()).append(SystemUtils.LINE_SEPARATOR);
+		map.put("TransformerFactory1-class", tFactory1.getClass().getName());
 		TransformerFactory tFactory2 = getTransformerFactory(2);
-		sb.append("TransformerFactory2-class =").append(tFactory2.getClass().getName()).append(SystemUtils.LINE_SEPARATOR);
+		map.put("TransformerFactory2-class", tFactory2.getClass().getName());
 
-		sb.append("Apache-XML tool version info:").append(SystemUtils.LINE_SEPARATOR);
-
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		map.put("XMLInputFactory-class", xmlInputFactory.getClass().getName());
+		
+		map.put("XML tool version info:", null);
 		try {
-			sb.append("Xerces-Version=").append(org.apache.xerces.impl.Version.getVersion()).append(SystemUtils.LINE_SEPARATOR);
+			map.put("Xerces-Version", org.apache.xerces.impl.Version.getVersion());
 		}  catch (Throwable t) {
-			sb.append("Xerces-Version not found (").append(t.getClass().getName()).append(": ").append(t.getMessage()).append(")").append(SystemUtils.LINE_SEPARATOR);
+			map.put("Xerces-Version","not found (" + t.getClass().getName() + "): "+ t.getMessage() + ")");
 		}
 
 		try {
 			String xalanVersion = org.apache.xalan.Version.getVersion();
- 			sb.append("Xalan-Version=" + xalanVersion + SystemUtils.LINE_SEPARATOR);
-		}  catch (Throwable t) {
-			sb.append("Xalan-Version not found (").append(t.getClass().getName()).append(": ").append(t.getMessage()).append(")").append(SystemUtils.LINE_SEPARATOR);
+ 			map.put("Xalan-Version", xalanVersion);
+		} catch (Throwable t) {
+			map.put("Xalan-Version","not found (" + t.getClass().getName() + "): "+ t.getMessage() + ")");
 		}
-
 		try {
-//			sb.append("XmlCommons-Version="+org.apache.xmlcommons.Version.getVersion()+SystemUtils.LINE_SEPARATOR);
-		}  catch (Throwable t) {
-			sb.append("XmlCommons-Version not found (").append(t.getClass().getName()).append(": ").append(t.getMessage()).append(")").append(SystemUtils.LINE_SEPARATOR);
+			String saxonVersion = net.sf.saxon.Version.getProductTitle();
+ 			map.put("Saxon-Version", saxonVersion);
+		} catch (Throwable t) {
+			map.put("Saxon-Version","not found (" + t.getClass().getName() + "): "+ t.getMessage() + ")");
+		}
+		try {
+			if (xmlInputFactory instanceof WstxInputFactory) {
+				ReaderConfig woodstoxConfig = ((WstxInputFactory)xmlInputFactory).createPrivateConfig();
+				String woodstoxVersion = ReaderConfig.getImplName()+" "+ReaderConfig.getImplVersion()+"; xml1.1 "+(woodstoxConfig.isXml11()?"":"not ")+"enabled";
+	 			map.put("Woodstox-Version", woodstoxVersion);
+			}
+		} catch (Throwable t) {
+			map.put("Woodstox-Version","not found (" + t.getClass().getName() + "): "+ t.getMessage() + ")");
 		}
 
-		return sb.toString();
+//		try {
+//			map.put("XmlCommons-Version", org.apache.xmlcommons.Version.getVersion());
+//		} catch (Throwable t) {
+//			map.put("XmlCommons-Version","not found (" + t.getClass().getName() + "): "+ t.getMessage() + ")");
+//		}
+
+		return map.entrySet();
 	}
 
 	public static String source2String(Source source, boolean removeNamespaces) throws TransformerException {
