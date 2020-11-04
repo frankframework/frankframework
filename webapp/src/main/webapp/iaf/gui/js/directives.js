bdb656124033e3a6f252566385819eac40a51b67
Niels <nielsmeijer@hotmail.com>
2020-11-04 08:27:49 +0100

    Add pipe message log to Show IBISSTORE Summary page (#1230)
    * Fix error message format and healthcheck error format
    * Fix back button not always going back
    * Add pipe message log and fix back button on show ibisstore page

--- webapp/src/main/webapp/iaf/gui/js/directives.js
+++ webapp/src/main/webapp/iaf/gui/js/directives.js
@@ -1,3 +1,10 @@
+function parseStateRef(ref, current) {
+	var preparsed = ref.match(/^\s*({[^}]*})\s*$/), parsed;
+	if (preparsed) ref = current + '(' + preparsed[1] + ')';
+	parsed = ref.replace(/\n/g, " ").match(/^([^(]+?)\s*(\((.*)\))?$/);
+	if (!parsed || parsed.length !== 4) throw new Error("Invalid state ref '" + ref + "'");
+	return { state: parsed[1], paramExpr: parsed[3] || null };
+}
 angular.module('iaf.beheerconsole')
 
 .directive('pageTitle', ['$rootScope', '$timeout', '$state', '$transitions', 'Debug', function($rootScope, $timeout, $state, $transitions, Debug) {
@@ -66,6 +73,47 @@ angular.module('iaf.beheerconsole')
 			};
 		}
 	};
+})
+
+.directive('uiLref', ['$state', '$location', '$timeout', function($state, $location, $timeout) {
+	return {
+		link: function(scope, element, attributes) {
+			var ref = parseStateRef(attributes.uiLref, $state.current.name);
+			var params;
+			if (ref.paramExpr) {
+				params = angular.copy(scope.$eval(ref.paramExpr));
+			}
+
+			var transition = null;
+			element.bind("click", function() {
+				if(transition) {
+					$timeout.cancel(transition);
+				}
+				var adapter = scope.adapter;
+				if(adapter) {
+					$timeout(function() {
+						$location.hash(adapter.name);
+					});
+				}
+				transition = $timeout(function() {
+					$state.go(ref.state, params);
+				}, 5);
+			});
+		}
+	};
+}])
+
+.directive('backButton', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			element.bind('click', goBack);
+			function goBack() {
+				history.back();
+				scope.$apply();
+			}
+		}
+	}
 })
 
 .directive('timeSince', ['appConstants', '$interval', function(appConstants, $interval) {
