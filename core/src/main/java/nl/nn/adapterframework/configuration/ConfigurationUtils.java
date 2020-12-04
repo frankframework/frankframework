0aa712337e886a135b08c2484e218cddf9b06afa
ricardovh <ricardovanholst@gmail.com>
2020-12-04 15:25:06 +0100

    Fix typo in error message when uploading configuration without BuildInfo

--- core/src/main/java/nl/nn/adapterframework/configuration/ConfigurationUtils.java
+++ core/src/main/java/nl/nn/adapterframework/configuration/ConfigurationUtils.java
@@ -300,7 +300,7 @@ public class ConfigurationUtils {
 				}
 			}
 			if(!isBuildInfoPresent) {
-				throw new ConfigurationException("no ["+buildInfoFilename+"] persent in configuration");
+				throw new ConfigurationException("no ["+buildInfoFilename+"] present in configuration");
 			}
 		}
 
