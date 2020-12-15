e2982fb38376c01fd2562b5c0b87950511c2092c
Martijn Dirkse <martijn@integrationpartners.nl>
2020-12-15 13:06:25 +0100

    More tests

--- core/src/test/java/nl/nn/adapterframework/doc/model/NavigationCumulativeTest.java
+++ core/src/test/java/nl/nn/adapterframework/doc/model/NavigationCumulativeTest.java
@@ -4,6 +4,8 @@ import static java.util.Arrays.asList;
 import static nl.nn.adapterframework.doc.model.ElementChild.ALL;
 import static nl.nn.adapterframework.doc.model.ElementChild.IN_XSD;
 import static nl.nn.adapterframework.doc.model.ElementChild.NONE;
+import static nl.nn.adapterframework.doc.model.ElementChild.DEPRECATED;
+
 import static org.junit.Assert.assertEquals;
 
 import java.util.Collection;
@@ -21,27 +23,31 @@ import org.junit.runners.Parameterized.Parameters;
 public class NavigationCumulativeTest {
 	private static final String PACKAGE = "nl.nn.adapterframework.doc.testtarget.walking";
 
-	@Parameters(name = "{0} with {1}")
+	@Parameters(name = "{0} with {1} and {2}")
 	public static Collection<Object[]> data() {
 		return asList(new Object[][] {
-			{"Parent", IN_XSD, NONE, asList("parentAttributeFirst", "parentAttributeSecond")},
-			{"Child", IN_XSD, NONE, asList("childAttribute", "parentAttributeFirst", "parentAttributeSecond")},
-			{"Child", ALL, NONE, asList("childAttribute", "parentAttributeFirst", "parentAttributeSecond")},
-			{"GrandChild", ALL, NONE, asList("grandChildAttribute", "parentAttributeSecond", "childAttribute", "parentAttributeFirst")},
-			{"GrandChild2", ALL, NONE, asList("grandChild2Attribute", "child2Attribute", "parentAttributeFirst", "parentAttributeSecond")}
+			{"Parent", "Parent", IN_XSD, NONE, asList("parentAttributeFirst", "parentAttributeSecond")},
+			{"Child inXsd", "Child", IN_XSD, NONE, asList("childAttribute", "parentAttributeFirst", "parentAttributeSecond")},
+			{"Child all", "Child", ALL, NONE, asList("childAttribute", "parentAttributeFirst", "parentAttributeSecond")},
+			{"GrandChild", "GrandChild", ALL, NONE, asList("grandChildAttribute", "parentAttributeSecond", "childAttribute", "parentAttributeFirst")},
+			{"GrandChild2 no reject", "GrandChild2", ALL, NONE, asList("grandChild2Attribute", "child2Attribute", "parentAttributeFirst", "parentAttributeSecond")},
+			{"GrandChild2 reject deprecated", "GrandChild2", ALL, DEPRECATED, asList("grandChild2Attribute", "parentAttributeFirst", "parentAttributeSecond")} 
 		});
 	}
 
 	@Parameter(0)
-	public String simpleClassName;
+	public String title;
 
 	@Parameter(1)
-	public Predicate<ElementChild> childSelector;
+	public String simpleClassName;
 
 	@Parameter(2)
-	public Predicate<ElementChild> childRejector;
+	public Predicate<ElementChild> childSelector;
 
 	@Parameter(3)
+	public Predicate<ElementChild> childRejector;
+
+	@Parameter(4)
 	public List<String> childNames;
 
 	@Test
