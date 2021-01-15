ca5baae8565af4f66bc0571198145f972ef18521
Niels <nielsmeijer@hotmail.com>
2021-01-15 17:37:03 +0100

    Auto adjust dates to match UTC (#1398)

--- core/src/test/java/nl/nn/adapterframework/util/DateUtilsTest.java
+++ core/src/test/java/nl/nn/adapterframework/util/DateUtilsTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertNull;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -18,40 +18,55 @@ import org.junit.Test;
  */
 public class DateUtilsTest {
 
-	public static String TZ = Calendar.getInstance().getTimeZone().getID();
+	private static final TimeZone CI_TZ = Calendar.getInstance().getTimeZone();
+	private static final TimeZone TEST_TZ = TimeZone.getTimeZone("UTC");
+	private static final int TZ_DIFF = (CI_TZ.getRawOffset() - CI_TZ.getDSTSavings());
 
-	/**
-	 * Tests have been written in CET timezone, change it here so Travis/Azure don't fail when running in other timezones
-	 */
 	@BeforeClass
 	public static void setUp() {
-		String timezone = "Europe/Amsterdam";
-		System.out.println("settings timezone from ["+TZ+"] to [" + timezone + "]");
-		System.setProperty("user.timezone", timezone);
+		if(TZ_DIFF != 0) {
+			System.out.println("adjusting date settings from ["+CI_TZ.getDisplayName()+"] to [" + TEST_TZ.getDisplayName() + "] offset ["+TZ_DIFF+"]");
+		}
 	}
 
-	@AfterClass
-	public static void tearDown() {
-		System.out.println("reverting timezone back to [" + TZ + "]");
-		System.setProperty("user.timezone", TZ);
+	/**
+	 * Tests have been written in UTC, adjust the TimeZone so Travis/Azure/GitHub CI don't fail when running in other TimeZones
+	 */
+	private Date getCorrectedDate(Date date) {
+		if(TZ_DIFF != 0) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			calendar.add(Calendar.MILLISECOND, -TZ_DIFF);
+			calendar.setTimeZone(TEST_TZ);
+			return calendar.getTime();
+		} else {
+			return date;
+		}
 	}
 
+	/**
+	 * Tests have been written in UTC, adjust the TimeZone so Travis/Azure/GitHub CI don't fail when running in other TimeZones
+	 */
+	private long getCorrectedDate(long l) {
+		Date date = new Date(l);
+		return getCorrectedDate(date).getTime();
+	}
 
 	@Test
 	public void testFormatLong() throws Exception {
-		String date = DateUtils.format(1380924000000L);
+		String date = DateUtils.format(getCorrectedDate(1380924000000L));
 		assertEquals("2013-10-05 00:00:00.000", date);
 	}
 
 	@Test
 	public void testFormatDate() throws Exception {
-		String date = DateUtils.format(new Date(1380924000000L));
+		String date = DateUtils.format(getCorrectedDate(new Date(1380924000000L)));
 		assertEquals("2013-10-05 00:00:00.000", date);
 	}
 
 	@Test
 	public void testFormatForDateDateFormat() throws Exception {
-		Date d = new Date(1500000000);
+		Date d = getCorrectedDate(new Date(1500000000));
 		String time = DateUtils.format(d, DateUtils.FORMAT_FULL_GENERIC);
 		assertEquals("1970-01-18 09:40:00.000", time);
 
@@ -60,13 +75,13 @@ public class DateUtilsTest {
 	@Test
 	public void testParseToDate() throws Exception {
 		Date date = DateUtils.parseToDate("05-10-13", DateUtils.FORMAT_DATE);
-		assertEquals(1380924000000L, date.getTime());
+		assertEquals(getCorrectedDate(1380924000000L), date.getTime());
 	}
 
 	@Test
 	public void testParseToDateFullYear() throws Exception {
 		Date date = DateUtils.parseToDate("05-10-2014", DateUtils.FORMAT_DATE);
-		assertEquals(1412460000000L, date.getTime());
+		assertEquals(getCorrectedDate(1412460000000L), date.getTime());
 	}
 
 	@Test
@@ -84,13 +99,13 @@ public class DateUtilsTest {
 	@Test
 	public void testParseXmlDate() throws Exception {
 		Date date = DateUtils.parseXmlDateTime("2013-12-10");
-		assertEquals(1386630000000L, date.getTime());
+		assertEquals(getCorrectedDate(1386630000000L), date.getTime());
 	}
 
 	@Test
 	public void testParseXmlDateTime() throws Exception {
 		Date date = DateUtils.parseXmlDateTime("2013-12-10T12:41:43");
-		assertEquals(1386675703000L, date.getTime());
+		assertEquals(getCorrectedDate(1386675703000L), date.getTime());
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -101,30 +116,30 @@ public class DateUtilsTest {
 	@Test
 	public void testParseAnyDate1() throws Exception {
 		Date date = DateUtils.parseAnyDate("12-2013-10");
-		assertEquals(1386630000000L, date.getTime());
+		assertEquals(getCorrectedDate(1386630000000L), date.getTime());
 	}
 
 	@Test
 	public void testParseAnyDate2() throws Exception {
 		Date date = DateUtils.parseAnyDate("2013-12-10 12:41:43");
-		assertEquals(1386675703000L, date.getTime());
+		assertEquals(getCorrectedDate(1386675703000L), date.getTime());
 	}
 
 	@Test
 	public void testParseAnyDate3() throws Exception {
 		Date date = DateUtils.parseAnyDate("05/10/98 05:47:13");
-		assertEquals(907559233000L, date.getTime());
+		assertEquals(getCorrectedDate(907559233000L), date.getTime());
 	}
 
 	@Test
 	public void testFormatOptimal() throws Exception {
-		String date = DateUtils.formatOptimal(new Date(1386630000000L));
+		String date = DateUtils.formatOptimal(getCorrectedDate(new Date(1386630000000L)));
 		assertEquals("2013-12-10", date);
 	}
 
 	@Test
 	public void testFormatOptimalWithTime() throws Exception {
-		String date = DateUtils.formatOptimal(new Date(1500000000));
+		String date = DateUtils.formatOptimal(getCorrectedDate(new Date(1500000000)));
 		assertEquals("1970-01-18 09:40", date);
 	}
 
