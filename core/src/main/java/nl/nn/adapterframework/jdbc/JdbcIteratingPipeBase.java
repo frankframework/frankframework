9fbf655a7d92f4513d9c331a9bbd002460bcba64
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-02-01 16:23:36 +0100

    autowire MixedQuerySender in JdbcIteratingPipeBase

--- core/src/main/java/nl/nn/adapterframework/jdbc/JdbcIteratingPipeBase.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/JdbcIteratingPipeBase.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2013 Nationale-Nederlanden, 2020 WeAreFrank!
+   Copyright 2013 Nationale-Nederlanden, 2020, 2021 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -21,7 +21,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+
 import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.configuration.IbisContext;
 import nl.nn.adapterframework.core.HasPhysicalDestination;
 import nl.nn.adapterframework.core.IDataIterator;
 import nl.nn.adapterframework.core.IPipeLineSession;
@@ -73,6 +76,8 @@ public abstract class JdbcIteratingPipeBase extends StringIteratorPipe implement
 	@Override
 	public void configure() throws ConfigurationException {
 		super.configure();
+		IbisContext ibisContext = getAdapter().getConfiguration().getIbisManager().getIbisContext();
+		ibisContext.autowireBeanProperties(querySender, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
 		querySender.setName("source of "+getName());
 		querySender.configure();
 	}
@@ -141,10 +146,6 @@ public abstract class JdbcIteratingPipeBase extends StringIteratorPipe implement
 	@Override
 	public void addParameter(Parameter p) {
 		querySender.addParameter(p);
-	}
-
-	public void setDataSourceFactory(IDataSourceFactory dataSourceFactory) {
-		querySender.setDataSourceFactory(dataSourceFactory);
 	}
 
 	@Override
