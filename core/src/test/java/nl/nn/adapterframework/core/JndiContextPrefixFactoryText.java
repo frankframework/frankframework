d01961bb45fb9b16457473e2d93a3ec566720149
Niels Meijer <nielsmeijer@hotmail.com>
2021-02-11 16:47:11 +0100

    Remove JndiContextPrefix for WAS

---
+++ core/src/test/java/nl/nn/adapterframework/core/JndiContextPrefixFactoryText.java
@@ -0,0 +1,38 @@
+package nl.nn.adapterframework.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nl.nn.adapterframework.util.AppConstants;
+
+public class JndiContextPrefixFactoryText {
+	private enum ApplicationServer {
+		WAS,TOMCAT,JBOSS;
+	}
+
+	@Test
+	public void contextPrefixWAS() {
+		JndiContextPrefixFactory factory = createFactory(ApplicationServer.WAS);
+		assertEquals("", factory.getContextPrefix());
+	}
+
+	@Test
+	public void contextPrefixTomcat() {
+		JndiContextPrefixFactory factory = createFactory(ApplicationServer.TOMCAT);
+		assertEquals("java:comp/env/", factory.getContextPrefix());
+	}
+
+	@Test
+	public void contextPrefixJboss() {
+		JndiContextPrefixFactory factory = createFactory(ApplicationServer.JBOSS);
+		assertEquals("java:/", factory.getContextPrefix());
+	}
+
+	private JndiContextPrefixFactory createFactory(ApplicationServer appl) {
+		AppConstants.getInstance().setProperty(AppConstants.APPLICATION_SERVER_TYPE_PROPERTY, appl.name());
+		JndiContextPrefixFactory factory = new JndiContextPrefixFactory();
+		factory.afterPropertiesSet();
+		return factory;
+	}
+}
