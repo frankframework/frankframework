13120363a182d2336aaf2cb1fc20f90562ab651d
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-02-16 14:56:52 +0100

    Impl #1448 Automatic set default datasource name + call configure() (#1476)

--- core/src/main/java/nl/nn/adapterframework/jdbc/migration/Migrator.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/migration/Migrator.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2017, 2020, 2021 Integration Partners B.V.
+Copyright 2017, 2020, 2021 WeAreFrank!
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -58,10 +58,13 @@ public class Migrator extends JdbcFacade implements AutoCloseable {
 	}
 
 	public synchronized void configure(Configuration configuration, String changeLogFile) throws ConfigurationException {
+		AppConstants appConstants = AppConstants.getInstance(configuration.getClassLoader());
 		setName("JdbcMigrator for configuration["+ configuration.getName() +"]");
+		if(StringUtils.isEmpty(getDatasourceName())) {	
+			setDatasourceName(appConstants.getString("jdbc.migrator.dataSource", null));
+		}
 		super.configure();
 
-		AppConstants appConstants = AppConstants.getInstance(configuration.getClassLoader());
 
 		if(changeLogFile == null)
 			changeLogFile = appConstants.getString("liquibase.changeLogFile", "DatabaseChangelog.xml");
@@ -73,11 +76,6 @@ public class Migrator extends JdbcFacade implements AutoCloseable {
 			log.debug(msg);
 		}
 		else {
-			if(StringUtils.isEmpty(getDatasourceName())) {
-				String dataSource = appConstants.getString("jdbc.migrator.dataSource", appConstants.getResolvedProperty("jdbc.datasource.default"));
-				setDatasourceName(dataSource);
-			}
-
 			try {
 				JdbcConnection connection = new JdbcConnection(getConnection());
 				instance = new LiquibaseImpl(ibisContext, connection, configuration, changeLogFile);
