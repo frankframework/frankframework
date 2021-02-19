ba6bffcb3469c1ebfa1caa0f46b233d084996b7b
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-02-19 13:12:49 +0100

    fix line endings

--- docker/dbms/MsSqlServer/dockerInit.sh
+++ docker/dbms/MsSqlServer/dockerInit.sh
@@ -4,16 +4,16 @@
 #
 
 setup() {
-  #wait for the SQL Server to come up
-  sleep 25s
+#wait for the SQL Server to come up
+sleep 25s
 
-  mkdir /var/opt/mssql/iaf-test-data/
-  chown mssql /var/opt/mssql/iaf-test-data/
-  chgrp mssql /var/opt/mssql/iaf-test-data/
+mkdir /var/opt/mssql/iaf-test-data/
+chown mssql /var/opt/mssql/iaf-test-data/
+chgrp mssql /var/opt/mssql/iaf-test-data/
 
-  echo "running set up script"
-  #run the setup script to create the DB and the schema in the DB
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MssqlPass123 -d master -i /create_user.sql
+echo "running set up script"
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MssqlPass123 -d master -i /create_user.sql
 }
 
 setup &
