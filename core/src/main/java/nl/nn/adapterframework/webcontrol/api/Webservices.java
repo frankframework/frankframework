8adb0fe46b3fe82703680354e4b5a3f64f8ea715
alisihab <sihab.akcan@gmail.com>
2021-02-26 13:51:05 +0100

    Limit eligibility to have wsdl (#1508)

--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/Webservices.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/Webservices.java
@@ -54,7 +54,8 @@ import nl.nn.adapterframework.http.rest.ApiDispatchConfig;
 import nl.nn.adapterframework.http.rest.ApiListener;
 import nl.nn.adapterframework.http.rest.ApiServiceDispatcher;
 import nl.nn.adapterframework.receivers.Receiver;
-import nl.nn.adapterframework.soap.Wsdl;
+import nl.nn.adapterframework.soap.WsdlGenerator;
+import nl.nn.adapterframework.soap.WsdlGeneratorUtils;
 
 /**
  * Shows all monitors.
@@ -95,11 +96,14 @@ public final class Webservices extends Base {
 
 		List<Map<String, Object>> wsdls = new ArrayList<Map<String, Object>>();
 		for (Adapter adapter : getIbisManager().getRegisteredAdapters()) {
-			Map<String, Object> wsdlMap = new HashMap<String, Object>(2);
+			Map<String, Object> wsdlMap = null;
 			try {
-				Wsdl wsdl = new Wsdl(adapter.getPipeLine());
-				wsdlMap.put("name", wsdl.getName());
-				wsdlMap.put("extension", getWsdlExtension());
+				if(WsdlGeneratorUtils.canHaveWsdl(adapter)) { // check eligibility
+					wsdlMap = new HashMap<String, Object>(2);
+					WsdlGenerator wsdl = new WsdlGenerator(adapter.getPipeLine());
+					wsdlMap.put("name", wsdl.getName());
+					wsdlMap.put("extension", getWsdlExtension());
+				}
 			} catch (Exception e) {
 				wsdlMap.put("name", adapter.getName());
 
@@ -109,7 +113,9 @@ public final class Webservices extends Base {
 					wsdlMap.put("error", e.toString());
 				}
 			}
-			wsdls.add(wsdlMap);
+			if(wsdlMap != null) {
+				wsdls.add(wsdlMap);
+			}
 		}
 		returnMap.put("wsdls", wsdls);
 
@@ -180,7 +186,7 @@ public final class Webservices extends Base {
 			// from the adapter itself, or from appconstant wsdl.<adapterName>.location or wsdl.location
 			String servletName = "external address of ibis"; 
 			String generationInfo = "by FrankConsole";
-			Wsdl wsdl = new Wsdl(adapter.getPipeLine(), generationInfo);
+			WsdlGenerator wsdl = new WsdlGenerator(adapter.getPipeLine(), generationInfo);
 			wsdl.setIndent(indent);
 			wsdl.setUseIncludes(useIncludes||zip);
 			wsdl.init();
