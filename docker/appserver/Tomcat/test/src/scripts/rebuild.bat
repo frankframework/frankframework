ce47aa04f07660807b30946a10f9b075efc42d0c
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-03-03 13:29:42 +0100

    Fix project structure + Fix iaf-test FilePipe configuration

--- docker/appserver/Tomcat/test/src/scripts/rebuild.bat
+++ docker/appserver/Tomcat/test/src/scripts/rebuild.bat
@@ -1,7 +1,8 @@
 cd %~dp0/../..
 
-docker-compose down --volumes --remove-orphans
+docker container stop iaf-test-tomcat
+docker container rm iaf-test-tomcat
 
 docker build -t iaf-test-tomcat .
 
-docker-compose up --build
+docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext -e jdbc.dbms.default=h2 --name iaf-test-tomcat iaf-test-tomcat
