6083ff033c49d54a7fc6c4bc9814a39c089b494e
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-03-03 14:58:23 +0100

    Fix start script

--- docker/appserver/WildFly/test/src/scripts/start.bat
+++ docker/appserver/WildFly/test/src/scripts/start.bat
@@ -1,4 +1,4 @@
 cd %~dp0/../..
 docker container stop iaf-test-wildfly
 docker container rm iaf-test-wildfly
-docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext -e jdbc.dbms.default=h2 --name iaf-test-wildfly iaf-test-wildfly
+docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext --name iaf-test-wildfly iaf-test-wildfly
