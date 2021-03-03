4fd2e3493eed2f031e65c7690bc765b13c0f820b
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-03-03 13:55:52 +0100

    simplify script

--- docker/appserver/Tomcat/test/src/scripts/rebuild.bat
+++ docker/appserver/Tomcat/test/src/scripts/rebuild.bat
@@ -5,4 +5,4 @@ docker container rm iaf-test-tomcat
 
 docker build -t iaf-test-tomcat .
 
-docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext -e jdbc.dbms.default=h2 --name iaf-test-tomcat iaf-test-tomcat
+docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext --name iaf-test-tomcat iaf-test-tomcat
