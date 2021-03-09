a7ed29f4d510dad23403bef370541f57b91374a0
alisihab <sihab.akcan@gmail.com>
2021-03-09 15:42:37 +0100

    merge jboss and wildfly modules into one module, configure mssql datasource for testiaf

--- docker/appserver/Jboss-WildFly/test/src/scripts/add_modules.py
+++ docker/appserver/Jboss-WildFly/test/src/scripts/add_modules.py
@@ -1,22 +1,28 @@
 import os
+
+JBOSS_HOME = os.getenv("JBOSS_HOME")
+
 # base path to resources, configurations jars
-path_to_jars='/home/jboss/jboss-eap-6.4/standalone/lib/ext/'
+path_to_jars = JBOSS_HOME+"/standalone/lib/ext/"
+
 # name of the module that war file will depend on
-moduleName='module.frankframework.resources'
+moduleName = "module.frankframework.resources"
+
 # list of jar files
 jarList = os.listdir(path_to_jars)
+
 # resources that the module will be created upon
-resources = ''
-resourceDelimiter=':'
+resources = ""
+resourceDelimiter = ':'
+
 # client executable path
-jboss_cli = '/home/jboss/jboss-eap-6.4/bin/jboss-cli.sh'
+jboss_cli = JBOSS_HOME+"/bin/jboss-cli.sh"
 
 for jarFile in jarList:
 	resources += path_to_jars+jarFile+resourceDelimiter
 
-resources=resources[0:-1]
+resources = resources[0:-1]
 
 command="module add --name="+moduleName+" --resources="+resources
 
 os.system(jboss_cli+" --command='"+command+"'")
-
--- docker/appserver/Jboss/example/src/scripts/start.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0/../..
-docker container stop iaf-example-jboss
-docker container rm iaf-example-jboss
-docker run --publish 80:8080 --name iaf-example-jboss iaf-example-jboss
