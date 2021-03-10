165911f5d2735a6001d29484ea23349fde536576
alisihab <sihab.akcan@gmail.com>
2021-03-10 14:00:23 +0100

    FF! docker image on Jboss (6.4) (#1548)

--- docker/appserver/WildFly/example/src/scripts/start.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0/../..
-docker container stop iaf-example-wildfly
-docker container rm iaf-example-wildfly
-docker run --publish 80:8080 --name iaf-example-wildfly iaf-example-wildfly
--- docker/appserver/WildFly/test/src/scripts/start.bat
+++
@@ -1,4 +0,0 @@
-cd %~dp0/../..
-docker container stop iaf-test-wildfly
-docker container rm iaf-test-wildfly
-docker run --publish 80:8080 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext --name iaf-test-wildfly iaf-test-wildfly
