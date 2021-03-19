4dc19b0f7b55f057516b88bc4fc726499d787d1a
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-03-19 17:11:46 +0100

    Add comments

--- core/src/main/java/nl/nn/adapterframework/configuration/digester/DigesterRulesParser.java
+++ core/src/main/java/nl/nn/adapterframework/configuration/digester/DigesterRulesParser.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2021 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package nl.nn.adapterframework.configuration.digester;
 
 import java.util.HashSet;
@@ -33,7 +48,9 @@ public class DigesterRulesParser extends DigesterRulesHandler {
 		String pattern = rule.getPattern();
 
 		if (parsedPatterns.contains(pattern)) {
-			log.warn("pattern [{}] already parsed", pattern);
+			// Duplicate patterns are used to tell FrankDoc parser about changed multiplicity. 
+			// Original method will still be available to be used by digester, so second instance of rule can be ignored here.
+			log.warn("pattern [{}] already parsed", pattern); 
 			return;
 		}
 		parsedPatterns.add(pattern);
@@ -77,9 +94,8 @@ public class DigesterRulesParser extends DigesterRulesHandler {
 			}
 			if(object instanceof ObjectCreationFactory) {
 				return (ObjectCreationFactory) object;
-			} else {
-				throw new IllegalArgumentException("factory type must implement ObjectCreationFactory");
-			}
+			} 
+			throw new IllegalArgumentException("factory type must implement ObjectCreationFactory");
 		}
 		if(log.isTraceEnabled()) log.trace("no factory specified, returing default ["+GenericFactory.class.getCanonicalName()+"]");
 		return new GenericFactory();
