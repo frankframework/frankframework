f2a557ea068de35aee139baba71f3fdc5f96ad2d
Niels Meijer <nielsmeijer@hotmail.com>
2021-03-31 15:15:00 +0200

    add todo

--- core/src/test/java/nl/nn/adapterframework/configuration/TestConfiguration.java
+++ core/src/test/java/nl/nn/adapterframework/configuration/TestConfiguration.java
@@ -2,6 +2,8 @@ package nl.nn.adapterframework.configuration;
 
 public class TestConfiguration extends Configuration {
 
+	//Configures a standalone configuration.
+	//TODO create a spring configuration with a dummy scheduler
 	public TestConfiguration() {
 		super();
 		setConfigured(true);
