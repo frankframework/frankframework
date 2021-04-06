16a3f10267d554c34fdbeacc4754f7eb9c25f961
Niels Meijer <nielsmeijer@hotmail.com>
2021-04-06 18:15:21 +0200

    refactor a few methods

--- core/src/main/java/nl/nn/adapterframework/configuration/ConfigWarning.java
+++ core/src/main/java/nl/nn/adapterframework/configuration/ConfigWarning.java
@@ -1,56 +1,55 @@
 package nl.nn.adapterframework.configuration;
 
+import java.util.LinkedList;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationEventPublisherAware;
 
 import lombok.Setter;
 import nl.nn.adapterframework.core.IAdapter;
-import nl.nn.adapterframework.core.IConfigurable;
 import nl.nn.adapterframework.core.INamedObject;
-import nl.nn.adapterframework.http.HttpSender;
 import nl.nn.adapterframework.util.AppConstants;
 import nl.nn.adapterframework.util.ClassUtils;
 import nl.nn.adapterframework.util.LogUtil;
 
-public class ConfigWarning implements ApplicationEventPublisherAware, ApplicationContextAware, InitializingBean {
-	private ApplicationEventPublisher applicationEventPublisher;
+public class ConfigWarning implements ApplicationContextAware, InitializingBean {
 	private @Setter ApplicationContext applicationContext;
 	private AppConstants appConstants;
-
-	@Override
-	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-		this.applicationEventPublisher = applicationEventPublisher;
-	}
+	private LinkedList<String> warnings;
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		appConstants = AppConstants.getInstance(applicationContext.getClassLoader());
+		warnings = new LinkedList<>();
 	}
 
+	/**
+	 * Add configuration warning with INamedObject prefix
+	 */
 	public void add(INamedObject source, String message) {
 		add(source, message, null);
-//		ApplicationEvent event = null;
-//		applicationEventPublisher.publishEvent(event);
+	}
+
+	/**
+	 * Add configuration warning with INamedObject prefix and log the exception stack
+	 */
+	public void add(INamedObject source, String message, Throwable t) {
+		addWithNamedObjectPrefix(source, getLogger(source), message, null, t);
+	}
+
+	private Logger getLogger(INamedObject source) {
+		return LogUtil.getLogger(source); //HashTable key lookup
 	}
 
 //	2021-04-04 12:38:19,661 WARN  [localhost-startStop-1] null http.HttpSender - attribute [paramsInUrl] is deprecated: no longer required when using FORMDATA or MTOM requests
 //	2021-04-04 12:38:19,661 WARN  [localhost-startStop-1] null http.HttpSender - HttpSender [null] attribute [paramsInUrl] is deprecated: no longer required when using FORMDATA or MTOM requestsThis warning can be suppressed globally by setting the property 'warnings.suppress.deprecated=true'
 
-	public void add(INamedObject source, String message, SuppressKeys deprecationSuppressKey) {
-		String msg = (source==null?"":ClassUtils.nameOf(source) +" ["+source.getName()+"]")+" "+message;
-		System.out.println(source + " - "+ msg);
-		Logger log = LogUtil.getLogger(source);
-		log.warn(msg);
-	}
-
-	private void add(INamedObject object, String message, Logger log, SuppressKeys suppressionKey, IAdapter adapter) {
+	public void add(INamedObject source, String message, SuppressKeys suppressionKey, IAdapter adapter) {
 		if(!isSuppressed(suppressionKey, adapter)) {
+			Logger log = getLogger(source);
 			// provide suppression hint as info 
 			String hint = null;
 			if(log.isInfoEnabled()) {
@@ -63,14 +62,27 @@ public class ConfigWarning implements ApplicationEventPublisherAware, Applicatio
 					hint = ". This warning can be suppressed globally by setting the property '"+suppressionKey.getKey()+"=true'";
 				}
 			}
-//			add(object, log, message, null, hint);
+			addWithNamedObjectPrefix(source, log, message, hint, null);
 		}
 	}
 
-	private static void add(IConfigurable object, Logger log, String message, Throwable t, String messageSuffixForLog) {
-		
+	private void addWithNamedObjectPrefix(INamedObject source, Logger log, String message, String messageSuffixForLog, Throwable t) {
+		String msg = (source==null?"":ClassUtils.nameOf(source) +" ["+source.getName()+"]")+" "+message;
+		doAdd(log, msg, messageSuffixForLog, t);
 	}
 
+	private void doAdd(Logger log, String msg, String logHint, Throwable t) {
+		String logMsg = StringUtils.isNotEmpty(logHint) ? msg + logHint : msg;
+		if (t == null) {
+			log.warn(logMsg);
+		} else {
+			log.warn(logMsg, t);
+		}
+		boolean onlyOnce = (t==null);
+		if (!onlyOnce || !warnings.contains(msg)) {
+			warnings.add(msg);
+		}
+	}
 
 	public boolean isSuppressed(SuppressKeys key, IAdapter adapter) {
 		if(key == null) {
