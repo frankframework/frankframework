e032e7a4b46aa489e39979012c777c069314c625
Martijn Dirkse <martijn@integrationpartners.nl>
2021-04-12 18:18:28 +0200

    Temporarily add option to ignore javadoc comments in doclet
    Allows comparing doclet output with runtime output.

---
+++ frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/model/JavadocStrategy.java
@@ -0,0 +1,42 @@
+package nl.nn.adapterframework.frankdoc.model;
+
+import nl.nn.adapterframework.frankdoc.doclet.FrankClass;
+
+/**
+ * This class is temporary and it is meant to compare the doclet-produced Frank!Doc with the runtime produced Frank!Doc.
+ * When we are not interested in this comparison anymore, we will remove this class. This moment will come
+ * when we will gather more information from Javadocs instead of IbisDoc and IbisDocRef annotations.
+ * @author martijn
+ *
+ */
+enum JavadocStrategy {
+	IGNORE_JAVADOC(new DelegateIgnoreJavadoc()),
+	USE_JAVADOC(new DelegateUseJavadoc());
+
+	private final Delegate delegate;
+
+	private JavadocStrategy(Delegate delegate) {
+		this.delegate = delegate;
+	}
+
+	void completeFrankElement(FrankElement frankElement, FrankClass frankClass) {
+		delegate.completeFrankElement(frankElement, frankClass);
+	}
+
+	private static abstract class Delegate {
+		abstract void completeFrankElement(FrankElement frankElement, FrankClass frankClass);
+	}
+
+	private static class DelegateUseJavadoc extends Delegate {
+		@Override
+		void completeFrankElement(FrankElement frankElement, FrankClass clazz) {
+			frankElement.setDescription(clazz.getJavaDoc());
+		}
+	}
+
+	private static class DelegateIgnoreJavadoc extends Delegate {
+		@Override
+		void completeFrankElement(FrankElement frankElement, FrankClass clazz) {
+		}		
+	}
+}
