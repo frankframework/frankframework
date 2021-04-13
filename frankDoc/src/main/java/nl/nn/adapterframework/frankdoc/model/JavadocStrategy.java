b2d051193d147dbf8a75be165d2cc018cf03946b
Martijn Dirkse <martijn@integrationpartners.nl>
2021-04-13 20:10:10 +0200

    Fix build error

--- frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/model/JavadocStrategy.java
+++ frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/model/JavadocStrategy.java
@@ -32,7 +32,7 @@ enum JavadocStrategy {
 		void completeFrankElement(FrankElement frankElement, FrankClass clazz) {
 			frankElement.setDescription(clazz.getJavaDoc());
 			if(frankElement.getDescription() != null) {
-				String[] parts = frankElement.getDescription().split("\n\s*\n");
+				String[] parts = frankElement.getDescription().split("\n\\s*\n");
 				frankElement.setDescriptionHeader(parts[0]);
 			}
 		}
