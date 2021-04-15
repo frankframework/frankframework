bcf99e9fb9aea029d1d292e9cd2ed8155f830e45
Ricardo van Holst <57142353+ricardovh@users.noreply.github.com>
2021-04-15 16:50:33 +0200

    First version of the JWTEncodePipe

---
+++ core/src/main/java/nl/nn/adapterframework/jwt/EncoderSigningKeyResolver.java
@@ -0,0 +1,75 @@
+package nl.nn.adapterframework.jwt;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SigningKeyResolverAdapter;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import lombok.Setter;
+import nl.nn.adapterframework.util.CredentialFactory;
+import nl.nn.adapterframework.util.PkiUtil;
+
+public class EncoderSigningKeyResolver extends SigningKeyResolverAdapter {
+	private @Getter @Setter String secret;
+	private @Getter @Setter String authAlias;
+	private @Getter @Setter URL truststoreUrl;
+	private @Getter @Setter String truststoreType="pkcs12";
+	private @Getter @Setter String truststoreAlias;
+	private @Getter @Setter String truststorePassword;
+	private @Getter @Setter String truststoreAuthAlias;
+	private @Getter @Setter String trustManagerAlgorithm;
+	
+	private @Getter SecretKey secretKey;
+	private @Getter PublicKey publicKey;
+	
+	public void configure() {
+		if (secret!=null || authAlias!=null) {
+			// set the secret key used for HMAC requests only if the secret is set;
+			CredentialFactory cf = new CredentialFactory(secret, "", authAlias);
+			String cfSecret = cf.getPassword();
+			secretKey = Keys.hmacShaKeyFor(cfSecret.getBytes(StandardCharsets.UTF_8));
+		}
+		if (getTruststoreUrl()!=null) {
+			try {
+				Certificate certificate;
+				if ("pem".equals(truststoreType)) {
+					certificate = PkiUtil.getCertificateFromPem(getTruststoreUrl());
+				} else {
+					KeyStore keystore = PkiUtil.createKeyStore(getTruststoreUrl(), truststorePassword, truststoreType, "Keys for verifying tokens");
+					certificate = keystore.getCertificate(truststoreAlias);
+				}
+				publicKey = certificate.getPublicKey();
+			} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+				//throw new PipeStartException("cannot get Public Key for verification in keystore ["+keystoreUrl+"]", e);
+			}
+		}
+	}
+	
+	@Override
+	public Key resolveSigningKey(JwsHeader jwsHeader, Claims claims) {
+		//inspect the header or claims, lookup and return the signing key
+		// HMAC algorithms uses secrets, other algorithms use certificates
+		SignatureAlgorithm algorithm = SignatureAlgorithm.forName(jwsHeader.getAlgorithm());
+		
+		if (algorithm.isHmac()) {
+			return secretKey;
+		} else if (algorithm.isRsa() || algorithm.isEllipticCurve()) {
+			return publicKey;
+		}
+		return null;
+	}
+}
---
+++ core/src/main/java/nl/nn/adapterframework/pipes/JWTEncodePipe.java
@@ -0,0 +1,241 @@
+/*
+   Copyright 2021 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package nl.nn.adapterframework.pipes;
+
+import java.util.Date;
+import java.util.StringTokenizer;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.PrematureJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.Getter;
+import lombok.Setter;
+import nl.nn.adapterframework.configuration.ConfigurationException;
+import nl.nn.adapterframework.core.ParameterException;
+import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.core.PipeRunException;
+import nl.nn.adapterframework.core.PipeRunResult;
+import nl.nn.adapterframework.doc.IbisDoc;
+import nl.nn.adapterframework.jwt.EncoderSigningKeyResolver;
+import nl.nn.adapterframework.parameters.ParameterValue;
+import nl.nn.adapterframework.parameters.ParameterValueList;
+import nl.nn.adapterframework.stream.Message;
+import nl.nn.adapterframework.util.ClassUtils;
+
+/**
+ * Pipe that performs JWT encoding, output is the encoded JWT using the selected algorithm.
+ *
+ *
+ * @since   7.7
+ * @author  Ricardo van Holst
+ */
+public class JWTEncodePipe extends FixedForwardPipe {
+
+	private final static String PARAM_ISSUER = "issuer";
+	private final static String PARAM_SUBJECT = "subject";
+	private final static String PARAM_AUDIENCE = "audience";
+	private final static String PARAM_JTI = "jti";
+
+	private @Getter String issuer;
+	private @Getter String subject;
+	private @Getter String audience;
+	private @Getter String jti;
+	private @Getter String customClaimsParams;
+	
+	private @Getter boolean verifyExpiration = true;
+	private @Getter boolean verifyNotBefore = true;
+	private @Getter int allowedClockSkew = 60;
+	
+	private @Getter @Setter EncoderSigningKeyResolver encoderSigningKeyResolver;
+	
+	public JWTEncodePipe() {
+		super();
+		setEncoderSigningKeyResolver(new EncoderSigningKeyResolver());
+	}
+	
+	@Override
+	public void configure() throws ConfigurationException {
+		super.configure();
+		
+		if (getAllowedClockSkew() < 0) {
+			throw new ConfigurationException(getLogPrefix(null)+"allowedClockSkew is set to ["+getAllowedClockSkew()+"], which is not enough for adequate operation");
+		}
+				
+		getEncoderSigningKeyResolver().configure();
+	}
+
+	@Override
+	public PipeRunResult doPipe(Message message, PipeLineSession session) throws PipeRunException {
+		ParameterValueList pvl = null;
+		if (getParameterList() != null) {
+			try {
+				pvl = getParameterList().getValues(message, session);
+			} catch (ParameterException e) {
+				throw new PipeRunException(this, getLogPrefix(session) + "exception on extracting parameters", e);
+			}
+		}
+		
+		String issuer_work = getParameterValue(pvl, PARAM_ISSUER);
+		if (issuer_work==null) {
+			issuer_work = getIssuer();
+		}
+		String subject_work = getParameterValue(pvl, PARAM_SUBJECT);
+		if (subject_work==null) {
+			subject_work = getSubject();
+		}
+		String audience_work = getParameterValue(pvl, PARAM_AUDIENCE);
+		if (audience_work==null) {
+			audience_work = getAudience();
+		}
+		String jti_work = getParameterValue(pvl, PARAM_JTI);
+		if (jti_work==null) {
+			jti_work = getJti();
+		}
+		
+		JwtParserBuilder jwtParserBuilder = Jwts.parserBuilder().setAllowedClockSkewSeconds(getAllowedClockSkew()).setSigningKeyResolver(getEncoderSigningKeyResolver());
+		Date now = new Date();
+		
+		if (issuer_work!=null) {
+			jwtParserBuilder.requireIssuer(issuer_work);
+		}
+		if (subject_work!=null) {
+			jwtParserBuilder.requireSubject(subject_work);
+		}
+		if (audience_work!=null) {
+			jwtParserBuilder.requireAudience(audience_work);
+		}
+		if (jti_work!=null) {
+			jwtParserBuilder.requireId(jti_work);
+		}
+		if (isVerifyExpiration()) {
+			jwtParserBuilder.requireExpiration(now);
+		}
+		if (isVerifyNotBefore()) {
+			jwtParserBuilder.requireNotBefore(now);
+		}
+		if (getCustomClaimsParams() != null) {
+			StringTokenizer st = new StringTokenizer(getCustomClaimsParams(), ",");
+			while (st.hasMoreElements()) {
+				String paramName = st.nextToken();
+				ParameterValue paramValue = pvl.getParameterValue(paramName);
+				if(paramValue != null)
+					jwtParserBuilder.require(paramName, paramValue.asStringValue(null));
+			}
+		}
+		
+		Jwt<?, ?> jwt;
+		
+		try {
+			jwt = jwtParserBuilder.build().parse(message.asString());
+		} catch (SignatureException e) {
+			throw new PipeRunException(this, getLogPrefix(session) + "error validating JWT, invalid signature", e);
+		} catch (ExpiredJwtException e) {
+			throw new PipeRunException(this, getLogPrefix(session) + "error validating JWT, expired token", e);
+		} catch (MalformedJwtException e) {
+			throw new PipeRunException(this, getLogPrefix(session) + "error validating JWT, malformed token", e);
+		} catch (PrematureJwtException e) {
+			throw new PipeRunException(this, getLogPrefix(session) + "error validating JWT, premature token", e);
+		} catch (Exception e) {
+			throw new PipeRunException(this, getLogPrefix(session) + "error validating JWT", e);
+		}
+		
+		return new PipeRunResult(getForward(), jwt.getBody());
+	}
+
+	@IbisDoc({"1", "Issuer to verify the claim in token against when '" + PARAM_ISSUER + "' is not set, verification is skipped if not set", ""})
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+
+	@IbisDoc({"2", "Subject to verify the claim in token against when '" + PARAM_SUBJECT + "' is not set, verification is skipped if not set", ""})
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@IbisDoc({"3", "Audience to verify the claim in the token against when '" + PARAM_AUDIENCE + "' is not set, verification is skipped if not set", ""})
+	public void setAudience(String audience) {
+		this.audience = audience;
+	}
+
+	@IbisDoc({"4", "JWT ID to verify the claim in the token against when '" + PARAM_JTI + "' is not set, verification is skipped if not set", ""})
+	public void setJTI(String jti) {
+		this.jti = jti;
+	}
+
+	@IbisDoc({"5", "Verify if the token has not expired", "true"})
+	public void setVerifyExpiration(boolean verifyExpiration) {
+		this.verifyExpiration = verifyExpiration;
+	}	
+
+	@IbisDoc({"6", "Verify if the token has may be used", "true"})
+	public void setVerifyNotBefore(boolean verifyNotBefore) {
+		this.verifyNotBefore = verifyNotBefore;
+	}
+
+	@IbisDoc({"7", "Allowed clock skew for verifyExpiration and verifyNotBefore in seconds", "60"})
+	public void setAllowedClockSkew(int allowedClockSkew) {
+		this.allowedClockSkew = allowedClockSkew;
+	}
+
+	@IbisDoc({"8", "Comma separated list of parameter names to verify the claims in the token against", ""})
+	public void setCustomClaimsParams(String customClaimsParams) {
+		this.customClaimsParams = customClaimsParams;
+	}
+
+	@IbisDoc({"9", "Secret to use to verify the signature", "" })
+	public void setSecret(String secret) {
+		getEncoderSigningKeyResolver().setSecret(secret);
+	}
+
+	@IbisDoc({"10", "AuthAlias that contains the secret to use to verify the signature", "" })
+	public void setAuthAlias(String authAlias) {
+		getEncoderSigningKeyResolver().setAuthAlias(authAlias);
+	}
+
+	@IbisDoc({"11", "Truststore containing the certificate to use to verify the signature", "" })
+	public void setTruststore(String truststore) {
+		getEncoderSigningKeyResolver().setTruststoreUrl(ClassUtils.getResourceURL(this, truststore));
+	}
+
+	@IbisDoc({"12", "Type of the truststore", "pkcs12" })
+	public void setTruststoreType(String truststoreType) {
+		getEncoderSigningKeyResolver().setTruststoreType(truststoreType);
+	}
+
+	@IbisDoc({"13", "Alias of the certificate to use to verify the signature", "" })
+	public void setTruststoreAlias(String truststoreAlias) {
+		getEncoderSigningKeyResolver().setTruststoreAlias(truststoreAlias);
+	}
+
+	@IbisDoc({"14", "Password of the truststore", "" })
+	public void setTruststorePassword(String truststorePassword) {
+		getEncoderSigningKeyResolver().setTruststorePassword(truststorePassword);
+	}
+
+	@IbisDoc({"15", "AuthAlias containing the password of the truststore", "" })
+	public void setTruststoreAuthAlias(String truststoreAuthAlias) {
+		getEncoderSigningKeyResolver().setTruststoreAuthAlias(truststoreAuthAlias);
+	}
+
+	@IbisDoc({"16", "", "" })
+	public void setTrustManagerAlgorithm(String trustManagerAlgorithm) {
+		getEncoderSigningKeyResolver().setTrustManagerAlgorithm(trustManagerAlgorithm);
+	}
+}
