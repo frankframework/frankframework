d8f0169218384b40c75d3a980f00636d07df9f7d
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-04-21 10:19:45 +0200

    fix iaf-test queryTypes

--- larva/src/main/java/nl/nn/adapterframework/testtool/TestTool.java
+++ larva/src/main/java/nl/nn/adapterframework/testtool/TestTool.java
@@ -1443,7 +1443,7 @@ public class TestTool {
 						FixedQuerySender deleteQuerySender = (FixedQuerySender)ibisContext.createBeanAutowireByName(FixedQuerySender.class);
 						deleteQuerySender.setName("Test Tool pre delete query sender");
 						deleteQuerySender.setDatasourceName(datasourceName);
-						deleteQuerySender.setQueryType("delete");
+						deleteQuerySender.setQueryType("Other");
 						deleteQuerySender.setQuery("delete from " + preDelete);
 						try {
 							deleteQuerySender.configure();				 		
