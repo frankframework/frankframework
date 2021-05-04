b18df88de6c3796651fa5681645e3acb04f28a14
alisihab <sihab.akcan@gmail.com>
2021-05-04 14:14:35 +0200

    change super class

--- core/src/main/java/nl/nn/adapterframework/ldap/LdapChallengePipe.java
+++ core/src/main/java/nl/nn/adapterframework/ldap/LdapChallengePipe.java
@@ -28,7 +28,7 @@ import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
 import nl.nn.adapterframework.doc.IbisDoc;
 import nl.nn.adapterframework.parameters.Parameter;
-import nl.nn.adapterframework.pipes.AbstractPipe;
+import nl.nn.adapterframework.pipes.FixedForwardPipe;
 import nl.nn.adapterframework.stream.Message;
 
 /**
@@ -55,7 +55,7 @@ import nl.nn.adapterframework.stream.Message;
  */
 @Deprecated
 @ConfigurationWarning("please use LdapSender with operation challenge and check for returned message <LdapResult>Success</LdapResult>")
-public class LdapChallengePipe extends AbstractPipe {
+public class LdapChallengePipe extends FixedForwardPipe {
 
 	private String ldapProviderURL=null;
 	private String initialContextFactoryName=null;
@@ -148,7 +148,7 @@ public class LdapChallengePipe extends AbstractPipe {
 			return new PipeRunResult(findForward("invalid"), msg);
 		}
 						
-		return new PipeRunResult(findForward(PipeForward.SUCCESS_FORWARD_NAME), msg);
+		return new PipeRunResult(getSuccessForward(), msg);
 	}
 	
 	protected void handleError(LdapSender ldapSender, PipeLineSession session, int code, String message) {
