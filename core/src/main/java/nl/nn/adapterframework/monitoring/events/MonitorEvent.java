7bb262eef2a2cc4f7723585cad253614e8ec58b1
Niels Meijer <nielsmeijer@hotmail.com>
2021-05-28 16:41:45 +0200

    Use  Spring Events

--- core/src/main/java/nl/nn/adapterframework/monitoring/events/MonitorEvent.java
+++ core/src/main/java/nl/nn/adapterframework/monitoring/events/MonitorEvent.java
@@ -1,14 +1,23 @@
-package nl.nn.adapterframework.monitoring;
+package nl.nn.adapterframework.monitoring.events;
 
 import org.springframework.context.ApplicationEvent;
 
-import nl.nn.adapterframework.core.INamedObject;
+import nl.nn.adapterframework.monitoring.EventThrowing;
 
 public class MonitorEvent extends ApplicationEvent {
-	public String eventCode;
+	private String eventCode;
 
-	public MonitorEvent(INamedObject source, String eventCode) {
+	public MonitorEvent(EventThrowing source, String eventCode) {
 		super(source);
 		this.eventCode = eventCode;
+	}
+
+	@Override
+	public EventThrowing getSource() {
+		return (EventThrowing) super.getSource();
+	}
+
+	public String getEventCode() {
+		return eventCode;
 	}
 }
