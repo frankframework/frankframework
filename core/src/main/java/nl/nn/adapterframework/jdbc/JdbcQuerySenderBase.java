1401483e389867029a4b4fc37081cbd919c9f6cb
alisihab <sihab.akcan@gmail.com>
2021-06-01 20:58:16 +0200

    Trim columnsReturned in FixedQuerySender  (#1871)

--- core/src/main/java/nl/nn/adapterframework/jdbc/JdbcQuerySenderBase.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/JdbcQuerySenderBase.java
@@ -158,7 +158,7 @@ public abstract class JdbcQuerySenderBase<H> extends JdbcSenderBase<H> {
 			List<String> tempList = new ArrayList<String>();
 			StringTokenizer st = new StringTokenizer(getColumnsReturned(),",");
 			while (st.hasMoreTokens()) {
-				String column = st.nextToken();
+				String column = st.nextToken().trim();
 				tempList.add(column);
 			}
 			columnsReturnedList = new String[tempList.size()];
