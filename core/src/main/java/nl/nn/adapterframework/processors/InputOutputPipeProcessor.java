770fae71365e7d7dc004996eeac3e488a621f660
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-06-07 11:21:19 +0200

    Fix #1880 preserve output message that is stored in sessionKey too

--- core/src/main/java/nl/nn/adapterframework/processors/InputOutputPipeProcessor.java
+++ core/src/main/java/nl/nn/adapterframework/processors/InputOutputPipeProcessor.java
@@ -27,8 +27,8 @@ import org.apache.commons.lang3.StringUtils;
 import nl.nn.adapterframework.core.IExtendedPipe;
 import nl.nn.adapterframework.core.INamedObject;
 import nl.nn.adapterframework.core.IPipe;
-import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.PipeLine;
+import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.core.PipeRunResult;
 import nl.nn.adapterframework.pipes.FixedForwardPipe;
@@ -136,6 +136,14 @@ public class InputOutputPipeProcessor extends PipeProcessorBase {
 				if (log.isDebugEnabled()) log.debug("Pipeline of adapter ["+owner.getName()+"] storing result for pipe ["+pe.getName()+"] under sessionKey ["+pe.getStoreResultInSessionKey()+"]");
 				Message result = pipeRunResult.getResult();
 				pipeLineSession.put(pe.getStoreResultInSessionKey(),result.asObject());
+				if (!pe.isPreserveInput() && !result.isRepeatable()) {
+					// when there is a duplicate use of the result (in a sessionKey as well as as the result), then message must be repeatable
+					try {
+						result.preserve();
+					} catch (IOException e) {
+						throw new PipeRunException(pipe, "Pipeline of ["+pipeLine.getOwner().getName()+"] could not preserve output", e);
+					}
+				}
 			}
 			if (pe.isPreserveInput()) {
 				pipeRunResult.getResult().closeOnCloseOf(pipeLineSession);
