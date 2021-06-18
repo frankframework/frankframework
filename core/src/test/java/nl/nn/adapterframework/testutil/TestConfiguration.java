340bacb2fe19661633acee2c4189b3f256d2bec5
Niels Meijer <nielsmeijer@hotmail.com>
2021-06-18 15:51:59 +0200

    fix test

--- core/src/test/java/nl/nn/adapterframework/testutil/TestConfiguration.java
+++ core/src/test/java/nl/nn/adapterframework/testutil/TestConfiguration.java
@@ -1,12 +1,9 @@
 package nl.nn.adapterframework.testutil;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -72,16 +69,6 @@ public class TestConfiguration extends Configuration {
 		if(t.getClass().isInstance(originalBean)) {
 			fail("Unable to mock bean ["+originalBean+"] got ["+t.getClass().getName()+"] instead");
 		}
-	}
-
-	@Test
-	public void testTestConfiguration() {
-		Configuration config = new TestConfiguration(); //Validate it can create/init
-		assertTrue(config.isActive());
-		assertEquals(TEST_CONFIGURATION_NAME, config.getId());
-		config.close();
-		assertFalse(config.isActive());
-		config.close();
 	}
 
 	public void autowireByType(Object bean) {
