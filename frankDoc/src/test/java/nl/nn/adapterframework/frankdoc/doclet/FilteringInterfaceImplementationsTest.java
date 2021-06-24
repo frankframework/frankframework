d45d60890bb43c6f68eba66b73a677588cb20512
Martijn Dirkse <martijn@integrationpartners.nl>
2021-06-24 13:00:21 +0200

    Remove reflection side of wrapper API

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/doclet/FilteringInterfaceImplementationsTest.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/doclet/FilteringInterfaceImplementationsTest.java
@@ -3,9 +3,9 @@ package nl.nn.adapterframework.frankdoc.doclet;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,10 +15,10 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.sun.javadoc.ClassDoc;
+
 @RunWith(Parameterized.class)
 public class FilteringInterfaceImplementationsTest {
-	private static final int NUM_PARAMETERS = 5;
-
 	private static final String PREFIX = "nl.nn.adapterframework.frankdoc.testtarget.doclet.filtering.";
 	private static final String FIRST = "first.";
 	private static final String SECOND = "second.";
@@ -33,47 +33,33 @@ public class FilteringInterfaceImplementationsTest {
 	private static final String[] THIRD_IMPL_EXCLUDED = new String[] {SECOND_PACKAGE + THIRD_IMPL};
 	private static final String[] CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE_EXCLUDED = new String[] {SECOND_PACKAGE + CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE};
 
-	private static Collection<Object[]> cases = Arrays.asList(new Object[][] {
-		{"First package no exclude", asList(FIRST_PACKAGE), asList(NO_EXCLUDES), new String[] {FIRST_IMPL}},
-		{"First package with exclude", asList(FIRST_PACKAGE), asList(THIRD_IMPL_EXCLUDED), new String[] {FIRST_IMPL}},
-		{"Both packages no exclude", asList(BOTH_PACKAGES), asList(NO_EXCLUDES), new String[] {CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE, FIRST_IMPL, SECOND_IMPL, THIRD_IMPL}},
-		{"Both packages with exclude", asList(BOTH_PACKAGES), asList(THIRD_IMPL_EXCLUDED), new String[] {CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE, FIRST_IMPL,SECOND_IMPL}},
-		{"Both packages derived class of implementation excluded", asList(BOTH_PACKAGES), asList(CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE_EXCLUDED), new String[] {FIRST_IMPL, SECOND_IMPL, THIRD_IMPL}}});
-
 	@Parameters(name = "{0}, {1}")
 	public static Collection<Object[]> data() {
-		Collection<Object[]> result = new ArrayList<>();
-		for(Environment environment: Environment.values()) {
-			for(Object[] c: cases) {
-				Object[] row = new Object[NUM_PARAMETERS];
-				row[0] = environment;
-				for(int i = 0; i < 4; ++i) {
-					row[i+1] = c[i];
-				}
-				result.add(row);
-			}
-		}
-		return result;
+		return Arrays.asList(new Object[][] {
+			{"First package no exclude", asList(FIRST_PACKAGE), asList(NO_EXCLUDES), new String[] {FIRST_IMPL}},
+			{"First package with exclude", asList(FIRST_PACKAGE), asList(THIRD_IMPL_EXCLUDED), new String[] {FIRST_IMPL}},
+			{"Both packages no exclude", asList(BOTH_PACKAGES), asList(NO_EXCLUDES), new String[] {CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE, FIRST_IMPL, SECOND_IMPL, THIRD_IMPL}},
+			{"Both packages with exclude", asList(BOTH_PACKAGES), asList(THIRD_IMPL_EXCLUDED), new String[] {CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE, FIRST_IMPL,SECOND_IMPL}},
+			{"Both packages derived class of implementation excluded", asList(BOTH_PACKAGES), asList(CHILD_OF_FIRST_IMPL_IN_SECOND_PACKAGE_EXCLUDED), new String[] {FIRST_IMPL, SECOND_IMPL, THIRD_IMPL}}			
+		});
 	}
 
 	@Parameter(0)
-	public Environment environment;
-
-	@Parameter(1)
 	public String caseName;
 
-	@Parameter(2)
+	@Parameter(1)
 	public List<String> includes;
 
-	@Parameter(3)
+	@Parameter(2)
 	public List<String> excludes;
 
-	@Parameter(4)
+	@Parameter(3)
 	public String[] expectedImplementations;
 
 	@Test
 	public void test() throws FrankDocException {
-		FrankClassRepository repository = environment.getRepository(asList(BOTH_PACKAGES), includes, excludes, new ArrayList<>());
+		ClassDoc[] classDocs = TestUtil.getClassDocs(BOTH_PACKAGES);
+		FrankClassRepository repository = FrankClassRepository.getDocletInstance(classDocs, new HashSet<>(includes), new HashSet<>(excludes), new HashSet<>());
 		FrankClass clazz = repository.findClass(FIRST_PACKAGE + "MyInterface");
 		List<FrankClass> implementations = clazz.getInterfaceImplementations();
 		List<String> actualSimpleNames = implementations.stream()
