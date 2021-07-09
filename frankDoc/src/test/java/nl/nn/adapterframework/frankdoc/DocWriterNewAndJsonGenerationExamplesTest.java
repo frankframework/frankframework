520e955498dfed6f98f874b5ad0abc74519d6add
Martijn Dirkse <martijn@integrationpartners.nl>
2021-07-09 13:47:25 +0200

    Test compatibility.xsd like previous commit

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
@@ -47,7 +47,8 @@ public class DocWriterNewAndJsonGenerationExamplesTest {
 			{XsdVersion.STRICT, "examples-simple-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.deprecated.Master", null, "deprecated.json"},
 			{XsdVersion.STRICT, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.fortype.Start", "compatibility-test-expected-strict.xsd", null},
 			{XsdVersion.COMPATIBILITY, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.fortype.Start", "compatibility-test-expected-compatibility.xsd", null},
-			{XsdVersion.STRICT, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.multiple.Start", "compatibility-multiple-test-expected-strict.xsd", null}
+			{XsdVersion.STRICT, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.multiple.Start", "compatibility-multiple-test-expected-strict.xsd", null},
+			{XsdVersion.COMPATIBILITY, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.multiple.Start", "compatibility-multiple-test-expected-compatibility.xsd", null}
 		});
 	}
 
