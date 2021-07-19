30f734bcdb3f17b10be3b8f3218e0a8431ecaaf6
Martijn Dirkse <mhdirkse@live.nl>
2021-07-19 15:35:29 +0200

    Frank!Doc: Update compatibility.xsd to ignore the sequence of child elements (#1984)

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/DocWriterNewAndJsonGenerationExamplesTest.java
@@ -44,7 +44,11 @@ public class DocWriterNewAndJsonGenerationExamplesTest {
 			{XsdVersion.STRICT, "examples-simple-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.simple.Start", "simple.xsd", "simple.json"},
 			{XsdVersion.COMPATIBILITY, "examples-simple-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.simple.Start", "simpleForCompatibility.xsd", "simple.json"},
 			{XsdVersion.STRICT, "examples-sequence-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.sequence.Master", "sequence.xsd", "sequence.json"},
-			{XsdVersion.STRICT, "examples-simple-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.deprecated.Master", null, "deprecated.json"}
+			{XsdVersion.STRICT, "examples-simple-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.deprecated.Master", null, "deprecated.json"},
+			{XsdVersion.STRICT, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.fortype.Start", "compatibility-test-expected-strict.xsd", null},
+			{XsdVersion.COMPATIBILITY, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.fortype.Start", "compatibility-test-expected-compatibility.xsd", null},
+			{XsdVersion.STRICT, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.multiple.Start", "compatibility-multiple-test-expected-strict.xsd", null},
+			{XsdVersion.COMPATIBILITY, "compatibility-test-digester-rules.xml", "nl.nn.adapterframework.frankdoc.testtarget.examples.compatibility.multiple.Start", "compatibility-multiple-test-expected-compatibility.xsd", null}
 		});
 	}
 
@@ -95,6 +99,7 @@ public class DocWriterNewAndJsonGenerationExamplesTest {
 
 	@Test
 	public void testJson() throws Exception {
+		assumeNotNull(expectedJsonFileName);
 		FrankDocModel model = createModel();
 		FrankDocJsonFactory jsonFactory = new FrankDocJsonFactory(model);
 		JsonObject jsonObject = jsonFactory.getJson();
