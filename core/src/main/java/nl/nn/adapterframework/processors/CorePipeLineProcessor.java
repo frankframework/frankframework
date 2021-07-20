34a991dfec38299dbd2d2365fff543e0a8b51c38
Niels Meijer <nielsmeijer@hotmail.com>
2021-07-20 18:29:18 +0200

    Sigh, legacy code...

--- core/src/main/java/nl/nn/adapterframework/processors/CorePipeLineProcessor.java
+++ core/src/main/java/nl/nn/adapterframework/processors/CorePipeLineProcessor.java
@@ -1,4 +1,5 @@
 /*
+
    Copyright 2013 Nationale-Nederlanden, 2020, 2021 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -210,7 +211,7 @@ public class CorePipeLineProcessor implements PipeLineProcessor, ApplicationCont
 						String state=plExit.getState();
 						pipeLineResult.setState(state);
 						pipeLineResult.setExitCode(plExit.getExitCode());
-						if (!message.isEmpty() && !plExit.getEmptyResult()) {
+						if (message.asObject()!=null && !plExit.getEmptyResult()) { //TODO Replace with Message.isEmpty() once Larva can handle NULL responses...
 							pipeLineResult.setResult(message);
 						} else {
 							pipeLineResult.setResult(Message.nullMessage());
