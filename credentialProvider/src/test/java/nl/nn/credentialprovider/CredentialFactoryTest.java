2b129f86769fb03467da86676f8901cd58b6c4ea
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-07-28 12:07:01 +0200

    Fix test

--- credentialProvider/src/test/java/nl/nn/credentialprovider/CredentialFactoryTest.java
+++ credentialProvider/src/test/java/nl/nn/credentialprovider/CredentialFactoryTest.java
@@ -4,16 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import nl.nn.credentialprovider.util.AppConstants;
-
 public class CredentialFactoryTest {
 
 	@Test
 	public void testFindAliasNoPrefix() {
-		AppConstants ac = AppConstants.getInstance();
-		ac.setProperty("credentialFactory.class", MockMapCredentialFactory.class.getName());
-		ac.setProperty("credentialFactory.optionalPrefix", "fakePrefix:");
-		
+		// test depends on setting credentialFactory.class=nl.nn.credentialprovider.MockMapCredentialFactory in test/resources/credentialprovider.properties
 		ICredentials c = CredentialFactory.getCredentials("account", null, null);
 		assertEquals("fakeUsername", c.getUsername());
 		assertEquals("fakePassword", c.getPassword());
@@ -21,10 +16,6 @@ public class CredentialFactoryTest {
 	
 	@Test
 	public void testFindAliasWithPrefix() {
-		AppConstants ac = AppConstants.getInstance();
-		ac.setProperty("credentialFactory.class", MockMapCredentialFactory.class.getName());
-		ac.setProperty("credentialFactory.optionalPrefix", "fakePrefix:");
-		
 		ICredentials c = CredentialFactory.getCredentials("fakePrefix:account", null, null);
 		assertEquals("fakeUsername", c.getUsername());
 		assertEquals("fakePassword", c.getPassword());
