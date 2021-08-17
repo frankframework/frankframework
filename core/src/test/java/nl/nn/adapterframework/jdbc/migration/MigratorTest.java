e9a926e85cfc4b6c0f7ba4fe27568e48d931eeda
Niels Meijer <nielsmeijer@hotmail.com>
2021-08-17 14:31:49 +0200

    Remove import

--- core/src/test/java/nl/nn/adapterframework/jdbc/migration/MigratorTest.java
+++ core/src/test/java/nl/nn/adapterframework/jdbc/migration/MigratorTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
