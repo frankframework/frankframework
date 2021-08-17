f2dda40efd33a7fd2c7744b8af8aeb7f203a6f6f
Niels Meijer <nielsmeijer@hotmail.com>
2021-08-17 14:31:22 +0200

    Fix migrator test on Oracle

--- core/src/test/java/nl/nn/adapterframework/jdbc/migration/MigratorTest.java
+++ core/src/test/java/nl/nn/adapterframework/jdbc/migration/MigratorTest.java
@@ -1,10 +1,12 @@
 package nl.nn.adapterframework.jdbc.migration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -17,7 +19,7 @@ import nl.nn.adapterframework.util.JdbcUtil;
 import nl.nn.adapterframework.util.MessageKeeper;
 
 public class MigratorTest extends JdbcTestBase {
-	private static TestConfiguration configuration; //Static to sync over all DMBS' tests
+	private TestConfiguration configuration;
 	private Migrator migrator = null;
 	private IbisContext ibisContext = Mockito.spy(new IbisContext());
 
@@ -30,14 +32,16 @@ public class MigratorTest extends JdbcTestBase {
 
 	@After
 	public void tearDown() {
-		migrator.close();
+		if(migrator != null) {
+			migrator.close();
+		}
 	}
 
 	@Override
 	protected void prepareDatabase() throws Exception {
 		//Ignore programmatic creation of Temp table, run Liquibase instead!
 		if (dbmsSupport.isTablePresent(connection, "DUMMYTABLE")) {
-			JdbcUtil.executeStatement(connection, "DROP TABLE DUMMYTABLE");
+			JdbcUtil.executeStatement(connection, "DROP TABLE DUMMYTABLE cascade constraints");
 		}
 		if (dbmsSupport.isTablePresent(connection, "DATABASECHANGELOG")) {
 			JdbcUtil.executeStatement(connection, "DROP TABLE DATABASECHANGELOG");
@@ -54,6 +58,8 @@ public class MigratorTest extends JdbcTestBase {
 
 	@Test
 	public void testSimpleChangelogFile() throws Exception {
+		assertFalse("table [DUMMYTABLE] should not exist prior to the test", dbmsSupport.isTablePresent(connection, "DUMMYTABLE"));
+
 		AppConstants.getInstance().setProperty("liquibase.changeLogFile", "/Migrator/DatabaseChangelog.xml");
 		migrator.configure();
 		migrator.update();
@@ -62,10 +68,13 @@ public class MigratorTest extends JdbcTestBase {
 		assertNotNull("no message logged to the messageKeeper", messageKeeper);
 		assertEquals(1, messageKeeper.size());
 		assertEquals("Configuration [TestConfiguration] LiquiBase applied [2] change(s) and added tag [two:Niels Meijer]", messageKeeper.getMessage(0).getMessageText());
+		assertFalse("table [DUMMYTABLE] should not exist", dbmsSupport.isTablePresent(connection, "DUMMYTABLE"));
 	}
 
 	@Test
 	public void testFaultyChangelogFile() throws Exception {
+		assertFalse("table [DUMMYTABLE] should not exist prior to the test", dbmsSupport.isTablePresent(connection, "DUMMYTABLE"));
+
 		AppConstants.getInstance().setProperty("liquibase.changeLogFile", "/Migrator/DatabaseChangelogError.xml");
 		migrator.configure();
 		migrator.update();
@@ -76,6 +85,7 @@ public class MigratorTest extends JdbcTestBase {
 		String warning = warnings.get(0);
 		assertTrue(warning.contains("TestConfiguration [TestConfiguration] Error running LiquiBase update. Failed to execute [3] change(s)")); //Test ObjectName + Error
 		assertTrue(warning.contains("Migration failed for change set /Migrator/DatabaseChangelogError.xml::error::Niels Meijer")); //Test liquibase exception
-		assertTrue(warning.contains("Table \"DUMMYTABLE\" already exists")); //test tablename
+		//H2 logs 'Table \"DUMMYTABLE\" already exists' Oracle throws 'ORA-00955: name is already used by an existing object'
+		assertTrue("table [DUMMYTABLE] should exist", dbmsSupport.isTablePresent(connection, "DUMMYTABLE"));
 	}
 }
