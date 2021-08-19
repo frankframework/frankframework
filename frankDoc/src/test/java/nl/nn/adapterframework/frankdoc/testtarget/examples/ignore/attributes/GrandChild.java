fe10489f33ab5642ee9675bb1671d4e4a8d28bea
Martijn Dirkse <martijn@integrationpartners.nl>
2021-08-19 16:35:01 +0200

    Extend integration test rejecting attributes

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/ignore/attributes/GrandChild.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/ignore/attributes/GrandChild.java
@@ -1,15 +1,13 @@
 package nl.nn.adapterframework.frankdoc.testtarget.examples.ignore.attributes;
 
-public class Parent implements IChild1 {
+public class GrandChild extends SuppressingChild{
+	// Not re-introduced, technical override
 	@Override
 	public void setSuppressedAttribute(String value) {
 	}
 
+	/** Documented, so re-introduced as attribute */
 	@Override
 	public void setSuppressedAttribute2(String value) {
-	}
-
-	@Override
-	public void setNotSuppressedAttribute(String value) {
 	}
 }
--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/ignore/attributes/IChild1.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/ignore/attributes/IChild1.java
@@ -5,5 +5,6 @@ import nl.nn.adapterframework.doc.FrankDocGroup;
 @FrankDocGroup(name = "Group 1", order = 10)
 public interface IChild1 {
 	public void setSuppressedAttribute(String value);
+	public void setSuppressedAttribute2(String value);
 	public void setNotSuppressedAttribute(String value);
 }
