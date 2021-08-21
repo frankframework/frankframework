e1021ebfcf307fe45e765a5d547d1d4449e6a9c4
Martijn Dirkse <martijn@integrationpartners.nl>
2021-08-21 13:55:30 +0200

    Make AttributeTypeStrategyTest parameterized

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/AttributeTypeStrategyTest.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/AttributeTypeStrategyTest.java
@@ -3,8 +3,13 @@ package nl.nn.adapterframework.frankdoc;
 import static nl.nn.adapterframework.frankdoc.DocWriterNewXmlUtils.addComplexType;
 import static nl.nn.adapterframework.frankdoc.DocWriterNewXmlUtils.addElementWithType;
 import static nl.nn.adapterframework.frankdoc.DocWriterNewXmlUtils.getXmlSchema;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collection;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.sax.SAXSource;
@@ -14,6 +19,10 @@ import javax.xml.validation.Validator;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -26,142 +35,36 @@ import nl.nn.adapterframework.frankdoc.model.FrankAttribute;
 import nl.nn.adapterframework.frankdoc.model.FrankDocModel;
 import nl.nn.adapterframework.util.XmlBuilder;
 
+@RunWith(Parameterized.class)
 public class AttributeTypeStrategyTest {
 	private String schemaString;
 
-	@Before
-	public void setUp() {
-		String packageOfEnum = "nl.nn.adapterframework.frankdoc.testtarget.attribute.type.strategy.";
-		FrankClassRepository classRepository = TestUtil.getFrankClassRepositoryDoclet(packageOfEnum);
-		String digesterRulesFileName = "doc/empty-digester-rules.xml";
-		FrankDocModel model = FrankDocModel.populate(digesterRulesFileName, packageOfEnum + "Container", classRepository);
-		FrankAttribute attribute = model.findFrankElement(packageOfEnum + "Container").getAttributes(ElementChild.ALL).get(0);
-		AttributeEnum attributeEnum = model.findAttributeEnum(packageOfEnum + "Container.TestType");
-		schemaString = getXsd(attributeEnum, attribute);
-		System.out.println(schemaString);
-	}
-
-	@Test
-	public void whenNormalIntegerSuppliedThenAccepted() throws Exception {
-		validate(getIntTestXml("1"));
-	}
-
-	@Test
-	public void whenIntegerWithPlusSuppliedThenAccepted() throws Exception {
-		validate(getIntTestXml("+1"));
-	}
-
-	@Test
-	public void whenNegativeIntegerSuppliedThenAccepted() throws Exception {
-		validate(getIntTestXml("-12"));
-	}
-
-	@Test
-	public void whenRefSuppliedAsIntegerThenAccepted() throws Exception {
-		validate(getIntTestXml("${myVariable}"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenNonIntegerSuppliedThenRejected() throws Exception {
-		validate(getIntTestXml("aString"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenInvalidRefSuppliedAsIntegerThenRejected() throws Exception {
-		validate(getIntTestXml("${myVariable"));
-	}
-
-	@Test
-	public void whenTrueSuppliedAsBooleanThenAccepted() throws Exception {
-		validate(getBoolTestXml("true"));
-	}
-
-	@Test
-	public void whenFalseSuppliedAsBooleanThenAccepted() throws Exception {
-		validate(getBoolTestXml("false"));
-	}
-
-	@Test
-	public void whenRefSuppliedAsBooleanThenAccepted() throws Exception {
-		validate(getBoolTestXml("${myVariable}"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenStringSuppliedAsBooleanThenRejected() throws Exception {
-		validate(getBoolTestXml("aString"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenInvalidRefSuppliedAsBooleanThenRejected() throws Exception {
-		validate(getBoolTestXml("${myVariable"));
-	}
-
-	@Test
-	public void whenAttributeActiveIsLowerCaseTrueThenAccepted() throws Exception {
-		validate(getTestXmlActive("true"));
-	}
-
-	@Test
-	public void whenAttributeActiveIsLowerCaseFalseThenAccepted() throws Exception {
-		validate(getTestXmlActive("false"));		
-	}
-
-	@Test
-	public void whenAttributeActiveIsMixedCaseTrueThenAccepted() throws Exception {
-		validate(getTestXmlActive("True"));
-	}
-
-	@Test
-	public void whenAttributeActiveIsMixedCaseFalseThenAccepted() throws Exception {
-		validate(getTestXmlActive("False"));		
-	}
-
-	@Test
-	public void whenAttributeActiveIsVarRefThenAccepted() throws Exception {
-		validate(getTestXmlActive("${myVar}"));
-	}
-
-	@Test
-	public void whenAttributeActiveIsNegatedLiteralThenAccepted() throws Exception {
-		validate(getTestXmlActive("!true"));
-	}
-
-	@Test
-	public void whenAttributeActiveIsNegatedVarRefThenAccepted() throws Exception {
-		validate(getTestXmlActive("!${myVar}"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenAttributeActiveIsSimpleTextThenRejected() throws Exception {
-		validate(getTestXmlActive("xxx"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenAttributeActiveConcatsMultipleValidElementsThenCombinationRejected() throws Exception {
-		validate(getTestXmlActive("${myVar}true"));
-	}
-
-	@Test
-	public void whenRestrictedAttributeSatisfiesRestrictionThenAccepted() throws Exception {
-		validate(getEnumTestXml("TWO"));
-	}
-
-	@Test(expected = SAXException.class)
-	public void whenRestrictedAttributeDoesNotSatisfyRestrictionThenRejected() throws Exception {
-		validate(getEnumTestXml("xxx"));
-	}
-
-	private static String getXsd(AttributeEnum attributeEnum, FrankAttribute enumTypedAttribute) {
-		XmlBuilder schema = getXmlSchema();
-		XmlBuilder element = addElementWithType(schema, "myElement");
-		XmlBuilder complexType = addComplexType(element);
-		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttribute(complexType, "boolAttr", AttributeType.BOOL);
-		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttribute(complexType, "intAttr", AttributeType.INT);
-		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttributeActive(complexType);
-		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addRestrictedAttribute(complexType, enumTypedAttribute);
-		AttributeTypeStrategy.ALLOW_PROPERTY_REF.createHelperTypes().forEach(h -> schema.addSubElement(h));
-		schema.addSubElement(AttributeTypeStrategy.ALLOW_PROPERTY_REF.createAttributeEnumType(attributeEnum));
-		return schema.toXML(true);
+	@Parameters(name = "{0}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+			{"NormalInteger", getIntTestXml("1"), true},
+			{"IntegerWithPlus", getIntTestXml("+1"), true},
+			{"NegativeInteger", getIntTestXml("-12"), true},
+			{"RefAsInteger", getIntTestXml("${myVariable}"), true},
+			{"NonIntegerForInteger", getIntTestXml("aString"), false},
+			{"InvalidRefForInteger", getIntTestXml("${myVariable"), false},
+			{"True", getBoolTestXml("true"), true},
+			{"False", getBoolTestXml("false"), true},
+			{"RefAsBoolean", getBoolTestXml("${myVariable}"), true},
+			{"StringForBoolean", getBoolTestXml("aString"), false},
+			{"InvalidRefForBoolean", getBoolTestXml("${myVariable"), false},
+			{"AttributeActiveTrueLowerCase", getTestXmlActive("true"), true},
+			{"AttributeActiveFalseLowerCase", getTestXmlActive("false"), true},
+			{"AttributeActiveMixedCaseTrue", getTestXmlActive("True"), true},
+			{"AttributeActiveMixedCaseFalse", getTestXmlActive("False"), true},
+			{"RefAsAttributeActive", getTestXmlActive("${myVar}"), true},
+			{"AttributeActiveNotTrue", getTestXmlActive("!true"), true},
+			{"NegatedRefAsAttributeActive", getTestXmlActive("!${myVar}"), true},
+			{"StringForAttributeActive", getTestXmlActive("xxx"), false},
+			{"AttributeActiveConcatsMultipleValidValues", getTestXmlActive("${myVar}true"), false},
+			{"RestrictedAttribute", getEnumTestXml("TWO"), true},
+			{"InvalidValueRestrictedAttribute", getEnumTestXml("xxx"), false}
+		});
 	}
 
 	private static String getBoolTestXml(String value) {
@@ -180,7 +83,54 @@ public class AttributeTypeStrategyTest {
 		return String.format("<myElement active=\"%s\"/>", value);
 	}
 
-	private void validate(String testXml) throws Exception {
+	@Parameter(0)
+	public String title;
+
+	@Parameter(1)
+	public String testXml;
+
+	@Parameter(2)
+	public boolean allowPropertyRefShouldAccept;
+
+	@Before
+	public void setUp() {
+		String packageOfEnum = "nl.nn.adapterframework.frankdoc.testtarget.attribute.type.strategy.";
+		FrankClassRepository classRepository = TestUtil.getFrankClassRepositoryDoclet(packageOfEnum);
+		String digesterRulesFileName = "doc/empty-digester-rules.xml";
+		FrankDocModel model = FrankDocModel.populate(digesterRulesFileName, packageOfEnum + "Container", classRepository);
+		FrankAttribute attribute = model.findFrankElement(packageOfEnum + "Container").getAttributes(ElementChild.ALL).get(0);
+		AttributeEnum attributeEnum = model.findAttributeEnum(packageOfEnum + "Container.TestType");
+		schemaString = getXsd(attributeEnum, attribute);
+		System.out.println(schemaString);
+	}
+
+	private static String getXsd(AttributeEnum attributeEnum, FrankAttribute enumTypedAttribute) {
+		XmlBuilder schema = getXmlSchema();
+		XmlBuilder element = addElementWithType(schema, "myElement");
+		XmlBuilder complexType = addComplexType(element);
+		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttribute(complexType, "boolAttr", AttributeType.BOOL);
+		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttribute(complexType, "intAttr", AttributeType.INT);
+		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addAttributeActive(complexType);
+		AttributeTypeStrategy.ALLOW_PROPERTY_REF.addRestrictedAttribute(complexType, enumTypedAttribute);
+		AttributeTypeStrategy.ALLOW_PROPERTY_REF.createHelperTypes().forEach(h -> schema.addSubElement(h));
+		schema.addSubElement(AttributeTypeStrategy.ALLOW_PROPERTY_REF.createAttributeEnumType(attributeEnum));
+		return schema.toXML(true);
+	}
+
+	@Test
+	public void testAllowPropertyRef() {
+		boolean actualAccepted = true;
+		try {
+			validate(testXml);
+		} catch(SAXException e) {
+			actualAccepted = false;
+		} catch(IOException e) {
+			fail(String.format("Got IOException: %s - %s", e.getMessage(), e.getStackTrace()));
+		}
+		assertEquals(allowPropertyRefShouldAccept, actualAccepted);
+	}
+
+	private void validate(String testXml) throws SAXException, IOException {
 		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		Schema schema = schemaFactory.newSchema(new SAXSource(new InputSource(new StringReader(schemaString))));
 		Validator validator = schema.newValidator();
