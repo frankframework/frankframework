76a604024dbad19d10b90a7bf5bba73660b11fce
Martijn Dirkse <mhdirkse@live.nl>
2021-08-27 10:46:54 +0200

    Frank!Doc: Show full JavaDoc of each element (#2114)

--- frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/simple/TChild.java
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/examples/simple/TChild.java
@@ -1,7 +1,7 @@
 package nl.nn.adapterframework.frankdoc.testtarget.examples.simple;
 
 /**
- * This is the header of the JavaDoc of "TChild".
+ * This is the header of the JavaDoc of "TChild", with package name nl.nn.something.
  * 
  * And this is remaining documentation of "TChild".
  * @author martijn
