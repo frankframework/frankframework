94d961f0e312510386edf41ef7fb914804ee53e4
Martijn Dirkse <martijn@integrationpartners.nl>
2021-08-30 16:32:46 +0200

    Test conflicting candidates for default of generic element option
    The functionality was there already. We only have to test it.

---
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/plural/config/defaultClassname/Child2.java
@@ -0,0 +1,5 @@
+package nl.nn.adapterframework.frankdoc.testtarget.plural.config.defaultClassname;
+
+public class Child2 implements IChild2 {
+
+}
---
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/plural/config/defaultClassname/Interface1.java
@@ -0,0 +1,5 @@
+package nl.nn.adapterframework.frankdoc.testtarget.plural.config.defaultClassname;
+
+public class Interface1 implements IInterface1 {
+
+}
