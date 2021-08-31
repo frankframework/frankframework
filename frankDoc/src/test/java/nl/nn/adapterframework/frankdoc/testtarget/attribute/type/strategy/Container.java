19d50a311af41727be8bbb59cf3f86883014c0c0
Martijn Dirkse <mhdirkse@live.nl>
2021-08-31 09:25:42 +0200

    Frank!Doc: In compatibility xsd, allow property refs and case-insensitive enum values for attributes (#2141)

---
+++ frankDoc/src/test/java/nl/nn/adapterframework/frankdoc/testtarget/attribute/type/strategy/Container.java
@@ -0,0 +1,15 @@
+package nl.nn.adapterframework.frankdoc.testtarget.attribute.type.strategy;
+
+public class Container {
+	public static enum TestType {
+		ONE,
+		TWO;
+	}
+
+	public void setRestrictedAttribute(String value) {
+	}
+
+	public TestType getRestrictedAttributeEnum() {
+		return null;
+	}
+}
