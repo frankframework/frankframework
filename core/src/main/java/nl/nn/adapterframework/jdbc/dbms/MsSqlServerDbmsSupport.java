08231214cf32a157826556000431bb2862afab0c
Niels Meijer <nielsmeijer@hotmail.com>
2021-09-22 15:31:06 +0200

    Add copyright notice check plugin

--- core/src/main/java/nl/nn/adapterframework/jdbc/dbms/MsSqlServerDbmsSupport.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/dbms/MsSqlServerDbmsSupport.java
@@ -1,7 +1,6 @@
 /*
    Copyright 2013, 2018 Nationale-Nederlanden, 2020, 2021 WeAreFrank!
 
-
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
