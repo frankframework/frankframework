e35914f342f2298426fa355efe0b6e5c2367f82b
Martijn Dirkse <martijn@integrationpartners.nl>
2021-09-23 20:14:01 +0200

    Create element summary

---
+++ frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/FrankDocElementSummaryFactory.java
@@ -0,0 +1,97 @@
+package nl.nn.adapterframework.frankdoc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+import nl.nn.adapterframework.frankdoc.model.FrankDocModel;
+import nl.nn.adapterframework.frankdoc.model.FrankElement;
+
+public class FrankDocElementSummaryFactory {
+	private static final int MAX_NUM_PACKAGE_COMPONENTS_EXPECTED = 100;
+
+	private FrankDocModel model;
+
+	public FrankDocElementSummaryFactory(FrankDocModel model) {
+		this.model = model;
+	}
+
+	public String getText() {
+		Map<String, Integer> numComponentsMap = new HashMap<>();
+		Map<String, List<FrankElement>> elementsBySimpleName = model.getAllElements().values().stream()
+				.collect(Collectors.groupingBy(FrankElement::getSimpleName));
+		for(String simpleName: elementsBySimpleName.keySet()) {
+			List<String> fullNames = elementsBySimpleName.get(simpleName).stream().map(FrankElement::getFullName).collect(Collectors.toList());
+			numComponentsMap.put(simpleName, getNumComponentsForUnique(fullNames));
+		}
+		List<SummaryElement> summaryElements = new ArrayList<>();
+		for(FrankElement frankElement: model.getAllElements().values()) {
+			String simpleName = frankElement.getSimpleName();
+			int numComponents = numComponentsMap.get(simpleName);
+			String label = getAbbreviation(frankElement.getFullName(), numComponents);
+			String xmlElements = frankElement.getXmlElementNames().stream().collect(Collectors.joining(", "));
+			summaryElements.add(new SummaryElement(label, xmlElements));
+		}
+		int maxLabelWidth = summaryElements.stream().map(se -> se.label).map(String::length).collect(Collectors.maxBy(Comparator.naturalOrder())).get();
+		Map<String, SummaryElement> summaryElementsByLabel = summaryElements.stream().collect(Collectors.toMap(se -> se.label, se -> se));
+		List<String> sortedLabels = new ArrayList<>(summaryElementsByLabel.keySet());
+		Collections.sort(sortedLabels);
+		StringBuilder b = new StringBuilder();
+		for(String label: sortedLabels) {
+			SummaryElement e = summaryElementsByLabel.get(label);
+			b.append(String.format("%s: %s\n", StringUtils.leftPad(label, maxLabelWidth), e.xmlElements));
+		}
+		return b.toString();
+	}
+
+	private int getNumComponentsForUnique(List<String> fullNames) {
+		int result = 0;
+		while(result < MAX_NUM_PACKAGE_COMPONENTS_EXPECTED) {
+			final int numComponents = result;
+			int numUnique = (int) fullNames.stream().map(n -> getAbbreviation(n, numComponents)).distinct().collect(Collectors.counting()).longValue();
+			if(numUnique >= fullNames.size()) {
+				return result;
+			}
+			++result;
+		}
+		throw new IllegalArgumentException(String.format("Names are not unique: %s", fullNames.stream().collect(Collectors.joining(", "))));
+	}
+
+	private String getAbbreviation(String fullName, int numPackageComponents) {
+		List<String> components = Arrays.asList(fullName.split("\\."));
+		Collections.reverse(components);
+		int stopAt = 0;
+		while(Character.isUpperCase(components.get(stopAt).charAt(0))) {
+			++stopAt;
+		}
+		stopAt = Math.min(stopAt + numPackageComponents, components.size());
+		List<String> result = new ArrayList<>(components.subList(0, stopAt));
+		String head = result.get(0);
+		List<String> remainder = new ArrayList<>(result.subList(1, result.size()));
+		Collections.reverse(remainder);
+		if(remainder.isEmpty()) {
+			return head;
+		} else {
+			return head + String.format(" (from %s)", remainder.stream().collect(Collectors.joining(", ")));
+		}
+	}
+
+	static class SummaryElement {
+		String label;
+		String xmlElements;
+
+		SummaryElement(String label, String xmlElements) {
+			this.label = label;
+			this.xmlElements = xmlElements;
+		}
+	}
+
+	
+}
