c4cf72e1d1a1ad24f70e383c336211ec33f6cf28
Martijn Dirkse <martijn@integrationpartners.nl>
2021-09-23 17:32:37 +0200

    Calculate common interface hierarchy

--- frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/model/FrankElement.java
+++ frankDoc/src/main/java/nl/nn/adapterframework/frankdoc/model/FrankElement.java
@@ -304,13 +304,12 @@ public class FrankElement implements Comparable<FrankElement> {
 		if(! elementType.isFromJavaInterface()) {
 			return Utils.toUpperCamelCase(roleName);
 		}
-		// Depends on the fact that FrankDocModel.calculateHighestCommonInterfaces()
-		// and FrankDocModel.setHighestCommonInterface() have been executed.
+		// Depends on the fact that FrankDocModel.calculateCommonInterfacesHierarchies() has been executed.
 		//
 		// There is a subtle point here: the difference between ElementType.getHighestCommonInterface()
 		// and ElementRole.getHighestCommonInterface(). Consider for example the ElementRole
 		// (IWrapperPipe, outputWrapper). The highest common interface of ElementType IWrapperPipe is
-		// IPipe. For this reason, Java class ApiSoapWrapper will produce ApiSoapWrapperOutputWrapper
+		// IPipe. For this reason, Java class ApiSoapWrapperPipe will produce ApiSoapWrapperOutputWrapper
 		// as we want, not the erroneous name ApiSoapWrapperPipeOutputWrapper.
 		//
 		// On the other hand, the highest common interface of the mentioned ElementRole is just the same ElementRole.
