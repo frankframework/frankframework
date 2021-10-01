f97989261bead59a4a1a2907bd17295f4350c4bc
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2021-10-01 15:05:59 +0200

    remove unused imports

--- core/src/main/java/nl/nn/adapterframework/util/ClassUtils.java
+++ core/src/main/java/nl/nn/adapterframework/util/ClassUtils.java
@@ -24,7 +24,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
