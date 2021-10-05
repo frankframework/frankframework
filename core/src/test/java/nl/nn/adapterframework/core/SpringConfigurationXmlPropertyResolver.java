5fe64b4eb73e51c1052b755aa0a136de2edb5076
Niels Meijer <nielsmeijer@hotmail.com>
2021-10-05 11:56:39 +0200

    remove springIbisTest

--- core/src/test/java/nl/nn/adapterframework/core/SpringConfigurationXmlPropertyResolver.java
+++ core/src/test/java/nl/nn/adapterframework/core/SpringConfigurationXmlPropertyResolver.java
@@ -2,21 +2,15 @@ package nl.nn.adapterframework.core;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
 import org.junit.Test;
 
 import nl.nn.adapterframework.configuration.IbisContext;
 
 public class SpringConfigurationXmlPropertyResolver {
 
-	@After
-	public void teardown() {
-		System.setProperty("SPRING.CONFIG.LOCATIONS", "SpringApplicationContext.xml");
-	}
-
 	@Test
 	public void testSpringXmlPropertyResolver() {
-		System.setProperty("SPRING.CONFIG.LOCATIONS", "SpringApplicationContext.xml,springIBISTEST.xml");
+		System.setProperty("SPRING.CONFIG.LOCATIONS", "SpringApplicationContext.xml");
 
 		IbisContext context = new IbisContext();
 		context.init(false);
