a082184e0bd2bb215b10ca9d744aea072a61f09e
Martijn Dirkse <martijn@integrationpartners.nl>
2021-10-05 09:49:30 +0200

    Get data about environment in unit tests

---
+++ core/src/test/java/nl/nn/adapterframework/test/EnvironmentTest.java
@@ -0,0 +1,19 @@
+package nl.nn.adapterframework.test;
+
+import javax.xml.parsers.SAXParserFactory;
+
+import org.junit.Test;
+
+public class EnvironmentTest {
+	@Test
+	public void getJRE() {
+		String javaVersion = System.getProperty("java.version");
+		System.out.println(String.format("Java version: %s", javaVersion));
+	}
+
+	@Test
+	public void getSAXImplementation() {
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		System.out.println(String.format("SAXParserFactory implementation: %s", factory.getClass().getName()));
+	}
+}
