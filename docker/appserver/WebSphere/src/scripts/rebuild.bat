0f3f912218a40fc0351773e1b58f6aa28fbaced6
Ricardo <57142353+ricardovh@users.noreply.github.com>
2021-10-07 15:18:23 +0200

    Use same exposed port and dtap.stage for Websphere container

--- docker/appserver/WebSphere/src/scripts/rebuild.bat
+++ docker/appserver/WebSphere/src/scripts/rebuild.bat
@@ -1,7 +1,8 @@
 cd %~dp0/../..
 
-docker-compose down --volumes --remove-orphans
+docker container stop  iaf-test-as-websphere
+docker container rm  iaf-test-as-websphere
 
-docker build -t iaf-test-as-websphere .
+docker build -t  iaf-test-as-websphere .
 
-docker-compose up --build
+docker run --publish 80:9080 --publish 9443:9443 --publish 9043:9043 --mount type=bind,source="%~dp0/../../../../../../test/src/test/testtool",target=/opt/frank/testtool-ext --name iaf-test-as-websphere-test-as-websphere
