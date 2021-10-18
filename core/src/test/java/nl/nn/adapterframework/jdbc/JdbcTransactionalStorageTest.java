1ceb2c1077772ef53bc38d55e62b82917828caa4
alisihab <sihab.akcan@gmail.com>
2021-10-18 13:07:01 +0200

    remove unused imports

--- core/src/test/java/nl/nn/adapterframework/jdbc/JdbcTransactionalStorageTest.java
+++ core/src/test/java/nl/nn/adapterframework/jdbc/JdbcTransactionalStorageTest.java
@@ -13,7 +13,6 @@ import java.sql.Statement;
 import java.util.Date;
 import java.util.zip.DeflaterOutputStream;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
--- core/src/test/java/nl/nn/adapterframework/util/MessageBrowsingFilterTest.java
+++ core/src/test/java/nl/nn/adapterframework/util/MessageBrowsingFilterTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertEquals;
 import java.sql.PreparedStatement;
 import java.util.Date;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
