9c69370e46c12abfa2a6b2ae8efe9cbb66ab7709
Niels Meijer <nielsmeijer@hotmail.com>
2021-10-29 17:14:55 +0200

    Add test for DummyClassLoader

--- core/src/test/java/nl/nn/adapterframework/configuration/IbisContextTest.java
+++ core/src/test/java/nl/nn/adapterframework/configuration/IbisContextTest.java
@@ -7,8 +7,22 @@ import org.junit.Test;
 public class IbisContextTest {
 
 	@Test
-	public void startupShutdown() {
+	public void startupShutdownNoConfigurations() {
 		System.setProperty("configurations.names.application", "");
+
+		IbisContext context = new IbisContext();
+		context.init(false);
+
+		assertEquals("TestConfiguration", context.getApplicationName());
+
+		context.close();
+	}
+
+	@Test
+	public void startupDummyConfiguration() {
+		System.setProperty("configurations.names.application", "Dummy");
+		System.setProperty("configurations.Dummy.classLoaderType", "DummyClassLoader");
+
 		IbisContext context = new IbisContext();
 		context.init(false);
 
