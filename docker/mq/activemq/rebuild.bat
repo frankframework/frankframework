b8ed4c7a1476476bc89292e7aafee5dced337910
Niels Meijer <nielsmeijer@hotmail.com>
2021-10-29 09:09:55 +0200

    Fix docker build command to be run from the correct folder

--- docker/mq/activemq/rebuild.bat
+++ docker/mq/activemq/rebuild.bat
@@ -2,6 +2,6 @@
 docker container stop iaf-test-activemq
 docker container rm iaf-test-activemq
 
-docker build -t iaf-test-activemq .
+docker build -t iaf-test-activemq %~dp0
 
 docker run --publish 8161:8161 --publish 61616:61616 --name iaf-test-activemq iaf-test-activemq
