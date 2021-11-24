b9906169d25a72403f6b7759aa4f13dcd50de6c0
Niels Meijer <nielsmeijer@hotmail.com>
2021-11-24 14:53:55 +0100

    Code style

--- core/src/main/java/nl/nn/adapterframework/util/flow/graphviz/Options.java
+++ core/src/main/java/nl/nn/adapterframework/util/flow/graphviz/Options.java
@@ -19,10 +19,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Options {
-	private static final Pattern
-			FORMAT = Pattern.compile("format:'(.*?)'"),
-			MEMORY = Pattern.compile("totalMemory:'(.*?)'"),
-			Y_INVERT = Pattern.compile("yInvert:(.*?)");
+	private static final Pattern FORMAT = Pattern.compile("format:'(.*?)'");
+	private static final Pattern MEMORY = Pattern.compile("totalMemory:'(.*?)'");
+	private static final Pattern Y_INVERT = Pattern.compile("yInvert:(.*?)");
 
 	private Format format;
 	private Integer totalMemory;
