69150e5fcdda2931bc58d8f20e2945da1dfdd0cb
Ricardo <57142353+ricardovh@users.noreply.github.com>
2021-12-24 12:43:01 +0100

    Intermediate commit, qcf is being created

--- docker/appserver/WebSphere/src/scripts/configureJms.py
+++ docker/appserver/WebSphere/src/scripts/configureJms.py
@@ -4,9 +4,9 @@ server = 'server1'
 
 
 
-AdminJMS.createJMSProvider(node, server, 'ActiveMQ', 'org.apache.activemq.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:5000', 'classpath=/work/drivers/activemq-client.jar')
+AdminJMS.createJMSProvider(node, server, 'ActiveMQ', 'org.apache.activemq.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:5000', 'classpath=/work/drivers/activemq-client.jar;/work/drivers/hawtbuf.jar;/work/drivers/slf4j-api.jar')
 
-AdminJMS.createGenericJMSConnectionFactory(node, server, 'ActiveMQ', 'ActiveMQ', 'jms/qcf-activemq', 'jms/qcf-activemq')
+AdminJMS.createGenericJMSConnectionFactory(node, server, 'ActiveMQ', 'qcf', 'jms/qcf-activemq', 'jms/qcf-activemq', [['connectionPool',[['agedTimeout','100'],['connectionTimeout','1000'],['freePoolDistributionTableSize',10],['maxConnections','12'],['minConnections','5'],['numberOfFreePoolPartitions','3'],['numberOfSharedPoolPartitions','6'],['numberOfUnsharedPoolPartitions','3'],['purgePolicy','EntirePool'],['reapTime','10000'],['surgeCreationInterval','10'],['surgeThreshold','10'],['testConnection','true'],['testConnectionInterval','10'],['unusedTimeout','10000']]]])
 
 p0 = AdminJMS.listJMSProviders()
 print 'JMS Provider templates: ', p0
--- docker/mq/activemq/start.bat
+++ docker/mq/activemq/start.bat
@@ -2,4 +2,4 @@
 docker container stop iaf-test-activemq
 docker container rm iaf-test-activemq
 
-docker run --publish 8161:8161 --publish 61616:61616 --restart unless-stopped --name iaf-test-activemq iaf-test-activemq
+docker run --publish 8161:8161 --publish 5000:61616 --restart unless-stopped --name iaf-test-activemq iaf-test-activemq
