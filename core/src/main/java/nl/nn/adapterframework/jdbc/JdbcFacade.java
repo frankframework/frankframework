cab220f9ac89767b927293bf8ebc4404e3e6ef5d
Niels <nielsmeijer@hotmail.com>
2022-01-03 12:50:45 +0100

    Change jmsRealm deprecation text (#2581)

--- core/src/main/java/nl/nn/adapterframework/jdbc/JdbcFacade.java
+++ core/src/main/java/nl/nn/adapterframework/jdbc/JdbcFacade.java
@@ -202,7 +202,7 @@ public class JdbcFacade extends JndiBase implements HasPhysicalDestination, IXAE
 
 	@Override
 	@Deprecated
-	@ConfigurationWarning("Please use attribute dataSourceName instead")
+	@ConfigurationWarning("We discourage the use of jmsRealms for datasources. To specify a datasource other then the default, use the datasourceName attribute directly, instead of referring to a realm")
 	public void setJmsRealm(String jmsRealmName) {
 		super.setJmsRealm(jmsRealmName); //super.setJmsRealm(...) sets the jmsRealmName only when a realm is found
 		if(StringUtils.isEmpty(getJmsRealmName())) { //confirm that the configured jmsRealm exists
