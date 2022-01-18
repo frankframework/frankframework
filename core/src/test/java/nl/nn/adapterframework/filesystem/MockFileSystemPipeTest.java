205673015969a6c6a828fd7b2002c3a01414759d
Niels Meijer <nielsmeijer@hotmail.com>
2022-01-18 20:25:48 +0100

    Make FileSystemPipe abstract

--- core/src/test/java/nl/nn/adapterframework/filesystem/MockFileSystemPipeTest.java
+++ core/src/test/java/nl/nn/adapterframework/filesystem/MockFileSystemPipeTest.java
@@ -2,7 +2,6 @@ package nl.nn.adapterframework.filesystem;
 
 public class MockFileSystemPipeTest extends FileSystemPipeTest <FileSystemPipe<MockFile, MockFileSystem<MockFile>>, MockFile,MockFileSystem<MockFile>>{
 
-	
 	@Override
 	protected IFileSystemTestHelper getFileSystemTestHelper() {
 		return new MockFileSystemTestHelper<MockFile>();
@@ -10,10 +9,8 @@ public class MockFileSystemPipeTest extends FileSystemPipeTest <FileSystemPipe<M
 
 	@Override
 	public FileSystemPipe<MockFile, MockFileSystem<MockFile>> createFileSystemPipe() {
-		FileSystemPipe<MockFile,MockFileSystem<MockFile>> result=new FileSystemPipe<MockFile,MockFileSystem<MockFile>>();
+		FileSystemPipe<MockFile,MockFileSystem<MockFile>> result = new FileSystemPipe<MockFile,MockFileSystem<MockFile>>() {};
 		result.setFileSystem(((MockFileSystemTestHelper<MockFile>)helper).getFileSystem());
 		return result;
 	}
-
-
 }
