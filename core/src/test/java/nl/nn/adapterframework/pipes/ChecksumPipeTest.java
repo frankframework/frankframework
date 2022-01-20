e297ea4bbe1b22a35fd95324474f97c6fa72375e
alisihab <sihab.akcan@gmail.com>
2022-01-20 16:19:01 +0100

    Add tests for ChecksumPipe (#2643)

--- core/src/test/java/nl/nn/adapterframework/pipes/ChecksumPipeTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/ChecksumPipeTest.java
@@ -1,17 +1,18 @@
 package nl.nn.adapterframework.pipes;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.security.NoSuchAlgorithmException;
+import java.net.URL;
 
 import org.junit.Test;
 
 import nl.nn.adapterframework.core.PipeRunException;
+import nl.nn.adapterframework.core.PipeRunResult;
 import nl.nn.adapterframework.pipes.ChecksumPipe.ChecksumGenerator;
 import nl.nn.adapterframework.pipes.ChecksumPipe.ChecksumType;
 import nl.nn.adapterframework.stream.Message;
-
-
+import nl.nn.adapterframework.testutil.TestFileUtils;
 
 public class ChecksumPipeTest extends PipeTestBase<ChecksumPipe> {
 
@@ -28,7 +29,7 @@ public class ChecksumPipeTest extends PipeTestBase<ChecksumPipe> {
 
 		assertNotNull(result);
 	}
-	
+
 	@Test
 	public void rightChecksumGeneratedSHA() throws Exception {
 		pipe.setType(ChecksumType.SHA);
@@ -70,7 +71,6 @@ public class ChecksumPipeTest extends PipeTestBase<ChecksumPipe> {
 
 	@Test(expected = PipeRunException.class)
 	public void badCharset() throws Exception {
-		pipe.setInputIsFile(false);
 		pipe.setCharset("dummy");
 		configureAndStartPipe();
 		doPipe(pipe,"anotherDummy", session);
@@ -78,10 +78,53 @@ public class ChecksumPipeTest extends PipeTestBase<ChecksumPipe> {
 
 	@Test
 	public void emptyCharset() throws Exception {
-		pipe.setInputIsFile(false);
 		pipe.setCharset("");
 		configureAndStartPipe();
 		assertNotNull(doPipe(pipe,"anotherDummy", session));
+	}
+
+	@Test
+	public void testFileChecksumMD5() throws Exception {
+		String expected = "506c38ca885a4cebb13caad6c265f417";
+		URL file = TestFileUtils.getTestFileURL("/Pipes/2.txt");
+		assertEquals(expected, calculateChecksum(file.getPath(), ChecksumType.MD5, true));
+		assertEquals(expected, calculateChecksum(file.openStream(), ChecksumType.MD5));
+	}
+
+	@Test
+	public void testFileChecksumSHA() throws Exception {
+		String expected = "be1f0d458a058e1113baca693b505f5bf26fd01a";
+		URL file = TestFileUtils.getTestFileURL("/Pipes/2.txt");
+		assertEquals(expected, calculateChecksum(file.getPath(), ChecksumType.SHA, true));
+		assertEquals(expected, calculateChecksum(file.openStream(), ChecksumType.SHA));
+	}
+
+	@Test
+	public void testFileChecksumADLER32() throws Exception {
+		String expected = "48773695";
+		URL file = TestFileUtils.getTestFileURL("/Unzip/ab.zip");
+		assertEquals(expected, calculateChecksum(file.getPath(), ChecksumType.ADLER32, true));
+		assertEquals(expected, calculateChecksum(file.openStream(), ChecksumType.ADLER32));
+	}
+
+	@Test
+	public void testFileChecksumCRC32() throws Exception {
+		String expected = "e9065fc7";
+		URL file = TestFileUtils.getTestFileURL("/Unzip/ab.zip");
+		assertEquals(expected, calculateChecksum(file.getPath(), ChecksumType.CRC32, true));
+		assertEquals(expected, calculateChecksum(file.openStream(), ChecksumType.CRC32));
+	}
+
+	private String calculateChecksum(Object input, ChecksumType type) throws Exception {
+		return calculateChecksum(input, type, false);
+	}
+
+	private String calculateChecksum(Object input, ChecksumType type, boolean isFile) throws Exception {
+		pipe.setInputIsFile(isFile);
+		pipe.setType(type);
+		configureAndStartPipe();
+		PipeRunResult prr = doPipe(pipe, input, session);
+		return prr.getResult().asString();
 	}
 
 }
