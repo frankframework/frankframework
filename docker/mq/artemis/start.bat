79148a62d2241e554a58c87d5a2d8c252b466059
alisihab <sihab.akcan@gmail.com>
2022-02-08 10:45:54 +0100

    Add dockerfile for activemq-artemis

---
+++ docker/mq/artemis/start.bat
@@ -0,0 +1,5 @@
+
+docker container stop iaf-test-activemq-artemis
+docker container rm iaf-test-activemq-artemis
+
+docker run --publish 8161:8161 --publish 61616:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
