053081d8993e9d577de772b95a77f9ecd5b02703
alisihab <sihab.akcan@gmail.com>
2022-02-08 12:38:42 +0100

    change port numbers

--- docker/mq/artemis/rebuild.bat
+++ docker/mq/artemis/rebuild.bat
@@ -4,4 +4,4 @@ docker container rm iaf-test-activemq-artemis
 
 docker build -t iaf-test-activemq-artemis %~dp0
 
-docker run --publish 8161:8161 --publish 61616:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
+docker run --publish 8160:8161 --publish 61615:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
