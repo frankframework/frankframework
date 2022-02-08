5e74ee1e448358f48c24e9fde4e54acb13280f22
alisihab <sihab.akcan@gmail.com>
2022-02-08 11:46:38 +0100

    clean up docker file

--- docker/mq/artemis/docker-run.sh
+++ docker/mq/artemis/docker-run.sh
@@ -27,10 +27,8 @@ BROKER_HOME=/var/lib/
 CONFIG_PATH=$BROKER_HOME/etc
 export BROKER_HOME OVERRIDE_PATH CONFIG_PATH
 
-echo CREATE_ARGUMENTS=${CREATE_ARGUMENTS}
-
 if ! [ -f ./etc/broker.xml ]; then
-	/opt/activemq-artemis/bin/artemis create ${CREATE_ARGUMENTS} .
+	/opt/activemq-artemis/bin/artemis create --http-host 0.0.0.0 --allow-anonymous --silent --relax-jolokia .
 else
 	echo "broker already created, ignoring creation"
 fi
