66ab3c5fca2867d336981ee906191f26a7b9555d
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-02-17 10:18:23 +0100

    remove other case of inappropriate warning too

--- core/src/main/java/nl/nn/adapterframework/configuration/digester/ValidateAttributeRule.java
+++ core/src/main/java/nl/nn/adapterframework/configuration/digester/ValidateAttributeRule.java
@@ -119,9 +119,9 @@ public class ValidateAttributeRule extends DigesterRuleBase {
 	}
 
 	/**
-	 * Check if the value is not empty,
-	 * Can be parsed to match the Getters return type, 
-	 * Equals the default value (parsed by invoking the getter, if present).
+	 * Check if the value:,
+	 * - Can be parsed to match the Getters return type, 
+	 * - Does not equal the default value (parsed by invoking the getter, if present).
 	 * If no Getter is present, tries to match the type to the Setters first argument.
 	 */
 	private void checkTypeCompatibility(PropertyDescriptor pd, String name, String value, Map<String, String> attrs) {
@@ -133,12 +133,8 @@ public class ValidateAttributeRule extends DigesterRuleBase {
 				if (bean instanceof HasSpecialDefaultValues) {
 					defaultValue = ((HasSpecialDefaultValues)bean).getSpecialDefaultValue(name, defaultValue, attrs);
 				}
-				if (defaultValue!=null) {
-					if (value.length()==0) {
-						addLocalWarning("attribute ["+name+"] has no value");
-					} else if (equals(defaultValue, value)) {
-						addLocalWarning("attribute ["+name+"] already has a default value ["+value+"]");
-					}
+				if (defaultValue!=null && equals(defaultValue, value)) {
+					addLocalWarning("attribute ["+name+"] already has a default value ["+value+"]");
 				}
 			} catch (NumberFormatException e) {
 				addLocalWarning("attribute ["+ name+"] with value ["+value+"] cannot be converted to a number: "+e.getMessage());
