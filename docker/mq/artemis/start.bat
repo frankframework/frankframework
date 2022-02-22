5cf872ef34c6f3d98374ce4f5fb1842fc314f154
alisihab <sihab.akcan@gmail.com>
2022-02-22 09:06:36 +0100

    update ports

--- docker/mq/artemis/start.bat
+++ docker/mq/artemis/start.bat
@@ -2,4 +2,4 @@
 docker container stop iaf-test-activemq-artemis
 docker container rm iaf-test-activemq-artemis
 
-docker run --publish 8161:8161 --publish 61616:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
+docker run --publish 8160:8161 --publish 61615:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
