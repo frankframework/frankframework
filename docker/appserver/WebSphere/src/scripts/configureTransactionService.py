805866434aaf9d89736f89c69ff5ba479d22344a
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-02-23 16:53:34 +0100

    Revert "Set AcceptHeuristicHazard in WAS test container for one phase resources in two phase transactions (#2799)"
    This reverts commit d8a42290afa8ba58f08644e94639141d7a149193.

--- docker/appserver/WebSphere/src/scripts/configureTransactionService.py
+++
@@ -1,8 +0,0 @@
-print "Setting TransactionService settings"
-
-transactionService = AdminConfig.list('TransactionService')
-
-# Accept heuristic hazard to allow one phase resources (such as ActiveMQ) to participate in two phase commits
-AdminConfig.modify(transactionService, [['acceptHeuristicHazard', 'true']])
-
-AdminConfig.save()
