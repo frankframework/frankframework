e6bc9af9b79c6368f76dfe458c4571426f69699b
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-03-15 10:47:41 +0100

    Solve MessageStoreListener frankdoc issues

--- core/src/main/java/nl/nn/adapterframework/pipes/MessageSendingPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/MessageSendingPipe.java
@@ -393,7 +393,6 @@ public class MessageSendingPipe extends StreamingPipe implements HasSender, HasS
 		}
 	}
 
-	@IbisDoc({"name of the pipe", ""})
 	@Override
 	public void setName(String name) {
 		super.setName(name);
