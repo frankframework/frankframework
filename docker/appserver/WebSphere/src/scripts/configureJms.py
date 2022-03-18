92b7a7245256b546980b571d7ec77c996294b4b0
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-03-18 11:46:09 +0100

    Bring configureJms.py more in line with configureJdbc.py

--- docker/appserver/WebSphere/src/scripts/configureJms.py
+++ docker/appserver/WebSphere/src/scripts/configureJms.py
@@ -3,23 +3,39 @@ cell = AdminControl.getCell()
 node = AdminControl.getNode()
 server = 'server1'
 
+def createJMSProvider(name, extContextFactory, extProviderURL, attributes):
+	print "Creating JMS Provider: ", name
+	jmsProviderId = AdminJMS.createJMSProvider(node, server, name, extContextFactory, extProviderURL, attributes)
+	return(jmsProviderId)
+	
+def createGenericJMSCF(jmsProviderName, name, jndiName, extJndiName):
+	print "Creating Generic JMS Connection Factory: ", name
+	attributes = [['connectionPool',[['agedTimeout','100'],['connectionTimeout','1000'],['freePoolDistributionTableSize',10],['maxConnections','12'],['minConnections','5'],['numberOfFreePoolPartitions','3'],['numberOfSharedPoolPartitions','6'],['numberOfUnsharedPoolPartitions','3'],['purgePolicy','EntirePool'],['reapTime','10000'],['surgeCreationInterval','10'],['surgeThreshold','10'],['testConnection','true'],['testConnectionInterval','10'],['unusedTimeout','10000']]]]
+	jmsCFId = AdminJMS.createGenericJMSConnectionFactory(node, server, jmsProviderName, name, jndiName, extJndiName, attributes)
+	return(jmsCFId)
+
+def createGenericJMSDestination(jmsProviderName, name, jndiName, extJndiName):
+	print "Creating Generic JMS Destination: ", name
+	jmsDestinationId = AdminJMS.createGenericJMSDestination(node, server, jmsProviderName, name, jndiName, extJndiName)
+	return(jmsDestinationId)
+
 # ActiveMQ
-AdminJMS.createJMSProvider(node, server, 'ActiveMQ', 'org.apache.activemq.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:61616', 'classpath=/work/drivers/activemq-client.jar;/work/drivers/hawtbuf.jar;/work/drivers/slf4j-api.jar')
+createJMSProvider('ActiveMQ', 'org.apache.activemq.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:61616', 'classpath=/work/drivers/activemq-client.jar;/work/drivers/hawtbuf.jar;/work/drivers/slf4j-api.jar')
 
-AdminJMS.createGenericJMSConnectionFactory(node, server, 'ActiveMQ', 'qcf', 'jms/qcf-activemq', 'XAConnectionFactory', [['connectionPool',[['agedTimeout','100'],['connectionTimeout','1000'],['freePoolDistributionTableSize',10],['maxConnections','12'],['minConnections','5'],['numberOfFreePoolPartitions','3'],['numberOfSharedPoolPartitions','6'],['numberOfUnsharedPoolPartitions','3'],['purgePolicy','EntirePool'],['reapTime','10000'],['surgeCreationInterval','10'],['surgeThreshold','10'],['testConnection','true'],['testConnectionInterval','10'],['unusedTimeout','10000']]]])
+createGenericJMSCF('ActiveMQ', 'qcf', 'jms/qcf-activemq', 'XAConnectionFactory')
 
-AdminJMS.createGenericJMSDestination(node, server, 'ActiveMQ', 'i4testiaf_in-activemq', 'jms/i4testiaf_in-activemq', 'dynamicQueues/Q.TEST.IN')
-AdminJMS.createGenericJMSDestination(node, server, 'ActiveMQ', 'i4testiaf_out-activemq', 'jms/i4testiaf_out-activemq', 'dynamicQueues/Q.TEST.OUT')
-AdminJMS.createGenericJMSDestination(node, server, 'ActiveMQ', 'i4testiaf_ff-activemq', 'jms/i4testiaf_ff-activemq', 'dynamicQueues/Q.TEST.FF')
+createGenericJMSDestination('ActiveMQ', 'i4testiaf_in-activemq', 'jms/i4testiaf_in-activemq', 'dynamicQueues/Q.TEST.IN')
+createGenericJMSDestination('ActiveMQ', 'i4testiaf_out-activemq', 'jms/i4testiaf_out-activemq', 'dynamicQueues/Q.TEST.OUT')
+createGenericJMSDestination('ActiveMQ', 'i4testiaf_ff-activemq', 'jms/i4testiaf_ff-activemq', 'dynamicQueues/Q.TEST.FF')
 
 # Artemis
-AdminJMS.createJMSProvider(node, server, 'Artemis', 'org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:61615?type=XA_CF', 'classpath=/work/drivers/artemis-jms-client-all.jar')
+createJMSProvider('Artemis', 'org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory', 'tcp://host.docker.internal:61615?type=XA_CF', 'classpath=/work/drivers/artemis-jms-client-all.jar')
 
-AdminJMS.createGenericJMSConnectionFactory(node, server, 'Artemis', 'qcf', 'jms/qcf-activemq-artemis', 'XAConnectionFactory', [['connectionPool',[['agedTimeout','100'],['connectionTimeout','1000'],['freePoolDistributionTableSize',10],['maxConnections','12'],['minConnections','5'],['numberOfFreePoolPartitions','3'],['numberOfSharedPoolPartitions','6'],['numberOfUnsharedPoolPartitions','3'],['purgePolicy','EntirePool'],['reapTime','10000'],['surgeCreationInterval','10'],['surgeThreshold','10'],['testConnection','true'],['testConnectionInterval','10'],['unusedTimeout','10000']]]])
+createGenericJMSCF('Artemis', 'qcf', 'jms/qcf-activemq-artemis', 'XAConnectionFactory')
 
-AdminJMS.createGenericJMSDestination(node, server, 'Artemis', 'i4testiaf_in-activemq-artemis', 'jms/i4testiaf_in-activemq-artemis', 'dynamicQueues/Q.TEST.IN')
-AdminJMS.createGenericJMSDestination(node, server, 'Artemis', 'i4testiaf_out-activemq-artemis', 'jms/i4testiaf_out-activemq-artemis', 'dynamicQueues/Q.TEST.OUT')
-AdminJMS.createGenericJMSDestination(node, server, 'Artemis', 'i4testiaf_ff-activemq-artemis', 'jms/i4testiaf_ff-activemq-artemis', 'dynamicQueues/Q.TEST.FF')
+createGenericJMSDestination('Artemis', 'i4testiaf_in-activemq-artemis', 'jms/i4testiaf_in-activemq-artemis', 'dynamicQueues/Q.TEST.IN')
+createGenericJMSDestination('Artemis', 'i4testiaf_out-activemq-artemis', 'jms/i4testiaf_out-activemq-artemis', 'dynamicQueues/Q.TEST.OUT')
+createGenericJMSDestination('Artemis', 'i4testiaf_ff-activemq-artemis', 'jms/i4testiaf_ff-activemq-artemis', 'dynamicQueues/Q.TEST.FF')
 
 
 #p0 = AdminJMS.listJMSProviders()
