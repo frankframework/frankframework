002ea2d73deedf4591803cf2ca350272e65de386
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-03-21 10:40:43 +0100

    Handle illegal charset in PartMessage (7.7)

---
+++ core/src/test/java/nl/nn/adapterframework/http/PartMessageTest.java
@@ -0,0 +1,29 @@
+package nl.nn.adapterframework.http;
+
+import static org.junit.Assert.assertNull;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+
+import org.junit.Test;
+
+public class PartMessageTest {
+
+	@Test
+	public void testIllegalCharset() throws MessagingException {
+		
+		TestPart testPart = new TestPart("application/pdf; charset=application/pdf");
+		
+		PartMessage partMessage = new PartMessage(testPart);
+		
+		assertNull(partMessage.getCharset());
+	}
+	
+	private class TestPart extends MimeBodyPart {
+		
+		TestPart(String contentType) {
+			super();
+			headers.addHeader("Content-Type", contentType);
+		}
+	}
+}
