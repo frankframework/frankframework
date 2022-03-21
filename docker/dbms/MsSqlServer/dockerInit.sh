496b123cf3ec396625e3fd862c1c45e65bb2ba62
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-03-21 14:31:36 +0100

    Solve CodeFactor issue

--- docker/dbms/MsSqlServer/dockerInit.sh
+++ docker/dbms/MsSqlServer/dockerInit.sh
@@ -11,8 +11,7 @@ setup() {
 	echo "Running create_user script"
 	for i in {1..120};
 		do
-		    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MssqlPass123 -d master -i /create_user.sql
-		    if [ $? -eq 0 ]
+		    if /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MssqlPass123 -d master -i /create_user.sql
 		    then
 		        echo "create_user completed"
 		        break
