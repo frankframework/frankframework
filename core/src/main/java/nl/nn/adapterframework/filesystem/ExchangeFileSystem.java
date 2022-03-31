e86fcfd5b531c28b87bef9260a356cbdae5949be
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-03-31 17:19:43 +0200

    add comment

--- core/src/main/java/nl/nn/adapterframework/filesystem/ExchangeFileSystem.java
+++ core/src/main/java/nl/nn/adapterframework/filesystem/ExchangeFileSystem.java
@@ -475,7 +475,7 @@ public class ExchangeFileSystem extends MailFileSystemBase<EmailMessage,Attachme
 	public String getName(EmailMessage f) {
 		try {
 			if (f.getId()==null) {
-				return null; // attachments don't have an id
+				return null; // attachments don't have an id, but appear to be loaded at the same time as the main message
 			}
 			return f.getId().toString();
 		} catch (ServiceLocalException e) {
@@ -486,13 +486,14 @@ public class ExchangeFileSystem extends MailFileSystemBase<EmailMessage,Attachme
 	public String getCanonicalName(EmailMessage f) throws FileSystemException {
 		try {
 			if (f.getId()==null) {
-				return null; // attachments don't have an id
+				return null; // attachments don't have an id, but appear to be loaded at the same time as the main message
 			}
 			return f.getId().getUniqueId();
 		} catch (ServiceLocalException e) {
 			throw new FileSystemException("Could not determine CanonicalName",e);
 		}
 	}
+
 	@Override
 	public Date getModificationTime(EmailMessage f) throws FileSystemException {
 		try {
@@ -813,8 +814,8 @@ public class ExchangeFileSystem extends MailFileSystemBase<EmailMessage,Attachme
 	@Override
 	public String getSubject(EmailMessage emailMessage) throws FileSystemException {
 		try {
-			if (emailMessage.getId()!=null) {
-				emailMessage.load(new PropertySet(ItemSchema.Subject));
+			if (emailMessage.getId()!=null) { // attachments don't have an id, but appear to be loaded at the same time as the main message
+				emailMessage.load(new PropertySet(ItemSchema.Subject)); 
 			}
 			return emailMessage.getSubject();
 		} catch (Exception e) {
