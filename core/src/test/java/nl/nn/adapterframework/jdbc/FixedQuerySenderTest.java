bb3db13e2cb5f4ccf9fff07d380436965e32b7bf
Niels Meijer <nielsmeijer@hotmail.com>
2022-04-01 12:02:59 +0200

    Create tests

--- core/src/test/java/nl/nn/adapterframework/jdbc/FixedQuerySenderTest.java
+++ core/src/test/java/nl/nn/adapterframework/jdbc/FixedQuerySenderTest.java
@@ -1,77 +1,50 @@
 package nl.nn.adapterframework.jdbc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.junit.Assume.assumeTrue;
+import static org.junit.Assert.assertThrows;
 
-import java.sql.Connection;
-
-import org.junit.Before;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import nl.nn.adapterframework.core.SenderException;
-import nl.nn.adapterframework.jdbc.dbms.IDbmsSupport;
 import nl.nn.adapterframework.parameters.Parameter;
-import nl.nn.adapterframework.senders.SenderTestBase;
 import nl.nn.adapterframework.stream.Message;
-import nl.nn.adapterframework.util.JdbcUtil;
 
-public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
+public class FixedQuerySenderTest extends JdbcSenderTestBase<FixedQuerySender> {
 
-	private final String resultColumnsReturned = "<result><fielddefinition>"
-			+ "<field name=\"TKEY\" type=\"INTEGER\" columnDisplaySize=\"11\" precision=\"32\" scale=\"0\" isCurrency=\"false\" columnTypeName=\"INTEGER\" columnClassName=\"java.lang.Integer\"/>"
-			+ "<field name=\"TVARCHAR\" type=\"VARCHAR\" columnDisplaySize=\"100\" precision=\"100\" scale=\"0\" isCurrency=\"false\" columnTypeName=\"CHARACTER VARYING\" columnClassName=\"java.lang.String\"/></fielddefinition>"
-			+ "<rowset><row number=\"0\"><"
-			+ "field name=\"TKEY\">1</field>"
-			+ "<field name=\"TVARCHAR\">value</field></row></rowset></result>";
+	private final String resultColumnsReturned = "<result><rowset><row number=\"0\"><field name=\"TKEY\">1</field><field name=\"TVARCHAR\">value</field></row></rowset></result>";
 
 	@Override
 	public FixedQuerySender createSender() throws Exception {
-		return new FixedQuerySender();
+		FixedQuerySender sender = new FixedQuerySender();
+		sender.setIncludeFieldDefinition(false);
+		return sender;
 	}
 
-	@Override
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-
-		sender.setDatasourceName("H2");
-		sender.setQuery("SELECT * FROM "+JdbcTestBase.TEST_TABLE);
-
-		boolean databaseIsPresent = false;
-		try {
-			sender.configure();
-			sender.open();
-			databaseIsPresent = true;
-		} catch (Exception e) {
-			System.out.println("Unable to connect to database ["+sender.getDatasourceName()+"], skipping test!");
+	private void assertSenderException(SenderException ex) {
+		if(getDataSourceName().equals("H2")) {
+			assertThat(ex.getMessage(), CoreMatchers.containsString("Syntax error in SQL statement"));
+		} else if(getDataSourceName().equals("PostgreSQL")) {
+			assertThat(ex.getMessage(), CoreMatchers.containsString("No value specified for parameter 1"));
+		} else if(getDataSourceName().equals("Oracle")) {
+			assertThat(ex.getMessage(), CoreMatchers.containsString("Missing IN or OUT parameter at index:: 1"));
+		} else if(getDataSourceName().equals("MS_SQL")) {
+			assertThat(ex.getMessage(), CoreMatchers.containsString("The value is not set for the parameter number 1"));
+		} else {
+			assertThat(ex.getMessage(), CoreMatchers.containsString("parameter"));
 		}
-		assumeTrue(databaseIsPresent);
+	}
 
-		IDbmsSupport dbmsSupport = sender.getDbmsSupport();
-		Connection connection = sender.getConnection();
+	private void assertColumnsReturned(Message response) throws Exception {
+		String result = response.asString();
 
-		try {
-			JdbcTestBase.dropTableIfPresent(dbmsSupport, connection, JdbcTestBase.TEST_TABLE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-		try {
-			JdbcUtil.executeStatement(connection,
-					"CREATE TABLE "+JdbcTestBase.TEST_TABLE+"(TKEY "+dbmsSupport.getNumericKeyFieldType()+ " PRIMARY KEY, TVARCHAR "+dbmsSupport.getTextFieldType()+"(100), TINT INT, TNUMBER NUMERIC(10,5), " +
-					"TDATE DATE, TDATETIME "+dbmsSupport.getTimestampFieldType()+", TBOOLEAN "+dbmsSupport.getBooleanFieldType()+", "+
-					"TCLOB "+dbmsSupport.getClobFieldType()+", TBLOB "+dbmsSupport.getBlobFieldType()+")");
-			log.warn(JdbcUtil.warningsToString(connection.getWarnings()));
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
+		assertEquals(resultColumnsReturned, result);
 	}
 
 	@Test
 	public void testNamedParametersTrue() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{namedParam1})");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?{namedParam1})");
 		sender.addParameter(new Parameter("namedParam1", "value"));
 		sender.setUseNamedParams(true);
 
@@ -84,7 +57,7 @@ public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
 
 	@Test
 	public void testNamedParameters() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param})");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?{param})");
 		sender.addParameter(new Parameter("param", "value"));
 
 		sender.configure();
@@ -96,7 +69,7 @@ public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
 
 	@Test
 	public void testUseNamedParametersStringValueContains_unp_start() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('3', '?{param}')");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('3', '?{param}')");
 
 		sender.configure();
 		sender.open();
@@ -104,7 +77,7 @@ public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
 		Message result = sendMessage("dummy");
 		assertEquals("<result><rowsupdated>1</rowsupdated></result>", result.asString());
 
-		sender.setQuery("SELECT TVARCHAR FROM "+JdbcTestBase.TEST_TABLE+" WHERE TKEY='3'");
+		sender.setQuery("SELECT tVARCHAR FROM "+JdbcTestBase.TEST_TABLE+" WHERE tKEY='3'");
 		sender.setQueryType("select");
 		sender.setScalar(true);
 
@@ -115,21 +88,19 @@ public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
 
 	@Test
 	public void testUseNamedParametersStringValueContains_unp_start_resolveParam() throws Exception {
-		exception.expect(SenderException.class);
-
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', '?{param}')");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', '?{param}')");
 
 		sender.addParameter(new Parameter("param", "value"));
 
 		sender.configure();
 		sender.open();
 
-		sendMessage("dummy");
+		assertThrows(SenderException.class, () -> sendMessage("dummy"));
 	}
 
 	@Test
 	public void testUseNamedParametersWithoutNamedParam() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', 'text')");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', 'text')");
 		sender.setUseNamedParams(true);
 		sender.configure();
 		sender.open();
@@ -140,78 +111,79 @@ public class FixedQuerySenderTest extends SenderTestBase<FixedQuerySender> {
 
 	@Test
 	public void testUseNamedParametersWithoutParam() throws Exception {
-		exception.expect(SenderException.class);
-		exception.expectMessage("Syntax error in SQL statement \"INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param})[*]\"");
-
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param})");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?{param})");
 		sender.setUseNamedParams(true);
 		sender.configure();
 		sender.open();
 
-		sendMessage("dummy");
+		SenderException ex = assertThrows(SenderException.class, () -> sendMessage("dummy"));
+
+		assertSenderException(ex);
 	}
 
 	@Test
 	public void testNamedParamInQueryFlagFalse() throws Exception {
-		exception.expect(SenderException.class);
-		exception.expectMessage("Syntax error in SQL statement \"INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param})[*]\"");
-
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param})");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?{param})");
 		sender.setUseNamedParams(false);
 		sender.configure();
 		sender.open();
 
-		sendMessage("dummy");
+		SenderException ex = assertThrows(SenderException.class, () -> sendMessage("dummy"));
+
+		assertSenderException(ex);
 	}
 
 	@Test
-	public void testInCompleteNamedParamInQuery() throws Exception {
-		exception.expect(SenderException.class);
-		exception.expectMessage("Syntax error in SQL statement \"INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param)[*]\"");
-
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?{param)");
+	public void testIncompleteNamedParamInQuery() throws Exception {
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?{param)");
 		sender.configure();
 		sender.open();
 
-		sendMessage("dummy");
+		SenderException ex = assertThrows(SenderException.class, () -> sendMessage("dummy") );
+
+		assertSenderException(ex);
 	}
+
 	@Test
 	public void testColumnsReturnedWithSpaceBetween() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?)");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?)");
 		sender.addParameter(new Parameter("param1", "value"));
 
-		sender.setColumnsReturned("TKEY, TVARCHAR");
+		sender.setColumnsReturned("tKEY, tVARCHAR");
 
 		sender.configure();
 		sender.open();
+
 		Message result=sendMessage("dummy");
-		assertEquals(resultColumnsReturned, result.asString());
+		assertColumnsReturned(result);
 	}
 
 	@Test
 	public void testColumnsReturnedWithDoubleSpace() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?)");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?)");
 		sender.addParameter(new Parameter("param1", "value"));
 
-		sender.setColumnsReturned("  TKEY,  TVARCHAR  ");
+		sender.setColumnsReturned("  tKEY,  tVARCHAR  ");
 
 		sender.configure();
 		sender.open();
+
 		Message result=sendMessage("dummy");
-		assertEquals(resultColumnsReturned, result.asString());
+		assertColumnsReturned(result);
 	}
 
 	@Test
 	public void testColumnsReturned() throws Exception {
-		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (TKEY, TVARCHAR) VALUES ('1', ?)");
+		sender.setQuery("INSERT INTO "+JdbcTestBase.TEST_TABLE+" (tKEY, tVARCHAR) VALUES ('1', ?)");
 		sender.addParameter(new Parameter("param1", "value"));
 
-		sender.setColumnsReturned("TKEY,TVARCHAR");
+		sender.setColumnsReturned("tKEY,tVARCHAR");
 
 		sender.configure();
 		sender.open();
+
 		Message result=sendMessage("dummy");
-		assertEquals(resultColumnsReturned, result.asString());
+		assertColumnsReturned(result);
 	}
 
 }
