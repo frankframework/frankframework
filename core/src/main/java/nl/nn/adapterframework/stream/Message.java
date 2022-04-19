a3843cf6f2fee6ab1b79dc23d12a5cc1fcb86314
Niels <nielsmeijer@hotmail.com>
2022-04-19 16:01:41 +0200

    Fix Parameter Message.asObject (#3074)

--- core/src/main/java/nl/nn/adapterframework/stream/Message.java
+++ core/src/main/java/nl/nn/adapterframework/stream/Message.java
@@ -159,7 +159,11 @@ public class Message implements Serializable {
 	 */
 	@Deprecated
 	public Object asObject() {
-		return request;
+		try {
+			return (request instanceof ThrowingSupplier) ? asInputStream() : request;
+		} catch (IOException e) {
+			throw Lombok.sneakyThrow(e);
+		}
 	}
 
 	public boolean isBinary() {
--- core/src/test/java/nl/nn/adapterframework/http/PartMessageTest.java
+++ core/src/test/java/nl/nn/adapterframework/http/PartMessageTest.java
@@ -1,11 +1,24 @@
 package nl.nn.adapterframework.http;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeBodyPart;
 
 import org.junit.Test;
+
+import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.parameters.Parameter;
+import nl.nn.adapterframework.parameters.ParameterValueList;
+import nl.nn.adapterframework.stream.Message;
+import nl.nn.adapterframework.testutil.TestFileUtils;
+import nl.nn.credentialprovider.util.Misc;
 
 public class PartMessageTest {
 
@@ -18,12 +31,44 @@ public class PartMessageTest {
 		
 		assertNull(partMessage.getCharset());
 	}
-	
+
 	private class TestPart extends MimeBodyPart {
-		
-		TestPart(String contentType) {
+		private InputStream stream = null;//non-repeatable
+
+		public TestPart(String contentType) {
 			super();
 			headers.addHeader("Content-Type", contentType);
 		}
+
+		public TestPart(URL url) throws IOException {
+			this.stream = url.openStream();
+			headers.addHeader("Content-Type", "application/octet-stream");
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException, MessagingException {
+			return stream;
+		}
+	}
+
+	@Test
+	public void testParameterValue() throws Exception {
+		String sessionKey = "file";
+
+		Parameter p = new Parameter();
+		p.setName("contents");
+		p.setSessionKey(sessionKey);
+		p.configure();
+
+		PipeLineSession session = new PipeLineSession();
+		TestPart testPart = new TestPart(TestFileUtils.getTestFileURL("/file.xml"));
+		session.put(sessionKey, new PartMessage(testPart));
+
+		ParameterValueList pvl = new ParameterValueList();
+		Message message = new Message("fakeMessage");
+
+		Object value = p.getValue(pvl, message, session, false);
+		assertTrue(value instanceof InputStream);
+		assertEquals("<file>in root of classpath</file>", Misc.streamToString((InputStream) value));
 	}
 }
