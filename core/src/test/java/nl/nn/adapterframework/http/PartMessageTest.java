faa17660a02649df9973766daa939f44a06f1d85
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-04-20 17:27:21 +0200

    restore test

--- core/src/test/java/nl/nn/adapterframework/http/PartMessageTest.java
+++ core/src/test/java/nl/nn/adapterframework/http/PartMessageTest.java
@@ -25,6 +25,7 @@ import nl.nn.adapterframework.stream.MessageTest;
 import nl.nn.adapterframework.testutil.SerializationTester;
 import nl.nn.adapterframework.testutil.TestFileUtils;
 import nl.nn.adapterframework.util.LogUtil;
+import nl.nn.adapterframework.util.Misc;
 
 public class PartMessageTest {
 	protected Logger log = LogUtil.getLogger(this);
@@ -94,8 +95,8 @@ public class PartMessageTest {
 		Message message = new Message("fakeMessage");
 
 		Object value = p.getValue(pvl, message, session, false);
-		assertTrue(value instanceof Message);
-		assertEquals("<file>in root of classpath</file>", ((Message)value).asString());
+		assertTrue(value instanceof InputStream);
+		assertEquals("<file>in root of classpath</file>", Misc.streamToString((InputStream) value));
 	}
 
 	@Test
