frankframework/frankframework

commit 1f266c24f6049a83a35220bc50f7e91b0a6e2d68

Imp #1215 soap wrapper using message (#1339)

core/src/main/java/nl/nn/adapterframework/http/PushingListenerAdapter.java
@@ -15,7 +15,6 @@
 */
 package nl.nn.adapterframework.http;
 
-import java.io.IOException;
 import java.util.Map;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -40,14 +39,13 @@ import nl.nn.adapterframework.util.LogUtil;
  * @author  Gerrit van Brakel 
  * @since   4.12
  */
-public class PushingListenerAdapter implements IPushingListener<String>, ServiceClient {
+public class PushingListenerAdapter implements IPushingListener<Message>, ServiceClient {
 	protected Logger log = LogUtil.getLogger(this);
 	private @Getter ClassLoader configurationClassLoader = Thread.currentThread().getContextClassLoader();
 
-	private IMessageHandler<String> handler;
+	private IMessageHandler<Message> handler;
 	private String name;
 	private boolean applicationFaultsAsExceptions=true;
-//	private IbisExceptionListener exceptionListener;
 	private boolean running;
 
 	/**
@@ -71,12 +69,12 @@ public class PushingListenerAdapter implements IPushingListener<String>, Service
 
 
 	@Override
-	public String getIdFromRawMessage(String rawMessage, Map<String, Object> threadContext) {
+	public String getIdFromRawMessage(Message rawMessage, Map<String, Object> threadContext) {
 		return null;
 	}
 	@Override
-	public Message extractMessage(String rawMessage, Map<String, Object> threadContext) {
-		return Message.asMessage(rawMessage);
+	public Message extractMessage(Message rawMessage, Map<String, Object> threadContext) {
+		return rawMessage;
 	}
 	@Override
 	public void afterMessageProcessed(PipeLineResult processResult, Object rawMessageOrWrapper, Map<String, Object> threadContext) throws ListenerException {
@@ -84,22 +82,18 @@ public class PushingListenerAdapter implements IPushingListener<String>, Service
 	}
 
 	@Override
-	public String processRequest(String correlationId, String rawMessage, Map<String, Object> requestContext) throws ListenerException {
+	public Message processRequest(String correlationId, Message rawMessage, Map<String, Object> requestContext) throws ListenerException {
 		Message message = extractMessage(rawMessage, requestContext);
 		try {
 			log.debug("PushingListenerAdapter.processRequerawMmessagest() for correlationId ["+correlationId+"]");
-			try {
-				return handler.processRequest(this, correlationId, rawMessage, message, requestContext).asString();
-			} catch (IOException e) {
-				throw new ListenerException(e);
-			} 
+			return handler.processRequest(this, correlationId, rawMessage, message, requestContext);
 		} catch (ListenerException e) {
 			if (isApplicationFaultsAsExceptions()) {
 				log.debug("PushingListenerAdapter.processRequest() rethrows ListenerException...");
 				throw e;
 			} 
 			log.debug("PushingListenerAdapter.processRequest() formats ListenerException to errormessage");
-			return handler.formatException(null,correlationId, message, e);
+			return new Message(handler.formatException(null,correlationId, message, e));
 		}
 	}
 
@@ -121,7 +115,7 @@ public class PushingListenerAdapter implements IPushingListener<String>, Service
 	}
 
 	@Override
-	public void setHandler(IMessageHandler<String> handler) {
+	public void setHandler(IMessageHandler<Message> handler) {
 		this.handler=handler;
 	}
 	@Override

core/src/main/java/nl/nn/adapterframework/http/RestListener.java
@@ -128,7 +128,11 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 			if(getProduces().equalsIgnoreCase("TEXT"))
 				requestContext.put("contentType", "text/plain");
 
-			response = super.processRequest(correlationId, message, requestContext);
+			try {
+				response = super.processRequest(correlationId, new Message(message), requestContext).asString();
+			} catch (IOException e) {
+				throw new ListenerException("Failed to read result", e);
+			}
 			if(response != null && !response.isEmpty())
 				eTag = response.hashCode();
 
@@ -141,7 +145,11 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 			}
 		}
 		else {
-			response = super.processRequest(correlationId, message, requestContext);
+			try {
+				response = super.processRequest(correlationId, new Message(message), requestContext).asString();
+			} catch (IOException e) {
+				throw new ListenerException("Failed to read result", e);
+			}
 			if(response != null && !response.isEmpty())
 				eTag = response.hashCode();
 		}

core/src/main/java/nl/nn/adapterframework/http/RestServiceDispatcher.java
@@ -305,8 +305,12 @@ public class RestServiceDispatcher  {
 				}
 			}
 
-			String result=listener.processRequest(null, request, context);
-
+			String result;
+			try {
+				result=listener.processRequest(null, new Message(request), context).asString();
+			} catch (IOException e) {
+				throw new ListenerException(e);
+			}
 			//Caching: pipeline has been processed, save etag
 			if(result != null && cache != null && context.containsKey("etag")) { //In case the eTag has manually been set and the pipeline exited in error state...
 				cache.put(etagCacheKey, context.get("etag"));

core/src/main/java/nl/nn/adapterframework/http/WebServiceListener.java
@@ -40,6 +40,7 @@ import nl.nn.adapterframework.doc.IbisDoc;
 import nl.nn.adapterframework.http.cxf.MessageProvider;
 import nl.nn.adapterframework.receivers.ServiceDispatcher;
 import nl.nn.adapterframework.soap.SoapWrapper;
+import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.util.XmlBuilder;
 
 /**
@@ -148,7 +149,7 @@ public class WebServiceListener extends PushingListenerAdapter implements HasPhy
 	}
 
 	@Override
-	public String processRequest(String correlationId, String message, Map<String, Object> requestContext) throws ListenerException {
+	public Message processRequest(String correlationId, Message message, Map<String, Object> requestContext) throws ListenerException {
 		if (!attachmentSessionKeysList.isEmpty()) {
 			XmlBuilder xmlMultipart = new XmlBuilder("parts");
 			for(String attachmentSessionKey: attachmentSessionKeysList) {
@@ -164,17 +165,17 @@ public class WebServiceListener extends PushingListenerAdapter implements HasPhy
 
 		if (isSoap()) {
 			try {
-				log.debug(getLogPrefix()+"received SOAPMSG [" + message + "]");
-				String request = soapWrapper.getBody(message);
-				String result = super.processRequest(correlationId, request, requestContext);
+				if (log.isDebugEnabled()) log.debug(getLogPrefix()+"received SOAPMSG [" + message + "]");
+				Message request = soapWrapper.getBody(message);
+				Message result = super.processRequest(correlationId, request, requestContext);
 
 				String soapNamespace = SOAPConstants.URI_NS_SOAP_1_1_ENVELOPE;
 				String soapProtocol = (String) requestContext.get("soapProtocol");
-				if(SOAPConstants.SOAP_1_2_PROTOCOL.equals(soapProtocol))
+				if(SOAPConstants.SOAP_1_2_PROTOCOL.equals(soapProtocol)) {
 					soapNamespace = SOAPConstants.URI_NS_SOAP_1_2_ENVELOPE;
-
-				String reply = soapWrapper.putInEnvelope(result, null, null, null, null, soapNamespace, null, false);
-				log.debug(getLogPrefix()+"replied SOAPMSG [" + reply + "]");
+				}
+				Message reply = soapWrapper.putInEnvelope(result, null, null, null, null, soapNamespace, null, false);
+				if (log.isDebugEnabled()) log.debug(getLogPrefix()+"replied SOAPMSG [" + reply + "]");
 				return reply;
 			} catch (Exception e) {
 				throw new ListenerException(e);

cmis/src/main/java/nl/nn/adapterframework/extensions/cmis/server/CmisEventDispatcher.java
@@ -15,6 +15,7 @@
  */
 package nl.nn.adapterframework.extensions.cmis.server;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +33,7 @@ import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.extensions.cmis.CmisEventListener;
 import nl.nn.adapterframework.extensions.cmis.CmisUtils;
 import nl.nn.adapterframework.receivers.JavaListener;
+import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.util.AppConstants;
 import nl.nn.adapterframework.util.DomBuilderException;
 import nl.nn.adapterframework.util.LogUtil;
@@ -86,7 +88,7 @@ public class CmisEventDispatcher {
 		try {
 			messageContext.put("CmisEvent", event.name());
 			CmisEventListener listener = eventListeners.get(event);
-			String result = listener.processRequest(null, message, messageContext);
+			String result = listener.processRequest(null, new Message(message), messageContext).asString();
 			if(StringUtils.isEmpty(result))
 				return XmlUtils.buildElement("<cmis/>");
 			else if (XmlUtils.isWellFormed(result, "cmis")) {
@@ -95,7 +97,10 @@ public class CmisEventDispatcher {
 			else {
 				throw new CmisRuntimeException("invalid or unparsable result");
 			}
+		} catch (IOException e) {
+			throw new CmisRuntimeException("invalid or unparsable result", e);
 		}
+
 		//Try and catch the original CMIS exception and throw that instead
 		catch (ListenerException e) {
 			if(e.getCause() instanceof PipeRunException) {

core/src/main/java/nl/nn/adapterframework/core/ITransactionalStorage.java
@@ -64,7 +64,7 @@ public interface ITransactionalStorage<S extends Serializable> extends IMessageB
 	public void setSlotId(String string);
 
 
-	@IbisDoc({"Possible values are E (error store), M (message store), L (message log for pipe) or A (message log for receiver). Receiver will always set type to E for errorStorage and always set type to A for messageLog. GenericMessageSendingPipe will set type to L for messageLog (when type isn't specified). See {@link MessagestoreSender} for type M", "E for errorstorage on receiver, A for messageLog on receiver and L for messageLog on Pipe"})
+	@IbisDoc({"Possible values are E (error store), M (message store), L (message log for pipe) or A (message log for receiver). Receiver will always set type to E for errorStorage and always set type to A for messageLog. SenderPipe will set type to L for messageLog (when type isn't specified). See {@link MessagestoreSender} for type M", "E for errorstorage on receiver, A for messageLog on receiver and L for messageLog on Pipe"})
 	public void setType(String string);
 	public String getType();
 	

core/src/main/java/nl/nn/adapterframework/errormessageformatters/SoapErrorMessage.java
@@ -30,7 +30,7 @@ public class SoapErrorMessage extends ErrorMessageFormatter {
 	public String format(String errorMessage, Throwable t, INamedObject location, Message originalMessage, String messageId, long receivedTime) {
 
 		try {
-			return SoapWrapper.getInstance().createSoapFaultMessage(getErrorMessage(errorMessage, t));
+			return SoapWrapper.getInstance().createSoapFaultMessage(getErrorMessage(errorMessage, t)).asString();
 		} catch (Exception e) {
 			log.error("got error getting soapWrapper instance", e);
 			return super.format(errorMessage, t, location, originalMessage, messageId, receivedTime);

core/src/main/java/nl/nn/adapterframework/extensions/bis/BisJmsListener.java
@@ -188,10 +188,10 @@ public class BisJmsListener extends JmsListener {
 	}
 
 	@Override
-	public String extractMessageBody(String rawMessageText, Map<String,Object> context, SoapWrapper soapWrapper) throws SAXException, TransformerException, IOException {
-		context.put(MESSAGETEXT_KEY, rawMessageText);
-		log.debug("extract messageBody from message [" + rawMessageText + "]");
-		String messageBody = requestTp.transform(rawMessageText, null, true);
+	public Message extractMessageBody(Message message, Map<String,Object> context, SoapWrapper soapWrapper) throws SAXException, TransformerException, IOException {
+		context.put(MESSAGETEXT_KEY, message);
+		log.debug("extract messageBody from message [" + message + "]");
+		String messageBody = requestTp.transform(message.asSource());
 		if (isLayByNamespace()) {
 			String messageBodyNamespace = XmlUtils.getRootNamespace(messageBody);
 			if (messageBodyNamespace != null) {
@@ -201,7 +201,7 @@ public class BisJmsListener extends JmsListener {
 				messageBody = XmlUtils.removeNamespaces(messageBody);
 			}
 		}
-		return messageBody;
+		return new Message(messageBody);
 	}
 
 	@Override

commit 804b51d38c647fdab88f0d441dd1c30a8603e67a

Fix bug introduced in #1339

core/src/main/java/nl/nn/adapterframework/http/PushingListenerAdapter.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2013, 2017, 2019 Nationale-Nederlanden, 2020 WeAreFrank!
+   Copyright 2013, 2017, 2019 Nationale-Nederlanden, 2020, 2022 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ import nl.nn.adapterframework.core.IPushingListener;
 import nl.nn.adapterframework.core.IbisExceptionListener;
 import nl.nn.adapterframework.core.ListenerException;
 import nl.nn.adapterframework.core.PipeLineResult;
-import nl.nn.adapterframework.doc.IbisDoc;
 import nl.nn.adapterframework.receivers.ServiceClient;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.util.LogUtil;
@@ -94,7 +93,7 @@ public abstract class PushingListenerAdapter implements IPushingListener<Message
 			if (isApplicationFaultsAsExceptions()) {
 				log.debug("PushingListenerAdapter.processRequest() rethrows ListenerException...");
 				throw e;
-			} 
+			}
 			log.debug("PushingListenerAdapter.processRequest() formats ListenerException to errormessage");
 			return handler.formatException(null,correlationId, message, e);
 		}
@@ -112,10 +111,12 @@ public abstract class PushingListenerAdapter implements IPushingListener<Message
 		return name;
 	}
 
+	/**
+	 * Name of the listener as known to the adapter
+	 */
 	@Override
-	@IbisDoc({"name of the listener as known to the adapter", ""})
 	public void setName(String name) {
-		this.name=name;
+		this.name = name;
 	}
 
 	@Override

core/src/main/java/nl/nn/adapterframework/http/RestListener.java
@@ -15,7 +15,6 @@
 */
 package nl.nn.adapterframework.http;
 
-import java.io.IOException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -41,7 +40,7 @@ import nl.nn.adapterframework.stream.Message;
  * Prepends the configured URI pattern with <code>rest/</code>. When you are writing a new Frank config, you are recommended
  * to use an {@link nl.nn.adapterframework.http.rest.ApiListener} instead. You can find all serviced URI patterns
  * in the Frank!Console: main menu item Webservice, heading Available REST Services.
- * 
+ *
  * <p>
  * Note:
  * Servlets' multipart configuration expects a Content-Type of <code>multipart/form-data</code> (see http://docs.oracle.com/javaee/6/api/javax/servlet/annotation/MultipartConfig.html).
@@ -104,9 +103,10 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 		RestServiceDispatcher.getInstance().unregisterServiceClient(getUriPattern(), getMethod());
 	}
 
-	public String processRequest(String correlationId, String message, PipeLineSession requestContext) throws ListenerException {
+	@Override
+	public Message processRequest(String correlationId, Message message, Map<String, Object> requestContext) throws ListenerException {
 		HttpServletRequest httpServletRequest = (HttpServletRequest) requestContext.get(PipeLineSession.HTTP_REQUEST_KEY);
-		String response;
+		Message response;
 		String contentType = (String) requestContext.get("contentType");
 
 		//Check if valid path
@@ -140,12 +140,8 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 				default:
 					throw new IllegalStateException("Unknown mediatype ["+getProduces()+"]");
 			}
-			
-			try {
-				response = super.processRequest(correlationId, new Message(message), requestContext).asString();
-			} catch (IOException e) {
-				throw new ListenerException("Failed to read result", e);
-			}
+
+			response = super.processRequest(correlationId, message, requestContext);
 			if(response != null && !response.isEmpty())
 				eTag = response.hashCode();
 
@@ -158,11 +154,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 			}
 		}
 		else {
-			try {
-				response = super.processRequest(correlationId, new Message(message), requestContext).asString();
-			} catch (IOException e) {
-				throw new ListenerException("Failed to read result", e);
-			}
+			response = super.processRequest(correlationId, message, requestContext);
 			if(response != null && !response.isEmpty())
 				eTag = response.hashCode();
 		}
@@ -174,25 +166,17 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 		return response;
 	}
 
-	public String transformToJson(String message) throws PipeRunException {
+	public Message transformToJson(Message message) throws PipeRunException {
 		JsonPipe pipe = new JsonPipe();
 		pipe.setDirection(Direction.XML2JSON);
-		PipeRunResult pipeResult = pipe.doPipe(new Message(message), new PipeLineSession());
-		try {
-			return pipeResult.getResult().asString();
-		} catch (IOException e) {
-			throw new PipeRunException(null,"cannot transform result",e);
-		}
+		PipeRunResult pipeResult = pipe.doPipe(message, new PipeLineSession());
+		return pipeResult.getResult();
 	}
 
-	public String transformToXml(String message) throws PipeRunException {
+	public Message transformToXml(Message message) throws PipeRunException {
 		JsonPipe pipe = new JsonPipe();
-		PipeRunResult pipeResult = pipe.doPipe(new Message(message), new PipeLineSession());
-		try {
-			return pipeResult.getResult().asString();
-		} catch (IOException e) {
-			throw new PipeRunException(null,"cannot transform result",e);
-		}
+		PipeRunResult pipeResult = pipe.doPipe(message, new PipeLineSession());
+		return pipeResult.getResult();
 	}
 
 	@Override
@@ -214,7 +198,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 	public String getRestUriPattern() {
 		return getRestPath().substring(1) + "/" + getUriPattern();
 	}
-	
+
 
 	@IbisDoc({"Uri pattern to match, the {uri} part in https://mydomain.com/ibis4something/rest/{uri}, where mydomain.com and ibis4something refer to 'your ibis'. ", ""})
 	public void setUriPattern(String uriPattern) {

core/src/main/java/nl/nn/adapterframework/http/RestServiceDispatcher.java
@@ -56,10 +56,10 @@ import nl.nn.adapterframework.util.LogUtil;
  * This class is exposed as a webservice, to be able to provide a single point
  * of entry to all adapters that have a ServiceListener as a IReceiver.
  */
-public class RestServiceDispatcher  {
+public class RestServiceDispatcher {
 	protected Logger log = LogUtil.getLogger(this);
 	protected Logger secLog = LogUtil.getLogger("SEC");
-	
+
 	private final String WILDCARD="*";
 	private final String KEY_LISTENER="listener";
 	private final String KEY_ETAG_KEY="etagKey";
@@ -101,18 +101,18 @@ public class RestServiceDispatcher  {
 		}
 		return matchingPattern;
 	}
-	
+
 	public Map<String,Object> getMethodConfig(String matchingPattern, String method) {
 		Map<String,Object> methodConfig;
 		Map<String,Map<String,Object>> patternEntry=patternClients.get(matchingPattern);
-		
+
 		methodConfig = patternEntry.get(method);
 		if (methodConfig==null) {
 			methodConfig = patternEntry.get(WILDCARD);
 		}
 		return methodConfig;
 	}
-	
+
 	public List<String> getAvailableMethods(String matchingPattern) {
 		Map<String,Map<String,Object>> patternEntry=patternClients.get(matchingPattern);
 		Iterator<Entry<String,Map<String,Object>>> it = patternEntry.entrySet().iterator();
@@ -133,14 +133,14 @@ public class RestServiceDispatcher  {
 	public String dispatchRequest(String restPath, String uri, HttpServletRequest httpServletRequest, String contentType, String request, PipeLineSession context, HttpServletResponse httpServletResponse, ServletContext servletContext) throws ListenerException {
 		String method = httpServletRequest.getMethod();
 		if (log.isTraceEnabled()) log.trace("searching listener for uri ["+uri+"] method ["+method+"]");
-		
+
 		String matchingPattern = findMatchingPattern(uri);
 		if (matchingPattern==null) {
 			throw new ListenerException("no REST listener configured for uri ["+uri+"]");
 		}
-		
+
 		Map<String,Object> methodConfig = getMethodConfig(matchingPattern, method);
-		
+
 		if (methodConfig==null) {
 			throw new ListenerException("No REST listener specified for uri ["+uri+"] method ["+method+"]");
 		}
@@ -173,7 +173,7 @@ public class RestServiceDispatcher  {
 				context.put("principal", principal.getName());
 			}
 		}
-		
+
 		String ctName = Thread.currentThread().getName();
 		try {
 			boolean writeToSecLog = false;
@@ -240,7 +240,7 @@ public class RestServiceDispatcher  {
 				}
 				Thread.currentThread().setName(restListener.getName() + "["+ctName+"]");
 			}
-	
+
 			if (etagKey!=null) context.put(etagKey,etag);
 			if (contentTypeKey!=null) context.put(contentTypeKey,contentType);
 			if (log.isTraceEnabled()) log.trace("dispatching request, uri ["+uri+"] listener pattern ["+matchingPattern+"] method ["+method+"] etag ["+etag+"] contentType ["+contentType+"]");
@@ -297,7 +297,7 @@ public class RestServiceDispatcher  {
 			}
 		}
 	}
-	
+
 	public void registerServiceClient(ServiceClient listener, String uriPattern,
 			String method, String etagSessionKey, String contentTypeSessionKey, boolean validateEtag) throws ConfigurationException {
 		uriPattern = unifyUriPattern(uriPattern);

core/src/main/java/nl/nn/adapterframework/http/WebServiceListener.java
@@ -197,8 +197,8 @@ public class WebServiceListener extends PushingListenerAdapter implements HasPhy
 				throw new ListenerException(e);
 			}
 		}
-		else
-			return super.processRequest(correlationId, message, requestContext);
+
+		return super.processRequest(correlationId, message, requestContext);
 	}
 
 	public String getLogPrefix() {