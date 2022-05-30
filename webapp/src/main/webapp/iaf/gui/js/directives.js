0c7c74ef21c210086e927e9d89d2a0d1fa4ccd5b
Niels Meijer <nielsmeijer@hotmail.com>
2022-05-30 10:31:42 +0200

    Create button to open a modal when not using flow images

--- webapp/src/main/webapp/iaf/gui/js/directives.js
+++ webapp/src/main/webapp/iaf/gui/js/directives.js
@@ -168,7 +168,7 @@ angular.module('iaf.beheerconsole')
 	}
 })
 
-.directive('flow', ['Misc', '$http', function(Misc, $http) {
+.directive('flow', ['Misc', '$http', '$uibModal', function(Misc, $http, $uibModal) {
 	return {
 		restrict: 'E',
 		transclude: true,
@@ -178,17 +178,35 @@ angular.module('iaf.beheerconsole')
 		link: function(scope) {
 			let adapter = scope.adapter;
 			let uri = Misc.getServerPath() + 'iaf/api/adapters/' + Misc.escapeURL(adapter.name) + "/flow?"+adapter.upSince;
-			scope.flow = {};
+			scope.flow = {"image":null,"url":uri};
 			$http.get(uri).then(function(data) {
 				let status = (data && data.status) ? data.status : 204;
 				if(status == 200) {
-					scope.flow.url = uri; //Display the flow url
-				} else {
+					let contentType = data.headers("Content-Type");
+					scope.flow.image = (contentType.indexOf("image") > 0 || contentType.indexOf("svg") > 0); //display an image or a button to open a modal
+					if(!scope.flow.image) { //only store metadata when required
+						data.adapter = adapter;
+						scope.flow.data = data;
+					}
+				} else { //If non successfull response, force no-image-available
+					scope.flow.image = true;
 					scope.flow.url = 'images/no_image_available.svg'
 				}
 			});
+
+			scope.openFlowModal = function (xhr) {
+				$uibModal.open({
+					templateUrl: 'views/flow-modal.html',
+					resolve: {
+						xhr: function() {
+							return xhr;
+						}
+					},
+					controller: 'FlowDiagramModalCtrl'
+				});
+			}
 		},
-		template: '<a ng-href="{{flow.url}}" target="_blank"><img ng-src="{{flow.url}}" alt="Flow diagram"></a>'
+		template: '<a ng-if="flow.image === true" ng-href="{{flow.url}}" target="_blank"><img ng-src="{{flow.url}}" alt="Flow Diagram"></a><button ng-if="flow.image === false" ng-click="openFlowModal(flow.data)" title="Generate Flow Diagram" class="btn btn-xs btn-info" type="button"><i class="fa fa-share-alt-square"></i> Flow Diagram</button>'
 	}
 }])
 
