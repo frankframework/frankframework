69b96eba66b51fce5689e1b5d7f73e6eada18ea8
alisihab <sihab.akcan@gmail.com>
2022-06-07 13:46:10 +0200

    unused import

--- core/src/main/java/nl/nn/adapterframework/pipes/WsdlXmlValidator.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/WsdlXmlValidator.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
