dc63f82cf22e6ed18c5aef80bc1dee485a0370a0
alisihab <sihab.akcan@gmail.com>
2022-06-10 16:23:20 +0200

    Update core/src/main/java/nl/nn/adapterframework/pipes/WsdlXmlValidator.java
    Co-authored-by: Gerrit van Brakel <g.vanbrakel@flux-it.nl>

--- core/src/main/java/nl/nn/adapterframework/pipes/WsdlXmlValidator.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/WsdlXmlValidator.java
@@ -363,7 +363,7 @@ public class WsdlXmlValidator extends SoapValidator {
 		this.wsdl = wsdl;
 	}
 
-	@IbisDoc({"Name of the child element of the SOAP body, or a comma separated list of names to choose from (only one is allowed) (wsdl generator will use the first element) (use empty value to allow an empty soap body, for example to allow element x and an empty soap body use: x,). In case the request contains SOAPAction header and the wsdl contains an element specific to that SOAPAction, it will use that element as soapBody.", "" })
+	@IbisDoc({"Name of the child element of the SOAP body, or a comma separated list of names to choose from (only one is allowed) (WSDL generator will use the first element) (use empty value to allow an empty SOAP body, for example to allow element x and an empty SOAP body use: x,). In case the request contains SOAPAction header and the WSDL contains an element specific to that SOAPAction, it will use that element as SOAP body.", "" })
 	public void setSoapBody(String soapBody) {
 		super.setSoapBody(soapBody);
 	}
