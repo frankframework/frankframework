6e3eeb17d827b9b339986ef8e8f3ef5d5f70ce95
ricardovh <57142353+ricardovh@users.noreply.github.com>
2022-06-29 11:50:53 +0200

    Update JBoss Docker images (#3436)

--- docker/appserver/JBoss/src/configuration/configuration.py
+++ docker/appserver/JBoss/src/configuration/configuration.py
@@ -1,22 +1,16 @@
 import os
 
-JBOSS_HOME = os.getenv("JBOSS_HOME")
-
-# base path to resources, configurations jars
-path_to_jars = JBOSS_HOME+"/standalone/lib/ext/"
-
 # name of the module that war file will depend on
-moduleName = "module.frankframework.resources"
+moduleName = "module.frank-framework"
 
 # list of jar files
+path_to_jars="/home/jboss/jboss-eap-7.3/standalone/lib/ext/"
 jarList = os.listdir(path_to_jars)
 
 # resources that the module will be created upon
 resources = ""
 resourceDelimiter = ':'
 
-# client executable path
-jboss_cli = JBOSS_HOME+"/bin/jboss-cli.sh"
 
 for jarFile in jarList:
 	resources += path_to_jars+jarFile+resourceDelimiter
@@ -25,4 +19,6 @@ resources = resources[0:-1]
 
 command="module add --name="+moduleName+" --resources="+resources
 
-os.system(jboss_cli+" --command='"+command+"'")
+print(command)
+
+os.system("/home/jboss/jboss-eap-7.3/bin/jboss-cli.sh --command='"+command+"'")
