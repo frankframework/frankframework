d8d430e9c87c219631bbd9e28aac67d79ec9f086
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-07-05 14:58:31 +0200

    Create Session in Receiver when necessary

--- core/src/main/java/nl/nn/adapterframework/receivers/Receiver.java
+++ core/src/main/java/nl/nn/adapterframework/receivers/Receiver.java
@@ -1063,6 +1063,10 @@ public class Receiver<M> extends TransactionAttributes implements IManagable, IR
 		}
 		long startExtractingMessage = System.currentTimeMillis();
 		boolean sessionCreated=false;
+		if (session==null) {
+			session = new PipeLineSession();
+			sessionCreated = true;
+		}
 		try {
 			if(isForceRetryFlag()) {
 				session.put(Receiver.RETRY_FLAG_SESSION_KEY, "true");
