e30237fd91fb0a35a91a981a0551fe718a7d3e3f
Martijn Dirkse <martijn@integrationpartners.nl>
2022-08-01 15:26:07 +0200

    Update JavaDoc of the Reintroduce annotation.

--- core/src/main/java/nl/nn/adapterframework/doc/Reintroduce.java
+++ core/src/main/java/nl/nn/adapterframework/doc/Reintroduce.java
@@ -25,7 +25,22 @@ import java.lang.annotation.Target;
 
 /**
  * If set on attribute setter or config child setter, forces attribute or config child to
- * be declared explicitly with the child. Useful for controlling the order of config children.
+ * be declared explicitly with the child. This annotation was introduced to modify the sort
+ * order of config children. Setting this application is only needed if the
+ * inheriting config child setter does not update the config child in another way. For example
+ * if the overriding method has a JavaDoc comment to update the description, then the config
+ * child is repeated already without the need to add this annotation.
+ *
+ * Although it is technically possible to apply this annotation on attributes, doing so does
+ * not make sense because XML schema does not enforce the order in which attributes appear
+ * in a Frank configuration.
+ * 
+ * This annotation has no impact on the Frank!Doc webapp. WeAreFrank! decided so because
+ * repeated config children would look confusing. If an overriding config child setter
+ * modifies the config child in another way then with this annotation (e.g other documentation),
+ * then the config child is repeated in the Frank!Doc webapp with the overriding class.
+ * 
+ * JavaDoc tag <code>@ff.reintroduce</code> has the same effect as this annotation.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
