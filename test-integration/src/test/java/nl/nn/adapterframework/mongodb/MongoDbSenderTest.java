b9565695cb8b2c73d639a33811685d325558f1f2
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-08-29 12:13:56 +0200

    replace javax.json with jakarta.json

--- test-integration/src/test/java/nl/nn/adapterframework/mongodb/MongoDbSenderTest.java
+++ test-integration/src/test/java/nl/nn/adapterframework/mongodb/MongoDbSenderTest.java
@@ -3,11 +3,11 @@ package nl.nn.adapterframework.mongodb;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
 
 import org.hamcrest.core.StringContains;
 import org.junit.Test;
