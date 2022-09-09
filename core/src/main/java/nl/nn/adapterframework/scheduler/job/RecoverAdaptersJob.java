e64c0c4c8e08f91fe601e32490494c7582764b1b
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-09-09 17:14:06 +0200

    Restore count of open Adapters and Receivers in heartbeat.log

--- core/src/main/java/nl/nn/adapterframework/scheduler/job/RecoverAdaptersJob.java
+++ core/src/main/java/nl/nn/adapterframework/scheduler/job/RecoverAdaptersJob.java
@@ -40,72 +40,69 @@ public class RecoverAdaptersJob extends JobDef {
 			if(configuration.isActive()) {
 				for (Adapter adapter: configuration.getRegisteredAdapters()) {
 					countAdapter++;
-					handleAdapter(adapter, countAdapterStateStarted, countReceiver, countReceiverStateStarted);
+					RunState adapterRunState = adapter.getRunState();
+					boolean startAdapter = false;
+					if (adapterRunState==RunState.ERROR) { //if not previously configured, there is no point in trying to do this again.
+						log.debug("trying to recover adapter [{}]", adapter::getName);
+
+						if (!adapter.configurationSucceeded()) { //This should only happen once, so only try to (re-)configure if it failed in the first place!
+							try {
+								adapter.configure();
+							} catch (ConfigurationException e) {
+								// log the warning and do nothing, it couldn't configure before, it still can't...
+								log.warn("error configuring adapter [" + adapter.getName() + "] while trying to recover", e);
+							}
+						}
+
+						if (adapter.configurationSucceeded()) {
+							startAdapter = adapter.isAutoStart(); // if configure has succeeded and adapter was in state ERROR try to auto (re-)start the adapter
+						}
+
+						log.debug("finished recovering adapter[{}]", adapter::getName);
+					}
+
+					String message = "adapter [" + adapter.getName() + "] has state [" + adapterRunState + "]";
+					if (adapterRunState==RunState.STARTED) {
+						countAdapterStateStarted++;
+						heartbeatLog.info(message);
+					} else if (adapterRunState==RunState.ERROR) {
+						heartbeatLog.error(message);
+					} else {
+						heartbeatLog.warn(message);
+					}
+
+					for (Receiver<?> receiver: adapter.getReceivers()) {
+						countReceiver++;
+
+						RunState receiverRunState = receiver.getRunState();
+						//Only try to (re-)start receivers in a running adapter. Receiver configure is done in Adapter.configure
+						if (adapterRunState==RunState.STARTED && (receiverRunState==RunState.ERROR || receiverRunState==RunState.EXCEPTION_STARTING) && receiver.configurationSucceeded()) {
+							log.debug("trying to recover receiver [{}] of adapter [{}]", receiver::getName, adapter::getName);
+
+							receiver.startRunning();
+
+							log.debug("finished recovering receiver [{}] of adapter [{}]", receiver::getName, adapter::getName);
+						}
+
+						receiverRunState = receiver.getRunState();
+						message = "receiver [" + receiver.getName() + "] of adapter [" + adapter.getName() + "] has state [" + receiverRunState + "]";
+						if (receiverRunState==RunState.STARTED) {
+							countReceiverStateStarted++;
+							heartbeatLog.info(message);
+						} else if (receiverRunState==RunState.ERROR) {
+							heartbeatLog.error(message);
+						} else {
+							heartbeatLog.warn(message);
+						}
+					}
+
+					if (startAdapter) { // can only be true if adapter was in error before and AutoStart is enabled
+						adapter.startRunning(); //ASync startup can still cause the Adapter to end up in an ERROR state
+					}
 				}
 			}
 		}
 		heartbeatLog.info("[" + countAdapterStateStarted + "/" + countAdapter + "] adapters and [" + countReceiverStateStarted + "/" + countReceiver + "] receivers have state [" + RunState.STARTED + "]");
 	}
 
-	private void handleAdapter(Adapter adapter, int countAdapterStateStarted, int countReceiver, int countReceiverStateStarted) {
-		RunState adapterRunState = adapter.getRunState();
-		boolean startAdapter = false;
-		if (adapterRunState==RunState.ERROR) { //if not previously configured, there is no point in trying to do this again.
-			log.debug("trying to recover adapter [{}]", adapter::getName);
-
-			if (!adapter.configurationSucceeded()) { //This should only happen once, so only try to (re-)configure if it failed in the first place!
-				try {
-					adapter.configure();
-				} catch (ConfigurationException e) {
-					// log the warning and do nothing, it couldn't configure before, it still can't...
-					log.warn("error configuring adapter [" + adapter.getName() + "] while trying to recover", e);
-				}
-			}
-
-			if (adapter.configurationSucceeded()) {
-				startAdapter = adapter.isAutoStart(); // if configure has succeeded and adapter was in state ERROR try to auto (re-)start the adapter
-			}
-
-			log.debug("finished recovering adapter[{}]", adapter::getName);
-		}
-
-		String message = "adapter [" + adapter.getName() + "] has state [" + adapterRunState + "]";
-		if (adapterRunState==RunState.STARTED) {
-			countAdapterStateStarted++;
-			heartbeatLog.info(message);
-		} else if (adapterRunState==RunState.ERROR) {
-			heartbeatLog.error(message);
-		} else {
-			heartbeatLog.warn(message);
-		}
-
-		for (Receiver<?> receiver: adapter.getReceivers()) {
-			countReceiver++;
-
-			RunState receiverRunState = receiver.getRunState();
-			//Only try to (re-)start receivers in a running adapter. Receiver configure is done in Adapter.configure
-			if (adapterRunState==RunState.STARTED && (receiverRunState==RunState.ERROR || receiverRunState==RunState.EXCEPTION_STARTING) && receiver.configurationSucceeded()) {
-				log.debug("trying to recover receiver [{}] of adapter [{}]", receiver::getName, adapter::getName);
-
-				receiver.startRunning();
-
-				log.debug("finished recovering receiver [{}] of adapter [{}]", receiver::getName, adapter::getName);
-			}
-
-			receiverRunState = receiver.getRunState();
-			message = "receiver [" + receiver.getName() + "] of adapter [" + adapter.getName() + "] has state [" + receiverRunState + "]";
-			if (receiverRunState==RunState.STARTED) {
-				countReceiverStateStarted++;
-				heartbeatLog.info(message);
-			} else if (receiverRunState==RunState.ERROR) {
-				heartbeatLog.error(message);
-			} else {
-				heartbeatLog.warn(message);
-			}
-		}
-
-		if (startAdapter) { // can only be true if adapter was in error before and AutoStart is enabled
-			adapter.startRunning(); //ASync startup can still cause the Adapter to end up in an ERROR state
-		}
-	}
 }
