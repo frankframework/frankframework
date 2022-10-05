48bdade6abfd4a94135bb235dd68b1a27eabf1d4
Niels Meijer <nielsmeijer@hotmail.com>
2022-10-05 21:25:22 +0200

    Clear jmsRealms

--- core/src/test/java/nl/nn/adapterframework/management/bus/TestSecurityItems.java
+++ core/src/test/java/nl/nn/adapterframework/management/bus/TestSecurityItems.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 
+import nl.nn.adapterframework.jms.JmsRealmFactory;
 import nl.nn.adapterframework.testutil.FixedQuerySenderMock.ResultSetBuilder;
 import nl.nn.adapterframework.testutil.MatchUtils;
 import nl.nn.adapterframework.testutil.TestFileUtils;
@@ -12,6 +13,7 @@ public class TestSecurityItems extends BusTestBase {
 
 	@Test
 	public void getSecurityItems() throws Exception {
+		JmsRealmFactory.getInstance().clear(); //TODO add some jms realms to test
 		mockQuery("select datasource from database", ResultSetBuilder.create().build());
 
 		MessageBuilder<String> request = createRequestMessage("NONE", BusTopic.SECURITY_ITEMS);
