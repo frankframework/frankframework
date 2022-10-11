c16d46093b7f22b6467106f70828fe97dcac7934
Niels Meijer <nielsmeijer@hotmail.com>
2022-10-11 12:00:10 +0200

    Remove old security items code

--- core/src/main/java/nl/nn/adapterframework/management/bus/endpoints/SecurityItems.java
+++ core/src/main/java/nl/nn/adapterframework/management/bus/endpoints/SecurityItems.java
@@ -33,6 +33,10 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.springframework.messaging.Message;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -86,6 +90,32 @@ public class SecurityItems {
 		return ResponseMessage.ok(returnMap);
 	}
 
+	private UserDetails getUserDetails() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+			return (UserDetails) authentication.getPrincipal();
+		}
+		return null;
+	}
+
+	/**
+	 * See AuthorityAuthorizationManager#ROLE_PREFIX
+	 */
+	private boolean hasAuthority(String role) {
+		UserDetails userDetails = getUserDetails();
+		boolean granted = false;
+		if(userDetails != null) {
+			for(GrantedAuthority authority : userDetails.getAuthorities()) {
+				String roleName = authority.getAuthority().substring(5); //chomp off the AuthorityAuthorizationManager#ROLE_PREFIX
+				granted = roleName.equals(role);
+				if(granted) {
+					return true;
+				}
+			}
+		}
+		return granted;
+	}
+
 	private Map<String, Object> getApplicationDeploymentDescriptor() {
 		String appDDString = null;
 		Map<String, Object> resultList = new HashMap<>();
@@ -109,12 +139,12 @@ public class SecurityItems {
 			return null;
 		}
 
-		if (xmlDoc==null) {
+		Map<String, Map<String, List<String>>> secBindings = getSecurityRoleBindings();
+
+		if (xmlDoc==null || secBindings == null) {
 			log.debug("could get deployment descriptor");
 			return null;
 		}
-
-		Map<String, Map<String, List<String>>> secBindings = getSecurityRoleBindings();
 
 		NodeList rowset = xmlDoc.getElementsByTagName("security-role");
 		for (int i = 0; i < rowset.getLength(); i++) {
@@ -128,14 +158,17 @@ public class SecurityItems {
 						tmp.put(field.getNodeName(), field.getTextContent());
 					}
 				}
-				if(secBindings.containsKey(row.getAttribute("id")))
+				if(secBindings.containsKey(row.getAttribute("id"))) {
 					tmp.putAll(secBindings.get(row.getAttribute("id")));
+				}
 				try {
 					if(tmp.containsKey("role-name")) {
 						String role = (String) tmp.get("role-name");
-						tmp.put("allowed", true); // TODO httpServletRequest.isUserInRole(role)
+						tmp.put("allowed", hasAuthority(role));
 					}
-				} catch(Exception e) {}
+				} catch(Exception e) {
+					log.warn("unable to check user authorities against the provided security roles", e);
+				}
 				resultList.put(row.getAttribute("id"), tmp); //items are stored under the security-role-id
 			}
 		}
