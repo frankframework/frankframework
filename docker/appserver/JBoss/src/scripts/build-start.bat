4e004e12a96f3a355ab91e25e2d908c64bf75afc
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-10-13 13:50:55 +0200

    Refactor build scripts for WebSphere, JBoss and WildFly (#3805)

---
+++ docker/appserver/JBoss/src/scripts/build-start.bat
@@ -0,0 +1,3 @@
+call %~dp0/setenv.bat
+
+call %~dp0/../../../scripts/build-start-body.bat
---
+++ docker/appserver/scripts/build-start-body.bat
@@ -0,0 +1,8 @@
+call %~dp0/../../scripts/build_iaf.bat
+
+docker image rm %IMAGE_NAME%
+
+echo "build %PRODUCT% iaf-test docker image" 
+call %MVN% %CLI_OPTS% -f %~dp0/../%PRODUCT% -s "%MVN_SETTINGS%" %MVN_OPTIONS% install
+
+call %~dp0/start-body.bat
