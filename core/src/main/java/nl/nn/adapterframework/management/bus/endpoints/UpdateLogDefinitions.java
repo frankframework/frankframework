0d94efceac261297af9215b62685ab7bdf48f5b5
Niels Meijer <nielsmeijer@hotmail.com>
2022-10-17 10:51:22 +0200

    Migrate logdefinitions

---
+++ core/src/main/java/nl/nn/adapterframework/management/bus/endpoints/UpdateLogDefinitions.java
@@ -0,0 +1,133 @@
+/*
+   Copyright 2022 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package nl.nn.adapterframework.management.bus.endpoints;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.spi.StandardLevel;
+import org.springframework.messaging.Message;
+
+import nl.nn.adapterframework.management.bus.ActionSelector;
+import nl.nn.adapterframework.management.bus.BusAction;
+import nl.nn.adapterframework.management.bus.BusAware;
+import nl.nn.adapterframework.management.bus.BusMessageUtils;
+import nl.nn.adapterframework.management.bus.BusTopic;
+import nl.nn.adapterframework.management.bus.ResponseMessage;
+import nl.nn.adapterframework.management.bus.TopicSelector;
+import nl.nn.adapterframework.util.LogUtil;
+
+@BusAware("frank-management-bus")
+@TopicSelector(BusTopic.LOG_DEFINITIONS)
+public class UpdateLogDefinitions {
+	private Logger log = LogUtil.getLogger(this);
+
+	private static final String FF_PACKAGE_PREFIX = "nl.nn.adapterframework";
+
+	@ActionSelector(BusAction.GET)
+	public Message<String> getLoggersAndDefinitions(Message<?> message) {
+		String filter = BusMessageUtils.getHeader(message, "filter", null);
+		LoggerContext logContext = LoggerContext.getContext(false);
+		Map<String, Object> result = new HashMap<>();
+
+		if(StringUtils.isEmpty(filter)) {
+			List<Object> defaultLoggers = new ArrayList<>();
+			Collection<LoggerConfig> loggerConfigs = logContext.getConfiguration().getLoggers().values();
+			for(LoggerConfig config : loggerConfigs) {
+				String name = config.getName();
+				if(StringUtils.isNotEmpty(name) && name.contains(".") && !name.startsWith(LogUtil.MESSAGE_LOGGER+".")) {
+					Map<String, Object> logger = new HashMap<>();
+					logger.put("name", name);
+					logger.put("level", config.getLevel().getStandardLevel());
+					Set<String> appenders = config.getAppenders().keySet();
+					if(!appenders.isEmpty()) {
+						logger.put("appenders", config.getAppenders().keySet());
+					}
+					defaultLoggers.add(logger);
+				}
+			}
+			result.put("definitions", defaultLoggers);
+
+			filter = FF_PACKAGE_PREFIX;
+		}
+
+		Map<String, StandardLevel> registeredLoggers = new TreeMap<>(); // A list with all Loggers that are logging to Log4j2
+		for (Logger logger : logContext.getLoggers()) {
+			String logName = logger.getName();
+			String packageName = null;
+			if(logName.contains(".")) {
+				packageName = logName.substring(0, logName.lastIndexOf("."));
+			} else {
+				packageName = logName;
+			}
+
+			if(filter == null || packageName.startsWith(filter)) {
+				StandardLevel newLevel = logger.getLevel().getStandardLevel();
+				StandardLevel oldLevel = registeredLoggers.get(packageName);
+				if(oldLevel != null && oldLevel.compareTo(newLevel) < 1) {
+					continue;
+				}
+				registeredLoggers.put(packageName, newLevel);
+			}
+		}
+		result.put("loggers", registeredLoggers);
+
+		return ResponseMessage.ok(result);
+	}
+
+	@ActionSelector(BusAction.MANAGE)
+	public Message<String> updateLogConfiguration(Message<?> message) {
+		String loglevelStr = BusMessageUtils.getHeader(message, "level", null);
+		Level level = Level.toLevel(loglevelStr, null);
+		String logPackage = BusMessageUtils.getHeader(message, "logPackage", null);
+		Boolean reconfigure = BusMessageUtils.getBooleanHeader(message, "reconfigure", null);
+
+		if(reconfigure != null) {
+			if(reconfigure) {
+				LoggerContext logContext = LoggerContext.getContext(false);
+				logContext.reconfigure();
+				log2SecurityLog("reconfigured logdefinitions");
+				return ResponseMessage.accepted();
+			}
+
+			return ResponseMessage.noContent();
+		}
+
+		if(StringUtils.isNotEmpty(logPackage) && level != null) {
+			Configurator.setLevel(logPackage, level);
+			log2SecurityLog("changed logdefinition ["+logPackage+"] to level ["+level.getStandardLevel().name()+"]");
+			return ResponseMessage.accepted();
+		}
+
+		return ResponseMessage.badRequest();
+	}
+
+	private void log2SecurityLog(String logMessage) {
+		log.warn(logMessage);
+		LogUtil.getLogger("SEC").info(logMessage);
+	}
+}
--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/UpdateLoggingConfig.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/UpdateLoggingConfig.java
@@ -15,15 +15,8 @@ limitations under the License.
 */
 package nl.nn.adapterframework.webcontrol.api;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
@@ -36,18 +29,11 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.spi.StandardLevel;
 
 import nl.nn.adapterframework.management.bus.BusAction;
 import nl.nn.adapterframework.management.bus.BusTopic;
 import nl.nn.adapterframework.management.bus.RequestMessageBuilder;
-import nl.nn.adapterframework.util.LogUtil;
 
 /**
  * Read and update logging configuration
@@ -58,8 +44,6 @@ import nl.nn.adapterframework.util.LogUtil;
 
 @Path("/")
 public class UpdateLoggingConfig extends FrankApiBase {
-
-	private static final String FF_PACKAGE_PREFIX = "nl.nn.adapterframework";
 
 	@GET
 	@PermitAll
@@ -94,95 +78,36 @@ public class UpdateLoggingConfig extends FrankApiBase {
 	@Relation("logging")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getLogSettings(@QueryParam("filter") String filter) {
-
-		LoggerContext logContext = LoggerContext.getContext(false);
-		Map<String, Object> result = new HashMap<>();
-
-		if(StringUtils.isEmpty(filter)) {
-			List<Object> defaultLoggers = new ArrayList<>();
-			Collection<LoggerConfig> loggerConfigs = logContext.getConfiguration().getLoggers().values();
-			for(LoggerConfig config : loggerConfigs) {
-				String name = config.getName();
-				if(StringUtils.isNotEmpty(name) && name.contains(".") && !name.startsWith(LogUtil.MESSAGE_LOGGER+".")) {
-					Map<String, Object> logger = new HashMap<>();
-					logger.put("name", name);
-					logger.put("level", config.getLevel().getStandardLevel());
-					Set<String> appenders = config.getAppenders().keySet();
-					if(!appenders.isEmpty()) {
-						logger.put("appenders", config.getAppenders().keySet());
-					}
-					defaultLoggers.add(logger);
-				}
-			}
-			result.put("definitions", defaultLoggers);
-
-			filter = FF_PACKAGE_PREFIX;
-		}
-
-		Map<String, StandardLevel> registeredLoggers = new TreeMap<>(); // A list with all Loggers that are logging to Log4j2
-		for (Logger log : logContext.getLoggers()) {
-			String logName = log.getName();
-			String packageName = null;
-			if(logName.contains(".")) {
-				packageName = logName.substring(0, logName.lastIndexOf("."));
-			} else {
-				packageName = logName;
-			}
-
-			if(filter == null || packageName.startsWith(filter)) {
-				StandardLevel newLevel = log.getLevel().getStandardLevel();
-				StandardLevel oldLevel = registeredLoggers.get(packageName);
-				if(oldLevel != null && oldLevel.compareTo(newLevel) < 1) {
-					continue;
-				}
-				registeredLoggers.put(packageName, newLevel);
-			}
-		}
-		result.put("loggers", registeredLoggers);
-
-		return Response.status(Response.Status.OK).entity(result).build();
+		RequestMessageBuilder request = RequestMessageBuilder.create(this, BusTopic.LOG_DEFINITIONS, BusAction.GET);
+		request.addHeader("filter", filter);
+		return callSyncGateway(request);
 	}
 
 	@PUT
 	@RolesAllowed({"IbisDataAdmin", "IbisAdmin", "IbisTester"})
 	@Path("/server/logging/settings")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateLogger(LinkedHashMap<String, Object> json) {
-		Level level = null;
-		String logPackage = null;
-		boolean reconfigure = false;
+	public Response updateLogger(Map<String, Object> json) {
+		RequestMessageBuilder request = RequestMessageBuilder.create(this, BusTopic.LOG_DEFINITIONS, BusAction.MANAGE);
 
 		for (Entry<String, Object> entry : json.entrySet()) {
 			String key = entry.getKey();
 			Object value = entry.getValue();
 			if(key.equalsIgnoreCase("level")) {
-				level = Level.toLevel(""+value, null);
+				Level level = Level.toLevel(""+value, null);
+				if(level != null) {
+					request.addHeader("level", level.name());
+				}
 			} else if(key.equalsIgnoreCase("logger")) {
-				logPackage = (String) value;
+				String logPackage = (String) value;
+				request.addHeader("logPackage", logPackage);
 			} else if(key.equalsIgnoreCase("reconfigure")) {
-				reconfigure = Boolean.parseBoolean(""+value);
+				boolean reconfigure = Boolean.parseBoolean(""+value);
+				request.addHeader("reconfigure", reconfigure);
 			}
 		}
 
-		if(reconfigure) {
-			LoggerContext logContext = LoggerContext.getContext(false);
-			logContext.reconfigure();
-			log2SecurityLog("reconfigured logdefinitions");
 
-			return Response.status(Response.Status.CREATED).build();
-		}
-
-		if(StringUtils.isNotEmpty(logPackage) && level != null) {
-			Configurator.setLevel(logPackage, level);
-			log2SecurityLog("changed logdefinition ["+logPackage+"] to level ["+level.getStandardLevel().name()+"]");
-			return Response.status(Response.Status.ACCEPTED).build();
-		}
-
-		return Response.status(Response.Status.BAD_REQUEST).build();
-	}
-
-	private void log2SecurityLog(String logMessage) {
-		log.warn(logMessage);
-		LogUtil.getLogger("SEC").info(logMessage);
+		return callSyncGateway(request);
 	}
 }
