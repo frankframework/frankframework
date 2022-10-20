82021e59a8339d2c1cfc2b8ffd0932e7d8b8b7a2
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-10-20 12:26:19 +0200

    Include root element of included files (#3885)

--- core/src/main/java/nl/nn/adapterframework/configuration/digester/IncludeFilter.java
+++ core/src/main/java/nl/nn/adapterframework/configuration/digester/IncludeFilter.java
@@ -35,6 +35,8 @@ import nl.nn.adapterframework.xml.SaxException;
 
 public class IncludeFilter extends FullXmlFilter {
 
+	private final char[] NEWLINE="\n".toCharArray();
+
 	private String targetElement = "Include";
 	private String targetAttribute = "ref";
 
@@ -55,7 +57,9 @@ public class IncludeFilter extends FullXmlFilter {
 	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
 		if (localName.equals(targetElement)) {
 			String ref = atts.getValue(targetAttribute);
+			characters(NEWLINE, 0, 1);
 			comment("start include '"+ref+"'");
+			characters(NEWLINE, 0, 1);
 			if (StringUtils.isNotEmpty(ref)) {
 				Resource subResource;
 				try {
@@ -66,7 +70,7 @@ public class IncludeFilter extends FullXmlFilter {
 				if (subResource==null) {
 					throw new SaxException("Cannot find include ["+ref+"]");
 				}
-				XMLFilterImpl handlerTail = new BodyOnlyFilter(getContentHandler());
+				XMLFilterImpl handlerTail = new BodyOnlyFilter(getContentHandler(), false);
 				ContentHandler includeHandler = handlerTail;
 				// the below filters need to be included if the filter is placed higher in the chain
 				// includeHandler = new OnlyActiveFilter(includeHandler, appConstants);
@@ -86,6 +90,8 @@ public class IncludeFilter extends FullXmlFilter {
 					}
 				}
 			}
+			characters(NEWLINE, 0, 1);
+			comment("end include '"+ref+"'");
 			return;
 		}
 		super.startElement(uri, localName, qName, atts);
@@ -94,7 +100,6 @@ public class IncludeFilter extends FullXmlFilter {
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		if (localName.equals(targetElement)) {
-			comment("end include");
 			return;
 		}
 		super.endElement(uri, localName, qName);
