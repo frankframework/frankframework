9ea8d958d82860cf0b6860ed4b9c7ddd8d98a180
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-10-24 11:45:10 +0200

    Remove secrets from JBoss image

--- docker/appserver/JBoss/src/configuration/configuration.py
+++ docker/appserver/JBoss/src/configuration/configuration.py
@@ -22,13 +22,3 @@ command="module add --name="+moduleName+" --resources="+resources
 print(command)
 
 os.system("/home/jboss/jboss-eap-7.3/bin/jboss-cli.sh --command='"+command+"'")
-
-
-path_to_secrets="/opt/frank/secrets/"
-secretsList = os.listdir(path_to_secrets)
-
-os.system("/home/jboss/jboss-eap-7.3/bin/elytron-tool.sh credential-store --create --location '/home/jboss/jboss-eap-7.3/standalone/data/CS.jceks' --password secret")
-
-for secret in secretsList:
-	password=open(path_to_secrets+secret+"/password").read()
-	os.system("/home/jboss/jboss-eap-7.3/bin/elytron-tool.sh credential-store --location '/home/jboss/jboss-eap-7.3/standalone/data/CS.jceks' --password secret -add "+secret+" --secret "+password)
