4d38f61bcd4f10e21c7ffb67cd8949bf8177001a
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-10-24 14:08:24 +0200

    Remove secrets from Wildfly image

--- docker/appserver/WildFly/src/configuration/configuration.py
+++ docker/appserver/WildFly/src/configuration/configuration.py
@@ -22,12 +22,3 @@ command="module add --name="+moduleName+" --resources="+resources
 print(command)
 
 os.system("/opt/jboss/wildfly/bin/jboss-cli.sh --command='"+command+"'")
-
-path_to_secrets="/opt/frank/secrets/"
-secretsList = os.listdir(path_to_secrets)
-
-os.system("/opt/jboss/wildfly/bin/elytron-tool.sh credential-store --create --location '/opt/jboss/wildfly/standalone/data/CS.jceks' --password secret")
-
-for secret in secretsList:
-	password=open(path_to_secrets+secret+"/password").read()
-	os.system("/opt/jboss/wildfly/bin/elytron-tool.sh credential-store --location '/opt/jboss/wildfly/standalone/data/CS.jceks' --password secret -add "+secret+" --secret "+password)
