9994cedf820005963d22edf2d759231dbffede57
Niels Meijer <nielsmeijer@hotmail.com>
2022-10-25 14:13:11 +0200

    Add copyright notice

--- core/src/main/java/nl/nn/adapterframework/management/bus/doa/ConfigurationDOA.java
+++ core/src/main/java/nl/nn/adapterframework/management/bus/doa/ConfigurationDOA.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2022 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package nl.nn.adapterframework.management.bus.doa;
 
 import java.util.Date;
@@ -93,7 +108,10 @@ public class ConfigurationDOA {
 			String name2 = rhs.getName();
 			if(name1 == null || name2 == null) return 0;
 
-			return name1.startsWith("IAF_") ? -1 : name2.startsWith("IAF_") ? 1 : compareNames(name1, name2);
+			if(name1.startsWith("IAF_")) {
+				return -1;
+			}
+			return name2.startsWith("IAF_") ? 1 : compareNames(name1, name2);
 		}
 	}
 }
