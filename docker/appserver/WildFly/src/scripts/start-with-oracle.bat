98d22cce7571f474f86b72c72ca337814ff7d171
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-10-26 15:01:49 +0200

    Fix iaf-test problems (#3920)
    - Fix WebSphere mysql driver
    - Fix JBoss/WildFly javamail 2 support
    - set Docker start scripts
    - Add apiAlias
    - Add Cloudwatch default namespace

--- docker/appserver/WildFly/src/scripts/start-with-oracle.bat
+++ docker/appserver/WildFly/src/scripts/start-with-oracle.bat
@@ -1,4 +1,3 @@
-docker container stop iaf-test-as-wildfly
-docker container rm iaf-test-as-wildfly
+set DOCKER_OPTIONS=-e jdbc.dbms.default=oracle
 
-docker run --publish 80:8080 -e jdbc.dbms.default=oracle --mount type=bind,src=%~dp0/../../../../../test/src/main/secrets,dst=/opt/frank/secrets --name iaf-test-as-wildfly iaf-test-as-wildfly
+call start.bat
