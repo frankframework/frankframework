72f6aedcc9cea5fe02cb6f51e274972d2e0a043c
Matt Johnson <matt.johnson.osu@gmail.com>
2022-11-18 12:45:16 -0500

    Comment on ranker removal

--- .lift/setup.sh
+++ .lift/setup.sh
@@ -2,4 +2,5 @@
 
 # Remove the default settings.xml that forces all repos to go through a mirror
 rm ~/.m2/settings.xml
+# ranker was causing an OOM exception so remove for now
 rm -rf /opt/muse/packages/ranker/
