d49cd939406dbe7963b11980327a968bb3456f66
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-11-18 13:22:28 +0100

    Always clear ThreadContext after processing, except in JavaListener

--- core/src/main/java/nl/nn/adapterframework/scheduler/ConfiguredJob.java
+++ core/src/main/java/nl/nn/adapterframework/scheduler/ConfiguredJob.java
@@ -55,9 +55,9 @@ public class ConfiguredJob extends BaseJob {
 			JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 			IJob jobDef = (IJob)dataMap.get(JOBDEF_KEY);
 			Thread.currentThread().setName(jobDef.getName() + "["+ctName+"]");
-			log.info(getLogPrefix(jobDef) + "executing");
+			log.trace(getLogPrefix(jobDef) + "executing");
 			jobDef.executeJob();
-			log.debug(getLogPrefix(jobDef) + "completed");
+			log.trace(getLogPrefix(jobDef) + "completed");
 		}
 		catch (Exception e) {
 			log.error("JobExecutionException while running "+getLogPrefix(context), e);
