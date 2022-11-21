eb6e4aa30f9326af5194c7df3cd6dfabeeedb07c
Matt Johnson <matt.johnson.osu@gmail.com>
2022-11-21 08:27:06 -0500

    Update setup.sh

--- .lift/setup.sh
+++ .lift/setup.sh
@@ -2,18 +2,3 @@
 
 # Remove the default settings.xml that forces all repos to go through a mirror
 rm ~/.m2/settings.xml
-# ranker was causing an OOM exception so remove for now
-rm -rf /opt/muse/packages/ranker/
-mkdir -p /opt/muse/packages/ranker/
-
-cat <<EOF >> /opt/muse/packages/ranker/rank.py
-#!/usr/bin/env python3
-
-if __name__=="__main__":
-    print([])
-    exit()
-
-
-EOF
-
-chmod a+x /opt/muse/packages/ranker/rank.py
