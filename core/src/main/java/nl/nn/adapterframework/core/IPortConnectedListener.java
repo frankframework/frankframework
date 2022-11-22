6935ecd719163455194d5d6a838f4ea7178cf491
Gerrit van Brakel <g.vanbrakel@flux-it.nl>
2022-11-22 11:32:19 +0100

    Fix issues

--- core/src/main/java/nl/nn/adapterframework/core/IPortConnectedListener.java
+++ core/src/main/java/nl/nn/adapterframework/core/IPortConnectedListener.java
@@ -39,5 +39,5 @@ public interface IPortConnectedListener<M> extends IPushingListener<M> {
 
 	public IListenerConnector<M> getListenerPortConnector();
 
-	public void checkTransactionManagerValidity();
+	default void checkTransactionManagerValidity() {}
 }
