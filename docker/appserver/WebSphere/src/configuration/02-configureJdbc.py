25df192f9fcdde18dc73994461807599c5799225
Ricardo <57142353+ricardovh@users.noreply.github.com>
2022-11-25 15:24:15 +0100

    Name DB2 datasource the same as Postgres with regards to XA for non-Tomcat

--- docker/appserver/WebSphere/src/configuration/02-configureJdbc.py
+++ docker/appserver/WebSphere/src/configuration/02-configureJdbc.py
@@ -80,7 +80,7 @@ createDatasource('ibis4test-postgres-xa', 'PostgreSQL JDBC Driver', authAliasNam
 		[['name', 'URL'],  ['value', 'jdbc:postgresql://host.docker.internal:5432/testiaf']]
 	])
 
-createDatasource('ibis4test-db2', 'DB2 JDBC Driver', authAliasName, [
+createDatasource('ibis4test-db2-xa', 'DB2 JDBC Driver', authAliasName, [
 		[['name', 'serverName'], ['value', 'host.docker.internal']],
 		[['name', 'portNumber'], ['value', '50000']],
 		[['name', 'databaseName'], ['value', 'testiaf']],
