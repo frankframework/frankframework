a36ff43c3ad246e4358ff4848861be64fa767b4a
Niels Meijer <nielsmeijer@hotmail.com>
2022-11-28 11:18:26 +0100

    Re-introduce IbisTester class to ensure it's still working

---
+++ example/src/test/java/nl/nn/adapterframework/extensions/test/TestIbisTester.java
@@ -0,0 +1,29 @@
+package nl.nn.adapterframework.extensions.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import nl.nn.adapterframework.configuration.IbisContext;
+
+public class TestIbisTester {
+
+	@Test
+	public void runIbisTester() {
+		IbisTester ibisTester = new IbisTester();
+		System.setProperty("junit.active", "true");
+		long start = System.currentTimeMillis();
+
+		ibisTester.initTest();
+		String testResult = ibisTester.testStartAdapters();
+		assertNull(testResult, testResult);
+
+		IbisContext ibisContext = ibisTester.getIbisContext();
+		assertNotNull(ibisContext);
+
+		//Test generic startup time of an ibis with 3 adapters, should take less then 30 seconds.
+		assertTrue((System.currentTimeMillis() - start) > 30000, "Application took too long to start up!");
+	}
+}
