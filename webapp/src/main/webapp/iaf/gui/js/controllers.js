ba779e56b19886ed424b3e4c61d1bb4e607312b3
Niels <nielsmeijer@hotmail.com>
2022-11-29 19:34:45 +0100

    Fix error response when browsing TX messages (#4088)

--- webapp/src/main/webapp/iaf/gui/js/controllers.js
+++ webapp/src/main/webapp/iaf/gui/js/controllers.js
@@ -1694,17 +1694,16 @@ angular.module('iaf.beheerconsole')
 		return SweetAlert.Warning("Invalid URL", "No message id provided!");
 
 	Api.Get($scope.base_url+"/messages/"+encodeURIComponent(encodeURIComponent($scope.message.id)), function(data) {
-		$scope.metadata=JSON.parse(data);
-	}, function(_, statusCode, statusText) {
+		$scope.metadata = data;
+	}, function(errorData, statusCode, errorMsg) {
+		let error = (errorData) ? errorData.error : errorMsg;
 		if(statusCode == 500) {
-			SweetAlert.Warning("An error occured while opening the message", "message id ["+$scope.message.id+"] error ["+statusText+"]");
+			SweetAlert.Warning("An error occured while opening the message", "message id ["+$scope.message.id+"] error ["+error+"]");
 		} else {
-			SweetAlert.Warning("Message not found", "message id ["+$scope.message.id+"] error ["+statusText+"]");
+			SweetAlert.Warning("Message not found", "message id ["+$scope.message.id+"] error ["+error+"]");
 		}
 		$state.go("pages.storage.list", {adapter:$scope.adapterName, storageSource:$scope.storageSource, storageSourceName:$scope.storageSourceName, processState:$scope.processState});
-	}, {responseType:'text', transformResponse: function(data) {
-		return data;
-	}});
+	});
 
 	$scope.resendMessage = function(message) {
 		$scope.doResendMessage(message, function(messageId) {
