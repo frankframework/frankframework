6dacbd8c3b2cbb67ef5eae6387208dab4f3d9785
ricardovh <57142353+ricardovh@users.noreply.github.com>
2022-12-13 15:00:03 +0100

    Move images for CI to separate repo (#4158)

--- docker/mq/artemis/rebuild.bat
+++
@@ -1,7 +0,0 @@
-
-docker container stop iaf-test-activemq-artemis
-docker container rm iaf-test-activemq-artemis
-
-docker build -t iaf-test-activemq-artemis %~dp0
-
-docker run --publish 8160:8161 --publish 61615:61616 --restart unless-stopped --name iaf-test-activemq-artemis iaf-test-activemq-artemis
