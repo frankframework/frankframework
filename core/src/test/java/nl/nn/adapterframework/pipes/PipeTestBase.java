a99294caca975f3e624cae828dfd2fe240e83867
Niels Meijer <nielsmeijer@hotmail.com>
2023-01-10 12:33:35 +0100

    Fix tests

--- core/src/test/java/nl/nn/adapterframework/pipes/PipeTestBase.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/PipeTestBase.java
@@ -74,7 +74,7 @@ public abstract class PipeTestBase<P extends IPipe> extends ConfiguredTestBase {
 		return doPipe(pipe, Message.asMessage(input), session);
 	}
 	protected PipeRunResult doPipe(P pipe, Message input, PipeLineSession session) throws PipeRunException {
-		session.put(PipeLineSession.originalMessageKey, input);
+		session.computeIfAbsent(PipeLineSession.originalMessageKey, k -> input);
 		return pipe.doPipe(input, session);
 	}
 
