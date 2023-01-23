35fcd3c556bb4110e970c9a7a331c302e1ade270
Tim van der Leeuw <tnleeuw@gmail.com>
2023-01-23 17:19:55 +0100

    Issues/4276 timeout guards extra logging (7.7) (#4376)

--- core/src/test/java/nl/nn/adapterframework/receivers/SlowPullingListener.java
+++ core/src/test/java/nl/nn/adapterframework/receivers/SlowPullingListener.java
@@ -8,15 +8,15 @@ import nl.nn.adapterframework.core.ListenerException;
 import nl.nn.adapterframework.core.PipeLineResult;
 import nl.nn.adapterframework.stream.Message;
 
-public class SlowStartingPullingListener extends SlowStartingListenerBase implements IPullingListener<String> {
+public class SlowPullingListener extends SlowListenerBase implements IPullingListener<javax.jms.Message> {
 
 	@Override
-	public String getIdFromRawMessage(String rawMessage, Map<String, Object> context) throws ListenerException {
+	public String getIdFromRawMessage(javax.jms.Message rawMessage, Map<String, Object> context) throws ListenerException {
 		return null;
 	}
 
 	@Override
-	public Message extractMessage(String rawMessage, Map<String, Object> context) throws ListenerException {
+	public Message extractMessage(javax.jms.Message rawMessage, Map<String, Object> context) throws ListenerException {
 		return Message.asMessage(rawMessage);
 	}
 
@@ -31,11 +31,11 @@ public class SlowStartingPullingListener extends SlowStartingListenerBase implem
 
 	@Override
 	public void closeThread(Map<String, Object> threadContext) throws ListenerException {
-		
+		log.debug("closeThread called in slow pulling listener");
 	}
 
 	@Override
-	public String getRawMessage(Map<String, Object> threadContext) throws ListenerException {
+	public javax.jms.Message getRawMessage(Map<String, Object> threadContext) throws ListenerException {
 		return null;
 	}
 
