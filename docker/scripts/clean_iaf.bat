8864f4402df61bddd2ed8203e93de15af940c7c7
Sergi Philipsens <philipsen.sergi@gmail.com>
2023-01-26 17:11:38 +0100

    feat: reorganize docker images and merge tomcat and tomcat weapp

--- docker/scripts/clean_iaf.bat
+++
@@ -1,7 +0,0 @@
-call %~dp0/setenv.bat
-
-call %MVN% %CLI_OPTS% -f %IAF_DIR% -s "%MVN_SETTINGS%" clean
-
-rem uncomment the below to clean up (all) docker images and volumes
-rem docker image prune --force
-rem docker volume prune --force
