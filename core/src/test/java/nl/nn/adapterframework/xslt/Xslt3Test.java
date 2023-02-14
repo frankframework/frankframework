a1e9d7de6a49daa86b6fdf2d88fd98f96e912b6d
Vinh Ban Pham <vinh.ban.pham@nn-group.com>
2023-02-14 10:07:45 +0100

    Rename and cleanup Xslt3Test

--- core/src/test/java/nl/nn/adapterframework/xslt/Xslt3Test.java
+++ core/src/test/java/nl/nn/adapterframework/xslt/Xslt3Test.java
@@ -24,89 +24,65 @@ import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.testutil.TestFileUtils;
 import nl.nn.adapterframework.util.TransformerPool.OutputType;
 
-public class XsltXml2JsonConversionTest extends XsltErrorTestBase<XsltPipe>{
+public class Xslt3Test extends XsltErrorTestBase<XsltPipe>{
 
 	@Override
 	public XsltPipe createPipe() {
 		return new XsltPipe();
 	}
-	
-	protected void assertResultsAreCorrect(String expected, String actual, PipeLineSession session) {
+
+	protected void assertResultsAreCorrect(String expected, String actual) {
 		assertEquals(expected,actual);
 	}
-	
-	protected void testXslt(String styleSheetName, String input, String expected, Boolean omitXmlDeclaration, Boolean skipEmptyTags, Boolean removeNamespaces) throws Exception {
+
+	protected void testXslt(String styleSheetName, String input, String expected) throws Exception {
 		pipe.setStyleSheetName(styleSheetName);
-		if (omitXmlDeclaration!=null) {
-			pipe.setOmitXmlDeclaration(omitXmlDeclaration);
-		}
-		if (skipEmptyTags!=null) {
-			pipe.setSkipEmptyTags(skipEmptyTags);
-		}
-		if (removeNamespaces!=null) {
-			pipe.setRemoveNamespaces(removeNamespaces);
-		}
 		pipe.configure();
 		pipe.start();
 
 		PipeRunResult prr = doPipe(pipe, input, session);
 		String result = Message.asString(prr.getResult());
-		assertResultsAreCorrect(expected, result.trim(), session);
+		assertResultsAreCorrect(expected, result.trim());
 	}
-	
+
 	@Test
 	public void testBasicWrappedJsonConversion() throws Exception {
 		String styleSheetName = "/Xslt3/conversion/jsonToXmlConversion.xsl";
 		String input = TestFileUtils.getTestFile("/Xslt3/conversion/wrappedOriginalJson.json");
-		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/expectedXml.xml");
-		Boolean omitXmlDeclaration=null;
-		Boolean skipEmptyTags=null;
-		Boolean removeNamespaces=null;
-		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/expectedXml.xml");
+		testXslt(styleSheetName, input, expected);
 	}
-	
+
 	@Test
 	public void testComplexWrappedJsonConversion() throws Exception {
 		String styleSheetName = "/Xslt3/conversion/jsonToXmlConversion.xsl";
 		String input = TestFileUtils.getTestFile("/Xslt3/conversion/complexOriginalJson.json");
-		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedXml.xml");
-		Boolean omitXmlDeclaration=null;
-		Boolean skipEmptyTags=null;
-		Boolean removeNamespaces=null;
-		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedXml.xml");
+		testXslt(styleSheetName, input, expected);
 	}
 
 	@Test
 	public void testBasicReturnXmlConversion() throws Exception {
 		String styleSheetName = "/Xslt3/conversion/xmlToJsonConversion.xsl";
 		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnOriginalXml.xml");
-		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/expectedJson.json");
-		Boolean omitXmlDeclaration=null;
-		Boolean skipEmptyTags=null;
-		Boolean removeNamespaces=null;
-		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/expectedJson.json");
+		testXslt(styleSheetName, input, expected);
 	}
-	
+
 	@Test
 	public void testComplexReturnXmlConversion() throws Exception {
 		String styleSheetName = "/Xslt3/conversion/xmlToJsonConversion.xsl";
 		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnComplexOriginalXml.xml");
-		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedJson.json");
-		Boolean omitXmlDeclaration=null;
-		Boolean skipEmptyTags=null;
-		Boolean removeNamespaces=null;
-		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedJson.json");
+		testXslt(styleSheetName, input, expected);
 	}
-	
+
 	@Test
 	public void testParameterizedJsonConversion() throws Exception {
 		String styleSheetName = "/Xslt3/conversion/unwrappedJsontoXmlConversion.xsl";
 		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnComplexOriginalXml.xml");
 		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/outputXml.xml");
-		Boolean omitXmlDeclaration=null;
-		Boolean skipEmptyTags=null;
-		Boolean removeNamespaces=null;
-		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+		testXslt(styleSheetName, input, expected);
 	}
 
 	@Override
