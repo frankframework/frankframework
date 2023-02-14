f33071baf9e50ec8ce87c135b3ec48b99779c83f
Vinh Ban Pham <vinh.ban.pham@nn-group.com>
2023-02-14 08:25:12 +0100

    Refactor and parameterize xsltconversion test

--- core/src/test/java/nl/nn/adapterframework/xslt/XsltXml2JsonConversionTest.java
+++ core/src/test/java/nl/nn/adapterframework/xslt/XsltXml2JsonConversionTest.java
@@ -1,25 +1,30 @@
 package nl.nn.adapterframework.xslt;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 import java.io.File;
 import java.io.StringReader;
 import java.io.StringWriter;
 
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.core.PipeRunResult;
-import nl.nn.adapterframework.pipes.PipeTestBase;
 import nl.nn.adapterframework.pipes.XsltPipe;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.testutil.TestFileUtils;
+import nl.nn.adapterframework.util.TransformerPool.OutputType;
 
-public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
+public class XsltXml2JsonConversionTest extends XsltErrorTestBase<XsltPipe>{
 
 	@Override
 	public XsltPipe createPipe() {
@@ -51,8 +56,8 @@ public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
 	
 	@Test
 	public void testBasicWrappedJsonConversion() throws Exception {
-		String styleSheetName=  "/Xslt3/conversion/jsonToXmlConversion.xsl";
-		String input   =TestFileUtils.getTestFile("/Xslt3/conversion/wrappedOriginalJson.json");
+		String styleSheetName = "/Xslt3/conversion/jsonToXmlConversion.xsl";
+		String input = TestFileUtils.getTestFile("/Xslt3/conversion/wrappedOriginalJson.json");
 		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/expectedXml.xml");
 		Boolean omitXmlDeclaration=null;
 		Boolean skipEmptyTags=null;
@@ -62,8 +67,8 @@ public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
 	
 	@Test
 	public void testComplexWrappedJsonConversion() throws Exception {
-		String styleSheetName=  "/Xslt3/conversion/jsonToXmlConversion.xsl";
-		String input   =TestFileUtils.getTestFile("/Xslt3/conversion/complexOriginalJson.json");
+		String styleSheetName = "/Xslt3/conversion/jsonToXmlConversion.xsl";
+		String input = TestFileUtils.getTestFile("/Xslt3/conversion/complexOriginalJson.json");
 		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedXml.xml");
 		Boolean omitXmlDeclaration=null;
 		Boolean skipEmptyTags=null;
@@ -73,8 +78,8 @@ public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
 
 	@Test
 	public void testBasicReturnXmlConversion() throws Exception {
-		String styleSheetName=  "/Xslt3/conversion/xmlToJsonConversion.xsl";
-		String input   =TestFileUtils.getTestFile("/Xslt3/conversion/returnOriginalXml.xml");
+		String styleSheetName = "/Xslt3/conversion/xmlToJsonConversion.xsl";
+		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnOriginalXml.xml");
 		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/expectedJson.json");
 		Boolean omitXmlDeclaration=null;
 		Boolean skipEmptyTags=null;
@@ -84,8 +89,8 @@ public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
 	
 	@Test
 	public void testComplexReturnXmlConversion() throws Exception {
-		String styleSheetName=  "/Xslt3/conversion/xmlToJsonConversion.xsl";
-		String input   =TestFileUtils.getTestFile("/Xslt3/conversion/returnComplexOriginalXml.xml");
+		String styleSheetName = "/Xslt3/conversion/xmlToJsonConversion.xsl";
+		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnComplexOriginalXml.xml");
 		String expected=TestFileUtils.getTestFile("/Xslt3/conversion/complexExpectedJson.json");
 		Boolean omitXmlDeclaration=null;
 		Boolean skipEmptyTags=null;
@@ -95,24 +100,62 @@ public class XsltXml2JsonConversionTest extends PipeTestBase<XsltPipe>{
 	
 	@Test
 	public void testParameterizedJsonConversion() throws Exception {
-		Source template = (new StreamSource(new File("/Xslt3/conversion/unwrappedJsontoXmlConversion.xsl")));
-		
-		StringWriter writer = new StringWriter();
-		Result output = new StreamResult(writer);
-		
-		try {
-			Transformer transformer = TransformerFactory.newInstance().newTransformer(template);
-			transformer.transform(new StreamSource(new StringReader("<empty/>")), output); // Empty xml necessary for transformer
-			
-		} catch(TransformerException e){
-			e.printStackTrace();
-		}
-		
-		
-		String result = writer.toString();
+		String styleSheetName = "/Xslt3/conversion/unwrappedJsontoXmlConversion.xsl";
+		String input = TestFileUtils.getTestFile("/Xslt3/conversion/returnComplexOriginalXml.xml");
 		String expected = TestFileUtils.getTestFile("/Xslt3/conversion/outputXml.xml");
-		
-		assertEquals(expected, result.trim());
-		
+		Boolean omitXmlDeclaration=null;
+		Boolean skipEmptyTags=null;
+		Boolean removeNamespaces=null;
+		testXslt(styleSheetName, input, expected, omitXmlDeclaration, skipEmptyTags, removeNamespaces);
+	}
+
+	@Override
+	protected void setXpathExpression(String xpathExpression) {
+		pipe.setXpathExpression(xpathExpression);
+	}
+
+	@Override
+	protected void setStyleSheetName(String styleSheetName) {
+		pipe.setStyleSheetName(styleSheetName);
+	}
+
+	@Override
+	protected void setStyleSheetNameSessionKey(String styleSheetNameSessionKey) {
+		pipe.setStyleSheetNameSessionKey(styleSheetNameSessionKey);
+	}
+
+	@Override
+	protected void setOmitXmlDeclaration(boolean omitXmlDeclaration) {
+		pipe.setOmitXmlDeclaration(omitXmlDeclaration);
+	}
+
+	@Override
+	protected void setIndent(boolean indent) {
+		pipe.setIndentXml(indent);
+	}
+
+	@Override
+	protected void setSkipEmptyTags(boolean skipEmptyTags) {
+		pipe.setSkipEmptyTags(skipEmptyTags);
+	}
+
+	@Override
+	protected void setRemoveNamespaces(boolean removeNamespaces) {
+		pipe.setRemoveNamespaces(removeNamespaces);
+	}
+
+	@Override
+	protected void setXslt2(boolean xslt2) {
+		pipe.setXslt2(xslt2);
+	}
+
+	@Override
+	protected void setOutputType(OutputType outputType) {
+		pipe.setOutputType(outputType);
+	}
+
+	@Override
+	protected void setHandleLexicalEvents(boolean handleLexicalEvents) {
+		pipe.setHandleLexicalEvents(handleLexicalEvents);
 	}
 }
