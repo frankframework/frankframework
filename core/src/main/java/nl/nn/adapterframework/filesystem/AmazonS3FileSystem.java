c76260afeb5f799ae105ff28662ca1567893f5cf
Laurens Mäkel <37303445+Laurens-makel@users.noreply.github.com>
2023-03-16 16:11:48 +0100

    Fix duplicate name attribute in AmazonS3Sender (#4604)

--- core/src/main/java/nl/nn/adapterframework/filesystem/AmazonS3FileSystem.java
+++ core/src/main/java/nl/nn/adapterframework/filesystem/AmazonS3FileSystem.java
@@ -85,8 +85,9 @@ public class AmazonS3FileSystem extends FileSystemBase<S3Object> implements IWri
 	@Override
 	public void configure() throws ConfigurationException {
 
-		if (StringUtils.isEmpty(getAccessKey()) || StringUtils.isEmpty(getSecretKey()))
-			throw new ConfigurationException(" empty credential fields, please prodive aws credentials");
+		if (StringUtils.isEmpty(getAuthAlias()) && StringUtils.isEmpty(getAccessKey()) && StringUtils.isEmpty(getSecretKey())) {
+			throw new ConfigurationException(" empty credential fields, please prodive aws credentials (accessKey and secretKey / authAlias)");
+		}
 
 		if (StringUtils.isEmpty(getClientRegion()) || !AVAILABLE_REGIONS.contains(getClientRegion()))
 			throw new ConfigurationException(" invalid region [" + getClientRegion() + "] please use one of the following supported regions " + AVAILABLE_REGIONS.toString());
@@ -312,7 +313,7 @@ public class AmazonS3FileSystem extends FileSystemBase<S3Object> implements IWri
 	@Override
 	public Map<String, Object> getAdditionalFileProperties(S3Object f) {
 		Map<String, Object> attributes = new HashMap<String, Object>();
-		attributes.put("name", bucketName);
+		attributes.put("bucketName", bucketName);
 		return attributes;
 	}
 	
