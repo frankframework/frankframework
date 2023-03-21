4aa73120d86496610079a4cf9616c8fa637c2deb
Tim van der Leeuw <tim@wearefrank.nl>
2023-03-21 08:09:16 +0100

    Add better JavaDoc

--- iaf-commons/src/main/java/nl/nn/adapterframework/functional/FunctionalUtil.java
+++ iaf-commons/src/main/java/nl/nn/adapterframework/functional/FunctionalUtil.java
@@ -25,6 +25,15 @@ public class FunctionalUtil {
 
 	/**
 	 * Helper function to create a {@link Supplier} to supply the single constant argument value.
+	 * <p>
+	 *     This function is useful to disambiguate method-overloads when an array of mixed arguments should be passed all as type {@link Supplier}
+	 *     to a function that takes a number of {@link Object} parameters, such as Log4J log methods (in particular the
+	 *     methods where an exception is passed as last argument).
+	 *     For example:
+	 *     <code>
+	 *         log.error("{} Error with message id [{}]", supplier(this::getLogPrefix), supply(messageId), e);
+	 *     </code>
+	 * </p>
 	 *
 	 * @param value Value to be supplied. NB: This should be a constant, otherwise its value is instantly
 	 *              computed instead of being delayed on-demand!
@@ -38,6 +47,27 @@ public class FunctionalUtil {
 	/**
 	 * Helper function to cast parameter as a {@link Supplier} when the compiler cannot work it
 	 * out by itself.
+	 * <p>
+	 *     This function is useful to disambiguate method-overloads when an array of mixed arguments should be passed all as type {@link Supplier}
+	 *     to a function that takes a number of {@link Object} parameters, such as Log4J log methods (in particular the
+	 *     methods where an exception is passed as last argument).
+	 * </p>
+	 * <p>
+	 *     For example:
+	 *     <code>
+	 *         log.error("{} Error with message id [{}]", supplier(this::getLogPrefix), supply(messageId), e);
+	 *     </code>
+	 *     This can also be useful when for instance a no-arguments function should be passed to a JUnit arguments
+	 *     supplier for a parameterized unit test:
+	 *     <code>
+	 *	public static Stream<Arguments> transactionManagers() {
+	 * 		return Stream.of(
+	 * 			Arguments.of(supplier(ReceiverTest::buildNarayanaTransactionManager)),
+	 * 			Arguments.of(supplier(ReceiverTest::buildBtmTransactionManager))
+	 * 		);
+	 *        }
+	 *     </code>
+	 * </p>
 	 *
 	 * @param s Supplier lambda or function reference.
 	 * @return The same Supplier but now type verified by the compiler.
@@ -50,8 +80,22 @@ public class FunctionalUtil {
 	/**
 	 * Helper function to cast parameter as {@link Function} when the compiler cannot work it
 	 * out by itself.
+	 * <p>
+	 *     This function is useful when a single-argument function needs to be passed to a method
+	 *     where the compiler cannot determine correct argument types due to method overloads of
+	 *     methods that take generic object parameters, such as JUnit {@code Arguments.of()}.
+	 * </p>
+	 * <p>For Example:</p>
+	 * <code>
+	 *  public static Stream<Arguments> transactionManagers() {
+	 * 		return Stream.of(
+	 * 			Arguments.of(function(ReceiverTest::buildNarayanaTransactionManager)),
+	 * 			Arguments.of(function(ReceiverTest::buildBtmTransactionManager))
+	 * 		);
+	 *  }
+	 * </code>
 	 *
-	 * @param f Lambda or function reference that takes a single parameter and has return type.
+	 * @param f Lambda or function reference that takes a single parameter and returns a value.
 	 * @return Same lambda or function reference but now type verified by the compiler.
 	 * @param <T> Type of the argument to the function parameter.
 	 * @param <R> Return type of the function parameter.
