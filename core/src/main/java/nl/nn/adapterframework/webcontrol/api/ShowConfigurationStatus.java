dc1305ceec23d0eafe8bbc327f1877179ad8ff69
Niels <nielsmeijer@hotmail.com>
2023-03-24 11:39:21 +0100

    Apply suggestions from code review

--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/ShowConfigurationStatus.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/ShowConfigurationStatus.java
@@ -719,6 +719,7 @@ public final class ShowConfigurationStatus extends Base {
 				try {
 					errorStoreMessageCount += esmb.getMessageCount();
 				} catch (ListenerException e) {
+						// Only log the stacktrace when loglevel == INFO. Otherwise it will pollute the log too much.
 					if(log.isInfoEnabled()) log.warn("Cannot determine number of messages in errorstore of [{}]", rcv.getName(), e);
 					else log.warn("Cannot determine number of messages in errorstore of [{}]: {}", rcv::getName, e::getMessage);
 				}
@@ -728,6 +729,7 @@ public final class ShowConfigurationStatus extends Base {
 				try {
 					messageLogMessageCount += mlmb.getMessageCount();
 				} catch (ListenerException e) {
+						// Only log the stacktrace when loglevel == INFO. Otherwise it will pollute the log too much.
 					if(log.isInfoEnabled()) log.warn("Cannot determine number of messages in errorstore of [{}]", rcv.getName(), e);
 					else log.warn("Cannot determine number of messages in errorstore of [{}]: {}", rcv::getName, e::getMessage);
 				}
