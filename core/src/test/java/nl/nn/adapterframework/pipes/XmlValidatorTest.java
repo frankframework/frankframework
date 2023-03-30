d4d52e130d987a4ef75fbe0a19f7460702a798b5
Tim van der Leeuw <tim@wearefrank.nl>
2023-03-30 22:49:01 +0200

    Rewrite more tests

--- core/src/test/java/nl/nn/adapterframework/pipes/XmlValidatorTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/XmlValidatorTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -38,6 +39,14 @@ import nl.nn.adapterframework.validation.XmlValidatorTestBase;
 public class XmlValidatorTest extends XmlValidatorTestBase {
 
 	private Class<AbstractXmlValidator> implementation;
+
+	private TestConfiguration configuration;
+
+	@Before
+	public void setup() {
+		configuration = new TestConfiguration();
+		ApplicationWarnings.removeInstance();
+	}
 
 	public XmlValidatorTest(Class<AbstractXmlValidator> implementation) {
 		this.implementation = implementation;
@@ -85,11 +94,15 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	}
 
 	private static void assertNoWarnings(TestConfiguration configuration) {
+		assertWarnings(configuration, 0);
+	}
+
+	private static void assertWarnings(TestConfiguration configuration, int expectedNrOfWarnings) {
 		// NB: We should get ConfigurationWarnings not ApplicationWarnings, but we check both just to be sure because in the past logic has changed
 		// in other places in the code, which silently made warnings go to one instead of the other, making checks on just one
 		// of them useless (as the warnings would never show up there anymore).
 		// By checking both we prevent this in the future.
-		assertEquals("No ConfigurationWarnings expected, got " + configuration.getConfigurationWarnings().getWarnings(), 0, configuration.getConfigurationWarnings().size());
+		assertEquals(expectedNrOfWarnings + " ConfigurationWarnings expected, got " + configuration.getConfigurationWarnings().getWarnings(), expectedNrOfWarnings, configuration.getConfigurationWarnings().size());
 		assertEquals("No ApplicationWarnings expected, got " + ApplicationWarnings.getWarningsList(), 0, ApplicationWarnings.getSize());
 	}
 
@@ -129,17 +142,13 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 		try {
 			PipeRunResult result=validator.doPipe(new Message(testXml), session);
 			PipeForward forward=result.getPipeForward();
-			evaluateResult(forward.getName(), session, null, expectedFailureReasons);
+			ValidationResult validationResult = forward.getName().equals("success") ? ValidationResult.VALID : ValidationResult.INVALID;
+			evaluateResult(validationResult, session, null, expectedFailureReasons);
 		} catch (Exception e) {
 			evaluateResult(ValidationResult.INVALID, session, e, expectedFailureReasons);
 			return ValidationResult.INVALID;
 		}
 		return null;
-	}
-
-	public void evaluateResult(String forwardName, PipeLineSession session, Exception e, String[] expectedFailureReasons) {
-		ValidationResult validationResult = forwardName.equals("success") ? ValidationResult.VALID : ValidationResult.INVALID;
-		evaluateResult(validationResult, session, e, expectedFailureReasons);
 	}
 
 	@Override
@@ -154,92 +163,83 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 		return runAndEvaluate(validator, inputfile, expectedFailureReasons);
 	}
 
-
-	/*
-	 * <tr> <td>{@link #setSoapNamespace(String) soapNamespace}</td> <td>the
-	 * namespace of the SOAP Envelope, when this property has a value and the
-	 * input message is a SOAP Message the content of the SOAP Body is used for
-	 * validation, hence the SOAP Envelope and SOAP Body elements are not
-	 * considered part of the message to validate. Please note that this
-	 * functionality is deprecated, using {@link
-	 * nl.nn.adapterframework.soap.SoapValidator} is now the preferred solution
-	 * in case a SOAP Message needs to be validated, in other cases give this
-	 * property an empty value</td>
-	 * <td>http://schemas.xmlsoap.org/soap/envelope/</td></tr>
-	 *
-	 */
-	public void testSoapNamespaceFeature(String schema, String root, String inputFile) throws ConfigurationException, IOException, PipeStartException {
-		XmlValidator validator = new XmlValidator();
+	@Test
+	public void testSoapNamespaceFeature() throws ConfigurationException, IOException, PipeStartException {
+		// Arrange
+		XmlValidator validator = buildXmlValidator(configuration, null, NO_NAMESPACE_SOAP_MSGROOT);
+		validator.setFullSchemaChecking(true);
 		try {
 			validator.setImplementation(implementation);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
-		validator.registerForward(createSuccessForward());
-		validator.setThrowException(true);
 		validator.setFullSchemaChecking(true);
-		validator.setRoot(root);
+		validator.setRoot(NO_NAMESPACE_SOAP_MSGROOT);
 		validator.setSoapNamespace("http://www.w3.org/2003/05/soap-envelope");
-		validator.setSchema(schema);
+		validator.setSchema(NO_NAMESPACE_SCHEMA);
+
+		// Act
 		validator.configure();
 		validator.start();
 
-		assertNull(runAndEvaluate(validator, inputFile, null));
-	}
+		// Assert
+		assertNoWarnings(configuration);
 
-	@Test
-	public void testSoapNamespaceFeature() throws ConfigurationException, IOException, PipeRunException, XmlValidatorException, PipeStartException {
-		testSoapNamespaceFeature(NO_NAMESPACE_SCHEMA,NO_NAMESPACE_SOAP_MSGROOT,NO_NAMESPACE_SOAP_FILE);
-	}
-
-	public void testStoreRootElement(String schema, String root, String inputFile) throws Exception {
-		XmlValidator validator = new XmlValidator();
-
-		validator.registerForward(createSuccessForward());
-		validator.setThrowException(true);
-		validator.setFullSchemaChecking(true);
-		validator.setRoot(root);
-		validator.setRootElementSessionKey("rootElement");
-		validator.setSchemaLocation(schema);
-		validator.configure();
-		validator.start();
-
-		String testXml = inputFile != null ? getTestXml(inputFile + ".xml") : null;
-		PipeLineSession session = new PipeLineSession();
-		PipeRunResult result = validator.doPipe(new Message(testXml), session);
-		PipeForward forward = result.getPipeForward();
-
-		assertEquals(root, session.get("rootElement"));
-		assertEquals("success", forward.getName());
+		// Act / Assert 2
+		assertNull(runAndEvaluate(validator, NO_NAMESPACE_SOAP_FILE, null));
 	}
 
 	@Test
 	public void testStoreRootElement() throws Exception {
-		testStoreRootElement(SCHEMA_LOCATION_BASIC_A_OK, "A", INPUT_FILE_BASIC_A_OK);
+		// Arrange
+		XmlValidator validator = buildXmlValidator(configuration, SCHEMA_LOCATION_BASIC_A_OK, "A");
+		validator.setFullSchemaChecking(true);
+		validator.setRootElementSessionKey("rootElement");
+
+		// Act
+		validator.configure();
+		validator.start();
+
+		// Assert
+		assertNoWarnings(configuration);
+
+		// Arrange 2
+		String testXml = getTestXml(INPUT_FILE_BASIC_A_OK + ".xml");
+		PipeLineSession session = new PipeLineSession();
+
+		// Act 2
+		PipeRunResult result = validator.doPipe(new Message(testXml), session);
+		PipeForward forward = result.getPipeForward();
+
+		// Assert 2
+		assertEquals("A", session.get("rootElement"));
+		assertEquals("success", forward.getName());
 	}
 
 	@Test
 	public void testWrongRootElement() throws Exception {
-		String schema = SCHEMA_LOCATION_BASIC_A_OK;
-		String inputFile = INPUT_FILE_BASIC_A_OK;
-		XmlValidator validator = new XmlValidator();
-
-		validator.registerForward(createSuccessForward());
-		validator.registerForward(createFailureForward());
-
+		// Arrange
+		XmlValidator validator = buildXmlValidator(configuration, SCHEMA_LOCATION_BASIC_A_OK,  "anotherElement");
 		validator.setFullSchemaChecking(true);
-		validator.setRoot("anotherElement");
 		validator.setReasonSessionKey("reason");
-		validator.setSchemaLocation(schema);
+		validator.setThrowException(false);
+
+		// Act
 		validator.configure();
 		validator.start();
 
-		String testXml = inputFile != null ? getTestXml(inputFile + ".xml") : null;
+		// Assert
+		assertWarnings(configuration, 1);
+
+		// Arrange 2
+		String testXml = getTestXml(INPUT_FILE_BASIC_A_OK + ".xml");
 		PipeLineSession session = new PipeLineSession();
+
+		// Act 2
 		PipeRunResult result = validator.doPipe(new Message(testXml), session);
 		PipeForward forward = result.getPipeForward();
 
+		// Assert 2
 		assertEquals("failure", forward.getName());
 		assertThat((String)session.get("reason"), containsString("Illegal element 'A'. Element(s) 'anotherElement' expected."));
 	}
@@ -247,74 +247,81 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 
 	@Test
 	public void testMultipleRootElement() throws Exception {
-		String schema = SCHEMA_LOCATION_BASIC_A_OK;
+		// Arrange
 		String root = "A";
-		String inputFile = INPUT_FILE_BASIC_A_OK;
-		XmlValidator validator = new XmlValidator();
-
-		validator.registerForward(createSuccessForward());
-		validator.registerForward(createFailureForward());
-
+		// if multiple root elements are specified, in a comma separated list, the validation succeeds if one of these root elements is found
+		XmlValidator validator = buildXmlValidator(configuration, SCHEMA_LOCATION_BASIC_A_OK, root + ",anotherElement");
 		validator.setFullSchemaChecking(true);
-		validator.setRoot(root+",anotherElement"); // if multiple root elements are specified, in a comma separated list, the validation succeeds if one of these root elements is found
-		validator.setSchemaLocation(schema);
+
+		// Act
 		validator.configure();
 		validator.start();
 
-		String testXml = inputFile != null ? getTestXml(inputFile + ".xml") : null;
+		// Assert
+		assertNoWarnings(configuration);
+
+		// Arrange 2
+		String testXml = getTestXml(INPUT_FILE_BASIC_A_OK + ".xml");
 		PipeLineSession session = new PipeLineSession();
+
+		// Act 2
 		PipeRunResult result = validator.doPipe(new Message(testXml), session);
 		PipeForward forward = result.getPipeForward();
 
+		// Assert 2
 		assertEquals("success", forward.getName());
 	}
 
 	@Test
 	public void testRuntimeRootElement() throws Exception {
-		String schema = SCHEMA_LOCATION_BASIC_A_OK;
-		String root = "A";
-		String inputFile = INPUT_FILE_BASIC_A_OK;
-		XmlValidator validator = new XmlValidator();
-
-		validator.registerForward(createSuccessForward());
-		validator.registerForward(createFailureForward());
-
+		// Arrange
+		// if multiple root elements are specified, in a comma separated list, the validation succeeds if one of these root elements is found
+		XmlValidator validator = buildXmlValidator(configuration, SCHEMA_LOCATION_BASIC_A_OK, "oneElement,anotherElement");
 		validator.setFullSchemaChecking(true);
-		validator.setRoot("oneElement,anotherElement"); // if multiple root elements are specified, in a comma separated list, the validation succeeds if one of these root elements is found
-		validator.setSchemaLocation(schema);
+
+		// Act
 		validator.configure();
 		validator.start();
 
-		String testXml = inputFile != null ? getTestXml(inputFile + ".xml") : null;
+		// Assert
+		assertWarnings(configuration, 2);
+
+		// Arrange 2
+		String testXml = getTestXml(INPUT_FILE_BASIC_A_OK + ".xml");
 		PipeLineSession session = new PipeLineSession();
-		PipeRunResult result = validator.validate(new Message(testXml), session, root);
+
+		// Act 2
+		PipeRunResult result = validator.validate(new Message(testXml), session, "A");
 		PipeForward forward = result.getPipeForward();
 
+		// Assert 2
 		assertEquals("success", forward.getName());
 	}
 
 	@Test
 	public void testWrongRuntimeRootElement() throws Exception {
-		String schema = SCHEMA_LOCATION_BASIC_A_OK;
+		// Arrange
 		String root = "A";
-		String inputFile = INPUT_FILE_BASIC_A_OK;
-		XmlValidator validator = new XmlValidator();
-
-		validator.registerForward(createSuccessForward());
-		validator.registerForward(createFailureForward());
-
-		validator.setFullSchemaChecking(true);
-		validator.setRoot(root);
+		XmlValidator validator = buildXmlValidator(configuration, SCHEMA_LOCATION_BASIC_A_OK, root);
 		validator.setReasonSessionKey("reason");
-		validator.setSchemaLocation(schema);
+		validator.setThrowException(false);
+
+		// Act
 		validator.configure();
 		validator.start();
 
-		String testXml = inputFile != null ? getTestXml(inputFile + ".xml") : null;
+		// Assert
+		assertNoWarnings(configuration);
+
+		// Arrange 2
+		String testXml = getTestXml(INPUT_FILE_BASIC_A_OK + ".xml");
 		PipeLineSession session = new PipeLineSession();
+
+		// Act 2
 		PipeRunResult result = validator.validate(new Message(testXml), session, "anotherElement");
 		PipeForward forward = result.getPipeForward();
 
+		// Assert 2
 		assertEquals("failure", forward.getName());
 		assertThat((String)session.get("reason"), containsString("Illegal element 'A'. Element(s) 'anotherElement' expected."));
 	}
@@ -322,8 +329,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test
 	public void testWithCircularReferenceInXsd1() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://xmlns/a /Validation/Circular/AB/xsd/A.xsd", "A");
 
 		// Act
@@ -348,8 +353,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test
 	public void testWithCircularReferenceInXsd2() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://www.egem.nl/StUF/sector/zkn/0310 /Validation/Circular/zds/xsd/zkn0310_msg_zs-dms_resolved2017.xsd", "Point");
 
 		// Act
@@ -374,8 +377,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test
 	public void testMultipleImport() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "urn:frank/root /Validation/MultipleImport/xsd/root.xsd", "root");
 
 		// Act
@@ -401,8 +402,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test //copied from iaf-test /XmlValidator/scenario07a
 	public void testImportIncludeOK() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://nn.nl/root /Validation/ImportInclude/xsd/root.xsd", "root");
 
 		// Act
@@ -427,8 +426,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test
 	public void testImportNestedIncludeOK() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://nn.nl/root /Validation/ImportNestedInclude/xsd/root.xsd", "root");
 
 		// Act
@@ -452,8 +449,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test //copied from iaf-test /XmlValidator/scenario07b
 	public void testImportIncludeError() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://nn.nl/root /Validation/ImportInclude/xsd/root.xsd", "root");
 		// Override throwing exception
 		validator.setThrowException(false);
@@ -480,8 +475,6 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 	@Test //copied from iaf-test /XmlValidator/scenario05a, then simplified
 	public void testIncludeOK() throws Exception {
 		// Arrange
-		ApplicationWarnings.removeInstance();
-		TestConfiguration configuration = new TestConfiguration();
 		XmlValidator validator = buildXmlValidator(configuration, "http://www.ibissource.org/tom /Validation/Include/xsd/main.xsd", "GetParties");
 
 		// Act
@@ -505,20 +498,26 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 
 	@Test //copied from iaf-test /XmlValidator/scenario05b, then simplified
 	public void testIncludeError() throws Exception {
-		XmlValidator validator = new XmlValidator();
-		validator.registerForward(createSuccessForward());
-		validator.registerForward(createFailureForward());
-		validator.setRoot("GetParties");
-		validator.setSchemaLocation("http://www.ibissource.org/tom /Validation/Include/xsd/main.xsd");
-		validator.setAddNamespaceToSchema(true);
+		// Arrange
+		XmlValidator validator = buildXmlValidator(configuration, "http://www.ibissource.org/tom /Validation/Include/xsd/main.xsd", "GetParties");
+		validator.setThrowException(false);
+
+		// Act
 		validator.configure();
 		validator.start();
 
+		// Assert
+		assertNoWarnings(configuration);
+
+		// Arrange 2
 		String testXml = getTestXml("/Validation/Include/in-err.xml");
 		PipeLineSession session = new PipeLineSession();
+
+		// Act 2
 		PipeRunResult result = validator.validate(new Message(testXml), session, "GetParties");
 		PipeForward forward = result.getPipeForward();
 
+		// Assert 2
 		assertEquals("failure", forward.getName());
 	}
 
@@ -536,5 +535,4 @@ public class XmlValidatorTest extends XmlValidatorTestBase {
 
 		assertEquals("success", forward.getName());
 	}
-
 }
