f57dc4a8e84dc8a5c7862fd30a4ded3ed3125795
Sergi Philipsen <philipsen.sergi@gmail.com>
2023-04-03 19:45:48 +0200

    Replace Batch scipts by intuative Docker Compose files (#4704)

--- docker/WildFly/src/scripts/start.bat
+++
@@ -1,3 +0,0 @@
-call %~dp0/setenv.bat
-
-call %~dp0/../../../scripts/start-body.bat
--- docker/scripts/build-start-body.bat
+++
@@ -1,6 +0,0 @@
-docker image rm %IMAGE_NAME%
-
-echo "build %PRODUCT% iaf-test docker image" 
-call %MVN% %CLI_OPTS% -f %~dp0/../%PRODUCT% -s "%MVN_SETTINGS%" %MVN_OPTIONS% install
-
-call %~dp0/start-body.bat
