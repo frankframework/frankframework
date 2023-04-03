48457d9d8c22c891e207b8517d24faa7a8553530
Tim van der Leeuw <tim@wearefrank.nl>
2023-04-03 11:33:51 +0200

    Add msg

--- core/src/main/java/nl/nn/adapterframework/filesystem/FileSystemActor.java
+++ core/src/main/java/nl/nn/adapterframework/filesystem/FileSystemActor.java
@@ -374,7 +374,7 @@ public class FileSystemActor<F, FS extends IBasicFileSystem<F>> implements IOutp
 							} catch (NoSuchFileException e) {
 								log.debug("ignore", e);
 							} catch (FileSystemException e) {
-								throw new IOException("Could not delete file [" + fileSystem.getName(file) + "]", e);
+								throw new IOException("Could not delete file [" + fileSystem.getName(file) + "]: " + e.getMessage(), e);
 							}
 						}
 
@@ -383,7 +383,7 @@ public class FileSystemActor<F, FS extends IBasicFileSystem<F>> implements IOutp
 							try {
 								close();
 							} catch (Exception e) {
-								log.warn("Could not close file [" + fileSystem.getName(file) + "]", e);
+								log.warn("Could not close file [" + fileSystem.getName(file) + "]: " + e.getMessage(), e);
 							}
 							super.finalize();
 						}
