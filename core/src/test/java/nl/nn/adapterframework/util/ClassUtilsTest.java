8d4f6036918ca9c5de365c5eae511fefe09c7651
Niels Meijer <nielsmeijer@hotmail.com>
2023-04-11 12:26:46 +0200

    Use AssertAll

--- core/src/test/java/nl/nn/adapterframework/util/ClassUtilsTest.java
+++ core/src/test/java/nl/nn/adapterframework/util/ClassUtilsTest.java
@@ -2,6 +2,7 @@ package nl.nn.adapterframework.util;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -315,19 +316,20 @@ public class ClassUtilsTest {
 
 	@Test
 	public void testConvertToType() {
-		assertEquals(7, ClassUtils.convertToType(int.class, "7"));
-		assertEquals(7, ClassUtils.convertToType(Integer.class, "7"));
-		assertEquals(7L, ClassUtils.convertToType(long.class, "7"));
-		assertEquals(7L, ClassUtils.convertToType(Long.class, "7"));
-		assertEquals("7", ClassUtils.convertToType(String.class, "7"));
-		assertEquals(true, ClassUtils.convertToType(boolean.class, "true"));
-		assertEquals(true, ClassUtils.convertToType(Boolean.class, "true"));
-		assertEquals(false, ClassUtils.convertToType(Boolean.class, "niet true"));
-		assertEquals(TestEnum.ONE, ClassUtils.convertToType(TestEnum.class, "one"));
+		assertAll(
+			() -> assertEquals(7, ClassUtils.convertToType(int.class, "7")),
+			() -> assertEquals(7, ClassUtils.convertToType(Integer.class, "7")),
+			() -> assertEquals(7L, ClassUtils.convertToType(long.class, "7")),
+			() -> assertEquals(7L, ClassUtils.convertToType(Long.class, "7")),
+			() -> assertEquals("7", ClassUtils.convertToType(String.class, "7")),
+			() -> assertEquals(true, ClassUtils.convertToType(boolean.class, "true")),
+			() -> assertEquals(true, ClassUtils.convertToType(Boolean.class, "true")),
+			() -> assertEquals(false, ClassUtils.convertToType(Boolean.class, "niet true")),
+			() -> assertEquals(TestEnum.ONE, ClassUtils.convertToType(TestEnum.class, "one")),
 
-		assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(Object.class, "dummy"));
-		assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(Long.class, "dummy"));
-
-		assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(int.class, "")); //Empty string
+			() -> assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(Object.class, "dummy")),
+			() -> assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(Long.class, "dummy")),
+			() -> assertThrows(IllegalArgumentException.class, ()->ClassUtils.convertToType(int.class, "")) //Empty string
+		);
 	}
 }
