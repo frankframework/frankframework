b618ee4168d0724ab1131f71a3412cf0b47736a4
Niels Meijer <nielsmeijer@hotmail.com>
2023-04-25 17:01:50 +0200

    Move IbisAction

--- console/backend/src/main/java/nl/nn/adapterframework/management/web/ShowConfigurationStatus.java
+++ console/backend/src/main/java/nl/nn/adapterframework/management/web/ShowConfigurationStatus.java
@@ -34,7 +34,7 @@ import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang3.StringUtils;
 
-import nl.nn.adapterframework.configuration.IbisManager.IbisAction;
+import nl.nn.adapterframework.management.IbisAction;
 import nl.nn.adapterframework.management.bus.BusAction;
 import nl.nn.adapterframework.management.bus.BusTopic;
 
