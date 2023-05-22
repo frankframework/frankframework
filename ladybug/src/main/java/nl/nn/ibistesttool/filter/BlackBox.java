c3086be59e4896a2b8fd3b081fd6740be76b2e20
Jaco de Groot <jaco.de.groot@ibissource.org>
2023-05-22 18:02:55 +0200

    Show more info of senders in Ladybug blackbox and graybox

--- ladybug/src/main/java/nl/nn/ibistesttool/filter/BlackBox.java
+++ ladybug/src/main/java/nl/nn/ibistesttool/filter/BlackBox.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Nationale-Nederlanden
+   Copyright 2023 WeAreFrank!, 2018 Nationale-Nederlanden
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,53 +15,31 @@
 */
 package nl.nn.ibistesttool.filter;
 
-import java.util.List;
-
 import nl.nn.testtool.Checkpoint;
-import nl.nn.testtool.Report;
-import nl.nn.testtool.filter.CheckpointMatcher;
 
 /**
  * @author Jaco de Groot
  */
-public class BlackBox implements CheckpointMatcher {
-	
-	public boolean match(Report report, Checkpoint checkpoint) {
-		if (
-				checkpoint.getName() != null
-				&&
-				(
-					checkpoint.getName().startsWith("Sender ")
-					// Also in stub4testtool.xsl
-					&& !"nl.nn.adapterframework.jdbc.DirectQuerySender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.jdbc.FixedQuerySender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.DelaySender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.EchoSender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.IbisLocalSender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.LogSender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.ParallelSenders".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.SenderSeries".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.SenderWrapper".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.XsltSender".equals(checkpoint.getSourceClassName())
-					// Not in stub4testtool.xsl
-					&& !"nl.nn.adapterframework.senders.FixedResultSender".equals(checkpoint.getSourceClassName())
-					&& !"nl.nn.adapterframework.senders.XmlValidatorSender".equals(checkpoint.getSourceClassName())
-				)
-			) {
+public class BlackBox extends GrayBox {
+
+	@Override
+	protected boolean match(Checkpoint checkpoint) {
+		if (checkpoint.getName() != null && checkpoint.getName().startsWith("Sender ")
+				// Also in stub4testtool.xsl
+				&& !"nl.nn.adapterframework.jdbc.DirectQuerySender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.jdbc.FixedQuerySender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.DelaySender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.EchoSender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.IbisLocalSender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.LogSender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.ParallelSenders".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.SenderSeries".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.SenderWrapper".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.XsltSender".equals(checkpoint.getSourceClassName())
+				// Not in stub4testtool.xsl
+				&& !"nl.nn.adapterframework.senders.FixedResultSender".equals(checkpoint.getSourceClassName())
+				&& !"nl.nn.adapterframework.senders.XmlValidatorSender".equals(checkpoint.getSourceClassName())) {
 			return true;
-		} else {
-			List checkpoints = report.getCheckpoints();
-			if (checkpoints.size() > 0) {
-				Checkpoint firstCheckpoint = (Checkpoint)checkpoints.get(0);
-				if (checkpoint.equals(firstCheckpoint)) {
-					return true;
-				} else {
-					Checkpoint lastCheckpoint = (Checkpoint)checkpoints.get(checkpoints.size() - 1);
-					if (checkpoint.equals(lastCheckpoint)) {
-						return true;
-					}
-				}
-			}
 		}
 		return false;
 	}
---
+++ ladybug/src/main/java/nl/nn/ibistesttool/filter/GrayBox.java
@@ -0,0 +1,66 @@
+/*
+   Copyright 2023 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package nl.nn.ibistesttool.filter;
+
+import java.util.List;
+
+import nl.nn.testtool.Checkpoint;
+import nl.nn.testtool.Report;
+import nl.nn.testtool.filter.CheckpointMatcher;
+
+/**
+ * @author Jaco de Groot
+ */
+public class GrayBox implements CheckpointMatcher {
+
+	public boolean match(Report report, Checkpoint checkpoint) {
+		if (checkpoint.getType() == Checkpoint.TYPE_INPUTPOINT || checkpoint.getType() == Checkpoint.TYPE_OUTPUTPOINT
+				 || checkpoint.getType() == Checkpoint.TYPE_INFOPOINT) {
+			List<Checkpoint> checkpoints = report.getCheckpoints();
+			for (int i = checkpoints.indexOf(checkpoint) - 1; i > 1; i--) {
+				if (checkpoints.get(i).getType() == Checkpoint.TYPE_STARTPOINT) {
+					return match(report, checkpoints.get(i));
+				}
+			}
+		} else {
+			if (match(checkpoint)) {
+				return true;
+			} else {
+				List<Checkpoint> checkpoints = report.getCheckpoints();
+				if (checkpoints.size() > 0) {
+					Checkpoint firstCheckpoint = (Checkpoint)checkpoints.get(0);
+					if (checkpoint.equals(firstCheckpoint)) {
+						return true;
+					} else {
+						Checkpoint lastCheckpoint = (Checkpoint)checkpoints.get(checkpoints.size() - 1);
+						if (checkpoint.equals(lastCheckpoint)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	protected boolean match(Checkpoint checkpoint) {
+		if (checkpoint.getName() != null &&
+				(checkpoint.getName().startsWith("Sender ") || checkpoint.getName().startsWith("Pipeline "))) {
+			return true;
+		}
+		return false;
+	}
+}
