87db1a3bf8e77699d842d3dc7b9e4bd60e82f616
Matthbo <matthijs.booman@hotmail.com>
2023-06-02 16:04:07 +0200

    fix breadcrumbs not loading because of missing $state

--- console/frontend/src/main/frontend/js/app/components/pages/pages-navigation.component.js
+++ console/frontend/src/main/frontend/js/app/components/pages/pages-navigation.component.js
@@ -1,9 +1,15 @@
 import { appModule } from "../../app.module";
 
+const PagesNavigationController = function ($state) {
+	const ctrl = this;
+	ctrl.$state = $state;
+}
+
 appModule.component('pagesNavigation', {
 	bindings: {
 		onOpenInfo: '&',
 		onOpenFeedback: '&'
 	},
+	controller: ['$state', PagesNavigationController],
 	templateUrl: 'js/app/components/pages/pages-navigation.component.html'
 });
--- console/frontend/src/main/frontend/js/app/components/pages/pages-topinfobar.component.js
+++ console/frontend/src/main/frontend/js/app/components/pages/pages-topinfobar.component.js
@@ -3,16 +3,11 @@ import {appModule} from "../../app.module";
 const PagesTopinfobarController = function ($scope, $state) {
 	const ctrl = this;
 
+	ctrl.$state = $state;
 	ctrl.loading = false;
 
 	ctrl.$onInit = function(){
 		$scope.$on('loading', function(event, loading) { ctrl.loading = loading; });
-	}
-
-	ctrl.$postLink = function () { // TEST
-		// setTimeout(() => {
-			console.log('TEST', $state.current.data.breadcrumbs);
-		// }, 5000);
 	}
 }
 
