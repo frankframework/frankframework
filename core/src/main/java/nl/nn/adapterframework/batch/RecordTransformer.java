9296b368fb4a5da2ca6a7c8c17a4160776ca787c
Tim van der Leeuw <tim@wearefrank.nl>
2023-06-13 12:34:42 +0200

    Cleanups in RecordTransformer, as result of suggestion by sonatype-liftbot and then following the warnings in IntelliJ

--- core/src/main/java/nl/nn/adapterframework/batch/RecordTransformer.java
+++ core/src/main/java/nl/nn/adapterframework/batch/RecordTransformer.java
@@ -18,10 +18,9 @@ package nl.nn.adapterframework.batch;
 import java.lang.reflect.Constructor;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
@@ -61,18 +60,16 @@ public class RecordTransformer extends AbstractRecordHandler {
 
 	private String outputSeparator;
 
-	private List<IOutputField> outputFields=new LinkedList<>();
+	private final List<IOutputField> outputFields = new ArrayList<>();
 
 	@Override
 	public String handleRecord(PipeLineSession session, List<String> parsedRecord) throws Exception {
 		StringBuilder output = new StringBuilder();
 		Stack<IOutputField> conditions = new Stack<>();
 
-		for(Iterator<IOutputField> outputFieldIt = outputFields.iterator(); outputFieldIt.hasNext();) {
-			IOutputField outputField = outputFieldIt.next();
-
+		for (IOutputField outputField : outputFields) {
 			// if outputfields are to be seperator with delimiter
-			if(outputSeparator != null && output.length() > 0) {
+			if (outputSeparator != null && output.length() > 0) {
 				output.append(outputSeparator);
 			}
 
@@ -137,7 +134,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	}
 
 	public void addDateOutput(int inputFieldIndex, String informat, String outformat) {
-		addOutputField(new FixedDateOutput(outformat, informat, inputFieldIndex-1));
+		addOutputField(new FixedDateOutput(outformat, informat, inputFieldIndex - 1));
 	}
 
 	public void addLookup(int inputFieldIndex, Map<String,String> lookupValues) {
@@ -153,7 +150,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	}
 
 	public void addIf(int inputFieldIndex, String comparator, String compareValue) throws ConfigurationException {
-		addOutputField(new IfCondition(inputFieldIndex-1, comparator, compareValue));
+		addOutputField(new IfCondition(inputFieldIndex - 1, comparator, compareValue));
 	}
 
 	public void addElseIf(int inputFieldIndex, String comparator, String compareValue) throws ConfigurationException {
@@ -171,72 +168,82 @@ public class RecordTransformer extends AbstractRecordHandler {
 	public void addOutputField(String fieldDef) throws ConfigurationException {
 		StringTokenizer st = new StringTokenizer(fieldDef, "(),");
 		String def = nextToken(st, "Function in outputFields must be parameterized [" + fieldDef +"]").trim().toUpperCase();
-		if ("STRING".equals(def)) {
-			addFixedOutput(nextToken(st, "Fixed function expects a value"));
-		}
-		else if ("NOW".equals(def)) {
-			addDateOutput(nextToken(st, "Now function expects an outformat"));
-		}
-		else if ("INCOPY".equals(def)) {
-			addOutputInput(Integer.parseInt(nextToken(st, "In function expects a numeric value")));
-		}
-		else if ("INDATE".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "Indate function expects a fieldnummer"));
-			addDateOutput(field, nextToken(st, "Indate function expects an in and outformat, seperated with ~"), nextToken(st, "Indate function expects an in and outformat, seperated with ~"));
-		}
-		else if ("FILL".equals(def)) {
-			int length = Integer.parseInt(nextToken(st, "Fill function expects a fieldlength"));
-			char fillChar = nextToken(st, "Fill function expects a fillcharacter").charAt(0);
-			addFillOutput(length, fillChar);
-		}
-		else if ("LOOKUP".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "Lookup function expects a fieldnummer"));
-			Map<String,String> keyValues = convertToKeyValueMap(st, '=');
-			addLookup(field, keyValues);
-		}
-		else if ("SUBSTR".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "Substr function expects a fieldnummer"));
-			int startIndex = Integer.parseInt(nextToken(st, "Substr function expects a startindex"));
-			int endIndex = Integer.parseInt(nextToken(st, "Substr function expects an endindex"));
-			addSubstring(field, startIndex, endIndex);
-		}
-		else if ("ALIGN".equals(def)) {
-			String fixedValue = nextToken(st, "Align function expects a fixed value");
-			int length = Integer.parseInt(nextToken(st, "Align function expects a fieldlength"));
-			boolean leftAlign= "LEFT".equals(nextToken(st, "Align function expects alignment left").toUpperCase());
-			char fillChar = nextToken(st, "Align function expects a fillcharacter").charAt(0);
-			addAlignedOutput(fixedValue, length, leftAlign, fillChar);
-		}
-		else if ("INALIGN".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "Inalign function expects a fieldnumber"));
-			int length = Integer.parseInt(nextToken(st, "Inalign function expects a fieldlength"));
-			boolean leftAlign= "LEFT".equals(nextToken(st, "Inalign function expects alignment left").toUpperCase());
-			char fillChar = nextToken(st, "Inalign function expects a fillcharacter").charAt(0);
-			addAlignedInput(field, length, leftAlign, fillChar);
-		}
-		else if ("EXTERNAL".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "External function expects a fieldnumber"));
-			String delegateName = nextToken(st, "External function expects a type name for the delegate");
-			String params = nextToken(st, "External function expects a parameter string");
-			addExternal(field, delegateName, params);
-		}
-		else if ("IF".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "If function expects a fieldnummer"));
-			String comparator = nextToken(st, "If function expects a comparator (EQ | NE | SW | NS)");
-			String compareValue = nextToken(st, "If function expects a compareValue");
-			addIf(field, comparator, compareValue);
-		}
-		else if ("ELSEIF".equals(def)) {
-			int field = Integer.parseInt(nextToken(st, "If function expects a fieldnummer"));
-			String comparator = nextToken(st, "If function expects a comparator (EQ | NE | SW | NS)");
-			String compareValue = nextToken(st, "If function expects a compareValue");
-			addElseIf(field, comparator, compareValue);
-		}
-		else if ("ENDIF".equals(def)) {
-			addEndIf();
-		}
-		else {
-			throw new ConfigurationException("Unexpected function [" + def + "] defined in outputFields");
+		switch (def) {
+			case "STRING":
+				addFixedOutput(nextToken(st, "Fixed function expects a value"));
+				break;
+			case "NOW":
+				addDateOutput(nextToken(st, "Now function expects an outformat"));
+				break;
+			case "INCOPY":
+				addOutputInput(Integer.parseInt(nextToken(st, "In function expects a numeric value")));
+				break;
+			case "INDATE": {
+				int field = Integer.parseInt(nextToken(st, "Indate function expects a fieldnummer"));
+				addDateOutput(field, nextToken(st, "Indate function expects an in and outformat, seperated with ~"), nextToken(st, "Indate function expects an in and outformat, seperated with ~"));
+				break;
+			}
+			case "FILL": {
+				int length = Integer.parseInt(nextToken(st, "Fill function expects a fieldlength"));
+				char fillChar = nextToken(st, "Fill function expects a fillcharacter").charAt(0);
+				addFillOutput(length, fillChar);
+				break;
+			}
+			case "LOOKUP": {
+				int field = Integer.parseInt(nextToken(st, "Lookup function expects a fieldnummer"));
+				Map<String, String> keyValues = convertToKeyValueMap(st, '=');
+				addLookup(field, keyValues);
+				break;
+			}
+			case "SUBSTR": {
+				int field = Integer.parseInt(nextToken(st, "Substr function expects a fieldnummer"));
+				int startIndex = Integer.parseInt(nextToken(st, "Substr function expects a startindex"));
+				int endIndex = Integer.parseInt(nextToken(st, "Substr function expects an endindex"));
+				addSubstring(field, startIndex, endIndex);
+				break;
+			}
+			case "ALIGN": {
+				String fixedValue = nextToken(st, "Align function expects a fixed value");
+				int length = Integer.parseInt(nextToken(st, "Align function expects a fieldlength"));
+				boolean leftAlign = "LEFT".equalsIgnoreCase(nextToken(st, "Align function expects alignment left"));
+				char fillChar = nextToken(st, "Align function expects a fillcharacter").charAt(0);
+				addAlignedOutput(fixedValue, length, leftAlign, fillChar);
+				break;
+			}
+			case "INALIGN": {
+				int field = Integer.parseInt(nextToken(st, "Inalign function expects a fieldnumber"));
+				int length = Integer.parseInt(nextToken(st, "Inalign function expects a fieldlength"));
+				boolean leftAlign = "LEFT".equalsIgnoreCase(nextToken(st, "Inalign function expects alignment left"));
+				char fillChar = nextToken(st, "Inalign function expects a fillcharacter").charAt(0);
+				addAlignedInput(field, length, leftAlign, fillChar);
+				break;
+			}
+			case "EXTERNAL": {
+				int field = Integer.parseInt(nextToken(st, "External function expects a fieldnumber"));
+				String delegateName = nextToken(st, "External function expects a type name for the delegate");
+				String params = nextToken(st, "External function expects a parameter string");
+				addExternal(field, delegateName, params);
+				break;
+			}
+			case "IF": {
+				int field = Integer.parseInt(nextToken(st, "If function expects a fieldnummer"));
+				String comparator = nextToken(st, "If function expects a comparator (EQ | NE | SW | NS)");
+				String compareValue = nextToken(st, "If function expects a compareValue");
+				addIf(field, comparator, compareValue);
+				break;
+			}
+			case "ELSEIF": {
+				int field = Integer.parseInt(nextToken(st, "If function expects a fieldnummer"));
+				String comparator = nextToken(st, "If function expects a comparator (EQ | NE | SW | NS)");
+				String compareValue = nextToken(st, "If function expects a compareValue");
+				addElseIf(field, comparator, compareValue);
+				break;
+			}
+			case "ENDIF":
+				addEndIf();
+				break;
+			default:
+				throw new ConfigurationException("Unexpected function [" + def + "] defined in outputFields");
 		}
 	}
 
@@ -251,11 +258,11 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Converts a string to a map
 	 */
 	private Map<String,String> convertToKeyValueMap(StringTokenizer st, char kvSep) {
-		Map<String,String> result = new HashMap<String,String>();
+		Map<String,String> result = new HashMap<>();
 		while (st.hasMoreTokens()) {
 			String kv = st.nextToken();
 			int ndx = kv.indexOf(kvSep);
-			if (ndx > 0 && ndx < kv.length()) {
+			if (ndx > 0) {
 				result.put(kv.substring(0, ndx), kv.substring(ndx+1));
 			}
 		}
@@ -296,7 +303,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * @author John Dekker
 	 */
 	class OutputInput implements IOutputField {
-		private int inputFieldIndex;
+		private final int inputFieldIndex;
 
 		OutputInput(int inputFieldIndex) {
 			this.inputFieldIndex = inputFieldIndex;
@@ -330,8 +337,8 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * @author John Dekker
 	 */
 	class Substring extends OutputInput {
-		private int startIndex;
-		private int endIndex;
+		private final int startIndex;
+		private final int endIndex;
 
 		Substring(int inputFieldIndex, int startIndex, int endIndex) throws ConfigurationException {
 			super(inputFieldIndex);
@@ -362,7 +369,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 				}
 			}
 			else {
-				result.append(val.substring(startIndex, endIndex));
+				result.append(val, startIndex, endIndex);
 			}
 			return null;
 		}
@@ -373,13 +380,13 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * @author John Dekker
 	 */
 	class OutputAlignedInput extends OutputInput {
-		private int length;
-		private char fillchar;
-		private boolean leftAlign;
+		private final int length;
+		private final char fillChar;
+		private final boolean leftAlign;
 
 		OutputAlignedInput(int inputFieldIndex, int length, boolean leftAlign, char fill) {
 			super(inputFieldIndex);
-			this.fillchar = fill;
+			this.fillChar = fill;
 			this.length = length;
 			this.leftAlign = leftAlign;
 		}
@@ -387,7 +394,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 		@Override
 		public IOutputField appendValue(IOutputField curFunction, StringBuilder result, List<String> inputFields) throws ConfigurationException {
 			String val = super.toValue(inputFields).trim();
-			FileUtils.align(result, val, length, leftAlign, fillchar);
+			FileUtils.align(result, val, length, leftAlign, fillChar);
 			return null;
 		}
 	}
@@ -396,8 +403,8 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Sends a fixed value to the output
 	 * @author John Dekker
 	 */
-	class FixedOutput implements IOutputField {
-		private String fixedOutput;
+	static class FixedOutput implements IOutputField {
+		private final String fixedOutput;
 
 		FixedOutput(String fixedOutput) {
 			this.fixedOutput = fixedOutput;
@@ -414,7 +421,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Send x number of characters to the output
 	 * @author John Dekker
 	 */
-	class FixedFillOutput extends FixedOutput {
+	static class FixedFillOutput extends FixedOutput {
 		FixedFillOutput(int length, char fillchar) {
 			super(new String(FileUtils.getFilledArray(length, fillchar)));
 		}
@@ -424,7 +431,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Align a fixed value and send it to the output
 	 * @author John Dekker
 	 */
-	class FixedAlignedOutput extends FixedOutput {
+	static class FixedAlignedOutput extends FixedOutput {
 		FixedAlignedOutput(String fixedOutput, int length, boolean leftAlign, char fillchar) {
 			super(FileUtils.align(fixedOutput, length, leftAlign, fillchar));
 		}
@@ -435,7 +442,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * @author John Dekker
 	 */
 	class Lookup extends OutputInput {
-		private Map<String,String> lookupValues;
+		private final Map<String,String> lookupValues;
 
 		Lookup(int fieldNr, Map<String,String> lookupValues) {
 			super(fieldNr);
@@ -464,10 +471,10 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Send either a fixed date or a transformed input datevalue to the output
 	 * @author John Dekker
 	 */
-	class FixedDateOutput implements IOutputField {
+	static class FixedDateOutput implements IOutputField {
 		private int inputFieldIndex = -1;
-		private SimpleDateFormat outFormatter;
-		private SimpleDateFormat inFormatter;
+		private final SimpleDateFormat outFormatter;
+		private final SimpleDateFormat inFormatter;
 
 		FixedDateOutput(String outFormatPattern, String inFormatPattern, int inputFieldIndex) {
 			this.inputFieldIndex = inputFieldIndex;
@@ -507,7 +514,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * Abstract class for condition. Only if the condition is met, output is written
 	 * @author John Dekker
 	 */
-	abstract class Condition implements IOutputField {
+	abstract static class Condition implements IOutputField {
 		private boolean output;
 
 		@Override
@@ -547,32 +554,38 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * If condition
 	 * @author John Dekker
 	 */
-	class IfCondition extends Condition {
-		private int inputFieldIndex;
-		private int comparator;
-		private String compareValue;
+	static class IfCondition extends Condition {
+		private final int inputFieldIndex;
+		private final int comparator;
+		private final String compareValue;
 
 		IfCondition(int inputFieldIndex, String comparator, String compareValue) throws ConfigurationException {
 			this.inputFieldIndex = inputFieldIndex;
 
 			String comp = comparator.trim().toUpperCase();
-			if ("EQ".equals(comp))
-				this.comparator = 1;
-			else if ("NE".equals(comp))
-				this.comparator = 2;
-			else if ("SW".equals(comp))
-				this.comparator = 3;
-			else if ("NS".equals(comp))
-				this.comparator = 4;
-			else
-				throw new ConfigurationException("If function does not support [" + comparator + "]");
+			switch (comp) {
+				case "EQ":
+					this.comparator = 1;
+					break;
+				case "NE":
+					this.comparator = 2;
+					break;
+				case "SW":
+					this.comparator = 3;
+					break;
+				case "NS":
+					this.comparator = 4;
+					break;
+				default:
+					throw new ConfigurationException("If function does not support [" + comparator + "]");
+			}
 
 			this.compareValue = compareValue;
 		}
 
 		@Override
 		protected boolean conditionIsTrue(List<String> inputFields) throws ConfigurationException {
-			if (inputFieldIndex < 0 && inputFieldIndex >= inputFields.size()) {
+			if (inputFieldIndex < 0 || inputFieldIndex >= inputFields.size()) {
 				throw new ConfigurationException("Function refers to a non-existing inputfield [" + inputFieldIndex + "]");
 			}
 			String val = inputFields.get(inputFieldIndex);
@@ -628,7 +641,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 	 * End if marker
 	 * @author John Dekker
 	 */
-	class EndIfCondition implements IOutputField {
+	static class EndIfCondition implements IOutputField {
 		@Override
 		public IOutputField appendValue(IOutputField curFunction,StringBuilder result,List<String> inputFields) throws Exception {
 			throw new Exception("Endif function has no corresponding if");
@@ -644,8 +657,8 @@ public class RecordTransformer extends AbstractRecordHandler {
 	}
 
 	class DelegateOutput extends OutputInput {
-		private IOutputDelegate delegate;
-		private String params;
+		private final IOutputDelegate delegate;
+		private final String params;
 
 		DelegateOutput(int inputFieldIndex, String delegateName, String params) throws ConfigurationException {
 			super(inputFieldIndex);
@@ -653,7 +666,7 @@ public class RecordTransformer extends AbstractRecordHandler {
 			this.params = params;
 			try {
 				Class<?> delegateClass = Class.forName(delegateName);
-				Constructor<?> constructor = delegateClass.getConstructor(new Class[0]);
+				Constructor<?> constructor = delegateClass.getConstructor();
 				delegate = (IOutputDelegate)constructor.newInstance(new Object[0]);
 			}
 			catch(Exception e) {
