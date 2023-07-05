e6807a75f6632d6e3230c86d000cf28a6372d4b9
Laurens <hflaurens@hotmail.com>
2023-07-05 21:20:12 +0200

    Configurable header to extract jwt from

--- core/src/main/java/nl/nn/adapterframework/http/rest/ApiListenerServlet.java
+++ core/src/main/java/nl/nn/adapterframework/http/rest/ApiListenerServlet.java
@@ -295,7 +295,7 @@ public class ApiListenerServlet extends HttpServletBase {
 						}
 						break;
 					case JWT:
-						String authorizationHeader = request.getHeader("Authorization");
+						String authorizationHeader = request.getHeader(listener.getJwtHeader());
 						if(StringUtils.isNotEmpty(authorizationHeader) && authorizationHeader.contains("Bearer")) {
 							try {
 								Map<String, Object> claimsSet = listener.getJwtValidator().validateJWT(authorizationHeader.substring(7));
