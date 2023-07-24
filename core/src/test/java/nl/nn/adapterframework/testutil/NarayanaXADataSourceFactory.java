e1e82f168907c6c15dc155d9f9c3e887dbe77d41
Niels Meijer <nielsmeijer@hotmail.com>
2023-07-24 14:00:32 +0200

    Remove hikari

--- core/src/test/java/nl/nn/adapterframework/testutil/NarayanaXADataSourceFactory.java
+++ core/src/test/java/nl/nn/adapterframework/testutil/NarayanaXADataSourceFactory.java
@@ -5,9 +5,6 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import javax.sql.XADataSource;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import nl.nn.adapterframework.jta.narayana.NarayanaConfigurationBean;
 import nl.nn.adapterframework.jta.narayana.NarayanaDataSource;
 
@@ -26,22 +23,6 @@ public class NarayanaXADataSourceFactory extends URLXADataSourceFactory {
 
 	@Override
 	protected DataSource augmentXADataSource(XADataSource xaDataSource, String product) {
-		NarayanaDataSource ds = new NarayanaDataSource(xaDataSource, product);
-		return pool(ds, product);
-	}
-
-	private DataSource pool(DataSource dataSource, String dataSourceName) {
-		HikariConfig config = new HikariConfig();
-		config.setRegisterMbeans(false);
-		config.setMaxLifetime(0);
-		config.setIdleTimeout(60);
-		config.setMaximumPoolSize(20);
-		config.setMinimumIdle(0);
-		config.setDataSource(dataSource);
-		config.setPoolName(dataSourceName);
-
-		HikariDataSource poolingDataSource = new HikariDataSource(config);
-		log.info("created Hikari pool [{}]", poolingDataSource);
-		return poolingDataSource;
+		return new NarayanaDataSource(xaDataSource, product);
 	}
 }
