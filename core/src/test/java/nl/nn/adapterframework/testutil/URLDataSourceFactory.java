d7d3c454693842f8dd4efd61339fde2a5f5ccabf
Tim van der Leeuw <tim@wearefrank.nl>
2023-08-03 13:33:30 +0200

    Minor improvements to URLDataSourceFactory

--- core/src/test/java/nl/nn/adapterframework/testutil/URLDataSourceFactory.java
+++ core/src/test/java/nl/nn/adapterframework/testutil/URLDataSourceFactory.java
@@ -28,7 +28,7 @@ public class URLDataSourceFactory extends JndiDataSourceFactory {
 			{ "Oracle",     "jdbc:oracle:thin:@localhost:1521:XE", 			"testiaf_user", "testiaf_user00", false, "oracle.jdbc.xa.client.OracleXADataSource" },
 			{ "MS_SQL",     "jdbc:sqlserver://localhost:1433;database=testiaf;lockTimeout=10000", 	"testiaf_user", "testiaf_user00", false, "com.microsoft.sqlserver.jdbc.SQLServerXADataSource" },
 			{ "MySQL",      "jdbc:mysql://localhost:3307/testiaf?sslMode=DISABLED&disableMariaDbDriver=1&pinGlobalTxToPhysicalConnection=true&serverTimezone=Europe/Amsterdam", "testiaf_user", "testiaf_user00", true, "com.mysql.cj.jdbc.MysqlXADataSource" },
-			{ "MariaDB",    "jdbc:mariadb://localhost:3306/testiaf", 				"testiaf_user", "testiaf_user00", false, "org.mariadb.jdbc.MariaDbDataSource" }, // can have only one entry per product key
+			{ "MariaDB",    "jdbc:mariadb://localhost:3306/testiaf?pinGlobalTxToPhysicalConnection=true", 				"testiaf_user", "testiaf_user00", false, "org.mariadb.jdbc.MariaDbDataSource" }, // can have only one entry per product key
 			{ "PostgreSQL", "jdbc:postgresql://localhost:5432/testiaf", 			"testiaf_user", "testiaf_user00", true, "org.postgresql.xa.PGXADataSource" }
 		};
 
@@ -63,7 +63,7 @@ public class URLDataSourceFactory extends JndiDataSourceFactory {
 		try(Connection ignored = ds.getConnection()) {
 			return true;
 		} catch (Throwable e) {
-			log.warn("Cannot connect to [" + product + "], skipping:" + e.getMessage());
+			log.warn("Cannot connect to [{}], skipping: {}", product, e.getMessage());
 		}
 		return false;
 	}
