b9f6d6d83e776661c5c8f91162d0a013d02f3d8e
Niels Meijer <nielsmeijer@hotmail.com>
2023-08-03 13:43:25 +0200

    Forgot to commit these files

--- docker/appserver/WebSphere/src/scripts/configureJdbc.py
+++ docker/appserver/WebSphere/src/scripts/configureJdbc.py
@@ -78,7 +78,7 @@ createDatasource('ibis4test-mysql', 'MySQL JDBC Driver', authAliasName, [
 	])
 
 createDatasource('ibis4test-mariadb', 'MariaDB JDBC Driver', authAliasName, [
-		[['name', 'Url'], ['value', 'jdbc:mariadb://host.docker.internal:3306/testiaf']],
+		[['name', 'url'], ['value', 'jdbc:mariadb://host.docker.internal:3306/testiaf']],
 	])
 
 createDatasource('ibis4test-postgres-xa', 'PostgreSQL JDBC Driver', authAliasName, [
