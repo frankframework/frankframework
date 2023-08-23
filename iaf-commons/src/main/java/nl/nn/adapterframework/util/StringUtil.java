861b23943b7ac27a57a046952e5bb34441a9a030
Laurens Mäkel <37303445+Laurens-makel@users.noreply.github.com>
2023-08-23 20:31:20 +0200

    Update StringUtil.java

--- iaf-commons/src/main/java/nl/nn/adapterframework/util/StringUtil.java
+++ iaf-commons/src/main/java/nl/nn/adapterframework/util/StringUtil.java
@@ -236,12 +236,6 @@ public class StringUtil {
 				.collect(Collectors.toList());
 	}
 
-	public static List<String> splitAndTrim(@Nullable String input){
-		return splitToStream(input)
-		        .map(String::trim)
-				.collect(Collectors.toList());
-	}
-
 	/**
 	 * Splits a string into a stream of substrings using default delimiter {@literal ','}.
 	 * Spaces before or after separators, and any leading trailing spaces, are trimmed from the result.
