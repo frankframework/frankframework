97ba96768a91295918a598f01061ec8c12393906
Niels <nielsmeijer@hotmail.com>
2023-09-05 11:45:55 +0200

    Update core/src/main/java/nl/nn/adapterframework/http/rest/ApiListener.java

--- core/src/main/java/nl/nn/adapterframework/http/rest/ApiListener.java
+++ core/src/main/java/nl/nn/adapterframework/http/rest/ApiListener.java
@@ -253,7 +253,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 
 	/**
 	 * Automatically generate and validate etags
-	 * @ff.default <code>true</code>, can be changed by setting the property <code>api.etag.enabled</code>.
+	 * @ff.default <code>false</code>, can be changed by setting the property <code>api.etag.enabled</code>.
 	 */
 	public void setUpdateEtag(boolean updateEtag) {
 		this.updateEtag = updateEtag;
