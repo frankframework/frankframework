faddaee5ab5705c55c3679d324bf241306b44ed6
Niels Meijer <nielsmeijer@hotmail.com>
2023-09-06 10:25:12 +0200

    Add copyright and dont use sonar on dependabot pr's

--- core/src/main/java/nl/nn/adapterframework/pipes/CreateRestViewPipe.java
+++ core/src/main/java/nl/nn/adapterframework/pipes/CreateRestViewPipe.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2015-2018, 2020 Nationale-Nederlanden, 2022 WeAreFrank!
+   Copyright 2015-2020 Nationale-Nederlanden, 2021-2023 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
