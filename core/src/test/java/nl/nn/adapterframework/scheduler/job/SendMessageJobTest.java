c0dd698c7efd92dd0ed40131d79e6bb91c917a58
J. Koster <j.koster@gmx.com>
2023-09-07 10:54:26 +0200

    Fix Job response not read and closed: make compatible (#5340)

--- core/src/test/java/nl/nn/adapterframework/scheduler/job/SendMessageJobTest.java
+++ core/src/test/java/nl/nn/adapterframework/scheduler/job/SendMessageJobTest.java
@@ -1,21 +1,20 @@
 package nl.nn.adapterframework.scheduler.job;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.SenderException;
 import nl.nn.adapterframework.core.TimeoutException;
 import nl.nn.adapterframework.senders.IbisLocalSender;
 import nl.nn.adapterframework.stream.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class SendMessageJobTest {
 
@@ -30,23 +29,23 @@ class SendMessageJobTest {
 	@Test
 	void testHappyExecuteCall() throws JobExecutionException, TimeoutException, SenderException, IOException {
 		Message messageMock = mock(Message.class);
-		when(localSenderMock.sendMessageOrThrow(any(), any())).thenReturn(messageMock);
+		when(localSenderMock.sendMessage(any(), any())).thenReturn(messageMock);
 		jobDef.setLocalSender(localSenderMock);
 		jobDef.execute();
 
-		verify(localSenderMock).sendMessageOrThrow(any(), any());
+		verify(localSenderMock).sendMessage(any(), any());
 		verify(localSenderMock).close();
 		verify(messageMock).close();
 	}
 
 	@Test
 	void testFailingSenderStillGetsClosed() throws TimeoutException, SenderException {
-		when(localSenderMock.sendMessageOrThrow(any(), any())).thenThrow(SenderException.class);
+		when(localSenderMock.sendMessage(any(), any())).thenThrow(SenderException.class);
 		jobDef.setLocalSender(localSenderMock);
 		assertThrows(JobExecutionException.class, jobDef::execute);
 
 		verify(localSenderMock).close();
-		verify(localSenderMock).sendMessageOrThrow(any(), any());
+		verify(localSenderMock).sendMessage(any(), any());
 	}
 
 	@Test
