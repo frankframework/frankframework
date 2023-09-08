a75726e507d1f04d6cd6f922d569009abc9356a0
J. Koster <j.koster@gmx.com>
2023-09-08 14:07:11 +0200

    Improve code style, avoid empty block

--- core/src/main/java/nl/nn/adapterframework/scheduler/ServiceJob.java
+++ core/src/main/java/nl/nn/adapterframework/scheduler/ServiceJob.java
@@ -43,7 +43,7 @@ public class ServiceJob extends BaseJob {
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		try {
-			log.info("executing {}", getLogPrefix(context));
+			log.info("executing {}", () -> getLogPrefix(context));
 			JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 			String serviceName = dataMap.getString(JAVALISTENER_KEY);
 			Message message = new Message(dataMap.getString(MESSAGE_KEY));
@@ -56,9 +56,9 @@ public class ServiceJob extends BaseJob {
 			localSender.configure();
 
 			localSender.open();
-			try (Message ignored = localSender.sendMessageOrThrow(message, null)) {
-			}
-			finally {
+			try {
+				localSender.sendMessageOrThrow(message, null).close();
+			} finally {
 				localSender.close();
 			}
 		}
