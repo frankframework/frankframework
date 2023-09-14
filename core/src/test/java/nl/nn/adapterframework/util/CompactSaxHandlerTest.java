c155d4c6496ed71533507d264e026c3f42921186
J. Koster <j.koster@gmx.com>
2023-09-14 15:58:55 +0200

    CompactSaxHandler: removeCompactMsgNamespaces does not work for attributes (#5290)

---
+++ core/src/test/java/nl/nn/adapterframework/util/CompactSaxHandlerTest.java
@@ -0,0 +1,72 @@
+package nl.nn.adapterframework.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import javax.xml.soap.SOAPConstants;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.stream.Message;
+import nl.nn.adapterframework.testutil.TestFileUtils;
+
+class CompactSaxHandlerTest {
+
+	private static final String DEFAULT_BODY = "<root xmlns=\"urn:fakenamespace\" xmlns:soapenv=\"" + SOAPConstants.URI_NS_SOAP_1_1_ENVELOPE + "\"><attrib>1</attrib><attrib>2</attrib></root>";
+	private static final String DEFAULT_HEADER = "<soapenv:Header><info>234</info></soapenv:Header>";
+	private static final String SOAP_MESSAGE = "<soapenv:Envelope xmlns:soapenv=\"" + SOAPConstants.URI_NS_SOAP_1_1_ENVELOPE + "\">" + DEFAULT_HEADER + "<soapenv:Body>" + DEFAULT_BODY + "</soapenv:Body></soapenv:Envelope>";
+
+	private CompactSaxHandler handler;
+	@BeforeEach
+	void setUp() {
+		handler = new CompactSaxHandler();
+		handler.setRemoveCompactMsgNamespaces(false);
+	}
+
+	@Test
+	void testBasicXMLParsing() throws IOException, SAXException {
+		handler.setContext(new PipeLineSession());
+
+		Message message = Message.asMessage(SOAP_MESSAGE);
+		XmlUtils.parseXml(message.asInputSource(), handler);
+
+		assertEquals(SOAP_MESSAGE, handler.getXmlString());
+	}
+
+	@Test
+	void testRemoveCompactNamespaces() throws IOException, SAXException {
+		// Arrange
+		handler.setRemoveCompactMsgNamespaces(true);
+		handler.setContext(new PipeLineSession());
+
+		String testInputFile = TestFileUtils.getTestFile("/Util/CompactSaxHandler/input.xml");
+		Message message1 = Message.asMessage(testInputFile);
+
+		// Act
+		XmlUtils.parseXml(message1.asInputSource(), handler);
+
+		// Assert
+		String testOutputFile = TestFileUtils.getTestFile("/Util/CompactSaxHandler/output.xml");
+		assertEquals(testOutputFile, handler.getXmlString());
+	}
+
+	@Test
+	void testRemoveCompactNamespacesDisabled() throws IOException, SAXException {
+		// Arrange
+		handler.setRemoveCompactMsgNamespaces(false);
+		handler.setContext(new PipeLineSession());
+
+		String testInputFile = TestFileUtils.getTestFile("/Util/CompactSaxHandler/input.xml");
+		Message message1 = Message.asMessage(testInputFile);
+
+		// Act
+		XmlUtils.parseXml(message1.asInputSource(), handler);
+
+		// Assert
+		assertEquals(testInputFile, handler.getXmlString());
+	}
+}
