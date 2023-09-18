87b9183e9d021f353e1e0dca5ccb09ae2f301ae6
J. Koster <j.koster@gmx.com>
2023-09-18 17:34:29 +0200

    Reduce duplicate stacktrace logging (#4340)

--- core/src/main/java/nl/nn/adapterframework/core/Adapter.java
+++ core/src/main/java/nl/nn/adapterframework/core/Adapter.java
@@ -671,7 +671,7 @@ public class Adapter implements IAdapter, NamedBean {
 			}
 			processingSuccess = false;
 			incNumOfMessagesInError();
-			addErrorMessageToMessageKeeper("error processing message with messageId [" + messageId+"]: ",e);
+			log.warn("error processing message with messageId [{}]: {}", messageId, e.getMessage());
 			result = new PipeLineResult();
 			result.setState(ExitState.ERROR);
 			result.setResult(new Message(e.getMessage()));
