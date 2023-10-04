78a5537207d129ae5cbf5205d5569df51f606468
J. Koster <j.koster@gmx.com>
2023-10-04 17:50:06 +0200

    Remove BTM related stuff for v8.0; remove Tomcat version (#5487)

--- core/src/main/java/nl/nn/adapterframework/lifecycle/IbisApplicationInitializer.java
+++ core/src/main/java/nl/nn/adapterframework/lifecycle/IbisApplicationInitializer.java
@@ -38,10 +38,10 @@ import nl.nn.adapterframework.util.AppConstants;
 import nl.nn.adapterframework.util.LogUtil;
 
 /**
- * Starts a Spring Context before all Servlets are initialized. This allows the use of dynamically creating 
+ * Starts a Spring Context before all Servlets are initialized. This allows the use of dynamically creating
  * Spring wired Servlets (using the {@link ServletManager}). These beans can be retrieved later on from within
  * the IbisContext, and are unaffected by the {@link IbisContext#fullReload()}.
- * 
+ *
  * @author Niels Meijer
  *
  */
@@ -54,7 +54,6 @@ public class IbisApplicationInitializer extends ContextLoaderListener {
 		System.setProperty(EndpointImpl.CHECK_PUBLISH_ENDPOINT_PERMISSON_PROPERTY_WITH_SECURITY_MANAGER, "false");
 		APPLICATION_LOG.debug("Starting IBIS WebApplicationInitializer");
 
-		checkAndCorrectLegacyServerTypes();
 		determineApplicationServerType(servletContext);
 
 		XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
@@ -79,7 +78,7 @@ public class IbisApplicationInitializer extends ContextLoaderListener {
 		if(fileURL == null) {
 			LOG.warn("unable to locate TestTool configuration [{}] using classloader [{}]", file, classLoader);
 		} else {
-			if(file.indexOf(":") == -1) {
+			if(!file.contains(":")) {
 				file = ResourceUtils.CLASSPATH_URL_PREFIX+file;
 			}
 
@@ -111,21 +110,6 @@ public class IbisApplicationInitializer extends ContextLoaderListener {
 			LOG.fatal("IBIS ApplicationInitializer failed to initialize", e);
 			APPLICATION_LOG.fatal("IBIS ApplicationInitializer failed to initialize", e);
 			throw e;
-		}
-	}
-
-	//TODO: remove this in 8.0
-	private void checkAndCorrectLegacyServerTypes() {
-		//In case the property is explicitly set with an unsupported value, E.g. 'applName + number'
-		String applicationServerType = System.getProperty(AppConstants.APPLICATION_SERVER_TYPE_PROPERTY);
-		if (StringUtils.isNotEmpty(applicationServerType)) {
-			if (applicationServerType.equalsIgnoreCase("WAS5") || applicationServerType.equalsIgnoreCase("WAS6")) {
-				LOG.warn("interpeting value ["+applicationServerType+"] of property ["+AppConstants.APPLICATION_SERVER_TYPE_PROPERTY+"] as [WAS]");
-				System.setProperty(AppConstants.APPLICATION_SERVER_TYPE_PROPERTY, "WAS");
-			} else if (applicationServerType.equalsIgnoreCase("TOMCAT6")) {
-				LOG.warn("interpeting value ["+applicationServerType+"] of property ["+AppConstants.APPLICATION_SERVER_TYPE_PROPERTY+"] as [TOMCAT]");
-				System.setProperty(AppConstants.APPLICATION_SERVER_TYPE_PROPERTY, "TOMCAT");
-			}
 		}
 	}
 
