f52a4e98446895274d5758adc0bc64146637a866
Tim van der Leeuw <tim@wearefrank.nl>
2023-10-05 14:17:40 +0200

    Fix default-values in log configuration properties (#5508)
    (cherry picked from commit 6b04f06494b51d986637f3e6271d98884ab08a49)

--- core/src/main/java/nl/nn/adapterframework/logging/IbisLoggerConfigurationFactory.java
+++ core/src/main/java/nl/nn/adapterframework/logging/IbisLoggerConfigurationFactory.java
@@ -30,6 +30,7 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.apache.commons.lang3.StringUtils;
@@ -56,6 +57,7 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 	public static final String LOG_PREFIX = "IbisLoggerConfigurationFactory class ";
 	private static final String LOG4J_PROPS_FILE = "log4j4ibis.properties";
 	private static final String DS_PROPERTIES_FILE = "DeploymentSpecifics.properties";
+	public static final String LOG4J_PROPERTY_REGEX = "(?<=\\$\\{ctx:)([^:].*?)(?=(:-[^}]*+)?})";
 
 	static {
 		System.setProperty("java.util.logging.manager", org.apache.logging.log4j.jul.LogManager.class.getCanonicalName());
@@ -87,16 +89,7 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 
 			String configuration = readLog4jConfiguration(source.getInputStream());
 			Properties properties = getProperties();
-			Matcher m = Pattern.compile("\\$\\{(?:ctx:)?(.*?)(?:}|:-.*?})").matcher(configuration); //Look for properties in the Log4J2 XML
-			Map<String, String> substitutions = new HashMap<>();
-			while (m.find()) {
-				String key = m.group(1);
-				String value = resolveValueRecursively(properties, key);
-
-				if(value != null) {
-					substitutions.put(key, value);
-				}
-			}
+			Map<String, String> substitutions = populateThreadContextProperties(configuration, properties);
 			ThreadContext.putAll(substitutions); // Only add the substituted variables to the ThreadContext
 
 			initLogExpressionHiding(properties);
@@ -114,6 +107,21 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 		}
 	}
 
+	@Nonnull
+	protected static Map<String, String> populateThreadContextProperties(final String configuration, final Properties properties) {
+		Matcher m = Pattern.compile(LOG4J_PROPERTY_REGEX).matcher(configuration); //Look for properties in the Log4J2 XML
+		Map<String, String> substitutions = new HashMap<>();
+		while (m.find()) {
+			String key = m.group(1);
+			String value = resolveValueRecursively(properties, key);
+
+			if(value != null) {
+				substitutions.put(key, value);
+			}
+		}
+		return substitutions;
+	}
+
 	private static void initLogExpressionHiding(Properties properties) {
 		String logHideRegex = properties.getProperty("log.hideRegex");
 		if (StringUtils.isNotBlank(logHideRegex)) {
@@ -121,7 +129,8 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 		}
 	}
 
-	private String resolveValueRecursively(Properties properties, String key) {
+	@Nullable
+	private static String resolveValueRecursively(Properties properties, String key) {
 		String value = properties.getProperty(key);
 		if(StringUtils.isEmpty(value)) {
 			return null;
@@ -138,6 +147,7 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 		return value;
 	}
 
+	@Nonnull
 	private Properties getProperties() throws IOException {
 		Properties log4jProperties = getProperties(LOG4J_PROPS_FILE);
 		if(log4jProperties == null) {
@@ -232,14 +242,15 @@ public class IbisLoggerConfigurationFactory extends ConfigurationFactory {
 	}
 
 	/**
-	 * Checks if the log.level is set on system level.
-	 * If not set, sets it based on dtap.stage
+	 * Checks if the {@code log.level} is set in the system properties.
+	 * If not set, sets it based on {@code dtap.stage}: When system property {@code dtap.stage}
+	 * is {@code ACC} or {@code PRD} then the log level is set to {@code WARN}, otherwise to {@code DEBUG}.
 	 */
 	private static void setLevel() {
 		if (System.getProperty("log.level") == null) {
 			// In the log4j4ibis.xml the rootlogger contains the loglevel: ${log.level}
 			// You can set this property in the log4j4ibis.properties, or as system property.
-			// To make sure the IBIS can startup if no log.level property has been found, it has to be explicitly set
+			// To make sure the IBIS can start up if no log.level property has been found, it has to be explicitly set
 			String stage = System.getProperty("dtap.stage");
 			String logLevel = "DEBUG";
 			if("ACC".equalsIgnoreCase(stage) || "PRD".equalsIgnoreCase(stage)) {
