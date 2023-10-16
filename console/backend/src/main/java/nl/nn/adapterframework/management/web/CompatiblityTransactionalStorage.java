de759287f7b94bbefaf105e3787ac728f092dcc3
Niels Meijer <nielsmeijer@hotmail.com>
2023-10-16 11:54:49 +0200

    Fix CXF duplicate endpoints errors

--- console/backend/src/main/java/nl/nn/adapterframework/management/web/CompatiblityTransactionalStorage.java
+++ console/backend/src/main/java/nl/nn/adapterframework/management/web/CompatiblityTransactionalStorage.java
@@ -15,6 +15,13 @@
 */
 package nl.nn.adapterframework.management.web;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -25,17 +32,28 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
+import org.springframework.messaging.Message;
+
+import nl.nn.adapterframework.management.bus.BusAction;
+import nl.nn.adapterframework.management.bus.BusMessageUtils;
+import nl.nn.adapterframework.management.bus.BusTopic;
+import nl.nn.adapterframework.management.bus.ResponseMessageBase;
+import nl.nn.adapterframework.management.web.TransactionalStorage.StorageSource;
+import nl.nn.adapterframework.util.HttpUtils;
+import nl.nn.adapterframework.util.RequestUtils;
 
 /**
  * This class exists to provide backwards compatibility for endpoints without a configuration path prefix.
  */
 @Path("/")
-public class CompatiblityTransactionalStorage extends TransactionalStorage {
+public class CompatiblityTransactionalStorage extends FrankApiBase {
 
 	@GET
 	@RolesAllowed({"IbisDataAdmin", "IbisAdmin", "IbisTester"})
@@ -51,7 +69,21 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 				@QueryParam("configuration") String configuration
 			) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return browseMessage(config, adapterName, storageSource, storageSourceName, processState, messageId);
+		// messageId is double URLEncoded, because it can contain '/' in ExchangeMailListener
+		messageId = HttpUtils.urlDecode(messageId);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.GET);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapterName);
+		if(storageSource == StorageSource.PIPES) {
+			builder.addHeader("pipe", storageSourceName);
+		} else {
+			builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, storageSourceName);
+			builder.addHeader("processState", processState);
+		}
+
+		builder.addHeader("messageId", messageId);
+		return callSyncGateway(builder);
 	}
 
 	@GET
@@ -68,7 +100,21 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 			@QueryParam("configuration") String configuration
 		) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return downloadMessage(config, adapterName, storageSource, storageSourceName, processState, messageId);
+		// messageId is double URLEncoded, because it can contain '/' in ExchangeMailListener
+		messageId = HttpUtils.urlDecode(messageId);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.DOWNLOAD);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapterName);
+		if(storageSource == StorageSource.PIPES) {
+			builder.addHeader("pipe", storageSourceName);
+		} else {
+			builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, storageSourceName);
+			builder.addHeader("processState", processState);
+		}
+
+		builder.addHeader("messageId", messageId);
+		return callSyncGateway(builder);
 	}
 
 	@POST
@@ -85,7 +131,59 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 			MultipartBody input
 		) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return downloadMessages(config, adapterName, storageSource, storageSourceName, processState, input);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.DOWNLOAD);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapterName);
+		if(storageSource == StorageSource.PIPES) {
+			builder.addHeader("pipe", storageSourceName);
+		} else {
+			builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, storageSourceName);
+			builder.addHeader("processState", processState);
+		}
+
+		String[] messageIdArray = getMessageIds(input);
+
+		StreamingOutput stream = new StreamingOutput() {
+			@Override
+			public void write(OutputStream out) throws IOException {
+				try (ZipOutputStream zos = new ZipOutputStream(out)) {
+					for (String messageId : messageIdArray) {
+						// messageId is double URLEncoded, because it can contain '/' in ExchangeMailListener
+						messageId = HttpUtils.urlDecode(messageId);
+
+						builder.addHeader("messageId", messageId);
+						Message<?> message = sendSyncMessage(builder);
+						String mimeType = BusMessageUtils.getHeader(message, ResponseMessageBase.MIMETYPE_KEY);
+
+						String filenameExtension = ".txt";
+						if(MediaType.APPLICATION_JSON.equals(mimeType)) {
+							filenameExtension = ".json";
+						} else if(MediaType.APPLICATION_XML.equals(mimeType)) {
+							filenameExtension = ".xml";
+						}
+
+						String payload = (String) message.getPayload();
+						ZipEntry entry = new ZipEntry("msg-"+messageId+filenameExtension);
+						zos.putNextEntry(entry);
+						zos.write(payload.getBytes());
+						zos.closeEntry();
+					}
+				} catch (IOException e) {
+					throw new ApiException("Failed to create zip file with messages.", e);
+				}
+			}
+		};
+
+		return Response.ok(stream)
+				.type(MediaType.APPLICATION_OCTET_STREAM)
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"messages.zip\"")
+				.build();
+	}
+
+	private String[] getMessageIds(MultipartBody inputDataMap) {
+		String messageIds = RequestUtils.resolveStringFromMap(inputDataMap, "messageIds");
+		return messageIds.split(",");
 	}
 
 	@GET
@@ -115,7 +213,31 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 				@QueryParam("max") int maxMessages
 			) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return browseMessages(config, adapterName, storageSource, storageSourceName, processState, type, host, id, messageId, correlationId, comment, message, label, startDateStr, endDateStr, sort, skipMessages, maxMessages);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.FIND);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapterName);
+		if(storageSource == StorageSource.PIPES) {
+			builder.addHeader("pipe", storageSourceName);
+		} else {
+			builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, storageSourceName);
+			builder.addHeader("processState", processState);
+		}
+
+		builder.addHeader("type", type);
+		builder.addHeader("host", host);
+		builder.addHeader("idMask", id);
+		builder.addHeader("messageId", messageId);
+		builder.addHeader("correlationId", correlationId);
+		builder.addHeader("comment", comment);
+		builder.addHeader("message", message);
+		builder.addHeader("label", label);
+		builder.addHeader("startDate", startDateStr);
+		builder.addHeader("endDate", endDateStr);
+		builder.addHeader("sort", sort);
+		builder.addHeader("skip", skipMessages);
+		builder.addHeader("max", maxMessages);
+		return callSyncGateway(builder);
 	}
 
 
@@ -134,7 +256,16 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 			@QueryParam("configuration") String configuration
 		) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return resendReceiverMessage(config, adapter, receiver, messageId);
+
+		// messageId is double URLEncoded, because it can contain '/' in ExchangeMailListener
+		messageId = HttpUtils.urlDecode(messageId);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.STATUS);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapter);
+		builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, receiver);
+		builder.addHeader("messageId", messageId);
+		return callAsyncGateway(builder);
 	}
 
 	@POST
@@ -146,7 +277,33 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 	@Deprecated
 	public Response resendReceiverMessagesOld(@PathParam("adapterName") String adapter, @PathParam("receiverName") String receiver, @QueryParam("configuration") String configuration, MultipartBody input) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return resendReceiverMessages(config, adapter, receiver, input);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.STATUS);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapter);
+		builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, receiver);
+
+		String[] messageIds = getMessageIds(input);
+
+		List<String> errorMessages = new ArrayList<>();
+		for(int i=0; i < messageIds.length; i++) {
+			try {
+				builder.addHeader("messageId", messageIds[i]);
+				callAsyncGateway(builder);
+			}
+			catch(ApiException e) { //The message of an ApiException is wrapped in HTML, try to get the original message instead!
+				errorMessages.add(e.getCause().getMessage());
+			}
+			catch(Exception e) {
+				errorMessages.add(e.getMessage());
+			}
+		}
+
+		if(errorMessages.isEmpty()) {
+			return Response.status(Response.Status.OK).build();
+		}
+
+		return Response.status(Response.Status.ACCEPTED).entity(errorMessages).build();
 	}
 
 	@POST
@@ -160,7 +317,33 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 			@PathParam("processState") String processState, @PathParam("targetState") String targetState, MultipartBody input,
 			@QueryParam("configuration") String configuration) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return changeProcessState(config, adapter, receiver, processState, targetState, input);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.MANAGE);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapter);
+		builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, receiver);
+		builder.addHeader("processState", processState);
+		builder.addHeader("targetState", targetState);
+
+		String[] messageIds = getMessageIds(input);
+
+		List<String> errorMessages = new ArrayList<>();
+		for(int i=0; i < messageIds.length; i++) {
+			try {
+				builder.addHeader("messageId", messageIds[i]);
+				callAsyncGateway(builder);
+			} catch(ApiException e) { //The message of an ApiException is wrapped in HTML, try to get the original message instead!
+				errorMessages.add(e.getCause().getMessage());
+			} catch(Exception e) {
+				errorMessages.add(e.getMessage());
+			}
+		}
+
+		if(errorMessages.isEmpty()) {
+			return Response.status(Response.Status.OK).build();
+		}
+
+		return Response.status(Response.Status.ACCEPTED).entity(errorMessages).build();
 	}
 
 	@DELETE
@@ -175,7 +358,17 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 			@PathParam("messageId") String messageId,
 			@QueryParam("configuration") String configuration) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return deleteReceiverMessage(config, adapter, receiver, messageId);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.DELETE);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapter);
+		builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, receiver);
+
+		// messageId is double URLEncoded, because it can contain '/' in ExchangeMailListener
+		messageId = HttpUtils.urlDecode(messageId);
+
+		builder.addHeader("messageId", messageId);
+		return callAsyncGateway(builder);
 	}
 
 	@DELETE
@@ -187,6 +380,30 @@ public class CompatiblityTransactionalStorage extends TransactionalStorage {
 	@Deprecated
 	public Response deleteReceiverMessagesOld(@PathParam("adapterName") String adapter, @PathParam("receiverName") String receiver, @QueryParam("configuration") String configuration, MultipartBody input) {
 		final String config = (StringUtils.isNotEmpty(configuration)) ? configuration : null;
-		return deleteReceiverMessages(config, adapter, receiver, input);
+
+		RequestMessageBuilder builder = RequestMessageBuilder.create(this, BusTopic.MESSAGE_BROWSER, BusAction.DELETE);
+		builder.addHeader(BusMessageUtils.HEADER_CONFIGURATION_NAME_KEY, config);
+		builder.addHeader(BusMessageUtils.HEADER_ADAPTER_NAME_KEY, adapter);
+		builder.addHeader(BusMessageUtils.HEADER_RECEIVER_NAME_KEY, receiver);
+
+		String[] messageIds = getMessageIds(input);
+
+		List<String> errorMessages = new ArrayList<>();
+		for(int i=0; i < messageIds.length; i++) {
+			try {
+				builder.addHeader("messageId", messageIds[i]);
+				callAsyncGateway(builder);
+			} catch(ApiException e) { //The message of an ApiException is wrapped in HTML, try to get the original message instead!
+				errorMessages.add(e.getCause().getMessage());
+			} catch(Exception e) {
+				errorMessages.add(e.getMessage());
+			}
+		}
+
+		if(errorMessages.isEmpty()) {
+			return Response.status(Response.Status.OK).build();
+		}
+
+		return Response.status(Response.Status.ACCEPTED).entity(errorMessages).build();
 	}
 }
