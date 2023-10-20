098efb15264e000ab1266c475b4c45c3a46af853
Matthbo <matthijs.booman@hotmail.com>
2023-10-20 12:02:01 +0200

    add all routes but commented out those that dont work

--- console/frontend/src/main/frontend/src/app/app-routing.module.ts
+++ console/frontend/src/main/frontend/src/app/app-routing.module.ts
@@ -6,22 +6,41 @@ import { AdapterstatisticsComponent } from './views/adapterstatistics/adaptersta
 import { StorageComponent } from './views/storage/storage.component';
 import { StorageListComponent } from './views/storage/storage-list/storage-list.component';
 import { StorageViewComponent } from './views/storage/storage-view/storage-view.component';
+import { JmsBrowseQueueComponent } from './views/jms/jms-browse-queue/jms-browse-queue.component';
+import { JmsSendMessageComponent } from './views/jms/jms-send-message/jms-send-message.component';
+import { WebservicesComponent } from './views/webservices/webservices.component';
+import { EnvironmentVariablesComponent } from './views/environment-variables/environment-variables.component';
+import { SecurityItemsComponent } from './views/security-items/security-items.component';
+import { JdbcBrowseTablesComponent } from './views/jdbc/jdbc-browse-tables/jdbc-browse-tables.component';
+import { JdbcExecuteQueryComponent } from './views/jdbc/jdbc-execute-query/jdbc-execute-query.component';
+import { InlinestoreComponent } from './views/inlinestore/inlinestore.component';
+import { IbisstoreSummaryComponent } from './views/ibisstore-summary/ibisstore-summary.component';
+import { LiquibaseComponent } from './views/liquibase/liquibase.component';
+import { IframeCustomViewComponent } from './views/iframe/iframe-custom-view/iframe-custom-view.component';
+import { IframeLadybugComponent } from './views/iframe/iframe-ladybug/iframe-ladybug.component';
+import { IframeLarvaComponent } from './views/iframe/iframe-larva/iframe-larva.component';
+import { IframeLadybugBetaComponent } from './views/iframe/iframe-ladybug-beta/iframe-ladybug-beta.component';
 
 const routes: Routes = [
-  {
+  /* {
     path: 'login',
     component: LoginComponent,
     title: 'Login'
-  },
+  }, */
   {
     path: 'logout',
     component: LogoutComponent,
     title: 'Logout'
   },
-
-  // maybe put all pages in a pages base route again, but maybe solve login in an auth guard instead
-  // auth guard blocks all routes & prevents unauthorised access, is this good enough and what to do with this in app component
-
+  /* /status?configuration&filter&search */
+  {
+    path: 'status/:configuration',
+    component: StatusComponent,
+    title: 'Adapter Status',
+    data: {
+      breadcrumbs: 'Adapter > Status',
+    },
+  },
   {
     path: 'status',
     component: StatusComponent,
@@ -56,62 +75,62 @@ const routes: Routes = [
       }
     ]
   },
-  {
+  /* {
     path: 'notifications',
     component: NotificationsComponent,
     title: 'Notifications',
     data: {
       breadcrumbs: 'Notifications'
     },
-  },
-  {
+  }, */
+  /* {
     path: 'configurations',
     component: ConfigurationsComponent,
     title: 'Configurations',
     data: {
       breadcrumbs: 'Configurations > Show',
     },
-  },
-  {
+  }, */
+  /* {
     path: 'configurations/upload',
     component: ConfigurationsUploadComponent,
     title: 'Manage Configurations',
     data: {
       breadcrumbs: 'Configurations > Upload',
     }
-  },
-  {
+  }, */
+  /* {
     path: 'configurations/manage',
     component: ConfigurationsManageComponent,
     title: 'Manage Configurations',
     data: {
       breadcrumbs: 'Configurations > Manage',
     }
-  },
-  {
+  }, */
+  /* {
     path: 'configurations/manage/:name',
     component: ConfigurationsManageDetailsComponent,
     title: 'Manage Configurations',
     data: {
       breadcrumbs: 'Configurations > Manage',
     },
-  },
-  {
+  }, */
+  /* {
     path: 'logging',
     component: LoggingComponent,
     title: 'Logging',
     data: {
       breadcrumbs: 'Logging > Log Files'
     },
-  },
-  {
+  }, */
+  /* {
     path: 'logging/settings',
     component: LoggingManageComponent,
     title: 'Logging',
     data: {
       breadcrumbs: 'Logging > Log Settings'
     },
-  },
+  }, */
   {
     path: 'jms/send-message',
     component: JmsSendMessageComponent,
@@ -128,221 +147,202 @@ const routes: Routes = [
       breadcrumbs: 'JMS > Browse Queue'
     }
   },
-  {
+  /* {
     path: "/test-pipeline",
     component: TestPipelineComponent,
     title: 'Test a PipeLine',
     data: {
       breadcrumbs: 'Testing > Test a PipeLine'
     }
+  }, */
+  /* {
+    path: 'test-serviceListener',
+    component: TestServiceListenerComponent,
+    title: 'Test a ServiceListener',
+    data: {
+      breadcrumbs: 'Testing > Test a ServiceListener'
+    }
+  }, */
+  {
+    path: 'webservices',
+    component: WebservicesComponent,
+    title: 'Webservices',
+    data: {
+      breadcrumbs: 'Webservices'
+    }
+  },
+  /* {
+    path: 'scheduler',
+    component: SchedulerComponent,
+    title: 'Scheduler',
+    data: {
+      breadcrumbs: 'Scheduler'
+    }
+  }, */
+  /* {
+    path: 'scheduler/new',
+    component: SchedulerAddComponent,
+    title: 'Add Schedule',
+    data: {
+      breadcrumbs: 'Scheduler > Add Schedule'
+    },
+  }, */
+  /* {
+    path: 'scheduler/edit/:group/:name',
+    component: SchedulerEditComponent,
+    title: 'Edit Schedule',
+    data: {
+      breadcrumbs: 'Scheduler > Edit Schedule'
+    },
+  }, */
+  {
+    path: 'environment-variables',
+    component: EnvironmentVariablesComponent,
+    title: 'Environment Variables',
+    data: {
+      breadcrumbs: 'Environment Variables'
+    }
+  },
+  {
+    path: 'jdbc/execute-query',
+    component: JdbcExecuteQueryComponent,
+    title: 'Execute JDBC Query',
+    data: {
+      breadcrumbs: 'JDBC > Execute Query'
+    }
+  },
+  {
+    path: 'jdbc/browse-tables',
+    component: JdbcBrowseTablesComponent,
+    title: 'Browse JDBC Tables',
+    data: {
+      breadcrumbs: 'JDBC > Browse Tables'
+    }
+  },
+  {
+    path: 'security-items',
+    component: SecurityItemsComponent,
+    data: {
+      pageTitle: 'Security Items',
+      breadcrumbs: 'Security Items'
+    }
+  },
+  /* {
+    path: 'connections',
+    component: ConnectionsComponent,
+    title: 'Connection Overview',
+    data: {
+      breadcrumbs: 'Connection Overview'
+    }
+  }, */
+  {
+    path: 'inlinestores/overview',
+    component: InlinestoreComponent,
+    title: 'InlineStore Overview',
+    data: {
+      breadcrumbs: 'InlineStore Overview'
+    }
+  },
+  /* {
+    path: 'monitors',
+    component: MonitorsComponent,
+    title: 'Monitors',
+    data: {
+      breadcrumbs: 'Monitors'
+    }
+  }, */
+  /* {
+    path: 'monitors/:monitor/triggers/:trigger',
+    component: MonitorsAddEditComponent,
+    title: 'Edit Trigger',
+    data: {
+      breadcrumbs: 'Monitors > Triggers > Edit'
+    },
+  }, */
+  /* {
+    path: 'monitors/:monitor/triggers/new',
+    component: MonitorsAddEditComponent,
+    title: 'Add Trigger',
+    data: {
+      breadcrumbs: 'Monitors > Triggers > Add'
+    },
+  }, */
+  {
+    path: 'ibisstore-summary',
+    component: IbisstoreSummaryComponent,
+    title: 'Ibisstore Summary',
+    data: {
+      breadcrumbs: 'JDBC > Ibisstore Summary'
+    }
+  },
+  {
+    path: 'liquibase',
+    component: LiquibaseComponent,
+    title: 'Liquibase Script',
+    data: {
+      breadcrumbs: 'JDBC > Liquibase Script'
+    }
+  },
+  {
+    path: 'customView/:name',
+    component: IframeCustomViewComponent,
+    title: "Custom View",
+    data: {
+      breadcrumbs: 'Custom View',
+      iframe: true
+    },
+  },
+  {
+    path: 'testing/larva',
+    component: IframeLarvaComponent,
+    title: 'Larva',
+    data: {
+      breadcrumbs: 'Testing > Larva',
+      iframe: true
+    },
+  },
+  {
+    path: 'testing/ladybug',
+    component: IframeLadybugComponent,
+    title: 'Ladybug',
+    data: {
+      breadcrumbs: 'Testing > Ladybug',
+      iframe: true
+    },
+  },
+  {
+    path: 'testing/ladybug-beta',
+    component: IframeLadybugBetaComponent,
+    title: 'Ladybug (beta)',
+    data: {
+      breadcrumbs: 'Testing > Ladybug (beta)',
+      iframe: true
+    },
+  },
+  /* {
+    path: 'empty_page',
+    templateUrl: "js/app/views/empty/empty_page.html",
+    title: 'Empty Page'
+  }, */
+  /* {
+    path: 'iaf-update',
+    component: IafUpdateStatusComponent,
+    title: 'IAF Update'
+  }, */
+  /* {
+    path: '',
+    pathMatch: 'full',
+    component: LoadingComponent,
+  }, */
+  /* {
+    path: 'error',
+    component: ErrorComponent,
+  }, */
+  {
+    path: '**',
+    redirectTo: 'status',
   }
 ];
-
-/*
-      .state('pages.test_servicelistener', {
-        url: "/test-serviceListener",
-        component: "testServiceListener",
-        data: {
-          pageTitle: 'Test a ServiceListener',
-          breadcrumbs: 'Testing > Test a ServiceListener'
-        }
-      })
-      .state('pages.webservices', {
-        url: "/webservices",
-        component: "webservices",
-        data: {
-          pageTitle: 'Webservices',
-          breadcrumbs: 'Webservices'
-        }
-      })
-      .state('pages.scheduler', {
-        url: "/scheduler",
-        component: "scheduler",
-        data: {
-          pageTitle: 'Scheduler',
-          breadcrumbs: 'Scheduler'
-        }
-      })
-      .state('pages.add_schedule', {
-        url: "/scheduler/new",
-        component: "schedulerAdd",
-        data: {
-          pageTitle: 'Add Schedule',
-          breadcrumbs: 'Scheduler > Add Schedule'
-        },
-      })
-      .state('pages.edit_schedule', {
-        url: "/scheduler/edit/:group/:name",
-        component: "schedulerEdit",
-        data: {
-          pageTitle: 'Edit Schedule',
-          breadcrumbs: 'Scheduler > Edit Schedule'
-        },
-        params: {
-          name: "",
-          group: ""
-        }
-      })
-      .state('pages.environment_variables', {
-        url: "/environment-variables",
-        component: "environmentVariables",
-        data: {
-          pageTitle: 'Environment Variables',
-          breadcrumbs: 'Environment Variables'
-        }
-      })
-      .state('pages.execute_query', {
-        url: "/jdbc/execute-query",
-        component: 'jdbcExecuteQuery',
-        data: {
-          pageTitle: 'Execute JDBC Query',
-          breadcrumbs: 'JDBC > Execute Query'
-        }
-      })
-      .state('pages.browse_tables', {
-        url: "/jdbc/browse-tables",
-        component: "jdbcBrowseTables",
-        data: {
-          pageTitle: 'Browse JDBC Tables',
-          breadcrumbs: 'JDBC > Browse Tables'
-        }
-      })
-      .state('pages.security_items', {
-        url: "/security-items",
-        component: "securityItems",
-        data: {
-          pageTitle: 'Security Items',
-          breadcrumbs: 'Security Items'
-        }
-      })
-      .state('pages.connection_overview', {
-        url: "/connections",
-        component: "connections",
-        data: {
-          pageTitle: 'Connection Overview',
-          breadcrumbs: 'Connection Overview'
-        }
-      })
-      .state('pages.inlinestore_overview', {
-        url: "/inlinestores/overview",
-        component: "inlineStore",
-        data: {
-          pageTitle: 'InlineStore Overview',
-          breadcrumbs: 'InlineStore Overview'
-        }
-      })
-      .state('pages.monitors', {
-        url: "/monitors?configuration",
-        component: 'monitors',
-        data: {
-          pageTitle: 'Monitors',
-          breadcrumbs: 'Monitors'
-        },
-        params: {
-          configuration: { value: null, squash: true },
-        },
-      })
-      .state('pages.monitors_editTrigger', {
-        url: "/monitors/:monitor/triggers/:trigger?configuration",
-        component: 'monitorsAddEdit',
-        data: {
-          pageTitle: 'Edit Trigger',
-          breadcrumbs: 'Monitors > Triggers > Edit'
-        },
-        params: {
-          configuration: { value: null, squash: true },
-          monitor: "",
-          trigger: "",
-        },
-      })
-      .state('pages.monitors_addTrigger', {
-        url: "/monitors/:monitor/triggers/new?configuration",
-        component: 'monitorsAddEdit',
-        data: {
-          pageTitle: 'Add Trigger',
-          breadcrumbs: 'Monitors > Triggers > Add'
-        },
-        params: {
-          configuration: { value: null, squash: true },
-          monitor: "",
-        },
-      })
-      .state('pages.ibisstore_summary', {
-        url: "/ibisstore-summary",
-        component: "ibisStoreSummary",
-        data: {
-          pageTitle: 'Ibisstore Summary',
-          breadcrumbs: 'JDBC > Ibisstore Summary'
-        }
-      })
-      .state('pages.liquibase', {
-        url: "/liquibase",
-        component: "liquibase",
-        data: {
-          pageTitle: 'Liquibase Script',
-          breadcrumbs: 'JDBC > Liquibase Script'
-        }
-      })
-      .state('pages.customView', {
-        url: "/customView/:name",
-        component: "iframeCustomView",
-        data: {
-          pageTitle: "Custom View",
-          breadcrumbs: 'Custom View',
-          iframe: true
-        },
-        params: {
-          name: { value: '', squash: true },
-          url: { value: '', squash: true },
-        },
-      })
-      .state('pages.larva', {
-        url: "/testing/larva",
-        component: "iframeLarva",
-        data: {
-          pageTitle: 'Larva',
-          breadcrumbs: 'Testing > Larva',
-          iframe: true
-        },
-      })
-      .state('pages.ladybug', {
-        url: "/testing/ladybug",
-        component: "iframeLadybug",
-        data: {
-          pageTitle: 'Ladybug',
-          breadcrumbs: 'Testing > Ladybug',
-          iframe: true
-        },
-      })
-      .state('pages.ladybug_beta', {
-        url: "/testing/ladybug-beta",
-        component: "iframeLadybugBeta",
-        data: {
-          pageTitle: 'Ladybug (beta)',
-          breadcrumbs: 'Testing > Ladybug (beta)',
-          iframe: true
-        },
-      })
-      .state('pages.empty_page', {
-        url: "/empty_page",
-        templateUrl: "js/app/views/empty/empty_page.html",
-        data: { pageTitle: 'Empty Page' }
-      })
-      .state('pages.iaf_update', {
-        url: "/iaf-update",
-        component: "iafUpdateStatus",
-        data: { pageTitle: 'IAF Update' },
-      })
-      .state('pages.loading', {
-        url: "/",
-        component: "loading",
-      })
-      .state('pages.errorpage', {
-        url: "/error",
-        component: "error",
-      });
-
-*/
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
