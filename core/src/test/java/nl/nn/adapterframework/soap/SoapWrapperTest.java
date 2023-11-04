6de489eb888ec97e876733afb86f4c3504b86720
Niels Meijer <nielsmeijer@hotmail.com>
2023-11-04 15:32:27 +0100

    Fix broken test

--- core/src/test/java/nl/nn/adapterframework/soap/SoapWrapperTest.java
+++ core/src/test/java/nl/nn/adapterframework/soap/SoapWrapperTest.java
@@ -12,6 +12,7 @@ import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -274,7 +275,7 @@ public class SoapWrapperTest {
 		String expectedSoapBody = TestFileUtils.getTestFile("/Soap/signedSoap1_1_mock.xml");
 		MatchUtils.assertXmlEquals(expectedSoapBody, result);
 
-		assertTrue(verifySoapDigest(soapBody));
+		assertTrue(verifySoapDigest(toSoapMessage(soapBody.asInputStream())));
 	}
 
 	@Test
@@ -282,15 +283,15 @@ public class SoapWrapperTest {
 		URL file = TestFileUtils.getTestFileURL("/Soap/signedSoap1_1.xml");
 		assertNotNull(file); //ensure we can find the file
 
-		Message soapBody = toSoapMessage(file);
+		Message soapBody = toSoapMessage(file.openStream());
 		assertTrue(verifySoapDigest(soapBody));
 	}
 
-	private Message toSoapMessage(URL url) throws Exception {
+	private Message toSoapMessage(InputStream is) throws Exception {
 		MessageFactory factory = MessageFactory.newInstance(SOAPConstants.SOAP_1_1_PROTOCOL);
 		SOAPMessage msg = factory.createMessage();
 		SOAPPart part = msg.getSOAPPart();
-		part.setContent(new StreamSource(url.openStream()));
+		part.setContent(new StreamSource(is));
 
 		// create unsigned envelope
 		SOAPEnvelope unsignedEnvelope = part.getEnvelope();
@@ -309,8 +310,11 @@ public class SoapWrapperTest {
 		}
 
 		DOMValidateContext valContext = new DOMValidateContext(new UsernameTokenSelector(), nl.item(0));
+		valContext.setProperty("org.jcp.xml.dsig.secureValidation", false);
+		valContext.setProperty("org.apache.jcp.xml.dsig.secureValidation", false);
 		XMLSignatureFactory factory = XMLSignatureFactory.getInstance("DOM");
 		XMLSignature signature = factory.unmarshalXMLSignature(valContext);
+
 		return signature.validate(valContext);
 	}
 
