7bc7fa93ff825b422f6fa175136f1c8e2df16afa
Niels Meijer <nielsmeijer@hotmail.com>
2023-11-06 16:40:51 +0100

    Fix CMIS tests (#5694)

--- cmis/src/test/java/nl/nn/adapterframework/extensions/cmis/TestGetAction.java
+++ cmis/src/test/java/nl/nn/adapterframework/extensions/cmis/TestGetAction.java
@@ -1,38 +1,32 @@
 package nl.nn.adapterframework.extensions.cmis;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Collection;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.stream.Stream;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import nl.nn.adapterframework.extensions.cmis.CmisSender.CmisAction;
 import nl.nn.adapterframework.extensions.cmis.CmisSessionBuilder.BindingTypes;
 import nl.nn.adapterframework.parameters.Parameter;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.testutil.TestAssertions;
-import nl.nn.adapterframework.util.EnumUtils;
 
-@RunWith(Parameterized.class)
 public class TestGetAction extends CmisSenderTestBase {
 
-	@Rule
-	public ExpectedException exception = ExpectedException.none();
-
-	private static final String INPUT_WITH_PROPERTIES = "<cmis><id>id</id><objectId>dummy</objectId>"
+	private static final Message INPUT_WITH_PROPERTIES = new Message("<cmis><id>id</id><objectId>dummy</objectId>"
 			+ "<objectTypeId>cmis:document</objectTypeId><fileName>fileInput.txt</fileName>"
 			+ "<properties><property name=\"cmis:description\" type=\"string\">123456789</property>"
 			+ "<property name=\"cmis:lastModificationDate\" type=\"datetime\">2019-02-26T16:31:15</property>"
-			+ "<property name=\"cmis:creationDate\" type=\"boolean\">true</property></properties></cmis>";
+			+ "<property name=\"cmis:creationDate\" type=\"boolean\">true</property></properties></cmis>");
 	
 	private static final String GET_RESULT_FOR_INPUT= "dummy_stream";
 
@@ -44,51 +38,38 @@ public class TestGetAction extends CmisSenderTestBase {
 			+ "<property name=\"project:lastModified\" type=\"datetime\">2019-02-26T16:31:15</property>"
 			+ "<property name=\"project:onTime\" type=\"boolean\">true</property></properties></cmis>";
 
-	private String bindingType;
-	private String action;
-	private Message input;
-	private String expectedResult;
-	private Boolean resultToServlet;
-	private Boolean getProperties;
-	private Boolean getDocumentContent;
+	public static Stream<Arguments> allImplementations() {
+		return Stream.of(
+				Arguments.of(BindingTypes.ATOMPUB, GET_RESULT_FOR_INPUT, false, false, false),
+				Arguments.of(BindingTypes.ATOMPUB, GET_RESULT_TO_SERVLET, true, false, false),
+				Arguments.of(BindingTypes.ATOMPUB, GET_RESULT_FOR_GET_PROPERTIES, false, true, false),
+				Arguments.of(BindingTypes.ATOMPUB, GET_RESULT_FOR_GET_PROPERTIES, false, true, true),
 
-	@Parameters(name = "{0} - {1} - toServlet = {4} - getProperties = {5} - getDocumentContent = {6}")
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-				{ "atompub", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_INPUT, false, false, false},
-				{ "atompub", "get", INPUT_WITH_PROPERTIES, GET_RESULT_TO_SERVLET, true, false, false},
-				{ "atompub", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, false},
-				{ "atompub", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, true},
+				Arguments.of(BindingTypes.WEBSERVICES, GET_RESULT_FOR_INPUT, false, false, false),
+				Arguments.of(BindingTypes.WEBSERVICES, GET_RESULT_TO_SERVLET, true, false, false),
+				Arguments.of(BindingTypes.WEBSERVICES, GET_RESULT_FOR_GET_PROPERTIES, false, true, false),
+				Arguments.of(BindingTypes.WEBSERVICES, GET_RESULT_FOR_GET_PROPERTIES, false, true, true),
 
-				{ "webservices", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_INPUT, false, false, false},
-				{ "webservices", "get", INPUT_WITH_PROPERTIES, GET_RESULT_TO_SERVLET, true, false, false},
-				{ "webservices", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, false},
-				{ "webservices", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, true},
-
-				{ "browser", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_INPUT, false, false, false},
-				{ "browser", "get", INPUT_WITH_PROPERTIES, GET_RESULT_TO_SERVLET, true, false, false},
-				{ "browser", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, false},
-				{ "browser", "get", INPUT_WITH_PROPERTIES, GET_RESULT_FOR_GET_PROPERTIES, false, true, true},
-		});
+				Arguments.of(BindingTypes.BROWSER, GET_RESULT_FOR_INPUT, false, false, false),
+				Arguments.of(BindingTypes.BROWSER, GET_RESULT_TO_SERVLET, true, false, false),
+				Arguments.of(BindingTypes.BROWSER, GET_RESULT_FOR_GET_PROPERTIES, false, true, false),
+				Arguments.of(BindingTypes.BROWSER, GET_RESULT_FOR_GET_PROPERTIES, false, true, true)
+		);
 	}
 
-	public TestGetAction(String bindingType, String action, String input, String expected, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) {
-		this.bindingType = bindingType;
-		this.action = action;
-		this.input = new Message(input);
-		this.expectedResult = expected;
-		this.resultToServlet = resultToServlet;
-		this.getProperties = getProperties;
-		this.getDocumentContent = getDocumentContent;
+	@ParameterizedTest(name = "{0} - {1} - toServlet = {2} - getProperties = {3} - getDocumentContent = {4}")
+	@MethodSource("allImplementations")
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface TestAllImplementations {
 	}
 
-	public void configure() throws Exception {
+	private void configure(BindingTypes bindingType, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
 		sender.setGetProperties(getProperties);
 		sender.setGetDocumentContent(getDocumentContent);
 
 		sender.setStreamResultToServlet(resultToServlet);
-		sender.setBindingType(EnumUtils.parse(BindingTypes.class, bindingType));
-		sender.setAction(EnumUtils.parse(CmisAction.class, action));
+		sender.setBindingType(bindingType);
+		sender.setAction(CmisAction.GET);
 		sender.configure();
 
 		if(!STUBBED) {
@@ -96,19 +77,12 @@ public class TestGetAction extends CmisSenderTestBase {
 		}
 	}
 
-	public void configureWithParameters() throws Exception {
-		sender.addParameter(new Parameter("getProperties", getProperties.toString()));
-		sender.addParameter(new Parameter("getDocumentContent", getDocumentContent.toString()));
-
-		configure();
-	}
-
-	@Test
-	public void sendMessageFileStream() throws Exception {
+	@TestAllImplementations
+	public void sendMessageFileStream(BindingTypes bindingType, String expectedResult, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
 		sender.setFileInputStreamSessionKey("fis");
-		configure();
+		configure(bindingType, resultToServlet, getProperties, getDocumentContent);
 
-		String actualResult = sender.sendMessageOrThrow(input, session).asString();
+		String actualResult = sender.sendMessageOrThrow(INPUT_WITH_PROPERTIES, session).asString();
 		if(!getProperties && !resultToServlet) {
 			assertNull(actualResult);
 		} else {
@@ -123,23 +97,23 @@ public class TestGetAction extends CmisSenderTestBase {
 		}
 	}
 
-	@Test
-	public void sendMessageStreamResult() throws Exception {
-		sender.setBindingType(EnumUtils.parse(BindingTypes.class, bindingType));
-		sender.setAction(EnumUtils.parse(CmisAction.class, action));
+	@TestAllImplementations
+	public void sendMessageStreamResult(BindingTypes bindingType, String expectedResult, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
+		sender.setBindingType(bindingType);
+		sender.setAction(CmisAction.GET);
 		sender.configure();
 
-		Message actualResult = sender.sendMessageOrThrow(input, session);
-		assertTrue(actualResult.asObject() instanceof InputStream);
+		Message actualResult = sender.sendMessageOrThrow(INPUT_WITH_PROPERTIES, session);
+		assertInstanceOf(InputStream.class, actualResult.asObject());
 		assertEquals(GET_RESULT_FOR_INPUT, actualResult.asString());
 	}
 
-	@Test
-	public void sendMessageFileContentSessionKey() throws Exception {
+	@TestAllImplementations
+	public void sendMessageFileContentSessionKey(BindingTypes bindingType, String expectedResult, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
 		sender.setFileContentSessionKey("fileContent");
-		configure();
+		configure(bindingType, resultToServlet, getProperties, getDocumentContent);
 
-		String actualResult = sender.sendMessageOrThrow(input, session).asString();
+		String actualResult = sender.sendMessageOrThrow(INPUT_WITH_PROPERTIES, session).asString();
 		if(!getProperties && !resultToServlet) {
 			assertNull(actualResult);
 		} else {
@@ -154,12 +128,14 @@ public class TestGetAction extends CmisSenderTestBase {
 		}
 	}
 
-	@Test
-	public void sendMessageFileStreamWithParameters() throws Exception {
+	@TestAllImplementations
+	public void sendMessageFileStreamWithParameters(BindingTypes bindingType, String expectedResult, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
 		sender.setFileInputStreamSessionKey("fis");
-		configureWithParameters();
+		sender.addParameter(new Parameter("getProperties", getProperties.toString()));
+		sender.addParameter(new Parameter("getDocumentContent", getDocumentContent.toString()));
+		configure(bindingType, resultToServlet, getProperties, getDocumentContent);
 
-		String actualResult = sender.sendMessageOrThrow(input, session).asString();
+		String actualResult = sender.sendMessageOrThrow(INPUT_WITH_PROPERTIES, session).asString();
 		if(!getProperties && !resultToServlet) {
 			assertNull(actualResult);
 		} else {
@@ -174,12 +150,15 @@ public class TestGetAction extends CmisSenderTestBase {
 		}
 	}
 
-	@Test
-	public void sendMessageFileContentWithParameters() throws Exception {
+	@TestAllImplementations
+	public void sendMessageFileContentWithParameters(BindingTypes bindingType, String expectedResult, Boolean resultToServlet, Boolean getProperties, Boolean getDocumentContent) throws Exception {
 		sender.setFileContentSessionKey("fileContent");
-		configureWithParameters();
+		sender.addParameter(new Parameter("getProperties", getProperties.toString()));
+		sender.addParameter(new Parameter("getDocumentContent", getDocumentContent.toString()));
 
-		String actualResult = sender.sendMessageOrThrow(input, session).asString();
+		configure(bindingType, resultToServlet, getProperties, getDocumentContent);
+
+		String actualResult = sender.sendMessageOrThrow(INPUT_WITH_PROPERTIES, session).asString();
 		if(!getProperties && !resultToServlet) {
 			assertNull(actualResult);
 		} else {
