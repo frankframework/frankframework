a35dceb60eb735a7e0993b9e2558653bb08232fb
MatthijsSmets <s1122126@student.hsleiden.nl>
2023-11-09 12:24:57 +0100

    chore: fix assertEquals with for junit 5

--- dbms/src/test/java/nl/nn/adapterframework/dbms/SqlTranslatorOracleToH2Test.java
+++ dbms/src/test/java/nl/nn/adapterframework/dbms/SqlTranslatorOracleToH2Test.java
@@ -29,7 +29,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query   = " SELECT FIELD1, FIELD2, DECODE(FIELD3,'Y','true','N','false',NULL,'true') AS FIELD3, LISTAGG(FIELD4, ' + ') WITHIN GROUP (ORDER BY FIELD4) AS FIELD4 FROM TABLE1 GROUP BY FIELD1, FIELD2, FIELD3, FIELD4 ORDER BY FIELD4, FIELD2  \n";
 		String expected = "SELECT FIELD1, FIELD2, DECODE(FIELD3, 'Y', 'true', 'N', 'false', NULL, 'true') AS FIELD3, group_concat(FIELD4 ORDER BY FIELD4 SEPARATOR ' + ') AS FIELD4 FROM TABLE1 GROUP BY FIELD1, FIELD2, FIELD3, FIELD4 ORDER BY FIELD4, FIELD2";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -37,7 +37,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "CREATE SEQUENCE SEQ_IBISSTORE INCREMENT BY 1 MAXVALUE 999999999999999999999999999 MINVALUE 1 CACHE 20 NOORDER;";
 		String expected = "CREATE SEQUENCE SEQ_IBISSTORE INCREMENT BY 1 MAXVALUE 999999999999999999 MINVALUE 1 CACHE 20;";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -45,7 +45,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "CREATE TABLE TABLE1 (FIELD1 NUMBER(10, 0) NOT NULL, FIELD2 CHAR(2 CHAR), FIELD3 NUMBER(*, 0), FIELD4 VARCHAR2(35 CHAR), CONSTRAINT PK_TABLE1 PRIMARY KEY (FIELD1)) LOGGING NOCOMPRESS NOCACHE NOPARALLEL NOMONITORING";
 		String expected = "CREATE TABLE TABLE1(FIELD1 NUMBER(10, 0) NOT NULL, FIELD2 CHAR(2 CHAR), FIELD3 NUMBER(38, 0), FIELD4 VARCHAR2(35 CHAR), CONSTRAINT PK_TABLE1 PRIMARY KEY(FIELD1))";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Disabled("too hard for SqlTranslator to create identity column")
@@ -54,7 +54,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "CREATE TABLE IBISSTORE (MESSAGEKEY NUMBER(10) NOT NULL, TYPE CHAR(1 CHAR), SLOTID VARCHAR2(100 CHAR), HOST VARCHAR2(100 CHAR), MESSAGEID VARCHAR2(100 CHAR), CORRELATIONID VARCHAR2(256 CHAR), MESSAGEDATE TIMESTAMP(6), COMMENTS VARCHAR2(1000 CHAR), MESSAGE BLOB, EXPIRYDATE TIMESTAMP(6), LABEL VARCHAR2(100 CHAR), CONSTRAINT PK_IBISSTORE PRIMARY KEY (MESSAGEKEY));";
 		String expected = "CREATE TABLE IBISSTORE(MESSAGEKEY INT IDENTITY NOT NULL, TYPE CHAR(1 CHAR), SLOTID VARCHAR2(100 CHAR), HOST VARCHAR2(100 CHAR), MESSAGEID VARCHAR2(100 CHAR), CORRELATIONID VARCHAR2(256 CHAR), MESSAGEDATE TIMESTAMP(6), COMMENTS VARCHAR2(1000 CHAR), MESSAGE BLOB, EXPIRYDATE TIMESTAMP(6), LABEL VARCHAR2(100 CHAR), CONSTRAINT PK_IBISSTORE PRIMARY KEY(MESSAGEKEY));";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -62,7 +62,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "--------\n  --drop--\r\n--------\nDROP SEQUENCE SEQ_IBISSTORE";
 		String expected = "--------" + System.lineSeparator() + " --drop--" + System.lineSeparator() + "--------" + System.lineSeparator() + "DROP SEQUENCE IF EXISTS SEQ_IBISSTORE";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -70,7 +70,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "CREATE INDEX FBIX_TABLE1 ON TABLE1 (LOWER(FIELD3)) NOLOGGING PARALLEL;";
 		String expected = "CREATE INDEX FBIX_TABLE1 ON TABLE1(FIELD3);";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -78,7 +78,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "CREATE UNIQUE INDEX FBIX_TABLE1 ON TABLE1 (LOWER(FIELD3))";
 		String expected = "CREATE UNIQUE INDEX FBIX_TABLE1 ON TABLE1 (FIELD3)";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -86,28 +86,28 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "ALTER TABLE TABLE1 ADD CONSTRAINT FK_FIELD1 FOREIGN KEY (FIELD1) REFERENCES TABLE2 (FIELD1) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE VALIDATE;";
 		String expected = "ALTER TABLE TABLE1 ADD CONSTRAINT FK_FIELD1 FOREIGN KEY(FIELD1) REFERENCES TABLE2(FIELD1) ON DELETE CASCADE;";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
 	public void testSetDefineOff() throws JdbcException {
 		String query = "Set define off;";
 		String result = convertQuery(query);
-		assertEquals(query, null, result);
+		assertEquals(null, result, query);
 	}
 
 	@Test
 	public void testIgnoreCreateOrReplaceTrigger() throws JdbcException {
 		String query = "create or replace TRIGGER TRIGGER1 AFTER DELETE ON FIELD1 FOR EACH ROW DECLARE BEGIN DELETE FROM TABEL2 WHERE FIELD3 = FIELD1; END;";
 		String result = convertQuery(query);
-		assertEquals(query, null, result);
+		assertEquals(null, result, query);
 	}
 
 	@Test
 	public void testIgnoreAlterTrigger() throws JdbcException {
 		String query = "ALTER TRIGGER TRIGGER1 ENABLE;";
 		String result = convertQuery(query);
-		assertEquals(query, null, result);
+		assertEquals(null, result, query);
 	}
 
 	@Disabled("too hard for SqlTranslator to create identity column")
@@ -115,14 +115,14 @@ public class SqlTranslatorOracleToH2Test {
 	public void testIgnoreAlterTableIbisStore() throws JdbcException {
 		String query = "ALTER TABLE IBISSTORE ADD (CONSTRAINT PK_IBISSTORE PRIMARY KEY (MESSAGEKEY));";
 		String result = convertQuery(query);
-		assertEquals(query, null, result);
+		assertEquals(null, result, query);
 	}
 
 	@Test
 	public void testExit() throws JdbcException {
 		String query = "exit;";
 		String result = convertQuery(query);
-		assertEquals(query, null, result);
+		assertEquals(null, result, query);
 	}
 
 	@Test
@@ -130,7 +130,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "INSERT INTO IBISTEMP (tkey,tblob) VALUES (SEQ_IBISTEMP.NEXTVAL,EMPTY_BLOB());";
 		String expected = "INSERT INTO IBISTEMP(tkey, tblob) VALUES(NEXT VALUE FOR SEQ_IBISTEMP, '');";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -138,7 +138,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = " SELECT FIELD2 FROM TABLE1 WHERE FIELD1=? AND FIELD3 = ? FOR UPDATE";
 		String expected = "SELECT FIELD2, FIELD1 FROM TABLE1 WHERE FIELD1=? AND FIELD3=? FOR UPDATE";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -146,7 +146,7 @@ public class SqlTranslatorOracleToH2Test {
 		String query = "UPDATE IBISTEMP SET tblob1=EMPTY_BLOB() WHERE tkey=?;";
 		String expected = "UPDATE IBISTEMP SET tblob1='' WHERE tkey=?;";
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
--- dbms/src/test/java/nl/nn/adapterframework/dbms/SqlTranslatorOracleToMsSqlTest.java
+++ dbms/src/test/java/nl/nn/adapterframework/dbms/SqlTranslatorOracleToMsSqlTest.java
@@ -20,7 +20,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 	public void testNotConverted() throws JdbcException {
 		String query = "SELECT COUNT(*) FROM IBISSTORE";
 		String result = convertQuery(query);
-		assertEquals("unchanged", skipIrrelevantWhitespace(query), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(query), skipIrrelevantWhitespace(result), "unchanged");
 	}
 
 	@Test
@@ -28,7 +28,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "INSERT INTO IBISTEMP (tkey,tblob1) VALUES (SEQ_IBISTEMP.NEXTVAL,EMPTY_BLOB());";
 		String expected = "INSERT INTO IBISTEMP(tkey, tblob1) VALUES(NEXT VALUE FOR SEQ_IBISTEMP, 0x);";
 		String result = convertQuery(query);
-		assertEquals(query, skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 
@@ -37,7 +37,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "SELECT SEQ_IBISTEMP.NEXTVAL FROM DuaL";
 		String expected = "SELECT NEXT VALUE FOR SEQ_IBISTEMP";
 		String result = convertQuery(query);
-		assertEquals(query, skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -45,7 +45,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query    = "SELECT tblob1 FROM ibistemp WHERE tkey=? FOR UPDATE;";
 		String expected = "SELECT tblob1 FROM ibistemp WHERE tkey=? FOR UPDATE;";
 		String result = convertQuery(query);
-		assertEquals(query, skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -53,7 +53,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query   = " SELECT FIELD1, FIELD2, DECODE(FIELD3,'Y','true','N','false',NULL,'true') AS FIELD3, LISTAGG(FIELD4, ' + ') WITHIN GROUP (ORDER BY FIELD4) AS FIELD4 FROM TABLE1 GROUP BY FIELD1, FIELD2, FIELD3, FIELD4 ORDER BY FIELD4, FIELD2  \n";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -61,7 +61,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "CREATE SEQUENCE SEQ_IBISSTORE INCREMENT BY 1 MAXVALUE 999999999999999999999999999 MINVALUE 1 CACHE 20 NOORDER;";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -69,7 +69,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "CREATE TABLE TABLE1 (FIELD1 NUMBER(10, 0) NOT NULL, FIELD2 CHAR(2 CHAR), FIELD3 NUMBER(*, 0), FIELD4 VARCHAR2(35 CHAR), CONSTRAINT PK_TABLE1 PRIMARY KEY (FIELD1)) LOGGING NOCOMPRESS NOCACHE NOPARALLEL NOMONITORING";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -77,7 +77,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "CREATE TABLE IBISSTORE (MESSAGEKEY NUMBER(10) NOT NULL, TYPE CHAR(1 CHAR), SLOTID VARCHAR2(100 CHAR), HOST VARCHAR2(100 CHAR), MESSAGEID VARCHAR2(100 CHAR), CORRELATIONID VARCHAR2(256 CHAR), MESSAGEDATE TIMESTAMP(6), COMMENTS VARCHAR2(1000 CHAR), MESSAGE BLOB, EXPIRYDATE TIMESTAMP(6), LABEL VARCHAR2(100 CHAR), CONSTRAINT PK_IBISSTORE PRIMARY KEY (MESSAGEKEY));";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -85,7 +85,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "--------\n  --drop--\r\n--------\nDROP SEQUENCE SEQ_IBISSTORE";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -93,7 +93,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "CREATE INDEX FBIX_TABLE1 ON TABLE1 (LOWER(FIELD3)) NOLOGGING PARALLEL;";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -101,7 +101,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "CREATE UNIQUE INDEX FBIX_TABLE1 ON TABLE1 (LOWER(FIELD3))";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -109,7 +109,7 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = "ALTER TABLE TABLE1 ADD CONSTRAINT FK_FIELD1 FOREIGN KEY (FIELD1) REFERENCES TABLE2 (FIELD1) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE VALIDATE;";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 
 	@Test
@@ -138,6 +138,6 @@ public class SqlTranslatorOracleToMsSqlTest {
 		String query = " SELECT FIELD2 FROM TABLE1 WHERE FIELD1=? AND FIELD3 = ? FOR UPDATE";
 		String expected = query;
 		String result = convertQuery(query);
-		assertEquals(query,  skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result));
+		assertEquals(skipIrrelevantWhitespace(expected), skipIrrelevantWhitespace(result), query);
 	}
 }
