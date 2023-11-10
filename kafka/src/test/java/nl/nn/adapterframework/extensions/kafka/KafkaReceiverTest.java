9281a870f3392e5bf0485b9860c4e203ae41b758
Ruben <ruben@rubend.nl>
2023-11-10 14:58:58 +0100

    Fix codacy warning.

--- kafka/src/test/java/nl/nn/adapterframework/extensions/kafka/KafkaReceiverTest.java
+++ kafka/src/test/java/nl/nn/adapterframework/extensions/kafka/KafkaReceiverTest.java
@@ -17,7 +17,6 @@ package nl.nn.adapterframework.extensions.kafka;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
