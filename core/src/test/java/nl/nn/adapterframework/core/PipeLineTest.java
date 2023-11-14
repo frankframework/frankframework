15d7ed9883af4da3bdb5e58a8c757f742759935a
Niels Meijer <nielsmeijer@hotmail.com>
2023-11-14 12:28:15 +0100

    Create NN-Specials module (#5735)

--- core/src/test/java/nl/nn/adapterframework/core/PipeLineTest.java
+++ core/src/test/java/nl/nn/adapterframework/core/PipeLineTest.java
@@ -2,7 +2,6 @@ package nl.nn.adapterframework.core;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -14,10 +13,8 @@ import org.junit.jupiter.api.Test;
 
 import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.core.PipeLine.ExitState;
-import nl.nn.adapterframework.extensions.esb.DirectWrapperPipe;
 import nl.nn.adapterframework.pipes.AbstractPipe;
 import nl.nn.adapterframework.pipes.EchoPipe;
-import nl.nn.adapterframework.processors.CorePipeProcessor;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.testutil.TestConfiguration;
 
@@ -257,50 +254,5 @@ public class PipeLineTest {
 		assertEquals(pipeForwardName, pipe.getForwards().get(PipeForward.SUCCESS_FORWARD_NAME).getPath(), "pipe1 forward should default to next pipe");
 
 		assertTrue(pipe2.getForwards().isEmpty(), "pipe2 should not have a pipe-forward");
-	}
-
-	//Should add tests to assertThat(configuration.getConfigWarning(0), StringEndsWith.endsWith("] has no pipe forwards defined"));
-
-	@Test
-	public void testDirectWrapperPipeSuccessForward() throws ConfigurationException, PipeRunException {
-		PipeLine pipeline = configuration.createBean(PipeLine.class);
-
-		PipeForward pf = configuration.createBean(PipeForward.class);
-		pf.setName("success");
-		pf.setPath("nextPipe");
-
-		PipeForward toExit = configuration.createBean(PipeForward.class);
-		toExit.setName("success");
-		toExit.setPath("EXIT");
-
-		DirectWrapperPipe pipe = configuration.createBean(DirectWrapperPipe.class);
-		pipe.setName("DirectWrapperPipe");
-		pipe.registerForward(pf);
-		pipeline.addPipe(pipe);
-
-		EchoPipe echoPipe = configuration.createBean(EchoPipe.class);
-		echoPipe.setName("nextPipe");
-		echoPipe.setPipeLine(pipeline);
-		echoPipe.registerForward(toExit);
-		pipeline.addPipe(echoPipe);
-
-		PipeLineExit exit = configuration.createBean(PipeLineExit.class);
-		exit.setPath("exit");
-		exit.setState(ExitState.SUCCESS);
-		pipeline.registerPipeLineExit(exit);
-
-		pipeline.setOwner(pipe);
-		pipeline.configure();
-
-		CorePipeProcessor cpp = configuration.createBean(CorePipeProcessor.class);
-		PipeLineSession ps = configuration.createBean(PipeLineSession.class);
-
-		PipeRunResult pipeRunResult=cpp.processPipe(pipeline, pipe, new Message("<dummy/>"), ps);
-
-		PipeForward pipeForward=pipeRunResult.getPipeForward();
-
-		IForwardTarget target = pipeline.resolveForward(pipe, pipeForward);
-
-		assertNotNull(target);
 	}
 }
