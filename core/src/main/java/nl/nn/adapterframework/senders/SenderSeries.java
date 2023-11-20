3afa9170a665900fa48f68aa5525e69b03feb860
J. Koster <j.koster@gmx.com>
2023-11-20 11:07:58 +0100

    Restore SenderSeries.setSender method to support multiple senders

--- core/src/main/java/nl/nn/adapterframework/senders/SenderSeries.java
+++ core/src/main/java/nl/nn/adapterframework/senders/SenderSeries.java
@@ -120,6 +120,13 @@ public class SenderSeries extends SenderWrapperBase {
 		return false;
 	}
 
+
+
+	@Deprecated // replaced by registerSender, to allow for multiple senders in XSD. Method must be present, as it is used by Digester
+	public final void setSender(ISender sender) {
+		registerSender(sender);
+	}
+
 	/**
 	 * one or more specifications of senders that will be executed one after another. Each sender will get the result of the preceding one as input.
 	 * @ff.mandatory
