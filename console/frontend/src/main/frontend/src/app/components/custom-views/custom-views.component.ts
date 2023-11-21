3b14f72b5dbcec22c9a8e2548da3f23af71e9ac0
Laurens Mäkel <37303445+Laurens-makel@users.noreply.github.com>
2023-11-21 20:58:55 +0100

    Update custom-views.component.ts

--- console/frontend/src/main/frontend/src/app/components/custom-views/custom-views.component.ts
+++ console/frontend/src/main/frontend/src/app/components/custom-views/custom-views.component.ts
@@ -4,7 +4,7 @@ import { AppConstants } from 'src/angularjs/app/app.module';
 import { AppService } from 'src/angularjs/app/app.service';
 import { APPCONSTANTS } from 'src/app/app.module';
 
-type CustomView = {view: string, name: string, url: string};
+type CustomView = { view: string, name: string, url: string };
 
 @Component({
   selector: 'app-custom-views',
@@ -21,15 +21,15 @@ export class CustomViewsComponent implements OnInit {
 
   ngOnInit() {
     this.appService.appConstants$.pipe(first()).subscribe(() => {
-      const customViews = this.appConstants["customViews.names"];
+      const customViews = this.appConstants['customViews.names'];
       if (customViews && customViews.length > 0) {
         const views = customViews.split(",");
         for (const i in views) {
           const view = views[i];
-          const name = this.appConstants["customViews." + viewId + ".name"];
-          const url = this.appConstants["customViews." + viewId + ".url"];
+          const name = this.appConstants[`customViews.${viewId}.name`];
+          const url = this.appConstants[`customViews.${viewId}.url`];
           if (name && url){
-            this.customViews.push({view, name, url}); 
+            this.customViews.push({ view, name, url }); 
           }
         }
       }
