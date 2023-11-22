45bfdfc7daaa3f77dd579da5a600c37cf2e056cb
Tim van der Leeuw <tim@wearefrank.nl>
2023-11-22 14:39:06 +0100

    Update code to handle BytesMessage in various places where cast to TextMessage had been overlooked.

--- core/src/main/java/nl/nn/adapterframework/extensions/esb/EsbJmsListener.java
+++ core/src/main/java/nl/nn/adapterframework/extensions/esb/EsbJmsListener.java
@@ -15,12 +15,15 @@
 */
 package nl.nn.adapterframework.extensions.esb;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
+import javax.jms.BytesMessage;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -38,9 +41,11 @@ import nl.nn.adapterframework.core.PipeLineSession;
 import nl.nn.adapterframework.doc.Category;
 import nl.nn.adapterframework.doc.Default;
 import nl.nn.adapterframework.doc.Mandatory;
+import nl.nn.adapterframework.jms.BytesMessageInputStream;
 import nl.nn.adapterframework.jms.JmsListener;
 import nl.nn.adapterframework.receivers.RawMessageWrapper;
 import nl.nn.adapterframework.util.AppConstants;
+import nl.nn.adapterframework.util.StreamUtil;
 import nl.nn.adapterframework.util.StringUtil;
 import nl.nn.adapterframework.util.TransformerPool;
 import nl.nn.adapterframework.util.TransformerPool.OutputType;
@@ -132,10 +137,19 @@ public class EsbJmsListener extends JmsListener implements ITransactionRequireme
 		}
 
 		try {
-			TextMessage textMessage = (TextMessage) rawMessage;
-			String soapMessage = textMessage.getText();
+			String soapMessage;
+			if (rawMessage instanceof TextMessage) {
+				TextMessage textMessage = (TextMessage) rawMessage;
+				soapMessage = textMessage.getText();
+			} else if (rawMessage instanceof BytesMessage) {
+				BytesMessage bytesMessage = (BytesMessage) rawMessage;
+				InputStream input = new BytesMessageInputStream(bytesMessage);
+				soapMessage = StreamUtil.streamToString(input);
+			} else {
+				soapMessage = null;
+			}
 
-			if(!getxPathLogMap().isEmpty()) {
+			if(soapMessage != null && !getxPathLogMap().isEmpty()) {
 				StringBuilder xPathLogKeys = new StringBuilder();
 				for (Entry<String, String> pair : getxPathLogMap().entrySet()) {
 				String sessionKey = pair.getKey();
@@ -148,8 +162,8 @@ public class EsbJmsListener extends JmsListener implements ITransactionRequireme
 			}
 				messageProperties.put("xPathLogKeys", xPathLogKeys.toString());
 			}
-		} catch (JMSException e) {
-			log.debug("ignoring JMSException", e);
+		} catch (JMSException | IOException e) {
+			log.debug("ignoring Exception", e);
 		}
 		return messageProperties;
 	}
--- core/src/main/java/nl/nn/adapterframework/util/JdbcUtil.java
+++ core/src/main/java/nl/nn/adapterframework/util/JdbcUtil.java
@@ -46,15 +46,10 @@ import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 import java.util.zip.ZipException;
 
+import javax.jms.BytesMessage;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
 import javax.servlet.http.HttpServletResponse;
-
-import nl.nn.adapterframework.dbms.IDbmsSupport;
-import nl.nn.adapterframework.dbms.DbmsException;
-import nl.nn.adapterframework.dbms.JdbcException;
-import nl.nn.adapterframework.parameters.Parameter;
-import nl.nn.adapterframework.parameters.ParameterList;
 
 import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.lang3.StringUtils;
@@ -63,7 +58,13 @@ import org.xml.sax.SAXException;
 
 import nl.nn.adapterframework.core.ParameterException;
 import nl.nn.adapterframework.core.PipeLineSession;
+import nl.nn.adapterframework.dbms.DbmsException;
+import nl.nn.adapterframework.dbms.IDbmsSupport;
+import nl.nn.adapterframework.dbms.JdbcException;
+import nl.nn.adapterframework.jms.BytesMessageInputStream;
+import nl.nn.adapterframework.parameters.Parameter;
 import nl.nn.adapterframework.parameters.Parameter.ParameterType;
+import nl.nn.adapterframework.parameters.ParameterList;
 import nl.nn.adapterframework.parameters.ParameterValue;
 import nl.nn.adapterframework.parameters.ParameterValueList;
 import nl.nn.adapterframework.pipes.Base64Pipe.Direction;
@@ -421,12 +422,21 @@ public class JdbcUtil {
 			}
 			String rawMessage;
 			if (objectOK) {
+				// TODO: Direct handling of JMS messages in here should be removed. I do not expect any current instances to actually store unwrapped JMS Messages?
 				if (result instanceof MessageWrapper) {
 					rawMessage = ((MessageWrapper) result).getMessage().asString();
 				} else if (result instanceof TextMessage) {
 					try {
 						rawMessage = ((TextMessage) result).getText();
 					} catch (JMSException e) {
+						throw new JdbcException(e);
+					}
+				} else if (result instanceof BytesMessage) {
+					try {
+						BytesMessage bytesMessage = (BytesMessage) result;
+						InputStream input = new BytesMessageInputStream(bytesMessage);
+						rawMessage = StreamUtil.streamToString(input);
+					} catch (IOException e) {
 						throw new JdbcException(e);
 					}
 				} else {
