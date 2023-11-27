a773a51974dba8728479a9b335b871a4a91b1565
J. Koster <j.koster@gmx.com>
2023-11-27 18:12:49 +0100

    Allow short JWT secrets too (#5645)
    * Add right padding to fill them, when property allows it

--- core/src/test/java/nl/nn/adapterframework/pipes/JwtPipeTest.java
+++ core/src/test/java/nl/nn/adapterframework/pipes/JwtPipeTest.java
@@ -1,8 +1,8 @@
 package nl.nn.adapterframework.pipes;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
