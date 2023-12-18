c83371088fee4c76a71467ba38794503947f76a2
Yannick van Leeuwen <yannickvanleeuwen@i-lion.nl>
2023-12-18 14:10:07 +0100

    4282 - fixed tests

--- core/src/test/java/nl/nn/adapterframework/jdbc/FixedQuerySenderTest.java
+++ core/src/test/java/nl/nn/adapterframework/jdbc/FixedQuerySenderTest.java
@@ -66,7 +66,7 @@ public class FixedQuerySenderTest {
 		fixedQuerySender = new FixedQuerySender();
 		fixedQuerySender.setDatasourceName(dataSourceName);
 		fixedQuerySender.setName("FQS_TABLE");
-		fixedQuerySender.setIncludeFieldDefinition(true);
+		fixedQuerySender.setIncludeFieldDefinition(false);
 		getConfiguration().autowireByName(fixedQuerySender);
 
 		getConfiguration().getIbisManager();
