71942560a094a28e7b3f6622929d192fd097b1cf
Tim van der Leeuw <tim@wearefrank.nl>
2023-12-19 15:13:53 +0100

    Don't create new descriptor, but modify the existing one using dirty reflection tricks.

--- core/src/main/java/org/frankframework/util/RenamingObjectInputStream.java
+++ core/src/main/java/org/frankframework/util/RenamingObjectInputStream.java
@@ -1,11 +1,12 @@
 package org.frankframework.util;
 
-import static org.frankframework.util.ClassUtils.loadClass;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
+import java.lang.reflect.Field;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -24,12 +25,14 @@ public class RenamingObjectInputStream extends ObjectInputStream {
 		String className = resultClassDescriptor.getName();
 		if (className.startsWith("nl.nn.adapterframework")) {
 			String newClassName = className.replace("nl.nn.adapterframework", "org.frankframework");
-			Class<?> newClass = null;
 			try {
-				newClass = loadClass(newClassName);
-				return ObjectStreamClass.lookup(newClass);
-			} catch (ClassNotFoundException e) {
-				log.warn("Found old class [{}] in ObjectInputStream but cannot load class with new name [{}]", className, newClassName, e);
+				// Do not return a new ObjectStreamClass looked up from new class definitions, but modify the descriptor read from the stream.
+				// Otherwise some flags could be wrong, like the flag "hasWriteObjectData" when reading objects from 7.6/7.7.
+				Field nameField = ObjectStreamClass.class.getDeclaredField("name");
+				nameField.setAccessible(true);
+				FieldUtils.writeField(nameField, resultClassDescriptor, newClassName);
+			} catch (IllegalAccessException | NoSuchFieldException e) {
+				throw new RuntimeException(e);
 			}
 		}
 		return resultClassDescriptor;
