0321208a34299657ccfea1970a7fc2c49f3ea1e3
Tim van der Leeuw <tnleeuw@gmail.com>
2023-12-19 16:38:19 +0100

    Update core/src/main/java/nl/nn/adapterframework/parameters/Parameter.java
    Co-authored-by: Niels Meijer <nielsmeijer@hotmail.com>

--- core/src/main/java/nl/nn/adapterframework/parameters/Parameter.java
+++ core/src/main/java/nl/nn/adapterframework/parameters/Parameter.java
@@ -228,7 +228,7 @@ public class Parameter implements IConfigurable, IWithParameters {
 		 * Used for StoredProcedure OUT parameters when the database type is a {@code CURSOR} or {@link java.sql.JDBCType#REF_CURSOR}.
 		 * See also {@link nl.nn.adapterframework.jdbc.StoredProcedureQuerySender}.
 		 * <br/>
-		 * Type LIST can also be used in larva test to Convert a List to an xml-string (&lt;items&gt;&lt;item&gt;...&lt;/item&gt;&lt;item&gt;...&lt;/item&gt;&lt;/items&gt;) */
+		 * DEPRECATED: Type LIST can also be used in larva test to Convert a List to an xml-string (&lt;items&gt;&lt;item&gt;...&lt;/item&gt;&lt;item&gt;...&lt;/item&gt;&lt;/items&gt;) */
 		LIST,
 
 		/** (Used in larva only) Converts a Map&lt;String, String&gt; object to a xml-string (&lt;items&gt;&lt;item name='...'&gt;...&lt;/item&gt;&lt;item name='...'&gt;...&lt;/item&gt;&lt;/items&gt;) */
