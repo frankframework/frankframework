185b98d10f3eed2789a5b677dd0d5ed4f43a8143
Vivy <matthijs.booman@hotmail.com>
2023-12-21 16:04:10 +0100

    7.9 backport console adapter record keys are now changed for namespace instead of adaptername (#6031)

--- console/frontend/src/main/frontend/src/app/views/security-items/security-items.component.ts
+++ console/frontend/src/main/frontend/src/app/views/security-items/security-items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService, Certificate, Pipe } from 'src/app/app.service';
+import { AppService, Pipe } from 'src/app/app.service';
 import { AuthEntry, CertificateList, Datasource, JmsRealm, SapSystem, SecurityItemsService, SecurityRole, ServerProps } from './security-items.service';
 
 @Component({
@@ -23,8 +23,7 @@ export class SecurityItemsComponent implements OnInit {
   ) { };
 
   ngOnInit(): void {
-    for (const a in this.appService.adapters) {
-      var adapter = this.appService.adapters[a];
+    for (const adapter of Object.values(this.appService.adapters)) {
 
       if (adapter.pipes) {
         for (const p in adapter.pipes) {
@@ -32,7 +31,7 @@ export class SecurityItemsComponent implements OnInit {
 
           if (pipe.certificate) {
             this.certificates.push({
-              adapter: a,
+              adapter: adapter.name,
               pipe: p,
               certificate: pipe.certificate
             });
