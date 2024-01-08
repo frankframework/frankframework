284f3e025339ce3077ba60c4ca031c52fd4241f0
Matthbo <matthijs.booman@hotmail.com>
2024-01-08 17:09:49 +0100

    remove unused import

--- console/frontend/src/main/frontend/src/app/views/security-items/security-items.service.ts
+++ console/frontend/src/main/frontend/src/app/views/security-items/security-items.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AppService, Certificate } from 'src/app/app.service';
 
 export interface CertificateList {
