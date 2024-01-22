cbe068c8b007dd6c187d27c8c3ee09dbe16d75fb
Yannick van Leeuwen <yannickvanleeuwen@i-lion.nl>
2024-01-22 12:34:40 +0100

    5494 - added extra test case where closing message tag is missing in EscapePipeTest

--- core/src/test/java/org/frankframework/pipes/EscapePipeTest.java
+++ core/src/test/java/org/frankframework/pipes/EscapePipeTest.java
@@ -63,6 +63,17 @@ public class EscapePipeTest extends PipeTestBase<EscapePipe> {
 	}
 
 	@Test
+	public void testPartialEscapedMultiLineXmlInputMultiOccurencesMissingClosingTag() throws Exception {
+		pipe.setSubstringStart("<message>");
+		pipe.setSubstringEnd("</message>");
+		configureAndStartPipe();
+
+		PipeRunResult result = doPipe(getResource("no-closing-tag-configured.xml"));
+		String expected = getResource("no-closing-tag-configured.result").asString();
+		TestAssertions.assertEqualsIgnoreCRLF(expected, result.getResult().asString());
+	}
+
+	@Test
 	public void nothingGetsEscapedWhenNoSubstringEndConfigured() throws Exception {
 		pipe.setSubstringStart("<message>");
 		pipe.setSubstringEnd("");
