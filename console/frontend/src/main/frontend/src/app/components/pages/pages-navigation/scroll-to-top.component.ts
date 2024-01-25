7b804cec26361ee5c3db0a476a7e1361c8506536
Matthbo <matthijs.booman@hotmail.com>
2024-01-25 11:48:01 +0100

    Scroll-to-top button also scrolls up on sidebar

--- console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/scroll-to-top.component.ts
+++ console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/scroll-to-top.component.ts
@@ -1,11 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-scroll-to-top',
   template: '<div class="scroll-to-top"><a title="Scroll to top" (click)="scrollTop()"><i class="fa fa-arrow-up"></i> <span class="nav-label">Scroll To Top</span></a></div>'
 })
 export class ScrollToTopComponent {
+  @Input() navElem!: HTMLElement
+
   scrollTop() {
-    $(window).scrollTop(0);
+    window.scrollTo({ top: 0 });
+    this.navElem.scrollTo({ top: 0 });
   };
 }
