59eb05a6af5523bda272f77a5aba45d4a9c3ab4f
J. Koster <j.koster@gmx.com>
2024-01-26 11:57:42 +0100

    Add ConnectionPool information to SecurityItems (#6089)

--- core/src/main/java/org/frankframework/jta/btm/BtmDataSourceFactory.java
+++ core/src/main/java/org/frankframework/jta/btm/BtmDataSourceFactory.java
@@ -16,6 +16,7 @@
 package org.frankframework.jta.btm;
 
 import java.sql.Connection;
+import java.time.Duration;
 
 import javax.sql.CommonDataSource;
 import javax.sql.DataSource;
@@ -27,14 +28,15 @@ import org.apache.commons.dbcp2.PoolableConnection;
 import org.apache.commons.dbcp2.PoolableConnectionFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.frankframework.jdbc.datasource.OpenPoolingDataSource;
+import org.frankframework.jndi.JndiDataSourceFactory;
+import org.frankframework.util.AppConstants;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.jdbc.datasource.DelegatingDataSource;
 
 import bitronix.tm.resource.jdbc.PoolingDataSource;
 import lombok.Getter;
 import lombok.Setter;
-import org.frankframework.jndi.JndiDataSourceFactory;
-import org.frankframework.util.AppConstants;
 
 public class BtmDataSourceFactory extends JndiDataSourceFactory implements DisposableBean {
 
@@ -72,7 +74,9 @@ public class BtmDataSourceFactory extends JndiDataSourceFactory implements Dispo
 
 		poolableConnectionFactory.setAutoCommitOnReturn(false);
 		poolableConnectionFactory.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-		poolableConnectionFactory.setMaxConnLifetimeMillis((maxLifeTime > 0) ? maxLifeTime * 1000 : -1);
+		if (maxLifeTime > 0) {
+			poolableConnectionFactory.setMaxConn(Duration.ofSeconds(maxLifeTime));
+		}
 		poolableConnectionFactory.setRollbackOnReturn(true);
 		GenericObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnectionFactory);
 		connectionPool.setMinIdle(minPoolSize);
@@ -80,7 +84,7 @@ public class BtmDataSourceFactory extends JndiDataSourceFactory implements Dispo
 		connectionPool.setBlockWhenExhausted(true);
 		poolableConnectionFactory.setPool(connectionPool);
 
-		org.apache.commons.dbcp2.PoolingDataSource<PoolableConnection> ds = new org.apache.commons.dbcp2.PoolingDataSource<>(connectionPool);
+		OpenPoolingDataSource<PoolableConnection> ds = new OpenPoolingDataSource<>(connectionPool);
 		log.info("registered PoolingDataSource [{}]", ds);
 		return ds;
 	}
