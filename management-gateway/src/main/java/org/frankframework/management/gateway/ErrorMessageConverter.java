453e335cf7e3718345567dea402738a32923cc04
Niels Meijer <nielsmeijer@hotmail.com>
2024-01-30 13:56:25 +0100

    Merge master and revert import changes

--- management-gateway/src/main/java/org/frankframework/management/gateway/ErrorMessageConverter.java
+++ management-gateway/src/main/java/org/frankframework/management/gateway/ErrorMessageConverter.java
@@ -17,12 +17,13 @@ package org.frankframework.management.gateway;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.frankframework.management.bus.BusException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.integration.handler.AbstractReplyProducingMessageHandler;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageDeliveryException;
+
+import org.frankframework.management.bus.BusException;
 
 /**
  * Converter which (when used in combination with a PublishSubscribeChannel) retrieves
