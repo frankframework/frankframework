ec5845effbe24fc06a4f446db0e6949832e9a5f9
Niels Meijer <nielsmeijer@hotmail.com>
2024-02-19 11:17:07 +0100

    Don't catch all exceptions but rethrow (#6284)
    (cherry picked from commit d22273966650fe54de6b914c97bf532a2c7183ba)

---
+++ core/src/test/java/org/frankframework/receivers/MockPushingListener.java
@@ -0,0 +1,28 @@
+package org.frankframework.receivers;
+
+import org.frankframework.core.IMessageHandler;
+import org.frankframework.core.IPushingListener;
+import org.frankframework.core.IbisExceptionListener;
+import org.frankframework.core.ListenerException;
+import org.frankframework.core.PipeLineSession;
+import org.frankframework.stream.Message;
+
+import lombok.Setter;
+
+public class MockPushingListener extends MockListenerBase implements IPushingListener<String> {
+	private @Setter IMessageHandler<String> handler;
+	private @Setter IbisExceptionListener exceptionListener;
+
+	@Override
+	public void offerMessage(String text) throws ListenerException {
+		try(PipeLineSession session = new PipeLineSession()) {
+			RawMessageWrapper<String> raw = wrapRawMessage(text, session);
+			handler.processRequest(this, raw, Message.asMessage(text), session);
+		}
+	}
+
+	@Override
+	public RawMessageWrapper<String> wrapRawMessage(String rawMessage, PipeLineSession session) throws ListenerException {
+		return new RawMessageWrapper<>(rawMessage, session.getMessageId(), session.getCorrelationId());
+	}
+}
