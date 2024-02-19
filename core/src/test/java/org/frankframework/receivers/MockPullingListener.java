d22273966650fe54de6b914c97bf532a2c7183ba
Niels Meijer <nielsmeijer@hotmail.com>
2024-02-19 11:17:07 +0100

    Don't catch all exceptions but rethrow (#6284)

---
+++ core/src/test/java/org/frankframework/receivers/MockPullingListener.java
@@ -0,0 +1,41 @@
+package org.frankframework.receivers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.frankframework.core.IPullingListener;
+import org.frankframework.core.ListenerException;
+
+public class MockPullingListener extends MockListenerBase implements IPullingListener<String> {
+	private final BlockingQueue<String> value = new ArrayBlockingQueue<>(5);
+
+	@Override
+	public Map<String, Object> openThread() throws ListenerException {
+		return new HashMap<>();
+	}
+
+	@Override
+	public void closeThread(Map<String, Object> threadContext) throws ListenerException {
+		//No-op
+	}
+
+	@Override
+	void offerMessage(String text) {
+		value.add(text);
+	}
+
+	@Override
+	public RawMessageWrapper<String> getRawMessage(Map<String, Object> threadContext) throws ListenerException {
+		String message = value.poll();
+		if(message != null) {
+			if(message.equals("getRawMessageException")) {
+				throw new ListenerException(message);
+			}
+			return new RawMessageWrapper<>(message);
+		}
+		return null;
+	}
+
+}
