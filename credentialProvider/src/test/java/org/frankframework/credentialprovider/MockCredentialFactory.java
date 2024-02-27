c1260f43ae2d0949fe4df3971a8883e9f1076be5
J. Koster <j.koster@gmx.com>
2024-02-27 18:50:34 +0100

    Support multiple Credential factories (#6354)

--- credentialProvider/src/test/java/org/frankframework/credentialprovider/MockCredentialFactory.java
+++ credentialProvider/src/test/java/org/frankframework/credentialprovider/MockCredentialFactory.java
@@ -6,6 +6,20 @@ import java.util.function.Supplier;
 
 public class MockCredentialFactory extends HashMap<String, Credentials> implements ICredentialFactory {
 
+	private static MockCredentialFactory instance;
+
+	public static MockCredentialFactory getInstance() {
+		if (instance == null) {
+			instance = new MockCredentialFactory();
+		}
+		return instance;
+	}
+
+	@Override
+	public void initialize() {
+		instance = this;
+	}
+
 	@Override
 	public boolean hasCredentials(String alias) {
 		return containsKey(alias);
@@ -15,19 +29,19 @@ public class MockCredentialFactory extends HashMap<String, Credentials> implemen
 	public Credentials getCredentials(String alias, Supplier<String> defaultUsernameSupplier, Supplier<String> defaultPasswordSupplier) {
 		Credentials result = new Credentials(alias, defaultUsernameSupplier, defaultPasswordSupplier);
 		Credentials entry = get(alias);
-		if (entry!=null) {
-			if (entry.getUsername()!=null && !entry.getUsername().isEmpty()) result.setUsername(entry.getUsername());
-			if (entry.getPassword()!=null && !entry.getPassword().isEmpty()) result.setPassword(entry.getPassword());
+		if (entry != null) {
+			if (entry.getUsername() != null && !entry.getUsername().isEmpty()) result.setUsername(entry.getUsername());
+			if (entry.getPassword() != null && !entry.getPassword().isEmpty()) result.setPassword(entry.getPassword());
 		}
 		return result;
 	}
 
 	public void add(String alias, String username, String password) {
-		put(alias, new Credentials(alias, ()->username, ()->password));
+		put(alias, new Credentials(alias, () -> username, () -> password));
 	}
 
 	@Override
-	public Collection<String> getConfiguredAliases() throws Exception {
+	public Collection<String> getConfiguredAliases() {
 		return keySet();
 	}
 }
