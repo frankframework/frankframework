9fe5f8f68d7e0a97704c9387be5144bd08ba22e2
Matthbo <matthijs.booman@hotmail.com>
2024-03-06 18:32:12 +0100

    Add unit tests for VariablesFilterPipe & DropLastCharPipe

--- console/frontend/src/main/frontend/src/app/pipes/drop-last-char.pipe.spec.ts
+++ console/frontend/src/main/frontend/src/app/pipes/drop-last-char.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { DropLastCharPipe } from './drop-last-char.pipe';
 
 describe('DropLastCharPipe', () => {
+  let pipe: DropLastCharPipe;
+
+  beforeEach(() => {
+    pipe = new DropLastCharPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new DropLastCharPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('drops the last character', () => {
+    expect(pipe.transform('This is a test')).toBe('This is a tes');
+  });
+
+  it('to do nothing on an empty string', () => {
+    expect(pipe.transform('')).toBe('');
   });
 });
