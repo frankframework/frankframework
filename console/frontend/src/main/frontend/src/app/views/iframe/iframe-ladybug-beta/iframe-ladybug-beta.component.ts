919fd201828ca231939f675294e9c9f4d287f50a
Matthbo <matthijs.booman@hotmail.com>
2024-03-13 16:31:09 +0100

    remove fit-height for css & simpeler solution

--- console/frontend/src/main/frontend/src/app/views/iframe/iframe-ladybug-beta/iframe-ladybug-beta.component.ts
+++ console/frontend/src/main/frontend/src/app/views/iframe/iframe-ladybug-beta/iframe-ladybug-beta.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 import { AppService } from 'src/app/app.service';
+import { BaseIframeComponent } from '../iframe.base';
 
 @Component({
   selector: 'app-iframe-ladybug-beta',
   templateUrl: '../iframe.component.html',
   styleUrls: ['../iframe.component.scss'],
 })
-export class IframeLadybugBetaComponent implements OnInit {
-  url: string = '';
-  iframeSrc?: SafeResourceUrl;
-  redirectURL?: string;
-
+export class IframeLadybugBetaComponent
+  extends BaseIframeComponent
+  implements OnInit
+{
   constructor(
     private sanitizer: DomSanitizer,
     private appService: AppService,
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.url = `${this.appService.getServerPath()}iaf/ladybug`;
