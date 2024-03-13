b59af324a9d9f8107052fcd53c13fe66a5df6e8f
Matthbo <matthijs.booman@hotmail.com>
2024-03-13 17:22:26 +0100

    Add unit test for th-sortable directive

--- console/frontend/src/main/frontend/src/app/components/th-sortable.directive.spec.ts
+++ console/frontend/src/main/frontend/src/app/components/th-sortable.directive.spec.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
-import { ThSortableDirective } from './th-sortable.directive';
+import {
+  Component,
+  DebugElement,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import {
+  SortEvent,
+  ThSortableDirective,
+  basicTableSort,
+} from './th-sortable.directive';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 @Component({
   // standalone: true,
-  template: `<!-- TODO -->`,
+  template: `
+    <table>
+      <thead>
+        <tr>
+          <th sortable="name" (sorted)="onSort($event)">Name</th>
+          <th sortable="value" (sorted)="onSort($event)">Size</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let item of items">
+          <td>{{ item.name }}</td>
+          <td>{{ item.value }}</td>
+        </tr>
+      </tbody>
+    </table>
+  `,
   imports: [ThSortableDirective],
 })
-class TestComponent {}
+class TestComponent {
+  items = [
+    { name: 'a', value: 2 },
+    { name: 'b', value: 1 },
+  ];
+
+  @ViewChildren(ThSortableDirective) headers!: QueryList<ThSortableDirective>;
+
+  onSort(event: SortEvent): void {
+    this.items = basicTableSort(this.items, this.headers, event);
+  }
+}
 
 describe('ThSortableDirective', () => {
   let fixture: ComponentFixture<TestComponent>;
+  let directiveElements: DebugElement[];
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
       imports: [],
@@ -18,10 +55,56 @@ describe('ThSortableDirective', () => {
     }).createComponent(TestComponent);
 
     fixture.detectChanges(); // initial binding
+
+    directiveElements = fixture.debugElement.queryAll(
+      By.directive(ThSortableDirective),
+    );
   });
 
-  // temporatory, remove when making actual tests
-  it('parent component should exist', () => {
-    expect(fixture).toBeTruthy();
+  it('on click switches from asc to desc', () => {
+    directiveElements[0].nativeElement.click();
+    const directiveInstance =
+      directiveElements[0].injector.get(ThSortableDirective);
+
+    expect(directiveInstance.direction).toBe('asc');
+    directiveElements[0].nativeElement.click();
+    expect(directiveInstance.direction).toBe('desc');
+  });
+
+  it('sorts table rows', () => {
+    const directive0Instance =
+      directiveElements[0].injector.get(ThSortableDirective);
+    const directive0Element = directiveElements[0].nativeElement;
+    const directive1Instance =
+      directiveElements[1].injector.get(ThSortableDirective);
+    const directive1Element = directiveElements[1].nativeElement;
+
+    directive0Element.click();
+    expect(directive0Instance.direction).toBe('asc');
+    expect(fixture.componentInstance.items[0]).toEqual({
+      name: 'a',
+      value: 2,
+    });
+
+    directive0Element.click();
+    expect(directive0Instance.direction).toBe('desc');
+    expect(fixture.componentInstance.items[0]).toEqual({
+      name: 'b',
+      value: 1,
+    });
+
+    directive1Element.click();
+    expect(directive1Instance.direction).toBe('asc');
+    expect(fixture.componentInstance.items[0]).toEqual({
+      name: 'b',
+      value: 1,
+    });
+
+    directive1Element.click();
+    expect(directive1Instance.direction).toBe('desc');
+    expect(fixture.componentInstance.items[0]).toEqual({
+      name: 'a',
+      value: 2,
+    });
   });
 });
