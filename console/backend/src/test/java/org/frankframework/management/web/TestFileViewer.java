dae02a6154d7b296eee841e68d16357a0b156394
Matthbo <matthijs.booman@hotmail.com>
2024-03-18 17:23:21 +0100

    FileViewer override accept header using accept param

--- console/backend/src/test/java/org/frankframework/management/web/TestFileViewer.java
+++ console/backend/src/test/java/org/frankframework/management/web/TestFileViewer.java
@@ -65,10 +65,34 @@ public class TestFileViewer extends FrankApiTestBase<FileViewer> {
 		assertFalse(resultString.contains("<br>"));
 	}
 
+	@Test
+	public void testDownloadFile() throws IOException {
+		URL fileUrl = TestFileViewer.class.getResource("/FileViewer/FileViewer.txt");
+		String filePath = fileUrl.getPath();
+		String fileName = FilenameUtils.getName(filePath);
+
+		doAnswer((i) -> {
+			RequestMessageBuilder inputMessage = i.getArgument(0);
+			inputMessage.addHeader(ResponseMessageBase.STATUS_KEY, 200);
+			inputMessage.setPayload(new FileInputStream(filePath));
+			Message<?> msg = inputMessage.build();
+			MessageHeaders headers = msg.getHeaders();
+			assertEquals("FILE_VIEWER", headers.get("topic"));
+			assertEquals("octet-stream", headers.get("meta-resultType"));
+			return msg;
+		}).when(jaxRsResource).sendSyncMessage(any(RequestMessageBuilder.class));
+
+		String requestUrl = "/file-viewer?file=" + fileName + "&accept=application/octet-stream"; // ignore accept header & use parameter
+		Response response = dispatcher.dispatchRequest(HttpMethod.GET, requestUrl, null, IbisRole.IbisTester, Map.of("Accept", MediaType.TEXT_PLAIN));
+		InputStream result = (InputStream) response.getEntity();
+
+		String resultString = StreamUtil.streamToString(result);
+		assertFalse(resultString.contains("<br>"));
+	}
+
 	private static Message<?> getDefaultAnswer(InvocationOnMock i, String filePath) throws FileNotFoundException {
 		RequestMessageBuilder inputMessage = i.getArgument(0);
 		inputMessage.addHeader(ResponseMessageBase.STATUS_KEY, 200);
-		inputMessage.addHeader(ResponseMessageBase.MIMETYPE_KEY, MediaType.TEXT_HTML);
 		inputMessage.setPayload(new FileInputStream(filePath));
 		Message<?> msg = inputMessage.build();
 		MessageHeaders headers = msg.getHeaders();
