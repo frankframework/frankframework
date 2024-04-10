11bdc3b0a2d6eccc3e7f7777dbf27ec547b45b3f
Niels Meijer <nielsmeijer@hotmail.com>
2024-04-10 12:58:40 +0200

    Update core/src/main/java/org/frankframework/util/DirectoryCleaner.java
    Co-authored-by: Tim van der Leeuw <tnleeuw@gmail.com>

--- core/src/main/java/org/frankframework/util/DirectoryCleaner.java
+++ core/src/main/java/org/frankframework/util/DirectoryCleaner.java
@@ -113,7 +113,7 @@ public class DirectoryCleaner {
 	}
 
 	/**
-	 * Time (with suffix 'd', 'h', 'm' or 's') that must have passed at least before a file will be deleted.
+	 * Minimum amount of time (with suffix 'd', 'h', 'm' or 's') that must have passed before a file will be deleted.
 	 * You may only use one suffix!
 	 * @ff.default 30d
 	 */
