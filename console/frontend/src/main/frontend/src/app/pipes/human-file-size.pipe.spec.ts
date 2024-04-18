2edac5c327dbe6f18b6736bcd4e83fb399ece9a2
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-04-18 09:50:45 +0200

    fix: test names

--- console/frontend/src/main/frontend/src/app/pipes/human-file-size.pipe.spec.ts
+++ console/frontend/src/main/frontend/src/app/pipes/human-file-size.pipe.spec.ts
@@ -16,31 +16,31 @@ describe('HumanFileSizePipe', () => {
     expect(pipe.transform(123)).toBe('123 B');
   });
 
-  it('Transform 4 characters to IS', () => {
+  it('Transform 4 characters to binary (IEC)', () => {
     expect(pipe.transform(1234)).toBe('1.2 KiB');
   });
 
-  it('Transform 8 characters to IS', () => {
+  it('Transform 8 characters to binary (IEC)', () => {
     expect(pipe.transform(12_345_678)).toBe('11.8 MiB');
   });
 
-  it('Transform 8 characters to ICE', () => {
+  it('Transform 8 characters to metric (SI)', () => {
     expect(pipe.transform(12_345_678, true)).toBe('12.3 MB');
   });
 
-  it('Transform 8 characters to IS with no decimals', () => {
+  it('Transform 8 characters to binary (IEC) with no decimals', () => {
     expect(pipe.transform(12_345_678, false, 0)).toBe('12 MiB');
   });
 
-  it('Transform 8 characters to ICE with no decimals', () => {
+  it('Transform 8 characters to metric (SI) with no decimals', () => {
     expect(pipe.transform(12_345_678, true, 0)).toBe('12 MB');
   });
 
-  it('Transform 8 characters to IS with all decimals', () => {
+  it('Transform 8 characters to binary (IEC) with all decimals', () => {
     expect(pipe.transform(12_345_678, false, 6)).toBe('11.773756 MiB');
   });
 
-  it('Transform 8 characters to ICE with all decimals', () => {
+  it('Transform 8 characters to metric (SI) with all decimals', () => {
     expect(pipe.transform(12_345_678, true, 6)).toBe('12.345678 MB');
   });
 });
