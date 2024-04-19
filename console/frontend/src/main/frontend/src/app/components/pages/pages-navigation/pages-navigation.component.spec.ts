23ad5c05ff9b6b8f6eb242387072e6df345a805f
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-04-19 15:41:11 +0200

    test: fix routermodule in standalone components and add jquery to test scripts:

--- console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/pages-navigation.component.spec.ts
+++ console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/pages-navigation.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PagesNavigationComponent } from './pages-navigation.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../app-routing.module';
 
 describe('PagesNavigationComponent', () => {
   let component: PagesNavigationComponent;
@@ -9,7 +12,8 @@ describe('PagesNavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PagesNavigationComponent],
+      imports: [PagesNavigationComponent, HttpClientTestingModule],
+      providers: [provideRouter(routes)],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
