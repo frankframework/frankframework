8c9eb4c175ab8fa98543b7aef0c995bd566ca998
Matthbo <4380412+Matthbo@users.noreply.github.com>
2024-04-26 15:31:50 +0200

    Remake ShowAdapterStatistics & ShowConfiguration

--- console/backend/src/main/java/org/frankframework/management/web/spring/AdapterStatistics.java
+++ console/backend/src/main/java/org/frankframework/management/web/spring/AdapterStatistics.java
@@ -11,9 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 
+@RestController
 public class AdapterStatistics extends FrankApiBase {
 
 	@RolesAllowed({"IbisObserver", "IbisDataAdmin", "IbisAdmin", "IbisTester"})
