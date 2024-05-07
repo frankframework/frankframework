4586545a6e4e48af0b9fc79ebedd14115d315e97
Niels Meijer <nielsmeijer@hotmail.com>
2024-05-07 11:13:39 +0200

    Fix json conversion in restlistener (#6775)
    (cherry picked from commit df0056ea90dd53c3342856460c6c8202067bb123)

--- core/src/main/java/nl/nn/adapterframework/http/RestListener.java
+++ core/src/main/java/nl/nn/adapterframework/http/RestListener.java
@@ -62,6 +62,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 	private @Getter boolean writeToSecLog = false;
 	private @Getter boolean writeSecLogMessage = false;
 	private @Getter boolean retrieveMultipart = true;
+	private @Getter boolean automaticallyTransformToAndFromJson = true;
 
 	private @Getter MediaTypes consumes = MediaTypes.XML;
 	private @Getter MediaTypes produces = MediaTypes.XML;
@@ -103,7 +104,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 	public Message processRequest(Message message, PipeLineSession session) throws ListenerException {
 		HttpServletRequest httpServletRequest = (HttpServletRequest) session.get(PipeLineSession.HTTP_REQUEST_KEY);
 		Message response;
-		String contentType = (String) session.get("contentType");
+		String acceptHeaderThatIsSomeHowStoredUnderThisKey = (String) session.get("contentType");
 
 		//Check if valid path
 		String requestRestPath = (String) session.get("restPath");
@@ -112,7 +113,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 		}
 
 		//Check if consumes has been set or contentType is set to JSON
-		if(getConsumes()== MediaTypes.JSON && "application/json".equalsIgnoreCase(httpServletRequest.getContentType())) {
+		if(automaticallyTransformToAndFromJson && getConsumes()== MediaTypes.JSON && "application/json".equalsIgnoreCase(httpServletRequest.getContentType())) {
 			try {
 				message = transformToXml(message);
 			} catch (PipeRunException e) {
@@ -122,7 +123,7 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 		int eTag = 0;
 
 		//Check if contentType is not overwritten which disabled auto-converting and mediatype headers
-		if(contentType == null || StringUtils.isEmpty(contentType) || contentType.equalsIgnoreCase("*/*")) {
+		if(StringUtils.isEmpty(acceptHeaderThatIsSomeHowStoredUnderThisKey) || "*/*".equalsIgnoreCase(acceptHeaderThatIsSomeHowStoredUnderThisKey)) {
 			switch(getProduces()) {
 				case XML:
 					session.put("contentType", "application/xml");
@@ -141,10 +142,10 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 			if(response != null && !response.isEmpty())
 				eTag = response.hashCode();
 
-			if(getProduces()== MediaTypes.JSON) {
+			if(automaticallyTransformToAndFromJson && getProduces()== MediaTypes.JSON) {
 				try {
 					response = transformToJson(response);
-				} catch (PipeRunException e) {
+				} catch (PipeRunException | ConfigurationException e) {
 					throw new ListenerException("Failed to transform XML to JSON", e);
 				}
 			}
@@ -162,9 +163,14 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 		return response;
 	}
 
-	public Message transformToJson(Message message) throws PipeRunException {
+	public Message transformToJson(Message message) throws PipeRunException, ConfigurationException {
 		JsonPipe pipe = new JsonPipe();
 		pipe.setDirection(Direction.XML2JSON);
+		try {
+			pipe.configure();
+		} catch (ConfigurationException e) {
+			throw new ConfigurationException("unable to configure ");
+		}
 		PipeRunResult pipeResult = pipe.doPipe(message, new PipeLineSession());
 		return pipeResult.getResult();
 	}
@@ -290,5 +296,14 @@ public class RestListener extends PushingListenerAdapter implements HasPhysicalD
 	 */
 	public void setGenerateEtag(boolean b) {
 		this.generateEtag = b;
+	}
+
+	/**
+	 * Uses an JsonPipe to convert the json-input to xml, and xml-output to json.
+	 * Use with caution, a properly configured Input/Output-wrapper can do much more and is more robust!
+	 * @ff.default true
+	 */
+	public void setAutomaticallyTransformToAndFromJson(boolean b) {
+		automaticallyTransformToAndFromJson = b;
 	}
 }
