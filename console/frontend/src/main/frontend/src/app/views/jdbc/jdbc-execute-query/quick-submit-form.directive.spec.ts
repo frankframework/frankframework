8a2884480318916b1cd3ba9afe5e04bf13af0685
Matthbo <4380412+Matthbo@users.noreply.github.com>
2024-05-29 14:54:39 +0200

    Fix quick-submit-form test

--- console/frontend/src/main/frontend/src/app/views/jdbc/jdbc-execute-query/quick-submit-form.directive.spec.ts
+++ console/frontend/src/main/frontend/src/app/views/jdbc/jdbc-execute-query/quick-submit-form.directive.spec.ts
@@ -1,20 +1,22 @@
 import { Component, DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { QuickSubmitFormDirective } from './quick-submit-form.directive';
 import { By } from '@angular/platform-browser';
 
 @Component({
   // standalone: true,
-  template: `<textarea
-    appQuickSubmitForm
-    (quickSubmit)="changeTrigger()"
-  ></textarea>`,
+  template: `<form>
+    <textarea appQuickSubmitForm></textarea>
+    <button type="submit" (click)="changeTrigger($event)">Submit</button>
+  </form>`,
   imports: [QuickSubmitFormDirective],
 })
 class TestComponent {
   triggered = false;
-  changeTrigger(): void {
+  changeTrigger(event: Event): void {
+    event.preventDefault();
     this.triggered = true;
+    event.stopPropagation();
   }
 }
 
@@ -34,13 +36,14 @@ describe('QuickSubmitFormDirective', () => {
     );
   });
 
-  it('emits event when ctrl+enter is pressed', () => {
+  it('emits event when ctrl+enter is pressed', fakeAsync(() => {
     const event = new KeyboardEvent('keydown', {
       ctrlKey: true,
       key: 'Enter',
     });
     directiveElement.nativeElement.dispatchEvent(event);
 
+    console.log(fixture.componentInstance.triggered);
     expect(fixture.componentInstance.triggered).toBe(true);
-  });
+  }));
 });
