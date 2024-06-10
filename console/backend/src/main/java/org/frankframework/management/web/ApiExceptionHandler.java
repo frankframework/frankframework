c6ba467c4e7bfee833c2adb67a82c42fc9dee064
Matthbo <4380412+Matthbo@users.noreply.github.com>
2024-06-10 12:04:10 +0200

    Handle all thrown exceptions & simplify handleException method in ApiExceptionHandler

--- console/backend/src/main/java/org/frankframework/management/web/ApiExceptionHandler.java
+++ console/backend/src/main/java/org/frankframework/management/web/ApiExceptionHandler.java
@@ -16,6 +16,7 @@
 package org.frankframework.management.web;
 
 import jakarta.annotation.Nullable;
+import jakarta.servlet.ServletException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
@@ -27,6 +28,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -40,7 +42,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -57,49 +58,25 @@ public class ApiExceptionHandler {
 			HttpMediaTypeNotSupportedException.class,
 			HttpMediaTypeNotAcceptableException.class,
 			MissingPathVariableException.class,
-			MissingServletRequestParameterException.class,
-			MissingServletRequestPartException.class,
-			ServletRequestBindingException.class,
 			MethodArgumentNotValidException.class,
 			NoResourceFoundException.class,
 			AsyncRequestTimeoutException.class,
 			ErrorResponseException.class,
 			MaxUploadSizeExceededException.class,
-
+			Exception.class
 	})
-	protected final ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
-		if (ex instanceof HttpRequestMethodNotSupportedException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof HttpMediaTypeNotSupportedException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof HttpMediaTypeNotAcceptableException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof MissingPathVariableException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof MissingServletRequestParameterException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof MissingServletRequestPartException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof ServletRequestBindingException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof MethodArgumentNotValidException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof HandlerMethodValidationException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof NoResourceFoundException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof AsyncRequestTimeoutException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof ErrorResponseException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
-		} else if (ex instanceof MaxUploadSizeExceededException subEx) {
-			return handleSpringException(subEx, subEx.getStatusCode(), subEx.getHeaders());
+	protected final ResponseEntity<Object> handleException(Exception ex) {
+		if (ex instanceof ErrorResponse errorResponseEx){
+			return handleSpringException(ex, errorResponseEx.getStatusCode(), errorResponseEx.getHeaders());
 		}
 		return handleExceptionLowLevel(ex);
 	}
 
-	/** Lower level exceptions, and exceptions used symmetrically on client and server */
+	/** Lower level exceptions */
 	@ExceptionHandler({
+			MissingServletRequestParameterException.class,
+			MissingServletRequestPartException.class,
+			ServletRequestBindingException.class,
 			ConversionNotSupportedException.class,
 			TypeMismatchException.class,
 			HttpMessageNotReadableException.class,
@@ -110,7 +87,11 @@ public class ApiExceptionHandler {
 	protected final ResponseEntity<Object> handleExceptionLowLevel(Exception ex){
 		HttpHeaders headers = new HttpHeaders();
 		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-		if (ex instanceof TypeMismatchException || ex instanceof HttpMessageNotReadableException || ex instanceof BindException) {
+		if (ex instanceof ServletException
+				|| ex instanceof TypeMismatchException
+				|| ex instanceof HttpMessageNotReadableException
+				|| ex instanceof BindException
+		) {
 			status = HttpStatus.BAD_REQUEST;
 		}
 		return handleSpringException(ex, status, headers);
