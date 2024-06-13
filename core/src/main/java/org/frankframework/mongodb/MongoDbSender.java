32c3f6c0543369fe09167efef771ae3e365f382c
J. Koster <j.koster@gmx.com>
2024-06-13 21:24:25 +0200

    Add missing improvements from master

--- core/src/main/java/org/frankframework/mongodb/MongoDbSender.java
+++ core/src/main/java/org/frankframework/mongodb/MongoDbSender.java
@@ -118,6 +118,10 @@ public class MongoDbSender extends SenderWithParametersBase implements HasPhysic
 		UPDATEMANY,
 		DELETEONE,
 		DELETEMANY;
+
+		public static boolean isFind(MongoAction action) {
+			return action == FINDONE || action == FINDMANY;
+		}
 	}
 
 
@@ -167,7 +171,7 @@ public class MongoDbSender extends SenderWithParametersBase implements HasPhysic
 	public SenderResult sendMessage(Message message, PipeLineSession session) throws SenderException, TimeoutException {
 		message.closeOnCloseOf(session, this);
 		MongoAction mongoAction = getAction();
-		try (MessageOutputStream target = mongoAction==MongoAction.FINDONE || mongoAction==MongoAction.FINDMANY ? MessageOutputStream.getTargetStream(this, session, null) : new MessageOutputStreamCap(this, null)) {
+		try (MessageOutputStream target = MongoAction.isFind(mongoAction) ? MessageOutputStream.getTargetStream(this, session, null) : new MessageOutputStreamCap(this, null)) {
 			ParameterValueList pvl = ParameterValueList.get(getParameterList(), message, session);
 			MongoDatabase mongoDatabase = getDatabase(pvl);
 			MongoCollection<Document> mongoCollection = getCollection(mongoDatabase, pvl);
@@ -330,7 +334,7 @@ public class MongoDbSender extends SenderWithParametersBase implements HasPhysic
 		if (StringUtils.isEmpty(databaseName)) {
 			throw new SenderException("no database found from attribute or parameter");
 		}
-		return mongoDatabases.computeIfAbsent(databaseName, (d)->mongoClient.getDatabase(databaseName));
+		return mongoDatabases.computeIfAbsent(databaseName, d->mongoClient.getDatabase(databaseName));
 	}
 
 	protected MongoCollection<Document> getCollection(MongoDatabase mongoDatabase, ParameterValueList pvl) throws SenderException {
