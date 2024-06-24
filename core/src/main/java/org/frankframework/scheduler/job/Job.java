2d898ca9a9442edc57e1cf7d89b448b32ead2f71
Niels Meijer <nielsmeijer@hotmail.com>
2024-06-24 14:44:22 +0200

    Improve Parameter

--- core/src/main/java/org/frankframework/scheduler/job/Job.java
+++ core/src/main/java/org/frankframework/scheduler/job/Job.java
@@ -26,8 +26,8 @@ import org.frankframework.scheduler.JobDefFunctions;
 /**
  * Placeholder class to allow legacy configuration notations <code>&lt;job function='SendMessage' /&gt;</code> in the new Frank!Config XSD.
  * <p>
- * The attribute <code>function='SendMessage'</code> has been removed in favor of explicit JobDefinitions such as: <code>SendMessageJob</code>, <code>ExecuteQueryJob</code> and <code>IbisActionJob</code>.
- * Using the new elements enables the use of auto-completion.
+ * The attribute <code>function</code> has been removed in favor of explicit JobDefinitions such as: <code>SendMessageJob</code>, <code>ExecuteQueryJob</code> and <code>IbisActionJob</code>.
+ * Using the new elements enables the use of auto-completion for the specified type.
  *
  * @author Niels Meijer
  */
