b09236bf9f4afe029c0bd8a6c390c9e7ed3f8346
Tim van der Leeuw <tnleeuw@gmail.com>
2024-08-12 20:08:38 +0200

    Fix Json2XmlInputValidator with multiple optional elements (#7305)

--- core/src/main/java/org/frankframework/align/Tree2Xml.java
+++ core/src/main/java/org/frankframework/align/Tree2Xml.java
@@ -100,9 +100,6 @@ public abstract class Tree2Xml<C,N> extends ToXml<C,N> {
 		String result=getNodeText(elementDeclaration, node);
 		if (sp!=null && StringUtils.isEmpty(result) && (text=sp.getDefault(getContext()))!=null) {
 			if (log.isTraceEnabled()) log.trace("node ["+nodeName+"] default found ["+text+"]");
-			if (text instanceof String) {
-				result = (String)text;
-			}
 			result = text.toString();
 		}
 		if (log.isTraceEnabled()) log.trace("node ["+nodeName+"] returning value ["+result+"]");
@@ -118,9 +115,6 @@ public abstract class Tree2Xml<C,N> extends ToXml<C,N> {
 		return unProcessedChildren;
 	}
 
-	public SubstitutionProvider<?> getSubstitutionProvider() {
-		return sp;
-	}
 	public void setSubstitutionProvider(SubstitutionProvider<?> substitutions) {
 		this.sp = substitutions;
 	}
