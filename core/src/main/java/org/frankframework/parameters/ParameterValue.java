7284d50741b1be515ff928de7e4bd89268990c22
Thomas P <thomaspj2003@gmail.com>
2024-08-13 13:23:21 +0200

    Move the data parameter to string tests to it's own class and replace SimpleDateFormat with DateTimeFormatter.

--- core/src/main/java/org/frankframework/parameters/ParameterValue.java
+++ core/src/main/java/org/frankframework/parameters/ParameterValue.java
@@ -18,6 +18,9 @@ package org.frankframework.parameters;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
@@ -146,8 +149,8 @@ public class ParameterValue {
 		}
 
 		if (getDefinition() instanceof DateParameter dateParameter && value instanceof Date date) {
-			final DateFormat df = new SimpleDateFormat(dateParameter.getFormatString());
-			return df.format(date);
+			final DateTimeFormatter dtf = DateTimeFormatter.ofPattern(dateParameter.getFormatString()).withZone(ZoneId.systemDefault());
+			return dtf.format(date.toInstant());
 		}
 
 		return value.toString();
