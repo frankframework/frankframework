066196a8f16e48c4116426b6b9cc6d8b1d25fc01
Niels Meijer <nielsmeijer@hotmail.com>
2024-08-23 15:36:49 +0200

    Fix iDin equals check (#7383)

--- idin/src/test/java/org/frankframework/extensions/idin/IdinSenderTest.java
+++ idin/src/test/java/org/frankframework/extensions/idin/IdinSenderTest.java
@@ -29,7 +29,6 @@ import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
 import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -66,7 +65,6 @@ public class IdinSenderTest {
 
 		IMessenger messenger = new DummyMessenger();
 		sender.setMessenger(messenger);
-		sender.configure();
 		session = new PipeLineSession();
 	}
 
@@ -75,18 +73,23 @@ public class IdinSenderTest {
 		@Override
 		public String sendMessage(Configuration config, String request, URI url) throws CommunicatorException {
 			String requestType = url.toString().replaceAll("http://example.com/", "");
+			int testId = config.getMerchantSubID();
 
-			validateRequest(requestType, request);
+			// post-fix the last character of the merchantID to differentiate between tests
+			String expectedFile = testId != 0 ? requestType + testId : requestType;
+
+			validateRequest(expectedFile, request);
 			return generateResponse(config, requestType);
 		}
 
-		private void validateRequest(String requestType, String request) throws CommunicatorException {
+		private void validateRequest(String expectedFile, String request) throws CommunicatorException {
 			try {
-				URL expected = ClassUtils.getResourceURL("/messages/"+requestType+"-request.xml");
-				assertNotNull(expected, "did not find [/messages/"+requestType+"-request.xml]");
+				URL expected = ClassUtils.getResourceURL("/messages/"+expectedFile+"-request.xml");
+				assertNotNull(expected, "did not find [/messages/"+expectedFile+"-request.xml]");
 				String expectedString = StreamUtil.resourceToString(expected);
+
 				request = request.replaceAll("<createDateTimestamp[\\s\\S]*?<\\/createDateTimestamp>", "<createDateTimestamp/>");
-				request = request.replaceAll("AuthnRequest [\\s\\S]*?>", "AuthRequest signature=\"here\">");
+				request = request.replaceAll("AuthnRequest [\\s\\S]*?>", "AuthnRequest xmlns:ns3=\"http://dummy\" signature=\"here\">");
 				request = request.replaceAll("<Signature[\\s\\S]*?<\\/Signature>", "");
 
 				assertEquals(expectedString.replace("\r", ""), request.replace("\r", ""));
@@ -99,9 +102,8 @@ public class IdinSenderTest {
 			try {
 				URL response = ClassUtils.getResourceURL("/messages/"+requestType+"-response.xml");
 				assertNotNull(response);
-				String responseString = StreamUtil.resourceToString(response);
 				SigningKeyPair keyEntry = config.getKeyProvider().getMerchantSigningKeyPair();
-				return addSignature(keyEntry, responseString);
+				return addSignature(keyEntry, StreamUtil.resourceToString(response));
 			} catch(Exception e) {
 				throw new CommunicatorException("unable to find/read reponse message", e);
 			}
@@ -109,29 +111,123 @@ public class IdinSenderTest {
 	}
 
 	@Test
-	public void testSimpleRequestResponse() throws Exception {
+	public void testXmlConfigurationFile() throws Exception {
 		String message = "<idin><transactionID>1111111111111111</transactionID></idin>";
 
 		sender.setAction(Action.RESPONSE);
+		sender.configure();
+
+		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
+		String expected = """
+				<result>
+					<status>Success</status>
+					<saml>
+						<acquirerId>BANKNL2U</acquirerId>
+						<attributes>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.initials">SJĆ</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.partnerlastnameprefix">Ja</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.legallastnameprefix">Sm</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.bin">Some Subject</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.legallastname">Smith</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:bankid.deliveredserviceid">4096</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.preferredlastname">John</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.partnerlastname">Jane</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.preferredlastnameprefix">Jo</attribute>
+						</attributes>
+						<merchantReference>BANKID-1234029966811132</merchantReference>
+						<version>BANKNL2U</version>
+					</saml>
+					<transactionID>1234567890123457</transactionID>
+					<timestamp>2020-08-17 17:28:10.008</timestamp>
+				</result>\
+				""";
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testPropertiesLoadedFromAttributes() throws Exception {
+		String message = "<idin><transactionID>1111111111111111</transactionID></idin>";
+
+		sender.setConfigurationXML("configs/minimal-config.xml"); //A minimal config with log setting is required
+
+		// These were never set
+		sender.setMerchantID("1234567890");
+		sender.setMerchantReturnUrl("http://localhost");
+		sender.setAcquirerStatusUrl("http://example.com/status");
+
+		// Override the keystore and password
+		sender.setKeyStoreLocation("certificates/BankID2020.Libs.sha256.2048.csp.jks");
+		sender.setKeyStorePassword("123456");
+
+		sender.setAction(Action.RESPONSE);
+		sender.configure();
+
 		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
 
-		assertEquals("<result>\n" + "	<status>Success</status>\n" + "</result>", result);
+		String expected = """
+				<result>
+					<status>Success</status>
+					<saml>
+						<acquirerId>BANKNL2U</acquirerId>
+						<attributes>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.initials">SJĆ</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.partnerlastnameprefix">Ja</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.legallastnameprefix">Sm</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.bin">Some Subject</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.legallastname">Smith</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:bankid.deliveredserviceid">4096</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.preferredlastname">John</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.partnerlastname">Jane</attribute>
+							<attribute name="urn:nl:bvn:bankid:1.0:consumer.preferredlastnameprefix">Jo</attribute>
+						</attributes>
+						<merchantReference>BANKID-1234029966811132</merchantReference>
+						<version>BANKNL2U</version>
+					</saml>
+					<transactionID>1234567890123457</transactionID>
+					<timestamp>2020-08-17 17:28:10.008</timestamp>
+				</result>\
+				""";
+		assertEquals(expected, result);
 	}
 
 	@Test
 	public void getIssuersByCountry() throws Exception {
 		String message = "<idin><issuersByCountry>true</issuersByCountry></idin>";
 
+		sender.setMerchantSubID(1);
+		sender.setMerchantID("1234567891");
 		sender.setAction(Action.DIRECTORY);
+		sender.configure();
+
 		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
-		assertEquals("<result>\n"
-				+ "	<issuers>\n"
-				+ "		<country name=\"NL\">\n"
-				+ "			<issuer id=\"INGBNL2A\">TEST</issuer>\n"
-				+ "		</country>\n"
-				+ "	</issuers>\n"
-				+ "	<timestamp>2015-07-15 12:10:10.123</timestamp>\n"
-				+ "</result>", result);
+		assertEquals("""
+				<result>
+					<issuers>
+						<country name="NL">
+							<issuer id="INGBNL2A">TEST</issuer>
+						</country>
+					</issuers>
+					<timestamp>2015-07-15 12:10:10.123</timestamp>
+				</result>\
+				""", result);
+	}
+
+	@Test
+	public void getIssuer() throws Exception {
+		String message = "<idin/>";
+
+		sender.setAction(Action.DIRECTORY);
+		sender.configure();
+
+		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
+		assertEquals("""
+				<result>
+					<issuers>
+						<issuer id="INGBNL2A" country="NL">TEST</issuer>
+					</issuers>
+					<timestamp>2015-07-15 12:10:10.123</timestamp>
+				</result>\
+				""", result);
 	}
 
 	@Test
@@ -139,17 +235,106 @@ public class IdinSenderTest {
 		String message = "<idin><issuerId>BANKNL2Y</issuerId><requestedServiceId>21968</requestedServiceId><entranceCode>abc4ef7ons</entranceCode></idin>";
 
 		sender.setAction(Action.AUTHENTICATE);
+		sender.configure();
+
 		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
 
-		assertEquals("<result>\n"
-				+ "	<authenticationURL>http://localhost/consumerAuthenticationSim?hash=4444</authenticationURL>\n"
-				+ "	<transactionID>0050000021142024</transactionID>\n"
-				+ "	<createDateTimestamp>2015-07-15 12:10:10.123</createDateTimestamp>\n"
-				+ "</result>", result);
+		assertEquals("""
+				<result>
+					<authenticationURL>http://localhost/consumerAuthenticationSim?hash=4444</authenticationURL>
+					<transactionID>0050000021142024</transactionID>
+					<createDateTimestamp>2015-07-15 12:10:10.123</createDateTimestamp>
+				</result>\
+				""", result);
+	}
+
+	@Test
+	public void testMerchantReturnUrlSessionKey() throws Exception {
+		String message = "<idin><issuerId>BANKNL2Y</issuerId><requestedServiceId>21968</requestedServiceId><entranceCode>abc4ef7ons</entranceCode></idin>";
+
+		sender.setMerchantReturnUrlSessionKey("sessionKeyName");
+		sender.setMerchantSubID(2);
+		sender.setAction(Action.AUTHENTICATE);
+		sender.configure();
+		session.put("sessionKeyName", "http://localhost:8080");
+
+		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
+
+		assertEquals("""
+				<result>
+					<authenticationURL>http://localhost/consumerAuthenticationSim?hash=4444</authenticationURL>
+					<transactionID>0050000021142024</transactionID>
+					<createDateTimestamp>2015-07-15 12:10:10.123</createDateTimestamp>
+				</result>\
+				""", result);
+	}
+
+	@Test
+	public void testAuthenticateFull() throws Exception {
+		String message = """
+				<idin>
+					<issuerId>BANKNL2Y</issuerId>
+					<language>de</language>
+					<requestedServiceId>21968</requestedServiceId>
+					<merchantReference>ref234</merchantReference>
+					<expirationPeriod>PT2M</expirationPeriod>
+					<entranceCode>abc4ef7ons</entranceCode>
+					<assuranceLevel>Loa3</assuranceLevel>
+				</idin>
+				""";
+
+		sender.setMerchantSubID(1);
+		sender.setAction(Action.AUTHENTICATE);
+		sender.configure();
+
+		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
+
+		assertEquals("""
+				<result>
+					<authenticationURL>http://localhost/consumerAuthenticationSim?hash=4444</authenticationURL>
+					<transactionID>0050000021142024</transactionID>
+					<createDateTimestamp>2015-07-15 12:10:10.123</createDateTimestamp>
+				</result>\
+				""", result);
+	}
+
+	@Test
+	public void testAuthenticateDurationLargerThen5Minutes() throws Exception {
+		String message = """
+				<idin>
+					<issuerId>BANKNL2Y</issuerId>
+					<requestedServiceId>21968</requestedServiceId>
+					<expirationPeriod>PT10M</expirationPeriod>
+					<entranceCode>abc4ef7ons</entranceCode>
+				</idin>
+				""";
+
+		sender.setAction(Action.AUTHENTICATE);
+		sender.configure();
+
+		String result = sender.sendMessageOrThrow(new Message(message), session).asString();
+
+		assertEquals("""
+				<result>
+					<error>
+						<statusCode/>
+						<details/>
+						<message>incorrect value: PT10M</message>
+					</error>
+				</result>\
+				""", result);
+	}
+
+	@Test
+	public void testPhysicalDestinationName() throws Exception {
+		sender.setAcquirerStatusUrl("http://localhost.com");
+		sender.setMerchantReturnUrlSessionKey("test123");
+		sender.configure();
+		assertEquals("returnUrl[dynamicReturnUrl] statusUrl[http://localhost.com]", sender.getPhysicalDestinationName());
 	}
 
 	private static String addSignature(SigningKeyPair keyEntry, String xml) throws Exception {
-		X509Certificate cert = (X509Certificate) keyEntry.getCertificate();
+		X509Certificate cert = keyEntry.getCertificate();
 
 		XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
 
@@ -180,7 +365,7 @@ public class IdinSenderTest {
 		return serialize(doc);
 	}
 
-	private static String serialize(Document doc) throws TransformerConfigurationException, TransformerException {
+	private static String serialize(Document doc) throws TransformerException {
 		StringWriter sw = new StringWriter();
 		Transformer trans = TransformerFactory.newInstance().newTransformer();
 
