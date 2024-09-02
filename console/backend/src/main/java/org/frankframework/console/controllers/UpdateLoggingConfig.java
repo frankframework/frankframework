35f6d82f11d9c3b49c8159196154816266ecf6fb
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-09-02 17:49:15 +0200

    Removed unused import from UpdateLoggingConfig.java

--- console/backend/src/main/java/org/frankframework/console/controllers/UpdateLoggingConfig.java
+++ console/backend/src/main/java/org/frankframework/console/controllers/UpdateLoggingConfig.java
@@ -34,7 +34,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class UpdateLoggingConfig extends FrankApiBase {
