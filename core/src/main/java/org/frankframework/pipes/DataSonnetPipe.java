5a51cb27e500f68b0d2c0dd085823cf483ce3b4d
Niels Meijer <nielsmeijer@hotmail.com>
2024-09-05 15:51:44 +0200

    Fix typo
    Co-authored-by: Erik <dongenevan@gmail.com>

--- core/src/main/java/org/frankframework/pipes/DataSonnetPipe.java
+++ core/src/main/java/org/frankframework/pipes/DataSonnetPipe.java
@@ -56,7 +56,7 @@ import org.frankframework.util.StreamUtil;
  * from and to different file formats specified by supported {@link DataSonnetOutputType outputTypes}.
  * </p>
  * <p>
- * The input message will be reference to the key {@code payload}.
+ * The input message will be referenced by the key {@code payload}.
  * It's required for the input message to have a correct MimeType, else the text will be interpreted as a String.
  * </p>
  * <p>
