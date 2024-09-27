bbafd3f2723d88e7c3eba4285138061d027ff76d
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-09-27 12:04:07 +0200

    Fix `null` configuration warnings update not being picked up

--- console/frontend/src/main/frontend/src/app/utils.ts
+++ console/frontend/src/main/frontend/src/app/utils.ts
@@ -73,11 +73,3 @@ export const compare = (v1: string | number, v2: string | number): 1 | -1 | 0 =>
 
 /** This is pretty bad, non primitive types won't be covered correctly (null, undefined, object, etc) */
 export const anyCompare = <T>(v1: T, v2: T): 1 | -1 | 0 => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
-
-export function findIndexOfAll<T>(array: T[], condition: (item: T) => boolean): number[] {
-  const indices: number[] = [];
-  for (const [index, element] of array.entries()) {
-    if (condition(element)) indices.push(index);
-  }
-  return indices;
-}
