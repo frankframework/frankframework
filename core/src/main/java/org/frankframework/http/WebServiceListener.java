b42f86958d3ec284b75d12cb886f7046ec631e79
Niels Meijer <nielsmeijer@hotmail.com>
2024-10-08 16:21:15 +0200

    Also unregister address on servicedispatcher
    (cherry picked from commit 1001197a1e0ddf379c311d94072c564a3163c060)

--- core/src/main/java/org/frankframework/http/WebServiceListener.java
+++ core/src/main/java/org/frankframework/http/WebServiceListener.java
@@ -162,7 +162,9 @@ public class WebServiceListener extends PushingListenerAdapter implements HasPhy
 			endpoint.stop();
 		}
 
-		if (StringUtils.isEmpty(getAddress())) {
+		if (StringUtils.isNotEmpty(getAddress())) {
+			ServiceDispatcher.getInstance().unregisterServiceClient(getAddress());
+		} else {
 			if (StringUtils.isNotEmpty(getServiceNamespaceURI())) {
 				log.debug("unregistering listener ["+getName()+"] from ServiceDispatcher by serviceNamespaceURI ["+getServiceNamespaceURI()+"]");
 				ServiceDispatcher.getInstance().unregisterServiceClient(getServiceNamespaceURI());
