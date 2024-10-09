38eaad8fa7cdac5a34718036997357b40df2d629
Thomas P <thomaspj2003@gmail.com>
2024-10-09 15:08:32 +0200

    Update core/src/main/java/org/frankframework/pipes/RegExPipe.java
    Co-authored-by: Erik <dongenevan@gmail.com>

--- core/src/main/java/org/frankframework/pipes/RegExPipe.java
+++ core/src/main/java/org/frankframework/pipes/RegExPipe.java
@@ -65,8 +65,8 @@ import java.util.regex.Pattern;
 @Forward(name = RegExPipe.ELSE_FORWARD, description = "When no match is found.")
 public class RegExPipe extends AbstractPipe {
 
-	protected final static String THEN_FORWARD = "then";
-	protected final static String ELSE_FORWARD = "else";
+	protected static final String THEN_FORWARD = "then";
+	protected static final String ELSE_FORWARD = "else";
 
 	enum RegExFlag {
 		CANON_EQ(Pattern.CANON_EQ),
