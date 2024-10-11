6482aa8489f621ae7470cbfcefcfa753ff72a4a8
evandongen <dongenevan@gmail.com>
2024-10-11 14:00:25 +0200

    Add unit test for zip iterator pipe

--- core/src/main/java/org/frankframework/compression/ZipIteratorPipe.java
+++ core/src/main/java/org/frankframework/compression/ZipIteratorPipe.java
@@ -24,7 +24,9 @@ import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
+
 import org.frankframework.configuration.ConfigurationException;
 import org.frankframework.configuration.ConfigurationWarning;
 import org.frankframework.core.IDataIterator;
@@ -33,9 +35,6 @@ import org.frankframework.core.SenderException;
 import org.frankframework.pipes.IteratingPipe;
 import org.frankframework.stream.Message;
 import org.frankframework.util.StreamUtil;
-
-import lombok.Getter;
-
 
 /**
  * Sends a message to a Sender for each entry of its input, that must be an ZipInputStream. The input of the pipe must be one of:
@@ -46,52 +45,140 @@ import lombok.Getter;
  * </ul>
  * The message sent each time to the sender is the filename of the entry found in the archive.
  * The contents of the archive is available as a Stream or a String in a session variable.
- *
+ * <p>
  * <br/>
  *
- * @author  Gerrit van Brakel
- * @since   4.9.10
+ * @author Gerrit van Brakel
+ * @since 4.9.10
  */
 public class ZipIteratorPipe extends IteratingPipe<String> {
 
-	private @Getter String contentsSessionKey="zipdata";
-	private @Getter boolean streamingContents=true;
-	private @Getter boolean closeInputstreamOnExit=true;
-	private @Getter String charset=StreamUtil.DEFAULT_INPUT_STREAM_ENCODING;
-	private @Getter boolean processFile=false;
+	private @Getter String contentsSessionKey = "zipdata";
+	private @Getter boolean streamingContents = true;
+	private @Getter boolean closeInputstreamOnExit = true;
+	private @Getter String charset = StreamUtil.DEFAULT_INPUT_STREAM_ENCODING;
+	private @Getter boolean processFile = false;
 
 	@Override
 	public void configure() throws ConfigurationException {
 		super.configure();
-			if (StringUtils.isEmpty(getContentsSessionKey())) {
-				throw new ConfigurationException("attribute contentsKey must be specified");
+		if (StringUtils.isEmpty(getContentsSessionKey())) {
+			throw new ConfigurationException("attribute contentsKey must be specified");
+		}
+	}
+
+	protected ZipInputStream getZipInputStream(Message input, PipeLineSession session, Map<String, Object> threadContext) throws SenderException {
+		if (input == null) {
+			throw new SenderException("input is null. Must supply String (Filename, with processFile=true), File or InputStream as input");
+		}
+		InputStream source;
+		if (isProcessFile()) {
+			try {
+				String filename = null;
+				try {
+					filename = input.asString();
+				} catch (IOException e) {
+					throw new SenderException("cannot find filename [" + filename + "]", e);
+				}
+				source = new FileInputStream(filename);
+			} catch (FileNotFoundException e) {
+				throw new SenderException("could not find file [" + input + "]", e);
 			}
+		} else {
+			try {
+				source = input.asInputStream();
+			} catch (IOException e) {
+				throw new SenderException("cannot open stream", e);
+			}
+		}
+		if (!(source instanceof BufferedInputStream)) {
+			source = new BufferedInputStream(source);
+		}
+
+		return new ZipInputStream(source);
+	}
+
+	@Override
+	protected IDataIterator<String> getIterator(Message input, PipeLineSession session, Map<String, Object> threadContext) throws SenderException {
+		ZipInputStream source = getZipInputStream(input, session, threadContext);
+
+		if (source == null) {
+			throw new SenderException("no ZipInputStream found");
+		}
+
+		return new ZipStreamIterator(source, session);
+	}
+
+	/**
+	 * Session key used to store contents of each zip entry
+	 *
+	 * @ff.default zipdata
+	 */
+	public void setContentsSessionKey(String string) {
+		contentsSessionKey = string;
+	}
+
+	/**
+	 * If set to <code>false</code>, a string containing the contents of the entry is placed under the session key, instead of the inputstream to the contents
+	 *
+	 * @ff.default true
+	 */
+	public void setStreamingContents(boolean b) {
+		streamingContents = b;
+	}
+
+	/**
+	 * If set to <code>false</code>, the inputstream is not closed after it has been used
+	 *
+	 * @ff.default true
+	 */
+	public void setCloseInputstreamOnExit(boolean b) {
+		closeInputstreamOnExit = b;
+	}
+
+	/**
+	 * Charset used when reading the contents of the entry (only used if streamingContents=false)
+	 *
+	 * @ff.default utf-8
+	 */
+	public void setCharset(String string) {
+		charset = string;
+	}
+
+	/**
+	 * If set <code>true</code>, each entry is assumed to be the name of a file to be compressed. Otherwise, the input itself is compressed.
+	 *
+	 * @ff.default false
+	 */
+	@Deprecated(forRemoval = true, since = "7.8.0")
+	@ConfigurationWarning("Please add a LocalFileSystemPipe with action=read in front of this pipe instead")
+	public void setProcessFile(boolean b) {
+		processFile = b;
 	}
 
 	private class ZipStreamIterator implements IDataIterator<String> {
-
 		ZipInputStream source;
 		PipeLineSession session;
 
-		boolean nextRead=false;
-		boolean currentOpen=false;
+		boolean nextRead = false;
+		boolean currentOpen = false;
 		ZipEntry current;
 
 		ZipStreamIterator(ZipInputStream source, PipeLineSession session) {
 			super();
-			this.source=source;
-			this.session=session;
+			this.source = source;
+			this.session = session;
 		}
 
 		private void skipCurrent() throws IOException {
 			if (currentOpen) {
-				currentOpen=false;
-				nextRead=false;
+				currentOpen = false;
+				nextRead = false;
 				source.closeEntry();
 			}
 			if (!nextRead) {
-				current=source.getNextEntry();
-				nextRead=true;
+				current = source.getNextEntry();
+				nextRead = true;
 			}
 		}
 
@@ -100,7 +187,7 @@ public class ZipIteratorPipe extends IteratingPipe<String> {
 			log.debug("hasNext()");
 			try {
 				skipCurrent();
-				return current!=null;
+				return current != null;
 			} catch (IOException e) {
 				throw new SenderException(e);
 			}
@@ -111,14 +198,14 @@ public class ZipIteratorPipe extends IteratingPipe<String> {
 			log.debug("next()");
 			try {
 				skipCurrent();
-				currentOpen=true;
+				currentOpen = true;
 				log.debug("found zipEntry name [{}] size [{}] compressed size [{}]", current::getName, current::getSize, current::getCompressedSize);
-				String filename=current.getName();
+				String filename = current.getName();
 				if (isStreamingContents()) {
-					log.debug("storing stream to contents of zip entries under session key [{}]", ()->getContentsSessionKey());
+					log.debug("storing stream to contents of zip entries under session key [{}]", ZipIteratorPipe.this::getContentsSessionKey);
 					session.put(getContentsSessionKey(), StreamUtil.dontClose(source)); // do this each time, to allow reuse of the session key when an item is optionally encoded
 				} else {
-					log.debug("storing contents of zip entry under session key [{}]", ()->getContentsSessionKey());
+					log.debug("storing contents of zip entry under session key [{}]", ZipIteratorPipe.this::getContentsSessionKey);
 					String content = StreamUtil.streamToString(StreamUtil.dontClose(source), null, getCharset());
 					session.put(getContentsSessionKey(), content);
 				}
@@ -139,87 +226,5 @@ public class ZipIteratorPipe extends IteratingPipe<String> {
 				throw new SenderException(e);
 			}
 		}
-	}
-
-	protected ZipInputStream getZipInputStream(Message input, PipeLineSession session, Map<String,Object> threadContext) throws SenderException {
-		if (input==null) {
-			throw new SenderException("input is null. Must supply String (Filename, with processFile=true), File or InputStream as input");
-		}
-		InputStream source;
-		if (isProcessFile()) {
-			try {
-				String filename=null;
-				try {
-					filename = input.asString();
-				} catch (IOException e) {
-					throw new SenderException("cannot find filename ["+filename+"]", e);
-				}
-				source = new FileInputStream(filename);
-			} catch (FileNotFoundException e) {
-				throw new SenderException("could not find file ["+input+"]",e);
-			}
-		} else {
-			try {
-				source = input.asInputStream();
-			} catch (IOException e) {
-				throw new SenderException("cannot open stream", e);
-			}
-		}
-		if (!(source instanceof BufferedInputStream)) {
-			source=new BufferedInputStream(source);
-		}
-
-		return new ZipInputStream(source);
-	}
-
-	@Override
-	protected IDataIterator<String> getIterator(Message input, PipeLineSession session, Map<String,Object> threadContext) throws SenderException {
-		ZipInputStream source=getZipInputStream(input, session, threadContext);
-		if (source==null) {
-			throw new SenderException("no ZipInputStream found");
-		}
-		return new ZipStreamIterator(source,session);
-	}
-
-	/**
-	 * Session key used to store contents of each zip entry
-	 * @ff.default zipdata
-	 */
-	public void setContentsSessionKey(String string) {
-		contentsSessionKey = string;
-	}
-
-	/**
-	 * If set to <code>false</code>, a string containing the contents of the entry is placed under the session key, instead of the inputstream to the contents
-	 * @ff.default true
-	 */
-	public void setStreamingContents(boolean b) {
-		streamingContents = b;
-	}
-
-	/**
-	 * If set to <code>false</code>, the inputstream is not closed after it has been used
-	 * @ff.default true
-	 */
-	public void setCloseInputstreamOnExit(boolean b) {
-		closeInputstreamOnExit = b;
-	}
-
-	/**
-	 * Charset used when reading the contents of the entry (only used if streamingContents=false)
-	 * @ff.default utf-8
-	 */
-	public void setCharset(String string) {
-		charset = string;
-	}
-
-	/**
-	 * If set <code>true</code>, each entry is assumed to be the name of a file to be compressed. Otherwise, the input itself is compressed.
-	 * @ff.default false
-	 */
-	@Deprecated(forRemoval = true, since = "7.8.0")
-	@ConfigurationWarning("Please add a LocalFileSystemPipe with action=read in front of this pipe instead")
-	public void setProcessFile(boolean b) {
-		processFile = b;
 	}
 }
