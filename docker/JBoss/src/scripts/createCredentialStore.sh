dc3aa4eca5c2805b4b06c5cf8a522377c9e933b1
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-11-11 14:16:07 +0100

    update JBoss WildFly credential handling
    Refactor credential management in JBoss and WildFly Dockerfiles and scripts. 
    Replace the previous method of copying credential files with a new 
    approach that reads from a centralized credentials properties file. 
    Update the `createStore.sh` scripts to read credentials directly from 
    the specified file, ensuring a more streamlined and maintainable 
    configuration.

--- docker/JBoss/src/scripts/createCredentialStore.sh
+++ docker/JBoss/src/scripts/createCredentialStore.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# converts filesystem credentials into WildFly credentials
+# Converts credentials file into JBoss credentials
 
 SECRETS_TOOL=$JBOSS_HOME/bin/elytron-tool.sh
 SECRETS_LOCATION=$JBOSS_HOME/standalone/data/CS.jceks
 SECRETS_PASSWORD=secret
 
-SOURCE_PATH="/opt/frank/secrets"
+SECRETS_FILE="/opt/frank/secrets/credentials.properties"
 
 $SECRETS_TOOL credential-store --create --location "$SECRETS_LOCATION" --password "$SECRETS_PASSWORD"
 
 add_alias() {
 	name=$1
-	file=$SOURCE_PATH/$2
-	if [ -f "$file" ]
-	then
-		$SECRETS_TOOL credential-store --location "$SECRETS_LOCATION" --password "$SECRETS_PASSWORD" --add "$name" --secret "$(cat "$file")"
-	fi
+	secret=$2
+	$SECRETS_TOOL credential-store --location "$SECRETS_LOCATION" --password "$SECRETS_PASSWORD" --add "$name" --secret "$secret"
 }
 
-cd $SOURCE_PATH || return
-for alias in *
-do
-	if [ -d "$alias" ]
-	then
-		add_alias "$alias/username" "$alias/username"
-		add_alias "$alias"          "$alias/password"
-	else
-		add_alias "$alias" "$alias"
-	fi
-done
+echo "Adding credentials from $SECRETS_FILE"
+
+if [ -f "$SECRETS_FILE" ]; then
+	while read -r line; do
+		if [ -z "$line" ]; then
+			continue
+		fi
+		IFS='=' read -r alias_and_key secret <<< "$line"
+		IFS='/' read -r alias key <<< "$alias_and_key"
+		if [ "$key" == "password" ]; then
+			add_alias "$alias" "$secret"
+		else
+			add_alias "$alias/$key" "$secret"
+		fi
+	done < "$SECRETS_FILE"
+else
+	echo "No credentials file found"
+fi
