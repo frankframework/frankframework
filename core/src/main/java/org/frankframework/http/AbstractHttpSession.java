a1eea1a15dd338b1860d213468ded76a2c631a55
Thomas P <thomaspj2003@gmail.com>
2024-12-04 16:24:22 +0100

    Create a new AuthState for each request instead of re-using it

--- core/src/main/java/org/frankframework/http/AbstractHttpSession.java
+++ core/src/main/java/org/frankframework/http/AbstractHttpSession.java
@@ -404,7 +404,6 @@ public abstract class AbstractHttpSession implements ConfigurableLifecycle, HasK
 
 		sslSocketFactory = getSSLConnectionSocketFactory(); //Configure it here, so we can handle exceptions
 
-		preAuthenticate();
 		configureRedirectStrategy();
 
 		httpClientContext = defaultHttpClientContext; //Ensure a local instance is used when no SharedResource is present.
@@ -595,12 +594,17 @@ public abstract class AbstractHttpSession implements ConfigurableLifecycle, HasK
 		httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
 	}
 
-	private void preAuthenticate() {
+	/**
+	 * Adds a new authState instance to the clientContext when none exists. The authState is in {@literal CHALLENGED} state, which causes the request to always
+	 * require a challenge. A new authentication scheme (basic auth or oauth) is created for each request.
+	 * {@link OAuthAccessTokenManager} decides if and when to refresh the access token.
+	 */
+	private void preAuthenticate(HttpClientContext clientContext) {
 		if (credentials != null && !StringUtils.isEmpty(credentials.getUsername())) {
-			AuthState authState = defaultHttpClientContext.getTargetAuthState();
+			AuthState authState = clientContext.getTargetAuthState();
 			if (authState==null) {
 				authState = new AuthState();
-				defaultHttpClientContext.setAttribute(HttpClientContext.TARGET_AUTH_STATE, authState);
+				clientContext.setAttribute(HttpClientContext.TARGET_AUTH_STATE, authState);
 			}
 			authState.setState(AuthProtocolState.CHALLENGED);
 			authState.update(getPreferredAuthenticationScheme().createScheme(), getCredentials());
@@ -654,6 +658,7 @@ public abstract class AbstractHttpSession implements ConfigurableLifecycle, HasK
 
 		CloseableHttpClient client = getHttpClient();
 		HttpClientContext context = httpClientContext != null ? httpClientContext : getOrCreateHttpClientContext(client, session);
+		preAuthenticate(context);
 		log.trace("executing request using HttpClient [{}] and HttpContext [{}]", client::hashCode, () -> context);
 		return client.execute(targetHost, httpRequestBase, context);
 	}
