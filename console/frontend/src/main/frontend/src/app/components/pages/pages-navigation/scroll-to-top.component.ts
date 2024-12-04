4ae7b5a120fc587b9c43aca293ba96ca0f7e59d8
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-12-04 17:16:21 +0100

    Fix scroll-to-top button visibility and behavior (#8053)

--- console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/scroll-to-top.component.ts
+++ console/frontend/src/main/frontend/src/app/components/pages/pages-navigation/scroll-to-top.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-scroll-to-top',
   template:
-    '<div class="scroll-to-top"><a title="Scroll to top" (click)="scrollTop()"><i class="fa fa-arrow-up"></i> <span class="nav-label">Scroll To Top</span></a></div>',
+    '<div class="scroll-to-top hidden-scroll"><a title="Scroll to top" (click)="scrollTop()"><i class="fa fa-arrow-up"></i> <span class="nav-label">Scroll To Top</span></a></div>',
   standalone: true,
 })
 export class ScrollToTopComponent {
