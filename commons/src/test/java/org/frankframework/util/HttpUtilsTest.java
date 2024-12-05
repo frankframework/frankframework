769f564db8dc1dfd4ee51f6798133ef7c9f230d0
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-12-05 16:26:02 +0100

    fix: accedental deletion of URLEncode method

--- commons/src/test/java/org/frankframework/util/HttpUtilsTest.java
+++ commons/src/test/java/org/frankframework/util/HttpUtilsTest.java
@@ -9,6 +9,15 @@ import org.junit.jupiter.api.Test;
 public class HttpUtilsTest {
 
 	@Test
+	public void testUrlDecode() {
+		assertAll(
+				() -> assertEquals("/adapter/dummy adapter/receiver/receiver!", HttpUtils.urlDecode("/adapter/dummy%20adapter/receiver/receiver%21")),
+				() -> assertEquals("/adapter/dummy ædåpter", HttpUtils.urlDecode("/adapter/dummy%20ædåpter")),
+				() -> assertThrows(IllegalArgumentException.class, () -> HttpUtils.urlDecode("/adæapter/dummy%%20adapter")) //IllegalArgument
+		);
+	}
+
+	@Test
 	public void testDecodeBase64() {
 		assertAll(
 				() -> assertEquals("/adapter/dummy adapter/receiver/receiver!", HttpUtils.decodeBase64("L2FkYXB0ZXIvZHVtbXkgYWRhcHRlci9yZWNlaXZlci9yZWNlaXZlciE=")),
