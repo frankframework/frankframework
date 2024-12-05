54fa0615ef5cb1bb7a7d11d4b669a06d910d1803
Niels Meijer <nielsmeijer@hotmail.com>
2024-12-05 16:19:59 +0100

    Add test

--- security/src/main/java/org/frankframework/lifecycle/servlets/OAuth2Authenticator.java
+++ security/src/main/java/org/frankframework/lifecycle/servlets/OAuth2Authenticator.java
@@ -17,8 +17,9 @@ package org.frankframework.lifecycle.servlets;
 
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.List;
-import java.util.stream.Stream;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,6 +36,7 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.Getter;
 import lombok.Setter;
 
 import org.frankframework.util.ClassUtils;
@@ -94,6 +96,7 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 
 	private ClientRegistrationRepository clientRepository;
 	private String servletPath;
+	private @Getter String redirectUri;
 
 	private @Setter String roleMappingFile = "oauth-role-mapping.properties";
 	private URL roleMappingURL = null;
@@ -115,8 +118,11 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 	}
 
 	private void configure() throws FileNotFoundException {
-		if(StringUtils.isEmpty(clientId) || StringUtils.isEmpty(clientSecret)) {
+		if (StringUtils.isEmpty(clientId) || StringUtils.isEmpty(clientSecret)) {
 			throw new IllegalStateException("clientId and clientSecret must be set");
+		}
+		if (StringUtils.isEmpty(provider)) {
+			throw new IllegalStateException("A provider must be set");
 		}
 
 		roleMappingURL = ClassUtils.getResourceURL(roleMappingFile);
@@ -125,20 +131,20 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 		}
 		log.info("found rolemapping file [{}]", roleMappingURL);
 
-		servletPath = computeServletPath();
-		log.debug("using oauth servlet-path [{}]", servletPath);
+		String baseUrl = computeBaseUrl();
+		servletPath = (baseUrl == null) ? computeServletPath() : "";
+		redirectUri = computeRedirectUri(baseUrl);
+		log.debug("using oauth base-url [{}] servlet-path [{}] and redirect-uri [{}]", baseUrl, servletPath, redirectUri);
+
 		clientRepository = createClientRegistrationRepository();
 		SpringUtils.registerSingleton(getApplicationContext(), "clientRegistrationRepository", clientRepository);
 	}
 
 	public ClientRegistrationRepository createClientRegistrationRepository() {
-		Stream<String> providers = StringUtil.splitToStream(provider);
-		List<ClientRegistration> registrations = providers.map(this::getRegistration).toList();
-
-		return new InMemoryClientRegistrationRepository(registrations);
+		return new InMemoryClientRegistrationRepository(getRegistration(provider));
 	}
 
-	private ClientRegistration getRegistration(String provider) {
+	private ClientRegistration getRegistration(@Nonnull String provider) {
 		ClientRegistration.Builder builder = switch (provider.toLowerCase()) {
 			case "google", "github", "facebook", "okta" -> {
 				CommonOAuth2Provider commonProvider = EnumUtils.parse(CommonOAuth2Provider.class, provider);
@@ -149,9 +155,8 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 		};
 
 		builder.clientId(clientId).clientSecret(clientSecret);
-		String redirectUri = "%s/%s/oauth2/code/{registrationId}".formatted(computeBaseUrl(), servletPath);
-		log.debug("using oauth redirect-uri [{}]", redirectUri);
-		builder.redirectUri(redirectUri);
+
+		builder.redirectUri(getRedirectUri());
 
 		return builder.build();
 	}
@@ -173,32 +178,49 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 		return builder;
 	}
 
+
+	/**
+	 * Base-URL starts eg. `http(s)://{host}:{port}/`.
+	 */
+	@Nullable
 	private String computeBaseUrl() {
 		if (StringUtils.isEmpty(baseUrl)) {
-			return "{baseUrl}";
+			return null;
 		}
 
 		if(baseUrl.endsWith("/")) { // Ensure the url does not end with a slash
 			baseUrl = baseUrl.substring(0, baseUrl.length()-1);
 		}
 
+		log.debug("using baseUrl [{}]", baseUrl);
 		return baseUrl;
+	}
+
+	@Nonnull
+	private String computeRedirectUri(String baseUrl) {
+		if (baseUrl == null) {
+			String path = servletPath.startsWith("/") ? servletPath.substring(1) : servletPath;
+			return "{baseUrl}/%s/oauth2/code/{registrationId}".formatted(path);
+		}
+
+		return "%s/oauth2/code/{registrationId}".formatted(baseUrl);
 	}
 
 	/**
 	 * Servlet-Path that needs to be secured. May not end with a `*` or `/`.
 	 */
 	private String computeServletPath() {
-		String baseUrl = getPrivateEndpoints().stream().findFirst().orElse("");
+		String servletPath = getPrivateEndpoints().stream().findFirst().orElse("");
 
-		if(baseUrl.endsWith("*")) { // Strip the '*' if the url ends with it
-			baseUrl = baseUrl.substring(0, baseUrl.length()-1);
+		if(servletPath.endsWith("*")) { // Strip the '*' if the url ends with it
+			servletPath = servletPath.substring(0, servletPath.length()-1);
 		}
 
-		if(baseUrl.endsWith("/")) { // Ensure the url does not end with a slash
-			baseUrl = baseUrl.substring(0, baseUrl.length()-1);
+		if(servletPath.endsWith("/")) { // Ensure the url does not end with a slash
+			servletPath = servletPath.substring(0, servletPath.length()-1);
 		}
 
-		return baseUrl;
+		log.debug("using oauth servlet-path [{}]", servletPath);
+		return servletPath;
 	}
 }
