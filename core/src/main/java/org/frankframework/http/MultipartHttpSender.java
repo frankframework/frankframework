f1eaa07d87f7450e093a7c997f3005b3d435beb1
Tim van der Leeuw <tim@wearefrank.nl>
2024-12-11 10:30:13 +0100

    Remove dead code

--- core/src/main/java/org/frankframework/http/MultipartHttpSender.java
+++ core/src/main/java/org/frankframework/http/MultipartHttpSender.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019 Nationale-Nederlanden, 2021 WeAreFrank!
+   Copyright 2019 Nationale-Nederlanden, 2021, 2024 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,20 +15,6 @@
 */
 package org.frankframework.http;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.FormBodyPart;
-import org.apache.http.entity.mime.FormBodyPartBuilder;
-import org.apache.http.entity.mime.content.InputStreamBody;
-import org.apache.http.entity.mime.content.StringBody;
-import org.w3c.dom.Element;
-
-import org.frankframework.core.PipeLineSession;
-import org.frankframework.stream.Message;
-
 @Deprecated
 public class MultipartHttpSender extends HttpSender {
 
@@ -36,45 +22,5 @@ public class MultipartHttpSender extends HttpSender {
 		setPostType(HttpEntityType.FORMDATA);
 		setMethodType(HttpMethod.POST);
 		setFirstBodyPartName("message");
-	}
-
-	//@Override
-	protected FormBodyPart elementToFormBodyPart(Element element, PipeLineSession session) throws IOException {
-		String partType = element.getAttribute("type"); //File or otherwise
-		String partName = element.getAttribute("name"); //Name of the part
-		String fileName = StringUtils.isNotEmpty(element.getAttribute("filename")) ? element.getAttribute("filename") : element.getAttribute("fileName"); //Name of the file
-		String sessionKey = element.getAttribute("sessionKey"); //SessionKey to retrieve data from
-		String mimeType = element.getAttribute("mimeType"); //MimeType of the part
-		String partValue = element.getAttribute("value"); //Value when SessionKey is empty or not set
-		Message partObject = session.getMessage(sessionKey);
-
-		ContentType contentType = ContentType.create(mimeType, getCharSet());
-		if (partObject != null && partObject.isBinary()) {
-			InputStream fis = partObject.asInputStream();
-
-			if(StringUtils.isNotEmpty(fileName)) {
-				return createMultipartBodypart(partName, fis, fileName, contentType);
-			}
-			else if("file".equalsIgnoreCase(partType)) {
-				return createMultipartBodypart(sessionKey, fis, partName, contentType);
-			}
-			else {
-				return createMultipartBodypart(partName, fis, null, contentType);
-			}
-		} else {
-			String value = partObject.asString();
-			if(StringUtils.isEmpty(value))
-				value = partValue;
-
-			return FormBodyPartBuilder.create().setName(partName).setBody(new StringBody(value, contentType)).build();
-		}
-	}
-
-	private FormBodyPart createMultipartBodypart(String name, InputStream is, String fileName, ContentType contentType) {
-		log.debug("appending filepart [{}] with value [{}] fileName [{}] and contentType [{}]", name, is, fileName, contentType);
-		FormBodyPartBuilder bodyPart = FormBodyPartBuilder.create()
-			.setName(name)
-			.setBody(new InputStreamBody(is, contentType, fileName));
-		return bodyPart.build();
 	}
 }
