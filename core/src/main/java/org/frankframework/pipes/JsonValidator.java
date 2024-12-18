52d09fdd2293d434a7ac7102d7ef8ae263b50555
Niels Meijer <nielsmeijer@hotmail.com>
2024-12-18 16:57:38 +0100

    Update core/src/main/java/org/frankframework/pipes/JsonValidator.java

--- core/src/main/java/org/frankframework/pipes/JsonValidator.java
+++ core/src/main/java/org/frankframework/pipes/JsonValidator.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2022-204 WeAreFrank!
+   Copyright 2022-2024 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
