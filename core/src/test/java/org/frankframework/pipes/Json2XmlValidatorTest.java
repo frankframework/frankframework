b94aae89c07be94f3b98d0c33c4155f707c30bb2
Tim van der Leeuw <tim@wearefrank.nl>
2025-01-02 11:48:12 +0100

    Restore failing tests

--- core/src/test/java/org/frankframework/pipes/Json2XmlValidatorTest.java
+++ core/src/test/java/org/frankframework/pipes/Json2XmlValidatorTest.java
@@ -789,13 +789,13 @@ public class Json2XmlValidatorTest extends PipeTestBase<Json2XmlValidator> {
 	@ParameterizedTest(name = "With DeepSearch={0} Case={1}")
 	@DisplayName("Same Element-Name At Different Levels")
 	@CsvSource(value = {
-//			"true, ChildTypeFirstInXsdMissingInInput",
+			"true, ChildTypeFirstInXsdMissingInInput",
 			"false, ChildTypeFirstInXsdMissingInInput",
 			"true, ChildTypeFirstInXsdPresentInInput",
 			"false, ChildTypeFirstInXsdPresentInInput",
 			"true, ChildTypeLastInXsd",
 			"false, ChildTypeLastInXsd",
-//			"true, ParentNotRootChildMissing",
+			"true, ParentNotRootChildMissing",
 			"false, ParentNotRootChildMissing",
 			"true, WithIntermediateLevelChildMissing",
 			"false, WithIntermediateLevelChildMissing",
@@ -827,8 +827,9 @@ public class Json2XmlValidatorTest extends PipeTestBase<Json2XmlValidator> {
 
 		// Arrange
 		final String testCase;
-		testCase = "WithIntermediateLevelChildMissing";
-//		testCase = "ChildTypeFirstInXsdMissingInInput";
+//		testCase = "WithIntermediateLevelChildMissing"; // This now succeeds
+//		testCase = "ParentNotRootChildMissing"; // Still Fails
+		testCase = "ChildTypeFirstInXsdMissingInInput"; // Still Fails
 		pipe.setName("testSameNameDifferentLevelsDeepSearch=true");
 		pipe.setSchema("/Validation/Json2Xml/DeepSearch/" + testCase + "/Test.xsd");
 		pipe.setThrowException(true);
