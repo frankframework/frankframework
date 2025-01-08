089fe6754afe3170d7e41c254a7dde206fa671d8
Thomas P <thomaspj2003@gmail.com>
2025-01-08 14:42:21 +0100

    Ensure that sender has an a topic attribute or parameter

--- messaging/src/main/java/org/frankframework/extensions/mqtt/MqttSender.java
+++ messaging/src/main/java/org/frankframework/extensions/mqtt/MqttSender.java
@@ -52,6 +52,10 @@ public class MqttSender extends MqttFacade implements ISenderWithParameters {
 	public void configure() throws ConfigurationException {
 		if (paramList!=null) {
 			paramList.configure();
+
+			if (getTopic() == null && !paramList.hasParameter(TOPIC_PARAMETER_NAME)) {
+				throw new ConfigurationException("topic must be specified");
+			}
 		}
 
 		super.configure();
