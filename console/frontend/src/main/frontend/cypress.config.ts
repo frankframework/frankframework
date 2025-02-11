2313e6909970616d20515ba29d0c4b8e4f878899
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-02-11 13:25:37 +0100

    Fix package and add deps

--- console/frontend/src/main/frontend/cypress.config.ts
+++ console/frontend/src/main/frontend/cypress.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   e2e: {
     experimentalStudio: true,
     baseUrl: 'http://localhost:4200',
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
+    },
   },
 })
