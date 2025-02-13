393dcf83728a959a6cc01f61991ccf25078a5771
Niels Meijer <nielsmeijer@hotmail.com>
2025-02-13 16:58:44 +0100

    Fix assertion

--- core/src/test/java/org/frankframework/pipes/TestIteratingBasePipe.java
+++ core/src/test/java/org/frankframework/pipes/TestIteratingBasePipe.java
@@ -49,7 +49,8 @@ public class TestIteratingBasePipe extends IteratingPipeTestBase<IteratingTestPi
 
 	@Test
 	public void testParallelWithItemNoSessionKey() throws Exception {
-		pipe.setSender(new IndexAwareSlowRenderer());
+		StringBuffer results = new StringBuffer();
+		pipe.setSender(new IndexAwareSlowRenderer(results));
 		pipe.setParallel(true);
 		pipe.setMaxChildThreads(10);
 		pipe.setTaskExecutor(createTaskExecutor());
@@ -66,8 +67,9 @@ public class TestIteratingBasePipe extends IteratingPipeTestBase<IteratingTestPi
 		String expected = TestFileUtils.getTestFile("/IteratingPipe/HundredLines.xml");
 		assertEquals(expected, actual);
 
-		String resultLogTrimmed = resultLog.toString().trim();
+		String resultLogTrimmed = results.toString().trim();
 		String expectedLog = TestFileUtils.getTestFile("/IteratingPipe/HundredLinesLog.txt");
+		log.debug("async result:\n {}", resultLogTrimmed);
 		assertNotEquals(expectedLog, resultLogTrimmed); // Ensure it's not in chronological order
 		int occurance = StringUtils.countOccurrencesOf(resultLogTrimmed, "\n");
 		assertEquals(99, occurance);
@@ -80,9 +82,15 @@ public class TestIteratingBasePipe extends IteratingPipeTestBase<IteratingTestPi
 	}
 
 	private class IndexAwareSlowRenderer extends SlowRenderer {
+		private final StringBuffer buffer;
+		public IndexAwareSlowRenderer(StringBuffer buffer) {
+			this.buffer = buffer;
+		}
+
 		@Override
 		public SenderResult sendMessage(Message message, PipeLineSession session) throws SenderException {
-			SenderResult result = super.sendMessage(message, session);
+			sleep();
+
 			Integer index = session.getInteger("index");
 			if (index == null) {
 				fail("no index");
@@ -91,10 +99,24 @@ public class TestIteratingBasePipe extends IteratingPipeTestBase<IteratingTestPi
 				String msg = message.asString();
 				String number = msg.substring(0, msg.indexOf(" ="));
 				assertEquals(""+index, number);
+
+				String result = "["+msg+"]";
+				buffer.append(result + "\n"); // has to be in 1 concatenated string else newlines will be added asynchronously
+
+				return new SenderResult(result);
 			} catch (IOException e) {
 				throw new SenderException(e);
 			}
-			return result;
+		}
+
+		@SuppressWarnings("java:S2925")
+		private void sleep() {
+			try {
+				int random = (int) (Math.random() * 20);
+				Thread.sleep(random);
+			} catch (InterruptedException ignored) {
+				Thread.currentThread().interrupt();
+			}
 		}
 	}
 }
