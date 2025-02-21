0e0bee712b75de78ed79ee3407bbf4767d05184a
Tim van der Leeuw <tim@wearefrank.nl>
2025-02-21 13:08:56 +0100

    Run test only on CI, extra comments

--- core/src/test/java/org/frankframework/pipes/Json2XmlValidatorVeryLargeInputsTest.java
+++ core/src/test/java/org/frankframework/pipes/Json2XmlValidatorVeryLargeInputsTest.java
@@ -3,6 +3,7 @@ package org.frankframework.pipes;
 import static org.bson.assertions.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.io.InputStream;
@@ -10,6 +11,7 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -20,13 +22,26 @@ import org.frankframework.core.PipeRunResult;
 import org.frankframework.documentbuilder.DocumentFormat;
 import org.frankframework.stream.Message;
 import org.frankframework.testutil.LargeStructuredMockData;
+import org.frankframework.testutil.TestAssertions;
 
 
 @Tag("slow")
 public class Json2XmlValidatorVeryLargeInputsTest extends PipeTestBase<Json2XmlValidator> {
 
+	@BeforeAll
+	public static void beforeAll() {
+		// These tests collectively take a long time. When running the build locally, it can double
+		// the time to run unit tests in the module Core.
+		// So by default, the tests only run on the CI environment, and because of the tag "slow", only when running the "slow" tests in CI.
+		assumeTrue(TestAssertions.isTestRunningOnCI());
+	}
+
 	public static Stream<Arguments> testLargeInputArguments() {
 		return Stream.of(
+				// Parsing JSON is still very memory-intensive and thus slow. JSON output is also memory intensive but not as slow.
+				// So therefore tests parsing JSON have to be disabled for smaller values than tests parsing XML, and to a lesser extent the
+				// same goes for tests producing JSON output.
+				// Changing the JSON parser to be streaming will be a major undertaking and a major rewrite of the code.
 				arguments(DocumentFormat.XML, DocumentFormat.JSON, 1_000_000),
 				arguments(DocumentFormat.XML, DocumentFormat.XML, 1_000_000),
 				arguments(DocumentFormat.JSON, DocumentFormat.XML, 1_000_000),
