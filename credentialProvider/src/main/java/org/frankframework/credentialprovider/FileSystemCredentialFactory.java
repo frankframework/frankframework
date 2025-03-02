7184e1948ff9b694f7f2e4997e297a31cd5c1936
Niels Meijer <nielsmeijer@hotmail.com>
2025-03-02 14:11:32 +0100

    Update credentialProvider/src/main/java/org/frankframework/credentialprovider/FileSystemCredentialFactory.java

--- credentialProvider/src/main/java/org/frankframework/credentialprovider/FileSystemCredentialFactory.java
+++ credentialProvider/src/main/java/org/frankframework/credentialprovider/FileSystemCredentialFactory.java
@@ -28,7 +28,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.frankframework.credentialprovider.util.CredentialConstants;
 
 /**
- * It's required to set the property {@value FileSystemCredentialFactory#FILESYSTEM_ROOT_PROPERTY} to the directory you which to read credentials from.
+ * It's required to set the property {@value FileSystemCredentialFactory#FILESYSTEM_ROOT_PROPERTY} to the directory you wish to read credentials from.
  */
 public class FileSystemCredentialFactory implements ICredentialFactory {
 
