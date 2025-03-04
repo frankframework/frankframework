f29e0afd6289dc24a1a959f311329f9415302b0a
Erik <dongenevan@gmail.com>
2025-03-04 15:29:47 +0100

    Make health endpoints available for localhost without authorization (#8531)

--- console/backend/src/test/java/org/frankframework/console/configuration/SecurityChainTestConfiguration.java
+++ console/backend/src/test/java/org/frankframework/console/configuration/SecurityChainTestConfiguration.java
@@ -1,27 +1,13 @@
-/*
-   Copyright 2023-2025 WeAreFrank!
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
 package org.frankframework.console.configuration;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.MultipartConfigElement;
 
 import org.springframework.beans.BeansException;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import org.frankframework.console.ConsoleFrontend;
@@ -31,9 +17,20 @@ import org.frankframework.lifecycle.servlets.ServletConfiguration;
 import org.frankframework.security.config.ServletRegistration;
 import org.frankframework.util.SpringUtils;
 
-@Configuration
-public class RegisterServletEndpoints implements ApplicationContextAware {
+public class SecurityChainTestConfiguration implements ApplicationContextAware {
 	private ApplicationContext applicationContext;
+
+	/**
+	 * For MockMvc testing, we need to register the SpringSecurityFilterChain as a Filter in a Bean manually.
+	 * This is used by mockMvc configuration with `SecurityMockMvcConfigurers#springSecurity()`.
+	 */
+	@Bean
+	public FilterRegistrationBean<Filter> getFilterRegistrationBean() {
+		FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+		Filter filter = applicationContext.getBean("springSecurityFilterChain", Filter.class);
+		bean.setFilter(filter);
+		return bean;
+	}
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -41,11 +38,17 @@ public class RegisterServletEndpoints implements ApplicationContextAware {
 		SecuritySettings.setupDefaultSecuritySettings(applicationContext.getEnvironment());
 	}
 
+	/**
+	 * Needed to configure the backend endpoint for the IAF API to /* - we're missing the default /iaf/api/* mapping which is configured
+	 * in the RegisterServletEndpoints class on 'normal' startup.
+	 *
+	 * @see RegisterServletEndpoints
+	 */
 	@Bean
 	public ServletRegistration<DispatcherServlet> backendServletBean() {
 		ServletConfiguration servletConfiguration = SpringUtils.createBean(applicationContext, ServletConfiguration.class);
 		servletConfiguration.setName("IAF-API");
-		servletConfiguration.setUrlMapping("iaf/api/*");
+		servletConfiguration.setUrlMapping("/*");
 		servletConfiguration.setSecurityRoles(DynamicRegistration.ALL_IBIS_USER_ROLES);
 		servletConfiguration.setLoadOnStartup(1);
 		servletConfiguration.loadProperties();
