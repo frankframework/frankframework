4e3a984054037310a368669236ffbafff97d4d68
Niels Meijer <nielsmeijer@hotmail.com>
2025-03-13 15:59:50 +0100

    Don't change the thread name, and always revert it back.

--- core/src/main/java/org/frankframework/threading/ThreadNamingTaskDecorator.java
+++ core/src/main/java/org/frankframework/threading/ThreadNamingTaskDecorator.java
@@ -15,24 +15,21 @@
 */
 package org.frankframework.threading;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import jakarta.annotation.Nonnull;
 
 import org.springframework.core.task.TaskDecorator;
 
-import org.frankframework.util.ClassUtils;
-
 public class ThreadNamingTaskDecorator implements TaskDecorator {
-	private final AtomicLong counter = new AtomicLong();
 
+	/**
+	 * Reverts the thread-name back to the original.
+	 */
 	@Override
 	public @Nonnull Runnable decorate(@Nonnull Runnable runnable) {
 		return () -> {
 			final String threadName = Thread.currentThread().getName();
 			try {
 				// Give thread some name when we start a task on it
-				Thread.currentThread().setName(ClassUtils.nameOf(runnable) + "-FFWorker-" + counter.incrementAndGet());
 				runnable.run();
 			} finally {
 				// Restore the original name
