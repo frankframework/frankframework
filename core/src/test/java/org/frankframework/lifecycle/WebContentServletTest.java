efc758f91c7dbaf616cd5ef39fb1b7d7e9347066
evandongen <dongenevan@gmail.com>
2025-03-27 15:49:26 +0100

    Change WebContent to webcontent to make sure the naming is consistent

---
+++ core/src/test/java/org/frankframework/lifecycle/WebContentServletTest.java
@@ -0,0 +1,54 @@
+package org.frankframework.lifecycle;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import org.frankframework.configuration.classloaders.AbstractClassLoader;
+import org.frankframework.testutil.TestConfiguration;
+
+public class WebContentServletTest {
+
+	@Test
+	void testListDirectory() throws IOException {
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		WebContentServlet webContentServlet = new WebContentServlet();
+
+		TestConfiguration config = new TestConfiguration(TestConfiguration.TEST_CONFIGURATION_FILE);
+		config.setClassLoader(new AbstractClassLoader() {
+			@Override
+			public URL getLocalResource(String name) {
+				return this.getClass().getClassLoader().getResource(name);
+			}
+		});
+		webContentServlet.getWebContentForConfiguration(response, config);
+
+		String contentAsString = response.getContentAsString();
+
+		Assertions.assertTrue(contentAsString.contains("TestConfiguration"));
+	}
+
+	@Test
+	void testListDirectoryNotPresent() throws IOException {
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		WebContentServlet webContentServlet = new WebContentServlet();
+
+		// TODO get a configuration
+		TestConfiguration config = new TestConfiguration(TestConfiguration.TEST_CONFIGURATION_FILE);
+		config.setClassLoader(new AbstractClassLoader() {
+			@Override
+			public URL getLocalResource(String name) {
+				return this.getClass().getResource(name);
+			}
+		});
+
+		webContentServlet.getWebContentForConfiguration(response, config);
+
+		String contentAsString = response.getContentAsString();
+
+		Assertions.assertFalse(contentAsString.contains("TestConfiguration"));
+	}
+}
