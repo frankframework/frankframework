5daecd07760d63bb6aebd16fe3f91a8606ea703e
Tim van der Leeuw <tim@wearefrank.nl>
2025-04-03 22:57:39 +0200

    Improve output of LarvaTest runner for this first draft.

--- test/src/test/java/org/frankframework/runner/RunLarvaTests.java
+++ test/src/test/java/org/frankframework/runner/RunLarvaTests.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 
-import jakarta.annotation.Nullable;
+import jakarta.annotation.Nonnull;
 import jakarta.servlet.ServletContext;
 
 import org.junit.jupiter.api.AfterEach;
@@ -18,8 +18,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import lombok.extern.log4j.Log4j2;
-
 import org.frankframework.larva.LarvaLogLevel;
 import org.frankframework.larva.LarvaTool;
 import org.frankframework.stream.Message;
@@ -28,7 +26,6 @@ import org.frankframework.util.CloseUtils;
 
 @Tag("slow")
 @Tag("integration")
-@Log4j2
 public class RunLarvaTests {
 
 	private SpringApplication springApplication;
@@ -61,33 +58,39 @@ public class RunLarvaTests {
 
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setParameter("execute", scenarioRootDir);
-		request.setParameter("loglevel", LarvaLogLevel.SCENARIO_PASSED_FAILED.getName());
+		request.setParameter("loglevel", LarvaLogLevel.SCENARIO_FAILED.getName());
 
 		// Invoke Larva tests
 		MessageBuilder messageBuilder = new MessageBuilder();
 		Writer writer = messageBuilder.asWriter();
-		log.info("Starting Scenarios");
+		System.err.println("Starting Scenarios");
 		long start = System.currentTimeMillis();
 		int result = LarvaTool.runScenarios(servletContext, request, writer);
 		long end = System.currentTimeMillis();
-		log.info("Scenarios executed; duration: " + (end - start) + "ms");
+		System.err.println("Scenarios executed; duration: " + (end - start) + "ms");
 		writer.close();
 		larvaOutput = messageBuilder.build();
 
-		assertFalse(result < 0, ()-> "Error in LarvaTool execution, result is [" + result + "] instead of 0; output from LarvaTool:\n\n" + messageAsStringSafe(larvaOutput));
+		assertFalse(result < 0, ()-> "Error in LarvaTool execution, result is [" + result + "] instead of 0; output from LarvaTool:\n\n" + messageAsStringStripped(larvaOutput));
 
 		if (result > 0) {
-			log.warn("{} Larva tests failed; needs investigation but currently not yet failing the build on this. Larva output:\n\n{}", ()->result, ()->messageAsStringSafe(larvaOutput));
-			System.err.println(result + " Larva tests failed duration: " + (end - start)+ "ms; \n\n" + messageAsStringSafe(larvaOutput));
+			System.err.println(result + " Larva tests failed duration: " + (end - start)+ "ms; \n\n" + messageAsStringStripped(larvaOutput));
 		} else {
-			log.info("All Larva tests succeeded");
 			System.err.println("All Larva tests succeeded in " + (end - start) + "ms");
 		}
 	}
 
-	private @Nullable String messageAsStringSafe(Message message) {
+	private @Nonnull String messageAsStringStripped(Message message) {
 		try {
-			return message.asString();
+			String result = message.asString();
+			if (result == null) {
+				return "";
+			}
+			return result
+					.replaceAll("(?s)<form.*?</form>\n*", "")
+					.replaceAll("<div class='(odd|even)'></div>\n", "")
+					.replaceAll("</div>\n", "")
+					;
 		} catch (IOException e) {
 			return "Cannot read string from message: " + e.getMessage();
 		}
