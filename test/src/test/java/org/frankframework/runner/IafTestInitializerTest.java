ec91dcc544346529d9b43f9942e7f8ca13343f8e
evandongen <dongenevan@gmail.com>
2025-06-05 10:43:52 +0200

    Add test that checks if ladybug is working

--- test/src/test/java/org/frankframework/runner/IafTestInitializerTest.java
+++ test/src/test/java/org/frankframework/runner/IafTestInitializerTest.java
@@ -4,25 +4,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @Tag("slow")
 class IafTestInitializerTest {
 
+	private static ConfigurableApplicationContext applicationContext = null;
+
 	/**
 	 * Since we don't use @SpringBootApplication, we can't use @SpringBootTest here and need to manually configure the application
 	 */
-	@Test
-	void contextLoads() throws IOException {
+	@BeforeAll
+	static void setup() throws IOException {
 		SpringApplication springApplication = IafTestInitializer.configureApplication();
 
-		ConfigurableApplicationContext run = springApplication.run();
+		applicationContext = springApplication.run();
+	}
 
-		assertTrue(run.isRunning());
+	@AfterAll
+	static void tearDown() {
+		if (applicationContext != null) {
+			applicationContext.close();
+		}
+	}
 
-		run.close();
+	@Test
+	void contextLoads() {
+		assertTrue(applicationContext.isRunning());
+	}
+
+	@Test
+	void ladybugRuns() {
+		// Make sure to use the right context and port for the Tomcat server
+		TomcatServletWebServerFactory tomcat = applicationContext.getBean("tomcat", TomcatServletWebServerFactory.class);
+		String url = String.format("http://localhost:%d/%s/iaf/ladybug/api/testtool", tomcat.getPort(), tomcat.getContextPath());
+
+		// create a rest call to the ladybug endpoint at 'iaf/ladybug/api/testtool'  to check if it is running
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+		assertTrue(response.getStatusCode().is2xxSuccessful(), "Ladybug endpoint should return 2xx status");
 	}
 }
