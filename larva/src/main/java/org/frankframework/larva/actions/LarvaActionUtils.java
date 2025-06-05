a08b1226e3ff907c2b5c157cfe61d1815f11ff3b
Tim van der Leeuw <tim@wearefrank.nl>
2025-06-05 19:12:48 +0200

    Simpler regex for splitting map on '=' that doesn't have so many undesirable characteristics

--- larva/src/main/java/org/frankframework/larva/actions/LarvaActionUtils.java
+++ larva/src/main/java/org/frankframework/larva/actions/LarvaActionUtils.java
@@ -196,7 +196,7 @@ public class LarvaActionUtils {
 			Map<String, String> map = new LinkedHashMap<>();
 
 			for (String part : parts) {
-				String[] splitted = part.split("(\\s*?=\\s*)+", 2);
+				String[] splitted = part.split("\\s*=\\s*", 2);
 				if (splitted.length==2) {
 					map.put(splitted[0], splitted[1]);
 				} else {
