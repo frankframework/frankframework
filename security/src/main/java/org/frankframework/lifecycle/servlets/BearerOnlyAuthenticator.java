83c780811061b3ff7eea9cb2d1d1fd218df4128b
evandongen <dongenevan@gmail.com>
2025-06-12 14:28:46 +0200

    Add new authenticator for 'bearer only' authentication type

---
+++ security/src/main/java/org/frankframework/lifecycle/servlets/BearerOnlyAuthenticator.java
@@ -0,0 +1,66 @@
+/*
+   Copyright 2025 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package org.frankframework.lifecycle.servlets;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.Setter;
+
+/**
+ * Authenticator for bearer-only SSO authentication. This means that the application will not handle user login or logout, but expects the user to be
+ * authenticated by an external identity provider (IdP) using a JWT token. This has to be provided in the HTTP Authorization header as a Bearer token.
+ * <p>
+ * This authenticator should be configured by setting type to 'BEARER_ONLY' and have a issuerUri or jwkSetUri defined, for example:
+ * <pre>{@code
+ * application.security.console.authentication.type=BEARER_ONLY
+ * application.security.console.authentication.issuerUri=https://example.com/realms/myrealm
+ * }</pre>
+ * </p>
+ *
+ * @author evandongen
+ */
+public class BearerOnlyAuthenticator extends AbstractServletAuthenticator {
+
+	@Setter
+	private String issuerUri;
+
+	@Setter
+	private String jwkSetUri;
+
+	@Override
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		if (StringUtils.isAllEmpty(issuerUri, jwkSetUri)) {
+			throw new IllegalArgumentException("Either issuerUri or jwkSetUri must be set for BearerOnlyAuthenticator");
+		}
+
+		http.oauth2ResourceServer(oauth2 -> oauth2
+				.jwt(this::getJwtConfigurer));
+
+		return http.build();
+	}
+
+	private void getJwtConfigurer(OAuth2ResourceServerConfigurer.JwtConfigurer jwtConfigurer) {
+		if (jwkSetUri != null) {
+			jwtConfigurer.jwkSetUri(jwkSetUri);
+		} else if (issuerUri != null) {
+			jwtConfigurer.decoder(JwtDecoders.fromIssuerLocation(issuerUri));
+		}
+	}
+}
