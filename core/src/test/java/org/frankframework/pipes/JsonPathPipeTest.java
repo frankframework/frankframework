d52cb8f470be48d209ac9eef56c39f73e3d01f0b
Tim van der Leeuw <tim@wearefrank.nl>
2025-06-13 11:56:04 +0200

    Add more tests

--- core/src/test/java/org/frankframework/pipes/JsonPathPipeTest.java
+++ core/src/test/java/org/frankframework/pipes/JsonPathPipeTest.java
@@ -1,12 +1,21 @@
 package org.frankframework.pipes;
 
+import static org.frankframework.testutil.TestAssertions.assertJsonEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import org.frankframework.configuration.ConfigurationException;
 import org.frankframework.core.PipeForward;
 import org.frankframework.core.PipeLineSession;
 import org.frankframework.core.PipeRunResult;
@@ -32,16 +41,42 @@ class JsonPathPipeTest {
 		CloseUtils.closeSilently(session, result, input);
 	}
 
-	@Test
-	void doPipe() throws Exception {
+	static Stream<Arguments> doPipe() {
+		return Stream.of(
+				arguments(
+						"$.a", "Hello World",
+						"""
+								{"a": "Hello World"}
+								"""
+				),
+				arguments(
+						"$.*.a", "[1, 2]",
+						"""
+								{
+								"k1": {"a": 1},
+								"k2": {"a": 2},
+								}
+								"""
+				),
+				arguments("$.a", """
+						{"Hello": "World"}
+						""",
+						"""
+								{
+								  "a": {"Hello": "World"},
+								}
+								""")
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void doPipe(String jsonPath, String expected, Object messageData) throws Exception {
 		// Arrange
-		pipe.setJsonPathExpression("$.a");
+		pipe.setJsonPathExpression(jsonPath);
 		pipe.configure();
 
-		input = new Message("""
-		{"a": "Hello World"}
-		"""
-		);
+		input = Message.asMessage(messageData);
 
 		// Act
 		result = pipe.doPipe(input, session);
@@ -51,6 +86,19 @@ class JsonPathPipeTest {
 		assertNotNull(result.getResult());
 
 		String resultString = result.getResult().asString();
-		assertEquals("Hello World", resultString);
+		assertNotNull(resultString);
+		if (resultString.startsWith("[") || resultString.startsWith("{")) {
+			assertJsonEquals(expected, resultString);
+		} else {
+			assertEquals(expected, resultString);
+		}
+	}
+
+	@Test
+	void testConfigureFails() {
+
+		ConfigurationException e = assertThrows(ConfigurationException.class, pipe::configure);
+
+		assertEquals("jsonPathExpression has to be set", e.getMessage());
 	}
 }
