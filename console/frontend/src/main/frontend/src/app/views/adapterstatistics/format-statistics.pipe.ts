7bf76325dac3e1429be478a245e85c5fc9ea5840
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-07-30 16:52:18 +0200

    Convert per size / duration & percentile to percentage

--- console/frontend/src/main/frontend/src/app/views/adapterstatistics/format-statistics.pipe.ts
+++ console/frontend/src/main/frontend/src/app/views/adapterstatistics/format-statistics.pipe.ts
@@ -5,21 +5,34 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formatStatistics',
 })
 export class FormatStatisticsPipe implements PipeTransform {
-  transform(
-    input: Record<string, string | number | null>,
-    format: Record<string, unknown>,
-  ): KeyValue<string, string>[] {
+  transform(input: Record<string, string | number | null>, format: Record<string, string>): KeyValue<string, string>[] {
     const formatted: KeyValue<string, string>[] = [];
+
     for (const key in format) {
       let value = input[key];
-      if (!value && value !== 0) {
-        // if no value, return a dash
-        value = '-';
+
+      if (value || value === 0) {
+        const sum = input['sum'] ?? null;
+        const count = input['count'] ?? null;
+
+        if (typeof value === 'number') {
+          const formatValue = format[key];
+          if (key.startsWith('p') && typeof sum === 'number') value = this.convertToPercentage(value, sum);
+          else if (formatValue.startsWith('# < ') && typeof count === 'number')
+            value = this.convertToPercentage(value, count);
+        }
+        formatted.push({ key, value: value as string });
+        continue;
       }
-      formatted.push({ key, value: value as string });
+
+      formatted.push({ key, value: '-' }); // if no value, return a dash
     }
-    // Uncomment if really needed, but figure out how angular2+ handles this first
-    // formatted["$$hashKey"] = input["$$hashKey"]; //Copy the hashKey over so Angular doesn't trigger another digest cycle
     return formatted;
+  }
+
+  private convertToPercentage(value: number, total: number): string {
+    if (total === 0) return '0%';
+    const percentage = (value / total) * 100;
+    return `${percentage.toFixed(1)}%`;
   }
 }
