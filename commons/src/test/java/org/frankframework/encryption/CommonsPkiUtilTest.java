adaa305ac8a72e853e1fdf942bdaa3f41efc1253
matthijssmets <matthijs.smets@hotmail.com>
2025-08-13 13:38:33 +0200

    Update deps

--- commons/src/test/java/org/frankframework/encryption/CommonsPkiUtilTest.java
+++ commons/src/test/java/org/frankframework/encryption/CommonsPkiUtilTest.java
@@ -41,10 +41,7 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class CommonsPkiUtilTest {
 
 	private static final String DUMMY_PASSWORD = "dummyPassword";
