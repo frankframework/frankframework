dfe31e6646e3e11d8a9cfd4d205f1262a892e532
Niels Meijer <nielsmeijer@hotmail.com>
2025-08-16 15:14:10 +0200

    Backport of 4d445406e7ba3dd30c1ce766fc014e04d02e0cd5

--- core/src/main/java/org/frankframework/lifecycle/ConfiguringLifecycleProcessor.java
+++ core/src/main/java/org/frankframework/lifecycle/ConfiguringLifecycleProcessor.java
@@ -18,15 +18,25 @@ package org.frankframework.lifecycle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.CloseableThreadContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.Lifecycle;
 import org.springframework.context.support.DefaultLifecycleProcessor;
 
 import lombok.extern.log4j.Log4j2;
 
+import org.frankframework.configuration.Configuration;
 import org.frankframework.configuration.ConfigurationDigester;
 import org.frankframework.configuration.ConfigurationException;
+import org.frankframework.core.ManagableLifecycle;
+import org.frankframework.util.ClassUtils;
+import org.frankframework.util.Misc;
+import org.frankframework.util.StringUtil;
 
 /**
  * This class is a custom implementation of the Spring {@link DefaultLifecycleProcessor} that adds support for
@@ -37,6 +47,15 @@ import org.frankframework.configuration.ConfigurationException;
 @Log4j2
 public class ConfiguringLifecycleProcessor extends DefaultLifecycleProcessor implements ConfigurableLifecycle {
 
+	private final String className;
+	private final ApplicationContext applicationContext;
+	private final AtomicBoolean onlySmartAutostartAndNormalLifeLycleBeans = new AtomicBoolean();
+
+	public ConfiguringLifecycleProcessor(ApplicationContext context) {
+		applicationContext = context;
+		className = ClassUtils.classNameOf(context).toLowerCase();
+	}
+
 	/**
 	 * The {@link ConfigurationDigester} may add new Lifecycle beans.
 	 * Which is why it cannot be a {@link ConfigurableLifecycle} itself.
@@ -44,6 +63,103 @@ public class ConfiguringLifecycleProcessor extends DefaultLifecycleProcessor imp
 	@Override
 	public void configure() throws ConfigurationException {
 		log.trace("configuring all LifeCycle beans");
+		long startTime = System.currentTimeMillis();
+		try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.put(className, applicationContext.getId())) {
+			if (log.isDebugEnabled()) log.debug("configuring all ConfigurableLifecycle beans: {}", this::getConfigurableLifecycleBeanNames);
+			else log.info("configuring {}", () -> StringUtil.ucFirst(className));
+
+			doConfigure();
+			log.info("configured {} in {}", () -> StringUtil.ucFirst(className), () -> Misc.getDurationInMs(startTime));
+		}
+	}
+
+	// This triggers an internal startBeans method, and does not call #start().
+	// NOTE: the name/id has not been set yet (as this point), so it uses the hashcode instead. At the moment this is useful for the partent's context.
+	@Override
+	public void onRefresh() {
+		long startTime = System.currentTimeMillis();
+		try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.put(className, applicationContext.getId())) {
+			if (log.isDebugEnabled()) log.debug("refresh (start) all 'autostart' LifeCycle beans: {}", this::getConfigurableLifecycleBeanNames);
+			else log.info("refreshing {}", () -> StringUtil.ucFirst(className));
+
+			super.onRefresh();
+			log.info("refreshed {} in {}", () -> StringUtil.ucFirst(className), () -> Misc.getDurationInMs(startTime));
+		}
+	}
+
+	/**
+	 * Auto-start triggered by {@link Configuration} after {@link #configure()} has been called,
+	 * which starts all newly (non-started) registered beans.
+	 * 
+	 * Only (Smart-)LifeCycle beans or beans with {@link ManagableLifecycle#isAutoStartup()}
+	 * will be started.
+	 */
+	@Override
+	public void start() {
+		long startTime = System.currentTimeMillis();
+		try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.put(className, applicationContext.getId())) {
+			if (log.isDebugEnabled()) log.debug("starting all LifeCycle beans: {}", this::getConfigurableLifecycleBeanNames);
+			else log.info("starting {}", () -> StringUtil.ucFirst(className));
+
+			try {
+				onlySmartAutostartAndNormalLifeLycleBeans.set(true);
+				super.start();
+			} finally {
+				onlySmartAutostartAndNormalLifeLycleBeans.setRelease(false);
+			}
+			log.info("started {} in {}", () -> StringUtil.ucFirst(className), () -> Misc.getDurationInMs(startTime));
+		}
+	}
+
+	@Override
+	public void stop() {
+		long startTime = System.currentTimeMillis();
+		try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.put(className, applicationContext.getId())) {
+			if (log.isDebugEnabled()) log.debug("stopping all LifeCycle beans: {}", this::getConfigurableLifecycleBeanNames);
+			else log.info("stopping {}", () -> StringUtil.ucFirst(className));
+
+			super.stop();
+			log.info("stopped {} in {}", () -> StringUtil.ucFirst(className), () -> Misc.getDurationInMs(startTime));
+		}
+	}
+
+	/**
+	 * Get a list of all bean names that implement ConfigurableLifecycle.
+	 */
+	private List<String> getConfigurableLifecycleBeanNames() {
+		return getLifecycleBeans()
+				.values()
+				.stream()
+				.filter(ConfigurableLifecycle.class::isInstance)
+				.map(ClassUtils::nameOf)
+				.toList();
+	}
+
+	/**
+	 * Override so we can filter on {@link ManagableLifecycle} beans.
+	 */
+	@Override
+	protected Map<String, Lifecycle> getLifecycleBeans() {
+		if (onlySmartAutostartAndNormalLifeLycleBeans.get()) {
+			return super.getLifecycleBeans()
+					.entrySet()
+					.stream()
+					.filter(this::filterSmartLifecycles)
+					.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+		}
+
+		return super.getLifecycleBeans();
+	}
+
+	/**
+	 * If it's not a ManagableLifecycle we can simply start the bean.
+	 * Only start ManagableLifecycle bean when autoStart is enabled.
+	 */
+	private boolean filterSmartLifecycles(Map.Entry<String, Lifecycle> entry) {
+		return (!(entry.getValue() instanceof ManagableLifecycle managable) || managable.isAutoStartup());
+	}
+
+	private void doConfigure() throws ConfigurationException {
 		Map<String, Lifecycle> lifecycleBeans = getLifecycleBeans();
 		Map<Integer, LifecycleGroup> phases = new TreeMap<>();
 
@@ -64,19 +180,6 @@ public class ConfiguringLifecycleProcessor extends DefaultLifecycleProcessor imp
 				throw e;
 			}
 		}
-	}
-
-	// This triggers an internal startBeans method, and does not call #start().
-	@Override
-	public void onRefresh() {
-		log.trace("refresh, starting all LifeCycle beans");
-		super.onRefresh();
-	}
-
-	@Override
-	public void start() {
-		log.trace("starting all LifeCycle beans");
-		super.start();
 	}
 
 	/**
