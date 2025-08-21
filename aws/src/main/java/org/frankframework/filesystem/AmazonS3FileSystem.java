4e081d830d3163316eab0efd261b8899cc92aedc
matthijssmets <matthijs.smets@hotmail.com>
2025-08-21 17:15:08 +0200

    Add full branch analysis

--- aws/src/main/java/org/frankframework/filesystem/AmazonS3FileSystem.java
+++ aws/src/main/java/org/frankframework/filesystem/AmazonS3FileSystem.java
@@ -86,6 +86,7 @@ public class AmazonS3FileSystem extends AbstractFileSystem<S3FileRef> implements
 
 	private static final String FILE_DELIMITER = "/";
 
+	// l
 	private @Getter String accessKey;
 	private @Getter String secretKey;
 	private @Getter String authAlias;
