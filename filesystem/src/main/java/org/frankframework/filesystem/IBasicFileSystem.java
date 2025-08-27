1026d5eb8b414ddda2a8885ded25f2760c1c7cfa
matthijssmets <matthijs.smets@hotmail.com>
2025-08-27 16:06:48 +0200

    Add comment about temporary default implementation in IBasicFileSystem

--- filesystem/src/main/java/org/frankframework/filesystem/IBasicFileSystem.java
+++ filesystem/src/main/java/org/frankframework/filesystem/IBasicFileSystem.java
@@ -52,7 +52,8 @@ public interface IBasicFileSystem<F> extends HasPhysicalDestination, AutoCloseab
 
 	/**
 	 * Lists files, directories or both, from a 'folder' or in the 'root' of the filesystem (when folder is null).
-	 * Only lists the objects as defined by the type filter.
+	 * Only lists the objects as defined by the type filter. <br />
+	 * Temporary default method until all implementations use {@link #list(F, TypeFilter)} See <a href="https://github.com/frankframework/frankframework/issues/7193">Issue 7193</a>
 	 */
 	default DirectoryStream<F> list(String folder, TypeFilter filter) throws FileSystemException {
 		F actualFolder = toFile(folder);
