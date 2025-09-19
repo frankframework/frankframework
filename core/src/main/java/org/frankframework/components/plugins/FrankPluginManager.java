7269acb93e25d060d395f8b1de265a4042c44aae
Niels Meijer <nielsmeijer@hotmail.com>
2025-09-19 15:10:31 +0200

    Add a test plugin and see if we can load it

--- core/src/main/java/org/frankframework/components/plugins/FrankPluginManager.java
+++ core/src/main/java/org/frankframework/components/plugins/FrankPluginManager.java
@@ -18,29 +18,51 @@ package org.frankframework.components.plugins;
 import java.nio.file.Path;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
+import org.pf4j.CompoundPluginRepository;
 import org.pf4j.DefaultPluginManager;
+import org.pf4j.DefaultPluginRepository;
+import org.pf4j.JarPluginRepository;
+import org.pf4j.PluginDescriptor;
 import org.pf4j.PluginDescriptorFinder;
+import org.pf4j.PluginRepository;
 import org.pf4j.PluginWrapper;
 import org.pf4j.RuntimeMode;
 
 import org.frankframework.util.AppConstants;
+import org.frankframework.util.LogUtil;
 
 public class FrankPluginManager extends DefaultPluginManager {
+	private static final Logger APPLICATION_LOG = LogUtil.getLogger("APPLICATION");
 
 	public FrankPluginManager(Path pluginDirectory) {
 		super(pluginDirectory);
 
 		String applVersion = AppConstants.getInstance().getProperty("application.version", null);
-		if (StringUtils.isNotBlank(applVersion)) {
+		if (StringUtils.isNotBlank(applVersion) && !isTestOrSnapshotVersion(applVersion)) {
 			setSystemVersion(applVersion);
 		}
 	}
 
+	/**
+	 * Don't set the version, or rather keep it set to '0.0.0' when we're developing.
+	 */
+	private boolean isTestOrSnapshotVersion(String applVersion) {
+		return "LOCKED_FF_CORE_VERSION".equals(applVersion) || applVersion.endsWith("-SNAPSHOT");
+	}
+
+	@Override
+	protected PluginRepository createPluginRepository() {
+		return new CompoundPluginRepository()
+				.add(new JarPluginRepository(getPluginsRoots()))
+				.add(new DefaultPluginRepository(getPluginsRoots()));
+	}
+
 	@Override
 	public RuntimeMode getRuntimeMode() {
-		return RuntimeMode.DEPLOYMENT;
+		return "0.0.0".equals(getSystemVersion()) ? RuntimeMode.DEVELOPMENT : RuntimeMode.DEPLOYMENT;
 	}
 
 	@Override
@@ -50,7 +72,7 @@ public class FrankPluginManager extends DefaultPluginManager {
 
 	@Override
 	protected boolean isPluginValid(PluginWrapper pluginWrapper) {
-		if ("0.0.0".equals(getSystemVersion())) {
+		if (isDevelopment()) {
 			return true;
 		}
 
@@ -60,5 +82,15 @@ public class FrankPluginManager extends DefaultPluginManager {
 		}
 
 		return super.isPluginValid(pluginWrapper);
+	}
+
+	@Override
+	protected PluginWrapper loadPluginFromPath(Path pluginPath) {
+		PluginWrapper plugin = super.loadPluginFromPath(pluginPath);
+
+		PluginDescriptor descriptor = plugin.getDescriptor();
+		APPLICATION_LOG.info("Loading Plugin [{}] version [{}] developed by [{}]", descriptor::getPluginId, descriptor::getVersion, descriptor::getProvider);
+
+		return plugin;
 	}
 }
--- core/src/main/java/org/frankframework/components/plugins/PluginLoader.java
+++ core/src/main/java/org/frankframework/components/plugins/PluginLoader.java
@@ -18,13 +18,20 @@ package org.frankframework.components.plugins;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import jakarta.annotation.Nullable;
+
 import org.apache.commons.lang3.StringUtils;
 import org.pf4j.PluginManager;
+import org.pf4j.PluginWrapper;
 import org.springframework.beans.factory.InitializingBean;
+
+import lombok.extern.log4j.Log4j2;
 
 import org.frankframework.util.AppConstants;
 
+@Log4j2
 public class PluginLoader implements InitializingBean {
+
 	private Path directory;
 	private PluginManager pluginManager;
 
@@ -41,13 +48,26 @@ public class PluginLoader implements InitializingBean {
 		}
 	}
 
+	@Nullable
+	public PluginWrapper findPlugin(String pluginName) {
+		if (pluginManager == null) {
+			return null;
+		}
+
+		return pluginManager.getPlugin(pluginName);
+	}
+
 	@Override
 	public void afterPropertiesSet() {
 		if (directory == null) {
+			log.info("disabled plugin capability");
 			return;
 		}
+		log.info("enabled plugin capability using directory [{}]", directory);
 
 		pluginManager = new FrankPluginManager(directory);
 		pluginManager.loadPlugins();
+
+		log.info("loaded [{}] plugin(s) / component(s)", pluginManager.getPlugins().size());
 	}
 }
