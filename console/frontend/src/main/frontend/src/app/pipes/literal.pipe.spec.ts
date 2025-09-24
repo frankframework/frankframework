50cb11eac2df516c96c531de4ac08bcc1faf90d9
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-09-24 15:20:10 +0200

    Add literal pipe to escape most whitespace characters

---
+++ console/frontend/src/main/frontend/src/app/pipes/literal.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LiteralPipe } from './literal.pipe';
+
+describe('LiteralPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LiteralPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
