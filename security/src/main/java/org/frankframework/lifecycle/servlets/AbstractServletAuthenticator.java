9c5e790ba45a2ddfa8720c79b2debdcc9e8047bf
Tim van der Leeuw <tim@wearefrank.nl>
2025-11-25 20:30:56 +0100

    Add an option to allow passing X-Forwarded headers to the application

--- security/src/main/java/org/frankframework/lifecycle/servlets/AbstractServletAuthenticator.java
+++ security/src/main/java/org/frankframework/lifecycle/servlets/AbstractServletAuthenticator.java
@@ -50,13 +50,13 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import lombok.Getter;
 
 public abstract class AbstractServletAuthenticator implements IAuthenticator, ApplicationContextAware {
 	public static final List<String> DEFAULT_IBIS_ROLES = List.of("IbisWebService", "IbisObserver", "IbisDataAdmin", "IbisAdmin", "IbisTester");
 	public static final String ALLOW_OPTIONS_REQUESTS_KEY = "application.security.http.allowUnsecureOptionsRequests";
+	public static final String ALLOW_FORWARDED_HEADERS_PASSTHROUGH_REQUESTS_KEY = "application.security.http.allowForwardedHeadersPassthrough";
 
 	private static final String HTTP_SECURITY_BEAN_NAME = "org.springframework.security.config.annotation.web.configuration.HttpSecurityConfiguration.httpSecurity";
 	protected static final String DEFAULT_ROLE_PREFIX = "ROLE_";
@@ -68,12 +68,14 @@ public abstract class AbstractServletAuthenticator implements IAuthenticator, Ap
 	private @Getter ApplicationContext applicationContext;
 	private Properties applicationConstants = null;
 	private boolean allowUnsecureOptionsRequest = false;
+	private boolean allowForwardedHeadersPassthrough = false;
 
 	@Override
 	public final void setApplicationContext(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
 		Environment env = applicationContext.getEnvironment();
 		allowUnsecureOptionsRequest = env.getProperty(ALLOW_OPTIONS_REQUESTS_KEY, boolean.class, false);
+		allowForwardedHeadersPassthrough = env.getProperty(ALLOW_FORWARDED_HEADERS_PASSTHROUGH_REQUESTS_KEY, boolean.class, false);
 	}
 
 	protected final synchronized Properties getEnvironmentProperties() {
@@ -205,8 +207,8 @@ public abstract class AbstractServletAuthenticator implements IAuthenticator, Ap
 		RequestMatcher authorizationRequestMatcher = new AndRequestMatcher(securityRequestMatcher, this::authorizationRequestMatcher);
 		http.authorizeHttpRequests(requests -> requests.requestMatchers(authorizationRequestMatcher).access(getAuthorizationManager()));
 
-		// This filter converts x-forwarded headers to their corresponding `normal` headers. Eg. `X-Forwarded-Proto` sets HttpServletRequest.isSecure to `true`.
-		http.addFilterBefore(new ForwardedHeaderFilter(), SecurityContextHolderFilter.class);
+		// This filter removes x-forwarded headers and converts them to their corresponding `normal` headers. Eg. `X-Forwarded-Proto` sets HttpServletRequest.isSecure to `true`.
+		http.addFilterBefore(new CustomizedForwardedHeaderFilter(allowForwardedHeadersPassthrough), SecurityContextHolderFilter.class);
 		return configure(http);
 	}
 
