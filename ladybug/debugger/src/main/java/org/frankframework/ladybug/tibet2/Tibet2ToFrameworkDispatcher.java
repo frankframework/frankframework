097a059fab3bedf8a6028984251271d123899f79
Niels Meijer <nielsmeijer@hotmail.com>
2026-01-18 14:40:14 +0100

    Add tests

--- ladybug/debugger/src/main/java/org/frankframework/ladybug/tibet2/Tibet2ToFrameworkDispatcher.java
+++ ladybug/debugger/src/main/java/org/frankframework/ladybug/tibet2/Tibet2ToFrameworkDispatcher.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2026 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.frankframework.ladybug.tibet2;
 
 import java.io.IOException;
@@ -31,7 +46,11 @@ public class Tibet2ToFrameworkDispatcher {
 	private final OutboundGateway gateway;
 
 	public Tibet2ToFrameworkDispatcher(ApplicationContext context) {
-		this.gateway = context.getBean("outboundGateway", OutboundGateway.class);
+		this(context.getBean("outboundGateway", OutboundGateway.class));
+	}
+
+	public Tibet2ToFrameworkDispatcher(OutboundGateway gateway) {
+		this.gateway = gateway;
 	}
 
 	public String authorisationCheck(String storageId, String viewName) {
