c49f367538536a0c9ad16545c6ce5062959aaaf8
Niels Meijer <nielsmeijer@hotmail.com>
2026-01-20 14:05:29 +0100

    Fix the Tibet2 ladybug circular dependency problem (#10285)

--- ladybug/debugger/src/main/java/org/frankframework/ladybug/tibet2/TibetView.java
+++ ladybug/debugger/src/main/java/org/frankframework/ladybug/tibet2/TibetView.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Nationale-Nederlanden, 2021-2024 WeAreFrank!
+   Copyright 2018 Nationale-Nederlanden, 2021-2026 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,71 +15,27 @@
 */
 package org.frankframework.ladybug.tibet2;
 
-import org.wearefrank.ladybug.echo2.BeanParent;
-import org.wearefrank.ladybug.echo2.Echo2Application;
-import org.wearefrank.ladybug.echo2.reports.ReportsComponent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.wearefrank.ladybug.filter.View;
 
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
+public class TibetView extends View implements ApplicationContextAware {
 
-import org.frankframework.configuration.Configuration;
-import org.frankframework.configuration.ConfigurationException;
-import org.frankframework.core.Adapter;
-import org.frankframework.core.PipeLineResult;
-import org.frankframework.core.PipeLineSession;
-import org.frankframework.ladybug.LadybugDebugger;
-import org.frankframework.stream.Message;
-import org.frankframework.util.MessageUtils;
+	private ApplicationContext applicationContext;
 
-@Log4j2
-public class TibetView extends View {
-	private static final String AUTHORISATION_CHECK_ADAPTER_NAME = "AuthorisationCheck";
-	private static final String AUTHORISATION_CHECK_ADAPTER_CONFIG = "main";
-	protected @Setter LadybugDebugger ibisDebugger;
-
-	/**
-	 * @see org.wearefrank.ladybug.echo2.Echo2Application#initBean()
-	 */
 	@Override
-	public void initBean(BeanParent beanParent) {
-		super.initBean(beanParent);
-		Tibet2DatabaseStorage storage = (Tibet2DatabaseStorage) getDebugStorage();
-		try {
-			storage.configure();
-		} catch (ConfigurationException e) {
-			log.fatal("Could not configure TibetView storage", e);
-		}
-		storage.setSecurityContext(getEcho2Application());
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
 	}
 
+	// storageId is an Integer...
 	@Override
 	public String isOpenReportAllowed(Object storageId) {
-		return isOpenReportAllowedViaAdapter(storageId);
+		return isOpenReportAllowedViaAdapter("" + storageId);
 	}
 
-	public String isOpenReportAllowedViaAdapter(Object storageId) {
-		Echo2Application app = getEcho2Application();
-		Configuration config = ibisDebugger.getIbisManager().getConfiguration(AUTHORISATION_CHECK_ADAPTER_CONFIG);
-		if(config == null) {
-			return "Not allowed. Could not find config " + AUTHORISATION_CHECK_ADAPTER_CONFIG;
-		}
-		Adapter adapter = config.getRegisteredAdapter(AUTHORISATION_CHECK_ADAPTER_NAME);
-		if(adapter == null) {
-			return "Not allowed. Could not find adapter " + AUTHORISATION_CHECK_ADAPTER_NAME;
-		}
-
-		try (PipeLineSession pipeLineSession = new PipeLineSession()) {
-			if (app.getUserPrincipal() != null) {
-				pipeLineSession.put("principal", app.getUserPrincipal().getName());
-			}
-			pipeLineSession.put("StorageId", storageId);
-			pipeLineSession.put("View", getName());
-			PipeLineResult processResult = adapter.processMessageDirect(MessageUtils.generateMessageId("tibet-message"), new Message("<dummy/>"), pipeLineSession);
-			if (processResult.isSuccessful()) {
-				return ReportsComponent.OPEN_REPORT_ALLOWED;
-			}
-			return "Not allowed. Result of adapter " + AUTHORISATION_CHECK_ADAPTER_NAME + ": " + processResult.getResult();
-		}
+	// Protected for tests
+	protected String isOpenReportAllowedViaAdapter(String storageId) {
+		return new Tibet2ToFrameworkDispatcher(applicationContext).authorisationCheck(storageId, getName());
 	}
 }
