b237a807fab1048bf8b1c66dd71e6555558e1c23
Niels Meijer <nielsmeijer@hotmail.com>
2026-01-22 15:06:55 +0100

    Fix NPE when Ladybug advice files cannot be found
    (cherry picked from commit 1f7017234214ff9fe288ef7986fb90e1158e1969)

--- ladybug/debugger/src/main/java/org/frankframework/ladybug/runner/LadybugDebugModule.java
+++ ladybug/debugger/src/main/java/org/frankframework/ladybug/runner/LadybugDebugModule.java
@@ -15,22 +15,53 @@
 */
 package org.frankframework.ladybug.runner;
 
+import java.net.URL;
 import java.util.List;
 
-import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
+import org.jspecify.annotations.Nullable;
 
 import org.frankframework.components.Module;
 import org.frankframework.util.AppConstants;
+import org.frankframework.util.LogUtil;
 
-@Log4j2
 public class LadybugDebugModule implements Module {
+	private static final String DEFAULT_ADVICE_FILE = "springIbisDebuggerAdvice.xml";
+	private static final Logger APPLICATION_LOG = LogUtil.getLogger("APPLICATION");
 
 	@Override
 	public List<String> getSpringConfigurationFiles() {
 		AppConstants properties = AppConstants.getInstance();
-		String customXmlfile = properties.getProperty("ibistesttool.custom", "");
-		String debuggerAdvice = "springIbisDebuggerAdvice%s.xml".formatted(customXmlfile);
-		log.debug("using Ladybug debugger file [{}]", debuggerAdvice);
-		return List.of(debuggerAdvice);
+		String customization = properties.getProperty("ibistesttool.custom", "");
+		if (StringUtils.isNotBlank(customization)) {
+			APPLICATION_LOG.info("Found ladybug customization [{}]", customization);
+
+			String debuggerAdvice = "springIbisDebuggerAdvice%s.xml".formatted(customization);
+
+			String file = findFile(debuggerAdvice);
+			if (file != null) {
+				return List.of(file);
+			}
+
+			APPLICATION_LOG.info("Unable to locate Ladybug debugger customization file [{}] fallback to default file [{}]", debuggerAdvice, DEFAULT_ADVICE_FILE);
+		}
+
+		String defaultAdviceFile = findFile(DEFAULT_ADVICE_FILE);
+		if (StringUtils.isBlank(defaultAdviceFile)) {
+			throw new IllegalStateException("unable to start Ladybug, configFile ["+DEFAULT_ADVICE_FILE+"] not found");
+		}
+		return List.of(DEFAULT_ADVICE_FILE);
+	}
+
+	@Nullable
+	static String findFile(String file) {
+		URL fileURL = LadybugDebugModule.class.getClassLoader().getResource(file);
+		if(fileURL == null) {
+			return null;
+		}
+
+		APPLICATION_LOG.info("Loading TestTool configuration [{}]", file);
+		return file;
 	}
 }
