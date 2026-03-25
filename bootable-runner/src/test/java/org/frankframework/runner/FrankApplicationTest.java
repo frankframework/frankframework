46dc2097bd69456dd072b2b62f26084d3e8079c9
Niels Meijer <nielsmeijer@hotmail.com>
2026-03-25 15:00:38 +0100

    Oops?

--- bootable-runner/src/test/java/org/frankframework/runner/FrankApplicationTest.java
+++ bootable-runner/src/test/java/org/frankframework/runner/FrankApplicationTest.java
@@ -6,13 +6,8 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-@Tag("slow")
 class FrankApplicationTest {
 
 	@SuppressWarnings({ "NullAway.Init", "java:S2637" })
@@ -22,8 +17,6 @@ class FrankApplicationTest {
 	 * Since we don't use @SpringBootApplication, we can't use @SpringBootTest here and need to manually configure the application
 	 */
 	@BeforeAll
-//	Enable this once JUNIT 6.1.0 has been released
-//	@SetSystemProperty(key = "configurations.names", value = "") // Don't load configurations to speed things up.
 	static void setup() throws IOException {
 		frankApplication = new FrankApplication();
 		frankApplication.run();
