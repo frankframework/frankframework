c6fbc975baa992efc0fd4a60fcc1eb28e73e7ea4
Niels Meijer <nielsmeijer@hotmail.com>
2026-04-07 13:00:02 +0200

    Remove pre

--- core/src/test/java/org/frankframework/testutil/SpringRootInitializer.java
+++ core/src/test/java/org/frankframework/testutil/SpringRootInitializer.java
@@ -7,13 +7,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 /**
  * A minimal Spring ApplicationContext which contains no beans and no configuration.
  * Enables the use of
- * <pre>
  * @SpringJUnitConfig(initializers = {SpringRootInitializer.class})
- * </pre>
  * And
- * <pre>
  * @WithMockUser(...)
- * </pre>
  *
  */
 public class SpringRootInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
