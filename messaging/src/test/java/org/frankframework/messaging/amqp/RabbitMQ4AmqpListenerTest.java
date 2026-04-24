7a5acd571d729f173784ee5762e44078f33e1044
Matthijs Smets <93487259+MatthijsSmets@users.noreply.github.com>
2026-04-24 15:46:04 +0200

    Downgrade rabbitmq test container image to 4.2 (#10739)
    (cherry picked from commit 0752bf3b6276be1afb578c504912459b35783195)

--- messaging/src/test/java/org/frankframework/messaging/amqp/RabbitMQ4AmqpListenerTest.java
+++ messaging/src/test/java/org/frankframework/messaging/amqp/RabbitMQ4AmqpListenerTest.java
@@ -12,7 +12,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 class RabbitMQ4AmqpListenerTest extends AmqpListenerTest {
 
-	private static final String RABBIT_MQ_DOCKER_TAG = "rabbitmq:4-alpine";
+	private static final String RABBIT_MQ_DOCKER_TAG = "rabbitmq:4.2-alpine";
 
 	@Container
 	private static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer(RABBIT_MQ_DOCKER_TAG)
