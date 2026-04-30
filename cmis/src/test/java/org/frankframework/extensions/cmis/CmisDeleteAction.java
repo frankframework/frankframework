9cc9c499dec62e748df5514735a17588430dd357
Niels Meijer <nielsmeijer@hotmail.com>
2026-04-30 10:08:26 +0200

    Remove CMIS ResultNotFound forward (#10763)

--- cmis/src/test/java/org/frankframework/extensions/cmis/CmisDeleteAction.java
+++ cmis/src/test/java/org/frankframework/extensions/cmis/CmisDeleteAction.java
@@ -3,18 +3,20 @@ package org.frankframework.extensions.cmis;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import org.frankframework.core.SenderException;
+import org.frankframework.core.SenderResult;
 import org.frankframework.extensions.cmis.CmisSessionBuilder.BindingTypes;
 import org.frankframework.stream.Message;
 
 public class CmisDeleteAction extends CmisSenderTestBase {
 	private static final String ALLOWED_SELECTOR = "<cmis><id>ALLOWED</id></cmis>";
 	private static final String NOT_ALLOWED_SELECTOR = "<cmis><id>NOT_ALLOWED</id></cmis>";
-	private static final String NOT_FOUND_SELECTOR = "<cmis><id>NOT_FOUND</id></cmis>";
+	private static final Message NOT_FOUND_SELECTOR = new Message("<cmis><id>NOT_FOUND</id></cmis>");
 
 	@Test
 	public void canConfigure() {
@@ -52,10 +54,12 @@ public class CmisDeleteAction extends CmisSenderTestBase {
 	public void objectNotFound() throws Exception {
 		sender.setBindingType(BindingTypes.BROWSER);
 		sender.setAction(CmisSender.CmisAction.DELETE);
-		sender.setResultOnNotFound("document-not-found");
 		sender.configure();
 
-		Message result = sendMessage(NOT_FOUND_SELECTOR);
-		assertThat(result.asString(), Matchers.startsWith("document-not-found"));
+		SenderResult result = sender.sendMessage(NOT_FOUND_SELECTOR, session);
+
+		assertTrue(Message.isNull(result.getResult()));
+		assertThat(result.getErrorMessage(), Matchers.startsWith("document with id ["));
+		assertThat(result.getErrorMessage(), Matchers.endsWith("] not found"));
 	}
 }
