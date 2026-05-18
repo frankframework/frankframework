ba5449320d73693c978a4e469c1dd084d18d6d08
Niels Meijer <nielsmeijer@hotmail.com>
2026-05-18 09:50:02 +0200

    Fixes the line numbers in included files (#10817)

--- core/src/main/java/org/frankframework/xml/SaxException.java
+++ core/src/main/java/org/frankframework/xml/SaxException.java
@@ -65,10 +65,10 @@ public class SaxException extends SAXException {
 	}
 
 	public static SAXException createSaxException(String message, Locator locator, Exception e) {
-		if (locator!=null) {
-			// prefer this solution of creating a SaxException with locatin info in the messgage over creating
-			// a SaxParseException, because that causes the location info to be duplicated in a combined errormessage
-			return new SaxException(getLocatedMessage(locator,message), e);
+		if (locator != null) {
+			// prefer this solution of creating a SaxException with location info in the message over creating
+			// a SaxParseException, because that causes the location info to be duplicated in a combined error message
+			return new SaxException(getLocatedMessage(locator, message), e);
 		}
 		return new SaxException(message, e);
 	}
