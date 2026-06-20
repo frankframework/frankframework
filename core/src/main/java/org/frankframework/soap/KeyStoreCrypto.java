8f7c831b80d010059d37558069b11dadf2d5cbe7
Niels Meijer <nielsmeijer@hotmail.com>
2026-06-20 11:18:48 +0200

    Add copyright notice

--- core/src/main/java/org/frankframework/soap/KeyStoreCrypto.java
+++ core/src/main/java/org/frankframework/soap/KeyStoreCrypto.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2026 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.frankframework.soap;
 
 import java.io.IOException;
@@ -50,7 +65,9 @@ import lombok.extern.log4j.Log4j2;
 
 /**
  * A Crypto implementation based on two Java KeyStore objects, one being the keystore, and one
- * being the truststore.
+ * being the truststore. When no truststore is provided the defaut (CACERTS) is used.
+ * 
+ * @author Niels Meijer
  */
 @Log4j2
 public class KeyStoreCrypto extends CryptoBase {
