78003c39421e53bbb211c7df7b173a86a4e90308
Sergi Philipsen <philipsen.sergi@gmail.com>
2026-07-15 14:51:06 +0200

    Add end-to-end tests for combobox interactions and improve pipeline (#11281)

--- console/frontend/src/main/frontend/cypress/support/e2e.ts
+++ console/frontend/src/main/frontend/cypress/support/e2e.ts
@@ -1,25 +1,9 @@
-// ***********************************************************
-// This example support/e2e.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import './commands';
 
-// When a command from ./commands is ready to use, import with `import './commands'` syntax
-// import './commands';
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // ignore monaco-editor uncaught exceptions (thanks microsoft)
-  if (err.name === 'TypeError' && err.message === 'Property descriptor must be an object, got undefined') {
+Cypress.on('uncaught:exception', (error, _runnable) => {
+  // Ignore monaco-editor uncaught exceptions (thanks Microsoft)
+  if (error.name === 'TypeError' && error.message === 'Property descriptor must be an object, got undefined') {
     return false;
   }
   return;
-})
+});
