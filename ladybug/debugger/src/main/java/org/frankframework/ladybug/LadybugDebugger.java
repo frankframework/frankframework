79ff496a31fa01a8a0cdaf4894e997e3720b4bc5
Martijn Dirkse <mhdirkse@pm.me>
2026-07-23 12:40:01 +0200

    Bump ladybug to 4.1-20260723.104852 - todo: update frontend

--- ladybug/debugger/src/main/java/org/frankframework/ladybug/LadybugDebugger.java
+++ ladybug/debugger/src/main/java/org/frankframework/ladybug/LadybugDebugger.java
@@ -21,6 +21,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.logging.log4j.Logger;
+
+import org.frankframework.util.AppConstants;
+
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,6 +84,8 @@ public class LadybugDebugger implements ApplicationContextAware, ApplicationList
 	@Override
 	public void afterPropertiesSet() {
 		if(applicationContext.containsBean(LADYBUG_TESTTOOL_NAME)) {
+			String instanceName = AppConstants.getInstance().getProperty("instance.name");
+			testtool.setApplication(instanceName);
 			testtool = applicationContext.getBean(LADYBUG_TESTTOOL_NAME, TestTool.class);
 			testtool.setDebugger(this);
 			log.info("configuring debugger on TestTool [{}]", testtool);
