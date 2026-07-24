d8bd9aa3d29aea9caeca5650ebb903c169f02cde
Niels Meijer <nielsmeijer@hotmail.com>
2026-07-24 13:58:22 +0200

    Pff

--- dbms/src/main/java/org/frankframework/dbms/DbmsException.java
+++ dbms/src/main/java/org/frankframework/dbms/DbmsException.java
@@ -16,9 +16,6 @@
 package org.frankframework.dbms;
 
 public class DbmsException extends JdbcException {
-	public DbmsException() {
-		super();
-	}
 
 	public DbmsException(String message) {
 		super(message);
