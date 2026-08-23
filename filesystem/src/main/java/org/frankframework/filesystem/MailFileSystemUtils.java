fb0c6c41b5ae15b3574c016f6d3e0c16e20a7e9b
Niels Meijer <nielsmeijer@hotmail.com>
2026-08-23 12:42:08 +0200

    Validate Content-Disposition and MailMessage replyTo field (#11486)

--- filesystem/src/main/java/org/frankframework/filesystem/MailFileSystemUtils.java
+++ filesystem/src/main/java/org/frankframework/filesystem/MailFileSystemUtils.java
@@ -140,8 +140,8 @@ public class MailFileSystemUtils {
 		if (StringUtils.isEmpty(replyAddressFields)) {
 			return null;
 		}
-		String result;
 		for(String field:replyAddressFields.split(",")) {
+			String result;
 			if (null != (result = getValidAddressFromHeader(field, headers))) {
 				return result;
 			}
---
+++ filesystem/src/test/java/org/frankframework/filesystem/exchange/MailMessageMapperTest.java
@@ -0,0 +1,91 @@
+/*
+   Copyright 2026 WeAreFrank!
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.frankframework.filesystem.exchange;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import org.frankframework.util.JacksonUtils;
+
+public class MailMessageMapperTest {
+
+	/**
+	 * Users reported an issue that mapping failed with a `replyTo` set in a received e-mail. This test ensures that an actual captured JSON response
+	 * can be parsed into a MailMessage.
+	 */
+	@Test
+	void testAllLongFieldsCanExceedIntegerMaxValue() {
+		String json = """
+				{
+				  "id": "dummy-id",
+				  "createdDateTime": "2026-08-19T09:26:42Z",
+				  "lastModifiedDateTime": "2026-08-19T09:26:42Z",
+				  "changeKey": "dummy",
+				  "categories": [],
+				  "receivedDateTime": "2026-08-19T09:26:42Z",
+				  "sentDateTime": "2026-08-19T09:26:42Z",
+				  "hasAttachments": false,
+				  "internetMessageId": "fake-id",
+				  "subject": "rawMessageFile",
+				  "bodyPreview": "Test Message Contents",
+				  "importance": "normal",
+				  "parentFolderId": "fake-parent-id",
+				  "conversationId": "fake-conversation-id",
+				  "conversationIndex": "fake-idx",
+				  "isDeliveryReceiptRequested": false,
+				  "isReadReceiptRequested": false,
+				  "isRead": true,
+				  "isDraft": true,
+				  "webLink": "https://example.com/exampleMessageLink",
+				  "inferenceClassification": "focused",
+				  "body": {
+				    "contentType": "text",
+				    "content": "Test Message Contents"
+				  },
+				  "toRecipients": [
+				    {
+				      "emailAddress": {
+				        "name": "Sergi Philipsen",
+				        "address": "sergi@frankframework.org"
+				      }
+				    }
+				  ],
+				  "ccRecipients": [],
+				  "bccRecipients": [],
+				  "replyTo": [
+				    {
+				      "emailAddress": {
+				        "name": "iaf-test-mailbox",
+				        "address": "frank-test-mailbox@frankframework.org"
+				      }
+				    }
+				  ],
+				  "flag": {
+				    "flagStatus": "notFlagged"
+				  }
+				}
+				""";
+
+		MailMessage mailMessage = JacksonUtils.convertToDTO(json, MailMessage.class);
+
+		MailMessage.EmailAddress recipient = mailMessage.getToRecipients().get(0);
+		assertEquals("Sergi Philipsen <sergi@frankframework.org>", recipient.get());
+
+		MailMessage.EmailAddress replyTo = mailMessage.getReplyTo().get(0);
+		assertEquals("iaf-test-mailbox <frank-test-mailbox@frankframework.org>", replyTo.get());
+	}
+}
