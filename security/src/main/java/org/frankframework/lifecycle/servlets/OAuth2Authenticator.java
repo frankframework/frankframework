821e61305bb895ea93f732dbc12579c259a9126a
evandongen <dongenevan@gmail.com>
2026-08-27 14:36:45 +0200

    Move shared fields from OAuth2 authenticators to AbstractOAuth2Authenticator

--- security/src/main/java/org/frankframework/lifecycle/servlets/OAuth2Authenticator.java
+++ security/src/main/java/org/frankframework/lifecycle/servlets/OAuth2Authenticator.java
@@ -84,7 +84,7 @@ import org.frankframework.util.StringUtil;
  * @see <a href="https://docs.spring.io/spring-security/reference/servlet/oauth2/client/authorization-grants.html#oauth2Client-auth-code-redirect-uri">Spring Security OAuth2 Authorization Grants</a>
  * @see OAuth2AuthorizationRequestRedirectFilter#DEFAULT_AUTHORIZATION_REQUEST_BASE_URI
  */
-public class OAuth2Authenticator extends AbstractServletAuthenticator {
+public class OAuth2Authenticator extends AbstractOAuth2Authenticator {
 
 	/**
 	 * The scopes to request from the OAuth2 provider.
@@ -112,61 +112,12 @@ public class OAuth2Authenticator extends AbstractServletAuthenticator {
 	private @Setter String tokenUri;
 
 	/**
-	 * The URI of the JSON Web Key (JWK) set containing the public keys used to verify any JWT token issued by the authorization server.
-	 * <p>
-	 * e.g. {@code https://www.googleapis.com/oauth2/v3/certs}
-	 * </p>
-	 */
-	private @Setter String jwkSetUri;
-
-	/**
-	 * The issuer identifier URI of the authorization server. This is used to validate the issuer claim in ID tokens.
-	 * <p>
-	 * e.g. {@code https://accounts.google.com}
-	 * </p>
-	 */
-	private @Setter String issuerUri;
-
-	/**
 	 * The base URL used to build the complete redirect URI. Must be an absolute URL for proper OAuth2 flow.
 	 * <p>
 	 * e.g. external absolute URL (must start with {@code http(s)://})
 	 * </p>
 	 */
 	private @Setter String baseUrl;
-
-	/**
-	 * The URI of the user info endpoint used to retrieve information about the authenticated user.
-	 * <p>
-	 * e.g. {@code https://www.googleapis.com/oauth2/v3/userinfo}
-	 * </p>
-	 */
-	private @Setter String userInfoUri;
-
-	/**
-	 * The attribute name used to extract the username from the OAuth2 user information or JWT token.
-	 * Different OAuth2 providers may use different attribute names to identify the user.
-	 * <p>
-	 * Common values include:
-	 * <ul>
-	 *   <li>{@code sub} - The subject identifier</li>
-	 *   <li>{@code email} - The user's email address</li>
-	 *   <li>{@code preferred_username} - The user's preferred username</li>
-	 * </ul>
-	 * </p>
-	 */
-	private @Setter String userNameAttributeName;
-
-	/**
-	 * <p>The claim name in the JWT token that contains the authorities of the user.
-	 * Defaults to any of {@code JwtGrantedAuthoritiesConverter#WELL_KNOWN_AUTHORITIES_CLAIM_NAMES} when this value is not set.</p>
-	 * <p>For keycloak, "realm_access.roles" is the standard claim, this is a 'nested' value. When we encounter a dot (.) in the claim name,
-	 * we assume it is a nested claim and use the custom mapper.</p>
-	 *
-	 * @ff.tip can only contain one dot (.) to indicate a nested claim, e.g. "realm_access.roles".
-	 */
-	@Setter
-	private String authoritiesClaimName;
 
 	/**
 	 * The client ID to use for the OAuth2 provider.
