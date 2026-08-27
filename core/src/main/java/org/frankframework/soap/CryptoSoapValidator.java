cd4502d5ee59c182127df49e9a6a09f4860f711d
Niels Meijer <nielsmeijer@hotmail.com>
2026-08-27 17:32:15 +0200

    Poeh!

--- core/src/main/java/org/frankframework/soap/CryptoSoapValidator.java
+++ core/src/main/java/org/frankframework/soap/CryptoSoapValidator.java
@@ -17,12 +17,17 @@ package org.frankframework.soap;
 
 import java.io.IOException;
 import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 import jakarta.xml.soap.SOAPException;
 
 import org.apache.wss4j.common.ext.WSSecurityException;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import org.frankframework.configuration.ConfigurationException;
 import org.frankframework.core.PipeLineSession;
@@ -39,7 +44,16 @@ public class CryptoSoapValidator extends SoapValidator implements HasKeystore {
 	private CredentialFactory certificateCf;
 	private KeyStore keystore;
 
+	private @Getter @Setter SoapUtils.KeyEncryptionAlgorithm keyEncryptionAlgorithm = SoapUtils.KeyEncryptionAlgorithm.RSA_OAEP;
+	private @Getter @Setter SoapUtils.DataEncryptionAlgorithm dataEncryptionAlgorithm = SoapUtils.DataEncryptionAlgorithm.AES_256;
+	private @Getter @Setter boolean includeCertificateInMessage = true;
+	private @Getter @Setter SoapUtils.KeyIdentifierType keyIdentifier = SoapUtils.KeyIdentifierType.X509_KEY_IDENTIFIER;
+	private @Getter @Setter int ttl = 300;
+	private @Getter @Setter SoapUtils.DigestAlgorithm digestAlgorithm = SoapUtils.DigestAlgorithm.SHA256;
+	private @Getter @Setter SoapUtils.SignatureAlgorithm signatureAlgorithm = SoapUtils.SignatureAlgorithm.RSA_SHA256;
+
 	private @Getter KeystoreConfiguration keystoreConfiguration;
+	private SecretKey symmetricKey;
 
 	@Override
 	public void configure() throws ConfigurationException {
@@ -51,12 +65,31 @@ public class CryptoSoapValidator extends SoapValidator implements HasKeystore {
 		} catch (EncryptionException e) {
 			throw new ConfigurationException("unable to open keystore", e);
 		}
+
+		try {
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(256);
+			symmetricKey = keyGen.generateKey();
+		} catch (NoSuchAlgorithmException e) {
+			throw new ConfigurationException("unable to generate symmetric key", e);
+		}
 	}
 
 	@Override
 	public PipeRunResult doPipe(Message input, PipeLineSession session, boolean responseMode, String messageRoot) throws PipeRunException {
 		if (responseMode) {
-			return super.doPipe(input, session, true, messageRoot);
+			try {
+				PipeRunResult pipeRunResult = super.doPipe(input, session, true, messageRoot);
+
+				Message validated = pipeRunResult.getResult();
+				Message encrypted = SoapUtils.encryptMessage(validated, keystore, getKeystoreAlias(), symmetricKey, includeCertificateInMessage, keyIdentifier, digestAlgorithm, keyEncryptionAlgorithm, dataEncryptionAlgorithm, ttl);
+				Message signed = SoapUtils.signMessage(encrypted, keystore, getKeystoreAlias(), certificateCf.getPassword(), includeCertificateInMessage, keyIdentifier, digestAlgorithm, signatureAlgorithm, ttl);
+
+				pipeRunResult.setResult(signed);
+				return pipeRunResult;
+			} catch (SOAPException | IOException | WSSecurityException e) {
+				throw new PipeRunException(this, "unable to verify and decrypt SOAP message", e);
+			}
 		}
 
 		try {
@@ -72,5 +105,4 @@ public class CryptoSoapValidator extends SoapValidator implements HasKeystore {
 	public void setKeystoreConfiguration(KeystoreConfiguration keystoreConfiguration) {
 		this.keystoreConfiguration = keystoreConfiguration;
 	}
-
 }
