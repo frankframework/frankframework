ef6c09ccf6cfc8cddcd984d5038b0f6378551e7e
Niels Meijer <nielsmeijer@hotmail.com>
2026-08-29 17:54:27 +0200

    Pff

--- core/src/main/java/org/frankframework/soap/CryptoSoapValidator.java
+++ core/src/main/java/org/frankframework/soap/CryptoSoapValidator.java
@@ -18,6 +18,7 @@ package org.frankframework.soap;
 import java.io.IOException;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -28,6 +29,7 @@ import org.apache.wss4j.common.ext.WSSecurityException;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
 
 import org.frankframework.configuration.ConfigurationException;
 import org.frankframework.core.PipeLineSession;
@@ -44,19 +46,45 @@ public class CryptoSoapValidator extends SoapValidator implements HasKeystore {
 	private CredentialFactory certificateCf;
 	private KeyStore keystore;
 
+	/**
+	 * Add the certificate to the signed or encrypted message.
+	 */
+	private @Getter @Setter boolean includeCertificateInMessage = true;
+
+	/**
+	 * Add a restriction on when the message must be read / verified by.
+	 * If this time has exceeded, the message is rejected.
+	 */
+	private @Getter @Setter int ttl = 300;
+
+	/**
+	 * Remove the `WSSE:Security` part from the SOAP:Header.
+	 */
+	private @Getter @Setter boolean removeSecurityHeader = true;
+
 	private @Getter @Setter SoapUtils.KeyEncryptionAlgorithm keyEncryptionAlgorithm = SoapUtils.KeyEncryptionAlgorithm.RSA_OAEP;
 	private @Getter @Setter SoapUtils.DataEncryptionAlgorithm dataEncryptionAlgorithm = SoapUtils.DataEncryptionAlgorithm.AES_256;
-	private @Getter @Setter boolean includeCertificateInMessage = true;
 	private @Getter @Setter SoapUtils.KeyIdentifierType keyIdentifier = SoapUtils.KeyIdentifierType.X509_KEY_IDENTIFIER;
-	private @Getter @Setter int ttl = 300;
 	private @Getter @Setter SoapUtils.DigestAlgorithm digestAlgorithm = SoapUtils.DigestAlgorithm.SHA256;
 	private @Getter @Setter SoapUtils.SignatureAlgorithm signatureAlgorithm = SoapUtils.SignatureAlgorithm.RSA_SHA256;
 
 	private @Getter KeystoreConfiguration keystoreConfiguration;
 	private SecretKey symmetricKey;
+	private List<Operation> operations = List.of();
+
+	public enum Operation {
+		SIGN, ENCRYPT, VERIFY, DECRYPT
+	}
 
 	@Override
 	public void configure() throws ConfigurationException {
+		if (operations.isEmpty()) {
+			throw new ConfigurationException("no operations specified");
+		}
+		if (isConfiguredForMixedValidation()) {
+			throw new ConfigurationException("this validator does not support input/output processing in '1' element, configure an OutputValidator explicitly");
+		}
+
 		super.configure();
 
 		certificateCf = new CredentialFactory(getKeystoreAliasAuthAlias(), null, getKeystoreAliasPassword());
@@ -66,39 +94,52 @@ public class CryptoSoapValidator extends SoapValidator implements HasKeystore {
 			throw new ConfigurationException("unable to open keystore", e);
 		}
 
-		try {
-			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-			keyGen.init(256);
-			symmetricKey = keyGen.generateKey();
-		} catch (NoSuchAlgorithmException e) {
-			throw new ConfigurationException("unable to generate symmetric key", e);
+		if (operations.contains(Operation.ENCRYPT)) {
+			try {
+				KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+				keyGen.init(256);
+				symmetricKey = keyGen.generateKey();
+			} catch (NoSuchAlgorithmException e) {
+				throw new ConfigurationException("unable to generate symmetric key", e);
+			}
 		}
 	}
 
+	@SneakyThrows
 	@Override
 	public PipeRunResult doPipe(Message input, PipeLineSession session, boolean responseMode, String messageRoot) throws PipeRunException {
-		if (responseMode) {
-			try {
-				PipeRunResult pipeRunResult = super.doPipe(input, session, true, messageRoot);
-
-				Message validated = pipeRunResult.getResult();
-				Message encrypted = SoapUtils.encryptMessage(validated, keystore, getKeystoreAlias(), symmetricKey, includeCertificateInMessage, keyIdentifier, digestAlgorithm, keyEncryptionAlgorithm, dataEncryptionAlgorithm, ttl);
-				Message signed = SoapUtils.signMessage(encrypted, keystore, getKeystoreAlias(), certificateCf.getPassword(), includeCertificateInMessage, keyIdentifier, digestAlgorithm, signatureAlgorithm, ttl);
-
-				pipeRunResult.setResult(signed);
-				return pipeRunResult;
-			} catch (SOAPException | IOException | WSSecurityException e) {
-				throw new PipeRunException(this, "unable to verify and decrypt SOAP message", e);
-			}
-		}
-
+		Message result = input;
 		try {
-			Message result = SoapUtils.verifyMessage(input, keystore, getKeystoreAlias(), certificateCf.getPassword(), false);
-			Message result2 = SoapUtils.decryptMessage(result, keystore, getKeystoreAlias(), certificateCf.getPassword(), true);
-			return super.doPipe(result2, session, false, messageRoot);
+			if (operations.contains(Operation.VERIFY)) {
+				result = SoapUtils.verifyMessage(result, keystore, getKeystoreAlias(), certificateCf.getPassword(), false);
+			}
+			if (operations.contains(Operation.DECRYPT)) {
+				result = SoapUtils.decryptMessage(result, keystore, getKeystoreAlias(), certificateCf.getPassword(), true);
+			}
+
+			PipeRunResult pipeRunResult = super.doPipe(result, session, responseMode, messageRoot);
+
+			Message validationResult = pipeRunResult.getResult();
+			if (operations.contains(Operation.ENCRYPT)) {
+				validationResult = SoapUtils.encryptMessage(validationResult, keystore, getKeystoreAlias(), symmetricKey, includeCertificateInMessage, keyIdentifier, digestAlgorithm, keyEncryptionAlgorithm, dataEncryptionAlgorithm, ttl);
+			}
+			if (operations.contains(Operation.SIGN)) {
+				validationResult = SoapUtils.signMessage(validationResult, keystore, getKeystoreAlias(), certificateCf.getPassword(), includeCertificateInMessage, keyIdentifier, digestAlgorithm, signatureAlgorithm, ttl);
+			}
+			pipeRunResult.setResult(validationResult);
+
+			return pipeRunResult;
 		} catch (SOAPException | IOException | WSSecurityException e) {
-			throw new PipeRunException(this, "unable to verify and decrypt SOAP message", e);
+			throw new PipeRunException(this, "unable to process SOAP crypto operations: " + operations, e);
 		}
+	}
+
+	/**
+	 * Which operations must be executed.
+	 * <p>It is not possible to use this validator as a Mixed input-output validator.</p>
+	 */
+	public void setOperations(Operation... operations) {
+		this.operations = List.of(operations);
 	}
 
 	@Override
