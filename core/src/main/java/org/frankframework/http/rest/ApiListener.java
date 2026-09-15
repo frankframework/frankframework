db829af4a87a8b7dd0c7b51235c0ba8ce743d548
Tim van der Leeuw <tim@wearefrank.nl>
2026-09-15 08:57:03 +0200

    Add multipartBodyName to allowedParameters (#11659)
    (cherry picked from commit dba0f3ef66cfb62bcf30402cb29a1ccfea957c21)

--- core/src/main/java/org/frankframework/http/rest/ApiListener.java
+++ core/src/main/java/org/frankframework/http/rest/ApiListener.java
@@ -132,7 +132,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 	private @Getter String headerParams = null;
 	private @Getter String contentDispositionHeaderSessionKey;
 	private @Getter String charset = null;
-	private @Getter @NonNull Set<String> allowedParameterSet = Set.of();
+	private @Getter @NonNull Set<String> allowedParameterSet = new HashSet<>(); // Set should be mutable
 	private @Getter Boolean allowAllParams = null;
 
 	// for jwt validation
@@ -229,6 +229,11 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 			ConfigurationWarnings.add(this, log, "[responseMtomContentTransferEncoding] should only be set when [responseType] is [MTOM]");
 		}
 
+		if (StringUtils.isNotEmpty(multipartBodyName)) {
+			allowedParameterSet.add(multipartBodyName);
+		}
+
+		// Make sure allowedParameters or allowAll is explicitly set
 		// Check that none of configured parameters or path-variables matches any of the reserved names.
 		if (allowedParameterSet.isEmpty() && allowAllParams == null) {
 			ConfigurationWarnings.add(this, log, "SECURITY RISK: All path parameters and query parameters will be copied into the session. Specify [allowedParameters] for your pipeline, or explicitly set [allowAllParams] to 'true'.", SuppressKeys.UNSAFE_ATTRIBUTE_SUPPRESS_KEY);
@@ -547,7 +552,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 	 * @param paramWhitelist Comma-separated list of allowed HTTP parameters.
 	 */
 	public void setAllowedParameters(@Nullable String paramWhitelist) {
-		this.allowedParameterSet = StringUtil.splitToStream(paramWhitelist).collect(Collectors.toSet());
+		this.allowedParameterSet = StringUtil.splitToStream(paramWhitelist).collect(Collectors.toCollection(HashSet::new)); // The set needs to be mutable while in configuration()
 	}
 
 	/**
