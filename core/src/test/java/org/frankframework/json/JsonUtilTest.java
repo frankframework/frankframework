b85c4b8696af57cb69f19417bc0739e249d32708
Tim van der Leeuw <tnleeuw@gmail.com>
2026-09-15 18:21:41 +0200

    Fix JSON for JsonPath evaluation work when BOM is present (#11678)

--- core/src/test/java/org/frankframework/json/JsonUtilTest.java
+++ core/src/test/java/org/frankframework/json/JsonUtilTest.java
@@ -1,10 +1,20 @@
 package org.frankframework.json;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.ByteOrderMark;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Test;
 
+import com.jayway.jsonpath.JsonPath;
+
+import org.frankframework.stream.Message;
 import org.frankframework.testutil.TestAssertions;
 import org.frankframework.testutil.TestFileUtils;
 import org.frankframework.util.StreamUtil;
@@ -29,4 +39,44 @@ class JsonUtilTest {
 		TestAssertions.assertEqualsIgnoreCRLF(expectedString, JsonUtil.jsonPretty(inputString));
 	}
 
+	@Test
+	public void testJsonPathWithBom() throws Exception {
+		// Arrange
+		Message input = createInputWithBom();
+		JsonPath jsonPath = JsonUtil.compileJsonPath("$.root.a");
+		assertNotNull(jsonPath);
+
+		// Act
+		Message result = JsonUtil.evaluateJsonPath(jsonPath, input);
+
+		// Assert
+		assertEquals("v1", result.asString());
+	}
+
+	private static @NonNull Message createInputWithBom() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.writeBytes(ByteOrderMark.UTF_8.getBytes());
+		baos.writeBytes("""
+				{
+				  "root": {
+				    "a": "v1"
+				  }
+				}
+				""".getBytes(StandardCharsets.UTF_8));
+		return Message.asMessage(baos.toByteArray());
+	}
+
+	@Test
+	public void testJsonPathSingleValueWithBom() throws Exception {
+		// Arrange
+		Message input = createInputWithBom();
+		JsonPath jsonPath = JsonUtil.compileJsonPath("$.root.a");
+		assertNotNull(jsonPath);
+
+		// Act
+		String result = JsonUtil.evaluateJsonPathToSingleValue(jsonPath, input);
+
+		// Assert
+		assertEquals("v1", result);
+	}
 }
