053d74f61ff2acf9e8719bcad56ca3bfc40a5f98
Erik <dongenevan@gmail.com>
2026-09-23 13:05:59 +0200

    Allow all params should work when multipart body name is set

--- core/src/main/java/org/frankframework/http/rest/ApiListener.java
+++ core/src/main/java/org/frankframework/http/rest/ApiListener.java
@@ -175,6 +175,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 		if (StringUtils.isEmpty(getUriPattern()))
 			throw new ConfigurationException("uriPattern cannot be empty");
 
+		// getCleanPattern can only be null if UriPattern is empty, which is already checked above.
 		if (!isValidUriPattern(getCleanPattern())) {
 			throw new ConfigurationException("uriPattern contains invalid wildcards");
 		}
@@ -189,6 +190,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 			if (hasMethod(HttpMethod.DELETE))
 				throw new ConfigurationException("cannot set consumes attribute when using method [DELETE]");
 		}
+
 		if (getConsumes() == MediaTypes.DETECT) {
 			throw new ConfigurationException("cannot set consumes attribute to [DETECT]");
 		}
@@ -210,6 +212,7 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 		if (responseType == null) {
 			responseType = HttpEntityType.BINARY;
 		}
+
 		responseEntityBuilder = HttpEntityFactory.Builder.create()
 				.entityType(responseType)
 				.multipartXmlSessionKey(responseMultipartXmlSessionKey)
@@ -225,11 +228,13 @@ public class ApiListener extends PushingListenerAdapter implements HasPhysicalDe
 				ConfigurationWarnings.add(this, log, "[responseResultBodyPartName] should only be set when [responseType] is [MTOM] or [FORMDATA]");
 			}
 		}
+
 		if (responseType != HttpEntityType.MTOM && StringUtils.isNotBlank(responseMtomContentTransferEncoding)) {
 			ConfigurationWarnings.add(this, log, "[responseMtomContentTransferEncoding] should only be set when [responseType] is [MTOM]");
 		}
 
-		if (StringUtils.isNotEmpty(multipartBodyName)) {
+		// Only add if allow all params is not set because that relies on an empty set of allowed parameters.
+		if (StringUtils.isNotEmpty(multipartBodyName) && !isAllowAllParams()) {
 			allowedParameterSet.add(multipartBodyName);
 		}
 
